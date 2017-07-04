1ad8bc894e4f44abbf3804f7b5960a266d4b8fbd
Guy <gpharwood@gmail.com>
2017-07-04 17:30:22 +0100

    test script for each app

--- pupil/test-old.sh
+++ pupil/test-old.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-npm start &
+node server.js &
 PID=$!
 
 MSG='node is running under process '
@@ -10,7 +10,7 @@ echo $MSG
 cd test
 gem install bundler
 bundle install
-cucumber
+BASE_URL=http://localhost:3000 cucumber
 CUCUMBER_EXIT_CODE=$?
 
 kill -9 $PID
