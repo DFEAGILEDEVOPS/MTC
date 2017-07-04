80235f8d7134218ad7e75ce99a1c8b71dfc1e2f9
Guy <gpharwood@gmail.com>
2017-07-04 10:31:30 +0100

    only activate newrelic when env var present

--- admin/newrelic.js
+++ admin/newrelic.js
@@ -6,21 +6,26 @@
  * See lib/config.default.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
-exports.config = {
-  /**
-   * Array of application names.
-   */
-  app_name: ['live-check-admin'],
-  /**
-   * Your New Relic license key.
-   */
-  license_key: 'f6f0a3e3940ba9f6120989d94e9dc39365523949',
-  logging: {
+
+if (process.env.NEW_RELIC_LICENSE_KEY) {
+  exports.config = {
     /**
-     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
-     * issues with the agent, 'info' and higher will impose the least overhead on
-     * production applications.
+     * Array of application names.
      */
-    level: 'info'
+    app_name: ['live-check-admin'],
+    /**
+     * Your New Relic license key.
+     */
+    license_key: process.env.NEW_RELIC_LICENSE_KEY,
+    logging: {
+      /**
+       * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+       * issues with the agent, 'info' and higher will impose the least overhead on
+       * production applications.
+       */
+      level: 'info'
+    }
   }
 }
+
+
