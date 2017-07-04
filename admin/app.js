f5c8962792d66c84b486bd420c655d1c17834a96
George Chatzigiannis <geohatzigiannis@gmail.com>
2017-07-04 17:23:17 +0100

    Refactor admin app routers (#10)
    * Delete spec file that was unused
    
    * Add fixture files used by tests
    
    * Add data migrations with test data
    
    * Add sample questions
    
    * Bump version to v0.2.0
    
    * Bump version to v0.2.0
    
    * Update docs
    
    * Update .gitignore
    
    * Update user model
    
    * Update .gitignore
    
    * number of questions are now 10
    
    * changing the expectation
    
    * changing the expectation for warm-up
    
    * Add newlines at end of file
    
    * fixed tests
    
    * Change let to const
    
    * Split out route logic to controllers and relocate helper functions
    
    * Standard JS compliance
    
    * Same principle for route index
    
    * Refactor route helper to service
    
    * Fix comment
    
    * Remove next call from service and include JSDoc changes

--- admin/app.js
+++ admin/app.js
@@ -31,7 +31,7 @@ autoIncrement.initialize(mongoose.connection)
 
 const index = require('./routes/index')
 const testDeveloper = require('./routes/test-developer')
-const school = require('./routes/school')
+const admin = require('./routes/admin')
 
 const app = express()
 const helpers = require('./helpers')(app)
@@ -144,7 +144,7 @@ app.use(function (req, res, next) {
 
 app.use('/', index)
 app.use('/test-developer', testDeveloper)
-app.use('/school', school)
+app.use('/school', admin)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
