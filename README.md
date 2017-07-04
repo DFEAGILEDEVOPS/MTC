7d0b25fee34612084bcb96b5c1e7bf7112882077
Jon Shipley <jon-shipley@users.noreply.github.com>
2017-07-04 15:08:45 +0100

    Feature/upgrade pupil app to async (#6)
    * Change let's to const's
    
    * update post to /feedback to async
    
    * fix lint errors
    
    * add code-style badge
    
    * Add eslint config file for codacy.com

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![Build Status](https://travis-ci.org/DFEAGILEDEVOPS/MTC.svg?branch=master)](https://travis-ci.org/DFEAGILEDEVOPS/MTC)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com)
 
 # Multiplication Tables Check (MTC) Project
 
--- pupil/authentication/passport-custom-5-digits.js
+++ pupil/authentication/passport-custom-5-digits.js
@@ -1,102 +1,100 @@
-'use strict';
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-const School = require('../models/school');
-const Pupil = require('../models/pupil');
-const LogonEvent = require('../models/logon-event');
+'use strict'
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+const School = require('../models/school')
+const Pupil = require('../models/pupil')
+const LogonEvent = require('../models/logon-event')
 
 // Form fields from the HTML
-const fieldSchoolPin = 'school-pin';
-const fieldPupilPin = 'pupil-pin';
+const fieldSchoolPin = 'school-pin'
+const fieldPupilPin = 'pupil-pin'
 
 module.exports = async function (req, done) {
-
   try {
-    const schoolPin = req.body[fieldSchoolPin];
-    const pupilPin = req.body[fieldPupilPin];
+    const schoolPin = req.body[fieldSchoolPin]
+    const pupilPin = req.body[fieldPupilPin]
 
     // Setup the logonEvent
-    let logonEvent = new LogonEvent;
-    logonEvent.sessionId = req.session.id;
-    logonEvent.schoolPin = req.body[fieldSchoolPin];
-    logonEvent.pupilPin = req.body[fieldPupilPin];
+    let logonEvent = new LogonEvent()
+    logonEvent.sessionId = req.session.id
+    logonEvent.schoolPin = req.body[fieldSchoolPin]
+    logonEvent.pupilPin = req.body[fieldPupilPin]
 
     // You can't log in without *both* pins
     if (!(schoolPin && pupilPin)) {
-      await saveFailedLogin(logonEvent, req, null, null);
-      return done(null, false);
+      await saveFailedLogin(logonEvent, req, null, null)
+      return done(null, false)
     }
 
-    const school = await School.findOne({schoolPin}).exec();
+    const school = await School.findOne({schoolPin}).exec()
 
     if (!school) {
       // school not found
-      await saveFailedLogin(logonEvent, req, school, null);
-      return done(null, false);
+      await saveFailedLogin(logonEvent, req, school, null)
+      return done(null, false)
     }
 
     const pupil = await Pupil.findOne({
       school: school._id,
       pin: pupilPin,
       pinExpired: false
-    }).exec();
+    }).exec()
 
     if (!pupil) {
-      await saveFailedLogin(logonEvent, req, school, pupil);
-      return done(null, false);
+      await saveFailedLogin(logonEvent, req, school, pupil)
+      return done(null, false)
     }
 
     // Authenticated
-    await saveSuccessfulLogin(logonEvent, school, pupil);
-    req.session.logonEvent = logonEvent.toJSON();
-    return done(null, pupil);
+    await saveSuccessfulLogin(logonEvent, school, pupil)
+    req.session.logonEvent = logonEvent.toJSON()
+    return done(null, pupil)
   } catch (error) {
-    console.error('Error during authentication: ', error);
-    return done(error);
+    console.error('Error during authentication: ', error)
+    return done(error)
   }
 }
 
-
-function validationErrors(req, sp, pp) {
-  let errorMsgs = [];
+function validationErrors (req, sp, pp) {
+  let errorMsgs = []
 
   if (!req.body[fieldSchoolPin]) {
-    errorMsgs.push('School PIN missing');
+    errorMsgs.push('School PIN missing')
   }
   if (!req.body[fieldPupilPin]) {
-    errorMsgs.push('Pupil PIN missing');
+    errorMsgs.push('Pupil PIN missing')
   }
   if (req.body[fieldSchoolPin] && sp === null) {
-    errorMsgs.push('Invalid school PIN (not in list)');
+    errorMsgs.push('Invalid school PIN (not in list)')
   }
   if (req.body[fieldPupilPin] && pp === null) {
-    errorMsgs.push('Invalid Pupil PIN (not in list)');
+    errorMsgs.push('Invalid Pupil PIN (not in list)')
   }
 
-  return errorMsgs;
+  return errorMsgs
 }
 
-
-async function saveFailedLogin(logonEvent, req, school, pupil) {
-  logonEvent.errorMsg = validationErrors(req, school, pupil);
-  logonEvent.isAuthenticated = false;
+async function saveFailedLogin (logonEvent, req, school, pupil) {
+  logonEvent.errorMsg = validationErrors(req, school, pupil)
+  logonEvent.isAuthenticated = false
   if (school && school._id) {
-    logonEvent.school = school._id;
+    logonEvent.school = school._id
   }
   if (pupil && pupil._id) {
-    logonEvent.pupil = pupil._id;
+    logonEvent.pupil = pupil._id
   }
-  return await logonEvent.save();
+  await logonEvent.save()
+  return logonEvent
 }
 
-async function saveSuccessfulLogin(logonEvent, school, pupil) {
-  logonEvent.isAuthenticated = true;
+async function saveSuccessfulLogin (logonEvent, school, pupil) {
+  logonEvent.isAuthenticated = true
   if (school && school._id) {
-    logonEvent.school = school._id;
+    logonEvent.school = school._id
   }
   if (pupil && pupil._id) {
-    logonEvent.pupil = pupil._id;
+    logonEvent.pupil = pupil._id
   }
-  await logonEvent.save();
-  return logonEvent;
+  await logonEvent.save()
+  return logonEvent
 }
