6922706d65380617d1704d425545c12a7c752742
Guy <gpharwood@gmail.com>
2017-07-04 16:17:08 +0100

    refactor as per Georges comment

--- pupil/app.js
+++ pupil/app.js
@@ -89,7 +89,7 @@ if (process.env.STD_LOG_FILE) {
   const appLog = fs.createWriteStream(process.env.STD_LOG_FILE)
   process.stdout.write = process.stderr.write = appLog.write.bind(appLog)
   process.on('uncaughtException', function (err) {
-    console.error((err && err.stack) ? err.stack : err)
+    console.error((err && err.stack) || err)
   })
 }
 
