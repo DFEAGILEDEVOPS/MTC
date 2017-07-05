2d3503b8c3b4419e5c6b085c374ab9dc18d5023a
Jon Shipley <js@ovix.co.uk>
2017-07-05 16:41:58 +0100

    Add additional coverage for school

--- admin/spec/models/school.spec.js
+++ admin/spec/models/school.spec.js
@@ -3,6 +3,13 @@
 /* global beforeEach, describe, it, expect */
 
 const School = require('../../models/school')
+const sinon = require('sinon')
+require('sinon-mongoose')
+
+// We need to require mongoose which is required by sinon-mongoose in order to override the internal mongoose promise
+// library, which is deprecated.
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
 
 describe('school schema', function () {
   let school
@@ -68,5 +75,33 @@ describe('school schema', function () {
       expect(error.errors.name).toBeDefined()
       done()
     })
+  })
+
+  it('has a method to provide a school pin', async function (done) {
+    expect(typeof School.getUniqueSchoolPin).toBe('function')
+    sinon.mock(School)
+      .expects('findOne')
+      .chain('exec')
+      .resolves(null)
+    const r1 = await School.getUniqueSchoolPin()
+    expect(r1.length).toBe(8)
+    School.findOne.restore()
+    done()
+  })
+
+  it('has a method to provide a school pin - error path', async function (done) {
+    sinon.mock(School)
+      .expects('findOne')
+      .chain('exec')
+      .rejects(new Error('something went wrong'))
+
+    try {
+      await School.getUniqueSchoolPin()
+      done(new Error('failed to catch rejected promise'))
+    } catch (err) {
+      expect(err.message).toBe('something went wrong')
+    }
+    School.findOne.restore()
+    done()
   })
 })
