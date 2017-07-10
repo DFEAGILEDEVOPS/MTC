863d71a99f15b54f9a355da4242ff23e23287ec5
Jon Shipley <js@ovix.co.uk>
2017-07-10 11:42:57 +0100

    Copy unit tests from admin for the pupil and school, as the models are not exactly the same.  Add sinon-* packages for pupil app.

---
+++ pupil/spec/models/pupil.spec.js
@@ -0,0 +1,110 @@
+'use strict'
+
+/* global beforeEach, describe, it, expect, xit */
+
+const Pupil = require('../../models/pupil')
+const moment = require('moment')
+
+describe('Pupil schema', function () {
+  let pupil
+
+  beforeEach(function () {
+    pupil = new Pupil({
+      school: 9999999,
+      upn: 'A999999999999',
+      foreName: 'Jonny',
+      lastName: 'Pupil',
+      middleNames: 'Giles',
+      gender: 'M',
+      // use utc for dob...and ignore the time component
+      dob: moment.utc(20060810, 'YYYYMMDD')
+    })
+  })
+
+  it('validates a correct pupil', function (done) {
+    pupil.validate(error => {
+      expect(error).toBe(null)
+      done()
+    })
+  })
+
+  it('requires a school ref', function (done) {
+    pupil.school = undefined
+    pupil.validate(error => {
+      expect(error.errors.school).toBeDefined()
+      done()
+    })
+  })
+
+  it('requires a forename', function (done) {
+    pupil.foreName = undefined
+    pupil.validate(error => {
+      expect(error.errors.foreName).toBeDefined()
+      done()
+    })
+  })
+
+  it('requires a lastname', function (done) {
+    pupil.lastName = undefined
+    pupil.validate(error => {
+      expect(error.errors.lastName).toBeDefined()
+      done()
+    })
+  })
+
+  it('has an optional middlename', function (done) {
+    pupil.middleNames = undefined
+    pupil.validate(error => {
+      expect(error).toBe(null)
+      done()
+    })
+  })
+
+  it('requires a gender', function (done) {
+    pupil.gender = undefined
+    pupil.validate(error => {
+      expect(error.errors.gender).toBeDefined()
+      done()
+    })
+  })
+
+  it('requires a gender and it must be M or F', function (done) {
+    pupil.gender = 'N'
+    pupil.validate(error => {
+      expect(error.errors.gender).toBeDefined()
+      done()
+    })
+  })
+
+  it('requires a gender and it must be M or F', function (done) {
+    pupil.gender = 'F'
+    pupil.validate(error => {
+      expect(error).toBe(null)
+      done()
+    })
+  })
+
+  it('requires a dob', function (done) {
+    pupil.dob = undefined
+    pupil.validate(error => {
+      expect(error.errors.dob).toBeDefined()
+      done()
+    })
+  })
+
+  it('has a method to retrieve all pupils', function (done) {
+    expect(typeof Pupil.getPupils).toBeDefined()
+    expect(typeof Pupil.getPupils).toBe('function')
+    done()
+  })
+
+  xit('method getPupils returns an object', async function (done) {
+    try {
+      const pupils = await Pupil.getPupils().limit(1).exec()
+      expect(typeof pupils).toEqual('object')
+      done()
+    } catch (error) {
+      done(error)
+    }
+  })
+})
