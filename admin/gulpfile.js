44716be62b73f3dbb1d3b9c5fd036488bc9cf5cb
George Chatzigiannis <george.chatzigiannis@smartfocus.com>
2017-07-10 13:04:04 +0100

    fix spacing on admin gulpfile

--- admin/gulpfile.js
+++ admin/gulpfile.js
@@ -9,78 +9,78 @@ const standard = require('gulp-standard')
 
 // These files will get uglified and packaged into `app.js`
 const jsBundleFiles = [
-    './assets/javascripts/jquery-1.12.4.js',
-    './assets/javascripts/details.polyfill.js',
-    './assets/javascripts/global-scripts.js'
+  './assets/javascripts/jquery-1.12.4.js',
+  './assets/javascripts/details.polyfill.js',
+  './assets/javascripts/global-scripts.js'
 ]
 
 const jsFilesForStandard = [
-    '!node_modules/**/*',
-    '!assets/javascripts/details.polyfill.js',
-    '!assets/javascripts/jquery-1.12.4.js',
-    '!public/govuk_template/javascripts/ie.js',
-    '!public/govuk_template/javascripts/govuk-template.js',
-    '!public/javascripts/app.js',
-    './**/*.js'
+  '!node_modules/**/*',
+  '!assets/javascripts/details.polyfill.js',
+  '!assets/javascripts/jquery-1.12.4.js',
+  '!public/govuk_template/javascripts/ie.js',
+  '!public/govuk_template/javascripts/govuk-template.js',
+  '!public/javascripts/app.js',
+  './**/*.js'
 ]
 
 gulp.task('watch', function () {
-    gulp.watch('./assets/**/*.scss', ['sass'])
-    gulp.watch('./assets/**/*.js', ['bundleJs'])
+  gulp.watch('./assets/**/*.scss', ['sass'])
+  gulp.watch('./assets/**/*.js', ['bundleJs'])
 })
 
 gulp.task('bundleJs', function () {
-    return gulp.src(jsBundleFiles)
-        .pipe(concat('app.js'))
-        .pipe(uglify({
-            compress: {screw_ie8: false},
-            mangle: {screw_ie8: false},
-            output: {screw_ie8: false}
-        }).on('error', function (e) {
-            console.log(e)
-        }))
-        .pipe(gulp.dest('./public/javascripts/'))
+  return gulp.src(jsBundleFiles)
+    .pipe(concat('app.js'))
+    .pipe(uglify({
+      compress: {screw_ie8: false},
+      mangle: {screw_ie8: false},
+      output: {screw_ie8: false}
+    }).on('error', function (e) {
+      console.log(e)
+    }))
+    .pipe(gulp.dest('./public/javascripts/'))
 })
 
 gulp.task('clean', function () {
-    return gulp.src([
-        'public/javascripts/app.js',
-        'public/stylesheets/application.css',
-        'public/stylesheets/application-ie8.css'
-    ], {read: false})
-        .pipe(clean())
+  return gulp.src([
+    'public/javascripts/app.js',
+    'public/stylesheets/application.css',
+    'public/stylesheets/application-ie8.css'
+  ], {read: false})
+    .pipe(clean())
 })
 
 gulp.task('copyImages', function () {
-    gulp
-        .src(['./assets/images/*'])
-        .pipe(gulp.dest('public/images'))
+  gulp
+    .src(['./assets/images/*'])
+    .pipe(gulp.dest('public/images'))
 })
 
 gulp.task('copyPDFs', function () {
-    gulp
-        .src(['./assets/PDFs/*'])
-        .pipe(gulp.dest('public/PDFs'))
+  gulp
+    .src(['./assets/PDFs/*'])
+    .pipe(gulp.dest('public/PDFs'))
 })
 
 gulp.task('standard', function () {
-    return gulp.src(jsFilesForStandard)
-        .pipe(standard())
-        .pipe(standard.reporter('default', {
-            breakOnError: true,
-            quiet: true
-        }))
+  return gulp.src(jsFilesForStandard)
+    .pipe(standard())
+    .pipe(standard.reporter('default', {
+      breakOnError: true,
+      quiet: true
+    }))
 })
 
 gulp.task('realclean', ['clean'], function () {
-    return gulp.src('./node_modules', {read: false})
-        .pipe(clean())
+  return gulp.src('./node_modules', {read: false})
+    .pipe(clean())
 })
 
 gulp.task('build', ['sass', 'bundleJs', 'copyImages', 'copyPDFs'])
 
 gulp.task('sass', function () {
-    return gulp.src('./assets/**/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./public'))
+  return gulp.src('./assets/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./public'))
 })
