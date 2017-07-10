acb2db8d7f575cccba5980d93675152a46d761e7
George Chatzigiannis <george.chatzigiannis@smartfocus.com>
2017-07-10 13:11:46 +0100

    standard fixes for conf

--- test/conf.js
+++ test/conf.js
@@ -1,48 +1,48 @@
 exports.config = {
 
-	// setting to launch tests directly without selenium server
+  // setting to launch tests directly without selenium server
   directConnect: true,
-	// address of running selenium server
+  // address of running selenium server
   seleniumAddress: 'http://localhost:4444/wd/hub',
 
-	// setting time outs
+  // setting time outs
   getPageTimeOut: 200000,
   allScriptsTimeout: 500000,
 
-	// setting framework
+  // setting framework
   framework: 'custom',
   frameworkPath: require.resolve('protractor-cucumber-framework'),
 
-	// setting protractor to ignore uncaught exceptions to take care by protractor-cucumber-framework
+  // setting protractor to ignore uncaught exceptions to take care by protractor-cucumber-framework
   ignoreUncaughtExceptions: true,
 
-	// configuration parameters
+  // configuration parameters
   params: {
     testEnv: 'test'
   },
 
-	// browser to launch tests
+  // browser to launch tests
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-      args: [ '--disable-extensions' ]
+      args: ['--disable-extensions']
     }
   },
 
-	// Specify Test Files
-	//
-	// Define which tests should execute
+  // Specify Test Files
+  //
+  // Define which tests should execute
   specs: ['features/*.feature'],
 
-	// Define which tests should be excluded from execution.
+  // Define which tests should be excluded from execution.
   // 'features/search.feature'
   exclude: [],
 
-	// Set log level and enables colors for log output
+  // Set log level and enables colors for log output
   logLevel: 'verbose',
   coloredLogs: true,
 
-	// arguments to cucumber.js
+  // arguments to cucumber.js
   cucumberOpts: {
     require: [
       'features/support/env.js',
