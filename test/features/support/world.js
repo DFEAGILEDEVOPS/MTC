12b7de324179a365c9828d9a57a26636c3ad7bb6
Raja Sekhar <rajasekhar.you@gmail.com>
2017-07-13 17:29:55 +0100

    update to world - adding attach and parameters for screenshot on failure to work

--- test/features/support/world.js
+++ test/features/support/world.js
@@ -2,11 +2,14 @@
 
 const {defineSupportCode} = require('cucumber')
 
-function CustomWorld () {
+function CustomWorld ({attach, parameters}) {
   this.signIn = require('../../page_objects/admin/signInPage')
   this.landingPage = require('../../page_objects/admin/landingPage')
   this.signInFailure = require('../../page_objects/admin/signInFailure')
   this.config = require('../../data/config.json')[browser.params.testEnv]
+  this.checkSignIn = require('../../page_objects/check/signInPage')
+  this.attach = attach
+  this.parameters = parameters
 }
 
 defineSupportCode(function ({setWorldConstructor}) {
