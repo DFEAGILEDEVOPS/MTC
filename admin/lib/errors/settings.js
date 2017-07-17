4672f792c9824b18a519a1b34cc6766c46192f6c
Paula Molinero <hello@pris54.co.uk>
2017-07-17 12:32:35 +0100

    Updated from one decimal to two allowed.

--- admin/lib/errors/settings.js
+++ admin/lib/errors/settings.js
@@ -1,6 +1,6 @@
 'use strict'
 
 module.exports = {
-  questionTimeLimit: 'Question time limit requires a number between 1 and 60, decimals are valid',
-  loadingTimeLimit: 'Time between questions requires a number between 1 and 5, decimals are valid'
+  questionTimeLimit: 'Question time limit requires a number between 1 and 60, up to two decimals are valid',
+  loadingTimeLimit: 'Time between questions requires a number between 1 and 5, up to two decimals are valid'
 }
