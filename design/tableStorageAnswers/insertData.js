a43ed5a7ef5687386065745b8a4c56d839c43172
Guy <gpharwood@gmail.com>
2017-07-17 17:39:21 +0100

    WIP.  batch too large

--- design/tableStorageAnswers/insertData.js
+++ design/tableStorageAnswers/insertData.js
@@ -3,76 +3,84 @@ let uuid = require('uuid/v4')
 
 const answersTable = 'answersQueryPerfTest'
 let tableSvc = azure.createTableService()
-let tableBatch = azure.tableBatch()
 
 function insertBatch(entityCount) {
 
-  tableBatch.createTableIfNotExists(answersTable, function (error, result, response) {
+  tableSvc.createTableIfNotExists(answersTable, function (error, result, response) {
     //  insert 500k entities
     if (error) {
       console.error('Error creating table:%s', error)
       return
     }
 
-    var entities = generateEntities(entityCount)
-    tableBatch.insertBatch(entities)
-    tableSvc.executeBatch(answersTable, tableBatch, function (error, result, response) {
-      if (!error) {
-        console.log('%s entities inserted', entityCount)
-      } else {
-        console.error('Error inserting entity batch:%s', error)
-      }
-    })
-  })
+    var tableBatch = new azure.TableBatch();
 
+    for (var batchCount = 0; batchCount < 1000; batchCount++) {
+
+      var entities = generateEntityBatch(tableBatch)
+
+      tableSvc.executeBatch(answersTable, tableBatch, function (error, result, response) {
+        if (!error) {
+          console.log('%s entities inserted', entityCount)
+        } else {
+          console.error('Error inserting entity batch:%s', error)
+        }
+      })
+    }
+  })
 }
 
-function generateEntities(count) {
+function generateEntityBatch(tableBatch) {
+
+  for (var index = 0; index < 100; index++) {
+
+    process.stdout.write('inserting entities...')
+    process.stdout.clearLine()
+    process.stdout.cursorTo(0)
+    process.stdout.write('inserting entity ' + index)
+
+    tableBatch.insertEntity(createPupilCheck())
+  }
+}
+
+function createPupilCheck() {
 
   var entGen = azure.TableUtilities.entityGenerator
-  var thousands = count / 1000;
-  if (thousands === 0) {
-    console.error('batch count must be in thousands')
-    return
+
+  var pupilCheck = {
+    PartitionKey: entGen.String("123456"),
+    RowKey: entGen.String("456789"),
+    createdAt: entGen.DateTime(Date.now),
+    logonEvent: entGen.Guid(uuid()),
+    testId: entGen.Guid(uuid()),
+    sessionId: entGen.Guid(uuid()),
+    answers: [],
+    creationDate: entGen.DateTime(Date.now),
+    isElectron: false
   }
 
-  var entities = []
+  for (var question = 0; question < 30; question++) {
 
-
-  for (var index = 0; index < count; index++) {
-
-    var pupilCheck = {
-      PartitionKey: entGen.String(123456),
-      RowKey: entGen.String(456789),
-      createdAt: entGen.DateTime(Date.now),
-      logonEvent: entGen.Guid(uuid()),
-      testId: entGen.Guid(uuid()),
-      sessionId: entGen.Guid(uuid()),
-      answers: [],
-      creationDate: entGen.DateTime(Date.now),
-      isElectron: false
+    var answer = {
+      pageLoadDate: entGen.DateTime(Date.now),
+      answerDate: entGen.DateTime(Date.now),
+      factor1: 7,
+      factor2: 1,
+      input: entGen.String("7"),
+      registeredInputs: [
+        {
+          input: entGen.String("7"),
+          eventType: entGen.String("touch keydown"),
+          clientInputDate: entGen.DateTime(Date.now)
+        }
+      ]
     }
-
-    for (var question = 0; question < 30; question++) {
-
-      var answer = {
-        pageLoadDate: entGen.DateTime(Date.now),
-        answerDate: entGen.DateTime(Date.now),
-        factor1: 7,
-        factor2: 1,
-        input: entGen.String("7"),
-        registeredInputs: [
-          {
-            input: entGen.String("7"),
-            eventType: entGen.String("touch keydown"),
-            clientInputDate: entGen.DateTime(Date.now)
-          }
-        ]
-      }
-      pupilCheck.answers.push(answer)
-    }
-
-    entities.push(pupilCheck)
+    pupilCheck.answers.push(answer)
   }
-  return entities
+
+  return pupilCheck
+}
+
+module.exports = {
+  insertBatch: insertBatch
 }
