4af3a3e7a5d3a2c23036e55bbb0f9f22b2d7a7ac
Raja Sekhar <rajasekhar.you@gmail.com>
2017-07-18 23:33:28 +0100

    adding tests configurable timers.

--- test/features/step_definitions/admin/timerSteps.js
+++ test/features/step_definitions/admin/timerSteps.js
@@ -4,9 +4,7 @@
 const {defineSupportCode} = require('cucumber')
 defineSupportCode(function ({Given, When, Then}) {
   Given(/^I am on the admin page$/, function () {
-    browser.manage().deleteAllCookies()
     browser.get(this.config.adminUrl)
-    // this.waitForVisibility(this.signIn.usernameField, 2000)
     this.signIn.login('teacher1', 'password')
     expect(this.signIn.heading.getText()).to.eventually.to.equal('Example School One')
   })
@@ -16,25 +14,19 @@ defineSupportCode(function ({Given, When, Then}) {
     return expect(this.administratorPage.checkSettings.isPresent()).to.eventually.be.true
   })
 
-  Then(/^I should see that Question time limit is set to default (\d) seconds$/, function (int) {
-    this.administratorPage.checkSettings.click()
-    expect(this.checkSettingsPage.questionTimeLimit.isPresent()).to.eventually.be.true
-    return expect(this.checkSettingsPage.questionTimeLimit.getAttribute('value')).to.eventually.to.equal(String(int))
-  })
-
-  Given(/^I have updated the Question time limit to (\d) seconds$/, function (int) {
+  Given(/^I have updated the Question time limit to (.*) seconds$/, function (int) {
     browser.get(this.config.adminUrl + 'administrator')
-    expect(this.administratorPage.checkSettings.isPresent()).to.eventually.be.true
     this.administratorPage.checkSettings.click()
     this.checkSettingsPage.updateQuestionTimeLimit(int)
     return expect(this.checkSettingsPage.successfulChanges.getText()).to.eventually.to.equal('Changes have been saved')
   })
 
-  Then(/^I should see that Question time limit is set to (\d+) seconds$/, function (int) {
+  Then(/^I should see that Question time limit is set to (default )*(.*) seconds$/, function (flag, int) {
     browser.get(this.config.adminUrl + 'administrator')
-    expect(this.administratorPage.checkSettings.isPresent()).to.eventually.be.true
     this.administratorPage.checkSettings.click()
-    return expect(this.checkSettingsPage.questionTimeLimit.getAttribute('value')).to.eventually.to.equal(String(int))
+    expect(this.checkSettingsPage.questionTimeLimit.getAttribute('value')).to.eventually.to.equal(int)
+    this.checkSettingsPage.updateQuestionTimeLimit(5)
+    return expect(this.checkSettingsPage.successfulChanges.getText()).to.eventually.to.equal('Changes have been saved')
   })
 
   When(/^I attempt to enter Question time limit as (.*) seconds$/, function (value) {
@@ -46,9 +38,8 @@ defineSupportCode(function ({Given, When, Then}) {
   })
 
   Then(/^I should see a validation error for Question time limit$/, function () {
-    expect(this.checkSettingsPage.errorSummary.isPresent()).to.eventually.be.true
     expect(this.checkSettingsPage.errorSummaryMessage.getText()).to.eventually.to.equal('Question time limit requires a number between 1 and 60, up to two decimals are valid')
-    expect(this.checkSettingsPage.errorMessage.getText()).to.eventually.to.equal('Question time limit requires a number between 1 and 60, up to two decimals are valid')
+    return expect(this.checkSettingsPage.errorMessage.getText()).to.eventually.to.equal('Question time limit requires a number between 1 and 60, up to two decimals are valid')
   })
 
   When(/^I attempt to enter Question time limit as (\d)$/, function (float) {
@@ -67,20 +58,32 @@ defineSupportCode(function ({Given, When, Then}) {
 
   })
 
-  Then(/^I should see that Time between questions is set to default {int} seconds$/, function (int) {
-
+  Then(/^I should see that Time between questions is set to (default )*(.*) seconds$/, function (flag, int) {
+    browser.get(this.config.adminUrl + 'administrator')
+    this.administratorPage.checkSettings.click()
+    expect(this.checkSettingsPage.timeBetweenQuestions.getAttribute('value')).to.eventually.to.equal(int)
+    this.checkSettingsPage.updateTimeBetweenQuestions(2)
+    return expect(this.checkSettingsPage.successfulChanges.getText()).to.eventually.to.equal('Changes have been saved')
   })
 
-  Given(/^I have updated the Time between questions to (\d) seconds$/, function (int) {
-
+  Given(/^I have updated the Time between questions to (.*) seconds$/, function (int) {
+    browser.get(this.config.adminUrl + 'administrator')
+    this.administratorPage.checkSettings.click()
+    this.checkSettingsPage.updateTimeBetweenQuestions(int)
+    return expect(this.checkSettingsPage.successfulChanges.getText()).to.eventually.to.equal('Changes have been saved')
   })
 
-  When(/^I attempt to enter Time between questions as (\d) seconds$/, function (stringInDoubleQuotes) {
-
+  When(/^I attempt to enter Time between questions as (.*) seconds$/, function (value) {
+    browser.get(this.config.adminUrl + 'administrator')
+    this.waitForVisibility(this.administratorPage.checkSettings, 2000)
+    this.administratorPage.checkSettings.click()
+    this.checkSettingsPage.updateTimeBetweenQuestions(value)
+    return expect(this.checkSettingsPage.successfulChanges.isPresent()).to.become(false)
   })
 
   Then(/^I should see a validation error for Time between questions$/, function () {
-
+    expect(this.checkSettingsPage.errorSummaryMessage.getText()).to.eventually.to.equal('Time between questions requires a number between 1 and 5, up to two decimals are valid')
+    return expect(this.checkSettingsPage.errorMessage.getText()).to.eventually.to.equal('Time between questions requires a number between 1 and 5, up to two decimals are valid')
   })
 
   When(/^I update the Time between questions from (\d) to (\d) seconds$/, function (int, int2) {
