27043ad3082296551f1327d58e0166dca8d414d3
Guy <gpharwood@gmail.com>
2017-07-18 16:31:31 +0100

    date issues

--- design/tableStorageAnswers/index.js
+++ design/tableStorageAnswers/index.js
@@ -1,2 +1,9 @@
-let batchInserter = require('./insertData.js')
-batchInserter.insertBatch(10000);
+let insertData = require('./insertData.js')
+insertData(function(err){
+  if(err){
+    console.log('error in insertData...')
+    console.dir(err)
+  } else {
+    console.log('Done')
+  }
+})
