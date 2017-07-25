43a645bd93822ef65c4ac1cd6573ff6997f00865
Jon Shipley <js@ovix.co.uk>
2017-07-25 16:12:54 +0100

    Add script to deploy admin to heroku
    This is made problematic because we have a multi-app repository.  The fix is to create a new temp git repo and deploy from there.

---
+++ dirty-deploy-heroku-admin.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -x
+
+CWD=`pwd`
+ADMIN_APP="$CWD/admin"
+TEMP_DIR=`mktemp -d`
+cd $TEMP_DIR
+cp -a "$ADMIN_APP" .
+cd admin
+rm -rf node_modules package-lock.json
+git init
+git add -A
+git commit -q -m 'deploying admin'
+heroku git:remote -a check-development
+git push -f heroku master
+cd $CWD
+rm -rf $TEMP_DIR
+
+
+
+
+
+
