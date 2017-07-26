430bdd431d9ef9d0f200168fff769d10ff26b7c9
Jon Shipley <js@ovix.co.uk>
2017-07-26 09:23:51 +0100

    Lint fixes

--- dirty-deploy-heroku-admin.sh
+++ dirty-deploy-heroku-admin.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -x
 
-CWD=`pwd`
+CWD=$(pwd)
 ADMIN_APP="$CWD/admin"
-TEMP_DIR=`mktemp -d`
-cd $TEMP_DIR
-cp -a "$ADMIN_APP" .
+TEMP_DIR=$(mktemp -d)
+cd ${TEMP_DIR}
+cp -a "${ADMIN_APP}" .
 cd admin
 rm -rf node_modules package-lock.json
 git init
@@ -13,5 +13,5 @@ git add -A
 git commit -q -m 'deploying admin'
 heroku git:remote -a check-development
 git push -f heroku master
-cd $CWD
-rm -rf $TEMP_DIR
+cd ${CWD}
+rm -rf ${TEMP_DIR}
