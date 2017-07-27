fa2f7c7b11172672f995ac4b6e607083abbb3b1d
Jon Shipley <js@ovix.co.uk>
2017-07-27 08:49:35 +0100

    Lint fixes for the js in pupil-spa

--- pupil-spa/protractor.conf.js
+++ pupil-spa/protractor.conf.js
@@ -1,7 +1,8 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+/* global jasmine */
+const { SpecReporter } = require('jasmine-spec-reporter')
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -17,12 +18,12 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () {}
   },
-  onPrepare() {
+  onPrepare () {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
-    });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    })
+    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }))
   }
-};
+}
