a07da0d7869fb14be0199a9df8fcfeb9e64dd613
Jon Shipley <jon-shipley@users.noreply.github.com>
2017-07-27 10:51:39 +0100

    Feature/pupil spa lint fixes (#57)
    * Add feedback from previous code review
    
    * Lint fixes for the js in pupil-spa
    
    * electron lint fixes
    
    * review fixes

--- electron/test/hooks.js
+++ electron/test/hooks.js
@@ -1,16 +1,18 @@
-var Application = require('spectron').Application;
-const path = require('path');
+/* global before after */
+
+const Application = require('spectron').Application
+const path = require('path')
 
 before(function () {
-    this.app = new Application({
-        path: path.join(__dirname, '../node_modules/.bin/electron'),
-        args: [path.join(__dirname, '..')]
-    });
-    return this.app.start()
-});
+  this.app = new Application({
+    path: path.join(__dirname, '../node_modules/.bin/electron'),
+    args: [path.join(__dirname, '..')]
+  })
+  return this.app.start()
+})
 
 after(function () {
-    if (this.app && this.app.isRunning()) {
-        return this.app.stop()
-    }
-});
+  if (this.app && this.app.isRunning()) {
+    return this.app.stop()
+  }
+})
