20abdf95eac88093ebdd087a43b4fb8e058dd681
Raja Sekhar <rajasekhar.you@gmail.com>
2017-07-27 17:47:51 +0100

    replace callbacks with promises

--- test/api-tests/test/test.js
+++ test/api-tests/test/test.js
@@ -3,15 +3,13 @@ const should = require('chai').should(),
   api = supertest('http://localhost:3001')
 
 describe('Questions API', function () {
-  it('returns questions as JSON', function (done) {
-    api.post('/api/questions')
+  it('returns questions as JSON', function () {
+    return api.post('/api/questions')
       .set('Content-Type', 'application/json')
-      .send({ "schoolPin": "abc12345", "pupilPin": "9999a" })
+      .send({'schoolPin': 'abc12345', 'pupilPin': '9999a'})
       .expect(200)
-      .end(function (err, res) {
-        if (err) return done(err)
-        res.body.should.have.property('questions')
-        done()
+      .then(response => {
+        response.body.should.have.property('questions')
       })
   })
 })
