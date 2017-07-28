9ef125fc2bed2a50370b6d7f09a4d9951b680c93
Paula Molinero <hello@pris54.co.uk>
2017-07-28 15:37:52 +0100

    WIP.

--- pupil-spa/src/app/app.component.ts
+++ pupil-spa/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: [
     './app.component.css',
+    '../../node_modules/govuk_frontend_toolkit/stylesheets/_colours.scss'
   ]
 })
 export class AppComponent {
