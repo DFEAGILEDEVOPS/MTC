effb01504df9234468369f3cec8a098ad0551bdc
Jon Shipley <js@ovix.co.uk>
2017-07-31 13:39:03 +0100

    Fix tests

--- pupil-spa/src/app/login-success/login-success.component.spec.ts
+++ pupil-spa/src/app/login-success/login-success.component.spec.ts
@@ -1,16 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginSuccessComponent } from './login-success.component';
+import * as responseMock from '../login.response.mock.json';
 
 describe('LoginSuccessComponent', () => {
   let component: LoginSuccessComponent;
   let fixture: ComponentFixture<LoginSuccessComponent>;
+  let store: {};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginSuccessComponent ]
     })
     .compileComponents();
+
+    spyOn(localStorage, 'getItem').and.callFake(function (key) {
+      return JSON.stringify(responseMock);
+    });
+    spyOn(localStorage, 'setItem').and.callFake(function (key, value) {
+      return store[key] = value + '';
+    });
+    spyOn(localStorage, 'clear').and.callFake(function () {
+      store = {};
+    });
   }));
 
   beforeEach(() => {
--- pupil-spa/src/app/login/login.component.spec.ts
+++ pupil-spa/src/app/login/login.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UserService } from '../user.service';
 
 import { LoginComponent } from './login.component';
 
@@ -8,9 +10,10 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      imports: [ RouterTestingModule ],
+      declarations: [ LoginComponent ],
+      providers: [ UserService ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
