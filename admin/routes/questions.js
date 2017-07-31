2c8a25c3f5c7442be90a810e1913595368a4c8b8
George Chatzigiannis <george.chatzigiannis@smartfocus.com>
2017-07-31 17:05:07 +0100

    Return status 405 for unhandled HTTP methods

--- admin/routes/questions.js
+++ admin/routes/questions.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { getQuestions } = require('../controllers/questions')
 
+router.route('/').get().all((req, res) => res.sendStatus(405))
 router.post('/', (req, res) => getQuestions(req, res))
 
 module.exports = router
