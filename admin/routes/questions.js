862c2a99bb693078951d8e2b529d45c0aba84de4
George Chatzigiannis <george.chatzigiannis@smartfocus.com>
2017-07-31 17:45:01 +0100

    Fix for handling questions route methods

--- admin/routes/questions.js
+++ admin/routes/questions.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const { getQuestions } = require('../controllers/questions')
 
-router.route('/').get().all((req, res) => res.sendStatus(405))
-router.post('/', (req, res) => getQuestions(req, res))
+router.route('/').all((req, res) => {
+  if (req.method !== 'POST') return res.sendStatus(405)
+  getQuestions(req, res)
+})
 
 module.exports = router
