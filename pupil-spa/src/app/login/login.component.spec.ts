57d42ee45c27f1b4710f4b38b8cb548e932f2192
Jon Shipley <js@ovix.co.uk>
2017-07-31 15:58:26 +0100

    whitespace changes

--- pupil-spa/src/app/login/login.component.spec.ts
+++ pupil-spa/src/app/login/login.component.spec.ts
@@ -1,8 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { UserService } from '../user.service';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
-import { LoginComponent } from './login.component';
+import {UserService} from '../user.service';
+import {LoginComponent} from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
