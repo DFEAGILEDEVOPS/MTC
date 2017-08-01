26404b1ac027ccf66a4e36ef14a79b81b1783178
Paula Molinero <hello@pris54.co.uk>
2017-08-01 15:01:55 +0100

    WIP.

--- pupil-spa/src/app/app.component.ts
+++ pupil-spa/src/app/app.component.ts
@@ -4,9 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [
-    './app.component.css'
+    './app.component.scss',
+    '../assets/shared-styles/styles.scss'
   ]
 })
 export class AppComponent {
-  title = 'app';
+  govukRoot = 'https://www.gov.uk';
+  homepageUrl = '/';
+  logoLinkTitle = 'Go to the home page';
+  govukAssetPath = 'assets/govuk_template';
+  globalHeaderText = 'GOV.UK';
 }
