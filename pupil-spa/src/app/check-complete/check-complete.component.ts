573d4fd3c7d078080e40d52ee92d181b940a5fdd
Jon Shipley <jon-shipley@users.noreply.github.com>
2017-08-02 16:31:57 +0100

    Add new check complete component - static page (#66)

---
+++ pupil-spa/src/app/check-complete/check-complete.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-check-complete',
+  templateUrl: './check-complete.component.html',
+  styleUrls: ['./check-complete.component.css']
+})
+export class CheckCompleteComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
