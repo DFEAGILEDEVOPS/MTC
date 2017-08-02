6ed24e4a0d0781912055afdb490924238b7e058a
George Chatzigiannis <george.chatzigiannis@smartfocus.com>
2017-08-02 09:40:09 +0100

    Display current and total questions from data

--- pupil-spa/src/app/instructions/instructions.component.ts
+++ pupil-spa/src/app/instructions/instructions.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-instructions',
   templateUrl: './instructions.component.html',
--- pupil-spa/src/app/loading/loading.component.ts
+++ pupil-spa/src/app/loading/loading.component.ts
@@ -1,18 +1,24 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+// to be imported from config
+const countdown = 3000;
+
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.css']
 })
-// to be imported from config
-const countdown = 2000;
 
 export class LoadingComponent implements AfterViewInit {
 
+  question;
+  total;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    this.question = 1;
+    this.total = 3
+  }
 
   ngAfterViewInit() {
     window.setTimeout(() => this.router.navigate(['warm-up-question']), countdown)
