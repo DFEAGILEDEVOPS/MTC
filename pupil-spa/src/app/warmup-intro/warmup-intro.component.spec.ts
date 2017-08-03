84f9a924e55a26e7caead47b3a07ab48ecadd846
George Chatzigiannis <geohatzigiannis@gmail.com>
2017-08-03 11:06:34 +0100

    Feature/14115 loading component (#65)
    * * add login component (wip)
    
    * Update docs
    
    Clarify that the `--force` flag is required to remove the npm cache
    
    * Remove ignored file
    
    * Add new components for login, dummy the response data with a JSON mock. WIP
    
    * Lint fixes
    
    * Fix some tests
    Check in package-lock.json
    
    * Update deps
    
    * Fix version and license in package.json
    
    * Fix tests
    
    * Fix lint error
    
    * Add test for Login Component
    
    * Add tests
    
    * whitespace changes
    
    * adding js tests for sign in and sign in failure
    
    * minor changes
    
    * Remove debug
    
    * Add ngSubmit to enhance the form submission
    
    * Prevent duplicate submits
    
    * refactor date format
    
    * Update logout tests
    
    * Additional testing for LoginComponent
    
    * Add tests for UserService
    
    * lint fixes
    
    * Loading component renders question component after countdown
    
    * Display current and total questions from data
    
    * remove unused attributes
    
    * test updates and fixes
    
    * Added tests loading component
    
    * Pass count for
    
    * adding tests for loading component

---
+++ pupil-spa/src/app/warmup-intro/warmup-intro.component.spec.ts
@@ -0,0 +1,39 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {Router} from '@angular/router';
+
+import { WarmupIntroComponent } from './warmup-intro.component';
+
+describe('WarmupIntroComponent', () => {
+  let component: WarmupIntroComponent;
+  let fixture: ComponentFixture<WarmupIntroComponent>;
+  let mockRouter;
+
+  beforeEach(async(() => {
+    mockRouter = {
+      navigate: jasmine.createSpy('navigate')
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [ WarmupIntroComponent ],
+      providers: [
+        {provide: Router, useValue: mockRouter}
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WarmupIntroComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('redirects to loading screen', () => {
+    component.onClick()
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['warm-up-start']);
+  });
+});
