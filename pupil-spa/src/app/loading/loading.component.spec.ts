6934c168f87827f904606cccbff84eeaa06cff8b
George Chatzigiannis <george.chatzigiannis@smartfocus.com>
2017-08-03 09:04:36 +0100

    Added tests loading component

--- pupil-spa/src/app/loading/loading.component.spec.ts
+++ pupil-spa/src/app/loading/loading.component.spec.ts
@@ -1,14 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {Router} from '@angular/router';
 
 import { LoadingComponent } from './loading.component';
 
 describe('LoadingComponent', () => {
   let component: LoadingComponent;
   let fixture: ComponentFixture<LoadingComponent>;
+  let mockRouter;
 
   beforeEach(async(() => {
+    mockRouter = {
+      navigate: jasmine.createSpy('navigate')
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ LoadingComponent ]
+      declarations: [ LoadingComponent ],
+      providers: [
+        {provide: Router, useValue: mockRouter, question: 1,total: 3, timeout: 3}
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +30,14 @@ describe('LoadingComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+  it(' will not redirect to warm up question page before timeout', () => {
+      expect(mockRouter.navigate).not.toHaveBeenCalledWith(['warm-up-question'])
+  });
+
+  it('redirects to warm up question page after initial timeout', () => {
+    setTimeout(function() {
+      expect(mockRouter.navigate).toHaveBeenCalledWith(['warm-up-question'])
+    }, 1000);
   });
 });
