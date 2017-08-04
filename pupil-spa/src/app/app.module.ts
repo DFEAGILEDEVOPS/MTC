840f3bd340f6a4587902329b9bb946966d50c2e3
George Chatzigiannis <george.chatzigiannis@smartfocus.com>
2017-08-04 11:14:40 +0100

    revert tracing

--- pupil-spa/src/app/app.module.ts
+++ pupil-spa/src/app/app.module.ts
@@ -52,7 +52,7 @@ const appRoutes: Routes = [
   imports: [
     RouterModule.forRoot(
       appRoutes,
-      {enableTracing: false} // <-- debugging purposes only
+      {enableTracing: true} // <-- debugging purposes only
     ),
     FormsModule,
     BrowserModule,
