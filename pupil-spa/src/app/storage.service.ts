742d3e681d297f99067f48b5f9e33ddb4d8e41af
Guy <gpharwood@gmail.com>
2017-08-08 16:27:27 +0100

    add superflous characters at end of lines

--- pupil-spa/src/app/storage.service.ts
+++ pupil-spa/src/app/storage.service.ts
@@ -6,51 +6,51 @@ export class StorageService {
   setItem(key, value): Promise<any> {
     return new Promise((resolve, reject) => {
       if (!key) {
-        var err = new Error('key is required')
-        reject(err)
+        const err = new Error('key is required');
+        reject(err);
       } else {
-        localStorage.setItem(key, value)
-        resolve()
+        localStorage.setItem(key, value);
+        resolve();
       }
-    })
+    });
   }
 
   getItem(key): Promise<any> {
     return new Promise((resolve, reject) => {
       if (!key) {
-        var err = new Error('key is required')
-        reject(err)
+        const err = new Error('key is required');
+        reject(err);
       } else {
-        let item = localStorage.getItem(key)
-        resolve(item)
+        const item = localStorage.getItem(key);
+        resolve(item);
       }
-    })
+    });
   }
 
   removeItem(key): Promise<any> {
     return new Promise((resolve, reject) => {
       if (!key) {
-        var err = new Error('key is required')
-        reject(err)
+        const err = new Error('key is required');
+        reject(err);
       } else {
-        localStorage.removeItem(key)
-        resolve()
+        localStorage.removeItem(key);
+        resolve();
       }
-    })
+    });
   }
 
   clear(): Promise<any> {
     return new Promise((resolve, reject) => {
-      localStorage.clear()
-      resolve()
-    })
+      localStorage.clear();
+      resolve();
+    });
   }
 
   getKeys(): Promise<any> {
-    return new Promise((resolve,reject) => {
-      var keys = Object.keys(localStorage)
-      resolve(keys)
-    })
+    return new Promise((resolve, reject) => {
+      const keys = Object.keys(localStorage);
+      resolve(keys);
+    });
   }
 
 }
