aafbcbdeb5eb7e02776cea0b0a40954069594cc3
Guy <gpharwood@gmail.com>
2017-08-08 10:54:02 +0100

    move assets exclusion to pupil-spa gitignore.  finish basic tests for storage service.  Feels like we should be spying on localStorage as some tests are weak

--- pupil-spa/src/app/storage-service/storage.service.spec.ts
+++ pupil-spa/src/app/storage-service/storage.service.spec.ts
@@ -6,6 +6,10 @@ let service: StorageService
 
 describe('StorageService', () => {
 
+  const shouldNotExecute = () => {
+    expect(1).toBe(2)
+  }
+
   beforeEach(() => {
     const injector = TestBed.configureTestingModule({
       providers: [StorageService]
@@ -20,9 +24,12 @@ describe('StorageService', () => {
   it('setItem: returns a promise that rejects when key missing', () => {
 
     service.setItem(null, 'xxxx').then(
+      () => {
+        shouldNotExecute()
+      },
       (err) => {
         expect(err).toBeTruthy()
-        expect(err).toEqual('key is required')
+        expect(err.message).toEqual('key is required')
       }
     )
   })
@@ -30,48 +37,74 @@ describe('StorageService', () => {
   it('setItem: returns a promise that resolves when key provided', () => {
 
     service.setItem('setItem_key', 'xxxx').then(
-      (err) => {
-        expect(err).toBeFalsy()
-      }
+      () => { },
+      (err) => { shouldNotExecute() }
     )
   })
 
   it('getItem: returns a promise that rejects when key missing', () => {
 
     service.getItem(null).then(
+      () => { shouldNotExecute() },
       (err) => {
         expect(err).toBeTruthy()
-        expect(err).toEqual('key is required')
+        expect(err.message).toEqual('key is required')
       })
   })
 
   it('getItem: returns a promise that returns item when key provided and item exists', () => {
-    const itemKey = 'x4tw8fdf'
-    const itemValue = 'sdjfkldsj489fg'
+    const itemKey = new Date().getTime()
+    const itemValue = itemKey.toString()
     service.setItem(itemKey, itemValue).then(function () {
       service.getItem(itemKey).then(
         (data) => {
           expect(data).toBeTruthy()
           expect(data).toEqual(itemValue)
-        })
+        },
+        (err) => { shouldNotExecute() })
     })
   })
 
   it('removeItem: returns a promise that rejects when key not provided', () => {
     service.removeItem(null).then(
+      () => { shouldNotExecute() },
       (err) => {
-        //expect(err).toBeTruthy()
-        console.log(err)
-        expect(err).toEqual('key is required')
+        expect(err).toBeTruthy()
+        expect(err.message).toEqual('key is required')
       })
   })
 
   it('removeItem: returns a promise that resolves when key provided', () => {
-    service.removeItem('removeItem_key').then(
-      (err) => {
-        expect(err).toBeFalsy()
-      }
+    service.removeItem('removeItemKey').then(
+      () => { },
+      (err) => { shouldNotExecute() }
     )
+  })
+
+  it('clear: returns a promise that resolves', () => {
+    service.clear().then(
+      () => { },
+      (err) => { shouldNotExecute() } //really??? we should be spying on localStorage...
+    )
+  })
+
+  it('getKeys: returns a promise that resolves all keys', () => {
+    let items = [
+      { key: 'item1', value: 'item1value' },
+      { key: 'item2', value: 'item2value' },
+      { key: 'item3', value: 'item3value' },
+    ]
+    items.forEach((item) => {
+      localStorage.setItem(item.key, item.value)
+    })
+    service.getKeys().then(
+        (keys) => {
+          expect(keys[0]).toEqual(items[0].key)
+          expect(keys[1]).toEqual(items[1].key)
+          expect(keys[2]).toEqual(items[2].key)
+        },
+        (err) => { shouldNotExecute() }
+      )
   })
 
 });
