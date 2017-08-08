373280925702e30d0f00dc9c2a54d2915255dbd8
Guy <gpharwood@gmail.com>
2017-08-08 17:25:43 +0100

    answer service test skeletons

---
+++ pupil-spa/src/app/answer.service.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { AnswerService } from './answer.service';
+
+let service: AnswerService;
+
+describe('AnswerService', () => {
+
+  const shouldNotExecute = () => {
+    expect(1).toBe(2);
+  };
+
+  beforeEach(() => {
+    const injector = TestBed.configureTestingModule({
+      providers: [AnswerService]
+    });
+    localStorage.clear();
+    service = injector.get(AnswerService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should persist answers to local storage via the storage service', () => {
+    spyOn(localStorage, 'setItem');
+
+    service.setAnswer(42);
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  it('should append answer to answers object in storage', () => {
+
+  });
+
+});
