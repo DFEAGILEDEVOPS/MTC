9879c7f42a1ca0e03ce948234907f52c879978fb
Guy <gpharwood@gmail.com>
2017-08-09 16:54:48 +0100

    remove promise

--- pupil-spa/src/app/answer.service.ts
+++ pupil-spa/src/app/answer.service.ts
@@ -15,24 +15,17 @@ export class AnswerService {
 
   constructor(private storageService: StorageService) { }
 
-  setAnswer(answer: Answer): Promise<any> {
+  setAnswer(answer: Answer): void {
 
-    return new Promise((resolve, reject) => {
-      this.storageService.getItem('answers')
-        .then((answers) => {
-          if(!answers){
-            answers = "[]";
-          }
-          let existingAnswers = JSON.parse(answers);
-          existingAnswers.push(answer);
-          this.storageService.setItem('answers', JSON.stringify(existingAnswers));
-          resolve();
-        },(err) => {
-          //TODO: fix as per Jons comment
-        }).catch((err) => {
-          reject(err);
-        });
-    })
+    var answers = this.storageService.getItem('answers');
+
+    if (!answers) {
+      answers = "[]";
+    }
+
+    let existingAnswers = JSON.parse(answers);
+    existingAnswers.push(answer);
+    this.storageService.setItem('answers', JSON.stringify(existingAnswers));
   }
 
 }
