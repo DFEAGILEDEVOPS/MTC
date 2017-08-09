5a132a43005bb0f7dd63b54bd85897562f675ddd
Jon Shipley <jon-shipley@users.noreply.github.com>
2017-08-09 10:37:34 +0100

    Feature/add auth guard to routes (#78)
    * Ignore GDS assets
    
    * Add authentication guard to routes that require authentication to be viewed
    
    * Update description

---
+++ pupil-spa/src/app/logged-in.guard.spec.ts
@@ -0,0 +1,44 @@
+import {TestBed, async, inject} from '@angular/core/testing';
+import {Router} from  '@angular/router';
+
+import {LoggedInGuard} from './logged-in.guard';
+import {UserService} from './user.service';
+
+let mockRouter;
+let mockUser;
+let isLoggedIn = false;
+
+describe('LoggedInGuard', () => {
+  mockRouter = {
+    navigate: jasmine.createSpy('navigate')
+  };
+  mockUser = {
+    isLoggedIn() {
+      return isLoggedIn;
+    }
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        LoggedInGuard,
+        {provide: UserService, useValue: mockUser},
+        {provide: Router, useValue: mockRouter}
+      ]
+    });
+  });
+
+  it('should instantiate', inject([LoggedInGuard], (guard: LoggedInGuard) => {
+    expect(guard).toBeTruthy();
+  }));
+
+  it('should redirect when the user is not signed in', inject([LoggedInGuard], (guard: LoggedInGuard) => {
+    expect(guard.canActivate()).toBe(false);
+    expect(mockRouter.navigate).toHaveBeenCalled();
+  }));
+
+  it('should return true when the user is signed in', inject([LoggedInGuard], (guard: LoggedInGuard) => {
+    isLoggedIn = true;
+    expect(guard.canActivate()).toBe(true);
+  }));
+});
