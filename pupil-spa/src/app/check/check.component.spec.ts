de7ed6e4c701c38e270c4f11d336ede2ba13242c
Jon Shipley <js@ovix.co.uk>
2017-08-09 13:09:24 +0100

    Fix unit tests - we don't need to test sub components

--- pupil-spa/src/app/check/check.component.spec.ts
+++ pupil-spa/src/app/check/check.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { CheckComponent } from './check.component';
 
@@ -8,9 +9,10 @@ describe('CheckComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CheckComponent ]
+      declarations: [ CheckComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ]         // we don't need to test sub-components
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
