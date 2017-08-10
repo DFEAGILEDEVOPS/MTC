4f9bba7739bb8064838b041c0ce72a23704d3620
Paula Molinero <hello@pris54.co.uk>
2017-08-10 10:56:44 +0100

    Unit test modifications.

--- pupil-spa/src/app/user.service.spec.ts
+++ pupil-spa/src/app/user.service.spec.ts
@@ -1,54 +1,72 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
 
 import { StorageService } from './storage.service';
 import { UserService } from './user.service';
-import * as userResponseMock from './user.data.mock.json';
+import * as responseMock from './login.response.mock.json';
 
 let userService: UserService;
+let storageService: StorageService;
 
 describe('UserService', () => {
+  let store: {};
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        UserService,
-        StorageService
-      ]
+    const injector = TestBed.configureTestingModule({
+      imports: [HttpModule],
+      providers: [UserService, StorageService]
     });
+
     userService = injector.get(UserService, StorageService);
+    storageService = injector.get(StorageService);
+
+    spyOn(storageService, 'getItem').and.callFake(function (key) {
+      return JSON.stringify(responseMock);
+    });
+
+    spyOn(storageService, 'setItem').and.callFake(function (key, value) {
+      return store[key] = value + '';
+    });
+
+    spyOn(storageService, 'clear').and.callFake(function () {
+      store = {};
+    });
   });
 
   it('should be created', () => {
     expect(userService).toBeTruthy();
   });
 
-  it('returns a promise that resolves on valid logon', () => {
+  it('should return a promise that resolves on valid logon', () => {
     userService.login('abc12345', '9999a').then(
       (res) => {
         expect(res['questions'].length).toBeGreaterThan(0);
+        userService.logout();
       }
     );
   });
 
-  it('returns a promise that rejects on invalid login', () => {
+  it('should return a promise that rejects on invalid login', () => {
     userService.login('xxx', 'xxx').then(
       (res) => {
         expect(1).toBe(2);
       },
     ).catch((err) => {
-      expect(err.status).toBe(401);
+        expect(err.status).toBe(401);
       }
     );
   });
 
-  it('returns a promise that rejects when insufficient data are provided', () => {
-    userService.login('xxx', '').then().catch((err) => {
+  it('should return a promise that rejects when insufficient data are provided', () => {
+    userService.login('xxx', '')
+      .then()
+      .catch((err) => {
         expect(err.status).toBe(400);
       }
     );
   });
 
-  it('logs a user out', () => {
+  it('should log the user out', () => {
     userService.login('abc12345', '9999a').then(
       () => {
         userService.logout();
