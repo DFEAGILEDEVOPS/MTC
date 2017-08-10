1491d6d6d226cb75cf97afc572b1fe313eb61634
Jon Shipley <js@ovix.co.uk>
2017-08-10 12:52:42 +0100

    Simplify the loadingComponent and tests now that the timing and view state is managed by the CheckComponent

--- pupil-spa/src/app/loading/loading.component.spec.ts
+++ pupil-spa/src/app/loading/loading.component.spec.ts
@@ -1,23 +1,15 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {Router} from '@angular/router';
 
 import {LoadingComponent} from './loading.component';
 
 describe('LoadingComponent', () => {
   let component: LoadingComponent;
   let fixture: ComponentFixture<LoadingComponent>;
-  let mockRouter;
 
   beforeEach(async(() => {
-    mockRouter = {
-      navigate: jasmine.createSpy('navigate')
-    };
 
     TestBed.configureTestingModule({
-      declarations: [ LoadingComponent ],
-      providers: [
-        { provide: Router, useValue: mockRouter }
-      ]
+      declarations: [ LoadingComponent ]
     })
       .compileComponents();
   }));
@@ -30,14 +22,5 @@ describe('LoadingComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
-  });
-  it(' will not redirect to warm up question page before timeout', () => {
-    expect(mockRouter.navigate).not.toHaveBeenCalledWith(['warm-up-question']);
-  });
-
-  it('redirects to warm up question page after initial timeout', () => {
-    setTimeout(function () {
-      expect(mockRouter.navigate).toHaveBeenCalledWith(['warm-up-question']);
-    }, 1000);
   });
 });
--- pupil-spa/src/app/loading/loading.component.ts
+++ pupil-spa/src/app/loading/loading.component.ts
@@ -1,5 +1,4 @@
-import { Component, AfterViewInit, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
@@ -7,19 +6,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./loading.component.scss']
 })
 
-export class LoadingComponent implements AfterViewInit {
+export class LoadingComponent {
 
-  @Input()
-  private countdown = 2000;
   @Input()
   private question = 0;
   @Input()
   private total = 0;
 
-  constructor(private router: Router) {
-  }
-
-  ngAfterViewInit() {
-    // window.setTimeout(() => this.router.navigate(['warm-up-question']), this.countdown);
+  constructor() {
   }
 }
