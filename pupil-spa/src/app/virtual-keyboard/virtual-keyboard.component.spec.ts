f8837362f70bf58e630f95580b37370d5e5bb72d
Paula Molinero <hello@pris54.co.uk>
2017-08-11 08:44:55 +0100

    Virtual keyboard.

--- pupil-spa/src/app/virtual-keyboard/virtual-keyboard.component.spec.ts
+++ pupil-spa/src/app/virtual-keyboard/virtual-keyboard.component.spec.ts
@@ -22,4 +22,29 @@ describe('VirtualKeyboardComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a buttons with values from 0 to 9', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(function(num) {
+      expect(compiled.querySelector('td.key.js-key > button#kb' + num).textContent).toMatch(
+        num.toString()
+      );
+    });
+  });
+
+  it('should have a Backspace button', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('td.key.js-key > button#kb-backspace')).toBeTruthy();
+  });
+
+  it('should have an Enter button', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('td.enter-key.js-key > button#kb-enter')).toBeTruthy();
+  });
+
+  xit('should clicking buttons populate the Answers box', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    component.onClickAnswer(1);
+    expect(compiled.querySelector('span#js-answer').innerHTML).toContain('1');
+  });
 });
