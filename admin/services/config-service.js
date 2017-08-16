6332c489c4c3ea7fcb2a3e4bfbec31f7bbac7eac
Jon Shipley <jon-shipley@users.noreply.github.com>
2017-08-16 07:39:58 +0100

    Feature/fix api config vars (#95)
    * Add new config service that overrides the default config file value with ones from the db
    
    * Use vanilla uuid V4 to send to the SPA as the session ID.  This fixes a security issue - leaking the admin user's session ID.

---
+++ admin/services/config-service.js
@@ -0,0 +1,31 @@
+'use strict'
+const Setting = require('../models/setting')
+const {QUESTION_TIME_LIMIT, TIME_BETWEEN_QUESTIONS} = require('../config')
+
+/** @namespace */
+
+const configService = {
+
+  getConfig: async () => {
+    let questionTime = QUESTION_TIME_LIMIT
+    let loadingTime = TIME_BETWEEN_QUESTIONS
+
+    try {
+      const timeSettings = await Setting.findOne().exec()
+      if (timeSettings) {
+        loadingTime = timeSettings.loadingTimeLimit
+        questionTime = timeSettings.questionTimeLimit
+      }
+    } catch (error) {
+      console.error('There was an error retrieving custom timings: ' + error.message)
+    }
+
+    return {
+      questionTime,
+      loadingTime
+    }
+  }
+
+}
+
+module.exports = configService
