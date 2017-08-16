d0f643f25ff4fe06ed302502506ddf3c3a5874c4
Guy <gpharwood@gmail.com>
2017-08-16 08:14:30 +0100

    Work in progress on audit service specs

---
+++ pupil-spa/src/app/audit.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { StorageService } from './storage.service';
+import { AuditEntry, AuditEntryType } from './auditEntry';
+
+@Injectable()
+export class AuditService {
+
+  constructor(private storageService: StorageService) { }
+
+  addEntry(auditEntry: AuditEntry): void {
+    let existingEntries = this.storageService.getItem('audit') as Array<AuditEntry>;
+    if(!existingEntries){
+      existingEntries = new Array<AuditEntry>();
+    }
+    existingEntries.push(auditEntry);
+    this.storageService.setItem('audit', existingEntries);
+  }
+}
