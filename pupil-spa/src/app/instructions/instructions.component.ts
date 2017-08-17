bafdfd28a0ef070d857f6b9957b5bcad30c5181f
Jon Shipley <js@ovix.co.uk>
2017-08-17 13:18:03 +0100

    Remove Input from import as no longer required

--- pupil-spa/src/app/instructions/instructions.component.ts
+++ pupil-spa/src/app/instructions/instructions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { QuestionService } from '../question.service';
