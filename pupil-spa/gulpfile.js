caa24e346f52f868a18395bfc2e6184b73a1c2e6
George Chatzigiannis <george.chatzigiannis@smartfocus.com>
2017-08-17 13:07:10 +0100

    Replace should occur only if env variable is defined

--- pupil-spa/gulpfile.js
+++ pupil-spa/gulpfile.js
@@ -2,7 +2,9 @@ const gulp = require('gulp')
 const replace = require('gulp-string-replace')
 
 gulp.task('setAppEnvironmentVariables', () => {
-  gulp.src(['./src/environments/environment.prod.ts'])
-    .pipe(replace('API_URL', process.env.API_URL))
-    .pipe(gulp.dest('./src/environments/'))
+  if (process.env.API_URL) {
+    gulp.src(['./src/environments/environment.prod.ts'])
+      .pipe(replace('API_URL', process.env.API_URL))
+      .pipe(gulp.dest('./src/environments/'))
+  }
 })
