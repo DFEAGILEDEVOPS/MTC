301c0de4bc41ec5f7646d95883c151c5a5c3687d
Jon Shipley <js@ovix.co.uk>
2017-08-17 13:11:17 +0100

    Make the variables in the instructions dynamic

--- pupil-spa/src/app/instructions/instructions.component.spec.ts
+++ pupil-spa/src/app/instructions/instructions.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import {Router} from '@angular/router';
 
 import { InstructionsComponent } from './instructions.component';
+import { QuestionServiceMock } from '../question.service.mock';
+import { QuestionService } from '../question.service';
 
 describe('InstructionsComponent', () => {
   let component: InstructionsComponent;
@@ -15,13 +17,14 @@ describe('InstructionsComponent', () => {
     TestBed.configureTestingModule({
       declarations: [InstructionsComponent],
       providers: [
-        {provide: Router, useValue: mockRouter}
+        {provide: Router, useValue: mockRouter},
+        {provide: QuestionService, useClass: QuestionServiceMock}
       ]
     })
       .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(() => 
     fixture = TestBed.createComponent(InstructionsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
