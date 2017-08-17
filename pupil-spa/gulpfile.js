42e1ed85559a2f29c3fd195d74fc15372f047b0a
George Chatzigiannis <george.chatzigiannis@smartfocus.com>
2017-08-17 12:07:23 +0100

    update gulp task name

--- pupil-spa/gulpfile.js
+++ pupil-spa/gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp')
 const replace = require('gulp-string-replace')
 
-gulp.task('replace', () => {
+gulp.task('setAppEnvironmentVariables', () => {
   gulp.src(['./src/environments/environment.prod.ts'])
     .pipe(replace('API_URL', process.env.API_URL))
     .pipe(gulp.dest('./src/environments/'))
