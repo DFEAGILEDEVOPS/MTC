20fbee1aabc100d7e8d5cd45436d1c5a20c89578
activemq <m-qureshi@hotmail.com>
2017-08-18 10:46:24 +0100

    adding keypad and keyboard tests

--- pupil-spa/test/features/check.feature
+++ pupil-spa/test/features/check.feature
@@ -22,3 +22,24 @@ Feature: Check questions
   Scenario: Route remains /check during the check
     Given I have started the check
     Then the route remains the same
+
+  Scenario: On screen keyboard is displayed on check page
+    Given I have started the check
+    Then I should see the on screen keyboard
+
+  Scenario: Users can enter answers by using the on screen keyboard
+    Given I have started the check
+    Then I can answer the question using the on screen keyboard
+
+  Scenario: Users can enter answers by using their phsyical keyboard
+    Given I have started the check
+    Then I can answer the question using their phsyical keyboard
+
+  Scenario: Users can complete the test using the on screen keyboard
+    Given I have started the check
+    Then I should be able to use the on screen keyboard to complete the test
+
+  Scenario: Users can correct their answer
+    Given I have started the check
+    When I have entered an incorrect answer
+    Then I should be able to correct my answer if i am quick enough
--- pupil-spa/test/features/step_definitions/check_steps.rb
+++ pupil-spa/test/features/step_definitions/check_steps.rb
@@ -40,3 +40,48 @@ Then(/^the route remains the same$/) do
   check_page.wait_for_question
   expect(current_url).to eql expected_url
 end
+
+Then(/^I should see the on screen keyboard$/) do
+  check_page.wait_for_question
+  expect(check_page).to have_number_pad
+end
+
+Then(/^I can answer the question using the on screen keyboard$/) do
+  check_page.wait_for_preload
+  @question = check_page.question.text
+  values = @question.gsub('=', '').split('×').map {|n| n.strip}
+  check_page.enter_answer_via_keyboard(values.first.to_i * values.last.to_i)
+  check_page.number_pad.enter.click
+  check_page.wait_for_question
+  expect(check_page.question.text).to_not eql @question
+end
+
+Then(/^I can answer the question using their phsyical keyboard$/) do
+  check_page.wait_for_preload
+  @question = check_page.question.text
+  values = @question.gsub('=', '').split('×').map {|n| n.strip}
+  check_page.enter_answer_via_physical_keyboard(values.first.to_i * values.last.to_i)
+  check_page.number_pad.one.send_keys(:enter)
+  check_page.wait_for_question
+  expect(check_page.question.text).to_not eql @question
+end
+
+Then(/^I should be able to use the on screen keyboard to complete the test$/) do
+  questions = JSON.parse page.evaluate_script('window.localStorage.getItem("questions");')
+  @answers = check_page.complete_check_with_correct_answers(questions.size)
+  expect(complete_page).to have_completion_text
+end
+
+When(/^I have entered an incorrect answer$/) do
+  check_page.wait_for_question
+  check_page.enter_answer_via_keyboard(20)
+  @value = check_page.answer.text
+end
+
+When(/^I should be able to correct my answer if i am quick enough$/) do
+  2.times {check_page.number_pad.backspace.click}
+  question = check_page.question.text
+  values = question.gsub('=', '').split('×').map {|n| n.strip}
+  check_page.enter_answer_via_physical_keyboard(values.first.to_i * values.last.to_i)
+  expect(check_page.answer.text).to_not eql @value
+end
