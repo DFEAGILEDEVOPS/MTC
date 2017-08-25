3fc0b1720a2a18c279cac15d989353c8d63a35f7
Paula Molinero <hello@pris54.co.uk>
2017-08-25 11:59:33 +0100

    WIP.

--- admin/app.js
+++ admin/app.js
@@ -55,6 +55,7 @@ const testDeveloper = require('./routes/test-developer')
 const administrator = require('./routes/administrator')
 const admin = require('./routes/admin')
 const questions = require('./routes/questions')
+const feedback = require('./routes/feedback')
 
 if (process.env.NODE_ENV === 'development') piping({ ignore: [/newrelic_agent.log/, /test/] })
 const app = express()
@@ -173,6 +174,7 @@ app.use('/test-developer', testDeveloper)
 app.use('/administrator', administrator)
 app.use('/school', admin)
 app.use('/api/questions', questions)
+app.use('/api/feedback', feedback)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
