edacd2f9461a03428b290ec24b51da7f284ac887
Jon Shipley <js@ovix.co.uk>
2017-08-25 13:03:25 +0100

    turning off tracing by default

--- pupil-spa/src/app/app.module.ts
+++ pupil-spa/src/app/app.module.ts
@@ -59,7 +59,7 @@ const appRoutes: Routes = [
   imports: [
     RouterModule.forRoot(
       appRoutes,
-      {enableTracing: true} // <-- debugging purposes only
+      {enableTracing: false} // <-- debugging purposes only
     ),
     FormsModule,
     BrowserModule,
