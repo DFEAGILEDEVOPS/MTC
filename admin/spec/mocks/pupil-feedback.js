83b9613838feed0c66b557a23b443e048fada42f
Paula Molinero <hello@pris54.co.uk>
2017-09-01 12:41:13 +0100

    WIP.

---
+++ admin/spec/mocks/pupil-feedback.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'feedbackData': {
+    _id: '59a6e92ecffb044b5ecbcd6e',
+    inputType: 3,
+    satisfactionRating: 2,
+    comments: 'Test',
+    sessionId: '6795661f-9646-422d-b3ac-a660c6812dd3'
+  }
+}
