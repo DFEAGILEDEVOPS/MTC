8bc84c3a6febba2cc2ace225397a483facb6d1ad
Jon Shipley <js@ovix.co.uk>
2017-09-05 10:11:15 +0100

    Sort angular imports alphabetically

--- pupil-spa/src/app/app.module.ts
+++ pupil-spa/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AnswerService } from './services/answer/answer.service';
 import { AppComponent } from './app.component';
