eb214075cbce84a206ea303439fc17e27ca868d5
Paula Molinero <hello@pris54.co.uk>
2017-09-11 16:48:23 +0100

    WIP.

--- admin/authentication/middleware.js
+++ admin/authentication/middleware.js
@@ -1,7 +1,23 @@
-function isAuthenticated () {
+function isAuthenticated (role) {
+  console.log('ROLE', role)
+  this.role = role
+
   return function (req, res, next) {
+    console.log('THIS', this)
+    console.log('ROLE(2)', this.role)
+    console.log('role / req.user.role', this.role, req.user.role)
+    if (typeof this.role === 'undefined') {
+      console.log('ROLE IS UNDEFINED')
+    }
     if (req.isAuthenticated()) {
-      return next()
+      switch (role) {
+        case undefined:
+        case req.user.role === role:
+          return next()
+        default:
+          console.log('UNAUTHORISED')
+          //return res.redirect('/sign-out')
+      }
     }
     res.redirect('/sign-in')
   }
