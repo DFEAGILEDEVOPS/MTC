9ac0aac695f7b2294efcffa2ae494b3ed63ed465
Jon Shipley <js@ovix.co.uk>
2017-09-11 13:08:55 +0100

    Update pupil-validator tests

--- admin/spec/lib/validator/pupil-validator.spec.js
+++ admin/spec/lib/validator/pupil-validator.spec.js
@@ -31,7 +31,7 @@ describe('pupil validator', function () {
       body: {},
       params: {},
       param: (name) => {
-        this.params[name]
+        this.params[name] = name
       }
     }
 
@@ -47,105 +47,124 @@ describe('pupil validator', function () {
     done()
   })
 
-  it('requires a foreName', async function (done) {
-    req.body = getBody()
-    req.body.foreName = ''
-    let validationError = await pupilValidator.validate(req)
-    expect(validationError.hasError()).toBe(true)
-    expect(validationError.isError('foreName')).toBe(true)
-    done()
-  })
-
-  it('allows latin chars, hyphen and apostrophe in the forename', async function (done) {
-    req.body = getBody()
-    req.body.foreName = 'Rén-\'e'
-    let validationError = await pupilValidator.validate(req)
-    expect(validationError.hasError()).toBe(false)
-    done()
-  })
-
-  it('does not allow punctuation in the forename', async function (done) {
-    req.body = getBody()
-    for (let char of notAllowed()) {
-      req.body.foreName = 'Réne' + char
+  describe('foreName', () => {
+    it('requires a foreName', async function (done) {
+      req.body = getBody()
+      req.body.foreName = ''
       let validationError = await pupilValidator.validate(req)
       expect(validationError.hasError()).toBe(true)
       expect(validationError.isError('foreName')).toBe(true)
-    }
-    done()
+      expect(validationError.get('foreName')).toBe('First name cannot be blank')
+      done()
+    })
+
+    it('requires foreName to be no more than 35 chars', async function (done) {
+      req.body = getBody()
+      req.body.foreName = 's'.repeat(36)
+      let validationError = await pupilValidator.validate(req)
+      expect(validationError.hasError()).toBe(true)
+      expect(validationError.isError('foreName')).toBe(true)
+      expect(validationError.get('foreName')).toBe('Check the first name')
+      done()
+    })
+
+    it('allows latin chars, hyphen and apostrophe in the forename', async function (done) {
+      req.body = getBody()
+      req.body.foreName = 'Rén-\'e'
+      let validationError = await pupilValidator.validate(req)
+      expect(validationError.hasError()).toBe(false)
+      done()
+    })
+
+    it('does not allow punctuation in the forename', async function (done) {
+      req.body = getBody()
+      for (let char of notAllowed()) {
+        req.body.foreName = 'Réne' + char
+        let validationError = await pupilValidator.validate(req)
+        expect(validationError.hasError()).toBe(true)
+        expect(validationError.isError('foreName')).toBe(true)
+        expect(validationError.get('foreName')).toBe('Check the first name does not contain special characters')
+      }
+      done()
+    })
+
+    it('foreName can include numbers', async function (done) {
+      req.body = getBody()
+      req.body.foreName = 'Smithy99'
+      let validationError = await pupilValidator.validate(req)
+      expect(validationError.hasError()).toBe(false)
+      expect(validationError.isError('foreName')).toBe(false)
+      done()
+    })
   })
 
-  it('optionally requires a middleName(s) with latin1 hyphen apostrophe and a space', async function (done) {
-    req.body = getBody()
-    req.body.middleNames = 'Mårk Anthøny Doublé-Barræll\'d'
-    let validationError = await pupilValidator.validate(req)
-    expect(validationError.hasError()).toBe(false)
-    expect(validationError.isError('middleNames')).toBe(false)
-    done()
-  })
+  describe('middleNames', () => {
+    it('is optional', async (done) => {
+      req.body = getBody()
+      req.body.middleNames = ''
+      let validationError = await pupilValidator.validate(req)
+      expect(validationError.hasError()).toBe(false)
+      expect(validationError.isError('middleNames')).toBe(false)
+      done()
+    })
 
-  it('does not allow punctuation in the middlename', async function (done) {
-    req.body = getBody()
-    for (let char of notAllowed()) {
-      req.body.middleNames = 'Réne' + char
+    it('allows latin1 hyphen apostrophe and a space', async (done) => {
+      req.body = getBody()
+      req.body.middleNames = 'Mårk Anthøny Doublé-Barræll\'d'
+      let validationError = await pupilValidator.validate(req)
+      expect(validationError.hasError()).toBe(false)
+      expect(validationError.isError('middleNames')).toBe(false)
+      done()
+    })
+
+    it('does not allow punctuation in the middlename', async (done) => {
+      req.body = getBody()
+      for (let char of notAllowed()) {
+        req.body.middleNames = 'Réne' + char
+        let validationError = await pupilValidator.validate(req)
+        expect(validationError.hasError()).toBe(true)
+        expect(validationError.isError('middleNames')).toBe(true)
+        expect(validationError.get('middleNames')).toBe('Check the middle name does not contain special characters')
+      }
+      done()
+    })
+
+    it('middlenames can be up to 35 chars long', async (done) => {
+      req.body = getBody()
+      req.body.middleNames = 's'.repeat(36)
       let validationError = await pupilValidator.validate(req)
       expect(validationError.hasError()).toBe(true)
       expect(validationError.isError('middleNames')).toBe(true)
-    }
-    done()
+      done()
+    })
+
+    it('middleNames can include numbers', async function (done) {
+      req.body = getBody()
+      req.body.middleNames = 'Smithy99'
+      let validationError = await pupilValidator.validate(req)
+      expect(validationError.hasError()).toBe(false)
+      expect(validationError.isError('middleNames')).toBe(false)
+      done()
+    })
   })
 
-  it('forenames can be up to 35 chars long', async function (done) {
-    req.body = getBody()
-    req.body.foreName = 's'.repeat(36)
-    let validationError = await pupilValidator.validate(req)
-    expect(validationError.hasError()).toBe(true)
-    expect(validationError.isError('foreName')).toBe(true)
-    done()
-  })
+  describe('lastname', () => {
+    it('lastName can be up to 35 chars long', async function (done) {
+      req.body = getBody()
+      req.body.lastName = 's'.repeat(36)
+      let validationError = await pupilValidator.validate(req)
+      expect(validationError.hasError()).toBe(true)
+      expect(validationError.isError('lastName')).toBe(true)
+      done()
+    })
 
-  it('middlenames can be up to 35 chars long', async function (done) {
-    req.body = getBody()
-    req.body.middleNames = 's'.repeat(36)
-    let validationError = await pupilValidator.validate(req)
-    expect(validationError.hasError()).toBe(true)
-    expect(validationError.isError('middleNames')).toBe(true)
-    done()
-  })
-
-  it('lastName can be up to 35 chars long', async function (done) {
-    req.body = getBody()
-    req.body.lastName = 's'.repeat(36)
-    let validationError = await pupilValidator.validate(req)
-    expect(validationError.hasError()).toBe(true)
-    expect(validationError.isError('lastName')).toBe(true)
-    done()
-  })
-
-  it('foreName can include numbers', async function (done) {
-    req.body = getBody()
-    req.body.foreName = 'Smithy99'
-    let validationError = await pupilValidator.validate(req)
-    expect(validationError.hasError()).toBe(false)
-    expect(validationError.isError('foreName')).toBe(false)
-    done()
-  })
-
-  it('middleNames can include numbers', async function (done) {
-    req.body = getBody()
-    req.body.middleNames = 'Smithy99'
-    let validationError = await pupilValidator.validate(req)
-    expect(validationError.hasError()).toBe(false)
-    expect(validationError.isError('middleNames')).toBe(false)
-    done()
-  })
-
-  it('lastName can include numbers', async function (done) {
-    req.body = getBody()
-    req.body.lastName = 'Smithy99'
-    let validationError = await pupilValidator.validate(req)
-    expect(validationError.hasError()).toBe(false)
-    expect(validationError.isError('lastName')).toBe(false)
-    done()
+    it('lastName can include numbers', async function (done) {
+      req.body = getBody()
+      req.body.lastName = 'Smithy99'
+      let validationError = await pupilValidator.validate(req)
+      expect(validationError.hasError()).toBe(false)
+      expect(validationError.isError('lastName')).toBe(false)
+      done()
+    })
   })
 })
