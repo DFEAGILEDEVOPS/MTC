69e4a4ef3d3aa88e19aaae15a26d121d9633febf
Paula Molinero <hello@pris54.co.uk>
2017-09-15 15:37:32 +0100

    Add/edit check windows.

--- admin/routes/administrator.js
+++ admin/routes/administrator.js
@@ -4,14 +4,24 @@ const express = require('express')
 const router = express.Router()
 const isAuthenticated = require('../authentication/middleware')
 const config = require('../config')
-const { getAdministration, getUpdateTiming, setUpdateTiming, getCheckWindows } = require('../controllers/administrator')
+const {
+  getUpdateTiming,
+  setUpdateTiming,
+  getCheckWindows,
+  checkWindowsForm,
+  getAdministration,
+  saveCheckWindows } = require('../controllers/administrator')
 
 /* Administration page */
 router.get('/home', isAuthenticated(config.ROLE_TEST_DEVELOPER), (req, res) => getAdministration(req, res))
 router.get('/check-windows', isAuthenticated(), (req, res, next) => getCheckWindows(req, res, next))
+router.get('/check-windows/:action', isAuthenticated(), (req, res, next) => checkWindowsForm(req, res, next))
+router.get('/check-windows/:action/:id', isAuthenticated(), (req, res, next) => checkWindowsForm(req, res, next))
 router.get('/check-windows/:sortField/:sortDirection', isAuthenticated(), (req, res, next) => getCheckWindows(req, res, next))
-router.get('/check-settings', isAuthenticated(config.ROLE_TEST_DEVELOPER), (req, res, next) => getUpdateTiming(req, res, next))
-router.get('/check-settings/:status', isAuthenticated(config.ROLE_TEST_DEVELOPER), (req, res, next) => getUpdateTiming(req, res, next))
-router.post('/check-settings', isAuthenticated(config.ROLE_TEST_DEVELOPER), (req, res, next) => setUpdateTiming(req, res, next))
+router.post('/check-windows/add', isAuthenticated(), (req, res, next) => saveCheckWindows(req, res, next))
+router.post('/check-windows/edit', isAuthenticated(), (req, res, next) => saveCheckWindows(req, res, next))
+router.get('/check-settings', isAuthenticated(), (req, res, next) => getUpdateTiming(req, res, next))
+router.get('/check-settings/:status', isAuthenticated(), (req, res, next) => getUpdateTiming(req, res, next))
+router.post('/check-settings', isAuthenticated(), (req, res, next) => setUpdateTiming(req, res, next))
 
 module.exports = router
