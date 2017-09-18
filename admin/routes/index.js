2f15e5dab5566feedbe23508903496b608c6b716
Paula Molinero <hello@pris54.co.uk>
2017-09-18 10:39:09 +0100

    Restored missing code.

--- admin/routes/index.js
+++ admin/routes/index.js
@@ -39,6 +39,60 @@ router.get('/profile', isAuthenticated(), (req, res) => getProfile(req, res))
 /* Administration page */
 router.get('/administrator', isAuthenticated(config.ROLE_TEST_DEVELOPER), (req, res) => getAdministration(req, res))
 /* Health check */
+/* Health check */
+async function getPing (req, res) {
+  // get build number from /build.txt
+  // get git commit from /commit.txt
+  let buildNumber = 'NOT FOUND'
+  let commitId = 'NOT FOUND'
+  try {
+    buildNumber = await getBuildNumber()
+  } catch (error) {
+
+  }
+
+  try {
+    commitId = await getCommitId()
+  } catch (error) {
+
+  }
+
+  res.setHeader('Content-Type', 'application/json')
+  let obj = {
+    'Build': buildNumber,
+    'Commit': commitId,
+    'CurrentServerTime': Date.now()
+  }
+  return res.status(200).send(obj)
+}
+
+function getCommitId () {
+  return new Promise(function (resolve, reject) {
+    var commitFilePath = path.join(__dirname, '..', 'public', 'commit.txt')
+    fs.readFile(commitFilePath, 'utf8', function (err, data) {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(new Error('NOT FOUND'))
+      }
+    })
+  })
+}
+
+function getBuildNumber () {
+  // Promise wrapper function
+  return new Promise(function (resolve, reject) {
+    var buildFilePath = path.join(__dirname, '..', 'public', 'build.txt')
+    fs.readFile(buildFilePath, 'utf8', function (err, data) {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(new Error('NOT FOUND'))
+      }
+    })
+  })
+}
+
 router.get('/ping', (req, res) => res.status(200).send('OK'))
 /* NCA Tools Authentication Endpoint */
 router.post('/auth',
