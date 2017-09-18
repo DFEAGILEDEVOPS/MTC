d57685f51f009a7c09e8787ca3b151700f8e876f
Jon Shipley <js@ovix.co.uk>
2017-09-18 20:44:20 +0100

    Fix lint error

--- admin/run-jasmine.js
+++ admin/run-jasmine.js
@@ -10,9 +10,9 @@ jasmine.jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000
 // setup console reporter
 const JasmineConsoleReporter = require('jasmine-console-reporter')
 const reporter = new JasmineConsoleReporter({
-  colors: 1,           // (0|false)|(1|true)|2
-  cleanStack: 1,       // (0|false)|(1|true)|2|3
-  verbosity: 4,        // (0|false)|1|2|(3|true)|4
+  colors: 1, // (0|false)|(1|true)|2
+  cleanStack: 1, // (0|false)|(1|true)|2|3
+  verbosity: 4, // (0|false)|1|2|(3|true)|4
   listStyle: 'indent', // "flat"|"indent"
   activity: false
 })
