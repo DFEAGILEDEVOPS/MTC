c7ddf572f50b7a8cc85d7722437b207c98a1cc63
Jon Shipley <js@ovix.co.uk>
2017-09-19 08:52:20 +0100

    Whitespace changes, editor reconfigured

--- admin/controllers/school.js
+++ admin/controllers/school.js
@@ -8,15 +8,15 @@ const ValidationError = require('../lib/validation-error')
 const errorConverter = require('../lib/error-converter')
 const hdfErrorMessages = require('../lib/errors/hdf')
 const hdfValidator = require('../lib/validator/hdf-validator')
-const {fetchPupilsData, fetchPupilAnswers, fetchScoreDetails} = require('../services/pupilService')
-const {sortRecords} = require('../utils')
+const { fetchPupilsData, fetchPupilAnswers, fetchScoreDetails } = require('../services/pupilService')
+const { sortRecords } = require('../utils')
 
 const getHome = async (req, res, next) => {
   res.locals.pageTitle = 'School Homepage'
   let schoolName = ''
 
   try {
-    const school = await School.findOne({'_id': req.user.School}).exec()
+    const school = await School.findOne({ '_id': req.user.School }).exec()
     if (!school) {
       return next(new Error(`School not found: ${req.user.School}`))
     }
@@ -26,23 +26,23 @@ const getHome = async (req, res, next) => {
   }
   return res.render('school/school-home', {
     schoolName,
-    breadcrumbs: [{'name': 'School Home'}]
+    breadcrumbs: [ { 'name': 'School Home' } ]
   })
 }
 
 const getPupils = async (req, res, next) => {
   res.locals.pageTitle = 'Pupil register'
-  const {sortColumn, sortOrder} = req.params
+  const { sortColumn, sortOrder } = req.params
   res.locals.sortColumn = sortColumn || 'lastName'
   const order = JSON.parse(sortOrder)
   res.locals.sortOrder = typeof order === 'boolean' ? !order : true
   res.locals.sortClass = order === false ? 'triangle down' : 'triangle'
-  const {pupils} = await fetchPupilsData(req.user.School)
+  const { pupils } = await fetchPupilsData(req.user.School)
   let pupilsFormatted = await Promise.all(pupils.map(async (p) => {
-    const {foreName, lastName, _id} = p
+    const { foreName, lastName, _id } = p
     const dob = moment(p.dob).format('DD/MM/YYYY')
     const answers = await fetchPupilAnswers(p._id)
-    const {score} = fetchScoreDetails(answers)
+    const { score } = fetchScoreDetails(answers)
     // TODO: Fetch pupil's group when it's implemented
     const group = 'N/A'
     return {
@@ -56,17 +56,17 @@ const getPupils = async (req, res, next) => {
   })).catch((error) => next(error))
   pupilsFormatted = sortRecords(pupilsFormatted, res.locals.sortColumn, order)
   pupilsFormatted.map((p, i) => {
-    if (pupilsFormatted[i + 1] === undefined) return
-    if (pupilsFormatted[i].foreName === pupilsFormatted[i + 1].foreName &&
-      pupilsFormatted[i].lastName === pupilsFormatted[i + 1].lastName) {
-      pupilsFormatted[i].showDoB = true
-      pupilsFormatted[i + 1].showDoB = true
+    if (pupilsFormatted[ i + 1 ] === undefined) return
+    if (pupilsFormatted[ i ].foreName === pupilsFormatted[ i + 1 ].foreName &&
+      pupilsFormatted[ i ].lastName === pupilsFormatted[ i + 1 ].lastName) {
+      pupilsFormatted[ i ].showDoB = true
+      pupilsFormatted[ i + 1 ].showDoB = true
     }
   })
   try {
     req.breadcrumbs(res.locals.pageTitle)
     res.render('school/pupil-register', {
-      highlight: new Set(Array.isArray(req.query.hl) ? req.query.hl : [req.query.hl]),
+      highlight: new Set(Array.isArray(req.query.hl) ? req.query.hl : [ req.query.hl ]),
       pupils: pupilsFormatted,
       breadcrumbs: req.breadcrumbs()
     })
@@ -77,11 +77,11 @@ const getPupils = async (req, res, next) => {
 
 const getResults = async (req, res, next) => {
   res.locals.pageTitle = 'Results'
-  const {pupils, schoolData} = await fetchPupilsData(req.user.School)
+  const { pupils, schoolData } = await fetchPupilsData(req.user.School)
   let pupilsFormatted = await Promise.all(pupils.map(async (p) => {
     const fullName = `${p.foreName} ${p.lastName}`
     const answers = await fetchPupilAnswers(p._id)
-    const {hasScore, score, percentage} = fetchScoreDetails(answers)
+    const { hasScore, score, percentage } = fetchScoreDetails(answers)
     return {
       fullName,
       hasScore,
@@ -108,7 +108,7 @@ const getResults = async (req, res, next) => {
 const downloadResults = async (req, res, next) => {
   // TODO: refactor to make it smaller
   const csvStream = csv.createWriteStream()
-  const {schoolData, pupils} = await fetchPupilsData(req.user.School)
+  const { schoolData, pupils } = await fetchPupilsData(req.user.School)
   // Format the pupils
   let pupilsFormatted = await Promise.all(pupils.map(async (p) => {
     const fullName = `${p.foreName} ${p.lastName}`
@@ -141,9 +141,9 @@ const downloadResults = async (req, res, next) => {
     }
   })).catch((error) => next(error))
   pupilsFormatted = pupilsFormatted.filter((p) => p)
-  const pupilStructure = pupilsFormatted[0]
+  const pupilStructure = pupilsFormatted[ 0 ]
   const csvContent = []
-  const csvHeaders = ['Full Name', 'Date of Birth']
+  const csvHeaders = [ 'Full Name', 'Date of Birth' ]
   // Generate the row headers
   pupilStructure.answers.forEach((answer, i) => {
     const question = `Question ${i + 1}`
@@ -155,14 +155,14 @@ const downloadResults = async (req, res, next) => {
   // Generate the pupils
   const csvPupils = []
   pupilsFormatted.forEach((p, i) => {
-    csvPupils[i] = []
+    csvPupils[ i ] = []
     Object.keys(p).forEach((k) => {
       if (k === 'answers') {
-        p[k].forEach((a) => {
-          Object.keys(a).forEach((ak) => csvPupils[i].push(a[ak].toString()))
+        p[ k ].forEach((a) => {
+          Object.keys(a).forEach((ak) => csvPupils[ i ].push(a[ ak ].toString()))
         })
       } else {
-        csvPupils[i].push(p[k])
+        csvPupils[ i ].push(p[ k ])
       }
     })
   })
@@ -177,18 +177,18 @@ const downloadResults = async (req, res, next) => {
 }
 
 const generatePins = async (req, res, next) => {
-  if (!req.body['pupil']) {
+  if (!req.body[ 'pupil' ]) {
     // TODO: inform the user via flash message?
     return res.redirect('/school/manage-pupils')
   }
-  const data = Object.values(req.body['pupil'] || null)
+  const data = Object.values(req.body[ 'pupil' ] || null)
   const chars = '23456789bcdfghjkmnpqrstvwxyz'
   const length = 5
   let pupils
 
   // fetch pupils
   try {
-    pupils = await Pupil.find({_id: data}).exec()
+    pupils = await Pupil.find({ _id: data }).exec()
   } catch (error) {
     console.error('Failed to find pupils: ' + error.message)
     return next(error)
@@ -225,16 +225,16 @@ const generatePins = async (req, res, next) => {
 const getSubmitAttendance = async (req, res, next) => {
   res.locals.pageTitle = 'Attendance register'
   req.breadcrumbs(res.locals.pageTitle)
-  const {pupils, schoolData} = await fetchPupilsData(req.user.School)
+  const { pupils, schoolData } = await fetchPupilsData(req.user.School)
   // Redirect to confirmation of submission if hdf has been signed
   if (schoolData.hdf && schoolData.hdf.signedDate) {
     return res.redirect('/school/declaration-form-submitted')
   }
   let pupilsFormatted = await Promise.all(pupils.map(async (p) => {
     const fullName = `${p.foreName} ${p.lastName}`
-    const {_id: id, hasAttended} = p
+    const { _id: id, hasAttended } = p
     const answers = await fetchPupilAnswers(p._id)
-    const {hasScore, percentage} = fetchScoreDetails(answers)
+    const { hasScore, percentage } = fetchScoreDetails(answers)
     return {
       id,
       fullName,
@@ -255,15 +255,15 @@ const getSubmitAttendance = async (req, res, next) => {
 }
 
 const postSubmitAttendance = async (req, res, next) => {
-  const attendees = req.body['attendee']
+  const attendees = req.body[ 'attendee' ]
   if (!attendees) {
     return res.redirect('/school/submit-attendance')
   }
-  const data = Object.values(req.body['attendee'] || null)
+  const data = Object.values(req.body[ 'attendee' ] || null)
   let selected
-  const {pupils} = await fetchPupilsData(req.user.School)
+  const { pupils } = await fetchPupilsData(req.user.School)
   try {
-    selected = await Pupil.find({_id: data}).exec()
+    selected = await Pupil.find({ _id: data }).exec()
   } catch (error) {
     return next(error)
   }
@@ -273,7 +273,7 @@ const postSubmitAttendance = async (req, res, next) => {
   // Expire all pins for school pupils
   pupils.forEach(p => (p.pinExpired = true))
   const pupilsPromises = pupils.map(p => p.save())
-  Promise.all([selectedPromises, pupilsPromises]).then(() => {
+  Promise.all([ selectedPromises, pupilsPromises ]).then(() => {
     return res.redirect('/school/declaration-form')
   },
   error => next(error))
@@ -282,11 +282,11 @@ const postSubmitAttendance = async (req, res, next) => {
 }
 
 const getDeclarationForm = async (req, res, next) => {
-  const {schoolData} = await fetchPupilsData(req.user.School)
+  const { schoolData } = await fetchPupilsData(req.user.School)
   if (schoolData.hdf && schoolData.hdf.signedDate) {
     return res.redirect('/school/declaration-form-submitted')
   }
-  req.body['fullName'] = req.user && req.user['UserName']
+  req.body[ 'fullName' ] = req.user && req.user[ 'UserName' ]
   res.locals.pageTitle = 'Headteacher\'s declaration form'
   req.breadcrumbs('Attendance register')
   req.breadcrumbs(res.locals.pageTitle)
@@ -298,8 +298,8 @@ const getDeclarationForm = async (req, res, next) => {
 }
 
 const postDeclarationForm = async (req, res, next) => {
-  const {jobTitle, fullName, declaration} = req.body
-  const school = await School.findOne({'_id': req.user.School}).exec()
+  const { jobTitle, fullName, declaration } = req.body
+  const school = await School.findOne({ '_id': req.user.School }).exec()
   school.hdf = {
     signedDate: Date.now(),
     declaration,
@@ -342,11 +342,11 @@ const getHDFSubmitted = async (req, res, next) => {
   req.breadcrumbs(res.locals.pageTitle)
   let school
   try {
-    school = await School.findOne({'_id': req.user.School}).exec()
+    school = await School.findOne({ '_id': req.user.School }).exec()
   } catch (error) {
     return next(error)
   }
-  const {hdf: {signedDate}} = school
+  const { hdf: { signedDate } } = school
   return res.render('school/declaration-form-submitted', {
     breadcrumbs: req.breadcrumbs(),
     signedDate: signedDate && moment(signedDate).format('Do MMMM YYYY')
