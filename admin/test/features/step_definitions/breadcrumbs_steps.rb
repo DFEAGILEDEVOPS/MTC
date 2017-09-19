f0e8ad602e647c0964173d510834455c22d6a219
activemq <m-qureshi@hotmail.com>
2017-09-19 13:09:59 +0100

    adding tests for scaff work

--- admin/test/features/step_definitions/breadcrumbs_steps.rb
+++ admin/test/features/step_definitions/breadcrumbs_steps.rb
@@ -13,3 +13,7 @@ end
 When(/^I am on the manage check windows page$/) do
   admin_page.manage_check_windows.click
 end
+
+When(/^I am on the pupils not taking check page$/) do
+  school_landing_page.pupils_not_taking_check.click
+end
