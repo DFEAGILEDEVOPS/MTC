a4cc3cc83ed33e84df61c73990245ba6b6c2f2b3
Amit Sinha <amit.kumar.sinha@businessthreezero.com>
2017-09-20 18:03:29 +0100

    added api testing for security stuff

--- api_tests/helpers/request_helper.rb
+++ api_tests/helpers/request_helper.rb
@@ -6,5 +6,13 @@ class RequestHelper
     self.class.post(BASE_URL+"/api/questions", :body => {:'schoolPin' => school_pin, :'pupilPin' => pupil_pin})
   end
 
+  def admin_home()
+    self.class.get(BASE_URL)
+  end
+
+  def spa_home()
+    self.class.get('http://localhost:3002')
+  end
+
 
 end
