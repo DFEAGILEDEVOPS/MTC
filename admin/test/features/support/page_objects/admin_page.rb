44ed880bed6311d8fb59540ba12c23879f3dac91
Jon Shipley <js@ovix.co.uk>
2017-09-21 06:36:31 +0100

    Update tests with revised xpath for support links

--- admin/test/features/support/page_objects/admin_page.rb
+++ admin/test/features/support/page_objects/admin_page.rb
@@ -7,12 +7,12 @@ class AdminPage < SitePrism::Page
   element :manage_check_windows, 'a', text: 'Manage check windows'
   element :manage_check_windows_text, 'p', text: 'Create and edit check administration windows'
   element :check_settings, "a[href='/administrator/check-settings']", text: 'Adjust question timings'
-  element :check_settings_text, "p", text: 'Change the setting of the check, for example timings of questions.'
+  element :check_settings_text, 'p', text: 'Change the setting of the check, for example timings of questions.'
   element :view_progress_reports, 'a', text: 'View progress reports'
   element :view_progress_reports_text, 'p', text: 'View reports of this check window.'
   element :manage_retake_requests, 'a', text: 'Manage retake requests'
   element :manage_retake_requests_text, 'p', text: 'View retake requests and approve or reject.'
-  element :guidance, "h3 + .list li a", text: 'Guidance'
-  element :back_to_top, "a", text: 'Back to top'
+  element :guidance, 'aside.support-column nav li a', text: 'Guidance'
+  element :back_to_top, 'a', text: 'Back to top'
 
 end
