63ffd90a5d48a57cf596a136699bd230035f6f01
Amit Sinha <amit.kumar.sinha@businessthreezero.com>
2017-09-25 11:06:56 +0100

    updated test from review comment

--- api_tests/spec/security_spec.rb
+++ api_tests/spec/security_spec.rb
@@ -1,5 +1,5 @@
 describe 'Security API spec' do
-  context 'GET request to admin home' do
+  context 'GET request to admin home', :azure do
     it 'should return response header' do
       request_helper = RequestHelper.new
       response = request_helper.admin_home
@@ -8,15 +8,10 @@ describe 'Security API spec' do
       expect(response.headers['x-frame-options']).to eql 'SAMEORIGIN'
       expect(response.headers['strict-transport-security']).to eql "max-age=31536000"
       expect(response.headers['x-download-options']).to eql 'noopen'
-      expect(response.headers['x-content-type-options']).to eql 'nosniff'
-      expect(response.headers['x-xss-protection']).to eql "1; mode=block"
-      expect(response.headers['content-security-policy']).to eql "default-src 'self'; script-src 'self' 'unsafe-inline' https://www.google-analytics.com; font-src 'self' data:; style-src 'self'; img-src 'self' https://www.google-analytics.com; connect-src 'self'; object-src 'none'; media-src 'none'; child-src 'none'"
-      expect(response.headers['x-content-security-policy']).to eql "default-src 'self'; script-src 'self' 'unsafe-inline' https://www.google-analytics.com; font-src 'self' data:; style-src 'self'; img-src 'self' https://www.google-analytics.com; connect-src 'self'; object-src 'none'; media-src 'none'; child-src 'none'"
-      expect(response.headers['x-webkit-csp']).to eql "default-src 'self'; script-src 'self' 'unsafe-inline' https://www.google-analytics.com; font-src 'self' data:; style-src 'self'; img-src 'self' https://www.google-analytics.com; connect-src 'self'; object-src 'none'; media-src 'none'; child-src 'none'"
     end
   end
 
-  context 'GET request to SPA home' do
+  context 'GET request to SPA home', :azure do
     it 'should return response header' do
       request_helper = RequestHelper.new
       response = request_helper.spa_home
