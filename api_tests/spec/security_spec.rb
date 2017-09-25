95215406f5c484ab3f689a3bc38b0cd3ca6a0c64
Amit Sinha <amit.kumar.sinha@businessthreezero.com>
2017-09-25 11:17:15 +0100

    updated test from review comment

--- api_tests/spec/security_spec.rb
+++ api_tests/spec/security_spec.rb
@@ -8,6 +8,9 @@ describe 'Security API spec' do
       expect(response.headers['x-frame-options']).to eql 'SAMEORIGIN'
       expect(response.headers['strict-transport-security']).to eql "max-age=31536000"
       expect(response.headers['x-download-options']).to eql 'noopen'
+      expect(response.headers['x-content-type-options']).to eql 'nosniff'
+      expect(response.headers['x-xss-protection']).to eql "1; mode=block"
+      expect(response.headers['content-security-policy']).to eql "default-src 'self'; script-src 'self' 'unsafe-inline' https://www.google-analytics.com; font-src 'self' data:; style-src 'self'; img-src 'self' https://www.google-analytics.com; connect-src 'self'; object-src 'none'; media-src 'none'; child-src 'none'"
     end
   end
 
