024168b3aa0c0b5b2fa294f420205db3bb6d4713
Jon Shipley <js@ovix.co.uk>
2017-09-26 13:28:04 +0100

    Add new check model to store that a pupil has started a check

---
+++ admin/models/check.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+const Schema = mongoose.Schema
+
+const Check = new Schema({
+  pupilId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'Pupil'
+  },
+  checkWindowId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'CheckWindow'
+  },
+  checkFormId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'CheckForm'
+  }
+}, {timestamps: true})
+
+module.exports = mongoose.model('Check', Check)
---
+++ admin/spec/models/check.spec.js
@@ -0,0 +1,52 @@
+'use strict'
+/* global describe beforeEach it expect */
+
+const { Types } = require('mongoose')
+const Check = require('../../models/check')
+
+describe('check model', () => {
+  let check
+
+  beforeEach(() => {
+    check = new Check({
+      _id: Types.ObjectId(),
+      pupilId: Types.ObjectId(),
+      checkWindowId: Types.ObjectId(),
+      checkFormId: Types.ObjectId()
+    })
+  })
+
+  it('validates a model correctly', (done) => {
+    check.validate((error) => {
+      expect(error).toBeNull()
+      done()
+    })
+  })
+
+  it('requires a pupilId', (done) => {
+    check.pupilId = undefined
+    check.validate((error) => {
+      expect(error).toBeDefined()
+      expect(error.errors.pupilId).toBeDefined()
+      done()
+    })
+  })
+
+  it('requires a checkWindowId', (done) => {
+    check.checkWindowId = undefined
+    check.validate((error) => {
+      expect(error).toBeDefined()
+      expect(error.errors.checkWindowId).toBeDefined()
+      done()
+    })
+  })
+
+  it('requires a checkFormId', (done) => {
+    check.checkFormId = undefined
+    check.validate((error) => {
+      expect(error).toBeDefined()
+      expect(error.errors.checkFormId).toBeDefined()
+      done()
+    })
+  })
+})
