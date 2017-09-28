1c238b26959403d88d60cc496b64362e7c881397
activemq <m-qureshi@hotmail.com>
2017-09-28 23:05:26 +0100

    adding check for start date to be in the past, and also that if it is in the future pupils cant login

--- pupil-spa/test/features/step_definitions/pupil_auth_steps.rb
+++ pupil-spa/test/features/step_definitions/pupil_auth_steps.rb
@@ -8,6 +8,7 @@ Then(/^I should see meta data stored in the DB$/) do
   expect(stored_check['pupilId']).to eql @pupil_information['_id']
   check_window_id = stored_check['checkWindowId'].to_s
   MongoDbHelper.get_check_window(check_window_id)
+  expect(MongoDbHelper.get_check_window(check_window_id)['startDate'] < current_time).to be_truthy
   expect(MongoDbHelper.get_check_window(check_window_id)['endDate'] > current_time).to be_truthy
   questions_recieved = JSON.parse page.evaluate_script('window.localStorage.getItem("questions");')
   questions_expected = MongoDbHelper.get_form(100)['questions']
@@ -15,23 +16,28 @@ Then(/^I should see meta data stored in the DB$/) do
   expect(stored_check['checkStartDate'].strftime('%d-%m-%y %H:%M')).to eql current_time.utc.strftime('%d-%m-%y %H:%M')
 end
 
-
 Given(/^I have failed to login$/) do
   @checks_count = MongoDbHelper.number_of_checks
   step "I have attempted to enter a school I do not attend upon login"
 end
 
-
 Then(/^I should see no pupil metadata stored$/) do
   expect(@checks_count).to eql MongoDbHelper.number_of_checks
 end
 
-
-Given(/^I attempt to login whilst the check window is not open$/) do
+Given(/^I attempt to login whilst the check window is not open as the end date is in the past$/) do
   original = MongoDbHelper.get_check_window_via_name('Summer 2017')
-  @original_date = original['endDate']
+  @original_date_time = {endDate: original['endDate']}
+  updated = original.each{ |key,str| original['endDate'] = Time.now - 60}
+  collection=CLIENT[:checkwindows]
+  collection.update_one({'_id' => original['_id']}, updated)
+  step 'I have logged in'
+end
 
-  updated = original.each{ |key,str| original['endDate'] = Time.now - 400}
+Given(/^I attempt to login whilst the check window is not open as the start date is in the future$/) do
+  original = MongoDbHelper.get_check_window_via_name('Summer 2017')
+  @original_date_time = {startDate: original['startDate']}
+  updated = original.each{ |key,str| original['startDate'] = Time.now + 60}
   collection=CLIENT[:checkwindows]
   collection.update_one({'_id' => original['_id']}, updated)
   step 'I have logged in'
--- pupil-spa/test/features/support/hooks.rb
+++ pupil-spa/test/features/support/hooks.rb
@@ -10,10 +10,10 @@ Before('@non_browserstack_compliant') do
   skip_this_scenario if Capybara.current_driver.to_s.include? 'bs'
 end
 
-After('@end_date_reset') do
+After('@window_date_time_reset') do
   original = MongoDbHelper.get_check_window_via_name('Summer 2017')
-  updated = original.each{ |key,str| original['endDate'] = @original_date}
+  updated = original.each{ |key,str| original[@original_date_time.keys.first.to_s] = @original_date_time.values.first}
   collection=CLIENT[:checkwindows]
   collection.update_one({'_id' => original['_id']}, updated)
-  p "Returned end date to #{@original_date}"
+  p "Returned #{@original_date_time.keys.first.to_s} to #{@original_date_time.values.first}"
 end
