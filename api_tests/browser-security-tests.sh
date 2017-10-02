29aa39a328ba092ed615f4a4375933bf438b2768
Guy <gpharwood@gmail.com>
2017-10-02 08:47:23 +0100

    failing with code 127

--- api_tests/browser-security-tests.sh
+++ api_tests/browser-security-tests.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -x
-
+echo "running browser security tests with tag $2 against site $1"
 gem install bundler
 bundle install
-echo "checking security headers against $1"
 BASE_URL=$1 rspec -t $2
