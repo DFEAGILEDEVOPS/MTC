b701d1a001474901460eaae9161970913456cca0
Guy Harwood <guy@milkchip.com>
2017-10-10 19:42:54 +0100

    express js footprint

---
+++ checkapi/controllers/auth.js
@@ -0,0 +1,10 @@
+const auth = (req, res, next) => {
+  // TODO parse request body
+  // TODO authenticate
+  // TODO create JWT token
+  // TODO return check with token as HTTP only
+}
+
+module.exports = {
+  auth
+}
---
+++ checkapi/controllers/completeCheck.js
@@ -0,0 +1,10 @@
+
+const completeCheck = (req, res, next) => {
+  // TODO: JWT token authentication
+  // TODO: request validation
+  // TODO: submit to backend
+}
+
+module.exports = {
+  completeCheck
+}
