53829421b5682f12144e76a35a75883f4d6d8c94
activemq <m-qureshi@hotmail.com>
2017-10-10 12:12:02 +0100

    updated with new copy

--- admin/test/features/support/page_objects/pupils_not_taking_check_page.rb
+++ admin/test/features/support/page_objects/pupils_not_taking_check_page.rb
@@ -2,7 +2,7 @@ class PupilsNotTakingCheckPage < SitePrism::Page
   set_url '/school/pupils-not-taking-check'
 
   element :heading, '.heading-xlarge', text: 'Pupils not taking the check'
-  element :info_text, 'p.lede', text: "Click on the button below to add reasons for pupils who are not taking the check"
+  element :info_text, 'p.lede', text: "All pupils should be considered for the multiplication tables check at the end of year 4. If the headteacher decides a pupil should not take the check, they must provide a reason."
   element :add_reason, 'a', text: "Add reason for pupil"
   element :back_to_top, 'a', text: "Back to top"
   element :generate_pins, 'a', text: "Generate PINs"
