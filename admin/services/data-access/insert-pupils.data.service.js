94c08fc3ebaf16698abcd5090ee789423dd76ad2
George Chatzigiannis <george.chatzigiannis@smartfocus.com>
2017-10-11 10:20:55 +0100

    Remove pointless azure tests and allow mongoose error to be naturally thrown back to the caller

--- admin/services/data-access/insert-pupils.data.service.js
+++ admin/services/data-access/insert-pupils.data.service.js
@@ -1,10 +1,6 @@
 const Pupil = require('../../models/pupil')
 
 module.exports.insertMany = async (pupils) => {
-  const savedPupils = await Pupil.insertMany(pupils, (err) => {
-    if (err) {
-      throw new Error(err)
-    }
-  })
+  const savedPupils = await Pupil.insertMany(pupils)
   return savedPupils
 }
