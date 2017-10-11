397be56d0d803dfed886abd3e6b2552e9030a200
George Chatzigiannis <george.chatzigiannis@smartfocus.com>
2017-10-12 00:39:11 +0100

    Change expected date format to DD/MM/YYYY and filter out duplicate error messages

--- admin/services/single-pupil-validation.service.js
+++ admin/services/single-pupil-validation.service.js
@@ -14,15 +14,15 @@ module.exports.validate = async (single, school) => {
   })
   const dob = single[ 4 ].split('/')
   const pupilData = Object.assign({
-    'dob-day': dob[ 1 ],
-    'dob-month': dob[ 0 ],
+    'dob-day': dob[ 0 ],
+    'dob-month': dob[ 1 ],
     'dob-year': dob[ 2 ]
   }, pupil)
   const validationError = await validate(pupilData)
   if (validationError.hasError()) {
     single[ 6 ] = []
     Object.keys(validationError.errors).forEach((e) => single[ 6 ].push(validationError.errors[ e ]))
-    single[ 6 ] = single[ 6 ].join(', ')
+    single[ 6 ] = single[ 6 ].filter((err, i, arr) => arr.indexOf(err) === i).join(', ')
   }
   return { pupil, single }
 }
