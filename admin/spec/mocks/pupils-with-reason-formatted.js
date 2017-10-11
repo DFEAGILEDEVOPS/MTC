b4a06ed1389229ac1a3bcef8d316d74a7580fc4e
Paula Molinero <hello@pris54.co.uk>
2017-10-11 10:17:26 +0100

    Unit tests in progress.

---
+++ admin/spec/mocks/pupils-with-reason-formatted.js
@@ -0,0 +1,58 @@
+const moment = require('moment')
+
+module.exports = [
+  {
+    _id: '59d02aad9b865f35a3f5193e',
+    updatedAt: moment('2017-10-10 11:43:49').toDate(),
+    createdAt: moment('2017-09-30 23:37:17').toDate(),
+    school: 9991001,
+    upn: 'F935322311201',
+    foreName: 'John',
+    lastName: 'Smith',
+    middleNames: 'Aaron',
+    gender: 'M',
+    dob: moment('1987-02-18 00:00:00').toDate(),
+    pin: 'rv8fh',
+    hasAttended: false,
+    pinExpired: false,
+    __v: 0,
+    id: null,
+    reason: 'Absent'
+  },
+  {
+    _id: '59d02aad9b865f35a3f5176b',
+    updatedAt: moment('2017-10-10 11:43:49').toDate(),
+    createdAt: moment('2017-09-30 23:37:17').toDate(),
+    school: 9991001,
+    upn: 'X765812302245',
+    foreName: 'Tom',
+    lastName: 'Miller',
+    middleNames: 'Richard',
+    gender: 'M',
+    dob: moment('1987-03-13 00:00:00').toDate(),
+    pin: '6rog4',
+    hasAttended: false,
+    pinExpired: false,
+    __v: 0,
+    id: null,
+    reason: 'Left school'
+  },
+  {
+    _id: '59d02aad9b865f35a3f5127v',
+    updatedAt: moment('2017-10-10 11:43:49').toDate(),
+    createdAt: moment('2017-09-30 23:37:17').toDate(),
+    school: 9991001,
+    upn: 'X765093405836',
+    foreName: 'Sarah',
+    lastName: 'Connor',
+    middleNames: '',
+    gender: 'F',
+    dob: moment('1987-04-20 00:00:00').toDate(),
+    pin: 'hf7r2',
+    hasAttended: false,
+    pinExpired: false,
+    __v: 0,
+    id: null,
+    reason: 'Incorrect Registration'
+  }
+]
