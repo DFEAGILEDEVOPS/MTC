1566a0af5ac91363258a2af7349d917fd2b2879c
Guy Harwood <guy@milkchip.com>
2017-10-13 14:28:24 +0100

    change check to binary

--- checkapi/services/data-access/complete-check.data.service.azure-table.js
+++ checkapi/services/data-access/complete-check.data.service.azure-table.js
@@ -15,7 +15,7 @@ completedCheckDataService.create = (data) => new Promise((resolve, reject) => {
   var item = {
     PartitionKey: entityGenerator.String('completedchecks'),
     RowKey: entityGenerator.String(uuid().toString()),
-    check: entityGenerator.String(encodedData)
+    check: entityGenerator.Binary(encodedData)
   }
   // TODO - can we optimise this by newing up only once in service?
   const tableService = azureStorage.createTableService()
