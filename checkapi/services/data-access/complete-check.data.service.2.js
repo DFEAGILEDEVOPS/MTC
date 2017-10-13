32645bf4d3d415081b18aee2bf103cb366b01634
Guy Harwood <guy@milkchip.com>
2017-10-13 09:32:38 +0100

    remove console log

--- checkapi/services/data-access/complete-check.data.service.2.js
+++ checkapi/services/data-access/complete-check.data.service.2.js
@@ -10,7 +10,6 @@ const mongoService = require('./mongo.service')
 completedCheckDataService.create = async function (data) {
   const mongo = mongoService.get()
   const checks = mongo.collection('completedchecks')
-  console.log('inserting:', data)
   await checks.insert(data)
 }
 
