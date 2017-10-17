992de21ee840ac89072e54aafb5436a12062fec4
Guy Harwood <guy@milkchip.com>
2017-10-17 15:46:33 +0100

    add routing restriction

--- checkapi/lean.js
+++ checkapi/lean.js
@@ -2,9 +2,11 @@
 
 require('dotenv').config()
 const http = require('http')
+const timer = require('simple-timer')
 const azure = require('azure-storage')
 const queueService = azure.createQueueService()
 const queueName = 'completedchecks'
+
 queueService.createQueueIfNotExists(queueName, function (error, result, response) {
   if (error) {
     throw new Error('unable to connect to azure storage:' + error.message)
@@ -14,6 +16,10 @@ queueService.createQueueIfNotExists(queueName, function (error, result, response
 
 http.createServer((request, response) => {
   // const { headers, method, url } = request
+  if (request.method !== 'POST' || request.url !== '/complete-check') {
+    response.statusCode = 404
+    response.end()
+  }
   let body = []
   request.on('error', (err) => {
     console.error(err)
@@ -21,14 +27,20 @@ http.createServer((request, response) => {
     body.push(chunk)
   }).on('end', () => {
     body = Buffer.concat(body).toString()
+    response.on('error', (err) => {
+      console.error(err)
+    })
+
     // TODO validation of request, authentication
+    timer.start('request')
     queueService.createMessage(queueName, body, function (error, result, queueResponse) {
+      timer.stop('request')
+      console.log(`queue call took ${timer.get('request').delta}ms`)
       if (error) {
         console.error(error)
+        response.writeHead(500)
+        response.end()
       }
-      response.on('error', (err) => {
-        console.error(err)
-      })
       response.writeHead(200, { 'Content-Type': 'application/json' })
       response.end()
     })
