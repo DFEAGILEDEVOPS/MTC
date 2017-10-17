bbc3f9103a1b44b9225c69c43ed4f1a67ac451be
Paula Molinero <hello@pris54.co.uk>
2017-10-17 10:47:46 +0100

    Updated data service tests.

--- admin/spec/service/data-access/pupils-not-taking-check.data.service.spec.js
+++ admin/spec/service/data-access/pupils-not-taking-check.data.service.spec.js
@@ -6,7 +6,10 @@ const sinon = require('sinon')
 require('sinon-mongoose')
 
 const Pupil = require('../../../models/pupil')
+const AttendanceCode = require('../../../models/attendance-code')
+
 const pupilsWithReasonsMock = require('../../mocks/pupils-with-reason')
+const attendanceCodesMock = require('../../mocks/attendance-codes')
 
 describe('pupils-not-taking-check.data.service', () => {
   let service, sandbox
@@ -29,6 +32,22 @@ describe('pupils-not-taking-check.data.service', () => {
 
     it('should return a list of pupils with reasons', () => {
       service.fetchPupilsWithReasons(9991001)
+      expect(mock.verify()).toBe(true)
+    })
+  })
+
+  describe('#getAttendanceCodes', () => {
+    let mock
+
+    beforeEach(() => {
+      mock = sinon.mock(AttendanceCode).expects('find').chain('sort').resolves(attendanceCodesMock)
+      service = proxyquire('../../../services/data-access/pupils-not-taking-check.data.service', {
+        '../../models/attencance-code': AttendanceCode
+      })
+    })
+
+    it('should return a list of attendance codes', () => {
+      service.getAttendanceCodes()
       expect(mock.verify()).toBe(true)
     })
   })
