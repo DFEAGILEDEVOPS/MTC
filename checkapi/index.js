928be4b3e3d60221d433a7acbd740efc025c3e24
Guy Harwood <guy@milkchip.com>
2017-10-18 16:40:30 +0100

    improve routing spaghetti code

--- checkapi/index.js
+++ checkapi/index.js
@@ -16,6 +16,16 @@ const entityGenerator = azure.TableUtilities.entityGenerator
 const uuid = require('uuidv4')
 const debug = process.env.NODE_ENV !== 'production'
 
+// grab disk data...
+let buildId
+try {
+  buildId = fs.readFileSync(path.join(__dirname, 'build.txt'))
+} catch (error) {
+  buildId = 'unknown'
+}
+
+const loaderid = fs.readFileSync(path.join(__dirname, 'public', 'loaderio-99a9565f58325293e217a36aa3ae695d.txt'))
+
 storageService.createTableIfNotExists(storageTargetName, function (error, result, response) {
   if (error) {
     throw new Error('unable to connect to azure storage:' + error.message)
@@ -26,16 +36,23 @@ storageService.createTableIfNotExists(storageTargetName, function (error, result
 console.log(`running on ${machineName} (${os.cpus().length} cores) under process ${processId}.`)
 
 http.createServer((request, response) => {
-  // const { headers, method, url } = request
-  if (request.method === 'GET' && request.url === '/') {
-    response.write('Raw')
+  if (request.method === 'GET') {
     response.statusCode = 200
-    response.end()
-  }
-
-  if (request.method === 'GET' && request.url === '/loaderio-99a9565f58325293e217a36aa3ae695d.txt') {
-    response.writeHead(200)
-    response.end(fs.readFileSync(path.join(__dirname, 'public', 'loaderio-99a9565f58325293e217a36aa3ae695d.txt')))
+    switch (request.url) {
+      case '/':
+        response.end(`Hello from ${machineName}`)
+        break
+      case '/ping':
+        response.end(buildId)
+        break
+      case '/loaderio-99a9565f58325293e217a36aa3ae695d.txt':
+        response.end(loaderid)
+        break
+      default:
+        response.statusCode = 404
+        response.end()
+    }
+    return
   }
 
   if (request.method !== 'POST' || request.url !== '/complete-check') {
