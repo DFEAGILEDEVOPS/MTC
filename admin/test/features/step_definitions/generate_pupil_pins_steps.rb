771348df84b4c8fd62ee054d63da633c61ecc19b
Amit Sinha <amit.kumar.sinha@businessthreezero.com>
2017-10-19 13:17:50 +0100

    updated test as per review comment

--- admin/test/features/step_definitions/generate_pupil_pins_steps.rb
+++ admin/test/features/step_definitions/generate_pupil_pins_steps.rb
@@ -1,7 +1,6 @@
 
 And(/^I am on the generate pupil pins page$/) do
   school_landing_page.generate_pupil_pin.click
-  @page = generate_pupil_pins_page
 end
 
 Then(/^I should see a heading for the Generate Pupil Pins$/) do
@@ -9,7 +8,7 @@ Then(/^I should see a heading for the Generate Pupil Pins$/) do
 end
 
 And(/^I can see the info message for generating the pupil pin$/) do
-  expect(@page.has_content?('Pupils will need a Personal Identification Number (PIN) and school password in order to start the check. These expire daily.')).to be_truthy, "Expected text: 'Pupils will need a Personal Identification Number (PIN) and school password in order to start the check. These expire daily.' ... Not found"
-  expect(@page.info_message.map {|message| message.text}).to include "Generate pupil Personal Identification Numbers (PINs) and a school password."
-  expect(@page.info_message.map {|message| message.text}).to include "Distribute the PINs and password to your pupils."
+  expect(generate_pupil_pins_page).to have_generate_pin_message
+  expect(generate_pupil_pins_page.info_message.map {|message| message.text}).to include "Generate pupil Personal Identification Numbers (PINs) and a school password."
+  expect(generate_pupil_pins_page.info_message.map {|message| message.text}).to include "Distribute the PINs and password to your pupils."
 end
