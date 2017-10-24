f7ec5e60229ab2997f48d60b0b83415cc9e19d46
Jon Shipley <js@ovix.co.uk>
2017-10-24 18:27:20 +0100

    Update mocks

--- admin/spec/mocks/check-with-results.js
+++ admin/spec/mocks/check-with-results.js
@@ -2,7 +2,7 @@ module.exports = {
   '_id': '59e649e6a3df42803ed3b95f',
   'updatedAt': '2017-10-17T18:20:22.817Z',
   'createdAt': '2017-10-17T18:20:22.817Z',
-  'pupilId': '59e643f947497b803c9db2e4',
+  'pupilId': '595cd5416e5ca13e48ed2518',
   'checkCode': '35961f41-e746-4a5c-bcec-c8957b12dffe',
   'checkWindowId': '59e643f847497b803c9db2d8',
   'checkFormId': 100,
