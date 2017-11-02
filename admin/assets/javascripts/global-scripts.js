fea4a1b0fb4d348975236974056007732c0f7ef5
Paula <hello@pris54.co.uk>
2017-11-02 11:51:45 +0000

    jQuery refactoring in progress.

--- admin/assets/javascripts/global-scripts.js
+++ admin/assets/javascripts/global-scripts.js
@@ -3,51 +3,56 @@
  */
 /* global $ */
 $(function () {
-  /**
-   * Tick all checkboxes. Common functionality.
-   * @param sel
-   * @param e
-   */
-  function tickAllCheckboxes (sel, e) {
-    $('#tickAllCheckboxes').on('change', function () {
-      $(sel + ' > tbody div > input:checkbox').not('[disabled]').prop('checked', ($(this).is(':checked')))
-      if ($('#selectAll')) {
-        if ($(this).is(':checked') === true) {
-          $('#selectAll').addClass('all-hide')
-          $('#unselectAll').removeClass('all-hide')
-        } else {
-          $('#unselectAll').addClass('all-hide')
-          $('#selectAll').removeClass('all-hide')
+  var checkboxesStatus = {
+    /**
+     * Check/uncheck all checkboxes (text links)
+     * @param sel
+     * @param e
+     */
+    toggleAllCheckboxes: function (sel, e) {
+      $('#tickAllCheckboxes').on('change', function () {
+        $(sel + ' > tbody div > input:checkbox').not('[disabled]').prop('checked', ($(this).is(':checked')))
+        var selectAll = $('#selectAll')
+        var unselectAll = $('#unselectAll')
+        if (selectAll) {
+          if ($(this).is(':checked') === true) {
+            selectAll.addClass('all-hide')
+            unselectAll.removeClass('all-hide')
+          } else {
+            unselectAll.addClass('all-hide')
+            selectAll.removeClass('all-hide')
+          }
         }
-      }
-    })
-  }
+      })
+    },
+    /**
+     * Radio, checkboxes and link interaction for page 'pupils not taking check'.
+     * @param sel
+     * @param e
+     */
+    checkboxCheckAll: function (sel, e) {
+      if ($(sel)) {
+        var lengthAll = $(sel + ' > tbody div > input:checkbox').length
+        var lengthChecked = $(sel + ' > tbody div > input:checkbox:disabled').length
 
-  /**
-   * Radio, checkboxes and link interaction for page 'pupils not taking check'.
-   * @param sel
-   * @param e
-   */
-  function disableCheckAll (sel, e) {
-    if ($(sel)) {
-      var lengthAll = $(sel + ' > tbody div > input:checkbox').length
-      var lengthChecked = $(sel + ' > tbody div > input:checkbox:disabled').length
-
-      if (lengthAll === lengthChecked) {
-        $('#tickAllCheckboxes').prop('disabled', true)
-      } else {
-        $('#tickAllCheckboxes').prop('disabled', false)
+        if (lengthAll === lengthChecked) {
+          $('#tickAllCheckboxes').prop('disabled', true)
+        } else {
+          $('#tickAllCheckboxes').prop('disabled', false)
+        }
       }
     }
   }
 
-  if ($('#checkFormsList').length > 0) tickAllCheckboxes('#checkFormsList')
-  if ($('#pupilsList').length > 0) tickAllCheckboxes('#pupilsList')
-  if ($('#attendanceList').length > 0)tickAllCheckboxes('#attendanceList')
+  if ($('#checkFormsList').length > 0) checkboxesStatus.toggleAllCheckboxes('#checkFormsList')
+  if ($('#pupilsList').length > 0) checkboxesStatus.toggleAllCheckboxes('#pupilsList')
+  if ($('#attendanceList').length > 0) checkboxesStatus.toggleAllCheckboxes('#attendanceList')
+  if ($('#generatePins').length > 0) checkboxesStatus.toggleAllCheckboxes('#generatePins')
 
-  if ($('#checkFormsList').length > 0) disableCheckAll('#checkFormsList')
-  if ($('#pupilsList').length > 0) disableCheckAll('#pupilsList')
-  if ($('#attendanceList').length > 0) disableCheckAll('#attendanceList')
+  if ($('#checkFormsList').length > 0) checkboxesStatus.checkboxCheckAll('#checkFormsList')
+  if ($('#pupilsList').length > 0) checkboxesStatus.checkboxCheckAll('#pupilsList')
+  if ($('#attendanceList').length > 0) checkboxesStatus.checkboxCheckAll('#attendanceList')
+  if ($('#generatePins').length > 0) checkboxesStatus.checkboxCheckAll('#generatePins')
 
   /**
    * Pupils not taking the check methods.
@@ -101,17 +106,20 @@ $(function () {
      * 'Select all' checkboxes link.
      */
     $('#selectAll').on('click', function (e) {
+      console.log('SELECT ALL')
       $(this).addClass('all-hide')
       $('#unselectAll').removeClass('all-hide')
       $('.multiple-choice-mtc > input:checkbox').attr('data-checked', true)
       isRadioChecked()
       countCheckboxes()
+      enableButtonStatus(true)
     })
 
     /**
      * 'Unselect all' checkboxes link.
      */
     $('#unselectAll').on('click', function (e) {
+      console.log('UNSELECT ALL')
       $(this).addClass('all-hide')
       $('#selectAll').removeClass('all-hide')
       enableButtonStatus(false)
@@ -127,7 +135,7 @@ $(function () {
         isRadioChecked()
       } else {
         $('.multiple-choice-mtc > input:checkbox').attr('data-checked', null)
-        enableButtonStatus(true)
+        enableButtonStatus(false)
       }
       countCheckboxes()
     })
@@ -164,10 +172,13 @@ $(function () {
    */
   var generatePins = function () {
     $('#generatePins .multiple-choice-mtc > input:checkbox').on('click', function () {
+      console.log('GENERATE PINS CLICK')
       var el = $('#generatePins .multiple-choice-mtc > input:checkbox:checked')
-      if (el.length > 0) {
+      if (el.length > 0 || ($('#selectAll').hasClass('all-hide'))) {
+        console.log('SHOW STICKY BANNER (2)')
         $('#stickyBanner').addClass('show')
       } else {
+        console.log('HIDE STICKY BANNER')
         $('#stickyBanner').removeClass('show')
       }
     })
