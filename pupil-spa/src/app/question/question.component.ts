43992f60eb8f73a316c83a59bca620b0b4080e2d
Jon Shipley <js@ovix.co.uk>
2017-11-06 13:29:13 +0000

    Remove debug logging from SPA

--- pupil-spa/src/app/question/question.component.ts
+++ pupil-spa/src/app/question/question.component.ts
@@ -87,7 +87,7 @@ export class QuestionComponent implements OnInit, AfterViewInit {
    */
   @HostListener('document:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
-    console.log('question.component: handleKeyboardEvent(): event: ', event);
+    // console.log('question.component: handleKeyboardEvent(): event: ', event);
     const key = event.key;
     // register inputs
     this.registerInputService.addEntry(event);
