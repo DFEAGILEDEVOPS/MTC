83d8d702e5a8a5e4d53dec3ca6492725d8614b2b
George Chatzigiannis <george.chatzigiannis@smartfocus.com>
2017-11-07 12:32:31 +0000

    test url typo

--- admin/spec/service/qr.service.spec.js
+++ admin/spec/service/qr.service.spec.js
@@ -2,35 +2,29 @@
 /* global describe, it, expect */
 
 const qrService = require('../../services/qr.service')
-const qrURL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEU' +
-  'gAAAIQAAACECAYAAABRRIOnAAAAAklEQVR4AewaftIAAAN2SURBVO3B' +
-  'QY5jBxYDweSD7n/lHC+84OoDglTlHpsR8S/M/O2YKcdMOWbKMVOOmXL' +
-  'MlGOmHDPlmCnHTDlmyjFTjplyzJRjprz4UBJ+k8qTJLxD5UkS3qHSkv' +
-  'CbVD5xzJRjphwz5cWXqXxTEp4k4YlKS8KTJDxRaUl4h8o3JeGbjplyz' +
-  'JRjprz4YUl4h8onVFoSniThiUpLwjcl4R0qP+mYKcdMOWbKi/8YlZaE' +
-  'pvJfdsyUY6YcM+XF/zmVloSfpNKS8G9yzJRjphwz5cUPU/knqbQkNJV' +
-  '/ksqf5Jgpx0w5ZsqLL0vCb0pCU2lJaCotCU2lJaGpfCIJf7Jjphwz5Z' +
-  'gpLz6k8l+ShHeo/D85ZsoxU46Z8uJDSWgq35SEpvIkCU2lJeEdKu9Iw' +
-  'hOVJ0l4h8o3HTPlmCnHTIl/4QNJeIdKS0JTeZKEJyotCZ9QaUloKu9I' +
-  'wjepfNMxU46ZcsyUF1+m8iQJ70hCU3mShCcqT5LQkvAkCU9UPqHSktC' +
-  'S0FQ+ccyUY6YcM+XFh1S+KQlNpSWhqTSVloRPqLQkNJWWhJ+k0pLwTc' +
-  'dMOWbKMVNefCgJn1B5koSm0pLwiSR8IglNpSXhicqf5Jgpx0w5ZsqLL' +
-  '1N5koSWhKbSVFoSmsqTJDxR+UkqLQmfSEJT+aZjphwz5ZgpL35YEprK' +
-  'O5LQVFoSmkpTaUl4ovIOlZaEdyThicqTJDSVTxwz5Zgpx0x58Q9LwhO' +
-  'VloR3JOGJyk9SaUl4otKS0FRaEr7pmCnHTDlmSvwLH0hCU3mShKbSkv' +
-  'BEpSWhqTxJQlP5RBJ+k8pPOmbKMVOOmfLiQyrvUHmi8pNUWhLeofJE5' +
-  'R1J+JMcM+WYKcdMefGhJPwmlabSktBUWhKayk9KQlN5ovKOJDSVTxwz' +
-  '5Zgpx0x58WUq35SEd6g8UWlJeKLyCZVvSsJPOmbKMVOOmfLihyXhHSr' +
-  'flISm8kSlJeEdSfhEEp6otCR80zFTjplyzJQX/zJJaCpPVH6TypMkNJ' +
-  'WWhJ90zJRjphwz5cW/XBLeodJUvikJTaWptCQ0lZaEbzpmyjFTjpny4' +
-  'oep/MlUniShqbQkPFFpKi0JTaWp/KZjphwz5ZgpL74sCb8pCU3lE0n4' +
-  'piQ8UfmTHDPlmCnHTIl/YeZvx0w5ZsoxU46ZcsyUY6YcM+WYKcdMOWb' +
-  'KMVOOmXLMlGOmHDPlf6gcfBrGIkCUAAAAAElFTkSuQmCC'
+const qrURL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIQAAACECAYAAABRRIOn' +
+  'AAAAAklEQVR4AewaftIAAAOOSURBVO3BQY5biRUEwcwH3v/KZS0GcK0+QJCtkeyKML8w849jphwz5Z' +
+  'gpx0w5ZsoxU46ZcsyUY6YcM+WYKcdMOWbKMVOOmfLiQyq/UxKaSktCU3mShKbyiSQ0ld8pCZ84Zsox' +
+  'U46Z8uLLkvBNKk+S8I4kNJUnSfimJHyTyjcdM+WYKcdMefHDVN6RhN8pCU9UniThEyrvSMJPOmbKMV' +
+  'OOmfLiL6fyJAnvUGlJeKLSkvA3O2bKMVOOmfLif0wSmsqTJLQkNJX/J8dMOWbKMVNe/LAk/E4qLQlN' +
+  '5R1JeKLyiST8SY6ZcsyUY6a8+DKVf1MSmkpLQlNpSWgqLQmfUPmTHTPlmCnHTDG/8BdTaUl4otKSMP' +
+  '91zJRjphwzxfzCB1RaEt6h0pLQVD6RhN9J5ZuS8ESlJeETx0w5ZsoxU158KAlN5R1JaCotCe9QeaLS' +
+  'ktBUvikJT1TeodKS8E3HTDlmyjFTXnxI5RMqLQlN5UkSPqHyJAlNpSXhHSrvUGlJ+EnHTDlmyjFTXn' +
+  'xZEppKS8ITlZaEptJUWhKaSktCU2lJaCotCe9QeZKEJypNpSXhm46ZcsyUY6a8+JepPFF5koSm8kSl' +
+  'JeFJEppKS0JTeZKEJyrvUGlJ+MQxU46ZcswU8wsfUGlJ+IRKS0JTeUcSnqi0JDSVTyThiUpLwhOVlo' +
+  'RvOmbKMVOOmWJ+4QepPElCU3mShKbyJAlN5ZuS8A6VTyThJx0z5Zgpx0x58YdLwk9KwhOVloR3qLwj' +
+  'CU2lqbQkfNMxU46ZcswU8wsfUPmmJDSVJ0loKi0JT1RaEprKnyQJP+mYKcdMOWaK+YW/mEpLQlNpSX' +
+  'iHSktCU2lJeIdKS8K/6Zgpx0w5ZsqLD6n8Tkl4RxLeofJNKi0Jf7Jjphwz5ZgpL74sCd+k8iQJTeVJ' +
+  'EppKS8I3JeEdKu9IwjcdM+WYKcdMefHDVN6RhHeoPElCU/lJKj8pCT/pmCnHTDlmyou/XBKaSlNpSW' +
+  'gqTaUloak8SUJTeZKEJypN5UkSPnHMlGOmHDPlxf+YJDSVpvIkCU2lJaGpNJUnSXii0pLQVFoSvumY' +
+  'KcdMOWbKix+WhH9TEr5JpSXhiUpTeYfK73TMlGOmHDPlxZep/E4qT5LQVJ4koSWhqbwjCZ9QeaLSkv' +
+  'CJY6YcM+WYKeYXZv5xzJRjphwz5Zgpx0w5ZsoxU46ZcsyUY6YcM+WYKcdMOWbKMVP+A5JsiSX0M+5q' +
+  'AAAAAElFTkSuQmCC'
 
 describe('qr.service', () => {
   describe('getDataURL', () => {
     it('returns a data url when config url parameter is defined', async () => {
-      const dataURL = await qrService.getDataURL('http//:google.com')
+      const dataURL = await qrService.getDataURL('https://www.google.co.uk')
       expect(dataURL).toBe(qrURL)
     })
     it('returns null when url parameter is not defined', async () => {
