14cd577b258a9a44e1c5b31e521c532e34c3fcb3
George Chatzigiannis <geohatzigiannis@gmail.com>
2017-11-07 22:06:15 +0000

    bug/16347 Display DoB for duplicate names in generated pins page (#267)
    * Relocate display DoB to pupil service and utilize it in active pupil pins screen
    
    * jsdoc
    
    * typo
    
    * updated failing test
    
    * If pupil has same first name and surname, then date of birth is also displayed. In addition if pupil has same first name, surname name and same date of birth, then middle name is also displayed
    
    * naming convention change and jsdoc clarification

--- admin/services/pupil.service.js
+++ admin/services/pupil.service.js
@@ -87,6 +87,28 @@ const pupilService = {
       }
     }
     return true
+  },
+  /**
+   * Adds show Date of Birth flag for pupils that have been alphabetically sorted by last name and have equal full names
+   * @param {Array} pupils
+   * @returns {Array}
+   */
+  addIdentificationFlags: (pupils) => {
+    pupils.forEach((p, i) => {
+      const currentPupil = pupils[ i ]
+      const nextPupil = pupils[ i + 1 ]
+      if (nextPupil === undefined) return
+      if (currentPupil.foreName === nextPupil.foreName && currentPupil.lastName === nextPupil.lastName &&
+        currentPupil.dob === nextPupil.dob) {
+        currentPupil.showMiddleNames = true
+        nextPupil.showMiddleNames = true
+      }
+      if (currentPupil.foreName === nextPupil.foreName && currentPupil.lastName === nextPupil.lastName) {
+        currentPupil.showDoB = true
+        nextPupil.showDoB = true
+      }
+    })
+    return pupils
   }
 }
 
