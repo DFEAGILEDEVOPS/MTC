c6043c0f31bf7e8ea3aa79cc4b75d3df1df0129a
Paula Molinero <pris54@gmail.com>
2017-11-08 11:36:52 +0000

    Feature/15093 check window validation (#250)
    * Check window validation.

--- admin/lib/errors/check-window.js
+++ admin/lib/errors/check-window.js
@@ -1,41 +1,50 @@
 'use strict'
 
 module.exports = {
-  checkWindowName: 'Error in Name of check window',
+  checkWindowName: '"Name of check window" is required',
+  checkWindowNameLength: '"Name of check window" can\'t be less than 2 characters',
 
-  adminStartDayWrongDay: 'Wrong admin start day',
-  adminStartDayInvalidChars: 'Invalid characters in admin start day',
-  adminStartDayRequired: 'Admin start day is required',
+  adminStartDayWrongDay: 'Please check "Day"',
+  adminStartDayInvalidChars: 'Entry must be numerical',
+  adminStartDayRequired: '"Administration start day" is required',
 
-  adminStartMonthWrongDay: 'Wrong admin start month',
-  adminStartMonthInvalidChars: 'Invalid characters in admin start month',
-  adminStartMonthRequired: 'Admin start month is required',
+  adminStartMonthWrongDay: 'Please check "Month"',
+  adminStartMonthInvalidChars: 'Entry must be numerical',
+  adminStartMonthRequired: '"Administration start month" is required',
 
-  adminStartYearWrongDay: 'Wrong admin start year',
-  adminStartYearInvalidChars: 'Invalid characters in admin start year',
-  adminStartYearRequired: 'Admin start year is required',
+  adminStartYearWrongDay: 'Please check "Year"',
+  adminStartYearInvalidChars: 'Entry must be numerical',
+  adminStartYearRequired: '"Administration start year" is required',
 
-  checkStartDayWrongDay: 'Wrong check start day',
-  checkStartDayInvalidChars: 'Invalid characters in check start day',
-  checkStartDayRequired: 'Check start day is required',
+  checkStartDayWrongDay: 'Please check "Day"',
+  checkStartDayInvalidChars: 'Entry must be numericaly',
+  checkStartDayRequired: '"Check start day" is required',
 
-  checkStartMonthWrongDay: 'Wrong check start month',
-  checkStartMonthInvalidChars: 'Invalid characters in check start month',
-  checkStartMonthRequired: 'Check start month is required',
+  checkStartMonthWrongDay: 'Please check "Month"',
+  checkStartMonthInvalidChars: 'Entry must be numerical',
+  checkStartMonthRequired: '"Check start month" is required',
 
-  checkStartYearWrongDay: 'Wrong check start year',
-  checkStartYearInvalidChars: 'Invalid characters in check start year',
-  checkStartYearRequired: 'Check start year is required',
+  checkStartYearWrongDay: 'Please check "Year"',
+  checkStartYearInvalidChars: 'Entry must be numerical',
+  checkStartYearRequired: '"Check start year" is required',
 
-  checkEndDayWrongDay: 'Wrong check end day',
-  checkEndDayInvalidChars: 'Invalid characters in check end day',
-  checkEndDayRequired: 'Check end day is required',
+  checkEndDayWrongDay: 'Please check "Day"',
+  checkEndDayInvalidChars: 'Entry must be numerical',
+  checkEndDayRequired: '"Check end day" is required',
 
-  checkEndMonthWrongDay: 'Wrong check end month',
-  checkEndMonthInvalidChars: 'Invalid characters in check end month',
-  checkEndMonthRequired: 'Check end month is required',
+  checkEndMonthWrongDay: 'Please check "Month"',
+  checkEndMonthInvalidChars: 'Entry must be numerical',
+  checkEndMonthRequired: '"Check end month" is required',
 
-  checkEndYearWrongDay: 'Wrong check end year',
-  checkEndYearInvalidChars: 'Invalid characters in check end year',
-  checkEndYearRequired: 'Check end year is required'
+  checkEndYearWrongDay: 'Please check "Year"',
+  checkEndYearInvalidChars: 'Entry must be numerical',
+  checkEndYearRequired: '"Check end year" is required',
+
+  adminDateInThePast: 'Start date must be in the future',
+  checkDateBeforeAdminDate: '"Check start date" must occur after the "Administration start date"',
+  checkStartDateAfterEndDate: '"Check start date" must occur before the "Check end date"',
+  checkStartDateInThePast: '"Check start date" must be in the future',
+  checkEndDateInThePast: '"Check end date" must be in the future',
+  checkEndDateBeforeStartDate: '"Check end date" must occur after "Check start date"',
+  enterValidYear: 'Please check "Year"'
 }
