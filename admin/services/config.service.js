35ea5ce4b8f99cce49d907570a544edeaea91845
Jon Shipley <jon-shipley@users.noreply.github.com>
2017-11-10 12:49:37 +0000

    Pass speech synth config to SPA (#280)
    * Update configService to use new settingDataService
    
    * Remove useless await calls
    
    * Merge pupil check config and pass to the SPA
    
    * Update design doc
    
    * Bugfix: correct logic when saving Pupils to allow speech synthesis flag to be passed correctly
    
    DB migration to flip the speech synthesis flag to on for a demo user
    
    * Update documnentation
    
    * Lint fixes
    
    * adding tests to check speech synthesis is added to local storage
    
    * removing debugger

--- admin/services/config.service.js
+++ admin/services/config.service.js
@@ -1,26 +1,36 @@
 'use strict'
-const Setting = require('../models/setting')
+
+const R = require('ramda')
+
+const settingDataService = require('./data-access/setting.data.service')
 const {QUESTION_TIME_LIMIT, TIME_BETWEEN_QUESTIONS} = require('../config')
 
 /** @namespace */
 
 const configService = {
 
-  getConfig: async () => {
+  /**
+   * Fetch the config for a particular pupil for a test
+   * @param {Object} pupil - plain pupil object
+   * @return {Promise.<{questionTime: *, loadingTime: *, speechSynthesis: boolean}>}
+   */
+  getConfig: async (pupil) => {
     let questionTime = QUESTION_TIME_LIMIT
     let loadingTime = TIME_BETWEEN_QUESTIONS
 
-    const timeSettings = await Setting.findOne().exec()
+    const timeSettings = await settingDataService.findOne({})
 
     if (timeSettings) {
       loadingTime = timeSettings.loadingTimeLimit
       questionTime = timeSettings.questionTimeLimit
     }
 
-    return {
+    const config = {
       questionTime,
       loadingTime
     }
+
+    return R.merge(config, pupil.checkOptions)
   }
 }
 
--- admin/spec/service/config.service.spec.js
+++ admin/spec/service/config.service.spec.js
@@ -1,10 +1,9 @@
 'use strict'
 /* global describe it expect beforeEach afterEach */
-const proxyquire = require('proxyquire')
+const proxyquire = require('proxyquire').noCallThru()
 const sinon = require('sinon')
-require('sinon-mongoose')
 
-const Settings = require('../../models/setting')
+const settingsDataService = require('../../services/data-access/setting.data.service')
 
 let sandbox
 let configService
@@ -12,27 +11,35 @@ const configMock = {
   QUESTION_TIME_LIMIT: 500,
   TIME_BETWEEN_QUESTIONS: 200
 }
+const pupilMock = require('../mocks/pupil')
 
 describe('config service', () => {
   describe('database values exist', () => {
     beforeEach(() => {
       sandbox = sinon.sandbox.create()
-      sandbox.mock(Settings)
+      sandbox.mock(settingsDataService)
         .expects('findOne')
-        .chain('exec')
         .resolves({
           loadingTimeLimit: 20,
           questionTimeLimit: 50
         })
-      configService = proxyquire('../../services/config.service', {'../models/setting': Settings})
+      configService = proxyquire('../../services/config.service', {
+        './data-access/setting.data.service': settingsDataService
+      })
     })
 
     afterEach(() => { sandbox.restore() })
 
     it('returns timings from the database', async (done) => {
-      const config = await configService.getConfig()
+      const config = await configService.getConfig(pupilMock)
       expect(config.loadingTime).toBe(20)
       expect(config.questionTime).toBe(50)
+      done()
+    })
+
+    it('adds the pupil checkOptions into the returned config', async (done) => {
+      const config = await configService.getConfig(pupilMock)
+      expect(config.speechSynthesis).toBeDefined()
       done()
     })
   })
@@ -40,17 +47,19 @@ describe('config service', () => {
   describe('database values do not exist', () => {
     beforeEach(() => {
       sandbox = sinon.sandbox.create()
-      sandbox.mock(Settings)
+      sandbox.mock(settingsDataService)
         .expects('findOne')
-        .chain('exec')
         .resolves(null)
-      configService = proxyquire('../../services/config.service', {'../models/setting': Settings, '../config': configMock})
+      configService = proxyquire('../../services/config.service', {
+        './data-access/setting.data.service': settingsDataService,
+        '../config': configMock
+      })
     })
 
     afterEach(() => { sandbox.restore() })
 
     it('returns timings from the config file', async (done) => {
-      const config = await configService.getConfig()
+      const config = await configService.getConfig(pupilMock)
       expect(config.loadingTime).toBe(200)
       expect(config.questionTime).toBe(500)
       done()
