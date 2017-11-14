e113ba33b84e36f75a2233f409e6188f081e23ad
Guy Harwood <guy@milkchip.com>
2017-11-14 11:18:10 +0000

    fix param issues

--- create-collections.sh
+++ create-collections.sh
@@ -21,5 +21,16 @@ declare -a collections=("adminlogonevents" "adminsessions" "attendancecodes" "ch
 for i in "${collections[@]}"
 do
    echo "creating collection $i with throughput of $highThroughput RUs"
-   az cosmosdb collection create --collection-name $collectionName --name $name --db-name $databaseName --resource-group $resourceGroupName --throughput $highThrougput
+   createCollection $i
 done
+
+# You can access them using echo "${arr[0]}", "${arr[1]}" also
+
+function createCollection() {
+  az cosmosdb collection create \
+	--collection-name $1 \
+	--name $cosmosInstanceName \
+	--db-name $databaseName \
+	--resource-group $resourceGroupName \
+	--throughput $highThrougput
+}
