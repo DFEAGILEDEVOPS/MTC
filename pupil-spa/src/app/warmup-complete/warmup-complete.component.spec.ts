a462396007ceea16f69e407ab6a51e8ef2aefe22
George Chatzigiannis <george.chatzigiannis@smartfocus.com>
2017-11-20 14:23:39 +0000

    updated failing tests

--- pupil-spa/src/app/warmup-complete/warmup-complete.component.spec.ts
+++ pupil-spa/src/app/warmup-complete/warmup-complete.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { WarmupCompleteComponent } from './warmup-complete.component';
 import { AuditService } from '../services/audit/audit.service';
 import { AuditServiceMock } from '../services/audit/audit.service.mock';
+import { CheckStartedAPICallSucceed, CheckStartedAPICallFailed } from '../services/audit/auditEntry';
 import { SubmissionServiceMock } from '../services/submission/submission.service.mock';
 import { SubmissionService } from '../services/submission/submission.service';
 import { WarmupCompleteRendered, AuditEntry } from '../services/audit/auditEntry';
@@ -12,6 +13,8 @@ describe('WarmupCompleteComponent', () => {
   let component: WarmupCompleteComponent;
   let fixture: ComponentFixture<WarmupCompleteComponent>;
   let submissionService;
+  let auditEntryInserted: AuditEntry;
+  let auditService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -27,34 +30,59 @@ describe('WarmupCompleteComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(WarmupCompleteComponent);
     component = fixture.componentInstance;
+    auditService = fixture.debugElement.injector.get(AuditService);
+    spyOn(auditService, 'addEntry').and.callFake((entry) => {
+      auditEntryInserted = entry;
+    });
     fixture.detectChanges();
-    submissionService = fixture.debugElement.injector.get(SubmissionService);
-    spyOn(submissionService, 'submitCheckStartData').and.returnValue(Promise.resolve());
   });
-
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
 
-  it('emits onClick()', async (() => {
-    component.clickEvent.subscribe( g => {
-      expect(g).toBe(null);
+  describe('onClick()', () => {
+    describe('when submission service call succeeds', () => {
+      beforeEach(() => {
+        submissionService = fixture.debugElement.injector.get(SubmissionService);
+        spyOn(submissionService, 'submitCheckStartData')
+          .and.returnValue(Promise.resolve());
+      });
+      it('successfully calls submission service and audit service', async (() => {
+        component.clickEvent.subscribe(g => {
+          expect(g).toBe(null);
+        });
+        component.onClick();
+        fixture.whenStable().then(() => {
+          fixture.detectChanges();
+          expect(auditService.addEntry).toHaveBeenCalledTimes(2);
+          expect(auditService.addEntry).toHaveBeenCalledWith(new CheckStartedAPICallSucceed());
+        });
+        expect(submissionService.submitCheckStartData).toHaveBeenCalledTimes(1);
+      }));
     });
-    component.onClick();
-    expect(submissionService.submitCheckStartData).toHaveBeenCalledTimes(1);
-  }));
+    describe('when submission service call fails', () => {
+      beforeEach(() => {
+        submissionService = fixture.debugElement.injector.get(SubmissionService);
+        spyOn(submissionService, 'submitCheckStartData')
+          .and.returnValue(Promise.reject(new Error('Error')));
+      });
+      it('throws the error and logs in audit service the failure', async(() => {
+        component.clickEvent.subscribe(g => {
+          expect(g).toBe(null);
+        });
+        component.onClick();
+        fixture.whenStable().catch(() => {
+          fixture.detectChanges();
+          expect(auditService.addEntry).toHaveBeenCalledTimes(2);
+          expect(auditService.addEntry).toHaveBeenCalledWith(new CheckStartedAPICallFailed());
+        });
+        expect(submissionService.submitCheckStartData).toHaveBeenCalledTimes(1);
+      }));
+    });
+  });
 
   describe('audit entry', () => {
-    let auditEntryInserted: AuditEntry;
-    let auditService;
-    beforeEach(() => {
-      auditService = fixture.debugElement.injector.get(AuditService);
-      spyOn(auditService, 'addEntry').and.callFake((entry) => {
-        auditEntryInserted = entry;
-      });
-    });
     it('is added on WarmupComplete rendered', () => {
-      component.ngAfterViewInit();
       expect(auditService.addEntry).toHaveBeenCalledTimes(1);
       expect(auditEntryInserted instanceof WarmupCompleteRendered).toBeTruthy();
     });
