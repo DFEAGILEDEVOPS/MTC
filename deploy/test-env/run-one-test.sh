feb94086a469cf84f487ad3d507dee66c8b2e574
Guy Harwood <guy@milkchip.com>
2017-11-22 18:01:40 +0000

    output ruby

--- deploy/test-env/run-one-test.sh
+++ deploy/test-env/run-one-test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 cd ./../../admin/test
+which ruby
+ruby -v
 gem install bundler
 bundle install
 rake features OPTS='BASE_URL=$(TEST_SITE_URL) MONGO_CONNECTION_STRING=$(TEST_DATABASE_CONNECTION) features/sign_in.feature'
