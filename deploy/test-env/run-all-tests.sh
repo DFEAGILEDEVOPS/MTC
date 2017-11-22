f2b45749ab5c8d22f8816383f39084c540c45469
Guy Harwood <guy@milkchip.com>
2017-11-22 17:54:54 +0000

    scripts for running automated tests

---
+++ deploy/test-env/run-all-tests.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd ./../admin/test
+gem install bundler
+bundle install
+rake features OPTS='BASE_URL=$(TEST_SITE_URL) MONGO_CONNECTION_STRING=$(TEST_DATABASE_CONNECTION)'
+CUCUMBER_EXIT_CODE=$?
+exit ${CUCUMBER_EXIT_CODE}
