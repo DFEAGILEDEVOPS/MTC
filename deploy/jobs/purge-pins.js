2962a6b864a1011b5e7dc5d38762074e7e601175
Guy Harwood <guy@milkchip.com>
2017-11-28 16:51:54 +0000

    improve output

--- deploy/jobs/purge-pins.js
+++ deploy/jobs/purge-pins.js
@@ -2,18 +2,18 @@
 
 try {
   print('erasing pupil pins...')
-  db.pupils.updateMany(
+  printjson(db.pupils.updateMany(
     { pin: {$ne: null} },
     { $set: { pin: null, pinExpiresAt: null } }
-  )
- printjson(db.runCommand('getlasterror'))
- print('erasing school pins...')
-  db.schools.updateMany(
+  ))
+
+  print('erasing school pins...')
+  printjson(db.schools.updateMany(
     { schoolPin: {$ne: null} },
     { $set: { schoolPin: null, pinExpiresAt: null } }
-  )
-  printjson(db.runCommand('getlasterror'))
-
+  ))
 } catch (e) {
+  print('ERROR...')
   print(e)
+  throw e
 }
