053f445031e183a28501f94f31c449f0952e5793
Guy Harwood <guy@milkchip.com>
2017-11-28 16:35:51 +0000

    print output

--- deploy/jobs/purge-pins.js
+++ deploy/jobs/purge-pins.js
@@ -2,13 +2,16 @@
 
 try {
   db.pupils.updateMany(
-     { pin: {$ne: null} },
-     { $set: { pin: null, pinExpiresAt: null } }
-  );
+    { pin: {$ne: null} },
+    { $set: { pin: null, pinExpiresAt: null } }
+  )
+  db.runCommand('getlasterror')
   db.schools.updateMany(
     { schoolPin: {$ne: null} },
     { $set: { schoolPin: null, pinExpiresAt: null } }
- );
+  )
+  print(db.runCommand('getlasterror'))
+
 } catch (e) {
-  print(e);
+  print(e)
 }
