82ec0f16b56f277b98f53acaf52f5b56eb0629dc
Guy Harwood <guy@milkchip.com>
2017-11-28 11:06:24 +0000

    source ruby

--- deploy/test-env/run-one-test.sh
+++ deploy/test-env/run-one-test.sh
@@ -1,11 +1,16 @@
 #!/bin/bash -x
 
+source /home/mtc-test/.rvm/scripts/rvm
+
 cd ./../../admin/test
-RUBES="$(which ruby)"
+
+RUBY_LOC="$(which ruby)"
 VER="$(ruby -v)"
-echo "ruby is ${RUBES} version ${VER}"
+echo "ruby is $RUBY_LOC version $VER"
+
 gem install bundler
 bundle install
 rake features OPTS='BASE_URL=$(TEST_SITE_URL) MONGO_CONNECTION_STRING=$(TEST_DATABASE_CONNECTION) features/sign_in.feature'
+
 CUCUMBER_EXIT_CODE=$?
 exit ${CUCUMBER_EXIT_CODE}
