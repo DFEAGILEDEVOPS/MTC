71e9d15233c27f13057e0f52c5079f743592ea88
Jon Shipley <js@ovix.co.uk>
2017-12-01 13:27:41 +0000

    Add jsdoc

--- admin/services/data-access/setting-log.data.service.js
+++ admin/services/data-access/setting-log.data.service.js
@@ -3,6 +3,11 @@
 const SettingLog = require('../../models/setting-log')
 const settingLogDataService = {}
 
+/**
+ * Create a new settingLog record in the DB
+ * @param data
+ * @return {Promise.<*>}
+ */
 settingLogDataService.create = async function (data) {
   const sl = new SettingLog(data)
   await sl.save()
