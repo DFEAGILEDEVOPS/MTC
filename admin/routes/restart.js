4024cb422ffae0317823d29f5561b9d512f8c0a2
George Chatzigiannis <george.chatzigiannis@smartfocus.com>
2017-12-01 09:58:02 +0000

    Refactor route to remove destructure assignment

--- admin/routes/restart.js
+++ admin/routes/restart.js
@@ -2,15 +2,10 @@ const config = require('../config')
 const express = require('express')
 const isAuthenticated = require('../authentication/middleware')
 const router = express.Router()
+const restartController = require('../controllers/restart')
 
-const {
-  getRestartOverview,
-  getSelectRestartList,
-  postSubmitRestartList
-} = require('../controllers/restart')
-
-router.get('/overview', isAuthenticated(config.ROLE_TEACHER), (req, res) => getRestartOverview(req, res))
-router.get('/select-restart-list', isAuthenticated(config.ROLE_TEACHER), (req, res, next) => getSelectRestartList(req, res, next))
-router.post('/submit-restart-list', isAuthenticated(config.ROLE_TEACHER), (req, res, next) => postSubmitRestartList(req, res, next))
+router.get('/overview', isAuthenticated(config.ROLE_TEACHER), (req, res) => restartController.getRestartOverview(req, res))
+router.get('/select-restart-list', isAuthenticated(config.ROLE_TEACHER), (req, res, next) => restartController.getSelectRestartList(req, res, next))
+router.post('/submit-restart-list', isAuthenticated(config.ROLE_TEACHER), (req, res, next) => restartController.postSubmitRestartList(req, res, next))
 
 module.exports = router
