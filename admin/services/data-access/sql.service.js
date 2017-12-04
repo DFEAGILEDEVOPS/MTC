284a2dbd33e26c8577373f00e4639729d1eec5bf
Guy Harwood <guy@milkchip.com>
2017-12-04 08:01:17 +0000

    add modify method and examples

--- admin/services/data-access/sql.service.js
+++ admin/services/data-access/sql.service.js
@@ -5,16 +5,15 @@ const connectionService = require('./sql.connection.service')
 
 const sqlService = {}
 
-sqlService.query = async (sql, params) => {
-  const con = await connectionService.getConnection()
-  console.log('got connection')
-  let results = []
-
-  return new Promise((resolve, reject) => {
+sqlService.query = (sql, params) => {
+  return new Promise(async (resolve, reject) => {
+    const con = await connectionService.getConnection()
+    let results = []
+    // http://tediousjs.github.io/tedious/api-request.html
     var request = new Request(sql, function (err, rowCount) {
       if (err) reject(err)
-      console.log('rows returned:', rowCount)
-      console.log('releasing connection')
+      // TODO request also emits a 'done' event for each SQL statement.
+      // which is a more deterministic completion?
       con.release()
       resolve(results)
     })
@@ -30,6 +29,27 @@ sqlService.query = async (sql, params) => {
     request.on('row', function (cols) {
       results.push(cols)
     })
+    con.execSql(request)
+  })
+}
+
+sqlService.modify = async (sql, params) => {
+  return new Promise(async (resolve, reject) => {
+    const con = await connectionService.getConnection()
+
+    var request = new Request(sql, function (err, rowCount) {
+      if (err) reject(err)
+      con.release()
+      resolve(rowCount)
+    })
+
+    if (params) {
+      for (let index = 0; index < params.length; index++) {
+        const param = params[index]
+        // TODO add support for other options
+        request.addParameter(param.name, param.type, param.value)
+      }
+    }
     con.execSql(request)
   })
 }
--- admin/services/data-access/sql.usage.example.js
+++ admin/services/data-access/sql.usage.example.js
@@ -1,9 +1,26 @@
+'use strict'
+
+const TYPES = require('tedious').TYPES
 const sqlConnectionService = require('./services/data-access/sql.connection.service')
 const sqlService = require('./services/data-access/sql.service')
 
 sqlConnectionService.init()
 
-module.exports = () => {
+const examples = {}
+
+examples.getData = () => {
   sqlService.query('SELECT * FROM settings')
-  .then(result => console.log('query settings table result:', result))
+    .then(result => console.log('query settings table result:', result))
 }
+
+examples.modifyData = () => {
+  const id = { name: 'id', type: TYPES.Int, value: 2 }
+  const updatedAt = { name: 'updatedAt', type: TYPES.DateTime2, value: TYPES.Null }
+  const loadingTimeLimit = { name: 'loadingTimeLimit', type: TYPES.TinyInt, value: 1 }
+  const questionTimeLimit = { name: 'questionTimeLimit', type: TYPES.TinyInt, value: 2 }
+
+  sqlService.modify('INSERT settings (id, updatedAt, loadingTimeLimit, questionTimeLimit) VALUES (@id, @updatedAt, @loadingTimeLimit, @questionTimeLimit)',
+    id, updatedAt, loadingTimeLimit, questionTimeLimit)
+}
+
+module.exports = examples
