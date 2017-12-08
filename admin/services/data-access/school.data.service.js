73fb2e3943adc54af749b2f628efa10a1af28ac0
Jon Shipley <jon-shipley@users.noreply.github.com>
2017-12-08 11:57:22 +0000

    Feature/17983 data layer refactor school controller (#339)
    * Remove Pupil from school controller
    Add a temporary method to the pupil data service: `update2` which may replace `update`
    
    * Remove mongoose dependencies from School controller
    
    * Change the call signature for pupilDataService.update
    
    * Add TODO comments

--- admin/services/data-access/school.data.service.js
+++ admin/services/data-access/school.data.service.js
@@ -17,13 +17,16 @@ schoolDataService.find = async function (criteria) {
   return School.find(criteria).lean().exec()
 }
 
+/**
+ * Update a single document
+ * Example: `const res = await schoolDataService.update({_id: 9991999}, { $set: {name: 'Some Primary School'}})`
+ * Example: `const res = await schoolDataService.update({_id: 9991999}, { _id: 9991999, name: 'Some Primary School', anotherProp: someVal, ...})`
+ * @param {object} id  - the _id to match in the db
+ * @param {object} doc - the update to run
+ * @return {Promise<void>} - E.g. `{ n: 1, nModified: 1, ok: 1 }`
+ */
 schoolDataService.update = async function (id, doc) {
-  return new Promise((resolve, reject) => {
-    School.updateOne({ _id: id }, doc, (error) => {
-      if (error) { return reject(error) }
-      resolve(null)
-    })
-  })
+  return School.updateOne({ _id: id }, doc).exec()
 }
 
 module.exports = schoolDataService
