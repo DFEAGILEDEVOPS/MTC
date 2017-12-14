4f837207a22627e3dc1702ef4633ff2e71570a63
Jon Shipley <js@ovix.co.uk>
2017-12-14 08:54:01 +0000

    Use env var when creating app user

--- admin/data/sql/migrations/003.do.create-admin-app-user.js
+++ admin/data/sql/migrations/003.do.create-admin-app-user.js
@@ -1,8 +1,8 @@
 'use strict'
 
-const createAzureUser = `CREATE USER mtcAdminUser WITH PASSWORD ='${process.env.SQL_APP_USER_PASSWORD}', DEFAULT_SCHEMA=[mtc_admin];`
+const createAzureUser = `CREATE USER ${process.env.SQL_APP_USER} WITH PASSWORD ='${process.env.SQL_APP_USER_PASSWORD}', DEFAULT_SCHEMA=[mtc_admin];`
 
-const createLocalSqlUser = `CREATE LOGIN mtcAdminUser WITH PASSWORD = '${process.env.SQL_APP_USER_PASSWORD}'; USE ${process.env.SQL_DATABASE}; CREATE USER mtcAdminUser FOR LOGIN mtcAdminUser WITH DEFAULT_SCHEMA = [mtc_admin];`
+const createLocalSqlUser = `CREATE LOGIN ${process.env.SQL_APP_USER} WITH PASSWORD = '${process.env.SQL_APP_USER_PASSWORD}'; USE ${process.env.SQL_DATABASE}; CREATE USER ${process.env.SQL_APP_USER} FOR LOGIN ${process.env.SQL_APP_USER} WITH DEFAULT_SCHEMA = [mtc_admin];`
 
 // TODO test on sql azure
 module.exports.generateSql = function () {
