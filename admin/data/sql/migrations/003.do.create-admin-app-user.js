83d9b0e34d5c15767b622ff10c8e55841d38eb68
Jon Shipley <js@ovix.co.uk>
2017-12-14 08:59:20 +0000

    Remove hard-coding of application username

--- admin/data/sql/migrations/003.do.create-admin-app-user.js
+++ admin/data/sql/migrations/003.do.create-admin-app-user.js
@@ -2,9 +2,9 @@
 
 const config = require('../../../config')
 
-const createAzureUser = `CREATE USER mtcAdminUser WITH PASSWORD ='${config.Sql.Application.Username}', DEFAULT_SCHEMA=[mtc_admin];`
+const createAzureUser = `CREATE USER ${config.Sql.Application.Username} WITH PASSWORD ='${config.Sql.Application.Password}', DEFAULT_SCHEMA=[mtc_admin];`
 
-const createLocalSqlUser = `CREATE LOGIN mtcAdminUser WITH PASSWORD = '${config.Sql.Application.Password}'; USE ${config.Sql.Database}; CREATE USER mtcAdminUser FOR LOGIN mtcAdminUser WITH DEFAULT_SCHEMA = [mtc_admin];`
+const createLocalSqlUser = `CREATE LOGIN ${config.Sql.Application.Username} WITH PASSWORD = '${config.Sql.Application.Password}'; USE ${config.Sql.Database}; CREATE USER ${config.Sql.Application.Username} FOR LOGIN ${config.Sql.Application.Username} WITH DEFAULT_SCHEMA = [mtc_admin];`
 
 // TODO test on sql azure
 module.exports.generateSql = function () {
