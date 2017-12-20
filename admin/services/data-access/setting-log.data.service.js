51eebc883b6389d9f844b9b7fae890835d13fc62
Guy Harwood <guy@milkchip.com>
2017-12-20 18:55:35 +0000

    create sql method

--- admin/services/data-access/setting-log.data.service.js
+++ admin/services/data-access/setting-log.data.service.js
@@ -1,7 +1,18 @@
 'use strict'
 
 const SettingLog = require('../../models/setting-log')
+const sqlService = require('../data-access/sql.service')
+
 const settingLogDataService = {}
+
+/**
+ * Create a new settingLog record in the DB
+ * @param data
+ * @return {Promise.<*>}
+ */
+settingLogDataService.sqlCreate = async function (data) {
+  // TODO
+}
 
 /**
  * Create a new settingLog record in the DB
