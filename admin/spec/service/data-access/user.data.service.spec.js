5b771f398450db18c2311064ad66166eb41aa80a
Jon Shipley <jon-shipley@users.noreply.github.com>
2017-12-20 17:46:27 +0000

    Feature/sql admin auth extra (#360)
    * Remove on-load behaviour to fetch data-type cache, instead it fetches it itself on the first call
    
    * Add tests for new data services

---
+++ admin/spec/service/data-access/user.data.service.spec.js
@@ -0,0 +1,28 @@
+'use strict'
+/* global describe, beforeEach, it, expect, spyOn */
+
+const proxyquire = require('proxyquire').noCallThru()
+const sqlService = require('../../../services/data-access/sql.service')
+
+describe('user.data.service', () => {
+  let service
+
+  describe('#sqlFindOneByIdentifier', () => {
+    beforeEach(() => {
+      spyOn(sqlService, 'query').and.returnValue(Promise.resolve( [ {userMock: 'true'} ]))
+      service = proxyquire('../../../services/data-access/user.data.service', {
+        './sql.service': sqlService
+      })
+      return service
+    })
+
+    it('makes the expected calls', async (done) => {
+      const identifier = 'teacher42'
+      const res = await service.sqlFindOneByIdentifier(identifier)
+      expect(sqlService.query).toHaveBeenCalled()
+      // as this is a find one - we expect an object back, not an array of objects
+      expect(typeof res).toBe('object')
+      done()
+    })
+  })
+})
