3e380ba9500ff45fcbac4a49978073fe34edb03f
Guy Harwood <guy@milkchip.com>
2017-12-21 13:30:59 +0000

    make user.school_id nullable

---
+++ admin/data/sql/migrations/016.undo.user-school-nullable.sql
@@ -0,0 +1,1 @@
+ALTER TABLE mtc.mtc_admin.[user] ALTER COLUMN school_id INT NOT NULL
---
+++ admin/data/sql/migrations/017.do.user-school-trigger.sql
@@ -0,0 +1,22 @@
+/* CREATE TRIGGER user_school_id_required_for_teachers  
+ON [mtcAdmin].[user]  
+FOR INSERT, UPDATE AS  
+IF @@ROWCOUNT = 1  
+BEGIN  
+   UPDATE Purchasing.PurchaseOrderHeader  
+   SET SubTotal = SubTotal + LineTotal  
+   FROM inserted  
+   WHERE PurchaseOrderHeader.PurchaseOrderID = inserted.PurchaseOrderID  
+
+END  
+ELSE  
+BEGIN  
+      UPDATE Purchasing.PurchaseOrderHeader  
+   SET SubTotal = SubTotal +   
+      (SELECT SUM(LineTotal)  
+      FROM inserted  
+      WHERE PurchaseOrderHeader.PurchaseOrderID  
+       = inserted.PurchaseOrderID)  
+   WHERE PurchaseOrderHeader.PurchaseOrderID IN  
+      (SELECT PurchaseOrderID FROM inserted)  
+END;  */ 
