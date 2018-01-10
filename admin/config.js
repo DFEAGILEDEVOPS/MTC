9ab0a89d3fe30f76aa418d9d81fe3afca84ad3e3
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-01-10 18:38:16 +0000

    env variable rename

--- admin/config.js
+++ admin/config.js
@@ -19,7 +19,7 @@ module.exports = {
   TIME_BETWEEN_QUESTIONS: 2,
   LINES_PER_CHECK_FORM: 25,
   Data: {
-    allowedPasswordWords: process.env.THREE_LETTER_WORDS
+    allowedPasswordWords: process.env.ALLOWED_WORDS
   },
   Sql: {
     Enabled: process.env.SQL_ENABLED || false,
