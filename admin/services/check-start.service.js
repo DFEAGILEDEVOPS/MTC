177fa070f2e2ae1c1c4dac8090967872f1d4f200
Guy Harwood <guy@milkchip.com>
2018-01-12 15:52:08 +0000

    fix stubbed code now checkForm and window are refactored

--- admin/services/check-start.service.js
+++ admin/services/check-start.service.js
@@ -4,6 +4,7 @@ const uuidv4 = require('uuid/v4')
 const moment = require('moment')
 const checkFormService = require('../services/check-form.service')
 const checkDataService = require('../services/data-access/check.data.service')
+const checkWindowDataService = require('../services/data-access/check-window.data.service')
 
 const checkStartService = {
   /**
@@ -14,15 +15,16 @@ const checkStartService = {
   startCheck: async function (pupilId) {
     // Allocate a checkForm to a pupil, or will throw an error
     const checkForm = await checkFormService.allocateCheckForm()
+    const checkWindow = await checkWindowDataService.fetchCurrentCheckWindow()
 
     // Generate a new CheckCode for this unique check
     const checkCode = uuidv4()
     // TODO: the hard coded values below are in place until check form and window move to SQL
     const checkData = {
-      pupil_id: 1,
+      pupil_id: pupilId,
       checkCode,
-      checkWindow_id: 1,
-      checkForm_id: 1,
+      checkWindow_id: checkWindow.id,
+      checkForm_id: checkForm.id,
       pupilLoginDate: moment.utc(),
       startedAt: null
     }
