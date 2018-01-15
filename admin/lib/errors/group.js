88799db746874186a93db81ffdfb214ced91b972
Paula <hello@pris54.co.uk>
2018-01-15 15:25:40 +0000

    Bug fix: Complete text for error message.

--- admin/lib/errors/group.js
+++ admin/lib/errors/group.js
@@ -4,6 +4,6 @@ module.exports.group = {
   nameIsRequired: 'Group name is required',
   nameInvalidCharacters: 'Enter a group name without special characters',
   nameIsTooLong: 'Enter a group name in no more than 35 characters',
-  nameAlreadyExists: ' already exists',
+  nameAlreadyExists: ' already exists. Enter a different name.',
   missingPupils: 'Assign pupils to this group'
 }
