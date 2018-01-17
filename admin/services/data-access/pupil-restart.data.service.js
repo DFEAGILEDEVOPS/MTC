0fb4338fbdabaa3f47255de488f48250419a0d54
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-01-17 16:31:37 +0000

    missing await keyword

--- admin/services/data-access/pupil-restart.data.service.js
+++ admin/services/data-access/pupil-restart.data.service.js
@@ -88,7 +88,7 @@ pupilRestartDataService.sqlGetNumberOfRestartsByPupil = async function (pupilId)
       type: TYPES.Int
     }
   ]
-  const result = sqlService.query(sql, params)
+  const result = await sqlService.query(sql, params)
   return R.head(result)
 }
 
