66a5f0e1b1615436140102a6f17be7dc453f8273
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-01-17 12:55:42 +0000

    requested changes in migrations

--- admin/data/sql/migrations/027.do.create-pupil-restart-table.sql
+++ admin/data/sql/migrations/027.do.create-pupil-restart-table.sql
@@ -20,3 +20,15 @@ CREATE TABLE [mtc_admin].[pupilRestart] (
       ALLOW_PAGE_LOCKS = ON
     )
 )
+
+ALTER TABLE [mtc_admin].[pupilRestart] WITH CHECK ADD CONSTRAINT [FK_pupilRestart_pupilRestartCode_id] FOREIGN KEY([pupilRestartCode_id])
+REFERENCES [mtc_admin].[pupilRestartCode] ([id])
+
+ALTER TABLE [mtc_admin].[pupilRestart] WITH CHECK ADD CONSTRAINT [FK_pupilRestart_pupil_id] FOREIGN KEY([pupil_id])
+REFERENCES [mtc_admin].[pupil] ([id])
+
+ALTER TABLE [mtc_admin].[pupilRestart] WITH CHECK ADD CONSTRAINT [FK_pupilRestart_recordedByUser_id] FOREIGN KEY([recordedByUser_id])
+REFERENCES [mtc_admin].[user] ([id])
+
+ALTER TABLE [mtc_admin].[pupilRestart] WITH CHECK ADD CONSTRAINT [FK_pupilRestart_deletedByUser_id] FOREIGN KEY([deletedByUser_id])
+REFERENCES [mtc_admin].[user] ([id])
--- admin/data/sql/migrations/028.undo.add-pupil-restart-constraints.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE [mtc_admin].pupilRestart DROP CONSTRAINT FK_pupilRestart_pupilRestartCode_id
-ALTER TABLE [mtc_admin].pupilRestart DROP CONSTRAINT FK_pupilRestart_pupil_id
