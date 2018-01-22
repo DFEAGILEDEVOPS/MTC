2a29cebe0121419f7f0b825d29d8ff3d431394a8
Jon Shipley <js@ovix.co.uk>
2018-01-22 17:20:34 +0000

    Deleted a test that only tested that mocking worked

--- admin/spec/service/pin.service.spec.js
+++ admin/spec/service/pin.service.spec.js
@@ -30,19 +30,7 @@ describe('pin.service', () => {
       pupil2._id = '595cd5416e5ca13e48ed2520'
       pupil2.pinExpiresAt = moment().startOf('day').add(16, 'hours')
     })
-    describe('if pins are valid', () => {
-      beforeEach(() => {
-        sandbox.useFakeTimers(moment().startOf('day').subtract(1, 'years').valueOf())
-        sandbox.mock(pupilDataService).expects('getSortedPupils').resolves([ pupil1, pupil2 ])
-        proxyquire('../../services/pupil.service', {
-          '../../services/data-access/pupil.data.service': pupilDataService
-        })
-      })
-      it('it should return a list of active pupils', async () => {
-        const pupils = await pinService.getPupilsWithActivePins(schoolMock._id)
-        expect(pupils.length).toBe(2)
-      })
-    })
+    
     describe('if pins are invalid', () => {
       beforeEach(() => {
         sandbox.useFakeTimers(moment().startOf('day').add(100, 'years').valueOf())
