7009ead8513539cfd310aa4abdefbac2a8a54ec5
Paula Molinero <pris54@gmail.com>
2018-01-24 12:09:29 +0000

    Feature/18016 update reasons (#417)
    * Update reasons not to take the check.

---
+++ admin/data/sql/migrations/050.undo.update-reasons.sql
@@ -0,0 +1,2 @@
+UPDATE [mtc_admin].[attendanceCode] SET reason = 'Just arrived' where reason = 'Just arrived with EAL';
+UPDATE [mtc_admin].[attendanceCode] SET reason = 'Working below the overall standard of the check' where reason = 'Working below expectation';
