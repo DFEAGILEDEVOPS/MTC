84e247a32ff63c4b1ddb134811992c16817c8f7b
Amit Sinha <amit.kumar.sinha@businessthreezero.com>
2018-01-25 10:58:15 +0000

    removed commented line

--- pupil-spa/test/features/support/sql_db_helper.rb
+++ pupil-spa/test/features/support/sql_db_helper.rb
@@ -25,11 +25,6 @@ class SqlDbHelper
   end
 
   def self.find_next_pupil
-    # collection=CLIENT[:pupils].find({})
-    # result = []
-    # collection.each { |pupil| result << pupil }
-    # result.select{|pupil| pupil['pin'] == nil}.first
-
     sql = "SELECT * FROM [mtc_admin].[pupil] WHERE pin is Null"
     result = SQL_CLIENT.execute(sql)
     pupil_details_res = result.first
@@ -71,13 +66,6 @@ class SqlDbHelper
     result.do
   end
 
-  # def self.get_completed_checks
-  #   collection=CLIENT[:completedchecks].find({})
-  #   result = []
-  #   collection.each { |check| result << check }
-  #   result
-  # end
-
   def self.get_pupil_check_metadata(check_code)
     sql = "SELECT * FROM [mtc_admin].[check] WHERE checkCode = '#{check_code}'"
     result = SQL_CLIENT.execute(sql)
@@ -88,11 +76,6 @@ class SqlDbHelper
   end
 
   def self.get_check_window(check_window_id)
-    # collection=CLIENT[:checkwindows].find({'_id': BSON::ObjectId(check_window_id)})
-    # result = []
-    # collection.find.each { |window| result << window }
-    # result.first
-
     sql = "SELECT * FROM [mtc_admin].[checkWindow] WHERE id = '#{check_window_id}'"
     result = SQL_CLIENT.execute(sql)
     chk_window_res = result.first
@@ -102,11 +85,6 @@ class SqlDbHelper
   end
 
   def self.get_check_window_via_name(name)
-    # collection=CLIENT[:checkwindows].find({'name': name})
-    # result = []
-    # collection.find.each { |window| result << window }
-    # result.first
-
     sql = "SELECT * FROM [mtc_admin].[checkWindow] WHERE name = '#{name}'"
     result = SQL_CLIENT.execute(sql)
     chk_window_res = result.first
@@ -125,9 +103,6 @@ class SqlDbHelper
   end
 
   def self.number_of_checks
-    # collection = CLIENT[:checks]
-    # collection.find.count
-
     sql = "SELECT * FROM [mtc_admin].[check]"
     result = SQL_CLIENT.execute(sql)
     chk_window_count = result.do
@@ -151,11 +126,6 @@ class SqlDbHelper
   end
 
   def self.get_feedback(check_id)
-    # result = []
-    # collection=CLIENT[:pupilfeedbacks].find({'sessionId': session_id})
-    # collection.each {|a| result << a}
-    # result.first
-
     sql = "SELECT * FROM [mtc_admin].[pupilFeedback] WHERE check_id = '#{check_id}'"
     result = SQL_CLIENT.execute(sql)
     chk_form_res = result.first
