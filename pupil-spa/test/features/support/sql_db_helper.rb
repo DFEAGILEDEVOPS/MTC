1f7ab11b7c62076d1bde922c79a1236c6c15da7f
sinhaamit1980 <sinhaamit1980@yahoo.com>
2018-01-25 11:02:20 +0000

    Feature/automation check data service (#429)
    * updated Restart and Pupil SPA Test

--- pupil-spa/test/features/support/sql_db_helper.rb
+++ pupil-spa/test/features/support/sql_db_helper.rb
@@ -1,15 +1,31 @@
 class SqlDbHelper
 
-  def self.pupil_details(upn)
-    sql = "SELECT * FROM [mtc_admin].[pupil] WHERE upn='#{upn}'"
+  def self.find_school(school_id)
+    sql = "SELECT * FROM [mtc_admin].[school] WHERE id='#{school_id}'"
+    result = SQL_CLIENT.execute(sql)
+    school_res = result.first
+    result.cancel
+    school_res
+  end
+
+  def self.get_list_of_schools
+    sql = "SELECT * FROM [mtc_admin].[school]"
+    result = SQL_CLIENT.execute(sql)
+    @array_of_schools = result.each{|row| row.map}
+    result.cancel
+    @array_of_schools
+  end
+
+  def self.find_pupil_via_pin(pin)
+    sql = "SELECT * FROM [mtc_admin].[pupil] WHERE pin='#{pin}'"
     result = SQL_CLIENT.execute(sql)
     pupil_details_res = result.first
     result.cancel
     pupil_details_res
   end
 
-  def self.pupil_details_using_names(firstname, lastname)
-    sql = "SELECT * FROM [mtc_admin].[pupil] WHERE foreName='#{firstname}' AND lastName='#{lastname}'"
+  def self.find_next_pupil
+    sql = "SELECT * FROM [mtc_admin].[pupil] WHERE pin is Null"
     result = SQL_CLIENT.execute(sql)
     pupil_details_res = result.first
     result.cancel
@@ -24,53 +40,25 @@ class SqlDbHelper
     pupil_details_res
   end
 
-  def self.find_teacher(name)
-    @array_of_users = []
-    sql = "SELECT * FROM [mtc_admin].[user] WHERE identifier='#{name}'"
-    result = SQL_CLIENT.execute(sql)
-    @array_of_users = result.each{|row| row.map}
-  end
 
-  def self.find_school(school_id)
-    sql = "SELECT * FROM [mtc_admin].[school] WHERE id='#{school_id}'"
+  def self.expire_pin(forename,lastname,school_id,flag=true)
+    sql = "UPDATE [mtc_admin].[pupil] set pinExpiresAt=null WHERE foreName='#{forename}' AND lastName='#{lastname}' AND school_id='#{school_id}'"
     result = SQL_CLIENT.execute(sql)
-    school_res = result.first
-    result.cancel
-    school_res
-  end
-
-  def self.find_school_by_dfeNumber(school_dfeNumber)
-    sql = "SELECT * FROM [mtc_admin].[school] WHERE dfeNumber='#{school_dfeNumber}'"
-    result = SQL_CLIENT.execute(sql)
-    school_res = result.first
-    result.cancel
-    school_res
-  end
-
-  def self.list_of_pupils_from_school(school_id)
-    @array_of_pupils = []
-    sql = "SELECT * FROM [mtc_admin].[pupil] WHERE school_id='#{school_id}'"
-    result = SQL_CLIENT.execute(sql)
-    @array_of_pupils = result.each{|row| row.map}
+    result.do
   end
 
   def self.reset_pin(forename,lastname,school_id,flag=nil)
-    sql = "UPDATE [mtc_admin].[pupil] set pin=null WHERE foreName='#{forename}' AND lastName='#{lastname}' AND school_id='#{school_id}'"
+    sql = "UPDATE [mtc_admin].[pupil] set pin=#{flag} WHERE foreName='#{forename}' AND lastName='#{lastname}' AND school_id='#{school_id}'"
     result = SQL_CLIENT.execute(sql)
     result.do
   end
 
-  def self.set_pupil_pin(forename,lastname,school_id,newPin)
-    sql = "UPDATE [mtc_admin].[pupil] set pin='#{newPin}' WHERE foreName='#{forename}' AND lastName='#{lastname}' AND school_id='#{school_id}'"
+  def self.set_pupil_pin_expiry(forename,lastname,school_id,new_time)
+    sql = "UPDATE [mtc_admin].[pupil] set pinExpiresAt='#{new_time}' WHERE foreName='#{forename}' AND lastName='#{lastname}' AND school_id='#{school_id}'"
     result = SQL_CLIENT.execute(sql)
     result.do
   end
 
-  def self.set_pupil_pin_expiry(forename,lastname,school_id,newTime)
-    sql = "UPDATE [mtc_admin].[pupil] set pinExpiresAt='#{newTime}' WHERE foreName='#{forename}' AND lastName='#{lastname}' AND school_id='#{school_id}'"
-    result = SQL_CLIENT.execute(sql)
-    result.do
-  end
 
   def self.set_school_pin_expiry(estab_code,newTime)
     sql = "UPDATE [mtc_admin].[school] set pinExpiresAt='#{newTime}' WHERE estabCode='#{estab_code}'"
@@ -78,49 +66,71 @@ class SqlDbHelper
     result.do
   end
 
-  def self.get_settings
-    sql = "SELECT * FROM [mtc_admin].[settings]"
-    result = SQL_CLIENT.execute(sql)
-    settings_res = result.first
-    result.cancel
-    settings_res
-  end
-
-  def self.latest_setting_log
-    sql = "SELECT * FROM [mtc_admin].[settingsLog] ORDER BY createdAt DESC"
-    result = SQL_CLIENT.execute(sql)
-    settingsLog_res = result.first
-    result.cancel
-    settingsLog_res
-  end
-
-  def self.check_window_details(check_name)
-    sql = "SELECT * FROM [mtc_admin].[checkWindow] WHERE name = '#{check_name}'"
+  def self.get_pupil_check_metadata(check_code)
+    sql = "SELECT * FROM [mtc_admin].[check] WHERE checkCode = '#{check_code}'"
     result = SQL_CLIENT.execute(sql)
     chk_window_res = result.first
     result.cancel
     chk_window_res
+
   end
+
+  def self.get_check_window(check_window_id)
+    sql = "SELECT * FROM [mtc_admin].[checkWindow] WHERE id = '#{check_window_id}'"
+    result = SQL_CLIENT.execute(sql)
+    chk_window_res = result.first
+    result.cancel
+    chk_window_res
+
+  end
+
+  def self.get_check_window_via_name(name)
+    sql = "SELECT * FROM [mtc_admin].[checkWindow] WHERE name = '#{name}'"
+    result = SQL_CLIENT.execute(sql)
+    chk_window_res = result.first
+    result.cancel
+    chk_window_res
+
+  end
+
+  def self.get_form(form_id)
+    sql = "SELECT * FROM [mtc_admin].[checkForm] WHERE id = '#{form_id}'"
+    result = SQL_CLIENT.execute(sql)
+    chk_form_res = result.first
+    result.cancel
+    chk_form_res
+
+  end
+
+  def self.number_of_checks
+    sql = "SELECT * FROM [mtc_admin].[check]"
+    result = SQL_CLIENT.execute(sql)
+    chk_window_count = result.do
+    chk_window_count
+  end
+
 
   def self.check_windows
     check_window_result = []
     sql = "SELECT * FROM [mtc_admin].[checkWindow]"
     result = SQL_CLIENT.execute(sql)
     check_window_result = result.each{|row| row.map}
+    result.cancel
+    check_window_result
   end
 
-  def self.check_form_details(check_form_name)
-    sql = "SELECT * FROM [mtc_admin].[checkForm] WHERE name = '#{check_form_name}'"
+  def self.update_check_window(id, column_name, new_date)
+    sql = "UPDATE [mtc_admin].[checkWindow] set #{column_name}='#{new_date}' WHERE id='#{id}'"
+    result = SQL_CLIENT.execute(sql)
+    result.do
+  end
+
+  def self.get_feedback(check_id)
+    sql = "SELECT * FROM [mtc_admin].[pupilFeedback] WHERE check_id = '#{check_id}'"
     result = SQL_CLIENT.execute(sql)
     chk_form_res = result.first
     result.cancel
     chk_form_res
-  end
-
-  def self.create_check(updatedime, createdTime, pupil_id, pupilLoginDate, checkStartedTime)
-    sql = "INSERT INTO [mtc_admin].[check] (updatedAt, createdAt, pupilId, checkCode, checkWindowId, checkFormId, pupilLoginDate, checkStartedAt) VALUES ('#{updatedime}', '#{createdTime}', '#{pupil_id}', '40e5356c-#{rand(1000)}-#{rand(1000)}-a46e-b100d346a9e6', '#{check_window_id}', '100', '#{pupilLoginDate}', '#{checkStartedTime}' )"
-    result = SQL_CLIENT.execute(sql)
-    result.insert
   end
 
 end
