4687d039044aae35dfb09edf2c788743d51709af
Guy Harwood <guy@milkchip.com>
2018-01-25 11:19:58 +0000

    remove db name from scripts

--- admin/data/sql/migrations/040.undo.group-school-id.sql
+++ admin/data/sql/migrations/040.undo.group-school-id.sql
@@ -1,2 +1,2 @@
-ALTER TABLE mtc.mtc_admin.[group] DROP CONSTRAINT group_school_id_fk
-ALTER TABLE mtc.mtc_admin.[group] DROP COLUMN school_id
+ALTER TABLE mtc_admin.[group] DROP CONSTRAINT group_school_id_fk
+ALTER TABLE mtc_admin.[group] DROP COLUMN school_id
