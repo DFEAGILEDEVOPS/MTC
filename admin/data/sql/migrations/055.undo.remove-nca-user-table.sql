744a96c13bdf8791447cf5f913054908c6876795
Guy Harwood <guy@milkchip.com>
2018-01-30 12:16:32 +0000

    remove db ref from migrations

--- admin/data/sql/migrations/055.undo.remove-nca-user-table.sql
+++ admin/data/sql/migrations/055.undo.remove-nca-user-table.sql
@@ -1,3 +1,3 @@
-ALTER TABLE mtc.mtc_admin.adminLogonEvent ADD ncaUserName NVARCHAR(MAX) NULL
-ALTER TABLE mtc.mtc_admin.adminLogonEvent ADD ncaEmailAddress NVARCHAR(MAX) NULL
-ALTER TABLE mtc.mtc_admin.adminLogonEvent ALTER COLUMN user_id INT NULL
+ALTER TABLE mtc_admin.adminLogonEvent ADD ncaUserName NVARCHAR(MAX) NULL
+ALTER TABLE mtc_admin.adminLogonEvent ADD ncaEmailAddress NVARCHAR(MAX) NULL
+ALTER TABLE mtc_admin.adminLogonEvent ALTER COLUMN user_id INT NULL
