87592ee0a1b8b628d996f3b0cc086216ef1aab5b
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-01-31 16:05:25 +0000

    Perform check data submission on submission pending component

--- pupil-spa/src/app/submission-pending/submission-pending.component.spec.ts
+++ pupil-spa/src/app/submission-pending/submission-pending.component.spec.ts
@@ -1,25 +1,68 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { SubmissionPendingComponent } from './submission-pending.component';
+import { SubmissionService } from '../services/submission/submission.service';
+import { SubmissionServiceMock } from '../services/submission/submission.service.mock';
 
 describe('SubmissionPendingComponent', () => {
-  let component: SubmissionPendingComponent;
   let fixture: ComponentFixture<SubmissionPendingComponent>;
-
+  let submissionService;
+  let component;
+  let router: Router;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SubmissionPendingComponent ]
+      declarations: [ SubmissionPendingComponent ],
+      imports: [    RouterTestingModule.withRoutes([])],
+      providers: [
+        { provide: SubmissionService, useClass: SubmissionServiceMock }
+      ]
     })
     .compileComponents();
+    router = TestBed.get(Router);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SubmissionPendingComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+  describe('ngOnInit()', () => {
+    it('calls loadComponent method when data submission is successful', async () => {
+      submissionService = fixture.debugElement.injector.get(SubmissionService);
+      spyOn(submissionService, 'submitData').and.returnValue(Promise.resolve());
+      spyOn(component, 'loadComponent').and.returnValue(Promise.resolve());
+      spyOn(component, 'sleep').and.returnValue(Promise.resolve());
+      await component.ngOnInit();
+      expect(submissionService.submitData).toHaveBeenCalled();
+      expect(component.loadComponent).toHaveBeenCalledWith(true);
+      expect(component.loadComponent).toHaveBeenCalledTimes(1);
+    });
+    it('calls loadComponent method when data submission throws an error', async () => {
+      submissionService = fixture.debugElement.injector.get(SubmissionService);
+      spyOn(submissionService, 'submitData').and.returnValue(Promise.reject(new Error()));
+      spyOn(component, 'loadComponent').and.returnValue(Promise.resolve());
+      spyOn(component, 'sleep').and.returnValue(Promise.resolve());
+      await component.ngOnInit();
+      expect(submissionService.submitData).toHaveBeenCalled();
+      expect(component.loadComponent).toHaveBeenCalledWith(false);
+      expect(component.loadComponent).toHaveBeenCalledTimes(1);
+    });
+  });
+  describe('loadComponent()', () => {
+    it('calls router navigate with check complete url path',  async () => {
+      spyOn(router, 'navigate');
+      await component.loadComponent(true);
+      expect(router.navigate).toHaveBeenCalledWith(['/check-complete']);
+    });
+    it('calls router navigate with submission failed url path',  async () => {
+      spyOn(router, 'navigate');
+      await component.loadComponent(false);
+      expect(router.navigate).toHaveBeenCalledWith(['/submission-failed']);
+    });
   });
 });
--- pupil-spa/src/app/submission-pending/submission-pending.component.ts
+++ pupil-spa/src/app/submission-pending/submission-pending.component.ts
@@ -1,4 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { SubmissionService } from '../services/submission/submission.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-submission-pending',
@@ -10,10 +13,37 @@ export class SubmissionPendingComponent implements OnInit {
   @Output()
   clickEvent: EventEmitter<any> = new EventEmitter();
 
-  constructor() {
-
+  constructor(private submissionService: SubmissionService,
+              private router: Router) {
   }
 
-  ngOnInit() {
+  async ngOnInit() {
+    const start = Date.now();
+    await this.submissionService.submitData()
+      .then(async () => {
+        // Display pending screen for the minimum configurable time
+        const end = Date.now();
+        const duration = end - start;
+        const minDisplay = environment.submissionPendingViewMinDisplay;
+        if (duration < minDisplay) {
+          const displayTime = minDisplay - duration;
+          await this.sleep(displayTime);
+        }
+        await this.loadComponent(true);
+        return;
+      })
+      .catch(async () => {
+        await this.loadComponent(false);
+        return;
+      });
+  }
+
+  async loadComponent(success) {
+    const path = success ? 'check-complete' : 'submission-failed';
+    return this.router.navigate([`/${path}`]);
+  }
+
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 }
