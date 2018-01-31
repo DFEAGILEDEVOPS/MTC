23626f02c56ae80182ff639286e5582cdb0b462f
Guy Harwood <guy@milkchip.com>
2018-01-31 09:43:21 +0000

    auto inc primary key

--- admin/data/sql/migrations/057.do.nca-tools-session-table.sql
+++ admin/data/sql/migrations/057.do.nca-tools-session-table.sql
@@ -1,11 +1,10 @@
 CREATE TABLE mtc_admin.ncaToolsSession
 (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY NOT NULL IDENTITY,
     sessionToken NVARCHAR(1024) NOT NULL,
     userName NVARCHAR(MAX) NOT NULL,
     userType NVARCHAR(50),
     emailAddress NVARCHAR(MAX),
     dfeSchoolNumber INT
 )
-CREATE UNIQUE INDEX ncaToolsSession_id_uindex ON mtc.mtc_admin.ncaToolsSession (id)
-CREATE UNIQUE INDEX ncaToolsSession_sessionToken_uindex ON mtc.mtc_admin.ncaToolsSession (sessionToken)
+CREATE UNIQUE INDEX ncaToolsSession_sessionToken_uindex ON mtc_admin.ncaToolsSession (sessionToken)
