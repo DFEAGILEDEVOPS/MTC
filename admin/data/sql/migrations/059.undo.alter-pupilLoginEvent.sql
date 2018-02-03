5002165e412e1c1ed3bc10116a85bf4532f3844e
Guy Harwood <guy@milkchip.com>
2018-02-03 14:49:23 +0000

    fix migration

--- admin/data/sql/migrations/059.undo.alter-pupilLoginEvent.sql
+++ admin/data/sql/migrations/059.undo.alter-pupilLoginEvent.sql
@@ -1,5 +1,5 @@
 -- We need to make the pupil and school pins long enough to
 -- capture incorrect pin entries (for analysis) but no so long as to allow
 -- a DoS attack to overwhelm the server.
-ALTER TABLE mtc.mtc_admin.pupilLogonEvent ALTER COLUMN pupilPin CHAR(5) NOT NULL;
-ALTER TABLE mtc.mtc_admin.pupilLogonEvent ALTER COLUMN schoolPin CHAR(8) NOT NULL;
+ALTER TABLE mtc_admin.pupilLogonEvent ALTER COLUMN pupilPin CHAR(5) NOT NULL;
+ALTER TABLE mtc_admin.pupilLogonEvent ALTER COLUMN schoolPin CHAR(8) NOT NULL;
