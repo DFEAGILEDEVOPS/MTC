69923d29418c8e27bd762e792a1cc81e1afaaa5e
Guy Harwood <guy@milkchip.com>
2018-02-05 09:20:23 +0000

    fix db prefix

--- admin/data/sql/migrations/059.do.alter-pupilLoginEvent.sql
+++ admin/data/sql/migrations/059.do.alter-pupilLoginEvent.sql
@@ -1,5 +1,5 @@
 -- We need to make the pupil and school pins long enough to
 -- capture incorrect pin entries (for analysis) but no so long as to allow
 -- a DoS attack to overwhelm the server.
-ALTER TABLE mtc.mtc_admin.pupilLogonEvent ALTER COLUMN pupilPin NVARCHAR(50) NOT NULL;
-ALTER TABLE mtc.mtc_admin.pupilLogonEvent ALTER COLUMN schoolPin NVARCHAR(50) NOT NULL;
+ALTER TABLE mtc_admin.pupilLogonEvent ALTER COLUMN pupilPin NVARCHAR(50) NOT NULL;
+ALTER TABLE mtc_admin.pupilLogonEvent ALTER COLUMN schoolPin NVARCHAR(50) NOT NULL;
