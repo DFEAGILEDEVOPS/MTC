e72fefe45b17c92187f3124623e6f12538f35d47
Guy Harwood <guy@milkchip.com>
2018-02-07 09:34:44 +0000

    fix to map email address instead of username

--- admin/authentication/nca-tools-authentication-strategy.js
+++ admin/authentication/nca-tools-authentication-strategy.js
@@ -41,13 +41,13 @@ module.exports = async function (req, done) {
       if (userData.School) {
         userData.School = parseInt(userData.School, 10)
       }
-      await ncaToolsUserService.recordLogonAttempt({
+      /* await ncaToolsUserService.recordLogonAttempt({
         sessionToken: userData.SessionToken,
         userName: userData.UserName,
         userType: userData.UserType,
         emailAddress: userData.EmailAddress,
         dfeSchoolNumber: userData.School
-      })
+      }) */
     } catch (error) {
       throw new Error('Failed to save NCA Tools Session Data - possible replay attack: ' + error.message)
     }
