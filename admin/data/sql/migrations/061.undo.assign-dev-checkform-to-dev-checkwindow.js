f47980dd99cb583f61f215f8fa1f462e5793c793
Jon Shipley <js@ovix.co.uk>
2018-02-07 15:15:04 +0000

    A more cautious approach to the SQL undo

--- admin/data/sql/migrations/061.undo.assign-dev-checkform-to-dev-checkwindow.js
+++ admin/data/sql/migrations/061.undo.assign-dev-checkform-to-dev-checkwindow.js
@@ -2,6 +2,11 @@ const config = require('../../../config')
 
 module.exports.generateSql = () => {
   if (process.env.NODE_ENV !== 'production') {
-    return `DELETE FROM [mtc_admin].[checkFormWindow];`
+    return `
+    DELETE 
+    FROM [mtc_admin].[checkFormWindow] 
+    WHERE
+     checkForm_id IN (SELECT id FROM [mtc_admin].[checkForm] WHERE name = 'MTC0100')
+    AND  checkWindow_id IN (SELECT id FROM [mtc_admin].[checkWindow] WHERE name = 'Development Phase')`
   }
 }
