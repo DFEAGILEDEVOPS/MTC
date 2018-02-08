9a41277d952f369c998795dd808bd23186bdeff9
Guy Harwood <guy@milkchip.com>
2018-02-08 13:13:50 +0000

    add debug output for isAuthenticated middleware

--- admin/authentication/middleware.js
+++ admin/authentication/middleware.js
@@ -1,7 +1,14 @@
+'use strict'
+
+const winston = require('winston')
+
 function isAuthenticated (role) {
+  winston.debug('entering /authentication/middleware.js:isAuthenticated with role:', role)
   return function (req, res, next) {
     if (req.isAuthenticated()) {
+      winston.debug('user authenticated:', req.user)
       const userRole = ((req.user).role || {})
+      winston.debug('role:', userRole)
       if (role === undefined || (role !== undefined && userRole && role === userRole)) {
         return next()
       } else {
