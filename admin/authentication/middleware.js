59c284a7b1021e5cf4cc7932a0766221c4f282d0
Guy Harwood <guy@milkchip.com>
2018-02-09 10:13:37 +0000

    update authentication middleware with guard

--- admin/authentication/middleware.js
+++ admin/authentication/middleware.js
@@ -5,9 +5,11 @@ const winston = require('winston')
 function isAuthenticated (role) {
   return function (req, res, next) {
     if (req.isAuthenticated()) {
-      winston.debug('req.user', req.user)
-      const userRole = ((req.user).role || {})
-      winston.debug('userRole:', userRole)
+      let userRole
+      if (req.user) {
+        userRole = ((req.user).role || {})
+      }
+      winston.debug(`checking authorisation on ${req.url} for role:${role} against userRole:${userRole}`)
       if (role === undefined || (role !== undefined && userRole && role === userRole)) {
         return next()
       } else {
