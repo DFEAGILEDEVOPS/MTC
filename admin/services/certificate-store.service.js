b23ea7aedbb881205e09c7b023c6d66bd499236f
Jon Shipley <js@ovix.co.uk>
2018-02-13 14:07:16 +0000

    Remove console.log entries

--- admin/services/certificate-store.service.js
+++ admin/services/certificate-store.service.js
@@ -7,19 +7,14 @@ let callCount = 0
 const service = {
   getNcaPublicKey: async () => {
     // cache after first call
-    console.log('call index:', callCount++)
     if (ncaPublicKey) {
-      console.log('value cached, returning')
       return ncaPublicKey
     }
-    console.log('value not cached')
     if (config.Certificates.Azure.BlobContainer) {
-      console.log('retrieving from azure...')
       const file = await azureBlobDataService.getBlobText(config.Certificates.Azure.BlobContainer, config.Certificates.Azure.NcaToolsPublicKeyName)
       ncaPublicKey = file
       return ncaPublicKey
     }
-    console.log('returning local env var')
     return config.Certificates.Local.NcaToolsPublicKey
   },
   getMtcPrivateKey: async () => {
