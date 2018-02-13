8bf666da8f23449ff513251c93a2d2f591e95b9d
Paula <hello@pris54.co.uk>
2018-02-13 14:00:34 +0000

    Roles bug fix.

--- admin/routes/restart.js
+++ admin/routes/restart.js
@@ -1,12 +1,12 @@
-const config = require('../config')
+const rolesConfig = require('../roles-config')
 const express = require('express')
 const isAuthenticated = require('../authentication/middleware')
 const router = express.Router()
 const restartController = require('../controllers/restart')
 
-router.get('/overview', isAuthenticated(config.ROLE_TEACHER), (req, res, next) => restartController.getRestartOverview(req, res, next))
-router.get('/select-restart-list', isAuthenticated(config.ROLE_TEACHER), (req, res, next) => restartController.getSelectRestartList(req, res, next))
-router.post('/submit-restart-list', isAuthenticated(config.ROLE_TEACHER), (req, res, next) => restartController.postSubmitRestartList(req, res, next))
-router.post('/delete', isAuthenticated(config.ROLE_TEACHER), (req, res, next) => restartController.postDeleteRestart(req, res, next))
+router.get('/overview', isAuthenticated(rolesConfig.ROLE_TEACHER), (req, res, next) => restartController.getRestartOverview(req, res, next))
+router.get('/select-restart-list', isAuthenticated(rolesConfig.ROLE_TEACHER), (req, res, next) => restartController.getSelectRestartList(req, res, next))
+router.post('/submit-restart-list', isAuthenticated(rolesConfig.ROLE_TEACHER), (req, res, next) => restartController.postSubmitRestartList(req, res, next))
+router.post('/delete', isAuthenticated(rolesConfig.ROLE_TEACHER), (req, res, next) => restartController.postDeleteRestart(req, res, next))
 
 module.exports = router
