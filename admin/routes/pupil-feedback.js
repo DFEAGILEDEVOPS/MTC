3e45b2aa6788b3051c59ac632dfb55081e846355
Paula <hello@pris54.co.uk>
2018-02-15 17:55:02 +0000

    Code improvements.

--- admin/routes/pupil-feedback.js
+++ admin/routes/pupil-feedback.js
@@ -2,17 +2,16 @@ const express = require('express')
 const router = express.Router()
 const moment = require('moment')
 const { setPupilFeedback } = require('../controllers/pupil-feedback')
-const winstonLogger = require('./../winstonLogger')
+const winston = require('winstonLogger')
 
 router.route('/').all((req, res) => {
-  const apiLogger = winstonLogger.apiLogger()
-  apiLogger.info(`MTC-API: ${req.url} (${moment()}) reached`)
+  winston.info(`MTC-API: ${req.url} (${moment()}) reached`)
   if (req.method !== 'POST') {
-    apiLogger.info(`MTC-API:${req.url}: req.method is not POST - res.sendStatus(405) - ${JSON.stringify(req.method)} `)
+    winston.error(`MTC-API:${req.url}: req.method is not POST - res.sendStatus(405) - ${JSON.stringify(req.method)} `)
     return res.sendStatus(405)
   }
   setPupilFeedback(req, res)
-  apiLogger.info(`MTC-API: ${req.url}: request.body: ${JSON.stringify(req.body)}`)
+  winston.info(`MTC-API: ${req.url}: request.body: ${JSON.stringify(req.body)}`)
 })
 
 module.exports = router
--- admin/routes/questions.js
+++ admin/routes/questions.js
@@ -2,17 +2,16 @@ const express = require('express')
 const router = express.Router()
 const moment = require('moment')
 const { getQuestions } = require('../controllers/questions')
-const winstonLogger = require('./../winstonLogger')
+const winston = require('winston')
 
 router.route('/').all((req, res) => {
-  const apiLogger = winstonLogger.apiLogger()
-  apiLogger.info(`MTC-API: ${req.url} (${moment()}) reached`)
+  winston.info(`MTC-API: ${req.url} (${moment()}) reached`)
   if (req.method !== 'POST') {
-    apiLogger.info(`MTC-API: ${req.url}: req.method is not POST - res.sendStatus(405) - ${JSON.stringify(req.method)}`)
+    winston.error(`MTC-API: ${req.url}: req.method is not POST - res.sendStatus(405) - ${JSON.stringify(req.method)}`)
     return res.sendStatus(405)
   }
   getQuestions(req, res)
-  apiLogger.info(`MTC-API: ${req.url}: request.body: ${JSON.stringify(req.body)}`)
+  winston.info(`MTC-API: ${req.url}: request.body: ${JSON.stringify(req.body)}`)
 })
 
 module.exports = router
