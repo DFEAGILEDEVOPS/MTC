5117c2b29cc6692a2e9c378caead048004e9c259
Paula <hello@pris54.co.uk>
2018-02-15 17:18:42 +0000

    Config fix.

--- admin/config.js
+++ admin/config.js
@@ -55,11 +55,11 @@ module.exports = {
   Logging: {
     // @TODO: Remove hardcoded key
     LogDna: {
-      key: '5a3d75a421e412afe3139e4a02a6ce19', // process.env.LOGDNA_API_KEY,
+      key: process.env.LOGDNA_API_KEY,
       hostname: `${os.hostname()}:${process.pid}`,
       ip: undefined,
       mac: undefined,
-      app: `MTC-${getEnvironment()}`,
+      app: `MTCAdmin:${getEnvironment()}`,
       env: `${getEnvironment()}`
     }
   },
