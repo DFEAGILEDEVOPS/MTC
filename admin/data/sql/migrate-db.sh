97e923c56a87d391ea5eb01fc23d8754d66e05f6
Guy Harwood <guy@milkchip.com>
2018-02-15 15:36:43 +0000

    remove duplicate file

--- admin/data/sql/migrate-db.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# $1 is the SQL_ADMIN_USER
-# $2 is the SQL_ADMIN_USER_PASSWORD
-# $3 is the SQL_APP_USER_PASSWORD
-cd $BUILD_REPOSITORY_LOCALPATH
-cd admin
-yarn install
-SQL_ADMIN_USER=$1 SQL_ADMIN_USER_PASSWORD=$2 SQL_APP_USER_PASSWORD=$3 yarn migrate-sql
