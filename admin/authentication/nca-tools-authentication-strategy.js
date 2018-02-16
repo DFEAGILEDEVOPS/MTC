d41f7f6f8d2adbf02bb2bc908df68e2998c763f5
Guy Harwood <guy@milkchip.com>
2018-02-16 17:17:13 +0000

    fix req.user.id for nca tools

--- admin/authentication/nca-tools-authentication-strategy.js
+++ admin/authentication/nca-tools-authentication-strategy.js
@@ -55,6 +55,7 @@ module.exports = async function (req, done) {
     const mtcUser = await ncaToolsUserService.mapNcaUserToMtcUser(userData)
     userData.role = mtcUser.mtcRole
     userData.schoolId = mtcUser.school_id
+    userData.id = mtcUser.id
     // auth success
     logonEvent.user_id = mtcUser.id
     logonEvent.isAuthenticated = true
