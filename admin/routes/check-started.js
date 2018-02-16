9f061713c2666685ef95aa99280e987e8c7650c1
Paula <hello@pris54.co.uk>
2018-02-16 10:30:19 +0000

    Removing moment.

--- admin/routes/check-started.js
+++ admin/routes/check-started.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const moment = require('moment')
 const { checkStarted } = require('../controllers/check-started')
 const winston = require('winston')
 
 router.route('/').all((req, res) => {
-  winston.info(`MTC-API: ${req.url} (${moment()}) reached`)
+  winston.info(`MTC-API: ${req.url} reached`)
   if (req.method !== 'POST') {
     winston.error(`MTC-API: ${req.url}: req.method is not POST - res.sendStatus(405) - ${JSON.stringify(req.method)} `)
     return res.sendStatus(405)
