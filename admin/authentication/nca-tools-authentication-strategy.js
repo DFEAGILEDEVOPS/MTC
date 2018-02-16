0a0999a096b84790d7491e53a12247d89bd5a163
Guy Harwood <guy@milkchip.com>
2018-02-16 11:54:08 +0000

    separate object from text

--- admin/authentication/nca-tools-authentication-strategy.js
+++ admin/authentication/nca-tools-authentication-strategy.js
@@ -51,7 +51,8 @@ module.exports = async function (req, done) {
     } catch (error) {
       throw new Error('Failed to save NCA Tools Session Data - possible replay attack: ' + error.message)
     }
-    winston.debug('NCA Tools Login Payload:', userData)
+    winston.debug('NCA Tools Login Payload:')
+    winston.debug(userData)
     const mtcUser = await ncaToolsUserService.mapNcaUserToMtcUser(userData)
     userData.role = mtcUser.mtcRole
     userData.schoolId = mtcUser.school_id
