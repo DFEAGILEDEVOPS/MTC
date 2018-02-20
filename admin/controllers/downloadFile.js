06fd478c919283abf41883de360875e57a2e239d
Paula <hello@pris54.co.uk>
2018-02-20 15:49:28 +0000

    Fix.

--- admin/controllers/downloadFile.js
+++ admin/controllers/downloadFile.js
@@ -1,8 +1,8 @@
 'use strict'
 
 module.exports = async (req, res) => {
-  const fileName = 'PDFs/MTC_CAG_Feb_2018_Trial.pdf'
+  const fileName = 'MTC_CAG_Feb_2018_Trial.pdf'
   res.setHeader('Content-disposition', 'filename=' + fileName)
   res.setHeader('Content-type', 'application/pdf')
-  res.download('public/' + fileName)
+  res.download('public/PDFs/' + fileName)
 }
