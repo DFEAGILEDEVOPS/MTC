341d0aedce7c7e20cbc5c4df31007b64376de11b
Paula Molinero <pris54@gmail.com>
2018-02-22 13:30:00 +0000

    Bug/19351 pupil register sorting 2 (#522)
    * Sorting by group.
    
    * Sorting by group for 'pupil register'. Expanded test coverage.
    
    * change link to pupil register
    
    * Updated default path to pupil register

--- admin/spec/service/pupil.service.spec.js
+++ admin/spec/service/pupil.service.spec.js
@@ -2,9 +2,11 @@
 
 const R = require('ramda')
 const proxyquire = require('proxyquire').noCallThru()
+const pupilService = require('../../services/pupil.service')
 const pupilDataService = require('../../services/data-access/pupil.data.service')
 const pupilMock = require('../mocks/pupil')
 const schoolMock = require('../mocks/school')
+const pupilWithGroupAndStatusMock = require('../mocks/pupils-with-group-and-status')
 const schoolDataService = require('../../services/data-access/school.data.service')
 
 /* global describe, it, expect, spyOn, fail */
@@ -108,6 +110,50 @@ describe('pupil service', () => {
       const service = setupService(pupilDataService)
       await service.getPupilsByUrlSlug(['slug'])
       expect(pupilDataService.sqlFindPupilsByUrlSlug).toHaveBeenCalledWith(['slug'])
+    })
+  })
+
+  describe('#sortByStatus', () => {
+    it('it sorts an array of pupils by status asc', async () => {
+      const mock = pupilWithGroupAndStatusMock
+      const service = setupService(pupilService)
+      const sortedPupils = await service.sortByStatus(mock, 'asc')
+      expect(sortedPupils[0].foreName).toBe('Juliana')
+      expect(sortedPupils[1].foreName).toBe('Gregory')
+      expect(sortedPupils[2].foreName).toBe('Burns')
+      expect(sortedPupils[3].foreName).toBe('Koch')
+    })
+
+    it('it sorts an array of pupils by status desc', async () => {
+      const mock = pupilWithGroupAndStatusMock
+      const service = setupService(pupilService)
+      const sortedPupils = await service.sortByStatus(mock, 'desc')
+      expect(sortedPupils[0].foreName).toBe('Ebony')
+      expect(sortedPupils[1].foreName).toBe('Nieves')
+      expect(sortedPupils[2].foreName).toBe('Juliana')
+      expect(sortedPupils[3].foreName).toBe('Gregory')
+    })
+  })
+
+  describe('#sortByGroup', () => {
+    it('it sorts an array of pupils by group asc', async () => {
+      const mock = pupilWithGroupAndStatusMock
+      const service = setupService(pupilService)
+      const sortedPupils = await service.sortByGroup(mock, 'asc')
+      expect(sortedPupils[0].foreName).toBe('Ebony')
+      expect(sortedPupils[1].foreName).toBe('Gregory')
+      expect(sortedPupils[2].foreName).toBe('Nieves')
+      expect(sortedPupils[3].foreName).toBe('Koch')
+    })
+
+    it('it sorts an array of pupils by group desc', async () => {
+      const mock = pupilWithGroupAndStatusMock
+      const service = setupService(pupilService)
+      const sortedPupils = await service.sortByGroup(mock, 'desc')
+      expect(sortedPupils[0].foreName).toBe('Nieves')
+      expect(sortedPupils[1].foreName).toBe('Koch')
+      expect(sortedPupils[2].foreName).toBe('Bessie')
+      expect(sortedPupils[3].foreName).toBe('Ebony')
     })
   })
 })
