29bb9745a8696f0ab65853f05053fb939e00221b
Jon Shipley <jon-shipley@users.noreply.github.com>
2018-02-22 14:41:45 +0000

    SPA: Capture device properties (#518)
    * Add deviceService for capturing client information that may affect the quality of the check
    
    * Capture device props from loginSuccess component - giving a snapshot at the time the test was started
    
    * Store the device properties on check submission
    
    * Whitespace changes: sort long prop list alphabetically
    
    * Tidy up the output when `hardwareConcurrency` is not available
    
    * Add reporting fields for device and browser
    
    * Lint fixes
    
    * Add utility script to inspect the json data captured from the check
    
    * Trivial update
    
    * Add morgan back in, as it's requied

--- pupil-spa/src/app/services/storage/storage.service.mock.ts
+++ pupil-spa/src/app/services/storage/storage.service.mock.ts
@@ -2,27 +2,31 @@ import { Injectable } from '@angular/core';
 import { StorageKey } from './storage.service';
 
 export class StorageServiceMock {
+  private store = {};
 
   setItem(key: StorageKey, value: Object | Array<Object>): void {
+    this.store[key] = value;
   }
 
   getItem(key: StorageKey): any {
-    return [];
+    return this.store[key];
   }
 
   removeItem(key: StorageKey): void {
     if (!key) {
       throw new Error('key is required');
     }
+    delete this.store[key];
   }
 
   clear(): void {
+    this.store = {};
   }
 
   getKeys(): string[] {
-    return [];
+    return Object.keys(this.store);
   }
   getAllItems(): any {
-    return {};
+    return this.store;
   }
 }
