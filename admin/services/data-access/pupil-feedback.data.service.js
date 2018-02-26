b667837a223ec32859b44e43008d4a1bd1411a6b
Guy Harwood <guy@milkchip.com>
2018-02-26 09:18:38 +0000

    schools, pupils

--- admin/services/data-access/pupil-feedback.data.service.js
+++ admin/services/data-access/pupil-feedback.data.service.js
@@ -1,22 +1,8 @@
 'use strict'
 
-const PupilFeedback = require('../../models/pupil-feedback')
 const sqlService = require('./sql.service')
 
 const pupilFeedbackDataService = {
-
-  /**
-   * CREATE a new pupil-feedback record
-   * @deprecated use sqlCreate
-   * @param data
-   * @return {Promise<*>}
-   */
-  create: async (data) => {
-    const pf = new PupilFeedback(data)
-    await pf.save()
-    return pf.toObject()
-  },
-
   /**
    * create new pupilFeedback record
    * @param data a JSON object containing the following: inputType:int, satisfactionRating:tinyint, comments:string, id:int
