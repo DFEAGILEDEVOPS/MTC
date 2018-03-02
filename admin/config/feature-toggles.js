0656fbb7c256e86aee85ce0d6672d6c0ccd5bd31
Jon Shipley <js@ovix.co.uk>
2018-03-02 11:04:11 +0000

    Add config file for now

---
+++ admin/config/feature-toggles.js
@@ -0,0 +1,12 @@
+'use strict'
+
+/**
+ * List of feature to toggle on or off
+ * NB features should be written as a positive
+ * @type {{groupCreate: boolean}}
+ */
+
+module.exports = {
+  groupCreate: false,
+  groupRemove: false
+}
