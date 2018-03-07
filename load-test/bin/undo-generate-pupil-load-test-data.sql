68e9fde0d37ff3cdb85b3b8f02c0b6f156ba38b7
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-03-07 15:51:22 +0000

    reference the database directly

--- load-test/bin/undo-generate-pupil-load-test-data.sql
+++ load-test/bin/undo-generate-pupil-load-test-data.sql
@@ -1,5 +1,5 @@
-DELETE FROM mtc.mtc_admin.pupilLogonEvent
-DELETE FROM mtc.mtc_admin.[check] WHERE pupil_id > 2
-DELETE FROM mtc.mtc_admin.answer
-DELETE FROM mtc.mtc_admin.pupil WHERE school_id > 6
-DELETE FROM mtc.mtc_admin.school WHERE id > 6
+DELETE FROM mtc_admin.pupilLogonEvent
+DELETE FROM mtc_admin.[check] WHERE pupil_id > 2
+DELETE FROM mtc_admin.answer
+DELETE FROM mtc_admin.pupil WHERE school_id > 6
+DELETE FROM mtc_admin.school WHERE id > 6
