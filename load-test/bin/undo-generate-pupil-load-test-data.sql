1116362a901dcc7825fc84ad13633840567bb3ec
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-03-07 15:47:41 +0000

    update for delete as per comment

--- load-test/bin/undo-generate-pupil-load-test-data.sql
+++ load-test/bin/undo-generate-pupil-load-test-data.sql
@@ -2,4 +2,4 @@ DELETE FROM mtc.mtc_admin.pupilLogonEvent
 DELETE FROM mtc.mtc_admin.[check] WHERE pupil_id > 2
 DELETE FROM mtc.mtc_admin.answer
 DELETE FROM mtc.mtc_admin.pupil WHERE school_id > 6
-DELETE FROM mtc.mtc_admin.school WHERE urn > 89005
+DELETE FROM mtc.mtc_admin.school WHERE id > 6
