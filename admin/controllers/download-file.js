beeb7535d824cda6b98e8d606be3bc2c8f2563b7
Jon Shipley <jon-shipley@users.noreply.github.com>
2018-03-08 14:17:48 +0000

    Feature/19773 change the guidance document (#560)
    * Add latest guidance pdf
    
    * Lowercase the pdfs directory
    
    * Update the Guidance links
    
    * use a direct link to the guidance pdf so it can be served from another domain
    
    * Remove unused code
    
    * Remove old PDF
    
    * Remove old PDF

--- admin/controllers/download-file.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-module.exports = async (req, res) => {
-  const fileName = 'mtc_cag_feb_2018_trial.pdf'
-  res.setHeader('Content-disposition', 'filename=' + fileName)
-  res.setHeader('Content-type', 'application/pdf')
-  res.download('public/pdfs/' + fileName)
-}
