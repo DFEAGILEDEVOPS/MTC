07984f54f1624e2e09c10cc9de5d10659cb0221a
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-03-13 15:22:07 +0000

    sql correction

--- admin/services/data-access/group.data.service.js
+++ admin/services/data-access/group.data.service.js
@@ -261,7 +261,7 @@ groupDataService.sqlFindGroupsByIds = async (schoolId, pupilIds) => {
 groupDataService.sqlFindOneGroupByPupilId = async (pupilId) => {
   if (!pupilId) return false
 
-  const sql = `SELECT * FROM mtc_admin.group g
+  const sql = `SELECT * FROM mtc_admin.[group] g
   INNER JOIN mtc_admin.pupilGroup pg ON g.id = pg.group_id
   WHERE pg.pupil_id = @pupilId AND g.isDeleted = 0`
 
