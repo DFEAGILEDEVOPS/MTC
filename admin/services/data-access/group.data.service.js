cfed4d2914941c0bf6d7e187115334a5178d2322
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-03-15 10:02:44 +0000

    Introduce loading time limit and question time limit within groups (#571)
    * Add timings to groups
    
    * rename migrations
    
    * fields type update
    
    * Fetch non-deleted group by pupil id
    
    * sql correction

--- admin/services/data-access/group.data.service.js
+++ admin/services/data-access/group.data.service.js
@@ -253,4 +253,28 @@ groupDataService.sqlFindGroupsByIds = async (schoolId, pupilIds) => {
   return sqlService.query(sql, params)
 }
 
+/**
+ * Find group by pupil id
+ * @param pupilId
+ * @return {Object}
+ */
+groupDataService.sqlFindOneGroupByPupilId = async (pupilId) => {
+  if (!pupilId) return false
+
+  const sql = `SELECT * FROM mtc_admin.[group] g
+  INNER JOIN mtc_admin.pupilGroup pg ON g.id = pg.group_id
+  WHERE pg.pupil_id = @pupilId AND g.isDeleted = 0`
+
+  const params = [
+    {
+      name: 'pupilId',
+      value: pupilId,
+      type: TYPES.Int
+    }
+  ]
+
+  const result = await sqlService.query(sql, params)
+  return R.head(result)
+}
+
 module.exports = groupDataService
