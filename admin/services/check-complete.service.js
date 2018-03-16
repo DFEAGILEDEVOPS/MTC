b35b6210086b2218c9d3dc855efe49b8152cadf4
Paula <hello@pris54.co.uk>
2018-03-16 11:47:24 +0000

    WIP.

--- admin/services/check-complete.service.js
+++ admin/services/check-complete.service.js
@@ -3,6 +3,7 @@
 const moment = require('moment')
 
 const completedCheckDataService = require('./data-access/completed-check.data.service')
+const checkDataService = require('./data-access/check.data.service')
 const jwtService = require('../services/jwt.service')
 const markingService = require('./marking.service')
 const pupilDataService = require('../services/data-access/pupil.data.service')
@@ -14,7 +15,9 @@ checkCompleteService.completeCheck = async function (completedCheck) {
   if (!(completedCheck && completedCheck.data)) {
     throw new Error('missing or invalid argument')
   }
+  console.log('++++ completedCheck.data', completedCheck.data)
   const decoded = jwtService.decode(completedCheck.data['access_token'])
+  console.log('++++ decoded', decoded)
   const pupilId = decoded.sub
   const pupil = await pupilDataService.sqlFindOneById(pupilId)
   // If pin expiration request failed previously ensure it is updated now
@@ -25,6 +28,38 @@ checkCompleteService.completeCheck = async function (completedCheck) {
   // TODO: This timestamp should be recorded in the application service tier instead
   completedCheck.receivedByServerAt = moment.utc()
 
+  const existingCheck = await checkDataService.sqlFindOneByCheckCode(completedCheck.data.pupil.checkCode)
+  if (!existingCheck.startedAt) {
+    const startedAt_ = completedCheck.data.audit.filter(logEntry => (logEntry.type === 'QuestionRendered'))
+
+    /*
+    [
+      {
+        type: 'QuestionRendered',
+        clientTimestamp: '2018-03-16T11:32:29.296Z',
+        data: { practiseSequenceNumber: 1, question: '1x7' }
+      },
+      {
+        type: 'QuestionRendered',
+        clientTimestamp: '2018-03-16T11:32:33.014Z',
+        data: { practiseSequenceNumber: 2, question: '3x10' }
+      }
+    ...
+    ]
+     */
+    const startedAt__ = completedCheck.data.audit.filter(logEntry => {
+      if (logEntry.type === 'QuestionRendered' && logEntry.data && logEntry.data.practiseSequenceNumber === 1) {
+        return logEntry.data.clientTimestamp
+      }
+    })
+
+    console.log('++++ (1)', startedAt_)
+    console.log('++++ (2)', startedAt__)
+    
+    const startedAt = moment()
+    await checkDataService.sqlUpdateCheckStartedAt(completedCheck.data.pupil.checkCode, startedAt)
+  }
+  existingCheck.startedAt = moment()
   // store to data store
   await completedCheckDataService.sqlAddResult(completedCheck.data.pupil.checkCode, completedCheck)
 
