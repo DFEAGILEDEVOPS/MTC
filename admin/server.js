373b6e776500205f52a6e7a27eabf6513e1c3787
Jon Shipley <js@ovix.co.uk>
2018-03-16 12:54:26 +0000

    Remove port ip address mod (shouldn't have been checked in)

--- admin/server.js
+++ admin/server.js
@@ -26,7 +26,7 @@ const server = http.createServer(app)
 /**
  * Listen on provided port, on all network interfaces.
  */
-server.listen(port, '0.0.0.0')
+server.listen(port)
 server.on('error', onError)
 server.on('listening', onListening)
 winston.info(`${config.Environment} server listening on port ${port}`)
