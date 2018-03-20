abcb39c604a1790822eba410c6f428a0699b3364
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-03-20 14:52:52 +0000

    Class disruption mandatory textarea on restart select screen

--- admin/lib/validator/restart-validator.js
+++ admin/lib/validator/restart-validator.js
@@ -11,7 +11,10 @@ const restartValidator = {}
 restartValidator.validateReason = (reason, specifyReason) => {
   const validationError = new ValidationError()
   if (reason === 'DNC' && specifyReason.length === 0) {
-    validationError.addError('didNotCompleteInfo', restartErrorMessages.didNotCompleteInfo)
+    validationError.addError('didNotCompleteInfo', restartErrorMessages.errorMessage)
+  }
+  if (reason === 'CLD' && specifyReason.length === 0) {
+    validationError.addError('classDisruptionInfo', restartErrorMessages.errorMessage)
   }
   return validationError
 }
