58b17ea34e25fd4d31c3ce52a235172e33a019f2
Jon Shipley <js@ovix.co.uk>
2018-03-26 12:30:55 +0100

    Add device type and user agent into report

--- admin/bin/detect-anomalies.js
+++ admin/bin/detect-anomalies.js
@@ -8,6 +8,7 @@ const chalk = require('chalk')
 const fs = require('fs')
 winston.level = 'info'
 const moment = require('moment')
+const useragent = require('useragent')
 
 const dateService = require('../services/date.service')
 const poolService = require('../services/data-access/sql.pool.service')
@@ -37,7 +38,7 @@ function detectWrongNumberOfAnswers (check) {
   const numberOfQuestions = check.data.questions.length
   const numberOfAnswers = check.data.answers.length
   if (numberOfAnswers !== numberOfQuestions) {
-    report(check.checkCode, 'Wrong number of answers', numberOfAnswers, numberOfQuestions)
+    report(check.checkCode, 'Wrong number of answers', check.data.device.navigator.userAgent, numberOfAnswers, numberOfQuestions)
   }
 }
 
@@ -45,7 +46,7 @@ function detectWrongNumberOfInputs (check) {
   const numberOfQuestions = check.data.questions.length
   const numberOfInputs = check.data.inputs.length
   if (numberOfInputs !== numberOfQuestions) {
-    report(check.checkCode, 'Wrong number of inputs', numberOfInputs, numberOfQuestions)
+    report(check.checkCode, 'Wrong number of inputs', check.data.device.navigator.userAgent, numberOfInputs, numberOfQuestions)
   }
 }
 
@@ -65,7 +66,7 @@ function detectLowBattery (check) {
   const battery = check.data.device.battery
   if (!battery) { return }
   if (battery.levelPercent < 20 && !battery.isCharging) {
-    report(check.checkCode, 'Low battery', '' + battery.levelPercent + '%' + ' charging ' + battery.isCharging, '> 20%')
+    report(check.checkCode, 'Low battery', check.data.device.navigator.userAgent, '' + battery.levelPercent + '%' + ' charging ' + battery.isCharging, '> 20%')
   }
 }
 
@@ -77,11 +78,11 @@ function detectInputBeforeOrAfterTheQuestionIsShown (check) {
   questions.forEach(question => {
     const questionRenderedEvent = check.data.audit.find(e => e.type === 'QuestionRendered' && e.data && e.data.sequenceNumber === question.order)
     if (!questionRenderedEvent) {
-      return report(check.checkCode, 'QuestionRenderedEvent not found', null, null, `Q${question.order}`)
+      return report(check.checkCode, 'QuestionRenderedEvent not found', check.data.device.navigator.userAgent, null, null, `Q${question.order}`)
     }
     const questionShownAt = moment(questionRenderedEvent.clientTimestamp)
     if (!questionShownAt.isValid()) {
-      return report(check.checkCode, 'QuestionRendered Timestamp is not valid', questionRenderedEvent.clientTimestamp, 'Valid ts')
+      return report(check.checkCode, 'QuestionRendered Timestamp is not valid', check.data.device.navigator.userAgent, questionRenderedEvent.clientTimestamp, 'Valid ts')
     }
     // If there are any inputs before `questionShownAt` it's an anomaly
     const inputs = check.data.inputs[question.order - 1]
@@ -97,15 +98,15 @@ function detectInputBeforeOrAfterTheQuestionIsShown (check) {
     // Check the inputs to make sure they all the right question property
     inputs.forEach(input => {
       if (input.question !== question.order) {
-        report(check.checkCode, 'Input fails property check', input.question, question.order)
+        report(check.checkCode, 'Input fails property check', check.data.device.navigator.userAgent, input.question, question.order)
       }
       const inputTimeStamp = moment(input.clientInputDate)
       if (inputTimeStamp.isBefore(questionShownAt)) {
-        report(check.checkCode, 'Input received before Question shown', input.clientInputDate, questionRenderedEvent.clientTimestamp, `Q${question.order}`)
+        report(check.checkCode, 'Input received before Question shown', check.data.device.navigator.userAgent, input.clientInputDate, questionRenderedEvent.clientTimestamp, `Q${question.order}`)
       }
       // We shouldn't have any input after the QuestionRendered ts + the question time-limit
       if (inputTimeStamp.isAfter(questionCutoffAt)) {
-        report(check.checkCode, 'Input received after cut-off', input.clientInputDate, dateService.formatIso8601(questionCutoffAt), `Q${question.order}`)
+        report(check.checkCode, 'Input received after cut-off', check.data.device.navigator.userAgent, input.clientInputDate, dateService.formatIso8601(questionCutoffAt), `Q${question.order}`)
       }
     })
   })
@@ -117,19 +118,19 @@ function detectMissingAudits (check) {
   const questionRenderedAudits = check.data.audit.filter(audit => audit.type === 'QuestionRendered')
   const numberOfPractiseQuestions = 3
   if (questionRenderedAudits.length !== numberOfQuestions + numberOfPractiseQuestions) {
-    report(check.checkCode, 'Wrong number of QuestionRendered audits', questionRenderedAudits.length, numberOfQuestions + numberOfPractiseQuestions)
+    report(check.checkCode, 'Wrong number of QuestionRendered audits', check.data.device.navigator.userAgent, questionRenderedAudits.length, numberOfQuestions + numberOfPractiseQuestions)
   }
 
   const pauseRenderedAudits = check.data.audit.filter(audit => audit.type === 'PauseRendered')
   if (pauseRenderedAudits.length !== numberOfQuestions + numberOfPractiseQuestions) {
-    report(check.checkCode, 'Wrong number of PauseRendered audits', pauseRenderedAudits.length, numberOfQuestions + numberOfPractiseQuestions)
+    report(check.checkCode, 'Wrong number of PauseRendered audits', check.data.device.navigator.userAgent, pauseRenderedAudits.length, numberOfQuestions + numberOfPractiseQuestions)
   }
 
   const detectMissingSingleAudit = function (auditType) {
     // Detect events should occur only once
     const audit = check.data.audit.find(audit => audit.type === auditType)
     if (!audit) {
-      report(check.checkCode, `Missing audit ${auditType}`)
+      report(check.checkCode, `Missing audit ${auditType}`, check.data.device.navigator.userAgent)
     }
   }
   const singleMandatoryAuditEvents = [
@@ -150,18 +151,18 @@ function detectInsufficientVerticalHeight (check) {
 
   // The vertical height required depends on the width, as we have 3 breakpoints
   if (width <= 640 && height < 558) {
-    report(check.checkCode, 'Insufficient browser vertical height', height, '> 558 pixels')
+    report(check.checkCode, 'Insufficient browser vertical height', check.data.device.navigator.userAgent, height, '> 558 pixels')
   } else if (width > 640 && width < 769 && height < 700) {
-    report(check.checkCode, 'Insufficient browser vertical height', height, '> 700 pixels')
+    report(check.checkCode, 'Insufficient browser vertical height', check.data.device.navigator.userAgent, height, '> 700 pixels')
   } else if (width > 769 && height < 660) {
-    report(check.checkCode, 'Insufficient browser vertical height', height, '> 660 pixels')
+    report(check.checkCode, 'Insufficient browser vertical height', check.data.device.navigator.userAgent, height, '> 660 pixels')
   }
 }
 
 function detectLowColourDisplays (check) {
   const colourDepth = check.data.device.screen.colorDepth
   if (colourDepth < 24) {
-    report(check.checkCode, 'Low colour display', colourDepth, '24')
+    report(check.checkCode, 'Low colour display', check.data.device.navigator.userAgent, colourDepth, '24')
   }
 }
 
@@ -176,22 +177,22 @@ function detectChecksThatTookLongerThanTheTheoreticalMax (check) {
   const checkStart = psUtilService.getClientTimestampFromAuditEvent('CheckStarted', check)
   const checkComplete = psUtilService.getClientTimestampFromAuditEvent('CheckSubmissionPending', check)
   if (!checkStart || !checkComplete) {
-    return report(check.checkCode, 'Missing audit event ' + (checkStart ? 'CheckStarted' : 'CheckSubmissionPending'))
+    return report(check.checkCode, 'Missing audit event ' + (checkStart ? 'CheckStarted' : 'CheckSubmissionPending'), check.data.device.navigator.userAgent)
   }
   if (checkStart === 'error' || checkComplete === 'error') {
-    return report(check.checkCode, 'Timestamp error ' + (checkStart === 'error' ? 'CheckStarted' : 'CheckSubmissionPending'))
+    return report(check.checkCode, 'Timestamp error ' + (checkStart === 'error' ? 'CheckStarted' : 'CheckSubmissionPending'), check.data.device.navigator.userAgent)
   }
   const checkStartDate = moment(checkStart)
   const checkCompleteDate = moment(checkComplete)
   if (!checkStartDate.isValid()) {
-    return report(check.checkCode, 'Invalid CheckStarted date', checkStart)
+    return report(check.checkCode, 'Invalid CheckStarted date', check.data.device.navigator.userAgent, checkStart)
   }
   if (!checkCompleteDate.isValid()) {
-    return report(check.checkCode, 'Invalid CheckSubmissionPending date', checkComplete)
+    return report(check.checkCode, 'Invalid CheckSubmissionPending date', check.data.device.navigator.userAgent, checkComplete)
   }
   const totalCheckSeconds = checkCompleteDate.diff(checkStartDate, 'seconds')
   if (totalCheckSeconds > maxCheckSeconds) {
-    report(check.checkCode, 'Check took too long', totalCheckSeconds, maxCheckSeconds)
+    report(check.checkCode, 'Check took too long', check.data.device.navigator.userAgent, totalCheckSeconds, maxCheckSeconds)
   }
 }
 
@@ -199,7 +200,7 @@ function detectInputThatDoesNotCorrespondToAnswers (check) {
   check.data.answers.forEach((answer, idx) => {
     const answerFromInputs = reconstructAnswerFromInputs(check.data.inputs[idx])
     if (answer.answer !== answerFromInputs) {
-      report(check.checkCode, 'Inputs do not correspond to given answer', answerFromInputs, answer.answer, `Q${idx + 1}`)
+      report(check.checkCode, 'Inputs do not correspond to given answer', check.data.device.navigator.userAgent, answerFromInputs, answer.answer, `Q${idx + 1}`)
     }
   })
 }
@@ -228,9 +229,10 @@ function reconstructAnswerFromInputs (events) {
   return ans
 }
 
-function report (checkCode, message, testedValue = null, expectedValue = null, questionNumber = null) {
+function report (checkCode, message, userAgent, testedValue = null, expectedValue = null, questionNumber = null) {
   anomalyCount += 1
-  reportedAnomalies.push([checkCode, message, testedValue, expectedValue, questionNumber])
+  const agent = useragent.lookup(userAgent)
+  reportedAnomalies.push([checkCode, agent.device.toString().replace('0.0.0', ''), agent.toString(), message, testedValue, expectedValue, questionNumber])
 }
 
 function writeCsv (data) {
@@ -246,7 +248,7 @@ function writeCsv (data) {
 
 async function main () {
   const ws = fs.createWriteStream(outputFilename, { flags: 'w' })
-  ws.write('Check Code,Message,Tested value,Expected value,Question number\n')
+  ws.write('Check Code,Device,Agent,Message,Tested value,Expected value,Question number\n')
   ws.end()
   const checkInfo = await completedCheckDataService.sqlFindMeta()
   winston.info(checkInfo)
