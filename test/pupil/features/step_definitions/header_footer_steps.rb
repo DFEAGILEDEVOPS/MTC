c0f7ade354941e0496d0ca14eb76665b8f13d129
Guy Harwood <guy@milkchip.com>
2018-03-26 12:10:21 +0100

    working pupil tests docker build

---
+++ test/pupil/features/step_definitions/header_footer_steps.rb
@@ -0,0 +1,43 @@
+Given(/^I am on the sign in page$/) do
+  sign_in_page.load
+end
+
+Then(/^I should see a global header$/) do
+  expect(sign_in_page).to have_global_header
+end
+
+Then(/^I should see a feedback text on the page$/) do
+  expect(sign_in_page.phase_banner).to have_feedback
+end
+
+Then(/^I should see a footer with a link to crown copyright$/) do
+  expect(sign_in_page).to have_footer_link
+end
+
+Given(/^I have logged in$/) do
+  sign_in_page.load
+  sign_in_page.load
+  ct = Time.now
+  new_time = Time.new(ct.year, ct.mon, ct.day, 22, 00, 00, "+02:00").strftime("%Y-%m-%d %H:%M:%S.%LZ")
+
+  SqlDbHelper.expire_pin("Standard","Pupil",1,false)
+  SqlDbHelper.reset_pin("Standard","Pupil",1, new_time, "9999")
+  @pupil_information = SqlDbHelper.find_pupil_via_pin("9999")
+  sign_in_page.login("abc12345","9999")
+  sign_in_page.sign_in_button.click
+
+end
+
+Given(/^I am on the welcome page$/) do
+  step 'I have logged in'
+end
+
+Given(/^I am on the instructions page$/) do
+  step 'I have logged in'
+  confirmation_page.read_instructions.click
+end
+
+Given(/^I am on the complete page$/) do
+  step 'I have started the check'
+  step 'I should be able to use the on screen keyboard to complete the test'
+end
--- test/pupil/features/support/env.rb
+++ test/pupil/features/support/env.rb
@@ -1,4 +1,3 @@
-require 'rubygems'
 require 'active_support'
 require 'capybara'
 require 'capybara/cucumber'
@@ -8,13 +7,13 @@ require 'selenium-webdriver'
 require 'site_prism'
 require 'pry'
 require 'capybara/poltergeist'
+require 'numbers_in_words'
 require 'mongo'
-require 'csv'
-require 'fileutils'
-require 'date'
 require 'waitutil'
 require 'tiny_tds'
+require 'show_me_the_cookies'
 require_relative '../../features/support/browserstack_driver_helper'
+
 require_relative 'helpers'
 include Helpers
 
@@ -23,7 +22,7 @@ raise 'Please pass a BASE_URL with the cucumber command.' unless ENV['BASE_URL']
 Capybara.configure do |config|
   config.default_driver = ENV["DRIVER"].to_sym
   config.app_host = ENV["BASE_URL"]
-  config.exact = true
+  config.exact_options = true
   config.ignore_hidden_elements = false
   config.visible_text_only = true
 end
@@ -36,16 +35,6 @@ Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, js_errors: false, timeout: 60)
 end
 
-# Capybara.register_driver :headless_chrome do |app|
-#   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-#   chromeOptions: { args: %w(headless)}
-#   )
-#
-#   Capybara::Selenium::Driver.new app,
-#   browser: :chrome,
-#   desired_capabilities: capabilities
-# end
-
 Capybara.register_driver :headless_chrome do |app|
   args = ["--window-size=1280,1696", "--disable-infobars", "--disable-notifications", "--no-sandbox", "--headless", "--disable-gpu"]
   Capybara::Selenium::Driver.new(app, {:browser => :chrome, :args => args})
@@ -55,29 +44,21 @@ Dir.mkdir("reports") unless File.directory?("reports")
 Capybara.javascript_driver = ENV["DRIVER"].to_sym
 
 Mongo::Logger.logger.level = ::Logger::FATAL
+ShowMeTheCookies.register_adapter(ENV['DRIVER'].to_sym, ShowMeTheCookies::Selenium) unless ENV['DRIVER'] == 'poltergeist'
+ShowMeTheCookies.register_adapter('poltergeist', ShowMeTheCookies::Selenium) if ENV['DRIVER'] == 'poltergeist'
 
-if ENV['MONGO_CONNECTION_STRING']
-  CLIENT = Mongo::Client.new(ENV['MONGO_CONNECTION_STRING'])
-else
-  CLIENT = Mongo::Client.new('mongodb://mongo/mtc')
-end
-sleep 20
 database = ENV['SQL_DATABASE'] || 'mtc'
-server = ENV['SQL_SERVER'] || 'sqldb'
+server = ENV['SQL_SERVER'] || 'localhost'
 port =  ENV['SQL_PORT'] || 1433
 admin_user = ENV['SQL_ADMIN_USER'] || 'sa'
 admin_password = ENV['SQL_ADMIN_USER_PASSWORD'] || 'Mtc-D3v.5ql_S3rv3r'
 
-begin
+
 SQL_CLIENT = TinyTds::Client.new(username: admin_user,
                                  password: admin_password,
                                  host: server,
                                  port: port,
                                  database: database)
-rescue TinyTds::Error => e
-  abort 'Test run failed due to - ' + e.to_s
-end
-
 SQL_CLIENT.execute('SET ANSI_NULLS ON').do
 SQL_CLIENT.execute('SET CURSOR_CLOSE_ON_COMMIT OFF').do
 SQL_CLIENT.execute('SET ANSI_NULL_DFLT_ON ON').do
@@ -87,5 +68,7 @@ SQL_CLIENT.execute('SET QUOTED_IDENTIFIER ON').do
 SQL_CLIENT.execute('SET ANSI_WARNINGS ON').do
 SQL_CLIENT.execute('SET CONCAT_NULL_YIELDS_NULL ON').do
 
-
 Capybara.visit Capybara.app_host
+AUTH='5'
+
+World(ShowMeTheCookies)
