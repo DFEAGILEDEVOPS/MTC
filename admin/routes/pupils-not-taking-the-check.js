5a99ce78292cf79a55fc8988cf9693aa02818fa2
Paula Molinero <pris54@gmail.com>
2018-03-27 15:09:30 +0100

    Feature/17255 school controller refactoring part 1 (#595)
    * School controller refactoring.

---
+++ admin/routes/pupils-not-taking-the-check.js
@@ -0,0 +1,49 @@
+const rolesConfig = require('../roles-config')
+const express = require('express')
+const router = express.Router()
+const isAuthenticated = require('../authentication/middleware')
+
+const pupilsNotTakingTheCheck = require('../controllers/pupils-not-taking-the-check')
+
+router.get(
+  '/select-pupils/:groupIds?',
+  isAuthenticated(rolesConfig.ROLE_TEACHER),
+  (req, res, next) => pupilsNotTakingTheCheck.getSelectPupilNotTakingCheck(req, res, next)
+)
+router.get(
+  '/select-pupils/:sortField/:sortDirection/:groupIds?',
+  isAuthenticated(rolesConfig.ROLE_TEACHER),
+  (req, res, next) => pupilsNotTakingTheCheck.getSelectPupilNotTakingCheck(req, res, next)
+)
+router.get(
+  '/save-pupils',
+  isAuthenticated(rolesConfig.ROLE_TEACHER),
+  (req, res, next) => pupilsNotTakingTheCheck.getSelectPupilNotTakingCheck(req, res, next)
+)
+router.post(
+  '/save-pupils',
+  isAuthenticated(rolesConfig.ROLE_TEACHER),
+  (req, res, next) => pupilsNotTakingTheCheck.savePupilNotTakingCheck(req, res, next)
+)
+router.get(
+  '/remove/:pupilId',
+  isAuthenticated(rolesConfig.ROLE_TEACHER),
+  (req, res, next) => pupilsNotTakingTheCheck.removePupilNotTakingCheck(req, res, next)
+)
+router.get(
+  '/view',
+  isAuthenticated(rolesConfig.ROLE_TEACHER),
+  (req, res, next) => pupilsNotTakingTheCheck.viewPupilsNotTakingTheCheck(req, res, next)
+)
+router.get(
+  ['/', '/pupils-list'],
+  isAuthenticated(rolesConfig.ROLE_TEACHER),
+  (req, res, next) => pupilsNotTakingTheCheck.getPupilNotTakingCheck(req, res, next)
+)
+router.get(
+  '/:removed',
+  isAuthenticated(rolesConfig.ROLE_TEACHER),
+  (req, res, next) => pupilsNotTakingTheCheck.getPupilNotTakingCheck(req, res, next)
+)
+
+module.exports = router
