06c891208e56e612c44fbe9b0a301da90283d337
Guy Harwood <guy@milkchip.com>
2018-03-27 17:53:16 +0100

    add logging and comments

--- run-compose-test-suite.sh
+++ run-compose-test-suite.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -e
 
-# Wrap docker-compose and return a non-zero exit code if any containers failed.
 
 docker-compose -f docker-compose.yml -f docker-compose.test.yml $@
+# get test container output while they run 
+docker logs -f admin_tests
+docker logs -f pupil_tests
+# capture test container exit codes
 ADMIN_RETURN_CODE=$(docker wait admin_tests)
 PUPIL_RETURN_CODE=$(docker wait pupil_tests)
 
 echo "admin code: $ADMIN_RETURN_CODE"
 echo "pupil code: $PUPIL_RETURN_CODE"
-
+# return non-zero exit code if either container failed
 if [ $ADMIN_RETURN_CODE -ne 0 ]
 then
   exit $ADMIN_RETURN_CODE
