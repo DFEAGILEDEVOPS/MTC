981c2d679cccb1ae483bcbc8e4d17d87e55e27d2
Jon Shipley <js@ovix.co.uk>
2018-04-06 16:11:49 +0100

    Update tests

--- pupil-spa/src/app/sound/sound.component.spec.ts
+++ pupil-spa/src/app/sound/sound.component.spec.ts
@@ -22,4 +22,18 @@ describe('SoundComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('has a function to play the end of question sound', () => {
+    expect(typeof component.playEndOfQuestionSound).toBe('function');
+  });
+
+  it('has a function to play an alert that the time is nearly up', () => {
+    expect(typeof component.playTimeRunningOutAlertSound).toBe('function');
+  });
+
+  it('should have two audio elements', () => {
+    const el: HTMLElement = fixture.nativeElement;
+    const audioElements = el.querySelectorAll('audio');
+    expect(audioElements.length).toBe(2);
+  });
 });
