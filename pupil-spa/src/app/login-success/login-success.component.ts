d7b6ad62b33c5e192c1a73cb1e727c63506956f8
Dan Williams <dan@ithium.net>
2018-04-09 16:36:02 +0100

    Added speech screen-reading on the welcome page
      - The `config` doesn’t have a service on its own, so I’ve used the
        questionService to call getConfig, in order to check the speech flag
        (if the user has speech enabled and should read the page or not).
    
      - Because of problems with not pausing in-between items, I got all the
        elements that could contain text separately (`'h1, h2, h3, h4, h5,
        h6, p, button'` - there might be others?) and adding an ‘artificial’
        pause (inserted a comma) for the speech reader.

--- pupil-spa/src/app/login-success/login-success.component.ts
+++ pupil-spa/src/app/login-success/login-success.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { Pupil } from '../pupil';
 import { School } from '../school';
 import { Router } from '@angular/router';
 import { StorageService } from '../services/storage/storage.service';
 import { DeviceService } from '../services/device/device.service';
+import { SpeechService } from '../services/speech/speech.service';
+import { QuestionService } from '../services/question/question.service';
 
 @Component({
   selector: 'app-login-success',
@@ -17,7 +19,10 @@ export class LoginSuccessComponent implements OnInit {
 
   constructor(private router: Router,
               private storageService: StorageService,
-              private deviceService: DeviceService) {
+              private deviceService: DeviceService,
+              private questionService: QuestionService,
+              private speechService: SpeechService,
+              private elRef: ElementRef) {
     const pupilData = storageService.getItem('pupil');
     const schoolData = storageService.getItem('school');
     this.pupil = new Pupil;
@@ -31,6 +36,23 @@ export class LoginSuccessComponent implements OnInit {
   async ngOnInit() {
     // Store various browser props in localStorage to be sent back to the server at the end of the check.
     await this.deviceService.capture();
+  }
+
+  // wait for the component to be rendered first, before parsing the text
+  ngAfterViewInit() {
+    if (this.questionService.getConfig().speechSynthesis) {
+      let speechText = '';
+
+      // get all elements containing text from the current component
+      let elements = this.elRef.nativeElement.querySelectorAll(
+        'h1, h2, h3, h4, h5, h6, p, button'
+      );
+
+      // add 'artificial' pauses to take visual newlines or spaces into account
+      elements.forEach(elem => speechText += elem.textContent + ' , ');
+
+      this.speechService.speak(speechText);
+    }
   }
 
   onClick() {
