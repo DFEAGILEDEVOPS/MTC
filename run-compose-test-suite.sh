f7fa9b0556a95e52c9e140f3ee4b98d82c3b4429
activemq <m-qureshi@hotmail.com>
2018-04-12 18:54:26 +0100

    attempt at getting the admin tests and pupil tests to run sequentially, as it may have been causing the DB conflicts

--- run-compose-test-suite.sh
+++ run-compose-test-suite.sh
@@ -2,10 +2,11 @@
 set -e
 
 
-docker-compose -f docker-compose.yml -f docker-compose.test.yml up -d --build
-# get test container output while they run
-docker logs -f admin_tests
-docker logs -f pupil_tests
+docker-compose -f docker-compose.yml -f docker-compose.test.yml build
+docker-compose -f docker-compose.yml -f docker-compose.test.yml up -d pupil-app
+docker-compose -f docker-compose.yml -f docker-compose.test.yml up admin-tests
+docker-compose -f docker-compose.yml -f docker-compose.test.yml up pupil-tests
+
 # capture test container exit codes
 ADMIN_RETURN_CODE=$(docker wait admin_tests)
 PUPIL_RETURN_CODE=$(docker wait pupil_tests)
