52783c1e3d41c326e88aca4904003b8ca18e96cb
Dan Williams <dan@ithium.net>
2018-04-19 15:19:31 +0100

    Stripped out config and whitelist-dev

--- pupil-auth/config.js
+++ pupil-auth/config.js
@@ -12,22 +12,11 @@ const getEnvironment = () => {
 
 module.exports = {
   AZURE_STORAGE_CONNECTION_STRING: process.env.AZURE_STORAGE_CONNECTION_STRING,
-  GOOGLE_TRACKING_ID: process.env.GOOGLE_TRACKING_ID,
   MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING || 'mongodb://localhost/mtc',
-  NCA_TOOLS_AUTH_URL: process.env.NCA_TOOLS_AUTH_URL,
   PORT: process.env.PORT || '3003',
-  PUPIL_APP_URL: process.env.PUPIL_APP_URL,
   QUESTION_TIME_LIMIT: 5,
-  RESTART_MAX_ATTEMPTS: 2,
-  SESSION_SECRET: process.env.NODE_ENV === 'production' ? process.env.SESSION_SECRET : 'anti tamper for dev',
   TIME_BETWEEN_QUESTIONS: 2,
-  LINES_PER_CHECK_FORM: 25,
-  // autoMark true | false - Automatically mark the check data when we receive it: boolean
-  autoMark: process.env.hasOwnProperty('AUTO_MARK') ? toBool(process.env.AUTO_MARK) : true,
   Data: {
-    allowedWords: process.env.ALLOWED_WORDS || 'aaa,bcd,dcd,tfg,bxx',
-    pinSubmissionMaxAttempts: process.env.PIN_SUBMISSION_MAX_ATTEMPTS || 100,
-    helplineNumber: process.env.HELPLINE_NUMBER || '0345 278 8080'
   },
   Sql: {
     Enabled: true, // deprecated, to be removed
@@ -46,11 +35,6 @@ module.exports = {
       MaxCount: process.env.SQL_POOL_MAX_COUNT || 10,
       LoggingEnabled: process.env.hasOwnProperty('SQL_POOL_LOG_ENABLED') ? toBool(process.env.SQL_POOL_LOG_ENABLED) : true
     },
-    Migrator: {
-      Username: process.env.SQL_ADMIN_USER || 'sa', // docker default
-      Password: process.env.SQL_ADMIN_USER_PASSWORD || 'Mtc-D3v.5ql_S3rv3r', // docker default
-      Timeout: process.env.SQL_MIGRATION_TIMEOUT || twoMinutesInMilliseconds
-    },
     Azure: {
       Scale: process.env.SQL_AZURE_SCALE
     }
@@ -68,19 +52,6 @@ module.exports = {
       UseWinston: process.env.EXPRESS_LOGGING_WINSTON || false
     }
   },
-  OverridePinExpiry: process.env.hasOwnProperty('OVERRIDE_PIN_EXPIRY') ? toBool(process.env.OVERRIDE_PIN_EXPIRY) : false,
-  Certificates: {
-    Azure: {
-      BlobContainer: process.env.CERT_BLOB_CONTAINER,
-      NcaToolsPublicKeyName: process.env.CERT_NCATOOLS_PUBLIC_KEY_NAME,
-      MtcPrivateKeyName: process.env.CERT_MTC_PRIVATE_KEY_NAME
-    },
-    Local: {
-      NcaToolsPublicKey: process.env.TSO_AUTH_PUBLIC_KEY,
-      MtcPrivateKey: process.env.MTC_AUTH_PRIVATE_KEY
-    }
-  },
   Environment: getEnvironment(),
-  AssetPath: process.env.ASSET_PATH || '/',
   JwtSecret: process.env.JWT_SECRET || '0bd8fbdab903fb50455cb4cf7ba9fb5beed3f209687ed2644aa731ee8231e14c'
 }
--- pupil-auth/whitelist-dev.js
+++ pupil-auth/whitelist-dev.js
@@ -3,14 +3,7 @@
  */
 
 const whitelist = [
-  'AZURE_STORAGE_CONNECTION_STRING',
-  'GOOGLE_TRACKING_ID',
-  'MTC_AUTH_PRIVATE_KEY',
-  'NCA_TOOLS_AUTH_URL',
-  'PUPIL_APP_URL',
-  'RESTART_MAX_ATTEMPTS',
-  'TSO_AUTH_PUBLIC_KEY',
-  'OverridePinExpiry'
+  'AZURE_STORAGE_CONNECTION_STRING'
 ]
 
 module.exports = process.env.NODE_ENV !== 'production' ? whitelist : []
