87b04dea80e5a8c652e56ced56ce323732716137
Dan Williams <dan@ithium.net>
2018-04-19 14:17:36 +0100

    Removed CSP

--- pupil-auth/app.js
+++ pupil-auth/app.js
@@ -65,31 +65,6 @@ if (config.Logging.Express.UseWinston === 'true') {
 
 app.use(cors())
 app.use(helmet())
-const scriptSources = ["'self'", "'unsafe-inline'"]
-const styleSources = ["'self'", "'unsafe-inline'"]
-const imgSources = ["'self'", 'data:']
-const objectSources = ["'self'"]
-
-if (config.AssetPath !== '/') {
-  // add CSP policy for assets domain
-  scriptSources.push(config.AssetPath)
-  styleSources.push(config.AssetPath)
-  imgSources.push(config.AssetPath)
-  objectSources.push(config.AssetPath)
-}
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: scriptSources,
-    fontSrc: ["'self'", 'data:'],
-    styleSrc: styleSources,
-    imgSrc: imgSources,
-    connectSrc: ["'self'"],
-    objectSrc: objectSources,
-    mediaSrc: ["'none'"],
-    childSrc: ["'none'"]
-  }
-}))
 
 // Sets request header "Strict-Transport-Security: max-age=31536000; includeSubDomains".
 const oneYearInSeconds = 31536000
