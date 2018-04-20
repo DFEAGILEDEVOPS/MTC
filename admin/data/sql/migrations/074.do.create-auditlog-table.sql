e6c5de16e947f43357f55e524583d723789da5d0
Guy Harwood <guy@milkchip.com>
2018-04-20 11:51:26 +0100

    index and bigint

--- admin/data/sql/migrations/074.do.create-auditlog-table.sql
+++ admin/data/sql/migrations/074.do.create-auditlog-table.sql
@@ -1,8 +1,11 @@
 CREATE TABLE mtc_admin.auditLog
 (
-	id int identity primary key,
+	id bigint identity primary key,
 	rowData nvarchar(max) not null,
 	tableName nvarchar(255) not null,
 	operation nvarchar(20) not null,
 	createdAt datetimeoffset default getutcdate() not null
 )
+
+create index auditLog_tableName_index
+	on mtc_admin.auditLog (tableName)
