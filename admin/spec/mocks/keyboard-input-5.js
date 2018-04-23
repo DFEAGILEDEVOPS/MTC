eafa74f65194694d763b2ec6e89880eef656445c
Jon Shipley <jon-shipley@users.noreply.github.com>
2018-04-23 17:28:47 +0100

    Bug/20320 question response time is recorded for entries without numeric values (#621)
    * Update psychometrician report
    
    * only consider input that is part of the submitted answer when reporting on `getFirstInputTime`, `getLastAnswerInputTime`, `getResponseTime`
    
    * Exclude the data-access directory from the coverage report, as there is little value in testing this
    
    * Rename a file that was missing the '.spec' extension (and wasn't being run in the tests
    
    * Lint warning fixes
    
    * Remove the routes from the code coverage report

---
+++ admin/spec/mocks/keyboard-input-5.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    'question': 1,
+    'clientInputDate': '2017-10-17T18:18:42.333Z',
+    'eventType': 'keydown',
+    'input': '1'
+  },
+  {
+    'question': 1,
+    'clientInputDate': '2017-10-17T18:20:44.447Z',
+    'eventType': 'keydown',
+    'input': '1'
+  },
+  {
+    'question': 1,
+    'clientInputDate': '2017-10-17T18:20:44.670Z',
+    'eventType': 'keydown',
+    'input': 'Backspace'
+  },
+  {
+    'question': 1,
+    'clientInputDate': '2017-10-17T18:20:44.670Z',
+    'eventType': 'keydown',
+    'input': 'Backspace'
+  },
+  {
+    'question': 1,
+    'clientInputDate': '2017-10-17T18:20:44.678Z',
+    'eventType': 'keydown',
+    'input': 'Enter'
+  }
+]
