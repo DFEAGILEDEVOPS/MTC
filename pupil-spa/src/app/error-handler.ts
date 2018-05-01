6ec08838fcca573a21717ce05979ed3519c19802
Jon Shipley <js@ovix.co.uk>
2018-05-01 17:00:41 +0100

    Update error handler to provide stack trace and url

--- pupil-spa/src/app/error-handler.ts
+++ pupil-spa/src/app/error-handler.ts
@@ -1,25 +1,37 @@
-import { ErrorHandler, Injectable} from '@angular/core';
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import { WindowRefService } from './services/window-ref/window-ref.service';
 import { AuditService } from './services/audit/audit.service';
 import { AppError } from './services/audit/auditEntry';
+import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
+
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
 
   protected window: any;
 
-  constructor(protected windowRefService: WindowRefService,
-              private auditService: AuditService) {
-    this.window = windowRefService.nativeWindow;
-  }
-  handleError(error) {
-    this.auditService.addEntry(
-      new AppError({
-        errorMessage: error.message
-      }
-    ));
+  constructor(private injector: Injector) { }
 
-    this.window.ga('send', {
+  handleError(error) {
+    // Pick up our dependencies.  Don't use dependency injection - so this module can be loaded first.
+    const auditService = this.injector.get(AuditService);
+    const windowRefService = this.injector.get(WindowRefService);
+    const window = windowRefService.nativeWindow;
+    const location = this.injector.get(LocationStrategy);
+
+    // Get the error details
+    const errorMessage =  error.message ? error.message : error.toString();
+    const url = location instanceof PathLocationStrategy ? location.path() : '';
+
+    auditService.addEntry(
+      new AppError({
+          errorMessage,
+          url,
+          stackTrace: error.stack
+        }
+      ));
+
+    window.ga('send', {
       hitType: 'pageview',
       page: '/error'
     });
