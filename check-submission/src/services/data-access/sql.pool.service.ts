08afe1f63b6b043681b52aa160080fc7b27eb408
Dan Williams <dan.github@ithium.net>
2018-05-03 11:01:40 +0100

    Feature/20097 check submission microservice (#631)
    * 20097 Check Submission Microservice
    
    Initial creation and dependency extraction from admin-app
    
    * Added initial TypeScript configuration
    
    Added initial serving / routing / linting for typescript
    
    * Converted check-submission service to TypeScript
    
    * Added to docker-files and made it customizable
    
    * Fixed problems when running inside docker
    
    * Changed the JWT dev token to be more descriptive
    
    Added the padding because of security and local checks for 64 length
    
    * Updated CurrentServerTime to be a moment ISOString
    
    * remove old checkapi spike project
    
    * Enabled CircleCI for check-submission microservice
    
    * Changed npm run build to yarn build in the dockerfile
    
    * remove semicolons, as per standard JS
    
    * fix codacy issues
    
    * Fixed gulpfile and added standard for plain .js files
    
    At the moment, gulpfile.js is the only .js file

---
+++ check-submission/src/services/data-access/sql.pool.service.ts
@@ -0,0 +1,71 @@
+'use strict'
+
+let ConnectionPool = require('tedious-connection-pool')
+const config = require('../../config')
+const winston = require('winston')
+// TODO add to config object
+let poolConfig = {
+  min: config.Sql.Pooling.MinCount,
+  max: config.Sql.Pooling.MaxCount,
+  log: config.Sql.Pooling.LoggingEnabled
+}
+
+// full config details: https://github.com/tediousjs/tedious/blob/master/src/connection.js
+let connectionConfig = {
+  appName: config.Sql.Application.Name,
+  userName: config.Sql.Application.Username,
+  password: config.Sql.Application.Password,
+  server: config.Sql.Server,
+  options: {
+    port: config.Sql.Port,
+    database: config.Sql.Database,
+    encrypt: true,
+    requestTimeout: config.Sql.Timeout,
+    useUTC: false
+  }
+}
+
+let pool = null
+
+const sqlPoolService = {
+  /**
+   * Initialise the connection pool.  Called once per application instance
+   */
+  init: () => {
+    if (pool !== null) return
+    pool = new ConnectionPool(poolConfig, connectionConfig)
+    pool.on('error', function (err) {
+      winston.error(err)
+    })
+  },
+
+  /**
+   * Get a connection from the pool.
+   * @return {Promise}
+   */
+  getConnection: () => {
+    return new Promise((resolve, reject) => {
+      if (pool === null) {
+        sqlPoolService.init()
+      }
+      pool.acquire(function (err, connection) {
+        if (err) {
+          reject(err)
+          return
+        }
+        resolve(connection)
+      })
+    })
+  },
+
+  /**
+   * Disconnect all pool connections
+   */
+  drain: () => {
+    if (pool) {
+      pool.drain()
+    }
+  }
+}
+
+export = sqlPoolService
