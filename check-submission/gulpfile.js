bcbf4389179e19c2bcc3806b8c7ecbc958ff8e9f
Dan Williams <dan@ithium.net>
2018-05-03 10:15:50 +0100

    Fixed gulpfile and added standard for plain .js files
    At the moment, gulpfile.js is the only .js file

--- check-submission/gulpfile.js
+++ check-submission/gulpfile.js
@@ -1,23 +1,23 @@
-const gulp = require('gulp');
-const ts = require('gulp-typescript');
-const JSON_FILES = ['src/*.json', 'src/**/*.json'];
+const gulp = require('gulp')
+const ts = require('gulp-typescript')
+const JSON_FILES = ['src/*.json', 'src/**/*.json']
 
 // pull in the project TypeScript config
-const tsProject = ts.createProject('tsconfig.json');
+const tsProject = ts.createProject('tsconfig.json')
 
 gulp.task('scripts', () => {
   const tsResult = tsProject.src()
-  .pipe(tsProject());
-  return tsResult.js.pipe(gulp.dest('dist'));
-});
+  .pipe(tsProject())
+  return tsResult.js.pipe(gulp.dest('dist'))
+})
 
 gulp.task('watch', ['scripts'], () => {
-  gulp.watch('src/**/*.ts', ['scripts']);
-});
+  gulp.watch('src/**/*.ts', ['scripts'])
+})
 
-gulp.task('assets', function() {
+gulp.task('assets', function () {
   return gulp.src(JSON_FILES)
-  .pipe(gulp.dest('dist'));
-});
+  .pipe(gulp.dest('dist'))
+})
 
-gulp.task('default', ['watch', 'assets']);
+gulp.task('default', ['watch', 'assets'])
