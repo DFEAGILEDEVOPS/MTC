aff63cf95cccee8c53422e6a1dd0673c16dbc77e
Dan Williams <dan.github@ithium.net>
2018-05-03 10:59:14 +0100

    Feature/20757 check started typescript conversion (#632)
    * 20098 Check Started Microservice
    
    Initial service creation, extracted dependencies from admin-app
    
    * Remove unneeded config service from check-started
    
    * Updated docker-compose files for the check-started service
    
    * Updated docker files for check-started and made it configurable
    
    * Changed the JWT dev token to be more descriptive
    
    Added the padding because of security and local checks for 64 length
    
    * Updated CurrentServerTime to be a moment ISOString
    
    * 20757 Converted check-started microservice to use TypeScript
    
    * Changed npm run build to yarn build in the dockerfile
    
    * Fixed docker-compose file for pupil-tests
    
    * Fixed gulpfile and added standard for plain .js files

---
+++ check-started/src/routes/index.ts
@@ -0,0 +1,95 @@
+'use strict'
+
+import { Router as Router, Request, Response } from 'express'
+import * as path from 'path'
+import * as fs from 'fs'
+import * as moment from 'moment'
+
+const { checkStarted } = require('../controllers/check-started')
+
+export class IndexRouter {
+  router: Router
+
+  /**
+   * Initialize the IndexRouter
+   */
+  constructor () {
+    this.router = Router()
+    this.init()
+  }
+
+  /**
+   * Take each handler, and attach to one of the Express.Router's
+   * endpoints.
+   */
+  public init () {
+    this.router.get('/ping', (req: Request, res: Response) => this.getPing(req, res))
+
+    /* check-started microservice */
+    this.router.route('/submit').all((req: Request, res: Response) => {
+      if (req.method !== 'POST') return res.sendStatus(405)
+      checkStarted(req, res)
+    })
+  }
+
+  private async getPing (req: Request, res: Response) {
+    // get build number from /build.txt
+    // get git commit from /commit.txt
+    let buildNumber: object | string = 'NOT FOUND'
+    let commitId: object | string = 'NOT FOUND'
+    try {
+      buildNumber = await this.getBuildNumber()
+    } catch (error) {
+      // error
+    }
+
+    try {
+      commitId = await this.getCommitId()
+    } catch (error) {
+      // error
+    }
+
+    res.setHeader('Content-Type', 'application/json')
+    let obj = {
+      'Build': buildNumber,
+      'Commit': commitId,
+      'CurrentServerTime': moment().toISOString()
+    }
+    return res.status(200).send(obj)
+  }
+
+  /* Health check */
+
+  private getCommitId () {
+    return new Promise(function (resolve, reject) {
+      const commitFilePath = path.join(__dirname, '..', 'public', 'commit.txt')
+      fs.readFile(commitFilePath, 'utf8', function (err, data) {
+        if (!err) {
+          resolve(data)
+        } else {
+          reject(new Error('NOT FOUND'))
+        }
+      })
+    })
+  }
+
+  private getBuildNumber () {
+    // Promise wrapper function
+    return new Promise(function (resolve, reject) {
+      const buildFilePath = path.join(__dirname, '..', 'public', 'build.txt')
+      fs.readFile(buildFilePath, 'utf8', function (err, data) {
+        if (!err) {
+          resolve(data)
+        } else {
+          reject(new Error('NOT FOUND'))
+        }
+      })
+    })
+  }
+}
+
+// Create the HeroRouter, and export its configured Express.Router
+const indexRoutes = new IndexRouter()
+indexRoutes.init()
+
+export default indexRoutes.router
