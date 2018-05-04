368c2fed6ef3661b21010e17b2e9debde3e0e234
Dan Williams <dan@ithium.net>
2018-05-04 18:12:21 +0100

    20614 - Added sequenceNumber and question to all other events

--- pupil-spa/src/app/warmup-loading/warmup-loading.component.ts
+++ pupil-spa/src/app/warmup-loading/warmup-loading.component.ts
@@ -1,6 +1,7 @@
 import { Component, AfterViewInit, ElementRef } from '@angular/core';
 import { LoadingComponent } from '../loading/loading.component';
 import { AuditService } from '../services/audit/audit.service';
+import { PauseRendered } from '../services/audit/auditEntry';
 import { SpeechService } from '../services/speech/speech.service';
 import { QuestionService } from '../services/question/question.service';
 
@@ -16,5 +17,27 @@ export class WarmupLoadingComponent extends LoadingComponent implements AfterVie
               speechService: SpeechService,
               elRef: ElementRef) {
     super(auditService, questionService, speechService, elRef);
+  }
+
+  ngAfterViewInit() {
+    this.auditService.addEntry(new PauseRendered({
+      practiseSequenceNumber: this.question.sequenceNumber,
+      question: `${this.question.factor1}x${this.question.factor2}`
+    }));
+
+    // wait for the component to be rendered first, before parsing the text
+    if (this.questionService.getConfig().speechSynthesis) {
+      this.speechService.speakElement(this.elRef.nativeElement);
+
+      setTimeout(() => {
+        this.speechService.waitForEndOfSpeech().then(() => {
+          this.sendTimeoutEvent();
+        });
+      }, this.loadingTimeout * 1000);
+    } else {
+      setTimeout(() => {
+        this.sendTimeoutEvent();
+      }, this.loadingTimeout * 1000);
+    }
   }
 }
