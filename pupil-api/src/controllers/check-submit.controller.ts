179bee0d2840b5fabc9d91f33f1da99a70e2e3ef
Guy Harwood <GuyHarwood@users.noreply.github.com>
2018-05-11 10:17:37 +0100

    Feature/20878 pupil api (#641)
    * pupil api project
    
    * restart with new baseplate from check-submission
    
    * new pupil api from check-submission (full ts files)
    
    * pull in routes and controllers from existing api projects.  set up route config
    
    * disable feedback route until implemented
    
    * builds but all routes are 404
    
    * add migrator utils
    
    * first set of migrations for 2nd database
    
    * add migration runner
    
    * fix routes
    
    * redo test docker files
    
    * re-format create db code.  fix docker compose config
    
    * compose config
    
    * working migrations
    
    * add auth & started datestamps to check table
    
    * fixes as per Dans comments
    
    * comment on intent of file purpose
    
    * wipe out old code (from admin app). create skeleton controllers with routes
    
    * delete obsolete API projects
    
    * include dotenv file in build
    
    * convert migrations to typescript, fix outstanding lint & build issues
    
    * fix circle config
    
    * disable pupil-api in travis until needed
    
    * remove superfluous services
    
    * add codacy config file to ignore SQL

---
+++ pupil-api/src/controllers/check-submit.controller.ts
@@ -0,0 +1,10 @@
+
+import { Request, Response } from 'express'
+
+class CheckSubmitController {
+  async postSubmit (req: Request, res: Response) {
+    return res.status(501).send('Not yet implemented')
+  }
+}
+
+export default new CheckSubmitController()
