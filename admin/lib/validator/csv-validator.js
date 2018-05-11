0a40a1873dd8a285b0df9e0f107ff87f82b1b49e
Jon Shipley <js@ovix.co.uk>
2018-05-11 09:02:33 +0100

    Add fix for the file upload count including blank lines

--- admin/lib/validator/csv-validator.js
+++ admin/lib/validator/csv-validator.js
@@ -1,5 +1,6 @@
 const ValidationError = require('../validation-error')
 const addBatchFileErrorMessages = require('../errors/csv-pupil-upload')
+const R = require('ramda')
 
 const validateHeader = (header) => header[0] === 'Surname' && header[1] === 'Forename' && header[2] === 'Middle name(s)' &&
   header[3] === 'Date of birth' && header[4] === 'Gender' && header[5] === 'UPN'
@@ -13,10 +14,41 @@ module.exports.validate = (dataSet, header, element) => {
   if (dataSet.some(r => r.length !== 6)) {
     errorArr.push(addBatchFileErrorMessages.not6Columns)
   }
-  if (dataSet.length < 2) errorArr.push(addBatchFileErrorMessages.hasOneRow)
-  if (dataSet.length >= 300) errorArr.push(addBatchFileErrorMessages.exceedsAllowedRows)
+  const dataSetCount = countNonEmptyRows(dataSet)
+  if (dataSetCount < 2) errorArr.push(addBatchFileErrorMessages.hasOneRow)
+  if (dataSetCount >= 300) errorArr.push(addBatchFileErrorMessages.exceedsAllowedRows)
   if (errorArr.length > 0) {
     validationError.addError(element, errorArr)
   }
   return validationError
+}
+
+/**
+ * Count non-empty lines in the data
+ * NB Excel may add several empty lines to CSVs.  This count will ignore them.
+ * @param {Array} dataSet Array of Arrays
+ * @return {number}
+ */
+function countNonEmptyRows (dataSet) {
+  if (!Array.isArray(dataSet)) {
+    throw new Error('dataSet is not an Array')
+  }
+  return R.reduce((accumulator, data) => {
+    if (arrayIsEmpty(data)) {
+      // Array with nothing in the elements; we expect strings here
+      // ['', '', '', '', '', '']
+      return accumulator
+    }
+
+    return accumulator + 1
+  }, 0, dataSet)
+}
+
+/**
+ * Return true if the array is comprised only of empty elements, false otherwise
+ * @param {Array} data
+ * @return Boolean
+ */
+function arrayIsEmpty (data) {
+  return R.all(R.isEmpty)(data)
 }
