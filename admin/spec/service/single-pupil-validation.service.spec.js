6ba04a2836f847a4db790c23bcf8557ead67ea9d
Jon Shipley <js@ovix.co.uk>
2018-05-17 08:29:03 +0100

    Fix unit tests

--- admin/spec/service/single-pupil-validation.service.spec.js
+++ admin/spec/service/single-pupil-validation.service.spec.js
@@ -1,10 +1,10 @@
 'use strict'
 /* global describe, beforeEach, afterEach, it, expect */
 
-const proxyquire = require('proxyquire').noCallThru()
 const sinon = require('sinon')
 const singlePupilValidationCSVService = require('../../services/single-pupil-validation.service')
 const PupilValidator = require('../../lib/validator/pupil-validator')
+const ValidationError = require('../../lib/validation-error')
 
 describe('single-pupil-validation.service', () => {
   let sandbox
@@ -17,39 +17,36 @@ describe('single-pupil-validation.service', () => {
 
   describe('generate with valid arguments', () => {
     beforeEach(() => {
-      sandbox.mock(PupilValidator).expects('validate').resolves({ hasError: () => false })
-      proxyquire('../../services/generate-error-csv.service', {
-        '../../lib/validator/pupil-validator': PupilValidator
-      })
+      // An empty validation error - indicating no errors at all
+      sandbox.mock(PupilValidator).expects('validate').resolves(new ValidationError())
     })
 
     it('returns a pupil with no errors', async (done) => {
       const school = { _id: '001' }
       const data = [ 'John', 'Lawrence', 'Smith', 'X822200014001', '5/22/2005', 'M' ]
+      singlePupilValidationCSVService.init()
       const { single } = await singlePupilValidationCSVService.validate(data, school)
       expect(single).toBeDefined()
       expect(single[6]).toBeUndefined()
       done()
     })
   })
+
   describe('generate with invalid arguments', () => {
-    const validationError = {
-      errors: { upn: 'UPN invalid (character 13 not a recognised value)',
-        'dob-year': 'Date of birth can\'t be in the future',
-        'dob-day': 'Date of birth can\'t be in the future',
-        'dob-month': 'Date of birth can\'t be in the future'
-      }
-    }
+    const validationError = new ValidationError()
+      .addError('upn', 'UPN invalid (character 13 not a recognised value)')
+      .addError('dob-year', 'Date of birth can\'t be in the future')
+      .addError('dob-day', 'Date of birth can\'t be in the future')
+      .addError('dob-month', 'Date of birth can\'t be in the future')
+
     beforeEach(() => {
-      sandbox.mock(PupilValidator).expects('validate').resolves({ hasError: () => true, ...validationError })
-      proxyquire('../../services/generate-error-csv.service', {
-        '../../lib/validator/pupil-validator': PupilValidator
-      })
+      sandbox.mock(PupilValidator).expects('validate').resolves(validationError)
     })
 
     it('returns a pupil with errors', async (done) => {
       const school = { _id: '001' }
       const data = [ 'John', 'Lawrence', 'Smith', 'X8222000140011', '5/22/2005', 'M' ]
+      singlePupilValidationCSVService.init()
       const { single } = await singlePupilValidationCSVService.validate(data, school)
       expect(single).toBeDefined()
       expect(single[6]).toBeDefined()
