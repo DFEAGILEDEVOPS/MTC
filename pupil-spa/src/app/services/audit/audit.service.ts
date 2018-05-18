42c56bf41744c1d6f9fa4b0952536a74af6910f8
Jon Shipley <jon-shipley@users.noreply.github.com>
2018-05-18 14:26:02 +0100

    Feature/20540 prevent duplicate answers (#662)
    * Update type declaration to be the base class
    
    * Prevent  duplicate answers for questions
    
    * Change to add additional clarity

--- pupil-spa/src/app/services/audit/audit.service.ts
+++ pupil-spa/src/app/services/audit/audit.service.ts
@@ -1,23 +1,14 @@
 import { Injectable } from '@angular/core';
 import { StorageService } from '../storage/storage.service';
 import * as AuditTypes from './auditEntry';
+import { AuditEntry } from './auditEntry';
 
 @Injectable()
 export class AuditService {
 
   constructor(private storageService: StorageService) { }
 
-  addEntry(auditEntry: AuditTypes.WarmupIntroRendered |
-    AuditTypes.CheckStarted |
-    AuditTypes.QuestionAnswered |
-    AuditTypes.QuestionRendered |
-    AuditTypes.QuestionTimerStarted |
-    AuditTypes.QuestionTimerEnded |
-    AuditTypes.QuestionTimerCancelled |
-    AuditTypes.PauseRendered |
-    AuditTypes.RefreshDetected |
-    AuditTypes.UtteranceStarted |
-    AuditTypes.UtteranceEnded): void {
+  addEntry(auditEntry: AuditEntry): void {
     let existingEntries = this.storageService.getItem('audit') as Array<AuditTypes.AuditEntry>;
     if (!existingEntries) {
       existingEntries = new Array<AuditTypes.AuditEntry>();
