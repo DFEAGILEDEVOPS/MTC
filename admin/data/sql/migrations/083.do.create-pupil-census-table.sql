56303e793204e23ee7a95b73eac94e60c12e1443
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-05-21 17:45:06 +0100

    Add new index and fields for monitoring validation, completion and error message

--- admin/data/sql/migrations/083.do.create-pupil-census-table.sql
+++ admin/data/sql/migrations/083.do.create-pupil-census-table.sql
@@ -1,10 +1,13 @@
 CREATE TABLE [mtc_admin].[pupilCensus] (
   id        INT IDENTITY (1, 1) NOT NULL,
-  name      [nvarchar](300) NOT NULL,
+  name      [nvarchar](MAX) NOT NULL,
   blobFileName [nvarchar](300) NOT NULL,
+  createdAt DATETIMEOFFSET(3)   NOT NULL DEFAULT GETUTCDATE(),
+  updatedAt DATETIMEOFFSET(3)   NOT NULL DEFAULT GETUTCDATE(),
   status [nvarchar](50) NOT NULL,
   isDeleted [BIT] NOT NULL DEFAULT 0,
-  createdAt DATETIMEOFFSET(3)   NOT NULL DEFAULT GETUTCDATE(),
-  updatedAt DATETIMEOFFSET(3)   NOT NULL DEFAULT GETUTCDATE()
+  isValidationSuccessful [BIT],
+  isCompleted [BIT],
+  errorMessage [nvarchar](MAX)
 );
 CREATE UNIQUE INDEX pupilCensus_blobFileName_uindex ON [mtc_admin].[pupilCensus] ([blobFileName]);
