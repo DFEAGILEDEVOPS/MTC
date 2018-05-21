06dd1066a8ed39703cb652c17fba287ad9c5bd81
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-05-21 15:44:16 +0100

    delete unneeded pupil fields

--- admin/data/sql/migrations/084.do.add-pupil-census-fields-to-pupil.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE mtc_admin.[pupil] ADD ncYearActual NVARCHAR(30) NULL;
-ALTER TABLE mtc_admin.[pupil] ADD enrolStatus NVARCHAR(30) NULL;
-ALTER TABLE mtc_admin.[pupil] ADD fsMeligible NVARCHAR(30) NULL;
-ALTER TABLE mtc_admin.[pupil] ADD [language] NVARCHAR(30) NULL;
-ALTER TABLE mtc_admin.[pupil] ADD senProvision NVARCHAR(30) NULL;
--- admin/data/sql/migrations/084.undo.add-pupil-census-fields-to-pupil.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE [mtc_admin].[pupil] DROP COLUMN ncYearActual;
-ALTER TABLE [mtc_admin].[pupil] DROP COLUMN enrolStatus;
-ALTER TABLE [mtc_admin].[pupil] DROP COLUMN fsMeligible;
-ALTER TABLE [mtc_admin].[pupil] DROP COLUMN [language];
-ALTER TABLE [mtc_admin].[pupil] DROP COLUMN senProvision;
