263ff989332c162de7a8586106567e85d15a974a
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-05-21 16:20:34 +0100

    Adding unique index on the blobFileName field

--- admin/data/sql/migrations/083.do.create-pupil-census-table.sql
+++ admin/data/sql/migrations/083.do.create-pupil-census-table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE [mtc_admin].[pupilCensus] (
   id        INT IDENTITY (1, 1) NOT NULL,
-  name      [nvarchar](MAX) NOT NULL,
-  blobFileName [nvarchar](MAX) NOT NULL,
+  name      [nvarchar](300) NOT NULL,
+  blobFileName [nvarchar](300) NOT NULL,
   status [nvarchar](50) NOT NULL,
   isDeleted [BIT] NOT NULL DEFAULT 0,
   createdAt DATETIMEOFFSET(3)   NOT NULL DEFAULT GETUTCDATE(),
   updatedAt DATETIMEOFFSET(3)   NOT NULL DEFAULT GETUTCDATE()
 );
-EXEC [mtc_admin].[spGenAuditTriggers]
+CREATE UNIQUE INDEX pupilCensus_blobFileName_uindex ON [mtc_admin].[pupilCensus] ([blobFileName]);
