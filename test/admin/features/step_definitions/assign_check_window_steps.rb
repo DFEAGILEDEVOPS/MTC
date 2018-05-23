f97e50d6031bd2565a4c8d789cb4a8bc93dd80d7
activemq <m-qureshi@hotmail.com>
2018-05-23 12:28:56 +0100

    adding test

--- test/admin/features/step_definitions/assign_check_window_steps.rb
+++ test/admin/features/step_definitions/assign_check_window_steps.rb
@@ -13,3 +13,13 @@ end
 Then(/^I should see check windows$/) do
   expect(assign_form_to_window_page.check_windows.rows.first).to have_name_of_window
 end
+
+And(/^the check window has started$/) do
+  SqlDbHelper.update_check_window_start_date( @check_window_hash[:check_name],'dsd')
+end
+
+Then(/^I should not be able to remove the check from the window$/) do
+  window = assign_form_to_window_page.check_windows.rows.select {|window| window.name_of_window.text.include? @check_window_hash[:check_name]}
+  window.first.name_of_window.click
+  unassign_form_page.check_forms.rows.each{|row| expect(row).to have_no_remove_from_window}
+end
