4db820f9b9d80f2b6a6fd676b9ae5dacbe4a73bd
Guy Harwood <guy@milkchip.com>
2018-05-25 12:14:28 +0100

    lookup user by unique id

--- admin/services/nca-tools-user.service.js
+++ admin/services/nca-tools-user.service.js
@@ -23,7 +23,7 @@ const service = {
       }
     }
 
-    let userRecord = await userDataService.sqlFindOneByIdentifier(ncaUser.EmailAddress)
+    let userRecord = await userDataService.sqlFindOneByIdentifier(ncaUser.externalAuthenticationId)
     if (!userRecord) {
       const mtcRoleName = roleService.mapNcaRoleToMtcRole(ncaUser.UserType, school)
       const role = await roleService.findByTitle(mtcRoleName)
