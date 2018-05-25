37a14a66a6f4aff7c8ce5789c8dc3f835c31fe88
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-05-25 16:42:02 +0100

    change to admin user config

--- spike-worker/config.js
+++ spike-worker/config.js
@@ -3,18 +3,18 @@
 require('dotenv').config()
 const toBool = require('to-bool')
 
-const oneMinuteInMilliseconds = 60000
+const twoMinutesInMilliseconds = 120000
 
 module.exports = {
   Sql: {
     Database: process.env.SQL_DATABASE || 'mtc',
     Server: process.env.SQL_SERVER || 'localhost',
     Port: process.env.SQL_PORT || 1433,
-    Timeout: process.env.SQL_TIMEOUT || oneMinuteInMilliseconds,
+    Timeout: process.env.SQL_MIGRATION_TIMEOUT || twoMinutesInMilliseconds,
     Encrypt: process.env.hasOwnProperty('SQL_ENCRYPT') ? toBool(process.env.SQL_ENCRYPT) : true,
     Name: process.env.SQL_APP_NAME || 'mtc-local-dev', // docker default
-    Username: process.env.SQL_APP_USER || 'mtcAdminUser', // docker default
-    Password: process.env.SQL_APP_USER_PASSWORD || 'your-chosen*P4ssw0rd_for_dev_env!' // docker default
+    Username: process.env.SQL_ADMIN_USER || 'sa', // docker default
+    Password: process.env.SQL_ADMIN_USER_PASSWORD || 'Mtc-D3v.5ql_S3rv3r' // docker default
   }
 
 }
