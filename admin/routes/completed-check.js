898c79232a364805637208161852b682b5e08cd4
Guy Harwood <GuyHarwood@users.noreply.github.com>
2018-06-12 15:06:43 +0100

    feature/21581 Implement restrictive CORS policy for endpoints only (#690)
    * remove global CORS policy and implement individual policies on API endpoints with config
    
    * remove commented code
    
    * fixed test after space added

--- admin/routes/completed-check.js
+++ admin/routes/completed-check.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const { postCheck } = require('../controllers/completed-check')
+const cors = require('cors')
+const corsOptions = require('../helpers/cors-options')
 
-router.route('/').all((req, res) => {
+router.route('/').all(cors(corsOptions), (req, res) => {
   if (req.method !== 'POST') return res.sendStatus(405)
   postCheck(req, res)
 })
