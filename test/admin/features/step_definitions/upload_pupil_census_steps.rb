ff82c375fc7197d6b2aa8836c3176f6bb73b2a3c
activemq <m-qureshi@hotmail.com>
2018-06-12 16:17:52 +0100

    adding new tests for delete

--- test/admin/features/step_definitions/upload_pupil_census_steps.rb
+++ test/admin/features/step_definitions/upload_pupil_census_steps.rb
@@ -40,3 +40,34 @@ Then(/^I should see the file uploaded$/) do
   jobs = SqlDbHelper.get_jobs
   expect(jobs.last['jobInput']).to include @file_name.split('.').first
 end
+
+Given(/^I have uploaded a pupil census file$/) do
+  step 'I am on the upload pupil census page'
+  step 'I have chosen a file to submit'
+  step 'I should see the file uploaded'
+end
+
+When(/^I decide to remove the file$/) do
+  upload_pupil_census_page.uploaded_file.remove.click
+  upload_pupil_census_page.removal_modal.yes.click
+end
+
+Then(/^it should be removed and the status updated$/) do
+  expect(upload_pupil_census_page).to have_removal_message
+  expect(upload_pupil_census_page.uploaded_file.status.text).to eql 'Removed'
+end
+
+Given(/^the pupil census file has been removed$/) do
+  step 'I am on the upload pupil census page'
+  step 'I have chosen a file to submit'
+  step 'I should see the file uploaded'
+  step 'I decide to remove the file'
+end
+
+Then(/^the pupils should be removed from the register$/) do
+  visit Capybara.app_host + '/sign-out'
+  step 'I am logged in'
+  pupil_register_page.load
+  names = pupil_register_page.pupil_list.pupil_row.map {|row| row.names}
+  expect(names).to_not include 'Surname9991001,Mohsen'
+end
--- test/admin/features/support/page_objects/upload_pupil_census_page.rb
+++ test/admin/features/support/page_objects/upload_pupil_census_page.rb
@@ -7,9 +7,16 @@ class UploadPupilCensusPage < SitePrism::Page
   element :cancel, 'a[href="/service-manager/home"]'
   element :uploaded_title, '.bold-small'
   section :uploaded_file, 'dl.pupil-census-uploaded-file' do
-    element :file, 'dd:first-of-type'
-    element :status, 'dd:last-of-type'
+    element :file, 'dd:nth-of-type(1)'
+    element :status, 'dd:nth-of-type(2)'
+    element :remove, 'dd:nth-of-type(3) a'
   end
+  section :removal_modal, '.modal-buttons' do
+    element :yes, '.modal-confirm'
+    element :no, '.modal-cancel'
+  end
+  element :removal_message, '.info-message', text: 'Pupil data successfully removed'
+
 
   def create_unique_check_csv(file_name, file_contents)
     out_file = File.new(file_name, "w")
