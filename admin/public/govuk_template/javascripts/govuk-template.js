d8c9eb0a62c3be376de81415fdafc6cb073d5357
activemq <m-qureshi@hotmail.com>
2018-06-21 15:08:06 +0100

    update

--- admin/public/govuk_template/javascripts/govuk-template.js
+++ admin/public/govuk_template/javascripts/govuk-template.js
@@ -70,7 +70,7 @@
 
     if (hasCookieMessage) {
       message.style.display = 'block';
-      GOVUK.cookie('seen_cookie_message', 'yes', { days: 28 * 3 });
+      GOVUK.cookie('seen_cookie_message', 'yes', { days: 28 });
     }
   };
 }).call(this);
