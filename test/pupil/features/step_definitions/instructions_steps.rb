7531aab9f2bad55415beb2f344ac648479a80b8e
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-06-21 14:21:50 +0100

    Feature/18999 Start of check updates (#700)
    * text and component changes
    
    * practice instructions component
    
    * Additional text changes
    
    * Remove rudundant component and correct text
    
    * login error box on unsuccessful login attempt
    
    * Add back submitted logic
    
    * Remove login failure component as on screen error appears on login component
    
    * Remove failure component from module list
    
    * change logic with loginSucceeded variable
    
    * Include questions intro component right after warm up complete screen
    
    * remove redundant commented code
    
    * updates to tests
    
    * fix issue

--- test/pupil/features/step_definitions/instructions_steps.rb
+++ test/pupil/features/step_definitions/instructions_steps.rb
@@ -13,11 +13,11 @@ end
 Then(/^I should see the timings between questions$/) do
   array_of_instructions = start_page.bulleted_list_instructions.map{|instruction| instruction.text}
   timings = JSON.parse page.evaluate_script('window.localStorage.getItem("config");')
-  expect(array_of_instructions).to include "There will be 10 questions."
   expect(array_of_instructions).to include "Each question will show for #{timings['questionTime']} seconds."
 end
 
 Then(/^I should see the total number of questions in the check$/) do
   questions = JSON.parse page.evaluate_script('window.localStorage.getItem("questions");')
-  expect(start_page.number_of_questions.text.scan(/\d+/).first.to_i).to eql questions.count
+  array_of_instructions = start_page.bulleted_list_instructions.map{|instruction| instruction.text}
+  expect(array_of_instructions).to include "Then there will be #{questions.size} questions."
 end
--- test/pupil/features/step_definitions/state_steps.rb
+++ test/pupil/features/step_definitions/state_steps.rb
@@ -49,6 +49,7 @@ Given(/^I am on question (.*) of the check$/) do |number|
   @question_strings = create_question_strings(JSON.parse(page.evaluate_script('window.localStorage.getItem("questions");')))
   step "I complete the warm up questions using the numpad"
   warm_up_complete_page.start_check.click
+  mtc_check_start_page.start_now.click
   check_page.wait_for_question
   check_page.wait_for_answer
   check_page.complete_check_with_correct_answers((@number-1), 'numpad') until check_page.question.text == @question_strings[@number -1]
@@ -66,7 +67,7 @@ But(/^the next question has loaded so I continue with the check$/) do
 end
 
 Then(/^the audit and inputs recorded should reflect this$/) do
-  refresh_audit_index = 24
+  refresh_audit_index = 25
   question_index = @number - 1
   audit_location_index = (question_index * 5) + refresh_audit_index
   expect(JSON.parse(page.evaluate_script('window.localStorage.getItem("audit");'))[audit_location_index]['type']).to eql 'RefreshDetected'
