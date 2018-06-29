df220704cda36f78a5ad8ee80eef6d108d4a9e16
Guy Harwood <guy@milkchip.com>
2018-06-29 15:20:37 +0100

    strip out surplus code and refine message push

--- spike-storage-queue/queue-util.js
+++ spike-storage-queue/queue-util.js
@@ -7,15 +7,22 @@ var encoder = new AzureStorage.Queue.QueueMessageEncoder.TextBase64QueueMessageE
 function checkParameters() {
     account = document.getElementById('account').value;
     sas = document.getElementById('sas').value;
+    queue = document.getElementById('queuename')
 
     if (account == null || account.length < 1)
     {
-        alert('Please enter a valid storage account name!');
+        alert('Please enter a valid storage account name');
         return false;
     }
     if (sas == null || sas.length < 1)
     {
-        alert('Please enter a valid SAS Token!');
+        alert('Please enter a valid SAS Token');
+        return false;
+    }
+
+    if (queue == null || queue.length < 1)
+    {
+        alert('Please enter a valid queue name');
         return false;
     }
 
@@ -26,125 +33,9 @@ function getQueueService() {
     if (!checkParameters())
         return null;
 
-    queueUri = 'https://' + account + '.queue.core.windows.net';
+    queueUri = 'https://' + account + '.queue.core.windows.net/' + queue;
     var queueService = AzureStorage.Queue.createQueueServiceWithSas(queueUri, sas).withFilter(new AzureStorage.Queue.ExponentialRetryPolicyFilter());
     return queueService;
-}
-
-function refreshQueueList()
-{
-    var queueService = getQueueService();
-    if (!queueService)
-        return;
-
-    document.getElementById('queues').innerHTML = 'Loading queue list...';
-    queueService.listQueuesSegmented(null, {maxResults : 200}, function(error, results) {
-        if (error) {
-            alert('List queue list error, please open browser console to view detailed error');
-            console.log(error);
-        } else {
-            var output = [];
-            output.push('<tr>',
-                            '<th>QueueName</th>',
-                            '<th>Operations</th>',
-                        '</tr>');
-
-            if (results.entries.length < 1) {
-                output.push('<tr><td>Empty results...</td></tr>');
-            }
-
-            for (var i = 0, queue; queue = results.entries[i]; i++) {
-                output.push('<tr>',
-                                '<td>', queue.name, '</td>',
-                                '<td>', '<button class="btn btn-xs btn-danger" onclick="deleteQueue(\'', queue.name ,'\')">Delete</button> ',
-                                        '<button class="btn btn-xs btn-success" onclick="viewQueue(\'', queue.name ,'\')">Select</button>', '</td>',
-                            '</tr>');
-            }
-            document.getElementById('queues').innerHTML = '<table class="table table-condensed table-bordered">' + output.join('') + '</table>';
-        }
-    });
-}
-
-function createQueue() {
-    var queueService = getQueueService();
-    if (!queueService)
-        return;
-
-    var queue = document.getElementById('newqueue').value;
-    if (!AzureStorage.Queue.Validate.queueNameIsValid(queue, function(err, res){})) {
-        alert('Invalid queue name!');
-        return;
-    }
-    queueService.createQueueIfNotExists(queue.toLowerCase(), function(error, result) {
-        if (error) {
-            alert('Create queue failed, open brower console for more detailed info.');
-            console.log(error);
-        } else {
-            alert('Create ' + queue + ' successfully!');
-            refreshQueueList();
-        }
-    });
-}
-
-function deleteQueue(queue) {
-    var queueService = getQueueService();
-    if (!queueService)
-        return;
-
-    queueService.deleteQueueIfExists(queue, function(error, result) {
-        if (error) {
-            alert('Delete queue failed, open brower console for more detailed info.');
-            console.log(error);
-        } else {
-            alert('Delete ' + queue + ' successfully!');
-            refreshQueueList();
-        }
-    });
-}
-
-function viewQueue(selectedQueue) {
-    queue = selectedQueue;
-    alert('Selected ' + queue + ' !');
-    refreshMessageList();
-}
-
-function refreshMessageList() {
-    var queueService = getQueueService();
-    if (!queueService)
-        return;
-    
-    if (queue == null || queue.length < 1) {
-        alert('Please select a queue from queue list!')
-        return;
-    }
-
-    document.getElementById('result').innerHTML = 'Loading queue messages...';
-    queueService.peekMessages(queue, {numOfMessages: 32}, function (error, results) {
-        if (error) {
-            alert('List queue messages error, please open browser console to view detailed error');
-            console.log(error);
-        } else {
-            var output = [];
-            output.push('<tr>',
-                            '<th>MessageId</th>',
-                            '<th>MessageContent</th>',
-                            '<th>DequeueCount</th>',
-                            '<th>InsertionTime</th>',
-                        '</tr>');
-            if (results.length < 1) {
-                output.push('<tr><td>Empty results...</td></tr>');
-            }
-            for (var i = 0, message; message = results[i]; i++) {
-                output.push('<tr>',
-                                '<td>', message.messageId, '</td>',
-                                '<td>', encoder.decode(message.messageText), '</td>',
-                                '<td>', message.dequeueCount, '</td>',
-                                '<td>', message.insertionTime, '</td>',
-                            '</tr>');
-            }
-            document.getElementById('result').innerHTML = '<table class="table table-condensed table-bordered">' + output.join('') + '</table>';
-        }
-    });
 }
 
 function addMessage() {
@@ -158,13 +49,13 @@ function addMessage() {
     }
 
     var message = document.getElementById('newmessage').value;
+    var messageDisplay = document.getElementById('ui-messages')
     queueService.createMessage(queue, encoder.encode(message), function(error, result, response) {
         if(error) {
-            alert('Create messages error, please open browser console to view detailed error');
+            messageDisplay.innerText = 'error creating message:' + result;
             console.log(error);
         } else {
-            alert('Create message successfully!');
-            refreshMessageList();
+            messageDisplay.innerText = 'Create message successfully!';
         }
     });
 }
--- spike-storage-queue/readme.md
+++ spike-storage-queue/readme.md
@@ -1,4 +1,9 @@
 # Storage Queue from Browser via SAS auth
 
-Taken from https://dmrelease.blob.core.windows.net/azurestoragejssample/samples/sample-queue.html
+Example code taken from [here](https://dmrelease.blob.core.windows.net/azurestoragejssample/samples/sample-queue.html)
+
+1. Install httpserver - `npm install -g httpserver`
+2. run httpserver from this directory
+
+
 
