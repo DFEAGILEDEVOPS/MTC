b46966634950eb1b6e4ded63b38edd0acffe1f87
Guy Harwood <GuyHarwood@users.noreply.github.com>
2018-06-29 15:58:09 +0100

    set flag to treat errors as exceptions (#740)
    * set flag to treat errors as exceptions
    
    * introduce explicit log level setting
    
    * Additional logging

--- admin/config.js
+++ admin/config.js
@@ -58,6 +58,7 @@ module.exports = {
     }
   },
   Logging: {
+    LogLevel: process.env.LOG_LEVEL || 'debug',
     LogDna: {
       key: process.env.LOGDNA_API_KEY,
       hostname: `${os.hostname()}:${process.pid}`,
