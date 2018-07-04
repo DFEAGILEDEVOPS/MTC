45d63a2e2302f38ad1d9aaf4ed6dab14a42f3411
Dan Williams <dan@ithium.net>
2018-07-04 18:30:14 +0100

    Removed the now unused variables in the environment files

--- pupil-spa/src/environments/environment.prod.ts
+++ pupil-spa/src/environments/environment.prod.ts
@@ -1,22 +1,3 @@
 export const environment = {
-  apiURL: 'API_URL',
-  checkStartedURL: 'CHECK_STARTED_URL',
-  authURL: 'AUTH_URL',
-  checkSubmissionURL: 'CHECK_SUBMISSION_URL',
-  production: true,
-  // api/check-started
-  // Delay (ms) during retries
-  checkStartAPIErrorDelay: 2000,
-  // Max attempts
-  checkStartAPIErrorMaxAttempts: 3,
-  // api/completed-check
-  // Delay (ms) during retries
-  checkSubmissionApiErrorDelay: 30000,
-  // Max attempts
-  checkSubmissionAPIErrorMaxAttempts: 10,
-  // Minimum display time for submission pending view
-  submissionPendingViewMinDisplay: 6000,
-  // Contact number in case check submission fails
-  supportNumber: '0345 278 8080',
-  googleAnalyticsTrackingCode: 'GA_CODE'
+  production: true
 };
--- pupil-spa/src/environments/environment.ts
+++ pupil-spa/src/environments/environment.ts
@@ -2,27 +2,6 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-const ip = 'localhost';
-
 export const environment = {
-  apiURL: `http://${ip}:3001`,
-  authURL: `http://${ip}:3001/api/questions`,
-  checkStartedURL: `http://${ip}:3001/api/check-started`,
-  checkSubmissionURL: `http://${ip}:3001/api/completed-check`,
-  production: false,
-  // api/check-started
-  // Delay (ms) during retries
-  checkStartAPIErrorDelay: 2000,
-  // Max attempts
-  checkStartAPIErrorMaxAttempts: 3,
-  // api/completed-check
-  // Delay (ms) during retries
-  checkSubmissionApiErrorDelay: 30000,
-  // Max attempts
-  checkSubmissionAPIErrorMaxAttempts: 10,
-  // Minimum display time for submission pending view
-  submissionPendingViewMinDisplay: 6000,
-  // Contact number in case check submission fails
-  supportNumber: '0345 278 8080',
-  googleAnalyticsTrackingCode: null
+  production: false
 };
