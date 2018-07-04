a00eaaf83c33d41560f1b5b4b1bd7cf328c99e2d
Dan Williams <dan@ithium.net>
2018-07-04 18:41:27 +0100

    Made the runtime config URL configurable, can be loaded externally

--- pupil-spa/gulpfile.js
+++ pupil-spa/gulpfile.js
@@ -1,49 +1,12 @@
 const gulp = require('gulp')
 const replace = require('gulp-string-replace')
 
-gulp.task('setApiURL', () => {
-  console.log('Attempting to update angular config from environment variables...')
-  console.log('env.API_URL is:', process.env.API_URL)
-  if (process.env.API_URL) {
-    gulp.src(['./src/environments/environment.prod.ts'])
-      .pipe(replace('API_URL', process.env.API_URL))
-      .pipe(gulp.dest('./src/environments/'))
+gulp.task('setRuntimeConfigURL', () => {
+  console.log('Attempting to update runtime config URL from the environment...')
+  console.log('env.RUNTIME_CONFIG_URL is:', process.env.RUNTIME_CONFIG_URL)
+  if (process.env.RUNTIME_CONFIG_URL) {
+    gulp.src(['./src/app/services/config/config.service.ts'])
+      .pipe(replace('/public/config.json', process.env.RUNTIME_CONFIG_URL))
+      .pipe(gulp.dest('./src/app/services/config/'))
   }
-})
-
-gulp.task('setAuthURL', () => {
-  console.log('Attempting to update angular config from environment variables...')
-  console.log('env.AUTH_URL is:', process.env.AUTH_URL)
-  if (process.env.AUTH_URL) {
-    gulp.src(['./src/environments/environment.prod.ts'])
-      .pipe(replace('AUTH_URL', process.env.AUTH_URL))
-      .pipe(gulp.dest('./src/environments/'))
-  }
-})
-
-gulp.task('setCheckStartedURL', () => {
-  console.log('Attempting to update angular config from environment variables...')
-  console.log('env.CHECK_STARTED_URL is:', process.env.CHECK_STARTED_URL)
-  if (process.env.CHECK_STARTED_URL) {
-    gulp.src(['./src/environments/environment.prod.ts'])
-      .pipe(replace('CHECK_STARTED_URL', process.env.CHECK_STARTED_URL))
-      .pipe(gulp.dest('./src/environments/'))
-  }
-})
-
-gulp.task('setCheckSubmissionURL', () => {
-  console.log('Attempting to update angular config from environment variables...')
-  console.log('env.CHECK_SUBMISSION_URL is:', process.env.CHECK_SUBMISSION_URL)
-  if (process.env.CHECK_SUBMISSION_URL) {
-    gulp.src(['./src/environments/environment.prod.ts'])
-      .pipe(replace('CHECK_SUBMISSION_URL', process.env.CHECK_SUBMISSION_URL))
-      .pipe(gulp.dest('./src/environments/'))
-  }
-})
-
-gulp.task('setGaCode', () => {
-  console.log('env.GA_CODE is:', process.env.GA_CODE)
-  gulp.src(['./src/environments/environment.prod.ts'])
-    .pipe(replace('GA_CODE', process.env.GA_CODE || ''))
-    .pipe(gulp.dest('./src/environments/'))
 })
