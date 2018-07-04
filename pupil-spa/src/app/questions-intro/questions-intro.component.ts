493f68f17bb2952e04199dd5039999c09dfee609
Dan Williams <dan@ithium.net>
2018-07-04 11:56:01 +0100

    Updated questions-intro page to set active state on button after speech

--- pupil-spa/src/app/questions-intro/questions-intro.component.ts
+++ pupil-spa/src/app/questions-intro/questions-intro.component.ts
@@ -44,11 +44,14 @@ export class QuestionsIntroComponent implements OnInit, AfterViewInit, OnDestroy
     this.auditService.addEntry(new QuestionIntroRendered());
 
     if (this.questionService.getConfig().speechSynthesis) {
-      this.speechService.speakElement(this.elRef.nativeElement);
+      this.speechService.speakElement(this.elRef.nativeElement).then(() => {
+        this.speechService.focusEndOfSpeech(this.elRef.nativeElement.querySelector('#start-now-button'));
+      });
 
-      this.speechListenerEvent = this.elRef.nativeElement.addEventListener('focus', (event) => {
-        this.speechService.speakFocusedElement(event.target);
-      }, true);
+      this.speechListenerEvent = this.elRef.nativeElement.addEventListener('focus',
+        (event) => { this.speechService.focusEventListenerHook(event); },
+        true
+      );
     }
   }
 
