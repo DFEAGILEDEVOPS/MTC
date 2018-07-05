3c865c8cf17b59951606fa40efca1b4ebd112698
Amit Sinha <amit.kumar.sinha@businessthreezero.com>
2018-07-05 16:01:32 +0100

    removed commneted out line

--- test/admin/features/step_definitions/add_multiple_pupil_steps.rb
+++ test/admin/features/step_definitions/add_multiple_pupil_steps.rb
@@ -18,27 +18,6 @@ Then(/^I can see the landing page as per the design$/) do
   expect(@page).to have_chose_file
 end
 
-# Then(/^I should see a heading for the Add Multiple pupils$/) do
-#   expect(add_multiple_pupil_page).to have_heading
-# end
-
-# And(/^I can see sub heading '(.*)' on Add Multiple Pupil Page$/) do|sub_heading|
-#   expect(@page.sub_heading.map {|message| message.text}).to include "#{sub_heading}"
-# end
-
-# And(/^I can see the Info message on adding Multiple Pupils$/) do
-#   expect(@page.info_message.map {|message| message.text}).to include "Download the template Pupil details template (CSV file)"
-#   expect(@page.info_message.map {|message| message.text}).to include "Open file in a programme like Excel, OpenOffice or Google Sheets"
-#   expect(@page.info_message.map {|message| message.text}).to include "Add pupil details using the given format"
-#   expect(@page.info_message.map {|message| message.text}).to include "Save as a CSV file on your computer"
-#   expect(@page.info_message.map {|message| message.text}).to include "Select 'Choose file' button and navigate to the file on your computer"
-#   expect(@page.info_message.map {|message| message.text}).to include "Choose the file to upload"
-# end
-
-# And(/^I should have the option to choose a csv file for adding multiple pupil$/) do
-#   expect(@page).to have_chose_file
-# end
-
 When(/^I Upload a valid CSV file to add Multiple Pupil$/) do
   dobs = add_multiple_pupil_page.get_dob_for_pupil_for_multiple_upload
   @old_date1 = dobs[0]
