79b85df74e46c3dc570676586cf09b6e77ac7c64
Guy Harwood <guy@milkchip.com>
2018-07-12 16:45:09 +0100

    persist check to table storage via binding

--- _spikes-poc/sas/fn-worker/complete-checks/index.js
+++ _spikes-poc/sas/fn-worker/complete-checks/index.js
@@ -1,5 +1,17 @@
+const uuid = require('uuid/v4')
+const moment = require('moment')
+
 module.exports = function (context, check) {
-  context.log('message received')
-  context.log('check code:', check.access_token)
+  context.log('complete check found on queue:', check.access_token)
+  // perform work here, such as updating check entry in admin system
+  context.bindings.table = []
+  const entity = {
+    PartitionKey: check.pupil.checkCode,
+    RowKey: uuid(),
+    payload: JSON.stringify(check),
+    processedAt: moment().toDate(),
+    processedBy: 'azure function'
+  }
+  context.bindings.table.push(entity)
   context.done()
 }
