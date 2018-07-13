bdeeea1664a266bb8161cb47e7969d5afbe665c8
Dan Williams <dan@ithium.net>
2018-07-13 11:34:06 +0100

    Updated migration numbers and added check familiarisation column

--- admin/data/sql/migrations/106.do.add-familiarisation-pin.sql
+++ admin/data/sql/migrations/106.do.add-familiarisation-pin.sql
@@ -1,2 +1,3 @@
 ALTER TABLE [mtc_admin].[pupil] ADD familiarisation_pin NVARCHAR(12) NULL
 ALTER TABLE [mtc_admin].[pupil] ADD familiarisation_pinExpiresAt [datetimeoffset](3) NULL
+ALTER TABLE [mtc_admin].[check] ADD is_familiarisation smallint NOT NULL DEFAULT 1
--- admin/data/sql/migrations/106.undo.add-familiarisation-pin.sql
+++ admin/data/sql/migrations/106.undo.add-familiarisation-pin.sql
@@ -1,2 +1,3 @@
 ALTER TABLE [mtc_admin].[pupil] DROP COLUMN familiarisation_pin;
 ALTER TABLE [mtc_admin].[pupil] DROP COLUMN familiarisation_pinExpiresAt;
+ALTER TABLE [mtc_admin].[check] DROP COLUMN is_familiarisation;
