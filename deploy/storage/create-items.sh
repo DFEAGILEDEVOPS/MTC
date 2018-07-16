b858176f45319af334caf14c62c7902c47690522
Guy Harwood <guy@milkchip.com>
2018-07-16 16:53:27 +0100

    add key param

--- deploy/storage/create-items.sh
+++ deploy/storage/create-items.sh
@@ -5,9 +5,11 @@ set -e
 
 # input parameters
 # $1 storage account name
+# $2 storage account key
 
 # Set variables
 storageAccountName=$1
+storageAccountKey=$2
 
 declare -a queuenames=('check-started' 'completed-check' 'pupil-auth' 'pupil-feedback' 'pupil-preferences' 'prepare-check')
 # create queues if they do not exist
@@ -16,6 +18,7 @@ do
 	echo "creating $q queue in $storageAccountName"
 	az storage queue create --name $q
                         --account-name $storageAccountName 
+												--account-key $storageAccountKey
                         --fail-on-exist false
 done
 
@@ -26,5 +29,6 @@ do
 	echo "creating $q table in $storageAccountName"
 	az storage table create --name $q
                         --account-name $storageAccountName 
+												--account-key $storageAccountKey
                         --fail-on-exist false
 done
