3c24c3a27209415a624b40b9d88b404e3d70c444
Guy Harwood <guy@milkchip.com>
2018-07-16 15:49:13 +0100

    create storage items script.  delete old cosmos stuff.

---
+++ deploy/storage/create-items.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# exit on error
+set -e
+
+# input parameters
+# $1 storage account name
+
+# Set variables
+storageAccountName=$2
+
+declare -a queuenames=('check-started' 'completed-check' 'pupil-auth' 'pupil-feedback' 'pupil-preferences' 'prepare-check')
+# create queues if they do not exist
+for q in $queuenames
+do
+	echo "creating $q queue in $storageAccountName"
+	az storage queue create --name $q
+                        --account-name $storageAccountName 
+                        --fail-on-exist false
+done
+
+declare -a tablenames=('pupilEvent' 'preparedCheck')
+# create tables if they do not exist
+for q in $tablenames
+do
+	echo "creating $q table in $storageAccountName"
+	az storage table create --name $q
+                        --account-name $storageAccountName 
+                        --fail-on-exist false
+done
