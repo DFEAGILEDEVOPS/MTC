a0ba2507d18aef1cbf5b8c7ae06e2206f05a57ef
Dan Williams <dan@ithium.net>
2018-07-17 11:59:21 +0100

    Removed is_familiarisation int from the check table

--- admin/data/sql/migrations/106.undo.add-familiarisation-pin.sql
+++ admin/data/sql/migrations/106.undo.add-familiarisation-pin.sql
@@ -1,3 +1,2 @@
 ALTER TABLE [mtc_admin].[pupil] DROP COLUMN familiarisation_pin;
 ALTER TABLE [mtc_admin].[pupil] DROP COLUMN familiarisation_pinExpiresAt;
-ALTER TABLE [mtc_admin].[check] DROP COLUMN is_familiarisation;
