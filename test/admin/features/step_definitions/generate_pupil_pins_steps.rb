10fc58b6e2ae3f28bdde39b4df5e4b6993021411
Amit Sinha <amit.kumar.sinha@businessthreezero.com>
2018-07-20 15:51:44 +0100

    updated Generate pin live area test automation

--- test/admin/features/step_definitions/generate_pupil_pins_steps.rb
+++ test/admin/features/step_definitions/generate_pupil_pins_steps.rb
@@ -16,8 +16,13 @@ end
 
 And(/^I can see the info message for generating the pupil pin$/) do
   expect(generate_pins_overview_page).to have_generate_pin_message
-  expect(generate_pins_overview_page.info_message.map {|message| message.text}).to include "Generate pupil PINs and school password."
-  expect(generate_pins_overview_page.info_message.map {|message| message.text}).to include "Distribute pupil PINs and school password to your pupils."
+  expect(generate_pins_familiarisation_overview_page).to have_access_arrangment_text
+  expect(generate_pins_familiarisation_overview_page).to have_access_arrangment_link
+  expect(generate_pins_familiarisation_overview_page).to have_instruction_section
+  generate_pins_familiarisation_overview_page.instruction_section.toggle.click
+  expect(generate_pins_familiarisation_overview_page.instruction_section.info_message[0].text).to include "Press 'Generate PINs' button."
+  expect(generate_pins_familiarisation_overview_page.instruction_section.info_message[1].text).to include "Select which pupils you are generating pins for."
+  expect(generate_pins_familiarisation_overview_page.instruction_section.info_message[2].text).to include "Distribute pupil PINs and school password to your pupils."
 end
 
 And(/^I click Generate PINs button$/) do
