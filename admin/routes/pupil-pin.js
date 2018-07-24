16f86b1d569c6dba6038023a5719ecbb03309529
Dan Williams <dan@ithium.net>
2018-07-24 09:23:46 +0100

    Fixed the route to custom print page

--- admin/routes/pupil-pin.js
+++ admin/routes/pupil-pin.js
@@ -15,8 +15,8 @@ const {
 router.get('/generate-:pinEnv-pins-overview', isAuthenticated(rolesConfig.ROLE_TEACHER), (req, res, next) => getGeneratePinsOverview(req, res, next))
 router.get('/generate-:pinEnv-pins-list/:groupIds?', isAuthenticated(rolesConfig.ROLE_TEACHER), (req, res, next) => getGeneratePinsList(req, res, next))
 router.post('/generate-:pinEnv-pins', isAuthenticated(rolesConfig.ROLE_TEACHER), (req, res, next) => postGeneratePins(req, res, next))
-router.get('/view-and-print-:pinEnv-pins', isAuthenticated(rolesConfig.ROLE_TEACHER), (req, res, next) => getViewAndPrintPins(req, res, next))
 router.get('/view-and-print-custom-:pinEnv-pins', isAuthenticated(rolesConfig.ROLE_TEACHER), (req, res, next) => getViewAndPrintCustomPins(req, res, next))
+router.get('/view-and-print-:pinEnv-pins', isAuthenticated(rolesConfig.ROLE_TEACHER), (req, res, next) => getViewAndPrintPins(req, res, next))
 router.get('/print-:pinEnv-pins', isAuthenticated(rolesConfig.ROLE_TEACHER), (req, res, next) => getPrintPins(req, res, next))
 
 module.exports = router
