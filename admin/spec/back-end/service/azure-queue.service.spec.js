5f2b5afd1c60674aa671698e925ed9e18602333b
Jon Shipley <js@ovix.co.uk>
2018-07-28 07:47:46 +0100

    Add spec

---
+++ admin/spec/back-end/service/azure-queue.service.spec.js
@@ -0,0 +1,48 @@
+/* global describe it expect jasmine fail beforeEach */
+
+require('dotenv').config()
+
+const azureQueueService = require('../../../services/azure-queue.service')
+
+describe('azure-queue-service', () => {
+  describe('add-message', () => {
+    let queueServiceMock
+    const payload = {some: 'payLoad'}
+    const queueName = 'some-test-queue'
+
+    beforeEach(() => {
+      queueServiceMock = {
+        createMessage: jasmine.createSpy()
+      }
+    })
+
+    it('throws an error if the queueName param is zero length', () => {
+      try {
+        azureQueueService.addMessage('', payload, queueServiceMock)
+        fail('expected to throw')
+      } catch (error) {
+        expect(error.message).toBe('Missing queueName')
+      }
+    })
+
+    it('throws an error if the queueName param is not provided', () => {
+      try {
+        azureQueueService.addMessage(null, payload, queueServiceMock)
+        fail('expected to throw')
+      } catch (error) {
+        expect(error.message).toBe('Missing queueName')
+      }
+    })
+
+    it('stringifies the payload', () => {
+      spyOn(JSON, 'stringify')
+      azureQueueService.addMessage(queueName, payload, queueServiceMock)
+      expect(JSON.stringify).toHaveBeenCalledWith(payload)
+    })
+
+    it('injects the message into the queue by calling `createMessage`', () => {
+      azureQueueService.addMessage(queueName, payload, queueServiceMock)
+      expect(queueServiceMock.createMessage).toHaveBeenCalled()
+    })
+  })
+})
