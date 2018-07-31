0653f0162071f991eacf9b0a22cdbe2039c46c2d
Dan Williams <dan@ithium.net>
2018-07-31 11:46:22 +0100

    Added initial monitor proxy and tests

---
+++ admin/helpers/monitor.js
@@ -0,0 +1,36 @@
+const appInsights = require('applicationinsights')
+const config = require('../config')
+
+const monitor = (type, obj) => {
+  if (!config.Logging.ApplicationInsights.Key) {
+    return obj
+  }
+
+  return new Proxy(obj, {
+    get (target, propKey) {
+      const initialProp = target[propKey]
+      if (typeof initialProp !== 'function') {
+        return initialProp
+      }
+      if (initialProp.constructor.name === 'AsyncFunction') {
+        return (...args) => {
+          const startTime = Date.now()
+          return initialProp(...args).then(result => {
+            const duration = Date.now() - startTime
+            appInsights.defaultClient.trackDependency({ dependencyTypeName: type, name: propKey, duration: duration })
+            return result
+          })
+        }
+      }
+      return (...args) => {
+        const startTime = Date.now()
+        const result = initialProp(...args)
+        const duration = Date.now() - startTime
+        appInsights.defaultClient.trackDependency({ dependencyTypeName: type, name: propKey, duration: duration })
+        return result
+      }
+    }
+  })
+}
+
+module.exports = monitor
