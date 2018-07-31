acaf80a3c63343112b6002babd6818ef633976d8
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-07-31 23:11:52 +0300

    Feature/22544 access arrangements list (#768)
    * Access arrangements overview page
    
    * Update sidebar hyperlinks
    
    * file changes as per prototype
    
    * access arrangement controller spec
    
    * Access arrangements table and ui basic changes
    
    * link url fix
    
    * Updated access arrangements overview link
    
    * Updated link for access arrangements on pins overview page
    
    * UI changes
    
    * added Test for access arrangment landing page
    
    * style changes on summary description
    
    * sidebar links and text update
    
    * Ui changes as per design review and missing test coverage added
    
    * Include feature toggling on ui and router level
    
    * end of line missing
    
    * jsdoc fix
    
    * jsdoc fix
    
    * UI changes as per sign off review
    
    * access arrangment tests and restart fixes
    
    * fix codacy issue

--- test/admin/features/support/sql_db_helper.rb
+++ test/admin/features/support/sql_db_helper.rb
@@ -149,6 +149,14 @@ class SqlDbHelper
     check_window_result = result.each {|row| row.map}
   end
 
+  def self.access_arrangements
+    sql = "SELECT * FROM [mtc_admin].[accessArrangements]"
+    result = SQL_CLIENT.execute(sql)
+    access_arrangement_array = result.each {|row| row.map}
+    result.cancel
+    access_arrangement_array
+  end
+
   def self.check_form_details(check_form_name)
     sql = "SELECT * FROM [mtc_admin].[checkForm] WHERE name = '#{check_form_name}'"
     result = SQL_CLIENT.execute(sql)
