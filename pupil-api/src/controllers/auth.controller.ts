3e06cc3ce4d4aeb1ee66d551075e778d54760f04
Jon Shipley <js@ovix.co.uk>
2018-08-14 15:02:35 +0100

    Add content-type check

--- pupil-api/src/controllers/auth.controller.ts
+++ pupil-api/src/controllers/auth.controller.ts
@@ -1,9 +1,13 @@
 
 import { Request, Response } from 'express'
+import * as apiResponse from './api-response'
 
 class AuthController {
   async postAuth (req: Request, res: Response) {
-    return res.status(501).send('Not yet implemented')
+    const contentType = req.get('Content-Type')
+    if (contentType !== 'application/json') {
+      return apiResponse.badRequest(res)
+    }
   }
 }
 
