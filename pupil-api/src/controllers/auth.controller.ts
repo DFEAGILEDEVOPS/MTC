b2612a3529d09867040b616a93c968f3199b4a12
Jon Shipley <js@ovix.co.uk>
2018-08-16 08:54:15 +0100

    wip

--- pupil-api/src/controllers/auth.controller.ts
+++ pupil-api/src/controllers/auth.controller.ts
@@ -3,18 +3,21 @@ import { Request, Response } from 'express'
 import * as winston from 'winston'
 
 import * as apiResponse from './api-response'
-import * as pupilAuthenticationService from '../services/pupil-authentication.service'
+import { PupilAuthenticationService } from '../services/pupil-authentication.service'
+import * as azureStorage from 'azure-storage'
 
 class AuthController {
   async postAuth (req: Request, res: Response) {
     const contentType = req.get('Content-Type')
-    if (contentType !== 'application/json') {
+    if (contentType !== 'application/json' && contentType !== 'application/json; charset=utf-8') {
+      winston.error('Bad Request: Content type is: ' + contentType)
       return apiResponse.badRequest(res)
     }
 
     const {pupilPin, schoolPin} = req.body
 
     try {
+      const pupilAuthenticationService = new PupilAuthenticationService(azureStorage.createTableService())
       await pupilAuthenticationService.authenticate(pupilPin, schoolPin)
     } catch (error) {
       winston.error('Failed to authenticate pupil: ' + error.message)
