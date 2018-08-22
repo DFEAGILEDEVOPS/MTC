d2605bd22a9e7fbbcf44415ed5cbabba876e4d3e
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-08-22 19:48:05 +0300

    Replace double underscore

--- admin/data/sql/migrations/113.do.add-access-arrangements-id-field.sql
+++ admin/data/sql/migrations/113.do.add-access-arrangements-id-field.sql
@@ -1,5 +1,5 @@
 ALTER TABLE [mtc_admin].pupilAccessArrangements ADD accessArrangements_id int NOT NULL
 ALTER TABLE [mtc_admin].pupilAccessArrangements
-ADD CONSTRAINT FK_pupilAccessArrangements__accessArrangements_id
+ADD CONSTRAINT FK_pupilAccessArrangements_accessArrangements_id
 FOREIGN KEY (accessArrangements_id) REFERENCES [mtc_admin].accessArrangements (id)
 ALTER TABLE [mtc_admin].pupilAccessArrangements DROP COLUMN accessArrangements_ids
