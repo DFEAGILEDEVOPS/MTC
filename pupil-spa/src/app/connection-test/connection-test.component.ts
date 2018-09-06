0f3a9339d8ebf20fa01de99d24ac0f72f458ba84
Dan Williams <dan@ithium.net>
2018-09-06 19:58:21 +0100

    Displays data from window object - wip

--- pupil-spa/src/app/connection-test/connection-test.component.ts
+++ pupil-spa/src/app/connection-test/connection-test.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StorageService } from '../services/storage/storage.service';
+import { WindowRefService } from '../services/window-ref/window-ref.service';
 
 @Component({
   selector: 'app-connection-test',
@@ -6,7 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./connection-test.component.scss']
 })
 export class ConnectionTestComponent {
+  private windowObj: any;
 
-  constructor() { }
+  constructor(
+    private storage: StorageService,
+    private windowRef: WindowRefService) {
+    // reset the feedback and preview flags when starting the test
+    this.storage.setItem('feedback_given', false);
+    this.storage.setItem('preview_completed', false);
+
+    this.windowObj = windowRef.nativeWindow;
+    
+    this.getBrowserData();
+    this.getDimensions();
+    this.getOperatingSystem();
+    this.getScreenWidth();
+
+  }
+
+  getBrowserData() {
+    //console.log(this.windowObj.navigator.platform);
+    console.log(this.windowObj.navigator.userAgent);
+    //console.log(this.windowObj.navigator.appVersion);
+    //console.log(this.windowObj.navigator.vendor);
+  }
+
+  getDimensions() {
+    console.log(this.windowObj.innerWidth);
+    console.log(this.windowObj.innerHeight);
+  }
+
+  getOperatingSystem() {
+    console.log(this.windowObj.navigator.platform);
+  }
+
+  getScreenWidth() {
+    console.log(this.windowObj.screen.width);
+  }
 
 }
