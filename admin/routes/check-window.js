a79df2a0db40ca93875887e2acfb7eb89f161468
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-09-06 11:04:54 +0100

    Include feature toggle for new check window feature

--- admin/routes/check-window.js
+++ admin/routes/check-window.js
@@ -5,8 +5,11 @@ const router = express.Router()
 const isAuthenticated = require('../authentication/middleware')
 const rolesConfig = require('../roles-config')
 const checkWindowController = require('../controllers/check-window')
+const featureToggles = require('feature-toggles')
 
-/* Check Window routing */
-router.get('/manage-check-windows', isAuthenticated(rolesConfig.ROLE_SERVICE_MANAGER), (req, res, next) => checkWindowController.getManageCheckWindows(req, res, next))
+if (featureToggles.isFeatureEnabled('newCheckWindow')) {
+  /* Check Window routing */
+  router.get('/manage-check-windows', isAuthenticated(rolesConfig.ROLE_SERVICE_MANAGER), (req, res, next) => checkWindowController.getManageCheckWindows(req, res, next))
+}
 
 module.exports = router
