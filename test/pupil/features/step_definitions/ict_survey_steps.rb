b35192d2b308772b9fd3b356d261f0ed247ad76e
Amit Sinha <amit.kumar.sinha@businessthreezero.com>
2018-09-06 15:07:46 +0100

    updated test for ict survey

--- test/pupil/features/step_definitions/ict_survey_steps.rb
+++ test/pupil/features/step_definitions/ict_survey_steps.rb
@@ -12,7 +12,6 @@ Then /^ICT survey landing page is displayed as per design$/ do
   expect(ict_survey_page). to have_start_now_button
 
   ict_survey_page.more_detail.toggle.click
-  binding.pry
   expect(ict_survey_page.more_detail).to be_all_there
 
   expect(ict_survey_page). to have_footer_privacy_link
--- test/pupil/features/support/page_objects/ict_survey_page.rb
+++ test/pupil/features/support/page_objects/ict_survey_page.rb
@@ -2,7 +2,7 @@ class IctSurveyPage < SitePrism::Page
   set_url 'ict-survey/start'
 
   element :heading, '.heading-xlarge', text: "Multiplication tables check ICT survey"
-  element :purpose_message, '.heading-medium', text: "The purpose of this survey is to help us understand the information and communications technology (ICT) used in schools."
+  element :purpose_message, '.lede', text: "The purpose of this survey is to help us understand the information and communications technology (ICT) used in schools."
   element :info_message_1, '.panel-border-wide p', text: "Please complete the survey using your school ICT. If you have a range of devices, complete the survey on them one at a time."
   element :info_message_2, '.column-two-thirds p', text: "Following the connection test, please give us feedback on any ICT issues you encounter."
   element :info_message_3, '.column-two-thirds p', text: "You will be able to preview the multiplication tables check system after the connections test."
@@ -11,13 +11,13 @@ class IctSurveyPage < SitePrism::Page
   section :more_detail, 'details' do
     element :toggle, 'summary'
     element :ict_test_info, '.panel-border-narrow p', text: "This test will determine your:"
-    element :ict_test_list, '.panel-border-narrow ul li', text: 'browser settings'
-    element :ict_test_list, '.panel-border-narrow ul li', text: 'connection speed'
-    element :ict_test_list, '.panel-border-narrow ul li', text: 'device type (if available)'
-    element :ict_test_list, '.panel-border-narrow ul li', text: 'operating system'
-    element :ict_test_list, '.panel-border-narrow ul li', text: 'processing speed'
-    element :ict_test_list, '.panel-border-narrow ul li', text: 'screen width'
-    element :ict_test_info, '.panel-border-narrow p', text: "During this process, data files will be downloaded, held temporarily and then deleted. All data will be anonymised unless contact details are provided in your feedback"
+    element :ict_test_list_item1, '.panel-border-narrow ul li', text: 'browser settings'
+    element :ict_test_list_item2, '.panel-border-narrow ul li', text: 'connection speed'
+    element :ict_test_list_item3, '.panel-border-narrow ul li', text: 'device type (if available)'
+    element :ict_test_list_item4, '.panel-border-narrow ul li', text: 'operating system'
+    element :ict_test_list_item5, '.panel-border-narrow ul li', text: 'processing speed'
+    element :ict_test_list_item6, '.panel-border-narrow ul li', text: 'screen width'
+    element :ict_test_info2, '.panel-border-narrow p', text: "During this process, data files will be downloaded, held temporarily and then deleted. All data will be anonymised unless contact details are provided in your feedback"
   end
 
   element :footer_privacy_link, '.footer-wrapper .footer-meta .footer-meta-inner ul li a', text: "Privacy"
