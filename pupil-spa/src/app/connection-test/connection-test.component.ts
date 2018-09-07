301fb4dcbaacb21f4815476deb8fcd08eaf5bdf4
Dan Williams <dan@ithium.net>
2018-09-07 17:02:46 +0100

    Added  fibonacci sequence generator to test speed

--- pupil-spa/src/app/connection-test/connection-test.component.ts
+++ pupil-spa/src/app/connection-test/connection-test.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { StorageService } from '../services/storage/storage.service';
 import { WindowRefService } from '../services/window-ref/window-ref.service';
 
@@ -7,43 +7,48 @@ import { WindowRefService } from '../services/window-ref/window-ref.service';
   templateUrl: './connection-test.component.html',
   styleUrls: ['./connection-test.component.scss']
 })
-export class ConnectionTestComponent {
+export class ConnectionTestComponent implements OnInit {
   private windowObj: any;
+  private timeout:number = 3000;
 
   constructor(
     private storage: StorageService,
     private windowRef: WindowRefService) {
+
     // reset the feedback and preview flags when starting the test
     this.storage.setItem('feedback_given', false);
     this.storage.setItem('preview_completed', false);
 
     this.windowObj = windowRef.nativeWindow;
     
+    setTimeout(this.fibonacciNumbers, 3000);
+  }
+
+  ngOnInit(): void {
     this.getBrowserData();
-    this.getDimensions();
-    this.getOperatingSystem();
-    this.getScreenWidth();
-
+    setTimeout(this.fibonacciNumbers, this.timeout);
   }
 
-  getBrowserData() {
-    //console.log(this.windowObj.navigator.platform);
+  getBrowserData():void {
+    //browser
     console.log(this.windowObj.navigator.userAgent);
-    //console.log(this.windowObj.navigator.appVersion);
-    //console.log(this.windowObj.navigator.vendor);
-  }
-
-  getDimensions() {
     console.log(this.windowObj.innerWidth);
     console.log(this.windowObj.innerHeight);
-  }
-
-  getOperatingSystem() {
+    console.log(this.windowObj.screen.width);
+    //OS
     console.log(this.windowObj.navigator.platform);
   }
 
-  getScreenWidth() {
-    console.log(this.windowObj.screen.width);
-  }
+  fibonacciNumbers():void {
+    let start = Date.now();
 
+    for (let i = 0; i < 10000; i++) {
+      let arr = [0, 1];
+      for (let i = 2; i < 50000 + 1; i++){
+        arr.push(arr[i - 2] + arr[i -1])
+      }
+    }
+    
+    console.log(Date.now() - start);
+  }
 }
