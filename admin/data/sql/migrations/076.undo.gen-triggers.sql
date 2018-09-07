e257fff9fcccf2aee9ba97bf1e6d92b0916ce486
Jon Shipley <jon-shipley@users.noreply.github.com>
2018-09-07 17:34:29 +0100

    Bug/24146 migration 113 undo contains invalid sql (#816)
    * Fix undo script
    
    This script only runs fine if the DB is empty: the corresponding add script adds a not null column without specifying a default.  Also, there are no data migrations from the flat
    structure contained in the `access_arrangements_ids` column to the new multi-line structure using `access_arrangements_id`.
    
    * Fix undo script 111 which was referencing incorrect constraints
    
    * Fix undo script for migration 102
    
    * Fix migration rollback 099
    
    * Remove database specifier
    
    * Fix migration 088 rollback
    
    * Fix migration 081 rollback
    
    * Fix migration 076 rollback

--- admin/data/sql/migrations/076.undo.gen-triggers.sql
+++ admin/data/sql/migrations/076.undo.gen-triggers.sql
@@ -1,8 +1,8 @@
 DECLARE @function NVARCHAR(255)
 DECLARE @sql NVARCHAR(MAX)
-  
+
 DECLARE db_cursor CURSOR FOR
-SELECT [name] FROM sysobjects WHERE [type] = 'TR' AND [name] LIKE 'audit_'
+SELECT [name] FROM sysobjects WHERE [type] = 'TR' AND [name] LIKE 'audit_%'
 
 OPEN db_cursor
 FETCH NEXT FROM db_cursor INTO @function
@@ -11,4 +11,8 @@ WHILE @@FETCH_STATUS = 0
 BEGIN
   SELECT @sql = 'DROP TRIGGER IF EXISTS mtc_admin.' + @function
   EXEC sp_executeSql @sql
+  FETCH NEXT FROM db_cursor INTO @function
 END
+
+CLOSE db_cursor
+DEALLOCATE db_cursor
--- admin/data/sql/migrations/112.undo.create-pupil-access-arrangements.sql
+++ admin/data/sql/migrations/112.undo.create-pupil-access-arrangements.sql
@@ -1,5 +1,5 @@
 ALTER TABLE [mtc_admin].pupilAccessArrangements DROP CONSTRAINT FK_pupilAccessArrangements_questionReaderReasons_id
-ALTER TABLE [mtc_admin].pupilAccessArrangements DROP CONSTRAINT FK_pupilRestart_pupil_id
-ALTER TABLE [mtc_admin].pupilAccessArrangements DROP CONSTRAINT FK_pupilRestart_recordedBy_user_id
+ALTER TABLE [mtc_admin].pupilAccessArrangements DROP CONSTRAINT FK_pupilAccessArrangements_pupil_id
+ALTER TABLE [mtc_admin].pupilAccessArrangements DROP CONSTRAINT FK_pupilAccessArrangements_recordedBy_user_id
 
 DROP TABLE [mtc_admin].[pupilAccessArrangements]
