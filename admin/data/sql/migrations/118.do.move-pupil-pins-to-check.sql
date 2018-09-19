0e5782b5beffe53072f1633a0006ad53403a96b6
Jon Shipley <js@ovix.co.uk>
2018-09-19 19:29:53 +0100

    update the migrations to allow the data to migrate even with existing data in the checkFormAllocation table
    also - rename some fk columns that were named incorrectly - should be <tablename> + '_' + <column>

--- admin/data/sql/migrations/118.do.move-pupil-pins-to-check.sql
+++ admin/data/sql/migrations/118.do.move-pupil-pins-to-check.sql
@@ -1,11 +1,35 @@
 alter table [mtc_admin].[checkFormAllocation]
   add
   pin nvarchar(12),
-  school_id int not null;
+  school_id int;
+
+go
+
+update cfa
+set school_id = p.school_id
+FROM [mtc_admin].[checkFormAllocation] cfa JOIN [mtc_admin].[pupil] p
+         ON (p.id = cfa.pupil_id);
+
+alter table [mtc_admin].[checkFormAllocation] alter column school_id int not null;
 
 go
 
 create unique index [checkFormAllocation_school_id_pin_uindex]
   on [mtc_admin].[checkFormAllocation] (school_id, pin)
   where pin is not null;
+
+go
+
+-- rename some oddities, note SQL SERVER is case insensitive though
+
+EXEC sp_rename 'mtc_admin.checkFormAllocation.pupil_Id', 'pupil_id', 'COLUMN';
+go
+
+EXEC sp_rename 'mtc_admin.checkFormAllocation.checkForm_Id', 'checkForm_id', 'COLUMN';
+go
+
+EXEC sp_rename 'mtc_admin.checkFormAllocation.checkWindow_Id', 'checkWindow_id', 'COLUMN';
+go
+
+
 
