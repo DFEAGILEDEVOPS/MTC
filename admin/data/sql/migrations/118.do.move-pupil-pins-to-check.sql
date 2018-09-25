0dfbc540eb89d2a3c721ff2a2d54e76c626df48a
Jon Shipley <js@ovix.co.uk>
2018-09-25 14:12:47 +0100

    SQL migration
    Move pupil pin to the check table - making it a check pin

--- admin/data/sql/migrations/118.do.move-pupil-pins-to-check.sql
+++ admin/data/sql/migrations/118.do.move-pupil-pins-to-check.sql
@@ -32,3 +32,30 @@ alter table [mtc_admin].[pupilFeedback]
 drop table [mtc_admin].[checkFormAllocation];
 
 
+-- move pupil pin to check pin
+alter table [mtc_admin].[check]
+  add
+  pin nvarchar(12),
+  pinExpiresAt datetimeoffset(3),
+  school_id int;
+go
+
+-- migrate school ids into new field
+update chk
+set school_id = p.school_id
+FROM [mtc_admin].[check] chk JOIN [mtc_admin].[pupil] p
+         ON (p.id = chk.pupil_id);
+
+-- make school_id not null
+alter table [mtc_admin].[check] alter column school_id int not null;
+go
+
+-- Add the school / pin index
+create unique index [check_school_id_pin_uindex]
+  on [mtc_admin].[check] (school_id, pin)
+  where pin is not null;
+
+-- remove the pin and expiry from the pupil table
+drop index [mtc_admin].[pupil].[pupil_school_id_pin_uindex];
+alter table [mtc_admin].[pupil]
+    drop column pin, pinExpiresAt;
