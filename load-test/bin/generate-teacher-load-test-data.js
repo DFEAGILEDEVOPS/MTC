cb91a61f8d6482241051fc1d891f2cb8b477c79d
Dan Williams <dan@ithium.net>
2018-10-08 10:29:07 +0100

    Fixed teacher generation lint errors and typos

--- load-test/bin/generate-teacher-load-test-data.js
+++ load-test/bin/generate-teacher-load-test-data.js
@@ -3,22 +3,20 @@ const { TYPES } = require('tedious')
 const sqlService = require('../../admin/services/data-access/sql.service')
 const sqlPoolService = require('../../admin/services/data-access/sql.pool.service')
 
-const passwordHash = "$2a$10$.WsawgZpWSAQVaa6Vz3P1.XO.1YntYJLd6Da5lrXCAkVxhhLpkOHK";
+const passwordHash = '$2a$10$.WsawgZpWSAQVaa6Vz3P1.XO.1YntYJLd6Da5lrXCAkVxhhLpkOHK'
 
 async function main () {
-  
   try {
-    
-    const schools = await sqlService.query(`SELECT * FROM school`)
-    let i = 0; const numSchools = schools.length; 
+    const schools = await sqlService.query(`SELECT id FROM school`)
+    const numSchools = schools.length
+    let school, teacherIdentifier
 
     winston.info(`${numSchools} schools`)
 
-    for(; i < numSchools; i++) {
-    
-      let school = schools[i]
-      let teacherIdentifier = 'teacher' + (i+1)
-      
+    for (let i = 0; i < numSchools; i++) {
+      school = schools[i]
+      teacherIdentifier = `teacher${i + 1}`
+
       // maybe use sqlService.generateParams
       let params = [
         {
@@ -44,28 +42,21 @@ async function main () {
       `
 
       try {
-
         await sqlService.query(sql, params)
-
-      } catch(error) {
-        
-        // Catch unique contraint errors
-        // We need to do this because a user with the identifier
-        // might exist in the db and we cant remove them.
-        // This makes the assumtion that the existing users
-        // password is a hash of 'password' and we can't be sure
-        // there is 1 user per school
-        if(error.number !== 2627) {
-          throw error;
+      } catch (error) {
+        // Catch unique constraint errors
+        // We’re doing this in case teachers with the same identifier exist so we don’t override them
+        // This makes the assumption that the existing users password is a hash of 'password'
+        // and we can't be sure there is 1 user per school
+        if (error.number !== 2627) {
+          throw error
         }
       }
     }
-    
+
     winston.info('Done')
     sqlPoolService.drain()
-    
   } catch (error) {
-
     winston.info(error)
     process.exitCode = 1
     sqlPoolService.drain()
