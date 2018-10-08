dccf35499be4e22fab0492b3a4688bc81474ef87
Dan Williams <dan@ithium.net>
2018-10-08 11:23:41 +0100

    Updated load test pupil generation script
    - Added numPupils parameter
    - Removed school creation
    - Updated SQL to catch constraint errors

--- load-test/bin/generate-pupil-load-test-data.js
+++ load-test/bin/generate-pupil-load-test-data.js
@@ -1,69 +1,34 @@
 const winston = require('winston')
 const { TYPES } = require('tedious')
 const moment = require('moment')
-const converter = require('number-to-words')
-const R = require('ramda')
 const sqlService = require('../../admin/services/data-access/sql.service')
 const sqlPoolService = require('../../admin/services/data-access/sql.pool.service')
-const schoolDataService = require('../../admin/services/data-access/school.data.service')
 const pupilDataService = require('../../admin/services/data-access/pupil.data.service')
-const pinGenerationService = require('../../admin/services/pin-generation.service')
 const checkStartService = require('../../admin/services/check-start.service')
 
 async function main () {
   try {
-    const schoolLength = parseInt(process.argv[2])
-    if (!schoolLength) {
-      throw new Error('School length argument is not supplied or is not a valid number')
+    const numPupils = parseInt(process.argv[2])
+    if (!numPupils) {
+      throw new Error('Pupil length argument is not supplied or is not a valid number')
     }
-    // Generate additional example schools based on schoolLength
-    winston.info('Generating schools...')
-    const dfeNumbers = []
-    for (let i = 0; i < schoolLength; i++) {
-      let estabCode = 1006 + i
-      const schoolNumber = converter.toWords(6 + i)
-      const exampleSchoolNumber = schoolNumber.charAt(0).toUpperCase() + schoolNumber.slice(1)
-      estabCode = estabCode.toString()
-      let name = `Example School ${exampleSchoolNumber}`
-      const dfeNumber = 9991006 + i
-      dfeNumbers.push(dfeNumber)
-      const urn = 89006 + i
-      const params = [
-        {
-          name: 'estabCode',
-          value: estabCode,
-          type: TYPES.NVarChar
-        },
-        {
-          name: 'name',
-          value: name,
-          type: TYPES.NVarChar
-        },
-        {
-          name: 'urn',
-          value: urn,
-          type: TYPES.Int
-        },
-        {
-          name: 'dfeNumber',
-          value: dfeNumber,
-          type: TYPES.Int
-        }
-      ]
-      const sql = `INSERT INTO ${sqlService.adminSchema}.school
-      (leaCode, estabCode, name, urn, dfeNumber)
-      VALUES (999, @estabCode, @name, @urn, @dfeNumber)`
-      await sqlService.query(sql, params)
-    }
-    const testSchools = await schoolDataService.sqlFindByDfeNumbers(dfeNumbers)
-    // Generate additional pupils for these schools and pins
-    winston.info('Generating pupils within schools and pins...')
-    await Promise.all(testSchools.map(async s => {
-      const params = [
+    const schools = await sqlService.query(`SELECT * FROM school`)
+    const numSchools = schools.length
+    let school, params
+
+    winston.info(`${numSchools} schools`)
+    winston.info(`Generating ${numPupils} pupils per school`)
+
+    // Not using Promise.all/schools.map because of pool errors
+    for (let i = 0; i < numSchools; i++) {
+      school = schools[i]
+
+      // maybe use sqlService.generateParams
+      params = [
         {
           name: 'schoolId',
-          value: s.id,
-          type: TYPES.NVarChar
+          value: school.id,
+          type: TYPES.Int
         },
         {
           name: 'dateOfBirth',
@@ -72,21 +37,26 @@ async function main () {
         }
       ]
       const sql = `
-      DECLARE @cnt INT = 0;
-      DECLARE @baseUpn INT = 80120000 + @schoolId
-      WHILE @cnt < 60
       BEGIN
-        INSERT ${sqlService.adminSchema}.[pupil] (school_id, foreName, lastName, gender, dateOfBirth, upn) 
-        VALUES (@schoolId, CAST(@cnt AS NVARCHAR), 'Pupil', 'M', @dateOfBirth, CAST(@baseUpn AS NVARCHAR) + CAST(@cnt AS NVARCHAR) + '1A')
-        SET @cnt = @cnt + 1;
-      END;`
+        DECLARE @cnt INT = 0;
+        DECLARE @baseUpn INT = 80120000 + @schoolId
+        WHILE @cnt < ${numPupils}
+        BEGIN
+          BEGIN TRY
+            INSERT ${sqlService.adminSchema}.[pupil] (school_id, foreName, lastName, gender, dateOfBirth, upn) 
+            VALUES (@schoolId, CAST(@cnt AS NVARCHAR), 'Pupil', 'M', @dateOfBirth, CAST(@baseUpn AS NVARCHAR) + CAST(@cnt AS NVARCHAR) + '1A')
+          END TRY
+          BEGIN CATCH
+          END CATCH
+          SET @cnt = @cnt + 1;
+        END;
+      END`
       await sqlService.query(sql, params)
-      const update = pinGenerationService.generateSchoolPassword(s)
-      await schoolDataService.sqlUpdate(R.assoc('id', s.id, update))
-      const pupils = await pupilDataService.sqlFindPupilsByDfeNumber(s.dfeNumber)
+      const pupils = await pupilDataService.sqlFindPupilsByDfeNumber(school.dfeNumber)
       const pupilsList = pupils.map(p => p.id)
-      await checkStartService.prepareCheck(pupilsList, s.dfeNumber, s.id)
-    }))
+      await checkStartService.prepareCheck(pupilsList, school.dfeNumber, school.id)
+    }
+
     winston.info('DONE')
     sqlPoolService.drain()
   } catch (error) {
