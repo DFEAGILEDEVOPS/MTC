aa2af2001619c494f59c9afdf376315592acf2e5
Jon Shipley <js@ovix.co.uk>
2018-10-09 18:21:18 +0100

    Update unique index check(school, pin)
    This unique index prevents pupils from having the same login pin at the same
    school (if that happened we couldn't identify the check to give the  pupil).
    
    This update means the index only needs to be maintained for checks that are NEW
    We don't care about pin collisions when the check is in other states as you can't log in to them.

--- admin/data/sql/migrations/124.do.add-pins-to-check-table.sql
+++ admin/data/sql/migrations/124.do.add-pins-to-check-table.sql
@@ -13,7 +13,8 @@ FROM [mtc_admin].[check] chk
        JOIN [mtc_admin].[pupil] p ON (p.id = chk.pupil_id);
 
 -- Add the school / pin index
-create unique index [check_school_id_pin_uindex]
+create unique index [check_school_id_pin_checkStatus_id_uindex]
   on [mtc_admin].[check] (school_id, pin)
-  where pin is not null and school_id is not null;
-
+where pin is not null
+  and school_id is not null
+  and checkStatus_id = 1;
