55c1bc5b6a28e7452b858cc6d211a086c51301cf
Jon Shipley <js@ovix.co.uk>
2018-10-09 17:40:37 +0100

    Refactor `sqlFindPupilsWithActivePins` to use a view (sql migration 125)

--- admin/services/data-access/pin-generation.data.service.js
+++ admin/services/data-access/pin-generation.data.service.js
@@ -26,26 +26,10 @@ const serviceToExport = {
     const param = { name: 'schoolId', type: TYPES.Int, value: schoolId }
     const sql = `
       SELECT 
-        p.id,
-        p.foreName,
-        p.lastName,
-        p.middleNames,
-        p.dateOfBirth,
-        p.urlSlug,
-        chk.pin,
-        chk.pinExpiresAt,        
-        g.group_id
-      FROM ${sqlService.adminSchema}.[pupil] p
-      INNER JOIN ${sqlService.adminSchema}.[school] s ON p.school_id = s.id
-      LEFT JOIN  ${sqlService.adminSchema}.[pupilGroup] g ON g.pupil_id = p.id
-      INNER JOIN ${sqlService.adminSchema}.[check] chk ON chk.pupil_id = p.id
-      INNER JOIN ${sqlService.adminSchema}.[checkStatus] chkStatus ON chk.checkStatus_id = chkStatus.id
-      WHERE chk.pin IS NOT NULL
-      AND s.id = @schoolId
-      AND chk.pinExpiresAt IS NOT NULL
-      AND chk.pinExpiresAt > GETUTCDATE()
-      AND chkStatus.code = 'NEW'
-      ORDER BY p.lastName ASC, p.foreName ASC, p.middleNames ASC, dateOfBirth ASC
+        *
+      FROM ${sqlService.adminSchema}.[vewPupilsWithActivePins] 
+      WHERE school_id = @schoolId
+      ORDER BY lastName ASC, foreName ASC, middleNames ASC, dateOfBirth ASC
       `
     return sqlService.query(sql, [param])
   },
