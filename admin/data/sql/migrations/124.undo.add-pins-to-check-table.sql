d63d6472408f71339f8184f8cf419046e84aa8d0
Jon Shipley <js@ovix.co.uk>
2018-10-09 21:07:56 +0100

    Bugfix: rename the undo script index name

--- admin/data/sql/migrations/124.undo.add-pins-to-check-table.sql
+++ admin/data/sql/migrations/124.undo.add-pins-to-check-table.sql
@@ -1,5 +1,5 @@
 
-drop index [check_school_id_pin_uindex] on [mtc_admin].[check];
+drop index [check_school_id_pin_checkStatus_id_uindex] on [mtc_admin].[check];
 
 alter table [mtc_admin].[check]
   drop column
