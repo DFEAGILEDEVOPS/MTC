065ae59fbee5f3b4051aea1e34393d48f03bd821
Jon Shipley <js@ovix.co.uk>
2018-10-19 06:39:17 +0100

    Clean sql up

--- admin/data/sql/migrations/152.do.drop-check-pin-column.sql
+++ admin/data/sql/migrations/152.do.drop-check-pin-column.sql
@@ -7,4 +7,3 @@ go
 -- school_id isn't needed on the check table, it was only introduced while we
 -- had a unqiue index on (school_id, pin, checkStatus_id)
 alter table [mtc_admin].[check] drop column pin, pinExpiresAt, school_id;
-go
