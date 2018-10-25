64ab4d02be5532197a536bfdc84f75f669756908
Guy Harwood <guy@milkchip.com>
2018-10-25 16:26:21 +0100

    completion of message processing call

--- functions/pupil-login/index.js
+++ functions/pupil-login/index.js
@@ -4,4 +4,5 @@ module.exports = async function (context, message) {
   context.log(
     `processing pupil-login message version:${message.version} for check:${message.checkCode}`
   )
+  context.done()
 }
