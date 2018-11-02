5cab98007cc4ab0d9801e28a6abfaa2951414707
Jon Shipley <js@ovix.co.uk>
2018-11-02 15:19:51 +0000

    Exclude pupils with unconsumed restarts from being eligible for a restart

--- admin/data/sql/migrations/173.do.alter-pupils-eligible-for-restart-view.sql
+++ admin/data/sql/migrations/173.do.alter-pupils-eligible-for-restart-view.sql
@@ -14,12 +14,15 @@ ALTER VIEW [mtc_admin].[vewPupilsEligibleForRestart] AS
          LEFT JOIN [mtc_admin].[pupilAttendance] pa ON (p.id = pa.pupil_id)
          INNER JOIN [mtc_admin].[check] AS chk ON (p.id = chk.pupil_id)
          INNER JOIN [mtc_admin].[checkStatus] AS chkStatus ON (chk.checkStatus_id = chkStatus.id)
+         LEFT JOIN [mtc_admin].[pupilRestart] as pr ON (p.id = pr.pupil_id)
   WHERE
           -- don’t select pupils who are not attending
           pa.id IS NULL
           -- pupils must have already attempted 1 or more live checks that are not expired
 AND      chkStatus.code IN ('CMP', 'EXP', 'NTR')
 AND      chk.isLiveCheck = 1
+          -- exclude pupils with an existing unconsumed restart
+AND      (pr.id IS NULL OR (pr.id IS NOT NULL AND pr.check_id IS NOT NULL))
 GROUP BY
          p.id,
          p.foreName,
@@ -28,5 +31,4 @@ GROUP BY
          p.dateOfBirth,
          p.urlSlug,
          p.school_id
-
 go
