4c27419172d1da155090322f88582d7ae0ff3897
Dan Williams <dan@ithium.net>
2018-11-09 08:56:11 +0000

    Updated create migration script to generate sql & js files

--- admin/data/sql/create-migration.js
+++ admin/data/sql/create-migration.js
@@ -4,26 +4,51 @@
 const moment = require('moment')
 const fs = require('fs')
 const path = require('path')
+const commandLineArgs = require('command-line-args')
 
 const migrationsDir = path.join(__dirname, 'migrations')
 
-const createMigration = name => {
+const optionDefinitions = [
+  { name: 'name', alias: 'n', type: String, defaultOption: true },
+  { name: 'type', alias: 't', type: String },
+  { name: 'help', alias: 'h', type: Boolean }
+]
+
+const jsMigrationTemplate = `
+'use strict'
+module.exports.generateSql = function () {
+}
+`
+
+const createMigration = options => {
   const version = moment().format('YYYYMMDDHHmmss')
-  const upFilename = `${version}.do.${name.toLowerCase()}.sql`
-  const downFilename = `${version}.undo.${name.toLowerCase()}.sql`
-  fs.closeSync(fs.openSync(path.join(migrationsDir, upFilename), 'w'))
-  fs.closeSync(fs.openSync(path.join(migrationsDir, downFilename), 'w'))
-  console.log(`Created ${upFilename} and ${downFilename}`)
+  const upFile = path.join(migrationsDir, `${version}.do.${options.name.toLowerCase()}`)
+  const downFile = path.join(migrationsDir, `${version}.undo.${options.name.toLowerCase()}`)
+  let upFileName, downFileName
+  if (options.type && options.type.toLowerCase() === 'js') {
+    upFileName = `${upFile}.js`
+    downFileName = `${downFile}.js`
+    fs.writeFileSync(upFileName, jsMigrationTemplate)
+    fs.writeFileSync(downFileName, jsMigrationTemplate)
+  } else {
+    upFileName = `${upFile}.sql`
+    downFileName = `${downFile}.sql`
+    fs.writeFileSync(upFileName, '')
+    fs.writeFileSync(downFileName, '')
+  }
+  console.log(`Created ${upFileName} and ${downFileName}`)
 }
 
 try {
-  if (process.argv.length < 3) {
+  const options = commandLineArgs(optionDefinitions)
+  if (options.help || !options.name) {
     console.log(`
-    Usage: node create-migration.js <migration-name>
+    Usage: create-migration.js <name> [--type <sql|js>]
     `)
     process.exit(0)
   }
-  createMigration(process.argv[2])
+  createMigration(options)
+  process.exit(0)
 } catch (error) {
   console.log(`Error: ${error.message}`)
   process.exit(1)
