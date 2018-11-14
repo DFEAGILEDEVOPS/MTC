37003d24a9953c9cdc4ce429a909e4d11a91249e
Guy Harwood <guy@milkchip.com>
2018-11-14 09:44:57 +0000

    add function dockerfile. output con type from diag function.  strip down tests for diagnostics purposes

--- functions/diag/index.js
+++ functions/diag/index.js
@@ -1,6 +1,15 @@
 module.exports = async function (context, req) {
+  const con = process.env.AZURE_STORAGE_CONNECTION_STRING
+  let conType = 'not found'
+  if (con.toLower().startsWith('usedevelopment')) {
+    conType = 'dev'
+  } else if (con.toLower().startsWith('DefaultEndpoints')) {
+    conType = 'connection'
+  }
+
   context.res = {
     status: 200,
-    body: `MTC functions. running node ${process.version}`
+    body: `MTC functions. running node ${process.version}. 
+            Storage Connection Type: ${conType}`
   }
 }
--- test/admin-hpa/admin_tests_2.sh
+++ test/admin-hpa/admin_tests_2.sh
@@ -16,8 +16,9 @@ MSG='admin app is running under process '
 MSG+=$PID
 echo $MSG
 
-cd test
-rake features OPTS='-t @generate_pupil_pins,@manage_check_window,@phase_banner,@pupils_not_taking_check,@pupil_register,@question_time_limits,@restarts'
+# cd test
+# rake features OPTS='-t @generate_pupil_pins,@manage_check_window,@phase_banner,@pupils_not_taking_check,@pupil_register,@question_time_limits,@restarts'
+rake features OPTS='-t @generate_pupil_pins,@manage_check_window,@phase_banner,@pupil_register,@question_time_limits'
 CUCUMBER_EXIT_CODE=$?
 
 kill -9 $PID
