971d66632db04c64bcf289f59470865b0ffe0b80
Guy Harwood <guy@milkchip.com>
2018-11-14 10:26:50 +0000

    install .net core & func tools for travis

--- functions/diag/index.js
+++ functions/diag/index.js
@@ -1,15 +1,7 @@
 module.exports = async function (context, req) {
-  const con = process.env.AZURE_STORAGE_CONNECTION_STRING
-  let conType = 'not found'
-  if (con.toLower().startsWith('usedevelopment')) {
-    conType = 'dev'
-  } else if (con.toLower().startsWith('DefaultEndpoints')) {
-    conType = 'connection'
-  }
 
   context.res = {
     status: 200,
-    body: `MTC functions. running node ${process.version}. 
-            Storage Connection Type: ${conType}`
+    body: `MTC functions. running node ${process.version}`
   }
 }
