4cd611f9f46bf1c86df487488fc1ec01ce0a4d99
Guy Harwood <guy@milkchip.com>
2018-11-14 15:16:09 +0000

    add log output for functions and API

--- test/pupil-hpa/hpa_travis_reduced.sh
+++ test/pupil-hpa/hpa_travis_reduced.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -x
 
 # start pupil-api
-cd ../../pupil-api && npm start > server.auth.log 2>&1 &
+cd ../../pupil-api && npm start > api.log 2>&1 &
 
 # start function
-cd ../../functions && npm start > server.auth.log 2>&1 &
+cd ../../functions && npm start > functions.log 2>&1 &
 
 # start pupil app
 cd ../../pupil-spa && npm start &
 
-cd ../../admin && yarn start > server.log 2>&1 &
+cd ../../admin && yarn start > admin.log 2>&1 &
 PID=$!
 
 MSG='admin app is running under process '
@@ -22,8 +22,18 @@ CUCUMBER_EXIT_CODE=$?
 kill -9 $PID
 
 #echo "************"
-#echo " SERVER LOG "
-#echo "*************"
-#cat ../server.log
+#echo " ADMIN LOG"
+#echo "************"
+#cat ../admin.log
+
+echo "***********"
+echo " API LOG"
+echo "***********"
+cat ../api.log
+
+echo "**************"
+echo " FUNCTIONS LOG"
+echo "**************"
+cat ../functions.log
 
 exit $CUCUMBER_EXIT_CODE
