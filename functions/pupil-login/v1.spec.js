4c9c351627acdb3ca18c341c25de9deb6c70c67f
Jon Shipley <js@ovix.co.uk>
2018-11-14 09:14:15 +0000

    Update test name

--- functions/pupil-login/v1.spec.js
+++ functions/pupil-login/v1.spec.js
@@ -6,7 +6,7 @@ const sqlService = require('less-tedious')
 const v1 = require('./v1')
 
 describe('v1-process', () => {
-  it('sqlService.modify', async () => {
+  it('calls sqlService.modify', async () => {
     spyOn(sqlService, 'modify')
     await v1.process('abc-def-123', '2018-12-31T16:23:59.123Z')
     expect(sqlService.modify).toHaveBeenCalled()
