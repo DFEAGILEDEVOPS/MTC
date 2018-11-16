6a75cf2a000aa065fafe47bb40f3e50e38cad3eb
Dan Williams <dan@ithium.net>
2018-11-16 16:23:30 +0000

    Moved sample data to the seeds

---
+++ admin/data/sql/seeds/20181116124850.custom.sample-user-school.sql
@@ -0,0 +1,26 @@
+DECLARE @schoolId INT
+
+INSERT INTO [mtc_admin].school (leaCode, estabCode, urn, dfeNumber, name) VALUES (999, '1001', 89001, 9991001, 'Example School One');
+SELECT @schoolId = @@IDENTITY
+INSERT INTO [mtc_admin].[user] (identifier, passwordHash, school_id, role_id) 
+  VALUES ('teacher1', '$2a$10$.WsawgZpWSAQVaa6Vz3P1.XO.1YntYJLd6Da5lrXCAkVxhhLpkOHK', @schoolId, 3);
+
+INSERT INTO [mtc_admin].school (leaCode, estabCode, urn, dfeNumber, name) VALUES (999, '1002', 89002, 9991002, 'Example School Two');
+SELECT @schoolId = @@IDENTITY
+INSERT INTO [mtc_admin].[user] (identifier, passwordHash, school_id, role_id) 
+  VALUES ('teacher2', '$2a$10$.WsawgZpWSAQVaa6Vz3P1.XO.1YntYJLd6Da5lrXCAkVxhhLpkOHK', @schoolId, 3);
+
+INSERT INTO [mtc_admin].school (leaCode, estabCode, urn, dfeNumber, name) VALUES (999, '1003', 89003, 9991003, 'Example School Three');
+SELECT @schoolId = @@IDENTITY
+INSERT INTO [mtc_admin].[user] (identifier, passwordHash, school_id, role_id) 
+  VALUES ('teacher3', '$2a$10$.WsawgZpWSAQVaa6Vz3P1.XO.1YntYJLd6Da5lrXCAkVxhhLpkOHK', @schoolId, 3);
+
+INSERT INTO [mtc_admin].school (leaCode, estabCode, urn, dfeNumber, name) VALUES (999, '1004', 89004, 9991004, 'Example School Four');
+SELECT @schoolId = @@IDENTITY
+INSERT INTO [mtc_admin].[user] (identifier, passwordHash, school_id, role_id) 
+  VALUES ('teacher4', '$2a$10$.WsawgZpWSAQVaa6Vz3P1.XO.1YntYJLd6Da5lrXCAkVxhhLpkOHK', @schoolId, 3);
+
+INSERT INTO [mtc_admin].school (leaCode, estabCode, urn, dfeNumber, name) VALUES (999, '1005', 89005, 9991005, 'Example School Five');
+SELECT @schoolId = @@IDENTITY
+INSERT INTO [mtc_admin].[user] (identifier, passwordHash, school_id, role_id) 
+  VALUES ('teacher5', '$2a$10$.WsawgZpWSAQVaa6Vz3P1.XO.1YntYJLd6Da5lrXCAkVxhhLpkOHK', @schoolId, 3);
---
+++ admin/data/sql/seeds/20181116150225.custom.sample-check-windows-in-past.sql
@@ -0,0 +1,1 @@
+INSERT INTO [mtc_admin].[checkWindow] ([name], adminStartDate, checkStartDate, checkEndDate) VALUES ('Past Development Phase', '2017-06-01', '2017-07-01', '2017-09-01') 
