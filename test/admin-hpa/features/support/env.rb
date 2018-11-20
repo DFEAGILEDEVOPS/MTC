c7218197c221256acdb7d78ee5d724f018ac48ba
Guy Harwood <guy@milkchip.com>
2018-11-20 16:37:44 +0000

    azure var

--- test/admin-hpa/features/support/env.rb
+++ test/admin-hpa/features/support/env.rb
@@ -51,21 +51,28 @@ end
 Dir.mkdir("reports") unless File.directory?("reports")
 Capybara.javascript_driver = ENV["DRIVER"].to_sym
 
+
 sleep 20
 database = ENV['SQL_DATABASE'] || 'mtc'
 server = ENV['SQL_SERVER'] || 'localhost'
 port =  ENV['SQL_PORT'] || 1433
 admin_user = ENV['SQL_ADMIN_USER'] || 'sa'
 admin_password = ENV['SQL_ADMIN_USER_PASSWORD'] || 'Mtc-D3v.5ql_S3rv3r'
-is_azure = ENV['AZURE'] || false
-
+azure_test = ENV['AZURE'] || 'false'
+if azure_test == 'true'
+  azure_var = true
+else
+  azure_var = false
+end
+p database, server, port, admin_password, admin_user
 begin
-SQL_CLIENT = TinyTds::Client.new(username: admin_user,
-                                 password: admin_password,
-                                 host: server,
-                                 port: port,
-                                 database: database,
-                                 azure:is_azure)
+  SQL_CLIENT = TinyTds::Client.new(username: admin_user,
+                                   password: admin_password,
+                                   host: server,
+                                   port: port,
+                                   database: database,
+                                   azure: azure_var
+  )
 rescue TinyTds::Error => e
   abort 'Test run failed due to - ' + e.to_s
 end
