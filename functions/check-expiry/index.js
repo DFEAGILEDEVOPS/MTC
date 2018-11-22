5f355e73f6aa706d54d9c7b27f6bcc878103ba10
Jon Shipley <js@ovix.co.uk>
2018-11-22 11:09:32 +0000

    Change check-expiry to be a timer function, that expires all the checks in the SQL DB directly using SQL

--- functions/check-expiry/index.js
+++ functions/check-expiry/index.js
@@ -1,36 +1,21 @@
 'use strict'
 
-const uuid = require('uuid/v4')
-const { performance } = require('perf_hooks');
+const { performance } = require('perf_hooks')
 
 const v1 = require('./v1')
 
 module.exports = async function (context, message) {
   const start = performance.now()
-  if (typeof message !== 'object') {
-    throw new Error('check-expiry: badly formed message')
-  }
-
-  context.log('check-expiry message received', message.checkCode)
 
   try {
     v1.process(message, context.log)
   } catch (error) {
-    context.log.error(`check-expiry: Failed to expire check ${message.checkCode}: ${error.message}`)
+    context.log.error(`check-expiry: ERROR: ${error.message}`)
     throw error
   }
 
-  context.bindings.pupilEventsTable = []
-  const entity = {
-    PartitionKey: message.checkCode,
-    RowKey: uuid(),
-    eventType: 'check-expiry',
-    payload: JSON.stringify(message),
-    processedAt: new Date()
-  }
-  context.bindings.pupilEventsTable.push(entity)
   const end = performance.now()
   const durationInMilliseconds = end - start
   const timeStamp = new Date().toISOString()
-  context.log(`check-expiry: ${timeStamp} run complete: expired 1 check in ${durationInMilliseconds} ms`)
+  context.log(`check-expiry: ${timeStamp} run took ${durationInMilliseconds} ms`)
 }
--- functions/check-expiry/v1.js
+++ functions/check-expiry/v1.js
@@ -1,62 +1,77 @@
 'use strict'
 
+const R = require('ramda')
 const sqlService = require('less-tedious')
-const { TYPES } = require('tedious')
 
-const sqlUtil = require('../lib/sql-helper')
+const config = require('../config')
+sqlService.initialise(config)
 const azureStorageHelper = require('../lib/azure-storage-helper')
 
 const v1 = {
-  process: async function process(message, logger) {
-    // step 1: change the check status - B0
-    await expireCheck(message.checkCode)
-    // step 2: delete from the prepared check table - A1
-    await deleteFromPreparedCheckTable(message.checkCode, logger)
-    // step 3: update the pupil status - B1
-    await updatePupilStatus(message.checkCode)
+  process: async function process (logger) {
+    // step 1: change the check status
+    const checkData = await expireChecks(logger)
+
+    // step 2: update the pupil status
+    await updatePupilStatus(logger, checkData)
   }
 }
 
 /**
- * Set the check status to Expired for a single check
- * @param checkCode
+ * Find expired checks and set the status accordingly
+ * Returns a minimal check object for each check expired
  * @return {Promise}
  */
-async function expireCheck(checkCode) {
-  const sql = `UPDATE [mtc_admin].[check]
-               SET checkStatus_id = (SELECT TOP (1) id FROM [mtc_admin].[checkStatus] WHERE code = 'EXP')
-               WHERE checkCode = @checkCode`
-  const params = [
-    { name: 'checkCode', value: checkCode, type: TYPES.UniqueIdentifier }
-  ]
-  return sqlService.modify(sql, params)
+async function expireChecks () {
+  const sql = `
+  DECLARE @updateLog table (
+      checkId int NOT NULL,
+      pupilId int NOT NULL,
+      checkCode uniqueidentifier NOT NULL
+  );
+      
+  UPDATE [mtc_admin].[check]
+  SET checkStatus_id = (SELECT TOP (1) id from [mtc_admin].[checkStatus] where code = 'EXP')
+  OUTPUT [inserted].id, [inserted].pupil_id, [inserted].[checkCode] INTO @updateLog
+  FROM [mtc_admin].[check] chk join [mtc_admin].[checkPin] cp
+    ON (chk.id = cp.check_id)
+  WHERE
+    cp.pinExpiresAt < GETUTCDATE();
+    
+  SELECT
+       checkId,
+       pupilId,
+       checkCode
+  FROM @updateLog;`
+
+  return sqlService.query(sql)
 }
 
 /**
- * Delete an entry in the preparedCheck table
- * @param checkCode
- * @param logger
- * @return {Promise<*|Promise<void>>}
- */
-async function deleteFromPreparedCheckTable(checkCode, logger) {
-  const azureTableService = azureStorageHelper.getPromisifiedAzureTableService()
-  try {
-    await azureStorageHelper.deleteFromPreparedCheckTableStorage(azureTableService, checkCode, logger)
-  } catch (error) {
-  }
-}
-
-/**
- * Make a request for the pupil-status to be updated
+ * Make a request for the pupil-status to be updated for multiple pupils
  * @param checkCode
  * @return {Promise<*|Promise<*>>}
  */
-async function updatePupilStatus(checkCode) {
-  const check = await sqlUtil.sqlFindCheckByCheckCode(checkCode)
-  return azureStorageHelper.addMessageToQueue('pupil-status', {
-    version: 1,
-    pupilId: check.pupil_id,
-    checkCode: check.checkCode
+async function updatePupilStatus (logger, checkData) {
+  logger.info(`check-expiry: updatePupilStatus(): got ${checkData.length} pupils`)
+  // Let's say we can expire 200K checks in one go
+  // we need to put 200K messages on the queue
+  // so we batch them at 1000 a time
+  const batches = R.splitEvery(1000, checkData)
+  checkData = null
+
+  batches.forEach(async (checks, batchNumber) => {
+    try {
+      const msgs = checks.map(check => azureStorageHelper.addMessageToQueue('pupil-status', {
+        version: 1,
+        pupilId: check.pupil_id,
+        checkCode: check.checkCode
+      }))
+      await Promise.all(msgs)
+      console.log(`Batch ${batchNumber} complete`)
+    } catch (error) {
+      logger.error(`check-expiry: updatePupilStatus(): ERROR: ${error.message}`)
+    }
   })
 }
 
