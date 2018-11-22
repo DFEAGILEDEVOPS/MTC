7ffba2dfb6868f6a035f2a884df104ba8c4dd20c
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-11-22 14:12:32 +0000

    prepared check sync processor implementation

--- functions/prepare-check/index.js
+++ functions/prepare-check/index.js
@@ -1,4 +1,5 @@
 const R = require('ramda')
+const preparedCheckSchemaValidator = require('../lib/prepared-check-schema-validator')
 
 /**
  * Write to Table Storage for fast pupil authentication
@@ -11,7 +12,7 @@ module.exports = function (context, prepareCheckMessage) {
   // TODO: Add a version strategy: version field, version handler.
   // TODO: Add batch processing: e.g. handle 100 at a time
   try {
-    validateMessage(prepareCheckMessage)
+    preparedCheckSchemaValidator.validateMessage(prepareCheckMessage)
   } catch (error) {
     // After 5 attempts at processing the message will be moved to the poison queue
     // https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-storage-queue#trigger---poison-messages
@@ -44,49 +45,4 @@ module.exports = function (context, prepareCheckMessage) {
   context.bindings[outputProp] = []
   context.bindings[outputProp].push(preparedCheck)
   context.done()
-}
-
-function validateMessage (message) {
-  const check = (prop) => { if (R.not(R.has(prop, message))) { throw new Error(`Message failed validation check: missing field: ${prop}`) } }
-  const topLevelProperties = ['schoolPin', 'pupilPin', 'pupil', 'school', 'tokens', 'config', 'questions']
-  topLevelProperties.map(check)
-  validatePupil(message.pupil)
-  validateQuestions(message.questions)
-  validateSchool(message.school)
-  validateConfig(message.config)
-}
-
-function validatePupil (pupil) {
-  const pupilProperties = ['firstName', 'lastName', 'dob', 'checkCode']
-  schemaValidator(pupil, pupilProperties, 'pupil')
-}
-
-function validateQuestions (questions) {
-  if (R.not(R.is(Array, questions))) {
-    throw new Error('Questions is not an Array')
-  }
-  questions.map(q => {
-    const res = R.props(['order', 'factor2', 'factor2'], q)
-    if (R.contains(undefined, res)) {
-      throw new Error('Invalid question')
-    }
-  })
-}
-
-function validateSchool (school) {
-  const schoolProperties = ['id', 'name']
-  schemaValidator(school, schoolProperties, 'school')
-}
-
-function validateConfig (config) {
-  const configProperties = ['questionTime', 'loadingTime', 'speechSynthesis']
-  schemaValidator(config, configProperties, 'config')
-}
-
-function schemaValidator (obj, requiredPropertyList, name = 'schema') {
-  const check = (prop) => { if (R.not(R.has(prop, obj))) { throw new Error(`Missing ${name} field: ${prop}`) } }
-  if (R.not(R.is(Object, obj))) {
-    throw new Error(`${name} is not an object`)
-  }
-  requiredPropertyList.map(check)
 }
