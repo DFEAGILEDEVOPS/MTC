10f7e328528c0890b1ef54f9d2c7179a2fb2e00c
Jon Shipley <js@ovix.co.uk>
2018-11-23 08:45:17 +0000

    check-expiry: return meta to process controller

--- functions/check-expiry/index.js
+++ functions/check-expiry/index.js
@@ -7,8 +7,9 @@ const v1 = require('./v1')
 module.exports = async function (context, message) {
   const start = performance.now()
 
+  let meta
   try {
-    v1.process(message, context.log)
+    meta = v1.process(context.log)
   } catch (error) {
     context.log.error(`check-expiry: ERROR: ${error.message}`)
     throw error
@@ -17,5 +18,5 @@ module.exports = async function (context, message) {
   const end = performance.now()
   const durationInMilliseconds = end - start
   const timeStamp = new Date().toISOString()
-  context.log(`check-expiry: ${timeStamp} run took ${durationInMilliseconds} ms`)
+  context.log(`check-expiry: ${timeStamp} processed ${meta.processCount} checks, run took ${durationInMilliseconds} ms`)
 }
