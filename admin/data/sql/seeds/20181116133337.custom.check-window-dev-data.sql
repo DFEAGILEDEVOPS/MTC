febe0f64d3b4efe841af5fc0516be479c61fb0a2
Dan Williams <dan@ithium.net>
2018-11-23 11:34:47 +0000

    Updated the adminEndDate to match the version from the up migration.

--- admin/data/sql/seeds/20181116133337.custom.check-window-dev-data.sql
+++ admin/data/sql/seeds/20181116133337.custom.check-window-dev-data.sql
@@ -1,5 +1,5 @@
 IF NOT EXISTS (SELECT id from [mtc_admin].[checkWindow] WHERE name = 'Development Phase')
 BEGIN
-INSERT INTO [mtc_admin].[checkWindow] ([name], adminStartDate, adminEndDate, checkStartDate, checkEndDate, familiarisationCheckStartDate, familiarisationCheckEndDate) 
-VALUES ('Development Phase', '2017-12-01', '2018-06-01', '2017-12-22', '2018-06-01', '2017-12-22', '2018-06-01') 
+INSERT INTO [mtc_admin].[checkWindow] ([name], adminStartDate, adminEndDate, checkStartDate, checkEndDate, familiarisationCheckStartDate, familiarisationCheckEndDate)
+VALUES ('Development Phase', '2017-12-01', '2018-06-01', '2017-12-22', '2019-02-01', '2017-12-22', '2018-06-01')
 END
