ff5fcec9cbe7147e867acd24d38c23053819411f
Dan Williams <dan@ithium.net>
2018-11-23 11:58:53 +0000

    Added admin logging and CORS options to api server

--- pupil-api/src/App.ts
+++ pupil-api/src/App.ts
@@ -12,6 +12,8 @@ import * as winston from 'winston'
 import * as expressWinston from 'express-winston'
 import * as azure from './azure'
 const config = require('./config')
+const corsOptions = require('../../admin/helpers/cors-options')
+const setupLogging = require('../../admin/helpers/logger')
 
 import authRoutes from './routes/auth'
 import pingRoute from './routes/ping'
@@ -38,33 +40,14 @@ class App {
 
   // Configure Express middleware.
   private middleware (): void {
-    if (config.Logging.Express.UseWinston === true) {
-      /**
-       * Express logging to winston
-       */
-      this.express.use(expressWinston.logger({
-        transports: [
-          new winston.transports.Console({
-            json: true,
-            colorize: true,
-            level: logLevel
-          })
-        ],
-        meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-        // msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
-        expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
-        colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
-        ignoreRoute: function (req, res) {
-          return false
-        } // optional: allows to skip some log messages based on request and/or response
-      }))
-    } else {
-      this.express.use(morgan('dev'))
-    }
+
+    /* Logging */
+
+    setupLogging(this.express)
 
     /* Security Directives */
 
-    this.express.use(cors())
+    this.express.use(cors(corsOptions))
     this.express.use(helmet())
 
     // Sets request header "Strict-Transport-Security: max-age=31536000; includeSubDomains".
