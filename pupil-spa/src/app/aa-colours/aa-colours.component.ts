944b4c69f64b2b3112ebeb333dc5b9aafcebff56
Dan Williams <dan@ithium.net>
2018-11-29 11:41:53 +0000

    Feature/26671 pupil views with question reader (#916)
    * Updated sign out links & titles in aa pages
    
    (cherry picked from commit 2918a41277d638d7ccbbea9d21c0071527e4e096)
    
    * Fixed issues in speech synthesis
    
    - Removed aria-hidden elements in speakFocusedElement
    - Removed leading comma from text to be spoken
    
    * Fixed start now button width
    
    * Added docstring for speech synthesis removeUnspokenElements
    
    * Fixed missing spoken elements in speakFocusedElement
    
    - Updated speakFocusedElement to use speakElement which
    in turn calls addTextBeforeSpeakingElement
    
    * Updated speech service to read buttons and links 'inline'
    
    - Replaced buttons and links with text node in parent instead of removing them
    - Updated speech synthesis query selectors
    - Updated warning svg visually hidden labels
    
    * Updated speech service to handle inputs and radio buttons
    
    * Added speech service to aa colours and fonts components
    
    * Changed text on thank you page
    
    * Fixed missing start button arrow on login page

--- pupil-spa/src/app/aa-colours/aa-colours.component.ts
+++ pupil-spa/src/app/aa-colours/aa-colours.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import {
   AccessArrangementsConfig,
@@ -7,23 +7,29 @@ import {
 import { StorageService } from '../services/storage/storage.service';
 import { RouteService } from '../services/route/route.service';
 import { PupilPrefsService } from '../services/pupil-prefs/pupil-prefs.service';
+import { SpeechService } from '../services/speech/speech.service';
+import { QuestionService } from '../services/question/question.service';
 
 @Component({
   selector: 'app-aa-colours',
   templateUrl: './aa-colours.component.html',
   styleUrls: ['./aa-colours.component.scss']
 })
-export class AAColoursComponent implements OnInit {
+export class AAColoursComponent implements OnInit, AfterViewInit, OnDestroy {
   selectedContrast;
   contrastSettings;
   accessArrangements;
   backLinkUrl;
+  speechListenerEvent: any;
 
   constructor(
     private router: Router,
     private storageService: StorageService,
     private routeService: RouteService,
-    private pupilPrefsService: PupilPrefsService
+    private pupilPrefsService: PupilPrefsService,
+    private questionService: QuestionService,
+    private elRef: ElementRef,
+    private speechService: SpeechService
   ) {
     this.contrastSettings = AccessArrangementsConfig.contrastSettings;
     this.accessArrangements = this.storageService.getItem(accessArrangementsDataKey);
@@ -51,6 +57,29 @@ export class AAColoursComponent implements OnInit {
       this.router.navigate(['access-settings']);
     } else {
       this.router.navigate(['sign-in-success']);
+    }
+  }
+
+  // wait for the component to be rendered first, before parsing the text
+  ngAfterViewInit() {
+    if (this.questionService.getConfig().questionReader) {
+      this.speechService.speakElement(this.elRef.nativeElement).then(() => {
+        this.speechService.focusEndOfSpeech(this.elRef.nativeElement.querySelector('#next'));
+      });
+
+      this.speechListenerEvent = this.elRef.nativeElement.addEventListener('focus',
+        (event) => { this.speechService.focusEventListenerHook(event); },
+        true
+      );
+    }
+  }
+
+  ngOnDestroy(): void {
+    // stop the current speech process if the page is changed
+    if (this.questionService.getConfig().questionReader) {
+      this.speechService.cancel();
+
+      this.elRef.nativeElement.removeEventListener('focus', this.speechListenerEvent, true);
     }
   }
 }
