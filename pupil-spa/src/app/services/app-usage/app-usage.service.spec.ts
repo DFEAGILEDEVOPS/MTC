7939fa9db5548d79b54aaa6af75fd0d1eb768c27
Dan Williams <dan@ithium.net>
2018-12-06 16:06:28 +0000

    27017 - Updated AppUsageService to load counter from local storage (#919)
    * Updated AppUsageService to load counter from local storage
    
    - Load the device data from local storage in the AppUsageService constructor
    - Added a store() method to AppUsageService
    - Call store() method in SubmissionService
    - Added tests
    
    * Moved appUsageService.store to CheckCompleteService
    
    - Moved call to appUsageService.store() to the check complete
    service for compatibility when hpa is enabled
    
    * Removed loading of app usage counter from local storage
    
    * removing duplicate reference
    
    * remove duplicate ref from spec
    
    * Removed duplicated app-usage-counter code from 26288 (#914)
    
    * updated scenarios
    
    * Revert "removing duplicate reference"
    
    * remove hook

--- pupil-spa/src/app/services/app-usage/app-usage.service.spec.ts
+++ pupil-spa/src/app/services/app-usage/app-usage.service.spec.ts
@@ -1,32 +1,47 @@
 import { TestBed } from '@angular/core/testing';
 import { AppUsageService } from './app-usage.service';
+import { StorageService } from '../storage/storage.service';
 
 let appUsageService: AppUsageService;
+const mockStorageService = {
+  getItem: jasmine.createSpy('getItem'),
+  setItem: jasmine.createSpy('setItem')
+};
 
 describe('AppUsageService', () => {
-  beforeEach(() => {
-    const inject = TestBed.configureTestingModule({
+
+  beforeEach(async() => {
+
+
+    const injector = TestBed.configureTestingModule({
         providers: [
           AppUsageService,
+          { provide: StorageService, useValue: mockStorageService },
         ]
       }
     );
-    appUsageService = inject.get(AppUsageService);
+    appUsageService = injector.get(AppUsageService);
   });
+
   it('should be created', () => {
     expect(appUsageService).toBeTruthy();
   });
-  describe('#getCounterValue', () => {
-    it('should fetch the app usage counter value', () => {
-      const counter = appUsageService.getCounterValue();
-      expect(counter).toBe(0);
-    });
+
+  it('should fetch the app usage counter value', () => {
+    const counter = appUsageService.getCounterValue();
+    expect(counter).toBe(0);
   });
-  describe('#increment', () => {
-    it('should increment the app usage counter by 1', () => {
-      appUsageService.increment();
-      const counter = appUsageService.getCounterValue();
-      expect(counter).toBe(1);
-    });
+
+  it('should increment the app usage counter by 1', () => {
+    appUsageService.increment();
+    const counter = appUsageService.getCounterValue();
+    expect(counter).toBe(1);
+  });
+
+  it('should store the app usage counter', () => {
+    appUsageService.increment();
+    appUsageService.store();
+    expect(mockStorageService.setItem).toHaveBeenCalledTimes(1);
+    expect(mockStorageService.setItem).toHaveBeenCalledWith('device', {appUsageCounter: 2});
   });
 });
