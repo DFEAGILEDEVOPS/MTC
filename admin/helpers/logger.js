adce7c946de8710650ee578c445887b1140d3ca9
Jon Shipley <js@ovix.co.uk>
2018-12-16 10:45:44 +0000

    Update logging helper - express page request logging

--- admin/helpers/logger.js
+++ admin/helpers/logger.js
@@ -1,49 +1,25 @@
 'use strict'
 
-const winston = require('winston')
 const config = require('../config')
 const morgan = require('morgan')
+const Logger = require('../models/logger')
+const logger = new Logger()
 
 let initialised
 
 const init = (app) => {
   if (initialised) return
 
-  winston.level = config.Logging.LogLevel
-
   if (config.Logging.Express.UseWinston === 'true') {
     /**
      * Express logging to winston
      */
     const expressWinston = require('express-winston')
-    app.use(expressWinston.logger({
-      transports: [
-        new winston.transports.Console({
-          json: true,
-          colorize: true
-        })
-      ],
-      meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-      // msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
-      expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
-      colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
-      ignoreRoute: function (req, res) {
-        return false
-      } // optional: allows to skip some log messages based on request and/or response
-    }))
+    app.use(expressWinston.logger({ winstonInstance: logger.getLogger() }))
   } else {
     app.use(morgan('dev'))
   }
 
-  if (config.Logging.SendToAppInsights) {
-    const aiLogger = require('winston-azure-application-insights').AzureApplicationInsightsLogger
-
-    winston.add(aiLogger, {
-      key: config.Monitoring.ApplicationInsights.Key,
-      treatErrorsAsExceptions: true
-    })
-    winston.info(`app insights winston transport enabled`)
-  }
   initialised = true
 }
 
