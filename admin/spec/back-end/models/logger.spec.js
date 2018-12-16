b65ea5a8a21ca451d37900c28b2acda9abf7b213
Jon Shipley <js@ovix.co.uk>
2018-12-16 10:20:01 +0000

    Add new Logger class

---
+++ admin/spec/back-end/models/logger.spec.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const Logger = require('../../../models/logger')
+require('../../../config')
+
+describe('logger class', () => {
+    it ('can initialise', () => {
+      const logger = new Logger()
+      expect(typeof logger).toBe('object')
+    })
+    describe('has', () => {
+      let logger
+      beforeEach(() => {
+        logger = new Logger()
+      })
+
+      it('a #log function', () => {
+        expect(typeof logger.log).toBe('function')
+        logger.log('info', 'log function test at info level')
+      })
+
+      it('a #alert function', () => {
+        expect(typeof logger.alert).toBe('function')
+        logger.alert('alert level test')
+      })
+
+      it('a #error function', () => {
+        expect(typeof logger.error).toBe('function')
+        logger.error('error level test')
+      })
+
+      it('a #warn function', () => {
+        expect(typeof logger.warn).toBe('function')
+        logger.warn('warn level test')
+      })
+
+      it('a #info function', () => {
+        expect(typeof logger.info).toBe('function')
+        logger.info('info level test')
+      })
+
+      it('a #debug function', (done) => {
+        expect(typeof logger.debug).toBe('function')
+        logger.debug('debug level test')
+      })
+    })
+})
