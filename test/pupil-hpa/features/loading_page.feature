1ad2effa0d7d649ef9229f07b56c13a20987407d
Dan Williams <dan@ithium.net>
2018-12-18 16:01:18 +0000

    Added test features for idle modal and check timeout

--- test/pupil-hpa/features/loading_page.feature
+++ test/pupil-hpa/features/loading_page.feature
@@ -1,7 +1,27 @@
 @loading_page
 Feature: Loading Page
-
+  
   Scenario: Warm up questions start after a 2 second delay
+    Given I have logged in
     Given I have read the instructions
     When I choose to start the warm up questions
     Then I should have 3 seconds before I see the first question
+  
+  Scenario: Next button appears after 2 second delay
+    Given I logged in with user with access arrangement 'Next between questions'
+    Given I have read the instructions and seen the settings page
+    When I choose to start the warm up questions
+    When I have been idle for 2 seconds
+    Then I should see the next button
+  
+  Scenario: Idle modal appears after delay
+    Given I logged in with user with access arrangement 'Next between questions'
+    Given I have read the instructions and seen the settings page
+    When I choose to start the warm up questions
+    Then I should not see the modal dialog
+    Then I should see the modal dialog after the idle timeout expires
+  
+  Scenario: Redirect occurs when check timer expires 
+    Given I logged in with user with access arrangement 'Next between questions'
+    Given I have read the instructions and seen the settings page
+    Then I should be redirected when the check time limit expires
--- test/pupil-hpa/features/step_definitions/loading_page_steps.rb
+++ test/pupil-hpa/features/step_definitions/loading_page_steps.rb
@@ -1,7 +1,10 @@
 Given(/^I have read the instructions$/) do
-  step 'I have generated a live pin'
-  step 'I have logged in'
   confirmation_page.read_instructions.click
+end
+
+Given(/^I have read the instructions and seen the settings page$/) do
+  step 'I have read the instructions'
+  access_arrangements_setting_page.next_btn.click
 end
 
 When(/^I choose to start the warm up questions$/) do
@@ -12,4 +15,32 @@ end
 Then(/^I should have (\d+) seconds before I see the first question$/) do |arg1|
   check_page.wait_for_preload_to_expire
   expect(check_page).to have_question
+end
+
+Then(/^I should not see the modal dialog$/) do
+  expect(loading_page).not_to have_idle_modal
+end
+
+Then(/^I should see the modal dialog after the idle timeout expires$/) do
+  # We dont have a method of reducing the 30 second idle modal timeout, so we call showWarningModal() manually
+  page.evaluate_script("ng.probe(document.getElementsByTagName('app-warmup-loading')[0]).componentInstance.showWarningModal()")
+  expect(loading_page).to have_idle_modal
+end
+
+When(/^I have been idle for (\d+) seconds$/) do |arg1|
+  loading_page.wait_for_idle_to_expire arg1
+end
+
+Then(/^I should see the next button$/) do
+  expect(loading_page).to have_next_button
+end
+
+Then(/^I should be redirected when the check time limit expires$/) do
+  config = JSON.parse(page.evaluate_script("window.localStorage.getItem('config');"))
+  config['checkTime'] = 0
+  page.evaluate_script("window.localStorage.setItem('config', '#{JSON.generate(config)}');")
+  visit '/check'
+  step 'I have been idle for 2 seconds'
+  expected_path = '/out-of-time'
+  expect(current_path).to eql expected_path
 end
