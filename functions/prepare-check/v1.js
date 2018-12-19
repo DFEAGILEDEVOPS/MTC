f85dbf42cf5cabc0ca7c074a191fc8b2ad981fd2
Jon Shipley <js@ovix.co.uk>
2018-12-19 10:03:07 +0000

    Factor out duplicate code

--- functions/prepare-check/v1.js
+++ functions/prepare-check/v1.js
@@ -1,13 +1,11 @@
 'use strict'
 
-const R = require('ramda')
-const azure = require('azure-storage')
-const moment = require('moment')
 const azureStorageHelper = require('../lib/azure-storage-helper')
 const preparedCheckSchemaValidator = require('../lib/prepared-check-schema-validator')
 const azureTableService = azureStorageHelper.getPromisifiedAzureTableService()
-const entGen = azure.TableUtilities.entityGenerator
 const preparedCheckTable = 'preparedCheck'
+const prepareEntity = require('./prepare-entity')
+
 
 async function process (context, v1Message) {
   try {
@@ -19,23 +17,7 @@ async function process (context, v1Message) {
     throw error
   }
 
-  const entity = {
-    PartitionKey: entGen.String(v1Message.schoolPin),
-    RowKey: entGen.String('' + v1Message.pupilPin),
-    checkCode: entGen.Guid(v1Message.pupil.checkCode),
-    collectedAt: null,
-    config: entGen.String(JSON.stringify(v1Message.config)),
-    createdAt: entGen.DateTime(new Date()),
-    isCollected: entGen.Boolean(false),
-    pinExpiresAt: entGen.DateTime(moment(v1Message.pupil.pinExpiresAt).toDate()),
-    pupil: entGen.String(JSON.stringify(R.omit(['id', 'checkFormAllocationId', 'pinExpiresAt'], v1Message.pupil))),
-    pupilId: entGen.Int32(v1Message.pupil.id),
-    questions: entGen.String(JSON.stringify(v1Message.questions)),
-    school: entGen.String(JSON.stringify(v1Message.school)),
-    schoolId: entGen.Int32(v1Message.school.id),
-    tokens: entGen.String(JSON.stringify(v1Message.tokens)),
-    updatedAt: entGen.DateTime(new Date())
-  }
+  const entity = prepareEntity(v1Message)
 
   await azureTableService.insertEntityAsync(preparedCheckTable, entity)
 
