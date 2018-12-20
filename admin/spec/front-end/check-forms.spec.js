edc7f3824abe3cb047e2aec64f11649701974fb4
George Chatzigiannis <geohatzigiannis@gmail.com>
2018-12-20 11:09:50 +0000

    Feature/27244 Checkform v2 validation and submission (#932)
    * Introduced new views for submitting check form v2
    
    * remove version from routes and views
    
    * remove old routes and views
    
    * Include new check form uploading url in busboy allowed paths
    
    * change check form type input name
    
    * remove flash message and add a comment to indicate that submission work is pending
    
    * Support multiple file uploading
    
    * test fix
    
    * Promisify azure blob data service and handle multiple file uploads to azure blob storage
    
    * remove redundant async
    
    * Change controller naming conventions and route paths
    
    * Remove placeholder service as it will be introduced at a later stage
    
    * Provide new busboy allowed path and simplify names on views
    
    * delete old views
    
    * Changed naming convention to be more clear
    
    * rename blob container for admin app uploaded files
    
    * check form validation and submission for single and multiple check forms
    
    * Corrections and test updates
    
    * additional concluded tests
    
    * Separated tests to highlight case
    
    * Amend check for error messages to take config into account
    
    * Naming conventions changes
    
    * Validation and submission UI changes
    
    * Change error message property name
    
    * Functionality to delete existing familiarisation check form if new one is being submitted and view changes to support modal confirmation
    
    * Perform deletion and insertion as part of a single data service call
    
    * front end js changes to accomodate modal display when appropriate and form submission within it
    
    * removed wrong expectation
    
    * Added validation rule to prevent overwriting familiarisation form when assigned and misc changes based on review
    
    * Moved check form types const on service level
    
    * Execute soft checkform deletes when familiarisation form needs to be overwritten and revoke delete permissions
    
    * correct query to fetch only non deleted familiarisation check form
    
    * Updated successful message
    
    * ensure modal is only triggered when familiarisation type radio is checked
    
    * trim extra spaces on file content
    
    * fix failing test that expected prop change via jquery
    
    * adding phase 1 scenarios for checkform v2
    
    * Read from processed string containing file content instead of directory
    
    * Display validation error when a value within csv is null
    
    * Added word break css property for error messaged
    
    * remove incorrect closing tags
    
    * add tests for check form validation

---
+++ admin/spec/front-end/check-forms.spec.js
@@ -0,0 +1,114 @@
+'use strict'
+/* global $ describe it expect beforeEach spyOn */
+
+function initUploadCheckFormElements () {
+  const hasExistingFamiliarisationCheckFormInput = `<input type="hidden" id="hasExistingFamiliarisationCheckForm" name="hasExistingFamiliarisationCheckForm" value="" />`
+  const checkFormTypes = [{ value: 'L', name: 'Live' }, { value: 'F', name: 'Familiarisation' }]
+  const checkFormTypeInputs = checkFormTypes.map(cft => `
+                        <div class="multiple-choice">
+                            <input id="checkFormType"
+                                   type="radio"
+                                   role="radio"
+                                   aria-checked="false"
+                                   name="checkFormType"
+                                   value=${cft.value}
+                            />
+                            <label for="live-check-form">${cft.name}</label>
+                        </div>
+  `)
+
+  const htmlForm = `<form action="/check-form/upload" method="post" enctype="multipart/form-data" id="check-form-upload-form">
+            <input type="hidden" name="_csrf" value="<%= csrftoken %>" />
+            ${hasExistingFamiliarisationCheckFormInput}
+            <div class="form-group">
+                <div class="font-xsmall">
+                    <input
+                      type="file"
+                      name="csvFiles"
+                      id="csvFiles"
+                      accept=".csv"
+                      aria-label="Choose a file to upload"
+                      multiple
+                    />
+                </div>
+                <input type="reset" class="link remove-file" id="removeUploadedFile" value="Remove files" aria-label="Remove uploaded files." />
+            </div>
+            <div class="form-group" id="check-form-type">
+                <fieldset role="radiogroup">
+                    <div class="inline">
+                    ${checkFormTypeInputs}
+                    </div>
+                </fieldset>
+            </div>
+
+            <div class="form-buttons">
+                <input type="submit" class="button" id="upload-check-forms" value="Upload" />
+                <a href="/check-form/upload-and-view-forms" class="button button-secondary">Cancel</a>
+            </div>
+        </form>
+  `
+  const modalBox = `<div class="modal-overlay" id="js-modal-overlay"></div>
+    <dialog class="modal-box" id="js-modal-box" role="dialog" aria-labelledby="modal-title" tabindex="-1">
+        <div role="document">
+            <h1 id="modal-title"></h1>
+            <div class="modal-content">
+                <p>
+                <div class="modal-buttons">
+                    <span class="modal-confirm">
+                        <a href="" class="button-secondary" id="js-modal-confirmation-button" title=""></a>
+                    </span>
+                    <span class="modal-cancel">
+                        <a href="" class="button-secondary" id="js-modal-cancel-button" data-focus-back="js-modal-link" title=""></a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </dialog>
+  `
+  $(document.body).append(htmlForm)
+  $(document.body).append(modalBox)
+}
+
+describe('check-forms', function () {
+  let formSubmitSpy
+  const checkFormSubmit = $('#upload-check-forms')
+  beforeEach(() => {
+    $('body').empty()
+    initUploadCheckFormElements()
+    window.GOVUK.checkForms()
+    formSubmitSpy = spyOn($.fn, 'submit')
+    spyOn(checkFormSubmit, 'click').and.returnValue(false)
+  })
+  it('should display modal when existing familiarisation form exists and new familiarisation form is pending submission', function () {
+    $('#hasExistingFamiliarisationCheckForm').val('true')
+    const familiarisationRadioButton = $('#check-form-type').find(':input[value="F"]')
+    familiarisationRadioButton.attr('checked', true)
+    const checkFormSubmit = $('#upload-check-forms')
+    checkFormSubmit.trigger('click')
+    expect($('#js-modal-box')[0].classList[1]).toBe('show')
+    expect(formSubmitSpy).not.toHaveBeenCalled()
+  })
+  it('should not display modal when existing familiarisation form does not exists', function () {
+    $('#hasExistingFamiliarisationCheckForm').val('false')
+    const familiarisationRadioButton = $('#check-form-type').find(':input[value="F"]')
+    familiarisationRadioButton.attr('checked', true)
+    checkFormSubmit.trigger('click')
+    expect($('#js-modal-box')[0].classList[1]).toBeUndefined()
+  })
+  it('should not display modal when live check form is being submitted and existing familiarisation form exists', function () {
+    $('#hasExistingFamiliarisationCheckForm').val('true')
+    const liveRadioButton = $('#check-form-type').find(':input[value="L"]')
+    liveRadioButton.attr('checked', true)
+    checkFormSubmit.trigger('click')
+    expect($('#js-modal-box')[0].classList[1]).toBeUndefined()
+  })
+  it('should submit form when modal confirm is selected', function () {
+    $('#hasExistingFamiliarisationCheckForm').val('true')
+    const familiarisationRadioButton = $('#check-form-type').find(':input[value="F"]')
+    familiarisationRadioButton.attr('checked', true)
+    const checkFormSubmit = $('#upload-check-forms')
+    checkFormSubmit.trigger('click')
+    $('#js-modal-confirmation-button').trigger('click')
+    expect(formSubmitSpy).toHaveBeenCalled()
+  })
+})
