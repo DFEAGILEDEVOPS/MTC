a29c260758aa324a1b20cd1a1072f1b5f9a42265
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-01-18 11:26:43 +0000

    Updates as per comments

--- admin/lib/errors/check-window-v2.js
+++ admin/lib/errors/check-window-v2.js
@@ -10,7 +10,7 @@ module.exports = {
   adminStartMonthInvalidChars: 'Enter a month in 2 digits for school administration start date',
 
   adminStartYearWrong: 'Enter a valid year for school administration start date',
-  adminStartYearInvalidChars: 'Enter a valid year in 4 digits for school administration start date',
+  adminStartYearInvalidChars: 'Enter a year in 4 digits for school administration start date',
 
   adminEndDayWrong: 'Enter a valid day for school administration end date',
   adminEndDayInvalidChars: 'Enter a day in 2 digits for school administration end date',
@@ -21,23 +21,23 @@ module.exports = {
   adminEndYearWrong: 'Enter a valid year for school administration end date',
   adminEndYearInvalidChars: 'Enter a year in 4 digits for school administration end date',
 
-  familiarisationCheckStartDayWrong: 'Enter a valid day for try it out check start date',
-  familiarisationCheckStartDayInvalidChars: 'Enter a day in 2 digits for try it out check start date',
+  familiarisationCheckStartDayWrong: 'Enter a valid day for try it out start date',
+  familiarisationCheckStartDayInvalidChars: 'Enter a day in 2 digits for try it out start date',
 
-  familiarisationCheckStartMonthWrong: 'Enter a valid month for try it out check start date',
-  familiarisationCheckStartMonthInvalidChars: 'Enter a month in 2 digits for try it out check start date',
+  familiarisationCheckStartMonthWrong: 'Enter a valid month for try it out start date',
+  familiarisationCheckStartMonthInvalidChars: 'Enter a month in 2 digits for try it out start date',
 
-  familiarisationCheckStartYearWrong: 'Enter a valid year for try it out check start date',
-  familiarisationCheckStartYearInvalidChars: 'Enter a year in 4 digits for try it out check start date',
+  familiarisationCheckStartYearWrong: 'Enter a valid year for try it out start date',
+  familiarisationCheckStartYearInvalidChars: 'Enter a year in 4 digits for try it out start date',
 
-  familiarisationCheckEndDayWrong: 'Enter a valid day for try it out check end date',
-  familiarisationCheckEndDayInvalidChars: 'Enter a day in 2 digits for try it out check end date',
+  familiarisationCheckEndDayWrong: 'Enter a valid day for try it out end date',
+  familiarisationCheckEndDayInvalidChars: 'Enter a day in 2 digits for try it out end date',
 
-  familiarisationCheckEndMonthWrong: 'Enter a valid month for try it out check end date',
-  familiarisationCheckEndMonthInvalidChars: 'Enter a month in 2 digits for try it out check end date',
+  familiarisationCheckEndMonthWrong: 'Enter a valid month for try it out end date',
+  familiarisationCheckEndMonthInvalidChars: 'Enter a month in 2 digits for try it out end date',
 
-  familiarisationCheckEndYearWrong: 'Enter a valid year for try it out check end date',
-  familiarisationCheckEndYearInvalidChars: 'Enter a year in 4 digits for try it out check end date',
+  familiarisationCheckEndYearWrong: 'Enter a valid year for try it out end date',
+  familiarisationCheckEndYearInvalidChars: 'Enter a year in 4 digits for try it out end date',
 
   liveCheckStartDayWrong: 'Enter a valid day for multiplication tables check start date',
   liveCheckStartDayInvalidChars: 'Enter a day in 2 digits for multiplication tables check start date',
@@ -59,29 +59,29 @@ module.exports = {
 
   adminStartDateInThePast: 'Enter a school administration start date in the future',
   adminEndDateInThePast: 'Enter a school administration end date in the future',
-  familiarisationCheckStartDateInThePast: 'Enter a try it out check start date in the future',
-  familiarisationCheckEndDateInThePast: 'Enter a try it out check end date in the future',
+  familiarisationCheckStartDateInThePast: 'Enter a try it out start date in the future',
+  familiarisationCheckEndDateInThePast: 'Enter a try it out end date in the future',
   liveCheckStartDateInThePast: 'Enter a multiplication tables check start date in the future',
   liveCheckEndDateInThePast: 'Enter a multiplication tables check end date in the future',
 
-  adminStartDateAfterFamiliarisationCheckStartDate: 'Enter a school administration start date which is before or on the same day as t he try it out check start date',
+  adminStartDateAfterFamiliarisationCheckStartDate: 'Enter a school administration start date which is before or on the same day as the try it out start date',
   adminStartDateAfterLiveCheckStartDate: 'Enter a school administration start date which is before the multiplication tables check start date',
   adminStartDateAfterAdminEndDate: 'Enter a school administration start date which is before the administration end date',
   adminEndDateBeforeAdminStartDate: 'Enter a school administration end date which is after the administration start date',
   adminEndDateBeforeLiveCheckEndDate: 'Enter a school administration end date which is after the multiplication tables check end date',
-  adminEndDateBeforeFamiliarisationCheckEndDate: 'Enter a school administration end date which is after the try it out check end date ',
-  familiarisationCheckStartDateBeforeAdminStartDate: 'Enter a try it out check start date which is the same date or after the administration start date',
-  familiarisationCheckStartDateAfterLiveCheckStartDate: 'Enter a try it out check start date which is before the multiplication tables check start date',
-  familiarisationCheckStartDateAfterFamiliarisationCheckEndDate: 'Enter a try it out check start date which is before the try it out check end date',
-  familiarisationCheckEndDateBeforeAdminStartDate: 'Enter a try it out check end date which is after the administration start date',
-  familiarisationCheckEndDateAfterAdminEndDate: 'Enter a try it out check end date which is before the administration end date',
-  familiarisationCheckEndDateBeforeFamiliarisationCheckStartDate: 'Enter a try it out check end date which is after the try it out check start date',
-  familiarisationCheckEndDateNotEqualLiveCheckEndDate: 'Enter a try it out check end date which is the same as multiplication tables check end date',
+  adminEndDateBeforeFamiliarisationCheckEndDate: 'Enter a school administration end date which is after the try it out end date ',
+  familiarisationCheckStartDateBeforeAdminStartDate: 'Enter a try it out start date which is the same date or after the administration start date',
+  familiarisationCheckStartDateAfterLiveCheckStartDate: 'Enter a try it out start date which is before the multiplication tables check start date',
+  familiarisationCheckStartDateAfterFamiliarisationCheckEndDate: 'Enter a try it out start date which is before the try it out end date',
+  familiarisationCheckEndDateBeforeAdminStartDate: 'Enter a try it out end date which is after the administration start date',
+  familiarisationCheckEndDateAfterAdminEndDate: 'Enter a try it out end date which is before the administration end date',
+  familiarisationCheckEndDateBeforeFamiliarisationCheckStartDate: 'Enter a try it out end date which is after the try it out start date',
+  familiarisationCheckEndDateNotEqualLiveCheckEndDate: 'Enter a try it out end date which is the same as multiplication tables check end date',
   liveCheckStartDateAfterLiveCheckEndDate: 'Enter a multiplication tables check start date which is before the multiplication tables check end date',
   liveCheckStartDateBeforeAdminStartDate: 'Enter a multiplication tables check start date which is after the administration start date',
-  liveCheckStartDateBeforeFamiliarisationCheckStartDate: 'Enter a multiplication tables check start date which is after the try it out check start date',
+  liveCheckStartDateBeforeFamiliarisationCheckStartDate: 'Enter a multiplication tables check start date which is after the try it out start date',
   liveCheckEndDateBeforeLiveCheckStartDate: 'Enter a multiplication tables check end date which is after the multiplication tables check start date',
   liveCheckEndDateBeforeAdminStartDate: 'Enter a multiplication tables check end date which is after the administration start date',
   liveCheckEndDateAfterAdminEndDate: 'Enter a multiplication tables check end date which is before the administration end date',
-  liveCheckEndDateNotEqualFamiliarisationCheckEndDate: 'Enter a multiplication tables check end date which is the same as try it out check end date'
+  liveCheckEndDateNotEqualFamiliarisationCheckEndDate: 'Enter a multiplication tables check end date which is the same as try it out end date'
 }
