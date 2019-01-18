2c657d6c0aac79360e72c54394af85df5feb5db9
Amit Sinha <amit.kumar.sinha@businessthreezero.com>
2019-01-18 14:32:26 +0000

    updated test with the new content changes

--- test/admin-hpa/features/support/page_objects/school_landing_page.rb
+++ test/admin-hpa/features/support/page_objects/school_landing_page.rb
@@ -21,7 +21,7 @@ class SchoolLandingPage < SitePrism::Page
   element :hdf, 'a', text: "Headteacher's declaration form"
   element :hdf_disabled, '.heading-small', text: "Headteacher's declaration form"
   element :hdf_text, 'p', text: "Complete the headteacher's declaration form once you have submitted your pupil register"
-  element :hdf_disabled_text, 'p', text: "Complete the headteacher's declaration form after you have submitted your pupil register"
+  element :hdf_disabled_text, 'p', text: "Complete the headteacher's declaration form once you have submitted your pupil register"
   element :results, '.disabled-link', text: 'Results'
   element :results_text, 'p', text: "View pupil results"
 
