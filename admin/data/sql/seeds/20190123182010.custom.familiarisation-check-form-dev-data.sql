4014f19d360d1b37df4468eefea547c6932a68a4
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-01-24 12:58:23 +0000

    simplified familiarisation check form

--- admin/data/sql/seeds/20190123182010.custom.familiarisation-check-form-dev-data.sql
+++ admin/data/sql/seeds/20190123182010.custom.familiarisation-check-form-dev-data.sql
@@ -4,27 +4,27 @@ INSERT INTO mtc_admin.checkForm ([name], isDeleted, isLiveCheckForm, formData) V
             "f2" : 6
         },
         {
-            "f1" : 10,
+            "f1" : 1,
             "f2" : 4
         },
         {
-            "f1" : 7,
-            "f2" : 8
+            "f1" : 4,
+            "f2" : 5
         },
         {
-            "f1" : 8,
-            "f2" : 8
-        },
-        {
-            "f1" : 1,
+            "f1" : 3,
             "f2" : 2
         },
         {
-            "f1" : 11,
-            "f2" : 10
+            "f1" : 5,
+            "f2" : 2
         },
         {
-            "f1" : 9,
+            "f1" : 2,
             "f2" : 6
+        },
+        {
+            "f1" : 3,
+            "f2" : 5
         }
 ]');
