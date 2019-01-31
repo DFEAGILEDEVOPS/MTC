4c4f98e18f3a7ae8586d61ab8074ff3d90acb1a2
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-01-31 15:05:49 +0000

    Included score fields

--- admin/data/sql/migrations/20190131100745.do.create-school-score-national-score.sql
+++ admin/data/sql/migrations/20190131100745.do.create-school-score-national-score.sql
@@ -1,9 +1,10 @@
 CREATE TABLE [mtc_admin].schoolScore
 (
     id INT IDENTITY(1,1) NOT NULL,
-    school_id int NOT NULL,
-    checkWindow_id int NOT NULL,
-    CONSTRAINT [PK_schoolScore] PRIMARY KEY CLUSTERED ([id] ASC),
+    school_id INT NOT NULL,
+    checkWindow_id INT NOT NULL,
+    score DECIMAL(4,2) NOT NULL,
+    CONSTRAINT [PK_schoolScore] PRIMARY KEY CLUSTERED ([id] ASC) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON),
     CONSTRAINT [FK_schoolScore_school_id_school_id] FOREIGN KEY (school_id) REFERENCES [mtc_admin].[school](id),
     CONSTRAINT [FK_schoolScore_checkWindow_id_checkWindow_id] FOREIGN KEY (checkWindow_id) REFERENCES [mtc_admin].[checkWindow](id),
 )
@@ -11,8 +12,9 @@ CREATE TABLE [mtc_admin].schoolScore
 CREATE TABLE [mtc_admin].nationalScore
 (
     id INT IDENTITY(1,1) NOT NULL,
-    checkWindow_id int NOT NULL,
+    checkWindow_id INT NOT NULL,
     complete BIT NOT NULL DEFAULT 0,
+    score DECIMAL(4,2) NOT NULL,
     CONSTRAINT [PK_nationalScore] PRIMARY KEY CLUSTERED ([id] ASC) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON),
     CONSTRAINT [FK_nationalScore_checkWindow_id_checkWindow_id] FOREIGN KEY (checkWindow_id) REFERENCES [mtc_admin].[checkWindow](id),
 )
