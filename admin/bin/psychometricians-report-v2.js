3a8e3b75010c3fc1a775a55775f9072ec16dbfd9
Jon Shipley <jon-shipley@users.noreply.github.com>
2019-02-01 16:05:56 +0000

    Feature/28374 refactor ps report to support hpa mode for feb trial (#1002)
    * Set HPA mode to be true as default
    
    * Fix the psychometrician report
    - comment out the anomaly report to fix on a different PR
    
    * Fix unit tests
    
    * Fix: Remove `winston` and use `logger` instead
    
    * Fix: anomaly report
    
    * Enable the anomaly report
    
    * Remove duplicate line

--- admin/bin/psychometricians-report-v2.js
+++ admin/bin/psychometricians-report-v2.js
@@ -3,9 +3,7 @@
 
 require('dotenv').config()
 const fs = require('fs')
-const winston = require('winston')
-
-const poolService = require('../services/data-access/sql.pool.service')
+const logger = require('../services/log.service').getLogger()
 const commandLineArgs = require('command-line-args')
 const checkProcessingService = require('../services/check-processing.service')
 const anomalyReportCacheDataService = require('../services/data-access/anomaly-report-cache.data.service')
@@ -25,7 +23,7 @@ async function main (options) {
   try {
     if (options.forceReprocess) {
       // force the report to re-calculate the cached ps-report
-      winston.info('force detected: re-processing all checks')
+      logger.info('force detected: re-processing all checks')
       await psychometricianReportCacheDataService.sqlDeleteAll()
       await anomalyReportCacheDataService.sqlDeleteAll()
     }
@@ -33,7 +31,7 @@ async function main (options) {
       requiresProcessing = true
     }
 
-    winston.info('main: Processing the completed checks')
+    logger.info('main: Processing the completed checks')
     if (requiresProcessing) {
       await checkProcessingService.process()
     }
@@ -41,7 +39,7 @@ async function main (options) {
     const report = await psychometricianReportService.generateReport()
     const filename = 'mtc-check.csv'
     fs.writeFileSync(filename, report)
-    winston.info('Generated psychometric report: ' + filename)
+    logger.info('Generated psychometric report: ' + filename)
 
     // Also generate a filtered version of the report, suitable for our customers
     //   const scoreReport = await psychometricianReportService.generateScoreReport()
@@ -49,15 +47,12 @@ async function main (options) {
     //   fs.writeFileSync(filename2, scoreReport)
     //   winston.info('Generated score report: ' + filename2)
   } catch (error) {
-    winston.error(error)
+    logger.error(error)
   }
 }
 
-main(options)
-  .then(() => {
-    poolService.drain()
-  })
-  .catch(e => {
-    console.warn(e)
-    poolService.drain()
-  })
+;(async function () {
+    await main(options)
+    process.exit(0)
+})()
+
