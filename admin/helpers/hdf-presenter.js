804be31054b0f239441aab4ff2a28d49f37df697
Dan Williams <dan@ithium.net>
2019-02-07 12:58:57 +0000

    Updated date functions in hdfPresenter

--- admin/helpers/hdf-presenter.js
+++ admin/helpers/hdf-presenter.js
@@ -4,11 +4,11 @@ const moment = require('moment')
 const hdfPresenter = {}
 
 hdfPresenter.getResultsDate = (hdf) => {
-  return moment(hdf.checkEndDate).add(1, 'weeks').isoWeekday('Monday').set({ hour: 8, minutes: 0, seconds: 0 }) // next monday after the check ends
+  return hdf.checkEndDate.add(1, 'weeks').isoWeekday('Monday').set({ hour: 8, minutes: 0, seconds: 0 }) // next monday after the check ends
 }
 
 hdfPresenter.getCanViewResults = (resultsDate) => {
-  return moment().isAfter(resultsDate)
+  return moment().utc().isAfter(resultsDate)
 }
 
 module.exports = hdfPresenter
