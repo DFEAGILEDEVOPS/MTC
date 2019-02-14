cb752305bbd767e354a9a606f491c4efd9132f62
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-02-14 16:51:22 +0000

    Added store procedure to handle average score refresh and updated view which exludes pupils not taking the check

--- admin/data/sql/migrations/20190210134733.do.create-view-schools-average.sql
+++ admin/data/sql/migrations/20190210134733.do.create-view-schools-average.sql
@@ -1,31 +1,28 @@
 CREATE VIEW [mtc_admin].[vewSchoolsAverage] AS
 
 SELECT
+p.school_id,
+ISNULL(latestPupilCheck.checkWindow_id, 1) as checkWindowId,
+CAST(SUM(ISNULL(latestPupilCheck.mark, 0)) AS DECIMAL(5,2)) / COUNT(p.id) as schoolScore
+FROM
+  mtc_admin.pupil p INNER JOIN
+  mtc_admin.pupilStatus ps ON (ps.id = p.pupilStatus_id)
+  -- FETCH COMPLETED PUPIL CHECK WITHIN THE CHECK WINDOW PERIOD
+  LEFT OUTER JOIN
+    (SELECT
+        chk.pupil_id,
+        chk.mark,
+        chk.checkWindow_id,
+        ROW_NUMBER() OVER (PARTITION BY chk.pupil_id ORDER BY chk.id DESC) as rank
+      FROM mtc_admin.[check] chk
+      INNER JOIN mtc_admin.checkStatus cs ON (cs.id = chk.checkStatus_id)
+      INNER JOIN mtc_admin.checkWindow cw ON (cw.id = chk.checkWindow_id)
+      WHERE cs.code = 'CMP'
+      AND isLiveCheck = 1
+      AND GETUTCDATE() BETWEEN cw.checkStartDate AND cw.adminEndDate
+    ) latestPupilCheck ON p.id = latestPupilCheck.pupil_id
+WHERE
+  ps.code <> 'NOT_TAKING'
+GROUP BY
   p.school_id,
-  CAST(SUM(
-      CASE
-      WHEN (pa.id IS NOT NULL) THEN 0
-      WHEN (latestPupilCheck.mark IS NOT NULL) THEN latestPupilCheck.mark
-      ELSE 0
-      END
-  ) AS DECIMAL(5,2)) / COUNT(p.id) AS [schoolAverage]
-FROM mtc_admin.pupil p
--- FETCH COMPLETED PUPIL CHECK WITHIN THE CHECK WINDOW PERIOD
-LEFT OUTER JOIN
-(SELECT chk2.id, chk2.pupil_id, chk2.mark, ROW_NUMBER() OVER ( PARTITION BY chk2.pupil_id ORDER BY chk2.markedAt DESC ) as rank
-  FROM mtc_admin.[check] chk2
-  INNER JOIN mtc_admin.checkStatus cs ON cs.id = chk2.checkStatus_id
-  INNER JOIN mtc_admin.checkWindow cw ON cw.id = chk2.checkWindow_id
-  WHERE cs.code = 'CMP'
-        AND GETUTCDATE() BETWEEN cw.checkStartDate AND cw.adminEndDate
-) latestPupilCheck
-    ON p.id = latestPupilCheck.pupil_id
-LEFT JOIN mtc_admin.pupilAttendance pa
-      ON (p.id = pa.pupil_id AND pa.isDeleted = 0)
-LEFT JOIN mtc_admin.attendanceCode ac
-  ON pa.attendanceCode_id = ac.id
--- FETCH PUPILS WITH ATTENDANCE CODES OTHER THAN LEFT AND INCORRECT REGISTRATION
-WHERE (ac.code IS NULL OR ac.code NOT IN ('LEFTT', 'INCRG'))
--- PICK THE PUPILS WITH THE LATEST COMPLETED CHECKS AND THE ONES WITH NO COMPLETED LIVE CHECK RECORDS
-AND (latestPupilCheck.rank = 1 OR latestPupilCheck.rank IS NULL)
-GROUP BY p.school_id
+  ISNULL(latestPupilCheck.checkWindow_id, 1);
