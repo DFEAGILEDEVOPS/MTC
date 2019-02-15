6c77d2dd846d5cc4fcbe3f3c78ee88ecc0aef4be
activemq <m-qureshi@hotmail.com>
2019-02-15 13:54:18 +0000

    update copy

--- admin/lib/errors/access-arrangements.js
+++ admin/lib/errors/access-arrangements.js
@@ -4,7 +4,7 @@ module.exports = {
   missingPupilName: 'Enter a pupil name',
   missingAccessArrangements: 'Select at least one access arrangement',
   missingInputAssistanceExplanation: 'Enter an explanation for input assistance',
-  missingNextButtonExplanation: 'Enter an explanation for next between questions',
+  missingNextButtonExplanation: 'Enter an explanation for next button between questions',
   missingScreenReaderExplanation: 'Enter an explanation for question reader',
   missingQuestionReaderReason: 'Select a reason'
 }
