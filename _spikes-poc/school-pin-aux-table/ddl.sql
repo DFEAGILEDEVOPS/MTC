332ab64968e57f0474d630775aae78a6bf8a0f78
Jon Shipley <js@ovix.co.uk>
2019-02-16 18:21:34 +0000

    Generate schoolPins using an SQL migration and a word list base table

--- _spikes-poc/school-pin-aux-table/ddl.sql
+++ _spikes-poc/school-pin-aux-table/ddl.sql
@@ -8,11 +8,95 @@ CREATE TABLE [mtc_admin].[schoolPin]
     [schoolPin] [nvarchar](12)       NULL,
     [school_id] [int]                NULL,
     CONSTRAINT [PK_schoolPin] PRIMARY KEY CLUSTERED ([id] ASC)
-      WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON),
+      WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = ON, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON),
     CONSTRAINT [IX_schoolPin_schoolPin_unique] UNIQUE (schoolPin),
     CONSTRAINT [FK_schoolPin_school_id_school_id] FOREIGN KEY (school_id) REFERENCES [mtc_admin].[school](id)
   );
 GO
+
+DROP TABLE IF EXISTS [mtc_admin].[wordlist];
+CREATE TABLE [mtc_admin].[wordlist]
+(
+  word        [nvarchar](6) NOT NULL,
+  [createdAt] [datetimeoffset](3)  NOT NULL DEFAULT GETUTCDATE(),
+  [updatedAt] [datetimeoffset](3)  NOT NULL DEFAULT GETUTCDATE(),
+  [version]   [rowversion],
+  CONSTRAINT  [PK_wordlist] PRIMARY KEY CLUSTERED ([word] ASC)
+      WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = ON, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON),
+);
+GO
+
+--
+-- Generate 128 random words to use as test words in lower environments
+--
+BEGIN
+    declare @alphabet varchar(26) = 'abcdefghijklmnopqrstuvwxyz';
+    declare @numInserts int = 0
+    begin
+        while @numInserts < 129
+        begin
+            insert into [mtc_admin].[wordlist] (word)
+            select substring(@alphabet, convert(int, ceiling(rand() * 26)), 1) +
+                   substring(@alphabet, convert(int, ceiling(rand() * 26)), 1) +
+                   substring(@alphabet, convert(int, ceiling(rand() * 26)), 1);
+            set @numInserts = @numInserts + 1
+        end
+    end
+end
+
+--   --
+--   -- small list to generate 25K records for comparison with existing migration
+--   --
+-- INSERT INTO mtc_admin.wordlist (word) values ('aaa'),
+--                                              ('bcd'),
+--                                              ('dcd'),
+--                                              ('tfg'),
+--                                              ('bxx'),
+--                                              ('foo'),
+--                                              ('bar'),
+--                                              ('baz'),
+--                                              ('zab'),
+--                                              ('rab'),
+--                                              ('cet'),
+--                                              ('rep'),
+--                                              ('waf'),
+--                                              ('cdr'),
+--                                              ('str'),
+--                                              ('piq'),
+--                                              ('sah'),
+--                                              ('inn'),
+--                                              ('tej'),
+--                                              ('tyl');
+
+
+--
+-- Generate all school pins available from the base word list
+-- V2 *** 300ms <-> 6s  for 1 million entries ***  select cartesian products including all numbers
+BEGIN
+  WITH x AS
+         (
+           select
+             w1.word as w1,
+             digits.num as digits,
+             w2.word as w2
+           FROM
+             mtc_admin.wordlist w1 cross join
+             mtc_admin.wordlist w2 cross join
+             (select
+                  A.col + B.col [num]
+              FROM
+                (values ('2'), ('3'), ('4'), ('5'), ('6'), ('7'), ('8'), ('9')) as A(col) cross join
+                (values ('2'), ('3'), ('4'), ('5'), ('6'), ('7'), ('8'), ('9')) as B(col)) as digits
+           where
+             w1.word is not null and w2.word is not null
+             AND w1.word <> w2.word
+         )
+  INSERT INTO mtc_admin.schoolPin (schoolPin)
+  SELECT x.w1 + x.digits + x.w2
+  FROM x;
+END
+
+
 
 --
 -- Add a unique index on the schoolPin.school_id field to ensure a school can only get
@@ -85,4 +169,3 @@ BEGIN CATCH
         );
 END CATCH
 GO
-
