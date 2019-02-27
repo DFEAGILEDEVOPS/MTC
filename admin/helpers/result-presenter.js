af222473046e40ae6f57f299bcb17a0d2133d1e4
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-02-27 08:23:35 +0000

    jsdoc fix

--- admin/helpers/result-presenter.js
+++ admin/helpers/result-presenter.js
@@ -24,7 +24,7 @@ resultPresenter.getResultsViewData = (pupils) => {
 }
 
 /**
- * Returns national score with one decimal place
+ * Returns score with one decimal place
  * @param {Number} score
  * @returns {Number}
  */
