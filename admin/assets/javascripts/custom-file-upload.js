2f034644c0a340ec5d7da9d2736fe423e6778bef
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-02-28 11:22:21 +0000

    Prevent upload button from being changed  or initiated to disabled state in all upload  forms

--- admin/assets/javascripts/custom-file-upload.js
+++ admin/assets/javascripts/custom-file-upload.js
@@ -8,13 +8,11 @@ $(function () {
   function customFileUpload (e) {
     var $formElement = $('#upload-form')
     var $removeElement = $('#removeUploadedFile')
-    var $uploadButton = $('#upload-form-submit')
     var isSubmitted = false
 
     $removeElement.on('click', function (e) {
       $removeElement.css('visibility', 'hidden')
       $removeElement.hide()
-      $uploadButton.attr('disabled', 'disabled')
     })
 
     $('input:file').change(function (e) {
@@ -24,7 +22,6 @@ $(function () {
       }
       $removeElement.css('visibility', 'visible')
       $removeElement.show()
-      $uploadButton.attr('disabled', false)
       var $fileName = $(this).val()
       $('.filename').html($fileName)
     })
