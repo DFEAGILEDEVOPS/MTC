ac87990ee6f89fd6fe20b2cfaba7b0419d8aed7a
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-03-01 11:57:39 +0000

    Age reason is required only if the date of birth provided is valid

--- admin/lib/validator/pupil-validator.js
+++ admin/lib/validator/pupil-validator.js
@@ -90,8 +90,8 @@ module.exports.validate = async (pupilData, isMultiplePupilsSubmission = false) 
     validationError.addError('dob-year', addPupilErrorMessages.dobOutOfRange)
   }
   // Age Reason
-  const requiredAgeReasonValidation = dob.isBetween(moment(`${academicYear - 11}-09-02`), moment(`${academicYear - 10}-09-01`), null, '[]') ||
-    dob.isBetween(moment(`${academicYear - 8}-09-02`), moment(`${academicYear - 7}-09-01`), null, '[]')
+  const requiredAgeReasonValidation = dob.isValid() && (dob.isBetween(moment(`${academicYear - 11}-09-02`), moment(`${academicYear - 10}-09-01`), null, '[]') ||
+    dob.isBetween(moment(`${academicYear - 8}-09-02`), moment(`${academicYear - 7}-09-01`), null, '[]'))
   if (isMultiplePupilsSubmission && requiredAgeReasonValidation) {
     validationError.addError('dob-day', addPupilErrorMessages.dobMultipleRequiresReason)
     validationError.addError('dob-month', addPupilErrorMessages.dobMultipleRequiresReason)
