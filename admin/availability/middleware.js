999d218311989335cf55864cff7fc4cf062f9e91
Dan Williams <dan@ithium.net>
2019-03-06 17:25:05 +0000

    Updated checkwindow dates to format only when checkWindow exists

--- admin/availability/middleware.js
+++ admin/availability/middleware.js
@@ -11,15 +11,16 @@ async function isAdminWindowAvailable (req, res, next) {
   }
   const currentDate = moment.utc()
   const checkWindow = await checkWindowV2Service.getActiveCheckWindow()
-  if (!checkWindow || !currentDate.isBetween(checkWindow.adminStartDate, checkWindow.adminEndDate)) {
-    res.locals.pageTitle = 'Unavailable'
-    return res.render('availability/admin-window-unavailable', {
-      currentDate,
-      checkWindow,
-      startDateFormatted: dateService.formatDayDateAndYear(checkWindow.adminStartDate)
-    })
+  if (checkWindow && currentDate.isBetween(checkWindow.adminStartDate, checkWindow.adminEndDate)) {
+    return next()
   }
-  return next()
+  const isBeforeStartDate = checkWindow && currentDate.isBefore(checkWindow.adminStartDate)
+  const startDateFormatted = checkWindow && dateService.formatDayDateAndYear(checkWindow.adminStartDate)
+  res.locals.pageTitle = 'Unavailable'
+  return res.render('availability/admin-window-unavailable', {
+    isBeforeStartDate,
+    startDateFormatted
+  })
 }
 
 module.exports = isAdminWindowAvailable
