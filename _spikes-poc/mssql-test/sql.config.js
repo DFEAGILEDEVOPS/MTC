2068f5f65a9955514dc83061defd2d4db1088fc8
Jon Shipley <js@ovix.co.uk>
2019-03-15 17:23:57 +0000

    Add a small test app to spike the behaviour of mssql under load

---
+++ _spikes-poc/mssql-test/sql.config.js
@@ -0,0 +1,24 @@
+'use strict'
+
+require('dotenv').config()
+const toBool = require('to-bool')
+
+const oneMinuteInMilliseconds = 60000
+
+module.exports = {
+  database: process.env.SQL_DATABASE || 'mtc',
+  server: process.env.SQL_SERVER || 'localhost',
+  port: process.env.SQL_PORT || 1433,
+  requestTimeout: process.env.SQL_TIMEOUT || oneMinuteInMilliseconds,
+  connectionTimeout: process.env.SQL_TIMEOUT || oneMinuteInMilliseconds,
+  user: process.env.SQL_APP_USER || 'mtcAdminUser', // docker default
+  password: process.env.SQL_APP_USER_PASSWORD || 'your-chosen*P4ssw0rd_for_dev_env!', // docker default
+  pool: {
+    min: process.env.SQL_POOL_MIN_COUNT || 0,
+    max: process.env.SQL_POOL_MAX_COUNT || 20
+  },
+  options: {
+    appName: process.env.SQL_APP_NAME || 'mtc-local-dev', // docker default
+    encrypt: process.env.hasOwnProperty('SQL_ENCRYPT') ? toBool(process.env.SQL_ENCRYPT) : true
+  }
+}
