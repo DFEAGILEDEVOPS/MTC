b7ef985a92bb5be2d39e315e95ee351916507eca
Jon Shipley <js@ovix.co.uk>
2019-03-18 09:28:43 +0000

    Allow the app to start without the database
    If the initial probe to the database fails, then the app will wait for 30 seconds and then exit.  This will be picked up by the PM2 node
    process manager which will restart the app.  The wait time allows the db to come up, and keeps control even when the db is unavailable.

--- admin/app.js
+++ admin/app.js
@@ -32,7 +32,34 @@ const logger = require('./services/log.service').getLogger()
 const sqlService = require('./services/data-access/sql.service')
 const app = express()
 setupLogging(app)
-sqlService.initPool()
+
+/**
+ * Sleep in ms
+ * @param ms - milliseconds
+ * @return {Promise}
+ */
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
+  })
+}
+
+// Initialise the Database Connection pool
+;(async function () {
+    try {
+      logger.debug('Attempting to initialise the connection pool')
+      await sqlService.initPool()
+    } catch (error) {
+      logger.alert('Failed to connect to the database', error)
+      // The initial probe connection was not able to connection: the DB is not available.  This will cause all
+      // connections in the connection pool to be initialised to closed connections. By pausing, we allow time for the
+      // db to become available.  When run in a docker container the PM2 process manager will restart the process, and
+      // hopefully the DB will be up by then.
+      await sqlService.drainPool()
+      await sleep(30000)
+      process.exit(1)
+    }
+})()
 
 /**
  * Load feature toggles
