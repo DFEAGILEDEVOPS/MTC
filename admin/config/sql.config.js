ab5ad7339e38976ea8d18dd4786e189d87280781
Jon Shipley <js@ovix.co.uk>
2019-03-19 18:37:05 +0000

    Force conversion of SQL TIMEOUT to an INT

--- admin/config/sql.config.js
+++ admin/config/sql.config.js
@@ -9,8 +9,8 @@ module.exports = {
   database: process.env.SQL_DATABASE || 'mtc',
   server: process.env.SQL_SERVER || 'localhost',
   port: process.env.SQL_PORT || 1433,
-  requestTimeout: process.env.SQL_TIMEOUT || oneMinuteInMilliseconds,
-  connectionTimeout: process.env.SQL_TIMEOUT || oneMinuteInMilliseconds,
+  requestTimeout:  parseInt(process.env.SQL_TIMEOUT, 10) || oneMinuteInMilliseconds,
+  connectionTimeout: parseInt(process.env.SQL_TIMEOUT, 10) || oneMinuteInMilliseconds,
   user: process.env.SQL_APP_USER || 'mtcAdminUser', // docker default
   password: process.env.SQL_APP_USER_PASSWORD || 'your-chosen*P4ssw0rd_for_dev_env!', // docker default
   pool: {
