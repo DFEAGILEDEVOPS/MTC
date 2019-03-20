1cc580cd3737188f23b34c43ef862c38a1397514
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-03-20 16:43:07 +0000

    Grant create table permission on user fix

--- admin/data/sql/migrations/20190320145618.do.grant-census-import-user-permissions.js
+++ admin/data/sql/migrations/20190320145618.do.grant-census-import-user-permissions.js
@@ -4,7 +4,10 @@ const config = require('../../../config')
 
 module.exports.generateSql = function () {
   if (config.Sql.Azure.Scale) {
-    return `GRANT UPDATE,INSERT,CREATE TABLE ON schema::[mtc_admin] to [${config.Sql.PupilCensus.Username}];`
+    return `
+    GRANT CREATE TABLE TO [${config.Sql.PupilCensus.Username}];
+    GRANT INSERT,UPDATE ON schema::[mtc_admin] TO [${config.Sql.PupilCensus.Username}];
+    `
   } else {
     return ''
   }
--- admin/data/sql/migrations/20190320145618.undo.grant-census-import-user-permissions.js
+++ admin/data/sql/migrations/20190320145618.undo.grant-census-import-user-permissions.js
@@ -4,7 +4,10 @@ const config = require('../../../config')
 
 module.exports.generateSql = function () {
   if (config.Sql.Azure.Scale) {
-    return `REVOKE UPDATE,INSERT,CREATE TABLE ON schema::[mtc_admin] to [${config.Sql.PupilCensus.Username}];`
+    return `
+    REVOKE CREATE TABLE TO [${config.Sql.PupilCensus.Username}];
+    REVOKE INSERT,UPDATE ON schema::[mtc_admin] TO [${config.Sql.PupilCensus.Username}];
+    `
   } else {
     return ''
   }
