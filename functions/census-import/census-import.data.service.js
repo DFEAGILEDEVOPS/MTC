09b6d01ee2d0c5ae5c3cf0345e7a8cb9a13d15a7
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-03-21 07:48:53 +0000

    Insert census table data to pupil table

--- functions/census-import/census-import.data.service.js
+++ functions/census-import/census-import.data.service.js
@@ -1,16 +1,16 @@
-const moment = require('moment')
 const mssql = require('mssql')
-const uuidv4 = require('uuid/v4')
+const sqlService = require('less-tedious')
 
 const config = require('../config')
 
 /**
  * Create census import table
  * @param {Object} context
+ * @param {String} censusTable
  * @param {Array} blobContent
  * @return {Object}
  */
-module.exports.sqlCreateCensusImportTable = async (context, blobContent) => {
+module.exports.sqlCreateCensusImportTable = async (context, censusTable, blobContent) => {
   const poolConfig = {
     database: config.Sql.Database,
     server: config.Sql.Server,
@@ -35,7 +35,7 @@ module.exports.sqlCreateCensusImportTable = async (context, blobContent) => {
   })
   await pool.connect()
 
-  const table = new mssql.Table(`[mtc_admin].[census-import-${moment.utc().format('YYYYMMDDHHMMSS')}-${uuidv4()}]`)
+  const table = new mssql.Table(censusTable)
   table.create = true
   table.columns.add('id', mssql.Int, { nullable: false, primary: true, identity: true })
   table.columns.add('lea', mssql.NVarChar(mssql.MAX), { nullable: false })
@@ -52,6 +52,25 @@ module.exports.sqlCreateCensusImportTable = async (context, blobContent) => {
   }
 
   const request = new mssql.Request(pool)
-  const result = await request.bulk(table)
-  return result.rowsAffected
+  await request.bulk(table)
+}
+
+module.exports.sqlInsertCensusImportTableData = async (context, censusTable) => {
+  const sql = `
+  INSERT INTO [mtc_admin].pupil
+  (school_id, foreName, middleNames, lastName, gender, dateOfBirth, upn)
+  SELECT
+    s.id,
+    tmp.forename,
+    tmp.middlenames,
+    tmp.surname,
+    tmp.gender,
+    CONVERT(DATETIMEOFFSET(3), tmp.dob, 103),
+    tmp.upn
+  FROM ${censusTable} tmp
+  INNER JOIN mtc_admin.school s
+    ON s.dfeNumber = CONVERT(INT, CONCAT(tmp.lea, tmp.estab))
+  `
+  const result = await sqlService.modify(sql)
+  return result.rowsModified
 }
--- functions/census-import/v1.js
+++ functions/census-import/v1.js
@@ -1,19 +1,23 @@
 'use strict'
 const csvString = require('csv-string')
+const moment = require('moment')
+const uuidv4 = require('uuid/v4')
 
 const censusImportDataService = require('./census-import.data.service')
 
 const v1 = {
   process: async function (context, blob) {
-    const rowsAffected = await this.handleCensusImport(context, blob)
+    const rowsModified = await this.handleCensusImport(context, blob)
     return {
-      processCount: rowsAffected
+      processCount: rowsModified
     }
   },
 
   handleCensusImport: async function (context, blob) {
     const blobContent = csvString.parse(blob.toString())
-    return censusImportDataService.sqlCreateCensusImportTable(context, blobContent)
+    const censusTable = `[mtc_admin].[census-import-${moment.utc().format('YYYYMMDDHHMMSS')}-${uuidv4()}]`
+    await censusImportDataService.sqlCreateCensusImportTable(context, censusTable, blobContent)
+    return censusImportDataService.sqlInsertCensusImportTableData(context, censusTable)
   }
 }
 
