d3110fcc29343e193198f1b630117d2c1fa849a8
Jon Shipley <js@ovix.co.uk>
2019-03-21 08:52:02 +0000

    Lint fix

--- admin/data/sql/migrations/20190320145618.do.grant-census-import-user-permissions.js
+++ admin/data/sql/migrations/20190320145618.do.grant-census-import-user-permissions.js
@@ -3,7 +3,7 @@
 const config = require('../../../config')
 
 module.exports.generateSql = function () {
-    return `
+  return `
     GRANT CREATE TABLE TO [${config.Sql.PupilCensus.Username}];
     GRANT SELECT,UPDATE,INSERT,EXECUTE ON schema::[mtc_admin] TO [${config.Sql.PupilCensus.Username}];
     `
--- admin/data/sql/migrations/20190320145618.undo.grant-census-import-user-permissions.js
+++ admin/data/sql/migrations/20190320145618.undo.grant-census-import-user-permissions.js
@@ -3,7 +3,7 @@
 const config = require('../../../config')
 
 module.exports.generateSql = function () {
-    return `
+  return `
     REVOKE CREATE TABLE TO [${config.Sql.PupilCensus.Username}];
     REVOKE SELECT,UPDATE,INSERT,EXECUTE  ON schema::[mtc_admin] TO [${config.Sql.PupilCensus.Username}];
     `
