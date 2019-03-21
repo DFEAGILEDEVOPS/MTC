6239a5f526353f2b8ae2e2160c94f688fe8e8778
Jon Shipley <js@ovix.co.uk>
2019-03-21 14:45:31 +0000

    new migration

--- admin/data/sql/migrations/20190320145618.do.grant-census-import-user-permissions.js
+++ admin/data/sql/migrations/20190320145618.do.grant-census-import-user-permissions.js
@@ -3,13 +3,12 @@
 const config = require('../../../config')
 
 module.exports.generateSql = function () {
-  return `
-    CREATE SCHEMA [mtc_census_import];
-    GO    
-    GRANT CONTROL ON schema::[mtc_census_import] TO [${config.Sql.PupilCensus.Username}];
-    GO
+  if (config.Sql.Azure.Scale) {
+    return `
     GRANT CREATE TABLE TO [${config.Sql.PupilCensus.Username}];
-    GO        
-    GRANT UPDATE,INSERT ON [mtc_admin].[pupil] TO [${config.Sql.PupilCensus.Username}];    
+    GRANT INSERT,UPDATE ON schema::[mtc_admin] TO [${config.Sql.PupilCensus.Username}];
     `
+  } else {
+    return ''
+  }
 }
--- admin/data/sql/migrations/20190320145618.undo.grant-census-import-user-permissions.js
+++ admin/data/sql/migrations/20190320145618.undo.grant-census-import-user-permissions.js
@@ -3,24 +3,12 @@
 const config = require('../../../config')
 
 module.exports.generateSql = function () {
-  return `
+  if (config.Sql.Azure.Scale) {
+    return `
     REVOKE CREATE TABLE TO [${config.Sql.PupilCensus.Username}];
-    REVOKE UPDATE,INSERT ON [mtc_admin].[pupil] TO [${config.Sql.PupilCensus.Username}];
-    GO
-    REVOKE CONTROL ON schema::[mtc_census_import] TO [${config.Sql.PupilCensus.Username}];
-    GO
-    
-    DECLARE @sql NVARCHAR(4000) = ''
-
-    SELECT @sql += 'DROP TABLE ' + '[mtc_census_import].' + QUOTENAME(table_name) + ';'
-    from information_schema.tables
-    where table_schema = 'mtc_census_import'
-    and table_type = 'BASE TABLE'
-    
-    -- PRINT @sql
-    Exec sp_executesql @sql
-    GO
-
-    DROP SCHEMA [mtc_census_import];
+    REVOKE INSERT,UPDATE ON schema::[mtc_admin] TO [${config.Sql.PupilCensus.Username}];
     `
+  } else {
+    return ''
+  }
 }
