e4be69bf28fbba41e2467be1f1f5b333fb72b796
Jon Shipley <js@ovix.co.uk>
2019-03-21 20:57:46 +0000

    Update upsert query to use 'WITH'

--- admin/data/sql/migrations/20190321102921.do.pupil-census-upsert.sql
+++ admin/data/sql/migrations/20190321102921.do.pupil-census-upsert.sql
@@ -3,32 +3,37 @@ CREATE PROCEDURE [mtc_admin].[spPupilCensusUpsert]
     @censusImportTable AS censusImportTableType READONLY
   )
 AS
+  WITH Source
+    (school_id, foreName, lastName, middlenames, gender, dateOfBirth, upn)
+    AS
+    (
+      SELECT
+            school.id, census.foreName, census.surname, census.middlenames, census.gender, census.dob, census.upn
+      FROM @censusImportTable as census JOIN
+        [mtc_admin].[school] school ON (school.dfeNumber = CONVERT(INT, CONCAT(census.lea, census.estab)))
+    )
   MERGE INTO
     mtc_admin.pupil AS Target
   USING
-    @censusImportTable AS Source
+    Source
   ON Target.upn = Source.upn
   WHEN MATCHED THEN
     UPDATE SET
-      Target.school_id   = (SELECT id
-                            FROM mtc_admin.school
-                            WHERE dfeNumber = CONVERT(INT, CONCAT(Source.lea, Source.estab))),
+      Target.school_id   = Source.school_id,
       Target.foreName    = Source.forename,
-      Target.lastName    = Source.surname,
+      Target.lastName    = Source.lastName,
       Target.middleNames = Source.middlenames,
       Target.gender      = Source.gender,
-      Target.dateOfBirth = CONVERT(DATETIMEOFFSET(3), Source.dob, 103)
+      Target.dateOfBirth = CONVERT(DATETIMEOFFSET(3), Source.dateOfBirth, 103)
   WHEN NOT MATCHED BY TARGET THEN
     INSERT (school_id, foreName, middleNames, lastName, gender, dateOfBirth, upn)
     VALUES
       (
-        (SELECT id
-         FROM mtc_admin.school
-         WHERE dfeNumber = CONVERT(INT, CONCAT(Source.lea, Source.estab))),
+        Source.school_id,
         Source.forename,
         Source.middlenames,
-        Source.surname,
+        Source.lastName,
         Source.gender,
-        CONVERT(DATETIMEOFFSET(3), Source.dob, 103),
+        CONVERT(DATETIMEOFFSET(3), Source.dateOfBirth, 103),
         Source.upn
       );
