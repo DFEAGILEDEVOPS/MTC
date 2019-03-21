e3271664195c341cfef4fe7e00e7a14760c1af9d
Jon Shipley <js@ovix.co.uk>
2019-03-21 12:06:33 +0000

    Update perms

--- admin/data/sql/migrations/20190320145618.do.grant-census-import-user-permissions.js
+++ admin/data/sql/migrations/20190320145618.do.grant-census-import-user-permissions.js
@@ -4,7 +4,12 @@ const config = require('../../../config')
 
 module.exports.generateSql = function () {
   return `
+    CREATE SCHEMA [mtc_census_import];
+    GO    
+    GRANT CONTROL ON schema::[mtc_census_import] TO [${config.Sql.PupilCensus.Username}];
+    GO
     GRANT CREATE TABLE TO [${config.Sql.PupilCensus.Username}];
-    GRANT SELECT,UPDATE,INSERT,EXECUTE ON schema::[mtc_admin] TO [${config.Sql.PupilCensus.Username}];
+    GO        
+    GRANT UPDATE,INSERT ON [mtc_admin].[pupil] TO [${config.Sql.PupilCensus.Username}];    
     `
 }
