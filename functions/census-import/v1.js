e8ca60c42e4a92130ccf9c14c74d399058b38492
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-03-23 12:48:44 +0000

    Update job status to processing once it lands in the function

--- functions/census-import/v1.js
+++ functions/census-import/v1.js
@@ -1,9 +1,11 @@
 'use strict'
 const csvString = require('csv-string')
 const moment = require('moment')
+const R = require('ramda')
 const uuidv4 = require('uuid/v4')
 
 const censusImportDataService = require('./census-import.data.service')
+const jobDataService = require('./job.data.service')
 
 const v1 = {
   process: async function (context, blob) {
@@ -14,6 +16,10 @@ const v1 = {
   },
 
   handleCensusImport: async function (context, blob) {
+    const jobUrlSlug = R.compose(arr => arr[arr.length - 1], r => r.split('/'))(context.bindingData.uri)
+    // Update job status to Processing
+    await jobDataService.sqlUpdateStatus(jobUrlSlug, 'PRC')
+
     const blobContent = csvString.parse(blob.toString())
     const censusTable = `[mtc_census_import].[census_import_${moment.utc().format('YYYYMMDDHHMMSS')}_${uuidv4()}]`
     const result = await censusImportDataService.sqlLoadStagingTable(context, censusTable, blobContent)
