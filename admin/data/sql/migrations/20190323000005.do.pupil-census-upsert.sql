5fe6fab83d420d74d8e31c40d88d7e6f29d227b7
Jon Shipley <js@ovix.co.uk>
2019-03-23 17:14:17 +0000

    Remove outer transaction

--- admin/data/sql/migrations/20190323000005.do.pupil-census-upsert.sql
+++ admin/data/sql/migrations/20190323000005.do.pupil-census-upsert.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE [mtc_census_import].[spPupilCensusImportFromStaging]
+ALTER PROCEDURE [mtc_census_import].[spPupilCensusImportFromStaging]
   (
     @censusImportTable AS censusImportTableType READONLY
   )
@@ -27,8 +27,6 @@ AS
   DECLARE @errorCount INT = 0
   DECLARE @errorText NVARCHAR(max) = ''
 
-  BEGIN TRY
-  BEGIN TRANSACTION
 
   -- Delete all existing pupils not registered to test schools
   DELETE FROM [mtc_admin].[pupil]
@@ -50,38 +48,12 @@ AS
     END TRY
     BEGIN CATCH
         SET @errorCount += 1;
-        PRINT 'ERROR dob is ' + CONVERT(VARCHAR, @dateOfBirth)
     END CATCH
     FETCH Source INTO @schoolId, @foreName, @middleNames, @lastName, @gender, @dateOfBirth, @upn
   END
 
   SELECT @insertCount as insertCount, @errorCount as errorCount, @errorText as errorText;
-  COMMIT TRANSACTION
 
   CLOSE Source
   DEALLOCATE Source
-
-  END TRY
-  BEGIN CATCH
-  IF (@@TRANCOUNT > 0)
-    BEGIN
-      ROLLBACK TRANSACTION
-      PRINT 'Error detected, all changes reversed'
-    END
-  DECLARE @ErrorMessage NVARCHAR(4000);
-  DECLARE @ErrorSeverity INT;
-  DECLARE @ErrorState INT;
-
-  SELECT @ErrorMessage = ERROR_MESSAGE(),
-         @ErrorSeverity = ERROR_SEVERITY(),
-         @ErrorState = ERROR_STATE();
-
-  -- Use RAISERROR inside the CATCH block to return
-  -- error information about the original error that
-  -- caused execution to jump to the CATCH block.
-  RAISERROR (@ErrorMessage, -- Message text.
-    @ErrorSeverity, -- Severity.
-    @ErrorState -- State.
-    );
-  END CATCH
 ;
