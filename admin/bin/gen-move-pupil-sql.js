8513c91483be1029b4119751c16dc70360573c9a
Jon Shipley <js@ovix.co.uk>
2019-04-02 10:27:10 +0100

    Remove lint

--- admin/bin/gen-move-pupil-sql.js
+++ admin/bin/gen-move-pupil-sql.js
@@ -3,8 +3,6 @@
 'use strict'
 const csvString = require('csv-string')
 const fs = require('fs')
-const sqlService = require('../services/data-access/sql.service')
-const config = require('../config')
 const csvFile = process.argv[2]
 
 if (!csvFile) {
@@ -12,13 +10,13 @@ if (!csvFile) {
   process.exit(1)
 }
 
-function generateSql(targetDfeNumber, pupilUpn) {
+function generateSql (targetDfeNumber, pupilUpn) {
   return `UPDATE [mtc_admin].[pupil] 
           SET school_id = (SELECT id from [mtc_admin].[school] where dfeNumber = ${targetDfeNumber})        
           WHERE upn = '${pupilUpn}';`
 }
 
-async function main() {
+async function main () {
   const fileData = fs.readFileSync(csvFile).toString()
   const arr = csvString.parse(fileData)
   const sql = []
@@ -27,15 +25,13 @@ async function main() {
     if (row[0].match(/Date/)) {
       return
     }
-    const currentDfeNumber = row[6]
     const targetDfeNumber = row[4]
     const pupilUpn = row[7].replace(/[^a-zA-Z0-9]/g, '')
     const s = generateSql(targetDfeNumber, pupilUpn)
     sql.push(s)
   })
-  console.log(sql.join("\n"))
+  console.log(sql.join('\n'))
 }
-
 
 ;(async function () {
   try {
