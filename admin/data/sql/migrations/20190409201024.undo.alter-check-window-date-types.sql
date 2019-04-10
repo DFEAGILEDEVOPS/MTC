7a57c658de5ff0ff91d699dd902811405158c184
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-04-10 12:55:55 +0100

    Added migrations to convert check window start and end date columns to datetime2 type

---
+++ admin/data/sql/migrations/20190409201024.undo.alter-check-window-date-types.sql
@@ -0,0 +1,30 @@
+DECLARE @columnName VARCHAR(max)
+DECLARE @sql NVARCHAR(max)
+
+DECLARE cur CURSOR FOR
+ SELECT COLUMN_NAME
+   FROM INFORMATION_SCHEMA.COLUMNS
+  WHERE COLUMN_NAME LIKE '%Date'
+    AND TABLE_NAME = 'checkWindow'
+
+OPEN cur
+
+FETCH NEXT FROM cur INTO @columnName;
+
+WHILE @@fetch_status = 0
+BEGIN
+    SET @sql = '
+    DECLARE @ObjectName NVARCHAR(100)
+    SELECT @ObjectName = OBJECT_NAME([default_object_id]) FROM SYS.COLUMNS
+    WHERE [object_id] = OBJECT_ID(''[mtc_admin].[checkWindow]'') AND [name] = ' + QUOTENAME(@columnName,'''') + '
+    EXEC(''ALTER TABLE [mtc_admin].[checkWindow] DROP CONSTRAINT '' + @ObjectName)
+    ALTER TABLE [mtc_admin].checkWindow ALTER COLUMN ' + @columnName + ' DATETIMEOFFSET(3) NOT NULL
+    ALTER TABLE [mtc_admin].checkWindow ADD DEFAULT GETUTCDATE() FOR ' + @columnName + '
+    '
+    EXEC(@sql)
+
+   FETCH NEXT FROM cur INTO @columnName;
+END
+
+CLOSE cur
+DEALLOCATE cur
