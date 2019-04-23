f0a92ccab4012904d367504a68f14a38461c3389
Guy Harwood <guy@milkchip.com>
2019-04-23 12:31:45 +0100

    revert to get to bottom of test failures

---
+++ admin/services/data-access/pupil-census-import.data.service.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const pupilCensusImportDataService = {}
+
+/**
+ * Calls bulk load data execution
+ * @param {Array} pupilData
+ * @param {Array} schools
+ * @param {Number} jobId
+ * @return {Promise<*>}
+ */
+pupilCensusImportDataService.sqlBulkImport = async (pupilData, schools, jobId) => {
+  return {}
+}
+
+module.exports = pupilCensusImportDataService
