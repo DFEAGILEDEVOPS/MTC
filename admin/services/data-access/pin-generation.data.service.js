e1d7a1f0c4ec3dcd58187247adad9865a901373c
Guy Harwood <guy@milkchip.com>
2019-05-01 15:33:50 +0100

    extend

--- admin/services/data-access/pin-generation.data.service.js
+++ admin/services/data-access/pin-generation.data.service.js
@@ -3,12 +3,6 @@
 const { TYPES } = require('./sql.service')
 const sqlService = require('./sql.service')
 
-const generateTryItOutQuerySql = () => {
-  return `
-  SELECT * FROM .....
-  `
-}
-
 const serviceToExport = {
   /**
    *
@@ -19,7 +13,39 @@ const serviceToExport = {
   sqlFindEligiblePupilsBySchool: async (schoolId, isLiveCheck) => {
     let sql
     if (!isLiveCheck) {
-      sql = generateTryItOutQuerySql() + ` WHERE school_id=${schoolId}`
+      sql = ` SELECT
+                  p.id,
+                  p.foreName,
+                  p.middleNames,
+                  p.lastName,
+                  p.dateOfBirth,
+                  p.school_id,
+                  g.group_id,
+                  p.urlSlug
+              FROM
+                [mtc_admin].[pupil] p
+                  LEFT JOIN [mtc_admin].[pupilAttendance] pa ON (p.id = pa.pupil_id and pa.isDeleted = 0)
+                  LEFT JOIN [mtc_admin].[attendanceCode] ac ON (pa.attendanceCode_id = ac.id )
+                  LEFT JOIN  [mtc_admin].[pupilGroup] g ON (g.pupil_id = p.id)
+                  INNER JOIN [mtc_admin].[pupilStatus] ps ON (p.pupilStatus_id = ps.id)
+              WHERE
+                -- include all pupils except those who are marked as not taking check because they left school
+                (ac.id IS NULL OR ac.code <> 'LEFTT')
+              AND    ps.code IN ('UNALLOC',
+                                'ALLOC',
+                                'LOGGED_IN')
+              AND school_id = ${schoolId}
+                  -- Exclude pupils who already have an active familiarisation check
+              AND p.id NOT IN (SELECT
+                  p.id
+              FROM
+                [mtc_admin].[pupil] p
+                  LEFT JOIN [mtc_admin].[check] AS chk ON (p.id = chk.pupil_id)
+                  LEFT JOIN [mtc_admin].[checkStatus] AS chkStatus ON (chk.checkStatus_id = chkStatus.id)
+              WHERE  chk.isLiveCheck = 0
+                AND    chkStatus.code IN ('NEW', 'STD', 'COL')
+                AND p.school_id = ${schoolId})
+      `
     } else {
       sql = `SELECT
                   *
@@ -54,21 +80,52 @@ const serviceToExport = {
     let sql
     let { params, paramIdentifiers } = sqlService.buildParameterList(pupilIds, TYPES.Int)
     if (!isLiveCheck) {
-      sql = generateTryItOutQuerySql()
+      sql = ` SELECT
+                  p.id,
+                  p.foreName,
+                  p.middleNames,
+                  p.lastName,
+                  p.dateOfBirth,
+                  p.school_id,
+                  g.group_id,
+                  p.urlSlug
+              FROM
+                [mtc_admin].[pupil] p
+                  LEFT JOIN [mtc_admin].[pupilAttendance] pa ON (p.id = pa.pupil_id and pa.isDeleted = 0)
+                  LEFT JOIN [mtc_admin].[attendanceCode] ac ON (pa.attendanceCode_id = ac.id )
+                  LEFT JOIN  [mtc_admin].[pupilGroup] g ON (g.pupil_id = p.id)
+                  INNER JOIN [mtc_admin].[pupilStatus] ps ON (p.pupilStatus_id = ps.id)
+              WHERE
+                -- include all pupils except those who are marked as not taking check because they left school
+                (ac.id IS NULL OR ac.code <> 'LEFTT')
+              AND    ps.code IN ('UNALLOC',
+                                'ALLOC',
+                                'LOGGED_IN')
+              AND school_id = ${schoolId}
+              AND p.id IN (${paramIdentifiers.join(', ')})
+                  -- Exclude pupils who already have an active familiarisation check
+              AND p.id NOT IN (SELECT
+                  p.id
+              FROM
+                [mtc_admin].[pupil] p
+                  LEFT JOIN [mtc_admin].[check] AS chk ON (p.id = chk.pupil_id)
+                  LEFT JOIN [mtc_admin].[checkStatus] AS chkStatus ON (chk.checkStatus_id = chkStatus.id)
+              WHERE  chk.isLiveCheck = 0
+                AND    chkStatus.code IN ('NEW', 'STD', 'COL')
+                AND p.school_id = ${schoolId})
+      `
     } else {
       sql = `SELECT
                   *
                 FROM mtc_admin.vewPupilsEligibleForLivePinGeneration
                 WHERE id IN (${paramIdentifiers.join(', ')}) AND school_id = @schoolId`
     }
-    const whereClause = `WHERE id IN (${paramIdentifiers.join(', ')}) AND school_id = @schoolId`
     params.push({
       name: 'schoolId',
       value: schoolId,
       type: TYPES.Int
     })
-    const query = [ sql, whereClause ].join(' ')
-    return sqlService.query(query, params)
+    return sqlService.query(sql, params)
   },
 
   /**
