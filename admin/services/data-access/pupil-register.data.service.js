8685b3f40246f96d5fc2eabb2de0128893d5ecac
Dan Williams <dan@ithium.net>
2019-05-03 17:00:05 +0100

    Changed pupil register to use Redis cache

--- admin/services/data-access/pupil-register.data.service.js
+++ admin/services/data-access/pupil-register.data.service.js
@@ -15,7 +15,7 @@ const service = {
       { name: 'schoolId', value: schoolId, type: TYPES.Int }
     ]
 
-    return sqlService.query(sql, params)
+    return sqlService.query(sql, params, 'pupilRegister.getPupilRegister')
   },
 
   /**
