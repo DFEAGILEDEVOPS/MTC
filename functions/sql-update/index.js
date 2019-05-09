57ae1c061b3d0603988ceac35c7f9e145fdaa97d
Dan Williams <dan@ithium.net>
2019-05-09 10:39:40 +0100

    Added SQL updating to `sql-update` queue listener

--- functions/sql-update/index.js
+++ functions/sql-update/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const process = require('process')
+const sqlService = require('../lib/sql/sql.service')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -8,4 +9,35 @@ if (process.env.NODE_ENV !== 'production') {
 
 module.exports = async function (context, sqlUpdateMessage) {
   context.log('sql-update: message received', sqlUpdateMessage)
+
+  const { messages } = sqlUpdateMessage
+
+  const queries = []
+
+  messages.forEach(message => {
+    const { table, data } = message
+    for (let id in data) {
+      let query = `UPDATE [mtc_admin].[${table}] SET `
+      for (let column in data[id]) {
+        query += `${column}='${data[id][column]}' `
+      }
+      query += `WHERE id=${id}`
+      queries.push(query)
+    }
+  })
+
+  const queriesLn = queries.length
+
+  for (let i = 0; i < queriesLn; i++) {
+    const query = queries[i]
+    try {
+      const res = await sqlService.modify(query)
+      if (res.rowsModified === 0) {
+        context.log(`sql-update: no rows modified. This may be a bad update: "${query}"`)
+      }
+    } catch (error) {
+      context.log(`sql-update: failed to update the SQL DB with "${query}": ${error.message}`)
+      throw error
+    }
+  }
 }
