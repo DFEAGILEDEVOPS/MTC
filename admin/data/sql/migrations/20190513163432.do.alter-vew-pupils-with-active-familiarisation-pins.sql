c88d433fda4a8a5cd7d5a5f9329bfdc3c42e4891
Dan Williams <dan@ithium.net>
2019-05-13 16:48:42 +0100

    Added migrations to fix active/familiarisation pin views
      - Marking a pupil as not attending and then reversing twice or more,
        caused the views to show duplicates. Due to a join on the
        pupilAttendance table. Added grouping to fix it

---
+++ admin/data/sql/migrations/20190513163432.do.alter-vew-pupils-with-active-familiarisation-pins.sql
@@ -0,0 +1,38 @@
+DROP VIEW [mtc_admin].[vewPupilsWithActiveFamiliarisationPins];
+GO
+CREATE VIEW [mtc_admin].[vewPupilsWithActiveFamiliarisationPins] AS
+  SELECT
+         p.id,
+         p.foreName,
+         p.lastName,
+         p.middleNames,
+         p.dateOfBirth,
+         p.urlSlug,
+         p.school_id,
+         pin.val as pin,
+         cp.pinExpiresAt,
+         g.group_id
+  FROM [mtc_admin].[pupil] p
+         INNER JOIN [mtc_admin].[school] s ON (p.school_id = s.id)
+         LEFT JOIN  [mtc_admin].[pupilGroup] g ON (g.pupil_id = p.id)
+         INNER JOIN [mtc_admin].[check] chk ON (chk.pupil_id = p.id)
+         INNER JOIN [mtc_admin].[checkStatus] chkStatus ON (chk.checkStatus_id = chkStatus.id)
+         INNER JOIN [mtc_admin].[checkPin] cp ON (chk.id = cp.check_id)
+         INNER JOIN [mtc_admin].[pin] pin ON (cp.pin_id = pin.id)
+         LEFT JOIN [mtc_admin].[pupilAttendance] paa ON paa.pupil_id = p.id
+  WHERE  cp.pinExpiresAt > GETUTCDATE()
+    AND  chkStatus.code IN ('NEW', 'STD', 'COL')
+    AND  chk.isLiveCheck = 0
+    AND (paa.id IS NULL OR paa.isDeleted = 1)
+  GROUP BY
+    p.id,
+    p.foreName,
+    p.lastName,
+    p.middleNames,
+    p.dateOfBirth,
+    p.urlSlug,
+    p.school_id,
+    pin.val,
+    cp.pinExpiresAt,
+    g.group_id;
+  
