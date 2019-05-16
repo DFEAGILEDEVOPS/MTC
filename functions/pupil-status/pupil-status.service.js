0c1553289a747352456b2271f70460b02f638427
Dan Williams <dan@ithium.net>
2019-05-16 12:22:34 +0100

    Revert "Current progress changing functions pupil-status to update redis cache"
    This reverts commit 16bb5b08a48f9a83142d784ac430d1e7726f749d.

--- functions/pupil-status/pupil-status.service.js
+++ functions/pupil-status/pupil-status.service.js
@@ -4,7 +4,6 @@ const pupilStatusAnalysisService = require('./pupil-status-analysis.service')
 const R = require('ramda')
 const sqlService = require('../lib/sql/sql.service')
 const { TYPES } = sqlService
-const redisCacheService = require('../../admin/services/redis-cache.service')
 
 async function recalculatePupilStatus (pupilId) {
   const currentData = await getCurrentPupilData(pupilId)
@@ -59,42 +58,15 @@ async function getCurrentPupilData (pupilId) {
   return R.head(res)
 }
 
-async function getPupilStatusID (targetStatusCode) {
-  const rows = await redisCacheService.get('table.pupilStatus')
-  if (!rows) {
-    const sql = `SELECT id FROM [mtc_admin].[pupilStatus] WHERE code = @code`
-    const params = [
-      { name: 'code', value: targetStatusCode, type: TYPES.NVarChar }
-    ]
-    const results = await sqlService.query(sql, params)
-    return results[0].id
-  } else {
-    return rows.find(r => r.code === targetStatusCode).id
-  }
-}
-
 async function changePupilState (pupilId, targetStatusCode) {
-  const statusID = await getPupilStatusID(targetStatusCode)
-  const pupils = await redisCacheService.get('table.pupil')
-  if (pupils) {
-    const pupilsLn = pupils.length
-    for (let i = 0; i < pupilsLn; i++) {
-      if (pupils[i].id === pupilId) {
-        pupils[i].pupilStatus_id = statusID
-        break
-      }
-    }
-    return redisCacheService.set('table.pupil', pupils)
-  } else {
-    const sql = `UPDATE [mtc_admin].[pupil]
-                 SET pupilStatus_id = @statusID
-                 WHERE id = @pupilId`
-    const params = [
-      { name: 'statusID', value: statusID, type: TYPES.Int },
-      { name: 'pupilId', value: pupilId, type: TYPES.Int }
-    ]
-    return sqlService.modify(sql, params)
-  }
+  const sql = `UPDATE [mtc_admin].[pupil]
+               SET pupilStatus_id = (SELECT id from [mtc_admin].[pupilStatus] WHERE code = @code)
+               WHERE id = @pupilId`
+  const params = [
+    { name: 'code', value: targetStatusCode, type: TYPES.NVarChar },
+    { name: 'pupilId', value: pupilId, type: TYPES.Int }
+  ]
+  return sqlService.modify(sql, params)
 }
 
 module.exports = {
