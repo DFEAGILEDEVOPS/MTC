8a6aec639a9c16eb6332044a32470b7db197409c
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-05-22 14:46:18 +0100

    Bug/27021 Access arrangements overview table layout issues (#1199)
    * Extracted presentation logic and remade access arrangements overview table
    
    * resolve breaking test
    
    * Extract presentation logic to dedicated presenter
    
    * removed extra space
    
    * todo comment achieved and removed

---
+++ admin/spec/back-end/helpers/access-arrangements-overview-presenter.spec.js
@@ -0,0 +1,42 @@
+/* global describe, expect, it */
+const accessArrangementsOverviewPresenter = require('../../../helpers/access-arrangements-overview-presenter')
+
+describe('accessArrangementsOverviewPresenter', () => {
+  describe('getPresentationData', () => {
+    it('adds verticalBarStyle property if the pupil requires highlighting', () => {
+      const pupils = [{ urlSlug: 'urlSlug1', arrangements: [] }, { urlSlug: 'urlSlug2', arrangements: [] }]
+      const hl = ['urlSlug1']
+      const formattedPupils = accessArrangementsOverviewPresenter.getPresentationData(pupils, {}, hl)
+      expect(formattedPupils[0].verticalBarStyle).toBeDefined()
+      expect(formattedPupils[1].verticalBarStyle).not.toBeDefined()
+    })
+    it('returns hasAAEditDisabled as true if user is not allowed to edit access arrangements for the particular pupil', () => {
+      const pupils = [{ urlSlug: 'urlSlug1', arrangements: [], hasCompletedCheck: false, notTakingCheck: false }]
+      const hl = []
+      const availabilityData = { canEditArrangements: false }
+      const formattedPupils = accessArrangementsOverviewPresenter.getPresentationData(pupils, availabilityData, hl)
+      expect(formattedPupils[0].hasAAEditDisabled).toBeTruthy()
+    })
+    it('returns hasAAEditDisabled as true if the pupil has a completed check', () => {
+      const pupils = [{ urlSlug: 'urlSlug1', arrangements: [], hasCompletedCheck: true, notTakingCheck: false }]
+      const hl = []
+      const availabilityData = { canEditArrangements: true }
+      const formattedPupils = accessArrangementsOverviewPresenter.getPresentationData(pupils, availabilityData, hl)
+      expect(formattedPupils[0].hasAAEditDisabled).toBeTruthy()
+    })
+    it('returns hasAAEditDisabled as true if the pupil cannot take the check', () => {
+      const pupils = [{ urlSlug: 'urlSlug1', arrangements: [], hasCompletedCheck: false, notTakingCheck: true }]
+      const hl = []
+      const availabilityData = { canEditArrangements: true }
+      const formattedPupils = accessArrangementsOverviewPresenter.getPresentationData(pupils, availabilityData, hl)
+      expect(formattedPupils[0].hasAAEditDisabled).toBeTruthy()
+    })
+    it('returns hasAAEditDisabled as false if the user can edit access arrangements for a particular pupil, current check is incomplete and the pupil is taking the check', () => {
+      const pupils = [{ urlSlug: 'urlSlug1', arrangements: [], hasCompletedCheck: false, notTakingCheck: false }]
+      const hl = []
+      const availabilityData = { canEditArrangements: true }
+      const formattedPupils = accessArrangementsOverviewPresenter.getPresentationData(pupils, availabilityData, hl)
+      expect(formattedPupils[0].hasAAEditDisabled).toBeFalsy()
+    })
+  })
+})
