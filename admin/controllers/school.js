cddd97931ac87fa68f3d2fb7425df85613155692
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-05-22 11:00:52 +0100

    Feature/32791 Override hdf submission on results view (#1195)
    * Added logic to override hdf submission on the second Monday after check end date
    
    * removed duplicated variables
    
    * missing specs
    
    * Moved logic from controller to presenter
    
    * Refactor result availability logic pieces from presenter to a dedicated service
    
    * Changed method name to reflect purpose in a generic way
    
    * Introduced incomplete hdf page and simplified availability service
    
    * resolved test failure
    
    * Provided results opening day within incomplete hdf view
    
    * fixed typo
    
    * added fullstop
    
    * changes based on comments

--- admin/controllers/school.js
+++ admin/controllers/school.js
@@ -1,9 +1,13 @@
 'use strict'
 
+const moment = require('moment-timezone')
+
 const checkWindowV2Service = require('../services/check-window-v2.service')
+const config = require('../config')
 const pupilRegisterService = require('../services/pupil-register.service')
 const schoolHomeFeatureEligibilityPresenter = require('../helpers/school-home-feature-eligibility-presenter')
 const schoolService = require('../services/school.service')
+const resultPageAvailabilityService = require('../services/results-page-availability.service')
 const controller = {}
 
 /**
@@ -20,11 +24,14 @@ controller.getSchoolLandingPage = async (req, res, next) => {
     const checkWindowData = await checkWindowV2Service.getActiveCheckWindow()
     const schoolName = await schoolService.findSchoolByDfeNumber(req.user.School)
     const featureEligibilityData = schoolHomeFeatureEligibilityPresenter.getPresentationData(checkWindowData, req.user.timezone)
+    const currentDate = moment.tz(req.user.timezone || config.DEFAULT_TIMEZONE)
+    const isResultsFeatureAccessible = resultPageAvailabilityService.isResultsFeatureAccessible(currentDate, checkWindowData.checkEndDate)
     const hasIncompleteChecks = await pupilRegisterService.hasIncompleteChecks(req.user.schoolId)
     return res.render('school/school-home', {
       breadcrumbs: [ { 'name': 'School Home' } ],
       featureEligibilityData,
       hasIncompleteChecks,
+      isResultsFeatureAccessible,
       schoolName
     })
   } catch (error) {
