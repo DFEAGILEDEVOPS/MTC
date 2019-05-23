87f40cec96e6d0b93671e28ea39ef678c8222362
Jon Shipley <js@ovix.co.uk>
2019-05-23 12:04:59 +0100

    Update tests to look for static methods

--- pupil-spa/src/app/services/compressor/compressor.service.spec.ts
+++ pupil-spa/src/app/services/compressor/compressor.service.spec.ts
@@ -3,41 +3,31 @@ import { TestBed, inject } from '@angular/core/testing';
 import { CompressorService } from './compressor.service';
 
 describe('CompressorService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [CompressorService]
+
+  it('should have a compress method', () => {
+    expect(typeof(CompressorService.compress)).toBe('function');
+  });
+
+  it('should have a decompress method', () => {
+    expect(typeof(CompressorService.decompress)).toBe('function');
+  });
+
+  it('the results of compressing and decompressing are equivalent to the input', () => {
+    const input = 'this is a test string !@#$%^&*()';
+    expect(CompressorService.decompress(CompressorService.compress(input))).toEqual(input);
+  });
+
+  describe('Compressor.compress', () => {
+    it('returns a string', () => {
+      const result = CompressorService.compress('test');
+      expect(typeof(result)).toBe('string');
     });
   });
 
-  it('should be created', inject([CompressorService], (service: CompressorService) => {
-    expect(service).toBeTruthy();
-  }));
-
-  it('should have a compress method', inject([CompressorService], (service: CompressorService) => {
-    expect(typeof(service.compress)).toBe('function');
-  }));
-
-  it('should have a decompress method', inject([CompressorService], (service: CompressorService) => {
-    expect(typeof(service.decompress)).toBe('function');
-  }));
-
-  it('the results of compressing and decompressing are equivalent to the input',
-    inject([CompressorService], (service: CompressorService) => {
-    const input = 'this is a test string !@#$%^&*()';
-    expect(service.decompress(service.compress(input))).toEqual(input);
-  }));
-
-  describe('Compressor.compress', () => {
-    it('returns a string',  inject([CompressorService], (service: CompressorService) => {
-      const result = service.compress('test');
-      expect(typeof(result)).toBe('string');
-    }));
-  });
-
   describe('Compressor.decompress', () => {
-    it('returns a string',  inject([CompressorService], (service: CompressorService) => {
-      const result = service.decompress('test');
+    it('returns a string',  () => {
+      const result = CompressorService.decompress('test');
       expect(typeof(result)).toBe('string');
-    }));
+    });
   });
 });
