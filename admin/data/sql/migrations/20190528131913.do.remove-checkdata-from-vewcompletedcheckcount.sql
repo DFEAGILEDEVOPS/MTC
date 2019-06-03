c2f7369ba63a34eb5591f9ddbde7c4acb07c388f
Dan Williams <dan@ithium.net>
2019-06-03 17:02:37 +0100

    Updated the complete and incomplete check count views to use checkResult

--- admin/data/sql/migrations/20190528131913.do.remove-checkdata-from-vewcompletedcheckcount.sql
+++ admin/data/sql/migrations/20190528131913.do.remove-checkdata-from-vewcompletedcheckcount.sql
@@ -1,5 +1,6 @@
 ALTER VIEW [mtc_admin].[vewCompletedCheckCount] AS
   SELECT c.pupil_id, COUNT(c.id) AS [CompletedCheckCount]
   FROM [mtc_admin].[check] c
-  WHERE c.pupilLoginDate IS NULL
+    LEFT JOIN [mtc_admin].checkResult ck ON (c.id = ck.check_id)
+  WHERE ck.id IS NOT NULL
   GROUP BY c.pupil_id
--- admin/data/sql/migrations/20190528132142.do.remove-checkdata-from-vewincompletecheckcount.sql
+++ admin/data/sql/migrations/20190528132142.do.remove-checkdata-from-vewincompletecheckcount.sql
@@ -1,5 +1,6 @@
 ALTER VIEW [mtc_admin].[vewIncompleteCheckCount] AS
   SELECT c.pupil_id, COUNT(c.id) AS [IncompleteCheckCount]
   FROM [mtc_admin].[check] c
-  WHERE c.pupilLoginDate IS NOT NULL
+    LEFT JOIN [mtc_admin].checkResult ck ON (c.id = ck.check_id)
+  WHERE c.pupilLoginDate IS NOT NULL AND ck.id IS NULL
   GROUP BY c.pupil_id
