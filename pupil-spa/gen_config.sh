5705739b972c62912ea4c1e0fbe97da8b57bb402
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-06-04 19:33:52 +0100

    Update gen config with new variables

--- pupil-spa/gen_config.sh
+++ pupil-spa/gen_config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 authUrl=${AUTH_URL:-"http://localhost:3003/auth"}
+authPingUrl=${AUTH_PING_URL:-"http://localhost:3003/ping"}
 production=${PRODUCTION:-"false"}
 checkStartAPIErrorDelay=${CHECK_START_ERROR_DELAY:-"2000"}
 checkStartAPIErrorMaxAttempts=${CHECK_START_MAX_ATTEMPTS:-"3"}
@@ -10,6 +11,12 @@ feedbackAPIErrorMaxAttempts=${CHECK_SUBMISSION_MAX_ATTEMPTS:-"3"}
 pupilPrefsAPIErrorDelay=${CHECK_SUBMISSION_ERROR_DELAY:-"3000"}
 pupilPrefsAPIErrorMaxAttempts=${CHECK_SUBMISSION_MAX_ATTEMPTS:-"3"}
 submissionPendingViewMinDisplay=${SUBMISSION_PENDING_MIN_DISPLAY:-"6000"}
+connectivityCheckViewMinDisplay=${CONNECTIVITY_CHECK_MIN_DISPLAY:-"6000"}
+testPupilConnectionQueueName=${TEST_PUPIL_CONNECTION_QUEUE_NAME:-""}
+testPupilConnectionQueueUrl=${TEST_PUPIL_CONNECTION_QUEUE_URL:-""}
+testPupilConnectionQueueToken=${TEST_PUPIL_CONNECTION_QUEUE_TOKEN:-""}
+testPupilConnectionDelay=${TEST_PUPIL_CONNECTION_ERROR_DELAY:-"3000"}
+testPupilConnectionMaxAttempts=${TEST_PUPIL_CONNECTION_MAX_ATTEMPTS:-"1"}
 supportNumber=${SUPPORT_NUMBER:-"0300 303 3013"}
 gaCode=${GA_CODE:-"null"}
 websiteOffline=${WEBSITE_OFFLINE:-"false"}
@@ -30,6 +37,7 @@ fi
 cat <<EOF > config.json
 {
   "authURL": "$authUrl",
+  "authPingURL": "$authPingUrl",
   "production": $production,
   "checkStartAPIErrorDelay": $checkStartAPIErrorDelay,
   "checkStartAPIErrorMaxAttempts": $checkStartAPIErrorMaxAttempts,
@@ -38,6 +46,12 @@ cat <<EOF > config.json
   "feedbackAPIErrorDelay": $feedbackAPIErrorDelay,
   "feedbackAPIErrorMaxAttempts": $feedbackAPIErrorMaxAttempts,
   "submissionPendingViewMinDisplay": $submissionPendingViewMinDisplay,
+  "connectivityCheckViewMinDisplay": $connectivityCheckViewMinDisplay,
+  "testPupilConnectionQueueName": "$testPupilConnectionQueueName",
+  "testPupilConnectionQueueUrl": "$testPupilConnectionQueueUrl",
+  "testPupilConnectionQueueToken": "$testPupilConnectionQueueToken",
+  "testPupilConnectionDelay": $testPupilConnectionDelay,
+  "testPupilConnectionMaxAttempts": $testPupilConnectionMaxAttempts,
   "supportNumber": "$supportNumber",
   "googleAnalyticsTrackingCode": $gaCodeParsed,
   "applicationInsightsInstrumentationKey": $applicationInsightsCodeParsed,
