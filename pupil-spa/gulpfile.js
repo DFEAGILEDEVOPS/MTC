77982fbecbc6213223209d005843371fdd700dbe
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-06-05 14:00:40 +0100

    rename default values for queue url and token and default to empty string if env values are not detected

--- pupil-spa/gulpfile.js
+++ pupil-spa/gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp')
 const replace = require('gulp-string-replace')
+require('dotenv').config()
 
 gulp.task('setRuntimeConfigURL', () => {
   console.log('Attempting to update runtime config URL from the environment...')
@@ -12,9 +13,8 @@ gulp.task('setRuntimeConfigURL', () => {
 })
 
 gulp.task('setSecretEnvVars', () => {
-  require('dotenv').config()
   gulp.src('src/public/config.json', { base: './' })
-    .pipe(replace('testPupilConnectionQueueUrlEnvValue', process.env.TEST_PUPIL_CONNECTION_QUEUE_URL))
-    .pipe(replace('testPupilConnectionQueueTokenEnvValue', process.env.TEST_PUPIL_CONNECTION_QUEUE_TOKEN))
+    .pipe(replace('testPupilConnectionQueueUrlValue', process.env.TEST_PUPIL_CONNECTION_QUEUE_URL || ''))
+    .pipe(replace('testPupilConnectionQueueTokenValue', process.env.TEST_PUPIL_CONNECTION_QUEUE_TOKEN || ''))
     .pipe(gulp.dest('./'))
 })
