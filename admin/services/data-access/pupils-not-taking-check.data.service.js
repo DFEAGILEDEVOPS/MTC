9c584c4d3e231fd8d0c3dbf26bca56cb5bfe4f76
Jon Shipley <js@ovix.co.uk>
2019-06-06 12:49:45 +0100

    [admin] - fix query for selecting pupils not attending
    We need to include pupils who have hhad a live check, but it has expired as long as they don't have any other live checks.
    It seems easier to just say that the pupil's process status must be NOT STARTED if they want to set them to not attending.
    
    The NOT STARTED process status is equivalent to the UNALLOC pupil status.

--- admin/services/data-access/pupils-not-taking-check.data.service.js
+++ admin/services/data-access/pupils-not-taking-check.data.service.js
@@ -47,13 +47,11 @@ const pupilsNotTakingCheckDataService = {
       FROM ${sqlService.adminSchema}.[pupil] p 
       LEFT JOIN ${sqlService.adminSchema}.[pupilAttendance] pa ON p.id = pa.pupil_id AND pa.isDeleted=0
       LEFT JOIN ${sqlService.adminSchema}.[pupilGroup] pg ON p.id = pg.pupil_id
-      LEFT JOIN ${sqlService.adminSchema}.[check] c ON p.id = c.pupil_id AND c.isLiveCheck=1
-      LEFT JOIN ${sqlService.adminSchema}.[checkStatus] cs ON cs.id = c.checkStatus_id
       LEFT JOIN ${sqlService.adminSchema}.[pupilRestart] pr ON pr.pupil_id = p.id AND pr.isDeleted=0 AND pr.check_id IS NULL
       WHERE
         p.school_id = @schoolId
-        AND pa.id IS NULL
-        AND (cs.code IS NULL OR pr.id IS NOT NULL)
+        AND p.pupilStatus_id = 1
+        AND pr.id IS NULL
       GROUP BY
         p.foreName,
         p.middleNames,
