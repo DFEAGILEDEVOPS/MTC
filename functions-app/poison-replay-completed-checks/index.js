4ab3a6004bfc01a9ef1f9739f21db6c08e5889c8
Guy Harwood <guy@milkchip.com>
2019-06-13 20:57:58 +0100

    ready to add decompression lib

--- functions-app/poison-replay-completed-checks/index.js
+++ functions-app/poison-replay-completed-checks/index.js
@@ -3,11 +3,20 @@
 const fs = require('fs')
 const uuid = require('uuid/v4')
 const path = require('path')
+const compressionService = require('../../functions/lib/compression.service')
 
 module.exports = async function (context, check) {
-  context.log('found a message!')
+  let version
+  let message
+  if (check.version && check.version === '2') {
+    version = 2
+    message =
+  } else {
+    version = 1
+  }
+  context.log(`processing v${version} message`)
   const id = uuid()
-  const file = path.join('./', 'msgblobs', `${id}.json`)
+  const file = path.join('./', `v${version}`, `${id}.json`)
   context.log(`saving to ${file}`)
   try {
     const string = JSON.stringify(check)
