dfbeeded1d75940de8aa39e8bc46e2fb662a1a23
Dan Williams <dan@ithium.net>
2019-06-17 09:20:44 +0100

    Added seeds to admin container start

--- admin/docker-start.sh
+++ admin/docker-start.sh
@@ -7,7 +7,7 @@ then
   echo "waiting $WAIT_TIME seconds for SQL Server to come online..."
   sleep $WAIT_TIME
   echo "running database migrations..."
-  yarn migrate-sql
+  yarn migrate-and-seed-sql
 fi
 
 pm2-docker start pm2.json
