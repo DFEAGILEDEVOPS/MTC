d439d59e0307a74019d65c0286fe51b71168d16a
Dan Williams <dan@ithium.net>
2019-06-19 10:24:12 +0100

    Added school timezone to NCA auth session data - and logging of each logged in user's timezone (#1257)
    * Added school timezone to NCA auth session data
    
      - and logging of each logged in user's timezone
    
    * Handle case when the role does not have a school
    
    * Update log msg

--- admin/authentication/nca-tools-authentication-strategy.js
+++ admin/authentication/nca-tools-authentication-strategy.js
@@ -4,6 +4,8 @@ const adminLogonEventDataService = require('../services/data-access/admin-logon-
 const ncaToolsUserService = require('../services/nca-tools-user.service')
 const certificateService = require('../services/certificate-store.service')
 const logger = require('../services/log.service').getLogger()
+const schoolDataService = require('../services/data-access/school.data.service')
+const config = require('../config')
 
 module.exports = async function (req, done) {
   // Post fields from NCA Tools, all fields are Base64 encoded.
@@ -42,15 +44,24 @@ module.exports = async function (req, done) {
     }
 
     const mtcUser = await ncaToolsUserService.mapNcaUserToMtcUser(userData)
+
+    let school
+    if (mtcUser.school_id) {
+      school = await schoolDataService.sqlFindOneById(mtcUser.school_id)
+    }
+
     userData.role = mtcUser.mtcRole
     userData.schoolId = mtcUser.school_id
     userData.id = mtcUser.id
     userData.displayName = mtcUser.displayName
+    userData.timezone = school ? school.timezone : config.DEFAULT_TIMEZONE
+
     // auth success
     logonEvent.user_id = mtcUser.id
     logonEvent.isAuthenticated = true
     logonEvent.authProviderSessionToken = mtcUser.SessionToken
     await adminLogonEventDataService.sqlCreate(logonEvent)
+
     return done(null, userData)
   } catch (error) {
     // auth failed
