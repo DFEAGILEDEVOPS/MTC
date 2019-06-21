22ca5515bb96f0ef9b12df46f0f406f80f98152f
Dan Williams <dan@ithium.net>
2019-06-21 11:05:32 +0100

    Changed admin seeding to only run if env var specified
      - Ruby tests require seeds to have been run in addition to migrations

--- admin/docker-start.sh
+++ admin/docker-start.sh
@@ -4,7 +4,12 @@ echo "migrate is $MIGRATE."
 if [ "$MIGRATE" == "1" ]
 then
   echo "running database migrations..."
-  yarn migrate-and-seed-sql
+  yarn migrate-sql
+fi
+if [ "$SEED" == "1" ]
+then
+  echo "running database seeds..."
+  yarn seed-sql
 fi
 
 pm2-docker start pm2.json
