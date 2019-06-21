04b74c36dad26a8d04db7c06f77369ed0ee26ae4
Dan Williams <dan@ithium.net>
2019-06-21 09:33:47 +0100

    Changed admin wait-sqldb script to expect WAIT_FOR_SQL_SERVER env var
      - To prevent this being run in production

--- admin/wait-sqldb.sh
+++ admin/wait-sqldb.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-echo "Waiting for $SQL_SERVER:$SQL_PORT to be available..."
 
-until [[ "$(curl -sS $SQL_SERVER:$SQL_PORT 2>/dev/stdout)" =~ "(52)" ]]; do
-  printf '.'
-  sleep 5
-done
+if [[ "${WAIT_FOR_SQL_SERVER:=false}" != "false" ]]; then
+  echo "Waiting for $SQL_SERVER:$SQL_PORT to be available..."
+
+  until [[ "$(curl -sS $SQL_SERVER:$SQL_PORT 2>/dev/stdout)" =~ "(52)" ]]; do
+    printf '.'
+    sleep 5
+  done
+fi
