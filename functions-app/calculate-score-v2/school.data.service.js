08ff031b48f924e0a77fe60d3d02b7d4cb50f8ae
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-06-25 15:36:32 +0100

    query correction

--- functions-app/calculate-score-v2/school.data.service.js
+++ functions-app/calculate-score-v2/school.data.service.js
@@ -6,7 +6,7 @@ const sqlService = require('../lib/sql/sql.service')
  * @return {Promise<object>}
  */
 module.exports.sqlFindSchoolIds = async () => {
-  const sql = `SELECT id [mtc_admin].[school]`
+  const sql = `SELECT id FROM [mtc_admin].[school]`
   const params = []
   const result = sqlService.query(sql, params)
   return R.map(i => i.id, result)
