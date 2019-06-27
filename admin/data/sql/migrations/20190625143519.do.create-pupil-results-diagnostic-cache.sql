e099e71e1ee937f982964b785a2deea1fcbb1104
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-06-27 12:50:56 +0100

    Changed pupilResultsDiagnosticCache creation migration to be idempotent

--- admin/data/sql/migrations/20190625143519.do.create-pupil-results-diagnostic-cache.sql
+++ admin/data/sql/migrations/20190625143519.do.create-pupil-results-diagnostic-cache.sql
@@ -1,6 +1,14 @@
-CREATE TABLE [mtc_admin].[pupilResultsDiagnosticCache] (
-  id INT IDENTITY,
-  [school_id] INT NOT NULL,
-  [payload] [NVARCHAR](MAX) NOT NULL,
-  CONSTRAINT [PK_pupilResultsDiagnosticCache_id] PRIMARY KEY NONCLUSTERED (id)
-);
+IF NOT EXISTS (
+    SELECT *
+    FROM INFORMATION_SCHEMA.TABLES
+    WHERE  TABLE_NAME = 'pupilResultsDiagnosticCache'
+      AND TABLE_SCHEMA = 'mtc_admin'
+    )
+BEGIN
+    CREATE TABLE [mtc_admin].[pupilResultsDiagnosticCache] (
+      id INT IDENTITY,
+      [school_id] INT NOT NULL,
+      [payload] [NVARCHAR](MAX) NOT NULL,
+      CONSTRAINT [PK_pupilResultsDiagnosticCache_id] PRIMARY KEY NONCLUSTERED (id)
+    );
+END
