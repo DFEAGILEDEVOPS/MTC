ead79bacb21d51fc0ef155c361a37fcc1e4825fe
Guy Harwood <guy@milkchip.com>
2019-06-30 11:54:49 +0100

    move parsing to root of config

--- functions-app/config.js
+++ functions-app/config.js
@@ -16,8 +16,8 @@ module.exports = {
     Server: process.env.SQL_SERVER || 'localhost',
     Port: process.env.SQL_PORT || 1433,
     Timeout: process.env.SQL_TIMEOUT || oneMinuteInMilliseconds,
-    requestTimeout: process.env.SQL_REQUEST_TIMEOUT || fiveMinutesInMilliseconds,
-    connectionTimeout: process.env.SQL_CONNECTION_TIMEOUT || oneMinuteInMilliseconds,
+    requestTimeout: parseInt(process.env.SQL_REQUEST_TIMEOUT, 10) || fiveMinutesInMilliseconds,
+    connectionTimeout: parseInt(process.env.SQL_CONNECTION_TIMEOUT, 10) || oneMinuteInMilliseconds,
     Encrypt: process.env.hasOwnProperty('SQL_ENCRYPT') ? toBool(process.env.SQL_ENCRYPT) : true,
     Application: {
       Name: process.env.SQL_APP_NAME || 'mtc-local-dev', // docker default
