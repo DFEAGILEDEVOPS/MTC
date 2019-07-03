c6b16a1b5ee4ab1c7faf1def5d0c5ce14c04184f
Jon Shipley <js@ovix.co.uk>
2019-07-03 09:15:34 +0100

    [functions-app] Small refactor

--- functions-app/report-psychometrician/service/psychometrician-util.service.js
+++ functions-app/report-psychometrician/service/psychometrician-util.service.js
@@ -19,15 +19,13 @@ psUtilService.getMark = function getMark (completedCheck) {
 psUtilService.getClientTimestampFromAuditEvent = function (auditEventType, completedCheck) {
   if (!completedCheck.data) return ''
   if (!completedCheck.data.audit) return ''
-  const logEntries = completedCheck.data.audit.filter(logEntry => logEntry.type === auditEventType)
+  const audits = R.pathOr([], ['data', 'audit'], completedCheck)
+  const logEntries = audits.filter(logEntry => logEntry.type === auditEventType)
   if (!logEntries.length) {
     return 'error'
   }
-  const logEntry = logEntries[0]
-  if (!logEntry.hasOwnProperty('clientTimestamp')) {
-    return 'error'
-  }
-  return logEntry.clientTimestamp
+  const logEntry = R.head(logEntries)
+  return R.propOr('error', 'clientTimestamp', logEntry)
 }
 
 psUtilService.getClientTimestampDiffFromAuditEvents = function (firstAuditEventType, secondAuditEventType, completedCheck) {
