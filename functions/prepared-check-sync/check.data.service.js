4176b0c71693611c3dd51d8f0820b939a557f564
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-07-03 08:50:17 +0100

    Updated query to receive latest non expired try out check

--- functions/prepared-check-sync/check.data.service.js
+++ functions/prepared-check-sync/check.data.service.js
@@ -9,34 +9,40 @@ const { TYPES } = sqlService
  */
 module.exports.sqlFindActiveCheckCodesByCheckCode = async function (checkCode) {
   const sql = `          
-    DECLARE @pupilId INT = ( SELECT pupil_id FROM [mtc_admin].[check] chk WHERE chk.checkCode = @checkCode )
-
-    SELECT
-       latestLivePupilCheck.checkCode AS livePupilCheckCode,
-       latestTryOutPupilCheck.checkCode AS tryOutPupilCheckCode
-    FROM [mtc_admin].[check] chk
-    LEFT JOIN (
-        SELECT
-            chk2.checkCode,
-            chk2.pupil_id,
-            ROW_NUMBER() OVER ( PARTITION BY chk2.pupil_id ORDER BY chk2.id DESC ) AS rank
-        FROM [mtc_admin].[check] chk2
-        JOIN [mtc_admin].checkStatus cs
-        ON cs.id = chk2.checkStatus_id
-        WHERE chk2.isLiveCheck = 1
-        AND cs.code NOT IN ('CMP', 'EXP', 'NTR')
-    ) latestLivePupilCheck
-        ON latestLivePupilCheck.pupil_id = @pupilId AND latestLivePupilCheck.rank = 1
-    LEFT JOIN (
-        SELECT
-            chk3.checkCode,
-            chk3.pupil_id,
-            ROW_NUMBER() OVER ( PARTITION BY chk3.pupil_id ORDER BY chk3.id DESC ) AS rank
-        FROM [mtc_admin].[check] chk3
-        WHERE chk3.isLiveCheck = 0
-    ) latestTryOutPupilCheck
-        ON latestTryOutPupilCheck.pupil_id = @pupilId AND latestTryOutPupilCheck.rank = 1
-    GROUP BY latestLivePupilCheck.checkCode, latestTryOutPupilCheck.checkCode
+  DECLARE
+      @pupilId INT = (SELECT pupil_id
+                      FROM [mtc_admin].[check] chk
+                      WHERE chk.checkCode = @checkCode)
+  
+  SELECT
+     latestLivePupilCheck.checkCode AS livePupilCheckCode,
+     latestTryOutPupilCheck.checkCode AS tryOutPupilCheckCode
+  FROM [mtc_admin].[check] chk
+  LEFT JOIN (
+      SELECT
+          chk2.checkCode,
+          chk2.pupil_id,
+          ROW_NUMBER() OVER ( PARTITION BY chk2.pupil_id ORDER BY chk2.id DESC ) as rank
+      FROM [mtc_admin].[check] chk2
+      JOIN [mtc_admin].checkStatus cs
+          ON cs.id = chk2.checkStatus_id
+      WHERE chk2.isLiveCheck = 1
+      AND cs.code NOT IN ('CMP', 'EXP', 'NTR')
+  ) latestLivePupilCheck
+      ON latestLivePupilCheck.pupil_id = @pupilId and latestLivePupilCheck.rank = 1
+  LEFT JOIN (
+      SELECT
+          chk3.checkCode,
+          chk3.pupil_id,
+          ROW_NUMBER() OVER ( PARTITION BY chk3.pupil_id ORDER BY chk3.id DESC ) as rank
+      FROM [mtc_admin].[check] chk3
+      JOIN [mtc_admin].checkStatus cs2
+          ON cs2.id = chk3.checkStatus_id
+      WHERE chk3.isLiveCheck = 0
+      AND cs2.code NOT IN ('EXP')
+  ) latestTryOutPupilCheck
+      ON latestTryOutPupilCheck.pupil_id = @pupilId AND latestTryOutPupilCheck.rank = 1
+  GROUP BY latestLivePupilCheck.checkCode, latestTryOutPupilCheck.checkCode
   `
 
   const params = [
