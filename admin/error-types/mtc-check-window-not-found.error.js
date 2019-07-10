3abb552b885b14a299c09e85248a2edd6b27aef6
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-07-10 15:05:33 +0100

    Update admin/error-types/mtc-check-window-not-found.error.js
    Co-Authored-By: Jon Shipley <jon-shipley@users.noreply.github.com>

--- admin/error-types/mtc-check-window-not-found.error.js
+++ admin/error-types/mtc-check-window-not-found.error.js
@@ -2,7 +2,7 @@ const mtcBaseError = require('./mtc-base.error')
 
 class MtcCheckWindowNotFoundError extends mtcBaseError {
   constructor (message) {
-    super(message, 'Service manager must select a valid check window')
+    super(message, 'The service manager must configure a valid check window')
     this.name = 'MtcCheckWindowNotFound'
   }
 }
