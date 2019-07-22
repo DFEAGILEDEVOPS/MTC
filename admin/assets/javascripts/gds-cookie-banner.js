1c9570b399cefd6b06b6ee4a452d8447a05688a7
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-07-22 12:38:07 +0100

    Added new gds layout with partials, changed asset build task and included missing govuk cookie message handling functionality

---
+++ admin/assets/javascripts/gds-cookie-banner.js
@@ -0,0 +1,62 @@
+var CookieBanner = {
+  init: function (name, value, options) {
+    if (typeof value !== 'undefined') {
+      if (value === false || value === null) {
+        return CookieBanner.setCookie(name, '', { days: -1 })
+      } else {
+        return CookieBanner.setCookie(name, value, options)
+      }
+    } else {
+      return CookieBanner.getCookie(name)
+    }
+  },
+  setCookie: function (name, value, options) {
+    if (typeof options === 'undefined') {
+      options = {}
+    }
+    var cookieString = name + '=' + value + '; path=/'
+    if (options.days) {
+      var date = new Date()
+      date.setTime(date.getTime() + (options.days * 24 * 60 * 60 * 1000))
+      cookieString = cookieString + '; expires=' + date.toGMTString()
+    }
+    if (document.location.protocol === 'https:') {
+      cookieString = cookieString + ' Secure'
+    }
+    document.cookie = cookieString
+  },
+  getCookie: function (name) {
+    var nameEQ = name + '='
+    var cookies = document.cookie.split(';')
+    for (var i = 0, len = cookies.length; i < len; i++) {
+      var cookie = cookies[i]
+      while (cookie.charAt(0) === ' ') {
+        cookie = cookie.substring(1, cookie.length)
+      }
+      if (cookie.indexOf(nameEQ) === 0) {
+        return decodeURIComponent(cookie.substring(nameEQ.length))
+      }
+    }
+    return null
+  },
+  addCookieMessage: function () {
+    var message = document.querySelector('.js-cookie-banner')
+    var hasCookieMessage = (message && CookieBanner.init('seen_cookie_message') === null)
+
+    if (hasCookieMessage) {
+      message.style.display = 'block'
+      CookieBanner.init('seen_cookie_message', 'yes', { days: 28 })
+    }
+  }
+}
+
+document.getElementById('close-cookie-message').addEventListener('click', function (e) {
+  e.preventDefault()
+  var message = document.getElementById('global-cookie-message')
+  if (message) {
+    message.style.display = 'none'
+  }
+})
+
+// Add cookie message
+CookieBanner.addCookieMessage()
