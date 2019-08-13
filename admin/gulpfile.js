d1c71b7f5eca01278090349e957acdace0f2dffd
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-08-13 18:58:41 +0100

    AA overview page and autocomplete component updates

--- admin/gulpfile.js
+++ admin/gulpfile.js
@@ -15,6 +15,7 @@ const config = require('./config')
 const jsBundleFiles = [
   './node_modules/govuk-frontend/govuk/all.js',
   './assets/javascripts/jquery-1.12.4.js',
+  './assets/javascripts/gds-autocomplete.js',
   './assets/javascripts/gds-cookie-banner.js',
   './assets/javascripts/gds-table-sorting.js',
   './assets/javascripts/accessible-autocomplete.min.js',
