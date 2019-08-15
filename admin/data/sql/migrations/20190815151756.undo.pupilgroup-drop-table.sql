da97340468886f9b17dc0f653ba0bcefdec129b2
Guy Harwood <guy@milkchip.com>
2019-08-15 15:28:15 +0100

    drop the pupilGroup table

---
+++ admin/data/sql/migrations/20190815151756.undo.pupilgroup-drop-table.sql
@@ -0,0 +1,21 @@
+create table mtc_admin.pupilGroup
+(
+    id int identity primary key,
+    group_id  int not null references mtc_admin.[group],
+    pupil_id  int not null references mtc_admin.pupil,
+    createdAt datetimeoffset(3) default getutcdate() not null,
+    updatedAt datetimeoffset(3) default getutcdate() not null
+)
+
+CREATE TRIGGER [mtc_admin].[pupilGroupUpdatedAtTrigger]
+    ON [mtc_admin].[pupilGroup]
+    FOR UPDATE
+    AS
+BEGIN
+    UPDATE [mtc_admin].[pupilGroup]
+    SET updatedAt = GETUTCDATE()
+    FROM inserted
+    WHERE [pupilGroup].id = inserted.id
+END
+
+
