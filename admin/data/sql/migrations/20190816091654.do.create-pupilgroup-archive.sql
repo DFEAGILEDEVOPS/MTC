e3750bdfa86cfcaa8a21b522ec38df54ea3591ac
Guy Harwood <guy@milkchip.com>
2019-08-16 10:49:38 +0100

    remove identity

--- admin/data/sql/migrations/20190816091654.do.create-pupilgroup-archive.sql
+++ admin/data/sql/migrations/20190816091654.do.create-pupilgroup-archive.sql
@@ -1,7 +1,7 @@
 
 create table mtc_admin.z_pupilGroup_archive
 (
-    id int identity primary key,
+    id int,
     group_id  int not null,
     pupil_id  int not null,
     createdAt datetimeoffset(3) default getutcdate() not null,
