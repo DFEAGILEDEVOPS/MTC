cd269a0ee2871b9c2a6d78874e72d0c92905039a
Guy Harwood <guy@milkchip.com>
2019-08-30 12:33:02 +0100

    rename poc

--- design/2020/complete-check-gen/new-q-msg/index.js
+++ design/2020/complete-check-gen/new-q-msg/index.js
@@ -2,8 +2,8 @@
 
 const lz = require('lz-string')
 const uuid = require('uuid/v4')
-const checkMessage = require('../complete-check-message-template.json')
-const largeCompleteCheck = require('../example-large-complete-check.json')
+const checkMessage = require('../complete-check-message-template.json.js')
+const largeCompleteCheck = require('../example-large-complete-check.json.js')
 
 module.exports = (context, req) => {
   const message = JSON.parse(JSON.stringify(checkMessage))
