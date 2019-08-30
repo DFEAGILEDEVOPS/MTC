3b684cabd42dd95b69646184018c48b56c2e4f38
Guy Harwood <guy@milkchip.com>
2019-08-30 12:18:02 +0100

    schemas

--- functions/check-receiver/v3.js
+++ functions/check-receiver/v3.js
@@ -2,6 +2,8 @@
 
 const moment = require('moment')
 
+// versions 1 and 2 of complete check message processing are in the obsolete completed-checks function
+
 const v3 = {
   process: async function (context, receivedCheck) {
     // persist to receivedCheck table
