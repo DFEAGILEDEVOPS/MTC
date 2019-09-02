5a63b8c868176c5d511b06d12d27d075e0f80671
Guy Harwood <guy@milkchip.com>
2019-09-02 10:21:39 +0100

    use lib instead of binding to avoid race condition

--- functions/check-receiver/v3.js
+++ functions/check-receiver/v3.js
@@ -1,21 +1,24 @@
 'use strict'
 
 const moment = require('moment')
+const azureStorageHelper = require('../lib/azure-storage-helper')
+const azureTableService = azureStorageHelper.getPromisifiedAzureTableService()
 
 // versions 1 and 2 of complete check message processing are in the obsolete completed-checks function
-
 const v3 = {
   process: async function (context, receivedCheck) {
     // persist to receivedCheck table
     const entity = {
-      checkReceivedAt: moment().toDate(),
-      partitionKey: receivedCheck.schoolUUID,
-      rowKey: receivedCheck.checkCode,
+      PartitionKey: receivedCheck.schoolUUID,
+      RowKey: receivedCheck.checkCode,
       archive: receivedCheck.archive,
+      checkReceivedAt: moment().toDate(),
       messageVersion: receivedCheck.version
     }
-    context.bindings.receivedCheckTable = []
-    context.bindings.receivedCheckTable.push(entity)
+    /*  context.bindings.receivedCheckTable = []
+    context.bindings.receivedCheckTable.push(entity) */
+    await azureTableService.insertEntityAsync('receivedCheck', entity)
+
     // put message on validation queue
     const message = {
       checkCode: receivedCheck.checkCode,
