8f1fc5318e766e7272f653d982f8d459ab72b117
Guy Harwood <GuyHarwood@users.noreply.github.com>
2019-09-04 14:02:06 +0100

    design/complete check process 2020 (#1337)
    * complete check journey
    
    * notes on table storage limits and cosmos
    
    * cosmos
    
    * cosmos spike & notes on storage options
    
    * poc functions
    
    * put compressed check in receivedCheck table poc
    
    * check validator function, and new tables, queues
    
    * check validator function and notes on processes
    
    * formalise new functions
    
    * add new functions
    
    * schemas
    
    * rename poc
    
    * tweaks
    
    * race condition with check-validator due to receivedCheck being empty on 1st execution
    
    * update notes on race condition
    
    * use lib instead of binding to avoid race condition
    
    * update bindings and refs
    
    * validator persisting errors and successes
    
    * update dummy message generator to create invalid message
    
    * check marker function:
    
    * add new function app
    
    * reset function project
    
    * complete check function app in TS
    
    * v1.process is not a function
    
    * stumped
    
    * queue message generator
    
    * working receiver and validator skeletons
    
    * check-submitted queue and marker function
    
    * remove js functions
    
    * add tslint
    
    * version message interfaces and rename json schemas
    
    * strict typing across project
    
    * implement lint and fix issues
    
    * no implicit any
    
    * remove old check gen code
    
    * remove obsolete typings
    
    * changes for JMS
    
    * update build cmds

---
+++ func-complete-check/check-validator/index.ts
@@ -0,0 +1,28 @@
+import { AzureFunction, Context } from '@azure/functions'
+import { SubmittedCheckMessageV3, ValidateCheckMessageV1 } from '../typings/message-schemas'
+import { performance } from 'perf_hooks'
+const functionName = 'check-validator'
+import V1 from './v1'
+
+const serviceBusQueueTrigger: AzureFunction = async function (context: Context, validateCheckMessage: ValidateCheckMessageV1): Promise<void> {
+  const start = performance.now()
+  const version = validateCheckMessage.version
+  context.log.info(`${functionName}: version:${version} message received for checkCode ${validateCheckMessage.checkCode}`)
+  try {
+    if (version !== '1') {
+      throw new Error(`Message schema version ${version} unsupported`)
+    }
+    await V1.process(context, validateCheckMessage)
+  } catch (error) {
+    context.log.error(`${functionName}: ERROR: ${error.message}`)
+    throw error
+  }
+
+  const end = performance.now()
+  const durationInMilliseconds = end - start
+  const timeStamp = new Date().toISOString()
+  context.log(`${functionName}: ${timeStamp} run complete: ${durationInMilliseconds} ms`)
+
+}
+
+export default serviceBusQueueTrigger
---
+++ func-complete-check/typings/message-schemas.ts
@@ -0,0 +1,44 @@
+export interface SubmittedCheckV1 {
+  answers: Array<object>
+  audit: Array<object>
+  config: object
+  device: object
+  inputs: Array<object>
+  pupil: object
+  questions: Array<object>
+  school: object
+  tokens: object
+  checkCode: string
+}
+
+export interface SubmittedCheckMessageV3 {
+  version: string
+  checkCode: string
+  schoolUUID: string
+  archive: string
+}
+
+export interface ValidateCheckMessageV1 {
+  checkCode: string
+  schoolUUID: string
+  version: string
+}
+
+export interface MarkCheckMessageV1 {
+  checkCode: string
+  schoolUUID: string
+  version: string
+}
+
+export interface ReceivedCheck {
+  PartitionKey: string // schoolUUID
+  RowKey: string // checkCode
+  archive: string
+  checkReceivedAt: Date
+  checkVersion: number
+  isValid?: boolean
+  validatedAt?: Date
+  validationError?: string
+  mark?: number
+  markedAt?: Date
+}
