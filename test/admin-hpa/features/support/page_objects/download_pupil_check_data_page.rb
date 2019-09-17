4c24c9fdf2aab359cde0d4b1a6087bcb3dfde2fb
Mohsen Qureshi <mjq@Mohsens-Mini.lan>
2019-09-17 10:34:14 +0100

    css updates

--- test/admin-hpa/features/support/page_objects/download_pupil_check_data_page.rb
+++ test/admin-hpa/features/support/page_objects/download_pupil_check_data_page.rb
@@ -1,8 +1,8 @@
 class DownloadPupilCheckDataPage < SitePrism::Page
   set_url '/test-developer/download-pupil-check-data'
 
-  element :heading, '.heading-xlarge', text: 'Download pupil check data'
-  element :information, '#lead-paragraph', text: 'Generate and download the raw pupil check data, including timings, responses and keystrokes, and an anomaly report.'
+  element :heading, '.govuk-heading-xl', text: 'Download pupil check data'
+  element :information, '.govuk-body', text: 'Generate and download the raw pupil check data, including timings, responses and keystrokes, and an anomaly report.'
   element :generate_latest_files_button, '#generate-check-data-button'
   element :error_message, '#error-summary-heading-1', text: 'There was an error when generating the latest file'
 
