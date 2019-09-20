96e11cfcb6a71ccd108982d2baf58f1e272ad763
Amit Sinha <amit.kumar.sinha@businessthreezero.com>
2019-09-20 14:35:48 +0100

    increased sleep time for checking for pupil not taking check to enable HDF

--- test/admin-hpa/features/step_definitions/declaration_review_pupil_steps.rb
+++ test/admin-hpa/features/step_definitions/declaration_review_pupil_steps.rb
@@ -36,7 +36,7 @@ Given(/^I have a new pupil with a reason for not taking a check$/) do
     fail if !(pupil_detail.nil?)
   rescue
     sleep(15)
-    retry if (retries += 1) < 5
+    retry if (retries += 1) < 10
   end
   Timeout.timeout(ENV['WAIT_TIME'].to_i, Timeout::Error, "There are still pupil with Not started status") {sleep 1 until SqlDbHelper.get_pupil_with_no_attandance_code(school_id).nil?}
 end
