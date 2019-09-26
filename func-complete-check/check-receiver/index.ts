1bc9277d7ca93015d0c9dc0c39e603efb64d01e1
Guy Harwood <GuyHarwood@users.noreply.github.com>
2019-09-26 16:27:03 +0100

    Feature/36387 check validator (#1357)
    * define classes for storage helper and specs
    
    * remove jasmine in prep for ts-jest
    
    * install jest-ts
    
    * add readme
    
    * implement table service mock
    
    * begin implementation of check validator
    
    * test stubs
    
    * first pass of tests for validator
    
    * tests done
    
    * implementation complete
    
    * add coverage call

--- func-complete-check/check-receiver/index.ts
+++ func-complete-check/check-receiver/index.ts
@@ -4,16 +4,16 @@ import { performance } from 'perf_hooks'
 const functionName = 'check-receiver'
 import V3 from './v3'
 
-const queueTrigger: AzureFunction = async function (context: Context, completedCheck: schemas.SubmittedCheckMessageV3): Promise<void> {
+const queueTrigger: AzureFunction = async function (context: Context, submittedCheck: schemas.SubmittedCheckMessageV3): Promise<void> {
   const start = performance.now()
-  const version = completedCheck.version
-  context.log.info(`${functionName}: version:${version} message received for checkCode ${completedCheck.checkCode}`)
+  const version = submittedCheck.version
+  context.log.info(`${functionName}: version:${version} message received for checkCode ${submittedCheck.checkCode}`)
   try {
     if (version !== '3') {
       // dead letter the message as we no longer support below v3
       throw new Error(`Message schema version:${version} unsupported`)
     }
-    await V3.process(context, completedCheck)
+    await V3.process(context, submittedCheck)
   } catch (error) {
     context.log.error(`${functionName}: ERROR: ${error.message}`)
     throw error
