dd3d22c5df93a8545eafd2bd18bd952509e6c45e
Guy Harwood <guy@milkchip.com>
2019-10-02 17:18:23 +0100

    fix test issue

--- func-check-gen/spec/check-allocator/check-allocator.v1.spec.ts
+++ func-check-gen/spec/check-allocator/check-allocator.v1.spec.ts
@@ -3,6 +3,9 @@ import * as subject from '../../check-allocator/check-allocator.v1'
 let sut: subject.CheckAllocatorV1
 
 describe('check-allocator/v1', () => {
+  beforeEach(() => {
+    sut = new subject.CheckAllocatorV1()
+  })
   test('should be defined', () => {
     expect(sut).toBeDefined()
   })
