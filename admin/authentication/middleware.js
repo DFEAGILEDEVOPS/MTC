a4dea4fbe2b2f2e1befcb16716c38103102aaaef
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-10-02 12:43:39 +0100

    Updated authentication middleware to compare against a list of allowed roles

--- admin/authentication/middleware.js
+++ admin/authentication/middleware.js
@@ -2,22 +2,20 @@
 
 const logger = require('../services/log.service').getLogger()
 
-function isAuthenticated (roles) {
+function isAuthenticated (arg) {
   return function (req, res, next) {
     if (req.isAuthenticated()) {
+      let roles = typeof arg === 'string' && arg.length > 1
+        ? [arg] : arg
       let userRole
       if (req.user) {
         userRole = ((req.user).role || {})
       }
-      logger.debug(`checking authorisation on ${req.url} for role:${roles} against userRole:${userRole}`)
-      // declare single role variable if input is not array of roles
-      const role = !Array.isArray(roles) && roles
-      if (role === undefined ||
-        (role !== undefined && userRole && role === userRole) ||
-        (Array.isArray(roles) && roles.some(r => r === userRole))) {
+      logger.debug(`checking authorisation on ${req.url} for roles:${roles && roles.join()} against userRole:${userRole}`)
+      if (roles === undefined || (Array.isArray(roles) && roles.some(r => r === userRole))) {
         return next()
       } else {
-        logger.warn(`could not authorise ${[roles].join()} against userRole:${userRole}, UserName:${req.user.UserName} ID:${req.user.id}`)
+        logger.warn(`could not authorise ${roles.join()} against userRole:${userRole}, UserName:${req.user.UserName} ID:${req.user.id}`)
         return res.redirect('/unauthorised')
       }
     }
