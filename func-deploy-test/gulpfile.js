c10704baa3538b0d4db42db0eff4d803f04c9e7a
Guy Harwood <guy@milkchip.com>
2019-10-10 17:28:09 +0100

    update task name

--- func-deploy-test/gulpfile.js
+++ func-deploy-test/gulpfile.js
@@ -3,7 +3,7 @@
 const gulp = require('gulp')
 const replace = require('gulp-replace')
 
-gulp.task('align-dist-targets', function (cb) {
+gulp.task('align-function-script-targets', function (cb) {
   gulp.src(['**/function.json'])
     .pipe(replace('"scriptFile": "../../tslib', '"scriptFile": "..'))
     .pipe(gulp.dest('./'))
