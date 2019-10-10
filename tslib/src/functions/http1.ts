60bb320a191e908641fde77f44fafc003db61ec2
Guy Harwood <guy@milkchip.com>
2019-10-10 14:52:05 +0100

    improve npm scripts

--- tslib/src/functions/http1.ts
+++ tslib/src/functions/http1.ts
@@ -3,7 +3,7 @@ import { CompressionService } from '../index'
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   context.log('HTTP trigger function processed a request.')
-  const name = (req.query.name || (req.body && req.body.name))
+  const name = (req.query.name)
 
   if (name) {
     const comp = new CompressionService().compress(name)
@@ -13,7 +13,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
   } else {
     context.res = {
       status: 400,
-      body: 'Please pass a name on the query string or in the request body'
+      body: 'Please pass a name as query string'
     }
   }
 }
