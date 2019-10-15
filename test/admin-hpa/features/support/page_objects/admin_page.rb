80e9e0ddf5bbac2819d02174b3e175aa04bf60af
Mohsen Qureshi <mjq@Mohsens-Mini.lan>
2019-10-15 10:44:52 +0100

    adding scenarios for service message

--- test/admin-hpa/features/support/page_objects/admin_page.rb
+++ test/admin-hpa/features/support/page_objects/admin_page.rb
@@ -16,6 +16,9 @@ class AdminPage < SitePrism::Page
   element :mod_schools_settings, 'a', text: "Settings for Ministry of Defence schools (MOD)"
   element :mod_schools_settings_text, 'p', text: 'Set timezones, convert and remove school listed as MOD'
   element :school_page_settings_text, 'p', text: 'Change the settings within the school pages'
+  element :manage_service_message, 'a', text: 'Manage service message'
+  element :manage_service_message_text, 'p', text: 'Create, edit or remove a service message'
+
 
   element :manage_access_arrangements, "a", text: 'Manage access arrangements'
   element :manage_access_arrangements_text, 'p', text: 'Approve or reject access arrangement requests'
