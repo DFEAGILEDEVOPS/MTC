251c2b0434890f7e7df57436383ef939254d7b6f
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-10-16 15:07:59 +0100

    removed edit scenario from presenter

--- admin/helpers/service-message-presenter.js
+++ admin/helpers/service-message-presenter.js
@@ -4,11 +4,8 @@ const serviceMessagePresenter = {}
 
 /**
  * Returns the flash message for creating or editing the service message
- * @param {string} requestData
  * @returns {string} flashMessage
  */
-serviceMessagePresenter.getFlashMessage = (requestData) =>
-  requestData.isEditView
-    ? 'Service message has been updated' : 'Service message has successfully been created'
+serviceMessagePresenter.getFlashMessage = () => 'Service message has successfully been created'
 
 module.exports = serviceMessagePresenter
--- admin/spec/back-end/helpers/service-message-presenter.spec.js
+++ admin/spec/back-end/helpers/service-message-presenter.spec.js
@@ -4,15 +4,9 @@ const serviceMessagePresenter = require('../../../helpers/service-message-presen
 
 describe('serviceMessagePresenter', () => {
   describe('getFlashMessage', () => {
-    it('returns the edited flash message if isEdit is found as true on the request data', () => {
-      const requestData = { isEditView: true }
-      const result = serviceMessagePresenter.getFlashMessage(requestData)
-      expect(result).toBe('Service message has been updated')
-    })
-    it('returns the created flash message is isEdit is found as false on the request data', () => {
-      const requestData = { isEditView: false }
-      const result = serviceMessagePresenter.getFlashMessage(requestData)
-      expect(result).toBe('Service message has been created')
+    it('returns the created flash message on the request data', () => {
+      const result = serviceMessagePresenter.getFlashMessage()
+      expect(result).toBe('Service message has successfully been created')
     })
   })
 })
