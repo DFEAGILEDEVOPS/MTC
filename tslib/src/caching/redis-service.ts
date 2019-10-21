699544a88bb3a521aabec8440e3e30371fbcc9ba
Guy Harwood <guy@milkchip.com>
2019-10-21 14:30:01 +0100

    redis impl

--- tslib/src/caching/redis-service.ts
+++ tslib/src/caching/redis-service.ts
@@ -1,8 +1,14 @@
+import * as redis from 'ioredis'
+
 export interface IRedisService {
   get (key: string): Promise<any>
   setex (key: string, value: string, ttl: number): Promise<any>
+  connect (): void
 }
 export class RedisService implements IRedisService {
+  connect (): void {
+    throw new Error('Method not implemented.')
+  }
   async get (key: string) {
     throw new Error('Method not implemented.')
   }
