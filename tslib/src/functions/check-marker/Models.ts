28e676936a64300b217d1b8841083a5410be858a
Guy Harwood <guy@milkchip.com>
2019-10-23 16:36:28 +0100

    remove semicolons

--- tslib/src/functions/check-marker/Models.ts
+++ tslib/src/functions/check-marker/Models.ts
@@ -1,26 +1,26 @@
 export interface ICheckMarkerFunctionBindings {
-  receivedCheckTable: Array<any>;
-  checkNotificationQueue: Array<any>;
+  receivedCheckTable: Array<any>
+  checkNotificationQueue: Array<any>
 }
 export interface Answer {
-  factor1: number;
-  factor2: number;
-  answer: string;
-  sequenceNumber: number;
-  question: string;
-  clientTimestamp: string;
+  factor1: number
+  factor2: number
+  answer: string
+  sequenceNumber: number
+  question: string
+  clientTimestamp: string
 }
 export interface CheckFormQuestion {
-  f1: number;
-  f2: number;
+  f1: number
+  f2: number
 }
 export interface MarkingData {
-  answers: Array<Answer>;
-  formQuestions: Array<CheckFormQuestion>;
-  results: any;
+  answers: Array<Answer>
+  formQuestions: Array<CheckFormQuestion>
+  results: any
 }
 export interface Mark {
-  mark: number;
-  maxMarks: number;
-  processedAt: Date;
+  mark: number
+  maxMarks: number
+  processedAt: Date
 }
