71c0347916f35e9cddea10acfd29ef5e130ae0a4
Guy Harwood <guy@milkchip.com>
2019-10-24 01:21:13 +0100

    json files do not work on relative paths not on function host root 🙃

--- tslib/src/functions/util-submit-check/index.ts
+++ tslib/src/functions/util-submit-check/index.ts
@@ -2,16 +2,16 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions'
 import * as lz from 'lz-string'
 import uuid from 'uuid/v4'
-import * as checkMessage from '../../schemas/complete-check-message-template.json'
-import * as largeCompleteCheck from '../../schemas/large-complete-check.json'
+import submittedCheck from '../../schemas/submitted-check.v3'
+import completeCheckPayload from '../../schemas/complete-check-payload'
 
 const httpTrigger: AzureFunction = function (context: Context, req: HttpRequest): void {
-  const message = JSON.parse(JSON.stringify(checkMessage))
+  const message = JSON.parse(JSON.stringify(submittedCheck))
   message.checkCode = req.query['checkCode']
   message.schoolUUID = uuid()
   // enable to create an invalid check
-  // delete largeCompleteCheck.answers
-  const archive = lz.compressToUTF16(JSON.stringify(largeCompleteCheck))
+  // delete completeCheckPayload.answers
+  const archive = lz.compressToUTF16(JSON.stringify(completeCheckPayload))
   message.archive = archive
   context.bindings.submittedCheckQueue = [ message ]
   context.done()
