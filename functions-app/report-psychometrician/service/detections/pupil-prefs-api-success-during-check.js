b5a500df67c8010f6d625e2eb94e52dd7d854f0b
Jon Shipley <js@ovix.co.uk>
2019-10-25 06:13:57 +0100

    [functions-app] add new detection for pupil prefs after the check

---
+++ functions-app/report-psychometrician/service/detections/pupil-prefs-api-success-during-check.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const { prop, propOr, assoc } = require('ramda')
+const { isPlainObj, isNotNilOrEmpty } = require('ramda-adjunct')
+
+const getCheckStartedDate = require('./get-check-started-date')
+const dateService = require('../../../lib/date.service')
+const report = require('./report')
+const removeDuplicates = require('./remove-duplicate-anomaly-reports')
+
+const detectPupilPrefsApiSuccessDuringCheck = function (data) {
+  if (!isPlainObj(data)) {
+    throw new TypeError('data should be an object')
+  }
+
+  const payload = prop('checkPayload', data)
+  const audits = propOr([], 'audit', payload)
+
+  const checkStartedDate = getCheckStartedDate(
+    null, // always get the date from the audit log
+    payload
+  )
+
+  const prefsAudits = audits.filter(o => o.type === 'PupilPrefsAPICallSucceeded')
+  const prefsAuditsWithMomentTimestamps = prefsAudits.map(o => {
+    const momentTs = dateService.convertIsoStringToMoment(o.clientTimestamp)
+    if (momentTs) {
+      return assoc('momentTimestamp', momentTs, o)
+    }
+    return o
+  })
+  const output = prefsAuditsWithMomentTimestamps.map(o => {
+    if (o.momentTimestamp && o.momentTimestamp.isAfter(checkStartedDate)) {
+      return report(data, 'Check disrupted by PupilPrefsAPICallSucceeded event')
+    }
+  })
+  return removeDuplicates(output.filter(o => isNotNilOrEmpty(o)))
+}
+
+module.exports = detectPupilPrefsApiSuccessDuringCheck
---
+++ functions-app/report-psychometrician/spec/detections/pupil-prefs-api-success-during-check.spec.js
@@ -0,0 +1,64 @@
+'use strict'
+
+/* global describe, it, expect, fail */
+
+const { isFunction, isArray } = require('ramda-adjunct')
+
+const detectPupilPrefsApiSuccessDuringCheck = require('../../service/detections/pupil-prefs-api-success-during-check')
+
+describe('detectPupilPrefsApiSuccessDuringCheck', () => {
+  it('is defined', () => {
+    expect(detectPupilPrefsApiSuccessDuringCheck).toBeDefined()
+  })
+
+  it('is a function', () => {
+    expect(isFunction(detectPupilPrefsApiSuccessDuringCheck)).toBeTrue()
+  })
+
+  it('takes a single `data` argument', () => {
+    try {
+      detectPupilPrefsApiSuccessDuringCheck() // missing arg
+      fail()
+    } catch (error) {
+      expect(error.message).toBe('data should be an object')
+    }
+  })
+
+  it('produces a report when the PupilPrefsApISuccessEvent is recorded after check start', () => {
+    const mockData = {
+      checkPayload: {
+        audit: [
+          { type: 'CheckStarted', clientTimestamp: '2019-09-17T11:33:55.123Z' },
+          {
+            'type': 'PupilPrefsAPICallSucceeded',
+            'clientTimestamp': '2019-09-17T11:33:55.124Z'
+          },
+          {
+            'type': 'PupilPrefsAPICallSucceeded',
+            'clientTimestamp': '2019-09-17T11:33:55.125Z'
+          }
+        ]
+      }
+    }
+    const r = detectPupilPrefsApiSuccessDuringCheck(mockData)
+    expect(isArray(r)).toBeTrue()
+    expect(r.length).toBe(1) // duplicates supressed
+    expect(r[0].Message).toBe('Check disrupted by PupilPrefsAPICallSucceeded event')
+  })
+
+  it('does not produce a report when the PupilPrefsApISuccessEvent is recorded before check start', () => {
+    const mockData = {
+      checkPayload: {
+        audit: [
+          { type: 'CheckStarted', clientTimestamp: '2019-09-17T11:33:55.123Z' },
+          {
+            'type': 'PupilPrefsAPICallSucceeded',
+            'clientTimestamp': '2019-09-17T11:33:55.122Z'
+          }
+        ]
+      }
+    }
+    const r = detectPupilPrefsApiSuccessDuringCheck(mockData)
+    expect(r).toEqual([])
+  })
+})
