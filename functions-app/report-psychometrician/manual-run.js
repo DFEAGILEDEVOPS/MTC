891c7d1f430b0f994662ed7872d0a1a2bb531de3
Jon Shipley <js@ovix.co.uk>
2019-10-25 09:05:36 +0100

    [functions-app] add time taken to manual run output

--- functions-app/report-psychometrician/manual-run.js
+++ functions-app/report-psychometrician/manual-run.js
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
 
+const getUnixTimeNow = () => Math.round((new Date()).getTime() / 1000)
+
+const displayTime = (secs) => {
+  const mins = Math.floor(secs / 60)
+  const remainingSecs = (secs % 60)
+  return `${mins.toString().padStart(2, '0')}:${remainingSecs.toString().padStart(2, '0')}`
+}
+
 async function main () {
+  const start = getUnixTimeNow()
   const mockContext = require('../mock-context')
   const v2 = require('./v2')
   await v2.process(mockContext.log)
+  const end = getUnixTimeNow()
+  const timeTakenSeconds = (end - start) / 60
+  console.log('Manual run completed in ', displayTime(timeTakenSeconds))
 }
 
 main()
