389191248fff535313ab981a799d48293d01917b
Guy Harwood <guy@milkchip.com>
2019-10-25 09:18:35 +0100

    remove obsolete index default export

--- tslib/src/index.ts
+++
@@ -1,7 +0,0 @@
-/* export { IAsyncTableService, AsyncTableService } from './azure/storage-helper'
-export { ICompressionService, CompressionService } from './common/compression-service'
-export { ILogger } from './common/ILogger'
-export { IRedisService, RedisService } from './caching/redis-service'
-export { IQueueService, SasToken, SasTokenService } from './azure/sas-token-service'
-export { primitiveToBoolean } from './common/to-bool' */
-// TODO better to alias each component or explicitly declare each?
