2d3d54e4bdadbd6d717b6a7c82c5c48b7ac14d9c
Guy Harwood <GuyHarwood@users.noreply.github.com>
2019-11-01 14:52:03 +0000

    Feature/37499 TS function infrastructure (#1392)
    * check window tests
    
    * check window implementation and add redis invalidation
    
    * block out WIP code
    
    * correct check window expiry
    
    * fix issues
    
    * remove spec files from function outputs
    
    * tighten up redis
    
    * fix tests due to redis refactor
    
    * issues with redis re-hydration
    
    * fixed persistence issue
    
    * tidy up comments
    
    * update comment
    
    * move test config to package.json
    
    * configure integration tests as separate test run
    
    * complete redis integration tests
    
    * convert redis item type into enum
    
    * switch to const
    
    * remove underscore prefix on member variables
    
    * remove meta prop prefix
    
    * add jsdoc for error thrown on get
    
    * log parsing error
    
    * fix integration tests
    
    * add integration tests to travis
    
    * add setex throws doc
    
    * close redis after tests finish.  thank you jest 💪🏼
    
    * add open handle detection to see if we get any output from travis on what is keeping jest from closing.  Could not reproduce locally
    
    * add detection to correct script 🙄
    
    * forceExit tests until code re-reviewed
    
    * fix missing redis member issue
    
    * wait for ioredis to disconnect
    
    * remove force exit
    
    * force exit due to issues with ioredis tidy up
    
    * shut down redis service

---
+++ tslib/src/services/check-window.service.ts
@@ -0,0 +1,53 @@
+
+import { ICheckWindow } from '../schemas/check-window'
+import { IRedisService, RedisService } from '../caching/redis-service'
+import { ICheckWindowDataService, CheckWindowDataService } from './data/check-window.data.service'
+import { ILogger, ConsoleLogger } from '../common/logger'
+
+export interface ICheckWindowService {
+  getActiveCheckWindow: Promise<ICheckWindow>
+}
+
+export class CheckWindowService {
+  private redisService: IRedisService
+  private checkWindowDataService: ICheckWindowDataService
+  private logger: ILogger
+  private twentyFourHoursInSeconds = 86400
+
+  constructor (redisService?: IRedisService, checkWindowDataService?: ICheckWindowDataService, logger?: ILogger) {
+    if (redisService === undefined) {
+      redisService = new RedisService()
+    }
+    this.redisService = redisService
+
+    if (checkWindowDataService === undefined) {
+      checkWindowDataService = new CheckWindowDataService()
+    }
+    this.checkWindowDataService = checkWindowDataService
+
+    if (logger === undefined) {
+      logger = new ConsoleLogger()
+    }
+    this.logger = logger
+  }
+
+  async getActiveCheckWindow (): Promise<ICheckWindow> {
+    let cachedWindow
+    try {
+      cachedWindow = await this.redisService.get('activeCheckWindow')
+    } catch (error) {
+      this.logger.error(error)
+    }
+    if (!cachedWindow) {
+      const window = await this.checkWindowDataService.getActiveCheckWindow()
+      try {
+        // tslint:disable-next-line: no-floating-promises
+        this.redisService.setex('activeCheckWindow', window, this.twentyFourHoursInSeconds)
+      } catch (error) {
+        this.logger.error(error)
+      }
+      return window
+    }
+    return cachedWindow
+  }
+}
