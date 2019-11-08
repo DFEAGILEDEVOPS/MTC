0451728be03b74d43259c6864b36fec7669d2ace
Guy Harwood <GuyHarwood@users.noreply.github.com>
2019-11-08 16:15:26 +0000

    Feature/37819 pupil api redis (#1400)
    * switch to jest
    
    * remove jasmine
    
    * remove nyc and jasmine deps
    
    * add feature toggles to api
    
    * fix issue with api container build
    
    * use tsc to build
    
    * redis auth implementation
    
    * move specs next to implementation
    
    * input validation
    
    * implement payload lookup
    
    * implement sevice in auth controller
    
    * feature toggle is loaded before its initialised due to default new controller export 🤷‍♂️
    
    * working implementation
    
    * need to fix up redis
    
    * switch to basic redis implementation until we move to typed
    
    * align features
    
    * remove debug noise
    
    * align toggles
    
    * fix check started issue
    
    * remove obsolete file

--- pupil-api/gulpfile.js
+++ pupil-api/gulpfile.js
@@ -1,23 +1,13 @@
 const gulp = require('gulp')
-const ts = require('gulp-typescript')
 const JSON_FILES = ['src/*.json', 'src/**/*.json']
-
-// pull in the project TypeScript config
-const tsProject = ts.createProject('tsconfig.json')
-
-gulp.task('scripts', () => {
-  const tsResult = tsProject.src()
-    .pipe(tsProject())
-  return tsResult.js.pipe(gulp.dest('dist'))
-})
 
 gulp.task('watch', () => {
   gulp.watch('src/**/*.ts', gulp.series('scripts'))
 })
 
-gulp.task('assets', function () {
+gulp.task('json', function () {
   return gulp.src(JSON_FILES)
     .pipe(gulp.dest('dist'))
 })
 
-gulp.task('default', gulp.parallel('watch', 'assets'))
+gulp.task('default', gulp.parallel('watch', 'json'))
---
+++ pupil-api/src/services/redis-pupil-auth.service.ts
@@ -0,0 +1,43 @@
+import { IRedisService, BasicRedisService } from './redis.service'
+import * as azureQueueService from './azure-queue.service'
+
+export interface IPupilAuthenticationService {
+  authenticate (schoolPin: string, pupilPin: string): Promise<object>
+}
+
+export class RedisPupilAuthenticationService implements IPupilAuthenticationService {
+
+  private redisService: IRedisService
+
+  constructor (redisService?: IRedisService) {
+    if (redisService === undefined) {
+      redisService = new BasicRedisService()
+    }
+    this.redisService = redisService
+  }
+
+  async authenticate (schoolPin: string, pupilPin: string): Promise<object> {
+    if (schoolPin.length === 0 || pupilPin.length === 0) {
+      throw new Error('schoolPin and pupilPin cannot be an empty string')
+    }
+    const cacheKey = this.buildCacheKey(schoolPin, pupilPin)
+    const cacheItem = await this.redisService.get(cacheKey)
+    if (!cacheItem) {
+      return null
+    }
+    const hydratedCacheItem = JSON.parse(cacheItem)
+      // Emit a successful login to the queue
+    const pupilLoginMessage = {
+      checkCode: hydratedCacheItem.checkCode,
+      loginAt: new Date(),
+      version: 1
+    }
+    azureQueueService.addMessage('pupil-login', pupilLoginMessage)
+    return hydratedCacheItem
+  }
+
+  private buildCacheKey (schoolPin: string, pupilPin: string): string {
+    return `preparedCheck:${schoolPin}:${pupilPin}`
+  }
+
+}
