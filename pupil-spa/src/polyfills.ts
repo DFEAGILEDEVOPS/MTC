c5c2b4307a1defcda47cf9fb0ee89b9c3804e5be
Jon Shipley <js@ovix.co.uk>
2019-11-08 17:47:36 +0000

    angular 7

--- pupil-spa/src/polyfills.ts
+++ pupil-spa/src/polyfills.ts
@@ -39,7 +39,6 @@ import 'core-js/es6/set';
 
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
 
 import 'govuk_frontend_toolkit/javascripts/govuk/details.polyfill';
 
