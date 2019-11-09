c25386cee55bf56449ea779b49e8aba2fe4d9250
Guy Harwood <guy@milkchip.com>
2019-11-09 08:01:20 +0000

    implement notification shell

--- tslib/src/functions/check-notifier/check-notifier.v1.spec.ts
+++ tslib/src/functions/check-notifier/check-notifier.v1.spec.ts
@@ -1,14 +1,55 @@
-export class CheckNotifier {}
+import { ICheckNotificationMessage, CheckNotificationType } from './check-notification-message'
+
+export class CheckNotifier {
+  private checkNotifierDataService: ICheckNotifierDataService
+
+  constructor (checkNotifierDataService?: ICheckNotifierDataService) {
+    if (checkNotifierDataService === undefined) {
+      checkNotifierDataService = new CheckNotifierDataService()
+    }
+    this.checkNotifierDataService = checkNotifierDataService
+  }
+
+  async notify (notification: ICheckNotificationMessage) {
+    return this.checkNotifierDataService.updateCheckAsComplete(notification.checkCode)
+  }
+}
+
+export class CheckNotifierDataService implements ICheckNotifierDataService {
+  updateCheckAsComplete (checkCode: string): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+}
+
+export interface ICheckNotifierDataService {
+  updateCheckAsComplete (checkCode: string): Promise<void>
+}
+
+const CheckNotifierDataServiceMock = jest.fn<ICheckNotifierDataService, any>(() => ({
+  updateCheckAsComplete: jest.fn()
+}))
 
 let sut: CheckNotifier
+let dataServiceMock: ICheckNotifierDataService
 
 describe('check-notifier/v1', () => {
 
   beforeEach(() => {
-    sut = new CheckNotifier()
+    dataServiceMock = new CheckNotifierDataServiceMock()
+    sut = new CheckNotifier(dataServiceMock)
   })
 
   test('should be defined', () => {
     expect(sut).toBeDefined()
+  })
+
+  test('checkComplete notification should update check as complete', async () => {
+    await sut.notify({
+      notificationType: CheckNotificationType.checkComplete,
+      checkCode: 'code',
+      version: 1
+    })
+    expect(dataServiceMock.updateCheckAsComplete).toHaveBeenCalledTimes(1)
+    expect(dataServiceMock.updateCheckAsComplete).toHaveBeenCalledWith('code')
   })
 })
