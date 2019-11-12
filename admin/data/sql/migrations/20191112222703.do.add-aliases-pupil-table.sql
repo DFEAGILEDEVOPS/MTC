3a258e532e0838deedd81b9c2ac3f0d58d7db766
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-11-12 22:41:10 +0000

    add migrations and UI changes

---
+++ admin/data/sql/migrations/20191112222703.do.add-aliases-pupil-table.sql
@@ -0,0 +1,2 @@
+ALTER TABLE mtc_admin.pupil ADD foreNameAlias [NVARCHAR](max) NULL
+ALTER TABLE mtc_admin.pupil ADD lastNameAlias [NVARCHAR](max) NULL
---
+++ admin/data/sql/migrations/20191112222703.undo.add-aliases-pupil-table.sql
@@ -0,0 +1,2 @@
+ALTER TABLE mtc_admin.pupil DROP COLUMN lastNameAlias
+ALTER TABLE mtc_admin.pupil DROP COLUMN foreNameAlias
