7e5151fe933691ebf85134501118d54e89afabb2
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-11-13 13:58:41 +0000

    changed migration so that all name fields are 128 chars

--- admin/data/sql/migrations/20191112222703.undo.add-aliases-pupil-table.sql
+++ admin/data/sql/migrations/20191112222703.undo.add-aliases-pupil-table.sql
@@ -1,2 +1,7 @@
-ALTER TABLE mtc_admin.pupil DROP COLUMN lastNameAlias
-ALTER TABLE mtc_admin.pupil DROP COLUMN foreNameAlias
+ALTER TABLE mtc_admin.pupil DROP COLUMN lastNameAlias;
+ALTER TABLE mtc_admin.pupil DROP COLUMN foreNameAlias;
+DROP INDEX [mtc_admin].[pupil].[idx_azure_recommended_pupil_school];
+ALTER TABLE mtc_admin.pupil ALTER COLUMN middleNames [NVARCHAR](MAX) NULL;
+ALTER TABLE mtc_admin.pupil ALTER COLUMN lastName [NVARCHAR](MAX) NULL;
+ALTER TABLE mtc_admin.pupil ALTER COLUMN foreName [NVARCHAR](MAX) NULL;
+CREATE NONCLUSTERED INDEX [idx_azure_recommended_pupil_school] ON [mtc_admin].[pupil] ([school_id]) INCLUDE ([createdAt], [dateOfBirth], [foreName], [gender], [isTestAccount], [job_id], [jwtSecret], [jwtToken], [lastName], [middleNames], [pin], [pinExpiresAt], [pupilAgeReason_id], [pupilStatus_id], [updatedAt], [upn], [urlSlug], [version]) WITH (ONLINE = ON)
