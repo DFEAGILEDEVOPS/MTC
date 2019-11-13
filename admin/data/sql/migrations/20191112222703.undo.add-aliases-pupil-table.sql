54be2dd75368e08ca9f3c4c1b3d293964773cad0
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-11-13 13:59:42 +0000

    added missing semicolon

--- admin/data/sql/migrations/20191112222703.undo.add-aliases-pupil-table.sql
+++ admin/data/sql/migrations/20191112222703.undo.add-aliases-pupil-table.sql
@@ -4,4 +4,4 @@ DROP INDEX [mtc_admin].[pupil].[idx_azure_recommended_pupil_school];
 ALTER TABLE mtc_admin.pupil ALTER COLUMN middleNames [NVARCHAR](MAX) NULL;
 ALTER TABLE mtc_admin.pupil ALTER COLUMN lastName [NVARCHAR](MAX) NULL;
 ALTER TABLE mtc_admin.pupil ALTER COLUMN foreName [NVARCHAR](MAX) NULL;
-CREATE NONCLUSTERED INDEX [idx_azure_recommended_pupil_school] ON [mtc_admin].[pupil] ([school_id]) INCLUDE ([createdAt], [dateOfBirth], [foreName], [gender], [isTestAccount], [job_id], [jwtSecret], [jwtToken], [lastName], [middleNames], [pin], [pinExpiresAt], [pupilAgeReason_id], [pupilStatus_id], [updatedAt], [upn], [urlSlug], [version]) WITH (ONLINE = ON)
+CREATE NONCLUSTERED INDEX [idx_azure_recommended_pupil_school] ON [mtc_admin].[pupil] ([school_id]) INCLUDE ([createdAt], [dateOfBirth], [foreName], [gender], [isTestAccount], [job_id], [jwtSecret], [jwtToken], [lastName], [middleNames], [pin], [pinExpiresAt], [pupilAgeReason_id], [pupilStatus_id], [updatedAt], [upn], [urlSlug], [version]) WITH (ONLINE = ON);
