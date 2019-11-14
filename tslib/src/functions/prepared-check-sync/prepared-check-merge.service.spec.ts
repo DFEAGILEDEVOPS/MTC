3c61dfba39fa1042bbde86a3b5655a2241879668
Guy Harwood <guy@milkchip.com>
2019-11-14 18:46:08 +0000

    remove semis 🤮

--- tslib/src/functions/prepared-check-sync/prepared-check-merge.service.spec.ts
+++ tslib/src/functions/prepared-check-sync/prepared-check-merge.service.spec.ts
@@ -1,6 +1,6 @@
 
 describe('prepared-check-merge.service', () => {
-  test.todo('updated and merged prepared checks are persisted back to redis');
-  test.todo('access arrangements are looked up in sql and error thrown if not found');
-  test.todo('access arrangement codes are looked up in sql and error thrown if not found');
+  test.todo('updated and merged prepared checks are persisted back to redis')
+  test.todo('access arrangements are looked up in sql and error thrown if not found')
+  test.todo('access arrangement codes are looked up in sql and error thrown if not found')
 })
