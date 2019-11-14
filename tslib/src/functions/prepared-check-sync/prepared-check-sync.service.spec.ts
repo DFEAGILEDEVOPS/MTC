7be32458374c2ce9c1c3f305a596e7fb77b84fc2
Guy Harwood <guy@milkchip.com>
2019-11-14 12:24:36 +0000

    look up checks

--- tslib/src/functions/prepared-check-sync/prepared-check-sync.service.spec.ts
+++ tslib/src/functions/prepared-check-sync/prepared-check-sync.service.spec.ts
@@ -1,18 +1,36 @@
 
 let sut: PreparedCheckSyncService
+let dataServiceMock: IPreparedCheckSyncDataService
+const PreparedCheckSyncDataServiceMock = jest.fn<IPreparedCheckSyncDataService, any>(() => ({
+  getCurrentChecksByPupilUuid: jest.fn(),
+  getAccessArrangementsCodesById: jest.fn(),
+  getAccessArrangementsByCheckCode: jest.fn()
+}))
 
 describe('prepared-check-sync.service', () => {
 
   beforeEach(() => {
-    sut = new PreparedCheckSyncService()
+    dataServiceMock = new PreparedCheckSyncDataServiceMock()
+    sut = new PreparedCheckSyncService(dataServiceMock)
   })
 
   test('subject should be defined', () => {
     expect(sut).toBeDefined()
   })
 
-  test('prepared check is looked up in redis and error thrown if not found', async () => {
-    
+  test('active checks for pupil are looked up in redis and error thrown if none found', async () => {
+    const pupilUUID = 'pupilUUID'
+    dataServiceMock.getCurrentChecksByPupilUuid = jest.fn(async (pupilUUID: string) => {
+      return []
+    })
+
+    try {
+      await sut.process(pupilUUID)
+      fail('error should have been thrown')
+    } catch (error) {
+      expect(dataServiceMock.getCurrentChecksByPupilUuid).toHaveBeenCalled()
+      expect(error.message).toBe(`no checks found for pupil UUID:${pupilUUID}`)
+    }
   })
 
   test.todo('access arrangements are looked up in sql and error thrown if not found')
@@ -21,8 +39,20 @@ describe('prepared-check-sync.service', () => {
 })
 
 export class PreparedCheckSyncService {
-  async process (): Promise<void> {
-    throw new Error('not implemented')
+
+  private dataService: IPreparedCheckSyncDataService
+
+  constructor (dataService?: IPreparedCheckSyncDataService) {
+    if (dataService === undefined) {
+      dataService = new PreparedCheckSyncDataService()
+    }
+    this.dataService = dataService
+  }
+  async process (pupilUUID: string): Promise<void> {
+    const checksToUpdate = await this.dataService.getCurrentChecksByPupilUuid(pupilUUID)
+    if (checksToUpdate.length === 0) {
+      throw new Error(`no checks found for pupil UUID:${pupilUUID}`)
+    }
   }
 }
 
@@ -30,6 +60,18 @@ export interface IPreparedCheckSyncDataService {
   getCurrentChecksByPupilUuid (pupilUUID: string): Promise<Array<string>>
   getAccessArrangementsCodesById (aaIds: Array<number>): Promise<Array<string>>
   getAccessArrangementsByCheckCode (checkCode: string): Promise<any>
+}
+
+export class PreparedCheckSyncDataService implements IPreparedCheckSyncDataService {
+  getCurrentChecksByPupilUuid (pupilUUID: string): Promise<string[]> {
+    throw new Error('Method not implemented.')
+  }
+  getAccessArrangementsCodesById (aaIds: number[]): Promise<string[]> {
+    throw new Error('Method not implemented.')
+  }
+  getAccessArrangementsByCheckCode (checkCode: string): Promise<any> {
+    throw new Error('Method not implemented.')
+  }
 }
 
 export interface IPreparedCheckSyncMessage {
