1475d76c99ff1dc3d0abc6e8736f2061aa9fa88a
sinhaamit1980 <sinhaamit1980@yahoo.com>
2019-11-14 09:52:37 +0000

    automation/update-automation-for-new-check-process (#1410)
    * updated check for pin from redis
    
    * updated method to check for prepare check key from redis
    
    * updated method to check for prepare check key from redis

--- test/pupil-hpa/features/support/env.rb
+++ test/pupil-hpa/features/support/env.rb
@@ -12,6 +12,7 @@ require 'numbers_in_words'
 require 'mongo'
 require 'waitutil'
 require 'tiny_tds'
+require 'redis'
 require 'webdrivers'
 require 'show_me_the_cookies'
 require 'httparty'
@@ -119,3 +120,18 @@ AZURE_BLOB_CLIENT = Azure::Storage::Blob::BlobService.create(storage_account_nam
 BLOB_CONTAINER = AzureBlobHelper.no_fail_create_container("screenshots-#{Time.now.strftime("%d-%m-%y")}")
 AzureBlobHelper.remove_old_containers
 SqlDbHelper.update_to_25_questions
+
+redis_key = ENV['REDIS_KEY'] || ''
+redis_port =  ENV['REDIS_PORT'] || 6379
+
+if ENV['DOCKER'] == 'true'
+  redis_host = 'redis'
+else
+  redis_host = ENV['REDIS_HOST'] || 'localhost'
+end
+
+if azure_test == 'true'
+  REDIS_CLIENT = Redis.new(host: "#{redis_host}", port: redis_port, password: "#{redis_key}", :ssl => :true)
+else
+  REDIS_CLIENT = Redis.new(host: "#{redis_host}", port: redis_port)
+end
