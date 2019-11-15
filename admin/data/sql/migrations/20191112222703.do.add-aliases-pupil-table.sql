d18688ae79b13f6c06799458ea090d677d9816b6
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-11-15 14:39:52 +0000

    include mask when name fields are altered

--- admin/data/sql/migrations/20191112222703.do.add-aliases-pupil-table.sql
+++ admin/data/sql/migrations/20191112222703.do.add-aliases-pupil-table.sql
@@ -4,4 +4,10 @@ DROP INDEX [mtc_admin].[pupil].[idx_azure_recommended_pupil_school];
 ALTER TABLE mtc_admin.pupil ALTER COLUMN foreName [NVARCHAR](128) NULL;
 ALTER TABLE mtc_admin.pupil ALTER COLUMN lastName [NVARCHAR](128) NULL;
 ALTER TABLE mtc_admin.pupil ALTER COLUMN middleNames [NVARCHAR](128) NULL;
+ALTER TABLE mtc_admin.[pupil]
+ALTER COLUMN foreName ADD MASKED WITH (FUNCTION = 'default()');
+ALTER TABLE mtc_admin.[pupil]
+ALTER COLUMN middleNames ADD MASKED WITH (FUNCTION = 'default()');
+ALTER TABLE mtc_admin.[pupil]
+ALTER COLUMN lastName ADD MASKED WITH (FUNCTION = 'default()');
 CREATE NONCLUSTERED INDEX [idx_azure_recommended_pupil_school] ON [mtc_admin].[pupil] ([school_id]) INCLUDE ([createdAt], [dateOfBirth], [foreName], [gender], [isTestAccount], [job_id], [jwtSecret], [jwtToken], [lastName], [middleNames], [foreNameAlias], [lastNameAlias], [pin], [pinExpiresAt], [pupilAgeReason_id], [pupilStatus_id], [updatedAt], [upn], [urlSlug], [version]) WITH (ONLINE = ON)
