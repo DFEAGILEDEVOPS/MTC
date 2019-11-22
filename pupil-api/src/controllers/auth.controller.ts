c5e7817b6a756cd0804410e7a8ed07f43f59af02
Guy Harwood <GuyHarwood@users.noreply.github.com>
2019-11-22 17:15:37 +0000

    fix pupil api tests and logic (#1419)
    * fix pupil api tests and logic
    
    * insert mock

--- pupil-api/src/controllers/auth.controller.ts
+++ pupil-api/src/controllers/auth.controller.ts
@@ -2,25 +2,22 @@ import { Request, Response } from 'express'
 import logger from '../services/log.service'
 import * as apiResponse from './api-response'
 import { pupilAuthenticationService } from '../services/azure-pupil-auth.service'
-import { RedisPupilAuthenticationService } from '../services/redis-pupil-auth.service'
+import { RedisPupilAuthenticationService, IPupilAuthenticationService } from '../services/redis-pupil-auth.service'
 import { FeatureService, IFeatureService } from '../services/feature.service'
 
 export class AuthController {
-  private useRedisAuth: boolean
-  private redisAuthService: RedisPupilAuthenticationService
+  private redisAuthService: IPupilAuthenticationService
   private featureService: IFeatureService
 
-  constructor (featureService?: IFeatureService) {
+  constructor (featureService?: IFeatureService, redisAuthService?: IPupilAuthenticationService) {
     if (featureService === undefined) {
       featureService = new FeatureService()
     }
     this.featureService = featureService
-    this.useRedisAuth = this.featureService.redisAuthMode()
-    const mode = this.useRedisAuth ? 'redis' : 'table storage'
-    logger.info(`auth mode:${mode}`)
-    if (this.useRedisAuth) {
-      this.redisAuthService = new RedisPupilAuthenticationService()
+    if (redisAuthService === undefined) {
+      redisAuthService = new RedisPupilAuthenticationService()
     }
+    this.redisAuthService = redisAuthService
   }
 
   async postAuth (req: Request, res: Response) {
@@ -31,11 +28,13 @@ export class AuthController {
     }
 
     const { pupilPin, schoolPin } = req.body
+    if (!schoolPin || !pupilPin) return apiResponse.unauthorised(res)
+
     let data
     try {
-      if (this.useRedisAuth) {
+      if (this.featureService.redisAuthMode()) {
         data = await this.redisAuthService.authenticate(schoolPin, pupilPin)
-        if (data === null) {
+        if (data === undefined) {
           return apiResponse.unauthorised(res)
         }
       } else {
