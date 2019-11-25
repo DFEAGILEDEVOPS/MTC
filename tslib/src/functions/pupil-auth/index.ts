78b10d46913562abad921de455d521a7386f1dcd
Guy Harwood <guy@milkchip.com>
2019-11-25 21:30:11 +0000

    add logging

--- tslib/src/functions/pupil-auth/index.ts
+++ tslib/src/functions/pupil-auth/index.ts
@@ -12,6 +12,8 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     return
   }
   const response = await pupilAuthService.authenticate(context.bindings as IPupilAuthFunctionBindings, context.req)
+  context.log.info(`response...`)
+  context.log.info(JSON.stringify(response, null, 2))
   context.res = response
 }
 
