3e650b64debac4ef1d0f8efa7025041cfff5e8c6
Guy Harwood <guy@milkchip.com>
2019-11-25 23:40:52 +0000

    remove CORS policy

--- tslib/src/functions/pupil-auth/pupil-auth.service.ts
+++ tslib/src/functions/pupil-auth/pupil-auth.service.ts
@@ -72,10 +72,10 @@ export class PupilAuthService {
       status: 200,
       body: preparedCheck,
       headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Methods': 'POST,OPTIONS',
+        'Content-Type': 'application/json'
+/*         'Access-Control-Allow-Methods': 'POST,OPTIONS',
         'Access-Control-Allow-Headers': 'content-type',
-        'Access-Control-Allow-Origin': config.PupilAuth.CorsWhitelist
+        'Access-Control-Allow-Origin': config.PupilAuth.CorsWhitelist */
       },
       isRaw: true
     }
