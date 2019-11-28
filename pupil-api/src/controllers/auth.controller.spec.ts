76602e21a77e2866caea28ada36c6eeffca04d79
Guy Harwood <guy@milkchip.com>
2019-11-28 13:16:25 +0000

    finish pupil api controller separation

--- pupil-api/src/controllers/auth.controller.spec.ts
+++ pupil-api/src/controllers/auth.controller.spec.ts
@@ -6,23 +6,13 @@ import { AuthController } from './auth.controller'
 import * as httpMocks from 'node-mocks-http'
 import logger from '../services/log.service'
 import { pupilAuthenticationService } from '../services/azure-pupil-auth.service'
-import { IFeatureService } from '../services/feature.service'
 import { IPupilAuthenticationService } from '../services/redis-pupil-auth.service'
-
-const RedisPupilAuthServiceMock = jest.fn<IPupilAuthenticationService, any>(() => ({
-  authenticate: jest.fn()
-}))
-
-const FeatureServiceMock = jest.fn<IFeatureService, any>(() => ({
-  redisAuthMode: jest.fn()
-}))
 
 let req
 let res
 let mockResponse = {}
 let mockErrorResponse = new Error('mock error')
 let authController: AuthController
-let featureService: IFeatureService
 let redisPupilAuthService: IPupilAuthenticationService
 
 describe('auth controller', () => {
@@ -30,12 +20,10 @@ describe('auth controller', () => {
     req = createMockRequest('application/json')
     req.body = { schoolPin: 'pin1', pupilPin: 'pin2' }
     res = httpMocks.createResponse()
-    featureService = new FeatureServiceMock()
-    redisPupilAuthService = new RedisPupilAuthServiceMock()
-    authController = new AuthController(featureService, redisPupilAuthService)
+    authController = new AuthController()
   })
 
-  it('returns an 400 error if the request is not JSON', async () => {
+  test('returns an 400 error if the request is not JSON', async () => {
     req = createMockRequest('text/html')
     spyOn(logger, 'error')
     await authController.postAuth(req, res)
@@ -44,7 +32,7 @@ describe('auth controller', () => {
     expect(data.error).toBe('Bad request')
   })
 
-  it('allows a content-type of application/json', async () => {
+  test('allows a content-type of application/json', async () => {
     spyOn(pupilAuthenticationService, 'authenticate').and.returnValue(Promise.resolve(mockResponse))
     req = createMockRequest('application/json')
     req.body = {
@@ -55,7 +43,7 @@ describe('auth controller', () => {
     expect(res.statusCode).toBe(200)
   })
 
-  it('allows a content-type of application/json with a charset', async () => {
+  test('allows a content-type of application/json with a charset', async () => {
     spyOn(pupilAuthenticationService, 'authenticate').and.returnValue(Promise.resolve(mockResponse))
     req = createMockRequest('application/json; charset=utf-8')
     req.body = {
@@ -66,17 +54,7 @@ describe('auth controller', () => {
     expect(res.statusCode).toBe(200)
   })
 
-  it('makes a call to the authentication service if redis not enabled', async () => {
-    featureService.redisAuthMode = jest.fn(() => {
-      return false
-    })
-    spyOn(pupilAuthenticationService, 'authenticate').and.returnValue(Promise.resolve(mockResponse))
-    req.body = { schoolPin: 'pin1', pupilPin: 'pin2' }
-    await authController.postAuth(req, res)
-    expect(pupilAuthenticationService.authenticate).toHaveBeenCalledWith('pin2', 'pin1')
-  })
-
-  it('returns unauthorised if the login failed', async () => {
+  test('returns unauthorised if the login failed', async () => {
     spyOn(pupilAuthenticationService, 'authenticate').and.returnValue(Promise.reject(mockErrorResponse))
     spyOn(logger, 'error')
     await authController.postAuth(req, res)
@@ -85,7 +63,7 @@ describe('auth controller', () => {
     expect(data.error).toBe('Unauthorised')
   })
 
-  it('shortcuts to return unauthorised if no schoolPin provided', async () => {
+  test('shortcuts to return unauthorised if no schoolPin provided', async () => {
     spyOn(pupilAuthenticationService, 'authenticate')
     spyOn(logger, 'error')
     req.body = {
@@ -93,13 +71,12 @@ describe('auth controller', () => {
     }
     await authController.postAuth(req, res)
     expect(pupilAuthenticationService.authenticate).not.toHaveBeenCalled()
-    expect(redisPupilAuthService.authenticate).not.toHaveBeenCalled()
     const data = JSON.parse(res._getData())
     expect(res.statusCode).toBe(401)
     expect(data.error).toBe('Unauthorised')
   })
 
-  it('shortcuts to return unauthorised if no pupilPin provided', async () => {
+  test('shortcuts to return unauthorised if no pupilPin provided', async () => {
     spyOn(pupilAuthenticationService, 'authenticate')
     spyOn(logger, 'error')
     req.body = {
@@ -107,13 +84,12 @@ describe('auth controller', () => {
     }
     await authController.postAuth(req, res)
     expect(pupilAuthenticationService.authenticate).not.toHaveBeenCalled()
-    expect(redisPupilAuthService.authenticate).not.toHaveBeenCalled()
     const data = JSON.parse(res._getData())
     expect(res.statusCode).toBe(401)
     expect(data.error).toBe('Unauthorised')
   })
 
-  it('returns a data packet to the client if authorisation is successful', async () => {
+  test('returns a data packet to the client if authorisation is successful', async () => {
     spyOn(pupilAuthenticationService, 'authenticate').and.returnValue(Promise.resolve(mockResponse))
     await authController.postAuth(req, res)
     const data = JSON.parse(res._getData())
@@ -121,27 +97,14 @@ describe('auth controller', () => {
     expect(data).toBeTruthy()
   })
 
-  it('returns a 401 if no redis preparedCheck found', async () => {
-    redisPupilAuthService.authenticate = jest.fn((schoolPin: string, pupilPin: string) => {
-      return undefined
+  function createMockRequest (contentType: String): any {
+    return httpMocks.createRequest({
+      method: 'POST',
+      url: '/auth',
+      headers: {
+        'Content-Type': `${contentType}`,
+        'Content-Length': '42' // needed to make `req.is` work
+      }
     })
-    featureService.redisAuthMode = jest.fn(() => {
-      return true
-    })
-    await authController.postAuth(req, res)
-    expect(res.statusCode).toBe(401)
-    const data = JSON.parse(res._getData())
-    expect(data.error).toBe('Unauthorised')
-  })
+  }
 })
-
-function createMockRequest (contentType: String): any {
-  return httpMocks.createRequest({
-    method: 'POST',
-    url: '/auth',
-    headers: {
-      'Content-Type': `${contentType}`,
-      'Content-Length': '42' // needed to make `req.is` work
-    }
-  })
-}
--- pupil-api/src/controllers/auth.controller.ts
+++ pupil-api/src/controllers/auth.controller.ts
@@ -2,24 +2,9 @@ import { Request, Response } from 'express'
 import logger from '../services/log.service'
 import * as apiResponse from './api-response'
 import { pupilAuthenticationService } from '../services/azure-pupil-auth.service'
-import { RedisPupilAuthenticationService, IPupilAuthenticationService } from '../services/redis-pupil-auth.service'
-import { FeatureService, IFeatureService } from '../services/feature.service'
 import { IAuthController } from '../routes/auth'
 
 export class AuthController implements IAuthController {
-  private redisAuthService: IPupilAuthenticationService
-  private featureService: IFeatureService
-
-  constructor (featureService?: IFeatureService, redisAuthService?: IPupilAuthenticationService) {
-    if (featureService === undefined) {
-      featureService = new FeatureService()
-    }
-    this.featureService = featureService
-    if (redisAuthService === undefined) {
-      redisAuthService = new RedisPupilAuthenticationService()
-    }
-    this.redisAuthService = redisAuthService
-  }
 
   async postAuth (req: Request, res: Response) {
     const contentType = req.get('Content-Type')
@@ -31,16 +16,8 @@ export class AuthController implements IAuthController {
     const { pupilPin, schoolPin } = req.body
     if (!schoolPin || !pupilPin) return apiResponse.unauthorised(res)
 
-    let data
     try {
-      if (this.featureService.redisAuthMode()) {
-        data = await this.redisAuthService.authenticate(schoolPin, pupilPin)
-        if (data === undefined) {
-          return apiResponse.unauthorised(res)
-        }
-      } else {
-        data = await pupilAuthenticationService.authenticate(pupilPin, schoolPin)
-      }
+      const data = await pupilAuthenticationService.authenticate(pupilPin, schoolPin)
       apiResponse.sendJson(res, data)
     } catch (error) {
       logger.error('Failed to authenticate pupil: ', error)
