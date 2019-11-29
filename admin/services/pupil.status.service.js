e53b97c061c6770924e415b202406afa68b2a4df
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-11-29 08:50:39 +0000

    drop cache when adding/removing pupil attendance flags

--- admin/services/pupil.status.service.js
+++ admin/services/pupil.status.service.js
@@ -7,6 +7,8 @@ const pupilAttendanceDataService = require('./data-access/pupil-attendance.data.
 const pupilDataService = require('./data-access/pupil.data.service')
 const pupilRestartDataService = require('./data-access/pupil-restart.data.service')
 const pupilStatusCodeDataService = require('./data-access/pupil-status-code.data.service')
+const redisCacheService = require('../services/data-access/redis-cache.service')
+const redisKeyService = require('../services/redis-key.service')
 const R = require('ramda')
 
 const pupilStatusService = {}
@@ -98,7 +100,8 @@ pupilStatusService.dispatchPupilStatusMessages = async (pupils) => {
 /**
  * Make a request to the pupil status service to recalculate the status for one or more pupils.
  * This function has to fabricate a checkCode as in the not-taking-check example one isn't available.
- * @param {[number]}postedPupilSlugs
+ * @param {Array<String>} pupilSlugs
+ * @param {Number} schoolId
  */
 pupilStatusService.recalculateStatusByPupilSlugs = async (pupilSlugs, schoolId) => {
   if (!(Array.isArray(pupilSlugs) && pupilSlugs.length > 0)) {
@@ -108,6 +111,8 @@ pupilStatusService.recalculateStatusByPupilSlugs = async (pupilSlugs, schoolId) 
   const pupils = await pupilDataService.sqlFindPupilsByUrlSlug(pupilSlugs, schoolId)
 
   await pupilStatusService.dispatchPupilStatusMessages(pupils)
+  const pupilRegisterRedisKey = redisKeyService.getPupilRegisterViewDataKey(schoolId)
+  await redisCacheService.drop(pupilRegisterRedisKey)
 }
 
 /**
