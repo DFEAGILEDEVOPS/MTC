ff4775213e6c21bd043827bf9068cf6517e91baf
George Chatzigiannis <geohatzigiannis@gmail.com>
2019-12-04 16:42:17 +0000

    Fixed issues with two column sorting

--- admin/helpers/pupil-pin-presenter.js
+++ admin/helpers/pupil-pin-presenter.js
@@ -4,23 +4,37 @@ const tableSorting = require('./table-sorting')
 
 const pupilPinPresenter = {}
 
+const generatePupilPinViewData = p => ({
+  pupilViewForeName: p.foreNameAlias || p.foreName,
+  pupilViewLastName: p.lastNameAlias || p.lastName,
+  ...p
+})
+
 /**
  * Prepares pupil data for the active pins view
  * @returns {Array<Object>}
  */
 pupilPinPresenter.getPupilPinViewData = (pupils) => {
-  const generatePupilPinViewData = p => ({
-    pupilViewForeName: p.foreNameAlias || p.foreName,
-    pupilViewLastName: p.lastNameAlias || p.lastName,
-    ...p
-  })
+  const pupilsWithAliases = R.map(generatePupilPinViewData, pupils)
   const listIncludesGroups = R.any(p => p.group && p.group.length > 0, pupils)
   if (listIncludesGroups) {
-    const sortedPupilsWithGroups = tableSorting.applySorting(R.map(generatePupilPinViewData, R.filter(p => p.group && p.group.length > 0, pupils)), 'group')
-    const sortedPupilsWithoutGroups = tableSorting.applySorting(R.map(generatePupilPinViewData, R.filter(p => !p.group || p.group.length === 0, pupils)), 'pupilViewLastName')
+    const sortedPupilsWithGroups = pupilPinPresenter.sortPupilsByGroupAndLastName(R.filter(p => p.group && p.group.length > 0, pupilsWithAliases))
+    const sortedPupilsWithoutGroups = tableSorting.applySorting(R.filter(p => !p.group || p.group.length === 0, pupilsWithAliases), 'pupilViewLastName')
     return R.concat(sortedPupilsWithGroups, sortedPupilsWithoutGroups)
   }
-  return tableSorting.applySorting(R.map(generatePupilPinViewData, pupils), 'pupilViewLastName')
+  return tableSorting.applySorting(pupilsWithAliases, 'pupilViewLastName')
+}
+
+pupilPinPresenter.sortPupilsByGroupAndLastName = (pupils) => {
+  const sortedByGroups = tableSorting.applySorting(pupils, 'group')
+  const groups = [...new Set(sortedByGroups.map(p => p.group))]
+  const result = []
+  groups.forEach(g => {
+    const groupedPupils = sortedByGroups.filter(p => p.group === g)
+    tableSorting.applySorting(groupedPupils, 'pupilViewLastName')
+    groupedPupils.forEach(gp => result.push(gp))
+  })
+  return result
 }
 
 module.exports = pupilPinPresenter
