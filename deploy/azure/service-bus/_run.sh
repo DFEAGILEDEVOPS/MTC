f9a3f10d75ad6d8b687483e14b9b91a3a3f38250
Guy Harwood <guy@milkchip.com>
2019-12-04 23:11:30 +0000

    remove unused param

--- deploy/azure/service-bus/_run.sh
+++ deploy/azure/service-bus/_run.sh
@@ -2,6 +2,5 @@
 
 RES_GROUP=$1 # the target resource group
 BUS_NAME=$2 # corresponds to *.params.json file prefix, such as dev, preprod, demo etc
-KEY_VAULT_NAME=$3 # the resource group key vault name
 
 az group deployment create --resource-group $RES_GROUP --template-file template.json --parameters bus_namespace=$BUS_NAME
