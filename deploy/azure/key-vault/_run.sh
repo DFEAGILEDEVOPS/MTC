73177a66b12a00f93904d4cae7e03f7c6c627d84
Guy Harwood <guy@milkchip.com>
2019-12-04 19:30:47 +0000

    sync storage account key

--- deploy/azure/key-vault/_run.sh
+++ deploy/azure/key-vault/_run.sh
@@ -5,16 +5,18 @@ KEY_VAULT_NAME=$2 # the key vault name
 SERVICE_BUS_NAME=$3 # the service bus to extract keys from
 STORAGE_ACCOUNT_NAME=$4 # the storage account to extract keys from
 
+### Service Bus
 # get connection string for consumer apps...
-CONSUMER_CONNECTION_STRING=$(az servicebus namespace authorization-rule keys list --resource-group $RES_GROUP --namespace-name $SERVICE_BUS_NAME --name mtc-consumer | jq -r .primaryConnectionString)
-
+SB_CONSUMER_CONNECTION_STRING=$(az servicebus namespace authorization-rule keys list --resource-group $RES_GROUP --namespace-name $SERVICE_BUS_NAME --name mtc-consumer | jq -r .primaryConnectionString)
 # put it in key vault...
-az keyvault secret set --vault-name $KEY_VAULT_NAME --name "ServiceBusConnectionString" --value "$CONSUMER_CONNECTION_STRING"
+az keyvault secret set --vault-name $KEY_VAULT_NAME --name "ServiceBusConnectionString" --value "$SB_CONSUMER_CONNECTION_STRING"
 
   # get connection string for consumer apps...
-DEPLOY_CONNECTION_STRING=$(az servicebus namespace authorization-rule keys list --resource-group $RES_GROUP --namespace-name $SERVICE_BUS_NAME --name mtc-deploy | jq -r .primaryConnectionString)
-
+SB_DEPLOY_CONNECTION_STRING=$(az servicebus namespace authorization-rule keys list --resource-group $RES_GROUP --namespace-name $SERVICE_BUS_NAME --name mtc-deploy | jq -r .primaryConnectionString)
 # put it in key vault...
-az keyvault secret set --vault-name $KEY_VAULT_NAME --name "ServiceBusConnectionString-Deploy" --value "$DEPLOY_CONNECTION_STRING"
+az keyvault secret set --vault-name $KEY_VAULT_NAME --name "ServiceBusConnectionString-Deploy" --value "$SB_DEPLOY_CONNECTION_STRING"
 
-# TODO storage account
+### Storage Account
+SA_CONNECTION_STRING=$(az storage account show-connection-string -g $RES_GROUP -n $STORAGE_ACCOUNT_NAME | jq -r .connectionString)
+# put it in key vault...
+az keyvault secret set --vault-name $KEY_VAULT_NAME --name "StorageAccountConnectionString" --value "$SA_CONNECTION_STRING"
