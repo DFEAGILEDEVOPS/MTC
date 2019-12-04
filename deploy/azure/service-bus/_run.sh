11f2c17c53c69265358af66c90eabad78bb07601
Guy Harwood <guy@milkchip.com>
2019-12-04 13:19:59 +0000

    raw output

--- deploy/azure/service-bus/_run.sh
+++ deploy/azure/service-bus/_run.sh
@@ -4,19 +4,16 @@ BUS_NAME=$1 # corresponds to *.params.json file prefix, such as dev, preprod, de
 RES_GROUP=$2 # the target resource group
 KEY_VAULT_NAME=$3 # the resource group key vault name
 
-az group deployment create \
-  --resource-group $RES_GROUP \
-  --template-file template.json \
-  --parameters bus_namespace=$BUS_NAME
+# az group deployment create --resource-group $RES_GROUP --template-file template.json --parameters bus_namespace=$BUS_NAME
+
+# get connection string for consumer apps...
+CONSUMER_CONNECTION_STRING=$(az servicebus namespace authorization-rule keys list --resource-group $RES_GROUP --namespace-name $BUS_NAME --name mtc-consumer | jq -r .primaryConnectionString)
+
+# put it in key vault...
+az keyvault secret set --vault-name $KEY_VAULT_NAME --name "ServiceBusConnectionString" --value "$CONSUMER_CONNECTION_STRING"
 
   # get connection string for consumer apps...
-  CONSUMER_CONNECTION_STRING=$(az servicebus namespace authorization-rule keys list --resource-group $RES_GROUP --namespace-name $BUS_NAME --name mtc-consumer | jq .primaryConnectionString)
+DEPLOY_CONNECTION_STRING=$(az servicebus namespace authorization-rule keys list --resource-group $RES_GROUP --namespace-name $BUS_NAME --name mtc-deploy | jq -r .primaryConnectionString)
 
-  # put it in key vault...
-  az keyvault secret set --vault-name $KEY_VAULT_NAME --name "ServiceBusConnectionString" --value "$CONSUMER_CONNECTION_STRING"
-
-    # get connection string for consumer apps...
-  DEPLOY_CONNECTION_STRING=$(az servicebus namespace authorization-rule keys list --resource-group $RES_GROUP --namespace-name $BUS_NAME --name mtc-deploy | jq .primaryConnectionString)
-
-  # put it in key vault...
-  az keyvault secret set --vault-name $KEY_VAULT_NAME --name "ServiceBusConnectionString-Deploy" --value "$DEPLOY_CONNECTION_STRING"
+# put it in key vault...
+az keyvault secret set --vault-name $KEY_VAULT_NAME --name "ServiceBusConnectionString-Deploy" --value "$DEPLOY_CONNECTION_STRING"
