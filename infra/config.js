66dbc00d2255cc4be1dc5a9fd82f3975705de163
Guy Harwood <guy@milkchip.com>
2019-12-18 17:13:06 +0000

    config

--- infra/config.js
+++ infra/config.js
@@ -1,9 +1,9 @@
 'use strict'
 require('dotenv').config()
 const toBool = require('to-bool')
+const sql = require('./sql.config')
 const twoMinutesInMilliseconds = 120000
 const thirtySecondsInMilliseconds = 30000
-const oneMinuteInMilliseconds = 60000
 
 const getEnvironment = () => {
   return process.env.ENVIRONMENT_NAME || 'Local-Dev'
@@ -11,32 +11,37 @@ const getEnvironment = () => {
 
 module.exports = {
   Environment: getEnvironment(),
-  database: process.env.SQL_DATABASE || 'mtc',
-  server: process.env.SQL_SERVER || 'localhost',
-  // Required for when SQL_PORT is passed in via docker-compose
-  port: process.env.SQL_PORT ? parseInt(process.env.SQL_PORT) : 1433,
-  requestTimeout: parseInt(process.env.SQL_REQUEST_TIMEOUT, 10) || oneMinuteInMilliseconds,
-  connectionTimeout: parseInt(process.env.SQL_CONNECT_TIMEOUT, 10) || thirtySecondsInMilliseconds,
-  user: process.env.SQL_ADMIN_USER || 'sa', // docker default
-  password: process.env.SQL_ADMIN_USER_PASSWORD || 'Mtc-D3v.5ql_S3rv3r', // docker default
-  pool: {
-    min: process.env.SQL_POOL_MIN_COUNT || 0,
-    max: process.env.SQL_POOL_MAX_COUNT || 5,
-    acquireTimeoutMillis: parseInt(process.env.SQL_POOL_ACQUIRE_TIMEOUT, 10) || thirtySecondsInMilliseconds
-  },
-  options: {
-    appName: process.env.SQL_APP_NAME || 'mtc-local-dev', // docker default
-    encrypt: {}.hasOwnProperty.call(process.env, 'SQL_ENCRYPT') ? toBool(process.env.SQL_ENCRYPT) : true
-  },
-  Migrator: {
-    Timeout: parseInt(process.env.SQL_MIGRATION_TIMEOUT, 10) || twoMinutesInMilliseconds
-  },
-  PupilCensus: {
-    Username: process.env.SQL_PUPIL_CENSUS_USER || 'CensusImportUser',
-    Password: process.env.SQL_PUPIL_CENSUS_USER_PASSWORD
-  },
-  Azure: {
-    Scale: process.env.SQL_AZURE_SCALE
+  Sql: {
+    Database: sql.database,
+    Server: sql.server,
+    Port: sql.port,
+    requestTimeout: sql.requestTimeout,
+    connectionTimeout: sql.connectionTimeout,
+    Encrypt: sql.options.encrypt,
+    Application: {
+      Name: sql.options.appName,
+      Username: sql.user,
+      Password: sql.password
+    },
+    Pooling: {
+      MinCount: sql.pool.min,
+      MaxCount: sql.pool.max,
+      // DEPRECATED - not supported in MSSQL
+      LoggingEnabled: {}.hasOwnProperty.call(process.env, 'SQL_POOL_LOG_ENABLED') ? toBool(process.env.SQL_POOL_LOG_ENABLED) : false
+    },
+    Migrator: {
+      Username: process.env.SQL_ADMIN_USER || 'sa', // docker default
+      Password: process.env.SQL_ADMIN_USER_PASSWORD || 'Mtc-D3v.5ql_S3rv3r', // docker default
+      Timeout: parseInt(process.env.SQL_MIGRATION_TIMEOUT, 10) || twoMinutesInMilliseconds
+    },
+    PupilCensus: {
+      Username: process.env.SQL_PUPIL_CENSUS_USER || 'CensusImportUser',
+      Password: process.env.SQL_PUPIL_CENSUS_USER_PASSWORD
+    },
+    Azure: {
+      Scale: process.env.SQL_AZURE_SCALE
+    },
+    AllowReadsFromReplica: {}.hasOwnProperty.call(process.env, 'SQL_ALLOW_REPLICA_FOR_READS') ? toBool(process.env.SQL_ALLOW_REPLICA_FOR_READS) : false
   },
   DatabaseRetry: {
     MaxRetryAttempts: parseInt(process.env.RETRY_MAX_ATTEMPTS, 10) || 3,
---
+++ infra/sql.config.js
@@ -0,0 +1,27 @@
+'use strict'
+
+require('dotenv').config()
+const toBool = require('to-bool')
+
+const thirtySecondsInMilliseconds = 30000
+const oneMinuteInMilliseconds = 60000
+
+module.exports = {
+  database: process.env.SQL_DATABASE || 'mtc',
+  server: process.env.SQL_SERVER || 'localhost',
+  // Required for when SQL_PORT is passed in via docker-compose
+  port: process.env.SQL_PORT ? parseInt(process.env.SQL_PORT) : 1433,
+  requestTimeout: parseInt(process.env.SQL_REQUEST_TIMEOUT, 10) || oneMinuteInMilliseconds,
+  connectionTimeout: parseInt(process.env.SQL_CONNECT_TIMEOUT, 10) || thirtySecondsInMilliseconds,
+  user: process.env.SQL_APP_USER || 'mtcAdminUser', // docker default
+  password: process.env.SQL_APP_USER_PASSWORD || 'your-chosen*P4ssw0rd_for_dev_env!', // docker default
+  pool: {
+    min: process.env.SQL_POOL_MIN_COUNT || 0,
+    max: process.env.SQL_POOL_MAX_COUNT || 5,
+    acquireTimeoutMillis: parseInt(process.env.SQL_POOL_ACQUIRE_TIMEOUT, 10) || thirtySecondsInMilliseconds
+  },
+  options: {
+    appName: process.env.SQL_APP_NAME || 'mtc-local-dev', // docker default
+    encrypt: {}.hasOwnProperty.call(process.env, 'SQL_ENCRYPT') ? toBool(process.env.SQL_ENCRYPT) : true
+  }
+}
