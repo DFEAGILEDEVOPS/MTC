63560db2e5dbd09d2aabb1fbbadc2c9116db1794
Guy Harwood <guy@milkchip.com>
2019-12-20 16:17:12 +0000

    scripts to insert pupils and schools

---
+++ infra/data/pupil.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const sql = require('mssql')
+const config = require('../config')
+const uuid = require('uuid/v4')
+
+const pupilCountPerSchool = 30
+let schoolCount = 10000
+let schoolId = 6
+
+const table = new sql.Table('mtc_admin.pupil')
+table.create = false
+table.columns.add('school_id', sql.Int, { nullable: false })
+table.columns.add('foreName', sql.NVarChar, { length: 128 })
+table.columns.add('lastName', sql.NVarChar, { length: 128 })
+table.columns.add('gender', sql.Char, { length: 1, nullable: false })
+table.columns.add('dateOfBirth', sql.DateTimeOffset(3), { nullable: false })
+table.columns.add('upn', sql.Char(13), { nullable: false })
+
+for (let schoolIdx = 0; schoolIdx < schoolCount; schoolIdx++) {
+  for (let pupilIndex = 0; pupilIndex < pupilCountPerSchool; pupilIndex++) {
+    table.rows.add(schoolId, `pupil ${pupilIndex + 1}`, `pupil ${pupilIndex + 1}`,
+      'F', new Date('2009-01-01'), genFakeUpn())
+    schoolId++
+  }
+}
+
+const pool = new sql.ConnectionPool(config.Sql)
+pool.connect()
+  .then((x) => {
+    console.log('connected')
+    console.log(`inserting ${pupilCountPerSchool} pupils into ${schoolCount} schools...`)
+    const request = new sql.Request(pool)
+    request.bulk(table, (err, result) => {
+      if (err) {
+        console.error(err.message)
+        process.exit(-1)
+      }
+      console.log('all done')
+      process.exit(0)
+    })
+  })
+
+function genFakeUpn () {
+  var id = uuid()
+  return id.replace('-', '').substr(0, 13)
+}
---
+++ infra/data/school.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const sql = require('mssql')
+const config = require('../config')
+
+const schoolCount = 20000
+
+const table = new sql.Table('mtc_admin.school')
+table.create = false
+table.columns.add('leaCode', sql.Int)
+table.columns.add('estabCode', sql.NVarChar, { length: 'max' })
+table.columns.add('name', sql.NVarChar, { length: 'max', nullable: false })
+table.columns.add('urn', sql.Int, { nullable: false })
+table.columns.add('dfeNumber', sql.Int, { nullable: false })
+for (let idx = 0; idx < schoolCount; idx++) {
+  table.rows.add(777, 'estab', `school ${idx + 1}`, idx + 1, idx + 1)
+}
+
+const pool = new sql.ConnectionPool(config.Sql)
+pool.connect()
+  .then((x) => {
+    console.log('connected')
+    console.log(`inserting ${schoolCount} schools...`)
+    const request = new sql.Request(pool)
+    request.bulk(table, (err, result) => {
+      if (err) {
+        console.error(err.message)
+        process.exit(-1)
+      }
+      console.log('all done')
+      process.exit(0)
+    })
+  })
