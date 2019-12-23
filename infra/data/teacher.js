4f43c0cd449ea1d302d34bbf6704d9b6414ee99d
Guy Harwood <guy@milkchip.com>
2019-12-23 09:52:05 +0000

    create teacher user accounts

---
+++ infra/data/teacher.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const sql = require('mssql')
+const config = require('../config')
+
+const teacherCount = 50000
+const password = '$2a$10$.WsawgZpWSAQVaa6Vz3P1.XO.1YntYJLd6Da5lrXCAkVxhhLpkOHK'
+const teacherRoleId = 3
+
+const table = new sql.Table('mtc_admin.user')
+table.create = false
+table.columns.add('identifier', sql.NVarChar(64), { nullable: false })
+table.columns.add('passwordHash', sql.NVarChar, { length: 'max' })
+table.columns.add('school_id', sql.Int)
+table.columns.add('role_id', sql.Int, { nullable: false })
+
+for (let idx = 0; idx < teacherCount; idx++) {
+  table.rows.add(`loadtest-teacher${idx + 1}`, password, idx + 1, teacherRoleId)
+}
+
+const pool = new sql.ConnectionPool(config.Sql)
+pool.connect()
+  .then((x) => {
+    console.log('connected')
+    console.log(`inserting ${teacherCount} teachers...`)
+    const request = new sql.Request(pool)
+    request.bulk(table, (err, result) => {
+      if (err) {
+        console.error(err.message)
+        process.exit(-1)
+      }
+      console.log('all done')
+      process.exit(0)
+    })
+  })
