64c719df4275f94899a347911e3a95caa3a4f263
Guy Harwood <guy@milkchip.com>
2020-01-13 09:22:43 +0000

    remove old compose files and migrations

--- admin/docker-start.sh
+++ admin/docker-start.sh
@@ -3,17 +3,5 @@
 echo "Starting SSH..."
 service ssh start
 
-echo "migrate is $MIGRATE."
-if [ "$MIGRATE" == "1" ]
-then
-  echo "running database migrations..."
-  yarn migrate-sql
-fi
-if [ "$SEED" == "1" ]
-then
-  echo "running database seeds..."
-  yarn seed-sql
-fi
-
 echo "Starting PM2..."
 pm2-docker start pm2.json
