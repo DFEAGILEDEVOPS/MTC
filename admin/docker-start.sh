49128de1537b4c7dfce77269b652c6913087fc0a
Guy Harwood <GuyHarwood@users.noreply.github.com>
2020-01-14 09:38:22 +0000

    Spike/38838 Add SSH support to azure docker images (#1465)
    * add dockerfile setup
    
    * working ssh support
    
    * add to spa and api too
    
    * refine package install
    
    * start ssh first
    
    * add docs for ssh
    
    * remove old compose files and migrations
    
    * put back vso scriptsg
    
    * rehouse scripts for pr validation vso job
    
    * fix port config
    
    * rename pm2 file in line with azure expectations
    
    * revert pm2 file
    
    * debug msg

--- admin/docker-start.sh
+++ admin/docker-start.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-echo "migrate is $MIGRATE."
-if [ "$MIGRATE" == "1" ]
-then
-  echo "running database migrations..."
-  yarn migrate-sql
-fi
-if [ "$SEED" == "1" ]
-then
-  echo "running database seeds..."
-  yarn seed-sql
-fi
+echo "Starting SSH..."
+service ssh start
 
+echo "Starting PM2..."
 pm2-docker start pm2.json
