6b40e6b16cf2b2eaf967851ffb246f1df5ac3eda
Jon Shipley <js@ovix.co.uk>
2020-01-15 14:41:42 +0000

    [admin] [pupil-api] 2020 mode is the new default
    Please DO NOT run the old `functions`
    Please DO run the new `func-consumption` functions

--- pupil-api/src/config.ts
+++ pupil-api/src/config.ts
@@ -44,7 +44,7 @@ export default {
   },
   RedisPreparedCheckExpiryInSeconds: parseToInt(process.env.PREPARED_CHECK_EXPIRY_SECONDS, 10) || 1800,
   FeatureToggles: {
-    _2020Mode: {}.hasOwnProperty.call(process.env, 'FEATURE_TOGGLE_2020_MODE') ? toBool(process.env.FEATURE_TOGGLE_2020_MODE) : false
+    _2020Mode: {}.hasOwnProperty.call(process.env, 'FEATURE_TOGGLE_2020_MODE') ? toBool(process.env.FEATURE_TOGGLE_2020_MODE) : true
   },
   ServiceBus: {
     connectionString: process.env.SERVICE_BUS_CONNECTION_STRING
