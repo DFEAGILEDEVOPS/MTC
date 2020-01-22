16807d2f0afb23d7c82d8c51a6043c4bdd6aca19
George Chatzigiannis <geohatzigiannis@gmail.com>
2020-01-22 12:25:17 +0000

    fixed test issues

--- pupil-spa/src/app/page-visibility/page-visibility.component.spec.ts
+++ pupil-spa/src/app/page-visibility/page-visibility.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { AuditService } from '../services/audit/audit.service';
 import { AuditServiceMock } from '../services/audit/audit.service.mock';
@@ -10,16 +11,22 @@ describe('PageVisibility', () => {
   let component: PageVisibilityComponent;
   let fixture: ComponentFixture<PageVisibilityComponent>;
   let mockAuditService;
+  let mockRouter;
 
   beforeEach(async(() => {
+    mockRouter = {
+      url: ''
+    };
     const injector = TestBed.configureTestingModule({
       declarations: [PageVisibilityComponent],
       schemas: [NO_ERRORS_SCHEMA],
       providers: [
-        {provide: AuditService, useClass: AuditServiceMock}
+        {provide: AuditService, useClass: AuditServiceMock},
+        {provide: Router, useValue: mockRouter}
       ],
     });
     mockAuditService = injector.get(AuditService);
+    mockRouter = injector.get(Router);
   }));
 
   beforeEach(() => {
@@ -30,6 +37,24 @@ describe('PageVisibility', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#unloadNotification', () => {
+    beforeEach(() => {
+      mockRouter.url = '';
+    });
+    it('should call auditService addEntry when url is other than /check', async () => {
+      spyOn(mockAuditService, 'addEntry');
+      mockRouter.url = '/sign-in-success';
+      component.unloadNotification();
+      expect(mockAuditService.addEntry).toHaveBeenCalled();
+    });
+    it('should not call auditService addEntry when url is /check', async () => {
+      spyOn(mockAuditService, 'addEntry');
+      mockRouter.url = '/check';
+      component.unloadNotification();
+      expect(mockAuditService.addEntry).not.toHaveBeenCalled();
+    });
   });
 
   describe('#visibilityChange', () => {
