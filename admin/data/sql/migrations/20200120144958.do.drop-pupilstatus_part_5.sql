c31f98997ae6ab6ec4fb39fa6b278315ae7d6703
Jon Shipley <js@ovix.co.uk>
2020-01-23 15:54:43 +0000

    [admin] drop index, as req in code review

--- admin/data/sql/migrations/20200120144958.do.drop-pupilstatus_part_5.sql
+++ admin/data/sql/migrations/20200120144958.do.drop-pupilstatus_part_5.sql
@@ -1,10 +1,4 @@
-IF NOT EXISTS (SELECT * FROM sys.indexes i
-                WHERE i.object_ID=object_id('mtc_admin.pupil')
-                  AND name ='idx_azure_recommended_pupil_school')
-    BEGIN
-        CREATE INDEX idx_azure_recommended_pupil_school
-            ON [mtc_admin].[pupil] (school_id) INCLUDE (attendanceId, checkComplete, createdAt, currentCheckId, dateOfBirth,
-                                                foreName, foreNameAlias, gender, group_id, isTestAccount, job_id,
-                                                jwtSecret, jwtToken, lastName, lastNameAlias, middleNames,
-                                                pupilAgeReason_id, updatedAt, upn, urlSlug, version);
-    END
+-- Originally this PR was going to re-create the Azure reccommended index on pupil
+-- following code review comments this is now now longer required, so this note exists
+-- instead of the drop statement as the index has already been dropped.
+
--- admin/data/sql/migrations/20200120144958.undo.drop-pupilstatus_part_5.sql
+++ admin/data/sql/migrations/20200120144958.undo.drop-pupilstatus_part_5.sql
@@ -1,1 +1,1 @@
-DROP INDEX IF EXISTS [mtc_admin].[pupil].[idx_azure_recommended_pupil_school];
+
