ca2397224c657a699bd284c52dc88ce95906d4a0
Jon Shipley <js@ovix.co.uk>
2020-01-24 08:26:49 +0000

    [admin] remove more outdated infra

--- admin/data/sql/migrations/20200120144958.do.drop-pupilstatus_part_5.sql
+++ admin/data/sql/migrations/20200120144958.do.drop-pupilstatus_part_5.sql
@@ -1,4 +1,4 @@
--- Originally this PR was going to re-create the Azure reccommended index on pupil
+-- Originally this PR was going to re-create the Azure recommended index on pupil
 -- following code review comments this is now now longer required, so this note exists
 -- instead of the drop statement as the index has already been dropped.
 
