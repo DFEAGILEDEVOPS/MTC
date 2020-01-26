c6292df07afed2871512262848c131fd560eb510
Guy Harwood <guy@milkchip.com>
2020-01-26 12:44:45 +0000

    exec from API

--- load-test/bin/undo-generate-pupil-load-test-data.sql
+++ load-test/bin/undo-generate-pupil-load-test-data.sql
@@ -4,9 +4,10 @@ DELETE FROM mtc_admin.psychometricianReportCache
 DELETE FROM mtc_admin.pupilRestart
 DELETE FROM mtc_admin.[checkPin]
 DELETE FROM mtc_admin.answer
+UPDATE mtc_admin.pupil SET checkComplete=0, currentCheckId=NULL
+DELETE FROM mtc_admin.[checkConfig]
 DELETE FROM mtc_admin.[check]
 DELETE FROM mtc_admin.auditLog
-UPDATE mtc_admin.pupil SET checkComplete=0
 DELETE FROM mtc_admin.pupilAccessArrangements
 DELETE FROM mtc_admin.pupilAgeReason
 DELETE FROM mtc_admin.pupilAttendance
---
+++ load-test/exec-flood-api.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+
+FLOOD_NAME=$1 # name of stream to run
+FLOOD_REGION=$2
+
+# Check we have the jq binary to make parsing JSON responses a bit easier
+command -v jq >/dev/null 2>&1 || \
+{ echo >&2 "Please install http://stedolan.github.io/jq/download/  Aborting."; exit 1; }
+
+# Start a flood
+echo
+echo "[$(date +%FT%T)+00:00] Starting flood"
+flood_uuid=$(curl -u flood_live_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx: -X POST https://api.flood.io/floods \
+-F "flood[tool]=jmeter" \
+-F "flood[threads]=5" \
+-F "flood[project]=MTC" \
+#-F "flood[privacy]=public" \
+-F "flood[name]=$FLOOD_NAME" \
+-F "flood_files[]=@data.csv" \
+-F "flood_files[]=@basic.jmx" \
+-F "flood[grids][][infrastructure]=demand" \
+-F "flood[grids][][instance_quantity]=1" \
+-F "flood[grids][][region]=$FLOOD_REGION" \
+-F "flood[grids][][instance_type]=m5.xlarge" \
+-F "flood[grids][][stop_after]=15" | jq -r ".uuid")
+
+# Wait for flood to finish
+echo "[$(date +%FT%T)+00:00] Waiting for flood $flood_uuid"
+while [ $(curl --silent --user $FLOOD_API_TOKEN: https://api.flood.io/floods/$flood_uuid | \
+  jq -r '.status == "finished"') = "false" ]; do
+  echo -n "."
+  sleep 3
+done
+
+# Get the summary report
+flood_report=$(curl --silent --user $FLOOD_API_TOKEN: https://api.flood.io/floods/$flood_uuid/report | \
+  jq -r ".summary")
+
+echo
+echo "[$(date +%FT%T)+00:00] Detailed results at https://flood.io/$flood_uuid"
+echo "$flood_report"
+
+# Optionally store the CSV results
+echo
+echo "[$(date +%FT%T)+00:00] Storing CSV results at results.csv"
+curl --silent --user $FLOOD_API_TOKEN: https://api.flood.io/floods/$flood_uuid/result.csv > result.csv
