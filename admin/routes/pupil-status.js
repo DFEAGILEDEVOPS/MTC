bd6686c92f948e4b53a53282ba0b8c3040014a0b
George Chatzigiannis <geohatzigiannis@gmail.com>
2020-01-27 14:23:14 +0000

    comment change

--- admin/routes/pupil-status.js
+++ admin/routes/pupil-status.js
@@ -6,7 +6,7 @@ const isAuthenticated = require('../authentication/middleware')
 const roles = require('../lib/consts/roles')
 const pupilStatusController = require('../controllers/pupil-status')
 
-/* Check Form v2 routing */
+/* Pupil Status routing */
 router.get('/', isAuthenticated([roles.teacher, roles.helpdesk]), (req, res, next) => pupilStatusController.getViewPupilStatus(req, res, next))
 
 module.exports = router
