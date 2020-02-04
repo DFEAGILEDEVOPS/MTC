a0c8575df60098dcb535f7b2f8c5121080263448
George Chatzigiannis <geohatzigiannis@gmail.com>
2020-02-04 14:50:07 +0000

    make undo and do migrations identical to solve the rollback issue. vewPupilStatus cannot be dropped and we cant revert to the previous version either because pupilStatus table does not exist

--- admin/data/sql/migrations/20200128110559.undo.edit-pupil-status-view.sql
+++ admin/data/sql/migrations/20200128110559.undo.edit-pupil-status-view.sql
@@ -1,1 +1,32 @@
-DROP VIEW IF EXISTS [mtc_admin].[vewPupilStatus]
+CREATE OR ALTER VIEW [mtc_admin].[vewPupilStatus] AS (
+    SELECT
+        p.id,
+        p.foreName,
+        p.lastName,
+        p.middleNames,
+        p.dateOfBirth,
+        p.group_id,
+        p.urlSlug,
+        p.school_id,
+        cs.code as checkStatusCode,
+        ac.reason,
+        ac.code as reasonCode,
+
+        -- the following fields are required to produce the pupil status
+        p.attendanceId,
+        p.checkComplete as pupilCheckComplete,
+        p.currentCheckId,
+        p.id as pupilId,
+        p.restartAvailable,
+        chk.received as checkReceived,
+        chk.complete as checkComplete,
+        chk.processingFailed,
+        chk.pupilLoginDate,
+        cp.pinExpiresAt
+    FROM
+        [mtc_admin].[pupil] p LEFT JOIN
+        [mtc_admin].[check] chk ON (p.currentCheckId = chk.id) LEFT JOIN
+        [mtc_admin].[checkStatus] cs ON (chk.checkStatus_id = cs.id) LEFT JOIN
+        [mtc_admin].[attendanceCode] ac ON (p.attendanceId = ac.id) LEFT JOIN
+        [mtc_admin].[checkPin] cp ON (chk.id = cp.check_id)
+);
