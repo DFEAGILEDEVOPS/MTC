a8eed6ba3ba12dcada63c8b481ed0748a4bd5244
George Chatzigiannis <geohatzigiannis@gmail.com>
2020-02-04 12:26:25 +0000

    Undo migration should drop vewPupilStatus since pupilStatus table no longer exists

--- admin/data/sql/migrations/20200128110559.undo.edit-pupil-status-view.sql
+++ admin/data/sql/migrations/20200128110559.undo.edit-pupil-status-view.sql
@@ -1,12 +1,1 @@
-CREATE OR ALTER VIEW [mtc_admin].[vewPupilStatus] AS (
-      SELECT
-              p.id as pupil_id,
-              p.foreName,
-              p.lastName,
-              p.middleNames,
-              p.dateOfBirth,
-              p.gender,
-              ps.code
-      FROM    [mtc_admin].[pupil] p
-              LEFT JOIN [mtc_admin].[pupilStatus] ps ON (p.pupilStatus_id = ps.id)
-);
+DROP VIEW IF EXISTS [mtc_admin].[vewPupilStatus]
