618e41ca1058e61d43491b640394b7a9e2c09e2b
Guy Harwood <guy@milkchip.com>
2020-02-05 16:13:35 +0000

    migrations working up and down

--- admin/data/sql/migrations/20200205100900.do.check-tables-indexes.sql
+++ admin/data/sql/migrations/20200205100900.do.check-tables-indexes.sql
@@ -25,25 +25,17 @@ DROP INDEX IF EXISTS [mtc_admin].[check].[check_receivedByServerAt_index];
 DROP INDEX IF EXISTS [mtc_admin].[check].[check_liveFlag_pupilId_index];
 
 
--- heavily used lookup
-IF EXISTS(SELECT * FROM sys.indexes WHERE object_id = object_id('mtc_admin.[check]') AND NAME ='check_pupil_id_index')
-BEGIN
-    DROP INDEX check_pupil_id_index ON mtc_admin.[check];
-END
-CREATE INDEX [check_pupil_id_index] ON [mtc_admin].[check] ([pupil_id]) INCLUDE ([checkForm_id])
-
-
 -- check config
-IF EXISTS(SELECT * FROM sys.indexes WHERE object_id = object_id('mtc_admin.[check]') AND NAME ='checkConfig_check_id_index')
+IF EXISTS(SELECT * FROM sys.indexes WHERE object_id = object_id('mtc_admin.[checkConfig]') AND NAME ='checkConfig_check_id_index')
 BEGIN
-    DROP INDEX checkConfig_check_id_index ON mtc_admin.[check];
+    DROP INDEX checkConfig_check_id_index ON mtc_admin.[checkConfig];
 END
 CREATE INDEX checkConfig_check_id_index ON mtc_admin.[checkConfig] (check_id)
 
 
 -- checkPin
-IF EXISTS(SELECT * FROM sys.indexes WHERE object_id = object_id('mtc_admin.[check]') AND NAME ='checkPin_pin_id_index')
+IF EXISTS(SELECT * FROM sys.indexes WHERE object_id = object_id('mtc_admin.[checkPin]') AND NAME ='checkPin_pin_id_index')
 BEGIN
-    DROP INDEX checkPin_pin_id_index ON mtc_admin.[check];
+    DROP INDEX checkPin_pin_id_index ON mtc_admin.[checkPin];
 END
 CREATE INDEX checkPin_pin_id_index ON mtc_admin.[checkPin] (pin_id)
--- admin/data/sql/migrations/20200205100900.undo.check-tables-indexes.sql
+++ admin/data/sql/migrations/20200205100900.undo.check-tables-indexes.sql
@@ -1,10 +1,11 @@
 DROP INDEX IF EXISTS [mtc_admin].[check].[check_pupil_id_index]
 DROP INDEX IF EXISTS [mtc_admin].[check].[check_checkForm_id_index]
 DROP INDEX IF EXISTS [mtc_admin].[check].[check_checkStatus_id_index]
+DROP INDEX IF EXISTS [mtc_admin].[checkConfig].[checkConfig_check_id_index]
+DROP INDEX IF EXISTS [mtc_admin].[checkPin].[checkPin_pin_id_index]
+
 
 CREATE INDEX check_receivedByServerAt_index ON mtc_admin.[check] (receivedByServerAt)
 CREATE INDEX check_liveFlag_pupilId_index ON mtc_admin.[check] (isLiveCheck, pupil_id) INCLUDE (checkCode, checkStatus_id)
 
-DROP INDEX IF EXISTS [mtc_admin].[check].[check_pupil_id_index]
-DROP INDEX IF EXISTS [mtc_admin].[check].[checkConfig_check_id_index]
-DROP INDEX IF EXISTS [mtc_admin].[check].[checkPin_pin_id_index]
+
