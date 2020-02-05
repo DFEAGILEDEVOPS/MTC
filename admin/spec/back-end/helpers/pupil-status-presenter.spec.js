68d94744b90dbc3a200a2d06f97a06707a816774
George Chatzigiannis <geohatzigiannis@gmail.com>
2020-02-05 12:16:53 +0000

    improved spec with a larger data set

--- admin/spec/back-end/helpers/pupil-status-presenter.spec.js
+++ admin/spec/back-end/helpers/pupil-status-presenter.spec.js
@@ -5,80 +5,80 @@ const moment = require('moment')
 const pupilStatusPresenter = require('../../../helpers/pupil-status-presenter')
 
 describe('pupilStatusPresenter', () => {
+  const pupils = [
+    {
+      status: 'Error in processing'
+    },
+    {
+      status: 'Error in processing'
+    },
+    {
+      status: 'Not started'
+    },
+    {
+      status: 'Not started'
+    },
+    {
+      status: 'Not started'
+    },
+    {
+      status: 'Incomplete'
+    },
+    {
+      status: 'Processing'
+    },
+    {
+      reason: 'Incorrect registration'
+    },
+    {
+      reason: 'Just arrived with EAL'
+    },
+    {
+      reason: 'Incorrect registration'
+    },
+    {
+      reason: 'Just arrived with EAL'
+    },
+    {
+      reason: 'Incorrect registration'
+    },
+    {
+      reason: 'Just arrived with EAL'
+    },
+    {
+      status: 'Complete'
+    }
+  ]
   describe('getPresentationData', () => {
     const checkWindowData = {
       checkStartDate: moment.utc().subtract(3, 'days'),
       checkEndDate: moment.utc().add(1, 'days')
     }
     it('collects pupils with errors', () => {
-      const pupils = [
-        {
-          status: 'Error in processing'
-        },
-        {
-          status: 'Incomplete'
-        }
-      ]
       const pupilStatusViewData = pupilStatusPresenter.getPresentationData(pupils, checkWindowData)
-      expect(pupilStatusViewData.pupilsWithErrors.length).toBe(2)
+      expect(pupilStatusViewData.pupilsWithErrors.length).toBe(3)
     })
     it('collects pupils that have not started', () => {
-      const pupils = [
-        {
-          status: 'Not started'
-        },
-        {
-          status: 'Processing'
-        }
-      ]
       const pupilStatusViewData = pupilStatusPresenter.getPresentationData(pupils, checkWindowData)
-      expect(pupilStatusViewData.pupilsNotStarted.length).toBe(2)
+      expect(pupilStatusViewData.pupilsNotStarted.length).toBe(4)
     })
     it('collects pupils that are not attending', () => {
-      const pupils = [
-        {
-          reason: 'Incorrect registration'
-        },
-        {
-          reason: 'Just arrived with EAL'
-        }
-      ]
       const pupilStatusViewData = pupilStatusPresenter.getPresentationData(pupils, checkWindowData)
-      expect(pupilStatusViewData.pupilsNotAttending.length).toBe(2)
+      expect(pupilStatusViewData.pupilsNotAttending.length).toBe(6)
     })
     it('collects pupils that are complete', () => {
-      const pupils = [
-        {
-          status: 'Complete'
-        }
-      ]
       const pupilStatusViewData = pupilStatusPresenter.getPresentationData(pupils, checkWindowData)
       expect(pupilStatusViewData.pupilsCompleted.length).toBe(1)
     })
     it('collects total pupil count', () => {
-      const pupils = [
-        {
-          status: 'Complete'
-        }
-      ]
       const pupilStatusViewData = pupilStatusPresenter.getPresentationData(pupils, checkWindowData)
-      expect(pupilStatusViewData.totalPupilsCount).toBe(1)
+      expect(pupilStatusViewData.totalPupilsCount).toBe(14)
     })
     it('collects formatted checkEndDate', () => {
-      const pupils = [
-        {
-          status: 'Complete'
-        }
-      ]
       const pupilStatusViewData = pupilStatusPresenter.getPresentationData(pupils, checkWindowData)
       expect(pupilStatusViewData.liveCheckEndDate).toBe(checkWindowData.checkEndDate.format('D MMMM YYYY'))
     })
     it('collects remaining live check days', () => {
-      const pupils = [
-        {
-          status: 'Complete'
-        }
-      ]
       const pupilStatusViewData = pupilStatusPresenter.getPresentationData(pupils, checkWindowData)
       expect(pupilStatusViewData.remainingLiveCheckDays).toBe(checkWindowData.checkEndDate.diff(checkWindowData.checkStartDate, 'days'))
     })
