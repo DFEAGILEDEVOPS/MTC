3ea3458853d6aa69d3afd1d2664f3776c8f4f9cb
Guy Harwood <guy@milkchip.com>
2020-02-06 06:24:10 +0000

    update cleardown script

--- load-test/bin/undo-generate-pupil-load-test-data.sql
+++ load-test/bin/undo-generate-pupil-load-test-data.sql
@@ -1,4 +1,3 @@
-
 truncate table mtc_admin.psychometricianReportCache
 truncate table mtc_admin.pupilRestart
 UPDATE mtc_admin.pupil SET checkComplete=0, currentCheckId=NULL
@@ -7,8 +6,9 @@ truncate table mtc_admin.checkConfig
 DELETE FROM mtc_admin.[check]
 truncate table mtc_admin.auditLog
 truncate table mtc_admin.pupilAccessArrangements
-DELETE FROM mtc_admin.pupilAgeReason
 truncate table mtc_admin.pupilAttendance
+/* only enable if part of scenario
 DELETE FROM mtc_admin.pupilColourContrasts
 DELETE FROM mtc_admin.pupilFontSizes
 DELETE FROM mtc_admin.[group]
+*/
