9f8a21a62e2e19c38cd279b144da4ddf643e09c6
Guy Harwood <guy@milkchip.com>
2020-02-07 11:35:12 +0000

    wip

--- tslib/src/functions/school-pin-generator/school-pin-replenishment.service.spec.ts
+++ tslib/src/functions/school-pin-generator/school-pin-replenishment.service.spec.ts
@@ -1,7 +1,9 @@
-import { SchoolPinReplenishmnentService, ISchoolPinReplenishmentDataService } from './school-pin-replenishment.service'
+import { SchoolPinReplenishmnentService, ISchoolPinReplenishmentDataService, UtcOffsetResolver, School, SchoolPinUpdate } from './school-pin-replenishment.service'
+import moment from 'moment'
 
 const SchoolPinGeneratorDataServiceMock = jest.fn<ISchoolPinReplenishmentDataService, any>(() => ({
-  getSchools: jest.fn()
+  getSchoolData: jest.fn(),
+  updatePin: jest.fn()
 }))
 
 let sut: SchoolPinReplenishmnentService
@@ -18,7 +20,64 @@ describe('school-pin-replenishment.service', () => {
     expect(sut).toBeDefined()
   })
 
-  it('should create a new pin for each school that meets schedule predicate', () => {
-    fail('not implemented')
+  it('should create a new pin for each school that requires one', async () => {
+    const oneHourAgo = moment().add(-1, 'hours')
+    const schools: School[] = [
+      {
+        id: 1,
+        name: 'school 1'
+      },
+      {
+        id: 2,
+        name: 'school 2',
+        pinExpiresAt: oneHourAgo.toDate(),
+        pin: 'abc12def'
+      },
+      {
+        id: 3,
+        name: 'school 3'
+      }
+    ]
+    dataService.getSchoolData = jest.fn(async () => {
+      return schools
+    })
+    let update: SchoolPinUpdate | undefined
+    dataService.updatePin = jest.fn(async (schoolUpdate) => {
+      update = schoolUpdate
+    })
+    await sut.process()
+    expect(dataService.updatePin).toHaveBeenCalledTimes(1)
+    expect(update).toBeDefined()
+    expect(update?.id).toEqual(2)
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+describe('utc-offset-resolver', () => {
+  let sut: UtcOffsetResolver
+  beforeEach(() => {
+    sut = new UtcOffsetResolver()
+  })
+
+  test('should be defined', () => {
+    expect(sut).toBeDefined()
+  })
+
+  test('it should resolve utc offset correctly', () => {
+    const tz = 'Asia/Tokyo'
+    const expected = 9
+    const actual = sut.resolveToHours(tz)
+    expect(actual).toEqual(expected)
   })
 })
--- tslib/src/functions/school-pin-generator/school-pin-replenishment.service.ts
+++ tslib/src/functions/school-pin-generator/school-pin-replenishment.service.ts
@@ -1,32 +1,68 @@
-
+import tz from 'moment-timezone'
 export class SchoolPinReplenishmnentService {
 
   private dataService: ISchoolPinReplenishmentDataService
 
-  constructor(dataService?: ISchoolPinReplenishmentDataService) {
+  constructor (dataService?: ISchoolPinReplenishmentDataService) {
     if (dataService === undefined) {
       dataService = new SchoolPinReplenishmentDataService()
     }
     this.dataService = dataService
   }
+
+  async process (): Promise<void> {
+    const x = await this.dataService.getSchoolData()
+    throw new Error(x.toString())
+  }
 }
 
 export class SchoolPinReplenishmentDataService implements ISchoolPinReplenishmentDataService {
-  getSchools (): School[] {
+  getSchoolData (): Promise<School[]> {
+/*     const getSchoolSql = `
+    SELECT s.id, s.name,  s.pinExpiresAt, s.pin, sce.id, sce.timezone
+    FROM mtc_admin.school s
+    LEFT OUTER JOIN mtc_admin.sce ON s.id = sce.school_id` */
     throw new Error('Method not implemented.')
   }
 
-  setPins (schools: Array<School>) {
+  updatePin (school: SchoolPinUpdate): Promise<void> {
     throw new Error('not implemented')
   }
 }
 
 export interface ISchoolPinReplenishmentDataService {
-  getSchools (): Array<School>
+  getSchoolData (): Promise<School[]>
+  updatePin (schoolPinUpdate: SchoolPinUpdate): Promise<void>
 }
 
 export interface School {
   id: number
   name: string
-  utcOffset: number
+  pinExpiresAt?: Date
+  pin?: string
+  sceId?: number
+  timezone?: string
+}
+
+export interface SchoolPinUpdate {
+  id: number
+  newPin: string
+  pinExpiresAt: Date
+}
+
+export class SchoolPinGenerator {
+  generate (): string {
+    return 'abc12def'
+  }
+}
+
+export interface ISchoolRequiresNewPinPredicate {
+  (currentPinExpiresAt?: Date, pin?: string): boolean
+}
+
+export class UtcOffsetResolver {
+  resolveToHours (timezone: string): number {
+    const minutesOffset = tz.tz(timezone).utcOffset() // .format('Z')
+    return minutesOffset / 60
+  }
 }
