ae8e8b39393edfd3c6efd0e1a005da3720292bf7
Jon Shipley <js@ovix.co.uk>
2020-02-11 11:09:32 +0000

    [admin] remove migration
    Original view is fine and not what DataGrip was showing so the migration was just not needed.
    
    Thanks for that DataGrip.

--- admin/data/sql/migrations/20200207124759.undo.update-vew-pupil-status.sql
+++
@@ -1,7 +0,0 @@
--- helper view for DBAs / developers
-
-CREATE OR ALTER VIEW [mtc_admin].[vewPupilStatus]
-AS
-(
-SELECT p.id AS pupil_id, p.foreName, p.lastName, p.middleNames, p.dateOfBirth, p.gender
-FROM [mtc_admin].[pupil] p )
