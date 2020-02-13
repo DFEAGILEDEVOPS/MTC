76da06585c9302c4a7c630e9f3bbe871d18aafbb
Guy Harwood <GuyHarwood@users.noreply.github.com>
2020-02-13 16:26:42 +0000

    support/useful-queries (#1528)
    * diagnostic queries
    
    * check summary query & notes on pin gen
    
    * add offset support for bulk data creation

---
+++ support/sql-azure/index-unused.sql
@@ -0,0 +1,39 @@
+-- Unused Index Script
+-- Original Author: Pinal Dave 
+-- https://blog.sqlauthority.com/2011/01/04/sql-server-2008-unused-index-script-download/
+/*
+ Please note, if you should not drop all the unused indexes this script suggest.
+ This is just for guidance. You should not create more than 5-10 indexes per table.
+ Additionally, this script sometime does not give accurate information so use your common sense.
+---------------------------------------------------------------------------------------------------------
+ You should pay attention to User Scan, User Lookup and User Update when you are going to drop the index.
+ The generic understanding is if this value is all high and User Seek is low, the index needs tuning.
+ The index drop script is also provided in the last column.
+ */
+SELECT TOP 25
+o.name AS ObjectName
+, i.name AS IndexName
+, i.index_id AS IndexID
+, dm_ius.user_seeks AS UserSeek
+, dm_ius.user_scans AS UserScans
+, dm_ius.user_lookups AS UserLookups
+, dm_ius.user_updates AS UserUpdates
+, p.TableRows
+, 'DROP INDEX ' + QUOTENAME(i.name)
++ ' ON ' + QUOTENAME(s.name) + '.'
++ QUOTENAME(OBJECT_NAME(dm_ius.OBJECT_ID)) AS 'drop statement'
+FROM sys.dm_db_index_usage_stats dm_ius
+INNER JOIN sys.indexes i ON i.index_id = dm_ius.index_id 
+AND dm_ius.OBJECT_ID = i.OBJECT_ID
+INNER JOIN sys.objects o ON dm_ius.OBJECT_ID = o.OBJECT_ID
+INNER JOIN sys.schemas s ON o.schema_id = s.schema_id
+INNER JOIN (SELECT SUM(p.rows) TableRows, p.index_id, p.OBJECT_ID
+FROM sys.partitions p GROUP BY p.index_id, p.OBJECT_ID) p
+ON p.index_id = dm_ius.index_id AND dm_ius.OBJECT_ID = p.OBJECT_ID
+WHERE OBJECTPROPERTY(dm_ius.OBJECT_ID,'IsUserTable') = 1
+AND dm_ius.database_id = DB_ID()
+AND i.type_desc = 'nonclustered'
+AND i.is_primary_key = 0
+AND i.is_unique_constraint = 0
+ORDER BY (dm_ius.user_seeks + dm_ius.user_scans + dm_ius.user_lookups) ASC
+GO
