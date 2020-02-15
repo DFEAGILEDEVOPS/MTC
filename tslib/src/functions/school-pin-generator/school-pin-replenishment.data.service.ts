fa5620eed1a2a4ef827255bf45fa2553c9a8e5db
Guy Harwood <guy@milkchip.com>
2020-02-15 12:45:41 +0000

    date filter

--- tslib/src/functions/school-pin-generator/school-pin-replenishment.data.service.ts
+++ tslib/src/functions/school-pin-generator/school-pin-replenishment.data.service.ts
@@ -15,7 +15,8 @@ export class SchoolPinReplenishmentDataService implements ISchoolPinReplenishmen
     const sql = `
     SELECT s.id, s.name,  s.pinExpiresAt, s.pin, sce.id, sce.timezone
     FROM mtc_admin.school s
-    LEFT OUTER JOIN mtc_admin.sce ON s.id = sce.school_id`
+    LEFT OUTER JOIN mtc_admin.sce ON s.id = sce.school_id
+    WHERE s.pinExpiresAt <= GETUTCDATE()`
     return this.sqlService.query(sql)
   }
   updatePin (school: SchoolPinUpdate): Promise<void> {
