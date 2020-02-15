3cb27718df6eb3d165b9249b9f01247870a8c698
Guy Harwood <guy@milkchip.com>
2020-02-15 17:39:18 +0000

    enable test data to already have a fresh pin

--- admin/data/sql/seeds/20181116124850.custom.sample-user-school.sql
+++ admin/data/sql/seeds/20181116124850.custom.sample-user-school.sql
@@ -1,7 +1,7 @@
 DECLARE @schoolId INT
 
 BEGIN TRY
-INSERT INTO [mtc_admin].school (leaCode, estabCode, urn, dfeNumber, name) VALUES (999, '1001', 89001, 9991001, 'Example School One');
+INSERT INTO [mtc_admin].school (leaCode, estabCode, urn, dfeNumber, name, pin, pinExpiresAt) VALUES (999, '1001', 89001, 9991001, 'Example School One', 'foo12foo', DATEADD(DAY, 1, GETUTCDATE()));
 SELECT @schoolId = SCOPE_IDENTITY()
 END TRY
 BEGIN CATCH
@@ -11,7 +11,7 @@ INSERT INTO [mtc_admin].[user] (identifier, passwordHash, school_id, role_id)
   VALUES ('teacher1', '$2a$10$.WsawgZpWSAQVaa6Vz3P1.XO.1YntYJLd6Da5lrXCAkVxhhLpkOHK', @schoolId, 3);
 
 BEGIN TRY
-INSERT INTO [mtc_admin].school (leaCode, estabCode, urn, dfeNumber, name) VALUES (999, '1002', 89002, 9991002, 'Example School Two');
+INSERT INTO [mtc_admin].school (leaCode, estabCode, urn, dfeNumber, name, pin, pinExpiresAt) VALUES (999, '1002', 89002, 9991002, 'Example School Two', 'bar12bar', DATEADD(DAY, 1, GETUTCDATE()));
 SELECT @schoolId = SCOPE_IDENTITY()
 END TRY
 BEGIN CATCH
@@ -21,7 +21,7 @@ INSERT INTO [mtc_admin].[user] (identifier, passwordHash, school_id, role_id)
   VALUES ('teacher2', '$2a$10$.WsawgZpWSAQVaa6Vz3P1.XO.1YntYJLd6Da5lrXCAkVxhhLpkOHK', @schoolId, 3);
 
 BEGIN TRY
-INSERT INTO [mtc_admin].school (leaCode, estabCode, urn, dfeNumber, name) VALUES (999, '1003', 89003, 9991003, 'Example School Three');
+INSERT INTO [mtc_admin].school (leaCode, estabCode, urn, dfeNumber, name, pin, pinExpiresAt) VALUES (999, '1003', 89003, 9991003, 'Example School Three', 'baz12baz', DATEADD(DAY, 1, GETUTCDATE()));
 SELECT @schoolId = SCOPE_IDENTITY()
 END TRY
 BEGIN CATCH
@@ -31,7 +31,7 @@ INSERT INTO [mtc_admin].[user] (identifier, passwordHash, school_id, role_id)
   VALUES ('teacher3', '$2a$10$.WsawgZpWSAQVaa6Vz3P1.XO.1YntYJLd6Da5lrXCAkVxhhLpkOHK', @schoolId, 3);
 
 BEGIN TRY
-INSERT INTO [mtc_admin].school (leaCode, estabCode, urn, dfeNumber, name) VALUES (999, '1004', 89004, 9991004, 'Example School Four');
+INSERT INTO [mtc_admin].school (leaCode, estabCode, urn, dfeNumber, name, pin, pinExpiresAt) VALUES (999, '1004', 89004, 9991004, 'Example School Four', 'qux12qux', DATEADD(DAY, 1, GETUTCDATE()));
 SELECT @schoolId = SCOPE_IDENTITY()
 END TRY
 BEGIN CATCH
@@ -41,7 +41,7 @@ INSERT INTO [mtc_admin].[user] (identifier, passwordHash, school_id, role_id)
   VALUES ('teacher4', '$2a$10$.WsawgZpWSAQVaa6Vz3P1.XO.1YntYJLd6Da5lrXCAkVxhhLpkOHK', @schoolId, 3);
 
 BEGIN TRY
-INSERT INTO [mtc_admin].school (leaCode, estabCode, urn, dfeNumber, name) VALUES (999, '1005', 89005, 9991005, 'Example School Five');
+INSERT INTO [mtc_admin].school (leaCode, estabCode, urn, dfeNumber, name, pin, pinExpiresAt) VALUES (999, '1005', 89005, 9991005, 'Example School Five', 'qix12qix', DATEADD(DAY, 1, GETUTCDATE()));
 SELECT @schoolId = SCOPE_IDENTITY()
 END TRY
 BEGIN CATCH
