8c0cfb737cd964f7ed88d4d74425ce80fad093f3
Guy Harwood <guy@milkchip.com>
2020-02-18 13:08:47 +0000

    refactor calls

--- admin/services/check-start.service/check-start.service.js
+++ admin/services/check-start.service/check-start.service.js
@@ -12,7 +12,6 @@ const setValidationService = require('../set-validation.service')
 const checkStartDataService = require('./data-access/check-start.data.service')
 
 // to be moved to the module
-const checkFormAllocationDataService = require('../data-access/check-form-allocation.data.service')
 const checkFormService = require('../check-form.service')
 const configService = require('../config.service')
 const dateService = require('../date.service')
@@ -122,12 +121,11 @@ checkStartService.prepareCheck2 = async function (
   // Create and return checks via spCreateChecks
   const newChecks = await pinGenerationDataService.sqlCreateBatch(checks)
   // phase 1 (will be obsolete when we remove createPupilCheckPayloads)
-  const newCheckIds = newChecks.map(c => c.check_id)
 
   let pupilChecks
   try {
     pupilChecks = await checkStartService
-      .createPupilCheckPayloads(newCheckIds, schoolId)
+      .createPupilCheckPayloads(newChecks, schoolId)
   } catch (error) {
     logger.error('Unable to prepare check messages', error)
     throw error
@@ -149,9 +147,9 @@ checkStartService.storeCheckConfigs = async function (preparedChecks, newChecks)
   }
   const config = preparedChecks.map(pcheck => {
     return {
-      checkCode: pcheck.checkCode,
+      checkCode: pcheck.check_checkCode,
       config: pcheck.config,
-      checkId: newChecks.find(check => check.checkCode === pcheck.checkCode).id
+      checkId: newChecks.find(check => check.check_checkCode === pcheck.checkCode).check_id
     }
   })
   checkStartDataService.sqlStoreBatchConfigs(config)
@@ -216,19 +214,17 @@ checkStartService.initialisePupilCheck = async function (
  * @param {number} schoolId - DB PK - school.id
  * @return {Promise<Array>}
  */
-checkStartService.createPupilCheckPayloads = async function (checkIds, schoolId) {
-  if (!checkIds) {
-    throw new Error('checkIds is not defined')
+checkStartService.createPupilCheckPayloads = async function (checks, schoolId) {
+  if (!checks) {
+    throw new Error('checks is not defined')
   }
 
-  if (!Array.isArray(checkIds)) {
-    throw new Error('checkIds must be an array')
+  if (!Array.isArray(checks)) {
+    throw new Error('checks must be an array')
   }
 
   const payloads = []
-  const checks = await checkFormAllocationDataService.sqlFindByIdsHydrated(
-    checkIds
-  )
+
   const sasExpiryDate = moment().add(config.Tokens.sasTimeOutHours, 'hours')
 
   const hasLiveChecks = R.all(c => R.equals(c.check_isLiveCheck, true))(checks)
@@ -262,7 +258,7 @@ checkStartService.createPupilCheckPayloads = async function (checkIds, schoolId)
     logger.error('Error generating pupil configs', error)
     throw error
   }
-
+  console.dir(checks[0])
   for (const o of checks) {
     // Pass the isLiveCheck config in to the SPA
     const pupilConfig = pupilConfigs[o.pupil_id]
