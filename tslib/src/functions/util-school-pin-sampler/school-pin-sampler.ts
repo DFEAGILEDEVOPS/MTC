5a691fe66f8ef27c516d381d39f4dcd0e9b15576
Guy Harwood <guy@milkchip.com>
2020-02-19 14:12:47 +0000

    finish sampler

--- tslib/src/functions/util-school-pin-sampler/school-pin-sampler.ts
+++ tslib/src/functions/util-school-pin-sampler/school-pin-sampler.ts
@@ -1,13 +1,13 @@
-import { TimezoneUtil } from '../school-pin-generator/timezone-util'
-
+import * as tzutil from '../school-pin-generator/timezone-util'
 import { SchoolPinGenerator } from '../school-pin-generator/school-pin-generator'
 import { SchoolPinExpiryGenerator } from '../school-pin-generator/school-pin-expiry-generator'
 import { IDateTimeService } from '../../common/datetime.service'
 import moment = require('moment')
+import { RandomGenerator } from '../school-pin-generator/random-generator'
 
 export class SchoolPinSampler {
-  generateSample (size: number, utcNow: moment.Moment): Array<SchoolPinSample> {
-    const tzUtil = new TimezoneUtil()
+  generateSample (size: number, utcNow: moment.Moment, randomiseSample?: boolean): Array<SchoolPinSample> {
+    const tzUtil = new tzutil.TimezoneUtil()
     const pinGenerator = new SchoolPinGenerator()
     const dateTimeService: IDateTimeService = {
       utcNow: () => utcNow,
@@ -16,9 +16,27 @@ export class SchoolPinSampler {
       formatIso8601: () => { throw new Error('no implementation') }
     }
     const expiryGenerator = new SchoolPinExpiryGenerator(dateTimeService)
+    if (randomiseSample === undefined) randomiseSample = false
+    const allZones = tzUtil.getTimezoneList()
 
-    const zones = tzUtil.getTimezoneList().splice(0, size)
-    return zones.map(z => {
+    if (size > allZones.length) {
+      throw new Error(`maximum sample size is ${allZones.length}`)
+    }
+
+    let reducedZoneset: Array<tzutil.Timezone> = []
+
+    if (randomiseSample) {
+      const rangen = new RandomGenerator()
+      for (let index = 0; index < size; index++) {
+        const randomIndex = rangen.generateNumberFromRangeInclusive(0, allZones.length)
+        const randomZone = allZones[randomIndex]
+        reducedZoneset.push(randomZone)
+      }
+    } else {
+      reducedZoneset = allZones.splice(0, size)
+    }
+
+    return reducedZoneset.map((z: tzutil.Timezone) => {
       const upd: SchoolPinSample = {
         pin: pinGenerator.generate(),
         pinExpiresAt: expiryGenerator.generate(),
