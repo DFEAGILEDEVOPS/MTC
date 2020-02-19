71a5ea66350f24323e85c5052e47d0b0cab43ee6
Guy Harwood <guy@milkchip.com>
2020-02-19 14:37:08 +0000

    iron out runtime kinks

--- tslib/src/functions/util-school-pin-sampler/index.ts
+++ tslib/src/functions/util-school-pin-sampler/index.ts
@@ -1,17 +1,22 @@
 
 import { AzureFunction, Context, HttpRequest } from '@azure/functions'
-import * as moment from 'moment'
+import moment from 'moment'
 import { SchoolPinSampler } from './school-pin-sampler'
 
 const schoolPinSampler: AzureFunction = function (context: Context, req: HttpRequest): void {
-
-  const utcNow = req.body.utcnow || moment.utc()
+  if (req.body === undefined) req.body = {}
+  const utcNow = req.body.utcnow ? moment(req.body.utcnow) : moment.utc()
   const sampleSize = req.body.samplesize || 20
   const randomise = req.body.randomise || false
   context.log.info(`creating sample of ${sampleSize} school pins generated at ${utcNow}`)
   const sampler = new SchoolPinSampler()
   const sample = sampler.generateSample(sampleSize, utcNow, randomise)
-  context.res = sample
+  context.res = {
+    body: sample,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
   context.done()
 }
 
