d8f905d36fd60d97fe5009fae000ab853e43e670
Guy Harwood <guy@milkchip.com>
2020-02-19 14:20:11 +0000

    update function scripts

--- tslib/src/functions/util-school-pin-sampler/index.ts
+++ tslib/src/functions/util-school-pin-sampler/index.ts
@@ -1,12 +1,17 @@
 
 import { AzureFunction, Context, HttpRequest } from '@azure/functions'
 import * as moment from 'moment'
+import { SchoolPinSampler } from './school-pin-sampler'
 
 const schoolPinSampler: AzureFunction = function (context: Context, req: HttpRequest): void {
 
-  const utcNow = req.query['utcnow'] || moment.utc()
-  const sampleSize = req.query['samplesize'] || 20
+  const utcNow = req.body.utcnow || moment.utc()
+  const sampleSize = req.body.samplesize || 20
+  const randomise = req.body.randomise || false
   context.log.info(`creating sample of ${sampleSize} school pins generated at ${utcNow}`)
+  const sampler = new SchoolPinSampler()
+  const sample = sampler.generateSample(sampleSize, utcNow, randomise)
+  context.res = sample
   context.done()
 }
 
