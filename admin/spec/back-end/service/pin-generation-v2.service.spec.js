8d676ba3326b934a236b11a5f0fdb83032cdca43
Jon Shipley <jon-shipley@users.noreply.github.com>
2020-03-02 18:02:54 +0000

    Feature/39655 vewpupilseligibleforlivepingeneration is expensive (#1548)
    * [admin] update index
    
    Add more leaf nodes so the vewPupilsElgibileForLivePinGeneration can use it.
    
    * [admin] remove orderby from common sql
    
    * [admin] awkwardly work around a bad test
    
    Co-authored-by: sinhaamit1980 <sinhaamit1980@yahoo.com>

--- admin/spec/back-end/service/pin-generation-v2.service.spec.js
+++ admin/spec/back-end/service/pin-generation-v2.service.spec.js
@@ -1,6 +1,6 @@
 'use strict'
-
-/* global describe it spyOn expect */
+/* global describe it spyOn expect beforeEach */
+const moment = require('moment')
 const pinGenerationDataService = require('../../../services/data-access/pin-generation.data.service')
 const pupilIdentificationFlagService = require('../../../services/pupil-identification-flag.service')
 
@@ -9,19 +9,37 @@ const pinGenerationV2Service = require('../../../services/pin-generation-v2.serv
 
 describe('pin-generation-v2.service', () => {
   describe('#getPupilsEligibleForPinGeneration', () => {
+    beforeEach(() => {
+      const pupils = [
+        { id: 1, lastName: 'Smith', foreName: 'Alfred', middleNames: '', dateOfBirth: moment('2011-01-22T09:00:00') },
+        { id: 2, lastName: 'Smith', foreName: 'Bertie', middleNames: '', dateOfBirth: moment('2011-01-22T09:00:00') },
+        { id: 3, lastName: 'Aardvark', foreName: 'Alfred', middleNames: 'John', dateOfBirth: moment('2011-01-22T09:00:00') },
+        { id: 4, lastName: 'Aardvark', foreName: 'Alfred', middleNames: 'George', dateOfBirth: moment('2011-01-22T09:00:00') }
+      ]
+      spyOn(pinGenerationDataService, 'sqlFindEligiblePupilsBySchool').and.returnValue(pupils)
+    })
+
     it('makes a call to the data service to fetch the pupils', async () => {
-      spyOn(pinGenerationDataService, 'sqlFindEligiblePupilsBySchool')
       spyOn(pupilIdentificationFlagService, 'addIdentificationFlags')
       const schoolId = 42
       await pinGenerationV2Service.getPupilsEligibleForPinGeneration(schoolId)
       expect(pinGenerationDataService.sqlFindEligiblePupilsBySchool).toHaveBeenCalledTimes(1)
     })
+
     it('makes a call to the pupil identification service to get display information for the GUI', async () => {
-      spyOn(pinGenerationDataService, 'sqlFindEligiblePupilsBySchool')
       spyOn(pupilIdentificationFlagService, 'addIdentificationFlags')
       const schoolId = 42
       await pinGenerationV2Service.getPupilsEligibleForPinGeneration(schoolId)
       expect(pupilIdentificationFlagService.addIdentificationFlags).toHaveBeenCalledTimes(1)
+    })
+
+    it('sorts the pupils', async () => {
+      const schoolId = 42
+      const result = await pinGenerationV2Service.getPupilsEligibleForPinGeneration(schoolId, true)
+      expect(result[0].id).toBe(4)
+      expect(result[1].id).toBe(3)
+      expect(result[2].id).toBe(1)
+      expect(result[3].id).toBe(2)
     })
   })
 
