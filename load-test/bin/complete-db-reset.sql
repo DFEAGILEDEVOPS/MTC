c565c027598e42b8557270cd973c9556394f8c64
Guy Harwood <guy@milkchip.com>
2020-03-17 15:28:07 +0000

    db reset script

---
+++ load-test/bin/complete-db-reset.sql
@@ -0,0 +1,43 @@
+
+/************************
+  resets common tables after a load-test
+  some statements should be excluded if you want to build up volume data
+ */
+
+TRUNCATE TABLE mtc_admin.adminLogonEvent
+TRUNCATE TABLE mtc_admin.psychometricianReportCache
+TRUNCATE TABLE mtc_admin.anomalyReportCache
+TRUNCATE TABLE mtc_admin.pupilRestart
+TRUNCATE TABLE mtc_admin.azureBlobFile
+TRUNCATE TABLE mtc_admin.auditLog
+
+-- user data
+TRUNCATE TABLE mtc_admin.answer
+TRUNCATE TABLE mtc_admin.checkPin
+TRUNCATE TABLE mtc_admin.checkConfig
+TRUNCATE TABLE mtc_admin.pupilAccessArrangements
+DELETE FROM mtc_admin.pupilAgeReason
+TRUNCATE TABLE mtc_admin.pupilAttendance
+DELETE FROM mtc_admin.pupilColourContrasts
+DELETE FROM mtc_admin.pupilResultsDiagnosticCache
+DELETE FROM mtc_admin.pupilFontSizes
+DELETE FROM mtc_admin.pupilRestart
+DELETE FROM mtc_admin.[hdf]
+DELETE FROM mtc_admin.[schoolScore]
+DELETE FROM mtc_admin.[serviceMessage]
+
+DELETE FROM mtc_admin.[check]
+DELETE FROM mtc_admin.pupil
+DELETE FROM mtc_admin.[job]
+DELETE FROM mtc_admin.[group]
+-- config
+DELETE FROM mtc_admin.[checkFormWindow]
+DELETE FROM mtc_admin.[checkForm]
+DELETE FROM mtc_admin.checkWindow
+TRUNCATE TABLE mtc_admin.sce
+TRUNCATE TABLE mtc_admin.settingsLog
+
+
+DELETE FROM mtc_admin.[user]
+DELETE FROM mtc_admin.school
+
