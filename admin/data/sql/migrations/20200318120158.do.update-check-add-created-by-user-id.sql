e27cdbe4cbe79051f82a939f187c2613cf8f9b58
George Chatzigiannis <geohatzigiannis@gmail.com>
2020-03-18 13:18:46 +0000

    fixed constraint name

--- admin/data/sql/migrations/20200318120158.do.update-check-add-created-by-user-id.sql
+++ admin/data/sql/migrations/20200318120158.do.update-check-add-created-by-user-id.sql
@@ -1,4 +1,4 @@
 ALTER TABLE [mtc_admin].[check]
 ADD createdBy_userId INT NOT NULL
-CONSTRAINT [FK_check_createdBy_userId_fk] FOREIGN KEY([createdBy_userId])
+CONSTRAINT [FK_check_createdBy_userId] FOREIGN KEY([createdBy_userId])
 REFERENCES [mtc_admin].[user] ([id])
--- admin/data/sql/migrations/20200318120158.undo.update-check-add-created-by-user-id.sql
+++ admin/data/sql/migrations/20200318120158.undo.update-check-add-created-by-user-id.sql
@@ -1,4 +1,4 @@
 ALTER TABLE [mtc_admin].[check]
-DROP CONSTRAINT [FK_check_createdBy_userId_fk];
+DROP CONSTRAINT [FK_check_createdBy_userId];
 ALTER TABLE [mtc_admin].[check]
 DROP COLUMN createdBy_userId;
