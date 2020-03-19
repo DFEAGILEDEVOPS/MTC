d9e31ffd1e6a60b2d9764f3ead8d0fc5d1a3ef31
George Chatzigiannis <geohatzigiannis@gmail.com>
2020-03-19 14:13:18 +0000

    added semi colon

--- admin/data/sql/migrations/20200318120158.do.update-check-add-created-by-user-id.sql
+++ admin/data/sql/migrations/20200318120158.do.update-check-add-created-by-user-id.sql
@@ -1,4 +1,4 @@
 ALTER TABLE [mtc_admin].[check]
-ADD createdBy_userId INT NULL
+ADD createdBy_userId INT NULL;
 CONSTRAINT [FK_check_createdBy_userId] FOREIGN KEY([createdBy_userId])
-REFERENCES [mtc_admin].[user] ([id])
+REFERENCES [mtc_admin].[user] ([id]);
