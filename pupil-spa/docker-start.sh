2a39c63bcd870d7afec7433a70663395a4a49a5f
Guy Harwood <guy@milkchip.com>
2020-03-26 15:43:46 +0000

    add gulp node and yarn to container

--- pupil-spa/docker-start.sh
+++ pupil-spa/docker-start.sh
@@ -4,7 +4,9 @@
 /usr/sbin/sshd
 
 # Always re-generate the config based on the env variables
-sh gen_config.sh && mv config.json /usr/share/nginx/html/public
+cd /usr/share/nginx/html/public
+gulp createConfigFile
+# sh gen_config.sh && mv config.json /usr/share/nginx/html/public
 
 # Update the account name used for the nginx azure proxy
 /bin/sed -i "s/<azure_account_name>/${AZURE_ACCOUNT_NAME}/g" /etc/nginx/conf.d/default.conf
