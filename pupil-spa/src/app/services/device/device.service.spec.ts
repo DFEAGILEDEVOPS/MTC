f39d69bbb221e63715a070d382c3a5b5c7d2e81a
George Chatzigiannis <geohatzigiannis@gmail.com>
2020-03-26 15:31:50 +0000

    Added further tests

--- pupil-spa/src/app/services/device/device.service.spec.ts
+++ pupil-spa/src/app/services/device/device.service.spec.ts
@@ -46,14 +46,43 @@ describe('DeviceService', () => {
     beforeEach(() => {
       windowRefService.nativeWindow.navigator.userAgent =
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome Safari/537.36';
-    })
-    it('returns true when browser is IE9/IE10', () => {
-      windowRefService.nativeWindow.navigator.userAgent = 'Mozilla/5.0 (compatible MSIE 9.0 Windows NT 6.1 Trident/5.0)';
+    });
+    it('returns true when browser is IE9', () => {
+      windowRefService.nativeWindow.navigator.userAgent = 'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)';
+      const isUnsupportedBrowser = service.isUnsupportedBrowser();
+      expect(isUnsupportedBrowser).toBeTruthy();
+    });
+    it('returns true when browser is IE10', () => {
+      windowRefService.nativeWindow.navigator.userAgent = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)';
       const isUnsupportedBrowser = service.isUnsupportedBrowser();
       expect(isUnsupportedBrowser).toBeTruthy();
     });
     it('returns true when browser is IE11', () => {
-      windowRefService.nativeWindow.navigator.userAgent = 'Mozilla/5.0 (Windows NT 10.0 WOW64 Trident/7.0)';
+      windowRefService.nativeWindow.navigator.userAgent = 'Mozilla/5.0 (Windows NT 10.0; Trident/7.0; rv:11.0) like Gecko';
+      const isUnsupportedBrowser = service.isUnsupportedBrowser();
+      expect(isUnsupportedBrowser).toBeFalsy();
+    });
+    it('returns true when browser is Edge version 80.0.361.62', () => {
+      windowRefService.nativeWindow.navigator.userAgent =
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' +
+        'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36 Edg/80.0.361.62';
+      const isUnsupportedBrowser = service.isUnsupportedBrowser();
+      expect(isUnsupportedBrowser).toBeFalsy();
+    });
+    it('returns true when browser is Firefox 74', () => {
+      windowRefService.nativeWindow.navigator.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/74.0';
+      const isUnsupportedBrowser = service.isUnsupportedBrowser();
+      expect(isUnsupportedBrowser).toBeFalsy();
+    });
+    it('returns true when browser is Safari 605.1.15', () => {
+      windowRefService.nativeWindow.navigator.userAgent =
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Safari/605.1.15';
+      const isUnsupportedBrowser = service.isUnsupportedBrowser();
+      expect(isUnsupportedBrowser).toBeFalsy();
+    });
+    it('returns true when browser is Chrome version 80.0.3987.149', () => {
+      windowRefService.nativeWindow.navigator.userAgent =
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36';
       const isUnsupportedBrowser = service.isUnsupportedBrowser();
       expect(isUnsupportedBrowser).toBeFalsy();
     });
