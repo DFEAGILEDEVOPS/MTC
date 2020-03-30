62e76e72c16d2c5c6dbd4d80aa519aa5b82cd53a
Mohsen Qureshi <mjq@Mohsens-Mac-mini.local>
2020-03-30 14:39:12 +0100

    added scenario to capture bug

--- test/admin-hpa/features/step_definitions/declaration_submitted_steps.rb
+++ test/admin-hpa/features/step_definitions/declaration_submitted_steps.rb
@@ -35,3 +35,70 @@ Then(/^I can see the declaration submitted form page not confirmed as per the de
   expect(declaration_submitted_form_page).to_not have_details_confirmed_list
   expect(declaration_submitted_form_page).to have_warning_notconfirmed_panel
 end
+
+Given(/^I have no pupils not taking the check for school 4$/) do
+  SqlDbHelper.delete_pupils_not_taking_check
+  SqlDbHelper.set_pupil_attendance_via_school(5, 'null')
+end
+
+Given(/^I have some pupils that have completed the check$/) do
+  step "I have no pupils not taking the check for school 4"
+  step "I am on the add multiple pupil page"
+  @upn_list = add_multiple_pupil_page.create_and_upload_multiple_pupils(3, 'pin_gen.csv')
+  generate_pins_overview_page.load
+  step 'I click Generate PINs button'
+  @pupil_names_arr = @upn_list.map {|upn| SqlDbHelper.pupil_details(upn)['lastName'] + ', ' + SqlDbHelper.pupil_details(upn)['foreName']}
+  generate_pins_overview_page.generate_pin_using_list_of_names(@pupil_names_arr)
+  step "I am on the generate pupil pins page"
+  @pupil_names_arr.each do |pupil|
+    pupil_lastname = pupil.split(',')[0]
+    pupil_firstname = pupil.split(',')[1].split(' Date')[0].split(' ')[0]
+    pupil_detail = SqlDbHelper.pupil_details_using_names(pupil_firstname, pupil_lastname)
+    pupil_id = pupil_detail['id']
+    check_entry = SqlDbHelper.check_details(pupil_id)
+    pupil_pin_detail = SqlDbHelper.get_pupil_pin(check_entry['id'])
+    pupil_pin = pupil_pin_detail['val']
+    school_password = SqlDbHelper.find_school(pupil_detail['school_id'])['pin']
+
+    Timeout.timeout(ENV['WAIT_TIME'].to_i) {sleep 1 until RequestHelper.auth(school_password, pupil_pin).code == 200}
+    response_pupil_auth = RequestHelper.auth(school_password, pupil_pin)
+    @parsed_response_pupil_auth = JSON.parse(response_pupil_auth.body)
+    AzureQueueHelper.create_check_submission_message(RequestHelper.build_check_submission_message(@parsed_response_pupil_auth).to_json)
+    school_uuid = @parsed_response_pupil_auth['school']['uuid']
+    check_code = @parsed_response_pupil_auth['checkCode']
+    AzureTableHelper.wait_for_received_check(school_uuid, check_code)
+  end
+end
+
+When(/^the live check window closes$/) do
+  @dev_phase_details_before = SqlDbHelper.check_window_details('Development Phase')
+  SqlDbHelper.update_check_end_date((Date.today) - 7)
+  visit ENV['ADMIN_BASE_URL'] + '/sign-out'
+  step "I am logged in with a service manager"
+  admin_page.manage_check_windows.click
+  @check_window = manage_check_window_page.find_check_row('Development Phase')
+  @check_window.check_name.click
+  add_edit_check_window_page.save_changes.click
+end
+
+
+And(/^I set the remaining pupils as not taking the check$/) do
+  visit ENV['ADMIN_BASE_URL'] + '/sign-out'
+  step 'I have signed in with teacher4'
+  pupils_not_taking_check_page.load
+  step 'I want to add a reason'
+  @page = pupil_reason_page
+  step "I select a reason"
+  step "I select all pupil for pupil not taking check"
+  pupil_reason_page.sticky_banner.confirm.click
+end
+
+
+Then(/^I should be able to submit the HDF$/) do
+  step 'I am on the HDF form page'
+  expect(@page).to have_first_name
+  expect(@page).to have_last_name
+  expect(@page).to have_is_headteacher_yes
+  expect(@page).to have_is_headteacher_no
+  expect(@page).to have_continue
+end
