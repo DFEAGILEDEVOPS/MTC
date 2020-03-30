77217b02fc3ae618559a11f85b7808ee82c0fd47
Guy Harwood <guy@milkchip.com>
2020-03-30 13:30:52 +0100

    build out service with mock data

--- admin/controllers/helpdesk-summary.js
+++ admin/controllers/helpdesk-summary.js
@@ -13,8 +13,33 @@ controller.getSummary = async (req, res, next) => {
   res.locals.pageTitle = 'School Summary'
   try {
     const summaryData = {
-      schoolName: 'School One',
-      items: [{}]
+      schoolName: '[School Name]',
+      dfeNumber: req.user.School,
+      register: {
+        completed: 1,
+        started: 3,
+        notTaking: 5
+      },
+      LiveCheckSummary: [
+        {
+          Date: '7th June',
+          PinsGenerated: 59,
+          LoggedIn: 58,
+          StartedCheck: 44
+        },
+        {
+          Date: '14th May',
+          PinsGenerated: 59,
+          LoggedIn: 58,
+          StartedCheck: 44
+        },
+        {
+          Date: '3rd May',
+          PinsGenerated: 59,
+          LoggedIn: 58,
+          StartedCheck: 44
+        }],
+      TioCheckSummary: []
     } // schoolSummaryService.getData(req.user.schoolId)
     req.breadcrumbs(res.locals.pageTitle)
     return res.render('helpdesk/school-summary', {
