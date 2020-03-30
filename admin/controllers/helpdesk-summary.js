5fb8c3e2e7151794a91ec659638465cdbd23e617
Guy Harwood <guy@milkchip.com>
2020-03-30 15:16:16 +0100

    mock summary data

--- admin/controllers/helpdesk-summary.js
+++ admin/controllers/helpdesk-summary.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const schoolSummaryService = require('../services/school-summary.service')
+
 const controller = {}
 
 /**
@@ -12,35 +14,7 @@ const controller = {}
 controller.getSummary = async (req, res, next) => {
   res.locals.pageTitle = 'School Summary'
   try {
-    const summaryData = {
-      schoolName: '[School Name]',
-      dfeNumber: req.user.School,
-      register: {
-        completed: 1,
-        started: 3,
-        notTaking: 5
-      },
-      LiveCheckSummary: [
-        {
-          Date: '7th June',
-          PinsGenerated: 59,
-          LoggedIn: 58,
-          StartedCheck: 44
-        },
-        {
-          Date: '14th May',
-          PinsGenerated: 59,
-          LoggedIn: 58,
-          StartedCheck: 44
-        },
-        {
-          Date: '3rd May',
-          PinsGenerated: 59,
-          LoggedIn: 58,
-          StartedCheck: 44
-        }],
-      TioCheckSummary: []
-    } // schoolSummaryService.getData(req.user.schoolId)
+    const summaryData = await schoolSummaryService.getSummary(req.user.schoolId)
     req.breadcrumbs(res.locals.pageTitle)
     return res.render('helpdesk/school-summary', {
       breadcrumbs: req.breadcrumbs(),
--- admin/services/school-summary.service.js
+++ admin/services/school-summary.service.js
@@ -2,4 +2,55 @@
 
 const service = {}
 
+service.getSummary = async (schoolId) => {
+  return {
+    schoolName: '[A very good school with a suitably long and descriptive name]',
+    dfeNumber: '[DFE Number]',
+    register: {
+      completed: 1,
+      notStarted: 3,
+      notTaking: 5
+    },
+    LiveCheckSummary: [
+      {
+        Date: '7th June',
+        PinsGenerated: 59,
+        LoggedIn: 52,
+        StartedCheck: 44
+      },
+      {
+        Date: '14th May',
+        PinsGenerated: 13,
+        LoggedIn: 11,
+        StartedCheck: 4
+      },
+      {
+        Date: '3rd May',
+        PinsGenerated: 19,
+        LoggedIn: 8,
+        StartedCheck: 8
+      }],
+    TioCheckSummary: [
+      {
+        Date: '4th April',
+        PinsGenerated: 59,
+        LoggedIn: 52,
+        StartedCheck: 44
+      },
+      {
+        Date: '15th May',
+        PinsGenerated: 13,
+        LoggedIn: 11,
+        StartedCheck: 4
+      },
+      {
+        Date: '16th May',
+        PinsGenerated: 19,
+        LoggedIn: 8,
+        StartedCheck: 8
+      }
+    ]
+  }
+}
+
 module.exports = service
