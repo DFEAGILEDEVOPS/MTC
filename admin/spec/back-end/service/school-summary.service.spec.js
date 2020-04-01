d1712462b4cf86d052f0cb9f4a50357d59f1dd2a
Guy Harwood <guy@milkchip.com>
2020-04-01 15:46:01 +0100

    tests done

--- admin/spec/back-end/service/school-summary.service.spec.js
+++ admin/spec/back-end/service/school-summary.service.spec.js
@@ -46,8 +46,38 @@ describe('schoolSummaryService', () => {
 
   it('should transform register data correctly', async () => {
     const summary = await schoolSummaryService.getSummary(1)
-    expect(summary.register.completed).toBe(mockRegisterData.Completed)
     expect(summary.register.total).toBe(mockRegisterData.TotalCount)
+    expect(summary.register.completed).toBe(mockRegisterData.Completed)
     expect(summary.register.notTaking).toBe(mockRegisterData.NotAttending)
+  })
+
+  it('should transform live check data correctly', async () => {
+    const summary = await schoolSummaryService.getSummary(1)
+    const liveCheck1 = summary.liveCheckSummary[0]
+    const mockLiveCheck1 = mockLiveData[0]
+    expect(liveCheck1.Date).toBe(mockLiveData[0].Date)
+    expect(liveCheck1.PinsGenerated).toBe(mockLiveCheck1.PinsGenerated)
+    expect(liveCheck1.LoggedIn).toBe(mockLiveCheck1.LoggedIn)
+    expect(liveCheck1.Complete).toBe(mockLiveCheck1.Complete)
+
+    const liveCheck2 = summary.liveCheckSummary[1]
+    const mockLiveCheck2 = mockLiveData[1]
+    expect(liveCheck2.Date).toBe(mockLiveCheck2.Date)
+    expect(liveCheck2.PinsGenerated).toBe(mockLiveCheck2.PinsGenerated)
+    expect(liveCheck2.LoggedIn).toBe(mockLiveCheck2.LoggedIn)
+    expect(liveCheck2.Complete).toBe(mockLiveCheck2.Complete)
+  })
+
+  it('should transform tio check data correctly', async () => {
+    const summary = await schoolSummaryService.getSummary(1)
+    const tioCheck1 = summary.tioCheckSummary[0]
+    const mockTioCheck1 = mockTioData[0]
+    expect(tioCheck1.Date).toBe(mockLiveData[0].Date)
+    expect(tioCheck1.PinsGenerated).toBe(mockTioCheck1.PinsGenerated)
+
+    const tioCheck2 = summary.tioCheckSummary[1]
+    const mockTioCheck2 = mockTioData[1]
+    expect(tioCheck2.Date).toBe(mockTioCheck2.Date)
+    expect(tioCheck2.PinsGenerated).toBe(mockTioCheck2.PinsGenerated)
   })
 })
