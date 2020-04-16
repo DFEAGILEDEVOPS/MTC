90dc59a8bbb1d84a589050cb543295b0d1b18f09
Guy Harwood <guy@milkchip.com>
2020-04-16 12:02:59 +0100

    finish implementation

--- admin/lib/validator/school-impersonation-validator.js
+++ admin/lib/validator/school-impersonation-validator.js
@@ -1,31 +1,6 @@
 const ValidationError = require('../validation-error')
 const SchoolImpersonationMessages = require('../errors/school-impersonation')
 
-const isDfeNumberValid = (dfeNumber) => {
-  const validationError = new ValidationError()
-  if (!dfeNumber) {
-    validationError.addError('dfeNumber', SchoolImpersonationMessages.noInput)
-    return validationError
-  }
-
-  if (typeof dfeNumber !== 'string') {
-    validationError.addError('dfeNumber', SchoolImpersonationMessages.incorrectType)
-    return validationError
-  }
-
-  dfeNumber = dfeNumber.trim()
-  if (dfeNumber[3] === '-' || dfeNumber[3] === '/') {
-    dfeNumber = dfeNumber.slice(0, 3) + dfeNumber.slice(4)
-  }
-
-  const isNumericRegex = new RegExp(/^\d+$/)
-  if (!isNumericRegex.test(dfeNumber)) {
-    validationError.addError('dfeNumber', SchoolImpersonationMessages.incorrectType)
-    return validationError
-  }
-  return validationError
-}
-
 const isDfeNumberValidV2 = (dfeNumber) => {
   const validationError = new ValidationError()
 
