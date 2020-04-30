514eca1382d3e389ab2af76130bbd9f6573ce617
Guy Harwood <guy@milkchip.com>
2020-04-30 16:34:14 +0100

    add sorting to tests

--- admin/spec/back-end/service/pupil.service.spec.js
+++ admin/spec/back-end/service/pupil.service.spec.js
@@ -6,6 +6,7 @@ const pupilService = require('../../../services/pupil.service')
 const pupilDataService = require('../../../services/data-access/pupil.data.service')
 const pupilMock = require('../mocks/pupil')
 const schoolMock = require('../mocks/school')
+const sorting = require('../../../helpers/table-sorting')
 
 /* global describe, it, expect, spyOn, fail */
 
@@ -42,18 +43,20 @@ describe('pupil service', () => {
   })
 
   describe('#getPupilsWithFullNames', () => {
-    it('it returns an object with combined name values and urlSlug', async () => {
+    it('it returns a sorted object with combined name values and urlSlug', async () => {
       const pupilMocks = [
         { foreName: 'John', middleNames: 'Test', lastName: 'Johnson', urlSlug: 'AA-12345' },
         { foreName: 'John2', middleNames: '', lastName: 'Johnson2', urlSlug: 'BB-12345' }
       ]
       spyOn(pupilDataService, 'sqlFindPupilsBySchoolId').and.returnValue(pupilMocks)
+      spyOn(sorting, 'sortByProps').and.returnValue(pupilMocks)
       let pupils
       try {
         pupils = await pupilService.getPupilsWithFullNames(1234567)
       } catch (error) {
         fail()
       }
+      expect(sorting.sortByProps).toHaveBeenCalled()
       expect(pupils[0].fullName).toBe('Johnson John Test')
       expect(pupils[1].fullName).toBe('Johnson2 John2')
       expect(pupils[0].urlSlug).toBe('AA-12345')
@@ -72,6 +75,20 @@ describe('pupil service', () => {
   })
 
   describe('#findPupilsBySchoolId', () => {
+    it('sorts the pupils before returning them', async () => {
+      const pupilMocks = [
+        { foreName: 'John', middleNames: 'Test', lastName: 'Johnson', urlSlug: 'AA-12345' },
+        { foreName: 'John2', middleNames: '', lastName: 'Johnson2', urlSlug: 'BB-12345' }
+      ]
+      spyOn(pupilDataService, 'sqlFindPupilsBySchoolId').and.returnValue(pupilMocks)
+      spyOn(sorting, 'sortByProps')
+      try {
+        await pupilService.findPupilsBySchoolId(1)
+        expect(sorting.sortByProps).toHaveBeenCalled()
+      } catch (error) {
+        fail(error.message)
+      }
+    })
     it('it throws an error when schoolId is not provided', async () => {
       spyOn(pupilDataService, 'sqlFindPupilsBySchoolId')
       try {
