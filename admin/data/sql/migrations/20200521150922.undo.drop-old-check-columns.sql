b321cb43bffb7012055cfc8669705916e3d87e5a
Jon Shipley <jon-shipley@users.noreply.github.com>
2020-05-27 15:38:30 +0100

    Feature/41607 remove old columns (#1648)
    * [admin] Remove columns from check table
    
    * [admin] Fix integration test
    
    * update chrome driver version
    
    * [admin] fix migration down
    
    Co-authored-by: Mohsen Qureshi <activemq@users.noreply.github.com>
    Co-authored-by: Mohsen Qureshi <mjq@Mohsens-Mini.lan>

---
+++ admin/data/sql/migrations/20200521150922.undo.drop-old-check-columns.sql
@@ -0,0 +1,23 @@
+IF NOT EXISTS(SELECT *
+                FROM sys.columns
+               WHERE object_ID = object_id('mtc_admin.check')
+                 AND col_name(object_ID, column_Id) = 'mark')
+    BEGIN
+        ALTER TABLE [mtc_admin].[check] ADD mark TINYINT
+    END
+
+IF NOT EXISTS(SELECT *
+                FROM sys.columns
+               WHERE object_ID = object_id('mtc_admin.check')
+                 AND col_name(object_ID, column_Id) = 'maxMark')
+    BEGIN
+        ALTER TABLE [mtc_admin].[check] ADD maxMark TINYINT
+    END
+
+IF NOT EXISTS(SELECT *
+                FROM sys.columns
+               WHERE object_ID = object_id('mtc_admin.check')
+                 AND col_name(object_ID, column_Id) = 'markedAt')
+    BEGIN
+        ALTER TABLE [mtc_admin].[check] ADD markedAt DATETIMEOFFSET(3);
+    END
