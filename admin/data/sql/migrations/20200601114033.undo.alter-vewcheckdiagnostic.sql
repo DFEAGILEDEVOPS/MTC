c6731a10a8023262eb1e3be4f534bea30d8118b0
Guy Harwood <guy@milkchip.com>
2020-06-01 11:48:40 +0100

    fix check diag view

---
+++ admin/data/sql/migrations/20200601114033.undo.alter-vewcheckdiagnostic.sql
@@ -0,0 +1,31 @@
+ALTER VIEW [mtc_admin].[vewCheckDiagnostic]
+AS
+SELECT
+    c.id AS check_id,
+    c.createdAt,
+    c.checkCode,
+    c.pupil_id,
+    p.foreName,
+    p.lastName,
+    s.id AS school_id,
+    s.dfeNumber,
+    s.name AS school_name,
+    s.pin AS school_pin,
+    c.pupilLoginDate,
+    c.startedAt,
+    c.receivedByServerAt,
+    c.isLiveCheck,
+    c.mark,
+    c.maxMark,
+    c.markedAt,
+    cs.code AS check_status,
+    cs.description AS check_desc,
+    pin.val AS pupil_pin,
+    cp.pinExpiresAt,
+    IIF(cp.pinExpiresAt IS NULL OR cp.pinExpiresAt < GETUTCDATE(), 'Y', 'N') pinHasExpired
+  FROM [mtc_admin].[check] c
+       JOIN [mtc_admin].[checkStatus] cs ON (c.checkStatus_id = cs.id)
+       JOIN [mtc_admin].[pupil] p ON (c.pupil_id = p.id)
+       JOIN [mtc_admin].[school] s ON (p.school_id = s.id)
+       LEFT JOIN [mtc_admin].[checkPin] cp ON (c.id = cp.check_id)
+       LEFT JOIN [mtc_admin].[pin] pin ON (cp.pin_id = pin.id)
