0e3c463fbcefce177255aef5fb315483a95b570a
Jon Shipley <js@ovix.co.uk>
2020-06-04 12:35:28 +0100

    formatting

--- admin/data/sql/migrations/20200604120400.do.drop-checkwindow-complete-default-value.sql
+++ admin/data/sql/migrations/20200604120400.do.drop-checkwindow-complete-default-value.sql
@@ -1,12 +1,11 @@
-IF EXISTS (
-        select *
-          from sys.all_columns c
-               join sys.tables t on t.object_id = c.object_id
-               join sys.schemas s on s.schema_id = t.schema_id
-               join sys.default_constraints d on c.default_object_id = d.object_id
-         where t.name = 'checkWindow'
-           and c.name = 'complete'
-           and s.name = 'mtc_admin')
+IF EXISTS(SELECT *
+            FROM sys.all_columns c
+                 JOIN sys.tables t ON t.object_id = c.object_id
+                 JOIN sys.schemas s ON s.schema_id = t.schema_id
+                 JOIN sys.default_constraints d ON c.default_object_id = d.object_id
+           WHERE t.name = 'checkWindow'
+             AND c.name = 'complete'
+             AND s.name = 'mtc_admin')
     BEGIN
         ALTER TABLE [mtc_admin].[checkWindow]
             DROP CONSTRAINT [completeDefault];
