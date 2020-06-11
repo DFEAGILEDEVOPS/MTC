3092d41b48bd9f72d0a61010b24f94d7e2f736b1
Jon Shipley <jon-shipley@users.noreply.github.com>
2020-06-11 13:30:09 +0100

    Bug/39577 ie11 pupil status page fix (#1663)
    * Fix IE11 issue
    
    * fix bad html
    
    * Fix bad html
    
    * [admin] support tedious enableArithAbort option
    
    * [admin] support tedious enableArithAbort option for readOnly config
    
    * [admin] fix html
    
    * [admin] bump minor/patch deps
    
    * [admni] make the pupil status traffic lights acessible to keyboard users
    
    * Fix test
    
    * [admin] fix build

--- admin/assets/javascripts/pupil-status.js
+++ admin/assets/javascripts/pupil-status.js
@@ -3,5 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // eslint-disable-next-line no-undef
   const stepByStepNavigation = new GOVUK.Modules.StepByStepNavigation()
   stepByStepNavigation.start(element)
+
+  // Bind actions to the coloured cards so they open and close the details elements
   window.MTCAdmin.pupilStatusSelection()
 })
