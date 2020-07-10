2d4b654762df5b3e8fa41be30e4e919a327c4415
Guy Harwood <guy@milkchip.com>
2020-07-10 14:46:56 +0100

    add migrations

---
+++ admin/data/sql/migrations/20200710143820.do.check-retro-input-assistant.sql
@@ -0,0 +1,8 @@
+ALTER TABLE [mtc_admin].[check]
+  ADD retroInputAssistantFirstName nvarchar(50) NULL
+
+ALTER TABLE [mtc_admin].[check]
+  ADD retroInputAssistantLastName nvarchar(50) NULL
+
+ALTER TABLE [mtc_admin].[check]
+  ADD retroInputAssistantReason nvarchar(1000) NULL
---
+++ admin/data/sql/migrations/20200710143820.undo.check-retro-input-assistant.sql
@@ -0,0 +1,8 @@
+ALTER TABLE [mtc_admin].[check]
+  DROP COLUMN retroInputAssistantFirstName
+
+ALTER TABLE [mtc_admin].[check]
+  DROP COLUMN retroInputAssistantLastName
+
+ALTER TABLE [mtc_admin].[check]
+  DROP COLUMN retroInputAssistantReason
