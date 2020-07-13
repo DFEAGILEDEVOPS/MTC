fae6ba25cd01d218fdaca7505f9b30acbad81695
Guy Harwood <guy@milkchip.com>
2020-07-13 16:59:12 +0100

    fix lint

--- admin/services/data-access/retro-input-assistant.data.service.js
+++ admin/services/data-access/retro-input-assistant.data.service.js
@@ -5,18 +5,19 @@ const TYPES = require('./sql.service').TYPES
 
 const service = {
   /**
- * @typedef {object} retroInputAssistantData
- * @property {string} firstName
- * @property {string} lastName
- * @property {string} reason
- * @property {number} checkId
- * @property {string} pupilUuid
- * @property {number} userId
- */
+   * @typedef {object} retroInputAssistantData
+   * @property {string} firstName
+   * @property {string} lastName
+   * @property {string} reason
+   * @property {number} checkId
+   * @property {string} pupilUuid
+   * @property {number} userId
+   */
 
   /**
    *
    * @param {retroInputAssistantData} data
+   * @returns {Promise<any>}
    */
   create: async function create (data) {
     const pupilIdSql = 'SELECT id FROM mtc_admin.pupil WHERE urlSlug=@pupilUuid'
@@ -29,11 +30,18 @@ const service = {
     if (!pupilId) {
       throw new Error(`Unable to save retro input assistant to database.\npupil with urlSlug:${data.pupilUuid} not found`)
     }
+    // TODO fix
+    const retroInputAssistantTypeId = 7
     const insertData = {
       pupil_id: pupilId,
-      recordedBy_user_id: data.userId
+      recordedBy_user_id: data.userId,
+      accessArrangements_id: retroInputAssistantTypeId,
+      retroInputAssistantFirstName: data.firstName,
+      retroInputAssistantLastName: data.lastName,
+      retroInputAssistantReason: data.reason,
+      retroInputAssistant_check_id: data.checkId
     }
-    const insertResult = sqlService.create('pupilAccessArrangements',)
+    return sqlService.create('pupilAccessArrangements', insertData)
   }
 }
 
