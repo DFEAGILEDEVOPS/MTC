cdfadcb813fcf78e3f2bd57be9e56c49893cc234
Guy Harwood <guy@milkchip.com>
2020-07-21 15:09:28 +0100

    fixes as per JMS feedback

--- admin/controllers/retro-input-assistant.js
+++ admin/controllers/retro-input-assistant.js
@@ -5,53 +5,51 @@ const businessAvailabilityService = require('../services/business-availability.s
 const ValidationError = require('../lib/validation-error')
 const retroInputAssistantService = require('../services/retro-input-assistant.service')
 
-const controller = {}
+const controller = {
+  getAddRetroInputAssistant: async function getAddRetroInputAssistant (req, res, next, error = null) {
+    res.locals.pageTitle = 'Record input assistant used for official check'
+    req.breadcrumbs('Select pupils and access arrangements', 'select-access-arrangements')
+    req.breadcrumbs('Record input assistant')
 
-controller.getAddRetroInputAssistant = async (req, res, next, error = null) => {
-  res.locals.pageTitle = 'Record input assistant used for official check'
-  req.breadcrumbs('Select pupils and access arrangements', 'select-access-arrangements')
-  req.breadcrumbs('Record input assistant')
-
-  try {
-    const checkWindowData = await checkWindowV2Service.getActiveCheckWindow()
-    await businessAvailabilityService.determineAccessArrangementsEligibility(checkWindowData)
-  } catch (error) {
-    return next(error)
-  }
-
-  let pupils
-  try {
-    pupils = await retroInputAssistantService.getEligiblePupilsWithFullNames(req.user.schoolId)
-  } catch (error) {
-    return next(error)
-  }
-  return res.render('access-arrangements/retro-add-input-assistant', {
-    breadcrumbs: req.breadcrumbs(),
-    pupils: pupils,
-    error: error || new ValidationError(),
-    formData: req.body
-  })
-}
-
-controller.postSubmitRetroInputAssistant = async (req, res, next) => {
-  const saveData = {
-    firstName: req.body.firstname,
-    lastName: req.body.lastname,
-    reason: req.body.reason,
-    pupilUuid: req.body.pupilUrlSlug,
-    userId: req.user.id
-  }
-
-  try {
-    await retroInputAssistantService.save(saveData)
-  } catch (error) {
-    if (error.name === 'ValidationError') {
-      return controller.getAddRetroInputAssistant(req, res, next, error)
+    try {
+      const checkWindowData = await checkWindowV2Service.getActiveCheckWindow()
+      await businessAvailabilityService.determineAccessArrangementsEligibility(checkWindowData)
+    } catch (error) {
+      return next(error)
     }
-    return next(error)
+
+    let pupils
+    try {
+      pupils = await retroInputAssistantService.getEligiblePupilsWithFullNames(req.user.schoolId)
+    } catch (error) {
+      return next(error)
+    }
+    return res.render('access-arrangements/retro-add-input-assistant', {
+      breadcrumbs: req.breadcrumbs(),
+      pupils: pupils,
+      error: error || new ValidationError(),
+      formData: req.body
+    })
+  },
+  postSubmitRetroInputAssistant: async function postSubmitRetroInputAssistant (req, res, next) {
+    const saveData = {
+      firstName: req.body.firstname,
+      lastName: req.body.lastname,
+      reason: req.body.reason,
+      pupilUuid: req.body.pupilUrlSlug,
+      userId: req.user.id
+    }
+    try {
+      await retroInputAssistantService.save(saveData)
+    } catch (error) {
+      if (error.name === 'ValidationError') {
+        return controller.getAddRetroInputAssistant(req, res, next, error)
+      }
+      return next(error)
+    }
+    req.flash('info', 'Retrospective Input assistant added')
+    res.redirect('/access-arrangements/overview')
   }
-  req.flash('info', 'Retrospective Input assistant added')
-  res.redirect('/access-arrangements/overview')
 }
 
 module.exports = controller
--- admin/data/sql/migrations/20200710143820.undo.check-retro-input-assistant.sql
+++ admin/data/sql/migrations/20200710143820.undo.check-retro-input-assistant.sql
@@ -1,5 +1,2 @@
-ALTER TABLE [mtc_admin].[check] DROP COLUMN retroInputAssistantFirstName
-ALTER TABLE [mtc_admin].[check] DROP COLUMN retroInputAssistantLastName
-ALTER TABLE [mtc_admin].[check] DROP COLUMN retroInputAssistantReason
-ALTER TABLE [mtc_admin].[check] DROP CONSTRAINT FK_pupilAccessArrangements_check_id
-ALTER TABLE [mtc_admin].[check] DROP COLUMN retroInputAssitant_check_id
+ALTER TABLE [mtc_admin].[pupilAccessArrangements] DROP CONSTRAINT FK_pupilAccessArrangements_check_id
+ALTER TABLE [mtc_admin].[pupilAccessArrangements] DROP COLUMN retroInputAssistantFirstName, retroInputAssistantLastName, retroInputAssistantReason, retroInputAssistant_check_id
