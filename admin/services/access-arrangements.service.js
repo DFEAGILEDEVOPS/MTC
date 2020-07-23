252589fefafe650195387e27a3cbd1fd215e5c1f
Guy Harwood <guy@milkchip.com>
2020-07-23 15:40:13 +0100

    rehouse service as object

--- admin/services/access-arrangements.service.js
+++ admin/services/access-arrangements.service.js
@@ -6,37 +6,34 @@ const preparedCheckSyncService = require('../services/prepared-check-sync.servic
 const pupilDataService = require('../services/data-access/pupil.data.service')
 const accessArrangementsValidator = require('../lib/validator/access-arrangements-validator.js')
 
-const accessArrangementsService = {}
-
+const accessArrangementsService = {
 /**
  * Get access arrangements
  * @returns {Promise<Array>}
  */
-accessArrangementsService.getAccessArrangements = async () => {
-  return accessArrangementsDataService.sqlFindAccessArrangements()
-}
-
-/**
+  getAccessArrangements: async function getAccessArrangements () {
+    return accessArrangementsDataService.sqlFindAccessArrangements()
+  },
+  /**
  * Submit access arrangements for single pupil
  * @param {Object} submittedData
  * @param {Number} schoolId
  * @param {Number} userId
  * @returns {Promise<Object>}
  */
-accessArrangementsService.submit = async (submittedData, schoolId, userId) => {
-  const urlSlug = submittedData.pupilUrlSlug || submittedData.urlSlug
-  const validationError = accessArrangementsValidator.validate(submittedData)
-  if (validationError.hasError()) {
-    throw validationError
-  }
-  const pupil = await pupilDataService.sqlFindOneBySlugAndSchool(urlSlug, schoolId)
-  const processedData = await accessArrangementsService.prepareData(submittedData, pupil, schoolId, userId)
-  const displayData = await accessArrangementsService.save(processedData, pupil)
-  await preparedCheckSyncService.addMessages(urlSlug)
-  return displayData
-}
-
-/**
+  submit: async function submit (submittedData, schoolId, userId) {
+    const urlSlug = submittedData.pupilUrlSlug || submittedData.urlSlug
+    const validationError = accessArrangementsValidator.validate(submittedData)
+    if (validationError.hasError()) {
+      throw validationError
+    }
+    const pupil = await pupilDataService.sqlFindOneBySlugAndSchool(urlSlug, schoolId)
+    const processedData = await accessArrangementsService.prepareData(submittedData, pupil, schoolId, userId)
+    const displayData = await accessArrangementsService.save(processedData, pupil)
+    await preparedCheckSyncService.addMessages(urlSlug)
+    return displayData
+  },
+  /**
  * Prepares access arrangements data for submission to the database
  * @param {Object} requestData
  * @param {Object} pupil
@@ -44,68 +41,67 @@ accessArrangementsService.submit = async (submittedData, schoolId, userId) => {
  * @param {Number} userId
  * @returns {Promise<Object>}
  */
-accessArrangementsService.prepareData = async (requestData, pupil, schoolId, userId) => {
-  const { accessArrangements: accessArrangementsCodes, questionReaderReason } = requestData
-  const pupilAccessArrangements = R.clone(requestData)
-  pupilAccessArrangements.accessArrangementsIdsWithCodes = await accessArrangementsDataService.sqlFindAccessArrangementsIdsWithCodes(accessArrangementsCodes)
-  let questionReaderReasonId
-  if (pupilAccessArrangements.accessArrangements.length === 0) {
-    throw new Error('No access arrangements found')
-  }
-  if (!pupil) {
-    throw new Error('Pupil object is not found')
-  }
-  const omittedFields = []
-  pupilAccessArrangements.pupil_id = pupil.id
-  pupilAccessArrangements.recordedBy_user_id = userId
-  pupilAccessArrangements.questionReaderReasonCode = questionReaderReason
-  if (!pupilAccessArrangements.accessArrangements.includes(accessArrangementsDataService.CODES.INPUT_ASSISTANCE)) {
-    omittedFields.push('inputAssistanceInformation')
-  }
-  if (!pupilAccessArrangements.accessArrangements.includes(accessArrangementsDataService.CODES.NEXT_BETWEEN_QUESTIONS)) {
-    omittedFields.push('nextButtonInformation')
-  }
-  if (pupilAccessArrangements.questionReaderReason !== questionReaderReasonsDataService.CODES.OTHER) {
-    omittedFields.push('questionReaderOtherInformation')
-  }
-  if (pupilAccessArrangements.accessArrangements.includes(accessArrangementsDataService.CODES.QUESTION_READER)) {
-    questionReaderReasonId = await questionReaderReasonsDataService.sqlFindQuestionReaderReasonIdByCode(pupilAccessArrangements.questionReaderReasonCode)
-  }
-  if (pupilAccessArrangements.accessArrangements.includes(accessArrangementsDataService.CODES.COLOUR_CONTRAST)) {
-    const pupilColourContrastAA = pupilAccessArrangements.accessArrangementsIdsWithCodes.find(paa => paa.code === 'CCT')
-    pupilColourContrastAA.pupilColourContrasts_id = await pupilAccessArrangementsDataService.sqlFindPupilColourContrastsId(pupil.id, pupilColourContrastAA.id)
-  }
-  if (pupilAccessArrangements.accessArrangements.includes(accessArrangementsDataService.CODES.FONT_SIZE)) {
-    const pupilFontSizeAA = pupilAccessArrangements.accessArrangementsIdsWithCodes.find(paa => paa.code === 'FTS')
-    pupilFontSizeAA.pupilFontSizes_id = await pupilAccessArrangementsDataService.sqlFindPupilFontSizesId(pupil.id, pupilFontSizeAA.id)
-  }
-  if (questionReaderReasonId) {
-    pupilAccessArrangements.questionReaderReasons_id = questionReaderReasonId
-  }
-  omittedFields.push('accessArrangements', 'questionReaderReason', 'pupilUrlSlug')
-  omittedFields.forEach(field => {
-    delete pupilAccessArrangements[field]
-  })
-  return pupilAccessArrangements
-}
-
-/**
+  prepareData: async function prepareData (requestData, pupil, schoolId, userId) {
+    const { accessArrangements: accessArrangementsCodes, questionReaderReason } = requestData
+    const pupilAccessArrangements = R.clone(requestData)
+    pupilAccessArrangements.accessArrangementsIdsWithCodes = await accessArrangementsDataService.sqlFindAccessArrangementsIdsWithCodes(accessArrangementsCodes)
+    let questionReaderReasonId
+    if (pupilAccessArrangements.accessArrangements.length === 0) {
+      throw new Error('No access arrangements found')
+    }
+    if (!pupil) {
+      throw new Error('Pupil object is not found')
+    }
+    const omittedFields = []
+    pupilAccessArrangements.pupil_id = pupil.id
+    pupilAccessArrangements.recordedBy_user_id = userId
+    pupilAccessArrangements.questionReaderReasonCode = questionReaderReason
+    if (!pupilAccessArrangements.accessArrangements.includes(accessArrangementsDataService.CODES.INPUT_ASSISTANCE)) {
+      omittedFields.push('inputAssistanceInformation')
+    }
+    if (!pupilAccessArrangements.accessArrangements.includes(accessArrangementsDataService.CODES.NEXT_BETWEEN_QUESTIONS)) {
+      omittedFields.push('nextButtonInformation')
+    }
+    if (pupilAccessArrangements.questionReaderReason !== questionReaderReasonsDataService.CODES.OTHER) {
+      omittedFields.push('questionReaderOtherInformation')
+    }
+    if (pupilAccessArrangements.accessArrangements.includes(accessArrangementsDataService.CODES.QUESTION_READER)) {
+      questionReaderReasonId = await questionReaderReasonsDataService.sqlFindQuestionReaderReasonIdByCode(pupilAccessArrangements.questionReaderReasonCode)
+    }
+    if (pupilAccessArrangements.accessArrangements.includes(accessArrangementsDataService.CODES.COLOUR_CONTRAST)) {
+      const pupilColourContrastAA = pupilAccessArrangements.accessArrangementsIdsWithCodes.find(paa => paa.code === 'CCT')
+      pupilColourContrastAA.pupilColourContrasts_id = await pupilAccessArrangementsDataService.sqlFindPupilColourContrastsId(pupil.id, pupilColourContrastAA.id)
+    }
+    if (pupilAccessArrangements.accessArrangements.includes(accessArrangementsDataService.CODES.FONT_SIZE)) {
+      const pupilFontSizeAA = pupilAccessArrangements.accessArrangementsIdsWithCodes.find(paa => paa.code === 'FTS')
+      pupilFontSizeAA.pupilFontSizes_id = await pupilAccessArrangementsDataService.sqlFindPupilFontSizesId(pupil.id, pupilFontSizeAA.id)
+    }
+    if (questionReaderReasonId) {
+      pupilAccessArrangements.questionReaderReasons_id = questionReaderReasonId
+    }
+    omittedFields.push('accessArrangements', 'questionReaderReason', 'pupilUrlSlug')
+    omittedFields.forEach(field => {
+      delete pupilAccessArrangements[field]
+    })
+    return pupilAccessArrangements
+  },
+  /**
  * Save access arrangements data
  * @param {Object} pupilAccessArrangements
  * @param {Object} pupil
  * @returns {Promise<Object>}
  */
-
-accessArrangementsService.save = async (pupilAccessArrangements, pupil) => {
-  const { urlSlug, foreName, lastName } = pupil
-  const pupilAccessArrangement = await pupilAccessArrangementsDataService.sqlFindPupilAccessArrangementsByPupilId(pupil.id)
-  if (pupilAccessArrangement && pupilAccessArrangement.length) {
-    const isUpdate = true
-    await pupilAccessArrangementsDataService.sqlInsertAccessArrangements(pupilAccessArrangements, isUpdate)
-  } else {
-    await pupilAccessArrangementsDataService.sqlInsertAccessArrangements(pupilAccessArrangements)
+  save: async function save (pupilAccessArrangements, pupil) {
+    const { urlSlug, foreName, lastName } = pupil
+    const pupilAccessArrangement = await pupilAccessArrangementsDataService.sqlFindPupilAccessArrangementsByPupilId(pupil.id)
+    if (pupilAccessArrangement && pupilAccessArrangement.length) {
+      const isUpdate = true
+      await pupilAccessArrangementsDataService.sqlInsertAccessArrangements(pupilAccessArrangements, isUpdate)
+    } else {
+      await pupilAccessArrangementsDataService.sqlInsertAccessArrangements(pupilAccessArrangements)
+    }
+    return { urlSlug, foreName, lastName }
   }
-  return { urlSlug, foreName, lastName }
 }
 
 module.exports = accessArrangementsService
