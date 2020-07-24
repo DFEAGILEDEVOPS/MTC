ea58b2e330c03ba914b346eae8e605bcbc1c9fa7
Jon Shipley <js@ovix.co.uk>
2020-07-24 16:18:59 +0100

    [admin] clean up routes

--- admin/routes/helpdesk.js
+++ admin/routes/helpdesk.js
@@ -7,11 +7,29 @@ const roles = require('../lib/consts/roles')
 const helpdeskImpersonationController = require('../controllers/helpdesk-impersonation')
 const helpdeskSummaryController = require('../controllers/helpdesk-summary')
 
-router.get('/', isAuthenticated(roles.helpdesk), (req, res, next) => helpdeskImpersonationController.getSchoolImpersonation(req, res, next))
-router.get('/school-impersonation', isAuthenticated(roles.helpdesk), (req, res, next) => helpdeskImpersonationController.getSchoolImpersonation(req, res, next))
-router.post('/add-school-impersonation', isAuthenticated(roles.helpdesk), (req, res, next) => helpdeskImpersonationController.postAddSchoolImpersonation(req, res, next))
-router.post('/remove-school-impersonation', isAuthenticated(roles.helpdesk), (req, res, next) => helpdeskImpersonationController.postRemoveSchoolImpersonation(req, res, next))
-router.get('/home', isAuthenticated(roles.helpdesk), (req, res, next) => helpdeskImpersonationController.getSchoolLandingPage(req, res, next))
-router.get('/school-summary', isAuthenticated(roles.helpdesk), (req, res, next) => helpdeskSummaryController.getSummary(req, res, next))
+router.get('/',
+  isAuthenticated(roles.helpdesk),
+  helpdeskImpersonationController.getSchoolImpersonation
+)
+router.get('/school-impersonation',
+  isAuthenticated(roles.helpdesk),
+  helpdeskImpersonationController.getSchoolImpersonation
+)
+router.post('/add-school-impersonation',
+  isAuthenticated(roles.helpdesk),
+  helpdeskImpersonationController.postAddSchoolImpersonation
+)
+router.post('/remove-school-impersonation',
+  isAuthenticated(roles.helpdesk),
+  helpdeskImpersonationController.postRemoveSchoolImpersonation
+)
+router.get('/home',
+  isAuthenticated(roles.helpdesk),
+  helpdeskImpersonationController.getSchoolLandingPage
+)
+router.get('/school-summary',
+  isAuthenticated(roles.helpdesk),
+  helpdeskSummaryController.getSummary
+)
 
 module.exports = router
