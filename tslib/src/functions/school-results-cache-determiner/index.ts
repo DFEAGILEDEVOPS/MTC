759798519654b9ea8bf5cd08839cf6f88c37d4f2
Jon Shipley <js@ovix.co.uk>
2020-07-29 23:12:47 +0100

    Add new function app skel

---
+++ tslib/src/functions/school-results-cache-determiner/index.ts
@@ -0,0 +1,20 @@
+import { AzureFunction, Context } from '@azure/functions'
+
+const functionName = 'school-results-cache-determiner'
+import { performance } from 'perf_hooks'
+
+function finish (start: number, context: Context) {
+  const end = performance.now()
+  const durationInMilliseconds = end - start
+  const timeStamp = new Date().toISOString()
+  context.log(`${functionName}: ${timeStamp} run complete: ${durationInMilliseconds} ms`)
+}
+
+const schoolResultsCacheDeterminer: AzureFunction = async function (context: Context, msg: any): Promise<void> {
+  context.log('ServiceBus queue trigger function processed message', msg)
+
+  const start = performance.now()
+  finish(start, context)
+}
+
+export default schoolResultsCacheDeterminer
