6f35f56c0205e905358c355ea84073233497b342
Guy Harwood <guy@milkchip.com>
2020-07-31 12:21:25 +0100

    new validation message

--- admin/lib/errors/retro-input-assistant.js
+++ admin/lib/errors/retro-input-assistant.js
@@ -1,8 +1,8 @@
 'use strict'
 
 module.exports = {
-  missingFirstName: 'Enter a first name of input assistance',
-  missingLastName: 'Enter a last name of input assistance',
+  missingFirstName: 'Enter a first name in no more than 128 characters',
+  missingLastName: 'Enter a last name in no more than 128 characters',
   missingReason: 'Enter an explanation for input assistance',
   invalidCheckId: 'associated check id is missing',
   invalidPupilUuid: 'Enter a pupil name',
