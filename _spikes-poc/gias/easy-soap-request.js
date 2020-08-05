9a9e654db43ceb7bf28b3e1004636bc9265aa212
Guy Harwood <guy@milkchip.com>
2020-08-05 16:49:24 +0100

    500 error, no details :/

--- _spikes-poc/gias/easy-soap-request.js
+++ _spikes-poc/gias/easy-soap-request.js
@@ -1,12 +1,14 @@
+'use strict'
+
+require('dotenv').config()
 const soapRequest = require('easy-soap-request')
-const fs = require('fs')
 
 const url = process.env.WS_URL
 const headers = {
   'method': 'POST',
   'user-agent': 'mtc-soap-service',
   'Content-Type': 'text/xmlcharset=UTF-8',
-  'soapAction': `${process.env.WSDL_URL}#GetEstablishment`
+  'soapAction': `${process.env.WSDL_URL}/GetEstablishment`
 }
 
 /* const soapAction = 'GetEstablishment'
@@ -15,8 +17,8 @@ const actionParams = {
 } */
 
 const soapEnvelope = `<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                        xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-                        xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+                        xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+                        xmlns:ws="${process.env.WS_NS}">
                           <soapenv:Header>
                             <wsse:Security soapenv:mustUnderstand="1"
                               xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
@@ -35,20 +37,15 @@ const soapEnvelope = `<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSch
                             </ws:GetEstablishment>
                           </soapenv:Body>
                       </soapenv:Envelope>`
-const oneSecondInMilliseconds = 1000
+const timeoutInMilliseconds = 5000
 
-soapRequest({ url: url, headers: headers, xml: soapEnvelope, timeout: oneSecondInMilliseconds })
+soapRequest({ url: url, headers: headers, xml: soapEnvelope, timeout: timeoutInMilliseconds })
   .then((response) => {
     const { headers, body, statusCode } = response
     console.log(headers)
     console.log(body)
     console.log(statusCode)
   })
-
-  /* (async () => {
-    const { response } = await soapRequest({ url: url, headers: sampleHeaders, xml: messageEnvelope, timeout: oneSecondInMilliseconds })
-    const { headers, body, statusCode } = response
-    console.log(headers)
-    console.log(body)
-    console.log(statusCode)
-  })() */
+  .catch((error) => {
+    console.error(error)
+  })
