3e934c620072250b08a0d9ea8b8e0d4806ee5d42
Guy Harwood <guy@milkchip.com>
2020-08-17 17:25:31 +0100

    consolidate implementation

--- _spikes-poc/gias/GiasExtractService.js
+++ _spikes-poc/gias/GiasExtractService.js
@@ -6,12 +6,48 @@ const MultipartMessage = require('./MultipartMessage')
 const soapRequest = require('easy-soap-request')
 const { promisify } = require('util')
 const parseXml = promisify(parseString)
+const _ = require('lodash')
 
 const requestTimeout = process.env.REQUEST_TIMEOUT || 5000
 const messageExpiryMs = process.env.WS_MESSAGE_EXPIRY || 10000
 
+const multipartSoapCall = async (action, params) => {
+  const requestBody = new SoapMessage(action, params)
+      .setUsernamePassword(process.env.WS_USERNAME, process.env.WS_PASSWORD)
+      .setMessageExpiry(messageExpiryMs).toXmlString()
+  const serviceUrl = process.env.WS_ENDPOINT
+  const headers = {
+    'method': 'POST',
+    'Content-Type': 'text/xml;charset=UTF-8',
+    'SOAPAction': `${process.env.WS_NS}/${action}`
+  }
+  const response = await soapRequest({
+    url: serviceUrl,
+    headers: headers,
+    xml: requestBody,
+    timeout: requestTimeout
+  }).then((resolve, reject) => {
+    console.dir(resolve)
+  }).catch((error) => {
+    console.error(error)
+  })
+  const responseMessage = MultipartMessage.parse(response.body, MultipartMessage.getBoundaryIdFromResponse(response))
+  const soapResponse = await parseXml(responseMessage.parts[0].content, {
+    tagNameProcessors: [processors.stripPrefix, processors.firstCharLowerCase],
+    explicitArray: false
+  })
+
+  const attachmentId = soapResponse.envelope.body[`${_.camelCase(action)}Response`].extract.include.$.href.substr(4).replace('%40', '@')
+  const attachment = responseMessage.parts.find(x => x.id === attachmentId)
+  return {
+    createdAt: new Date(soapResponse.envelope.header.security.timestamp.created),
+    expiresAt: new Date(soapResponse.envelope.header.security.timestamp.expires),
+    content: attachment.content
+  }
+}
+
 const service = {
-  getExtract: async function getExtract (extractId) {
+  getExtractImpl: async function getExtract (extractId) {
     const requestBody = new SoapMessage('GetExtract', { Id: extractId })
       .setUsernamePassword(process.env.WS_USERNAME, process.env.WS_PASSWORD)
       .setMessageExpiry(messageExpiryMs).toXmlString()
@@ -44,6 +80,12 @@ const service = {
       expiresAt: new Date(soapResponse.envelope.header.security.timestamp.expires),
       content: attachment.content
     }
+  },
+  getExtract: async function getExtract (extractId) {
+    return multipartSoapCall('GetExtract', { Id: extractId })
+  },
+  getGroupExtract: async function getGroupExtract () {
+    return multipartSoapCall('GetGroupExtract')
   }
 }
 
