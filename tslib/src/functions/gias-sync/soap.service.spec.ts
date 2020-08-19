597033c177286280b177fa9232e305b3007df72d
Guy Harwood <guy@milkchip.com>
2020-08-19 11:50:16 +0100

    security header specs

--- tslib/src/functions/gias-sync/soap.service.spec.ts
+++ tslib/src/functions/gias-sync/soap.service.spec.ts
@@ -1,26 +1,75 @@
 import { SoapService, ISoapMessageSpecification } from './soap.service'
+import { v4 as uuid } from 'uuid'
+import * as xmlParser from 'fast-xml-parser'
+import * as he from 'he'
 
 let sut: SoapService
+
+const xmlParserOptions = {
+  attributeNamePrefix : '',
+  attrNodeName: 'attr', // default is 'false'
+  textNodeName : 'value',
+  ignoreAttributes : false,
+  ignoreNameSpace : false,
+  allowBooleanAttributes : false,
+  parseNodeValue : true,
+  parseAttributeValue : false,
+  trimValues: true,
+  cdataTagName: '__cdata', // default is 'false'
+  cdataPositionChar: '\\c',
+  parseTrueNumberOnly: false,
+  arrayMode: false, // "strict"
+  attrValueProcessor: (val: any, attrName: string) => he.decode(val, { isAttributeValue: true }),// default is a=>a
+  tagValueProcessor : (val: any, tagName: string) => he.decode(val), // default is a=>a
+  stopNodes: ['parse-me-as-string']
+}
+
+let namespace: string
 
 describe('soap.service', () => {
   beforeEach(() => {
     sut = new SoapService()
+    namespace = uuid()
   })
 
   test('subject should be defined', () => {
     expect(sut).toBeDefined()
   })
 
-  test('returns a valid soap xml message with username and password in security header', () => {
+  test('returns an xml structure with namespace as specified', () => {
     const messageSpec: ISoapMessageSpecification = {
       action: 'action',
-      credentials: {
-        username: 'foo',
-        password: 'bar'
-      },
+      namespace: namespace,
       messageExpiryMs: 0
     }
-    const expectedHeader = `<username=${messageSpec.credentials.username}`
-    expect(sut.buildMessage(messageSpec)).toEqual(expectedHeader)
+    const receivedOutput = sut.buildMessage(messageSpec)
+    expect(receivedOutput).toBeDefined()
+    const xml = xmlParser.parse(receivedOutput, xmlParserOptions)
+    expect(xml['soapenv:Envelope'].attr['xmlns:ws']).toEqual(namespace)
+  })
+
+  test('when credentials specified a security header is included containing credentials', () => {
+    const username = 'foo'
+    const password = 'bar'
+    const messageSpec: ISoapMessageSpecification = {
+      action: 'action',
+      namespace: namespace,
+      messageExpiryMs: 0,
+      credentials: {
+        username: username,
+        password: password
+      }
+    }
+    const receivedOutput = sut.buildMessage(messageSpec)
+    expect(receivedOutput).toBeDefined()
+    const xml = xmlParser.parse(receivedOutput, xmlParserOptions)
+    const soapHeader = xml['soapenv:Envelope']['soapenv:Header']
+    expect(soapHeader).toBeDefined()
+    const securityElement = soapHeader['wsse:Security']
+    console.dir(securityElement)
+    expect(securityElement).toBeDefined()
+    const usernameToken = securityElement['wsse:UsernameToken']
+    expect(usernameToken['wsse:Username']).toEqual(username)
+    expect(usernameToken['wsse:Password'].value).toEqual(password)
   })
 })
--- tslib/src/functions/gias-sync/soap.service.ts
+++ tslib/src/functions/gias-sync/soap.service.ts
@@ -1,14 +1,31 @@
 export class SoapService {
 
   buildMessage (messageSpec: ISoapMessageSpecification): string {
-    return `<username=${messageSpec.credentials.username}`
+    let xml = `
+      <?xml version="1.0"?>
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+          xmlns:ws="${messageSpec.namespace}">`
+    xml = `${xml}<soapenv:Header>`
+    if (messageSpec.credentials || messageSpec.messageExpiryMs > 0) {
+      xml = `${xml}<wsse:Security soapenv:mustUnderstand="1"
+        xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">`
+      if (messageSpec.credentials) {
+        xml = `${xml}<wsse:UsernameToken>`
+        xml = `${xml}<wsse:Username>${messageSpec.credentials.username}</wsse:Username>`
+        xml = `${xml}<wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">${messageSpec.credentials.password}</wsse:Password>`
+        xml = `${xml}</wsse:UsernameToken>`
+      }
+    }
+    xml = `${xml}</soapenv:Header>`
+    return xml
   }
 }
 
 export interface ISoapMessageSpecification {
   action: string
+  namespace: string
   parameters?: any
-  credentials: ISoapMessageCredentials
+  credentials?: ISoapMessageCredentials
   messageExpiryMs: number
 }
 
