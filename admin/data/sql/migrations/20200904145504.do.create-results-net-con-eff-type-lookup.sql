335e39b52d734f2b9b2a7c93615d47488d598861
Jon Shipley <js@ovix.co.uk>
2020-09-04 15:58:18 +0100

    Add device tables

---
+++ admin/data/sql/migrations/20200904145504.do.create-results-net-con-eff-type-lookup.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS mtc_results.networkConnectionEffectiveTypeLookup;
+CREATE TABLE mtc_results.networkConnectionEffectiveTypeLookup
+([id]            [INT] IDENTITY ( 1, 1 ) NOT NULL,
+ [createdAt]     [DATETIMEOFFSET](3)     NOT NULL DEFAULT GETUTCDATE(),
+ [updatedAt]     [DATETIMEOFFSET](3)     NOT NULL DEFAULT GETUTCDATE(),
+ [version]       [ROWVERSION],
+ [effectiveType] nvarchar(10),
+ CONSTRAINT [PK_networkConnectionEffectiveTypeLookup] PRIMARY KEY CLUSTERED ([id] ASC) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON),
+ CONSTRAINT [networkConnectionEffectiveTypeLookup_effectiveType_uindex] UNIQUE (effectiveType)
+);
