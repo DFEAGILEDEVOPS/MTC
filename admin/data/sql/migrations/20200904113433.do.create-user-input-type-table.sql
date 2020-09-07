a560f8253782e27793cf2fc0592f5ec1b4d40ed3
Jon Shipley <js@ovix.co.uk>
2020-09-07 14:24:44 +0100

    Change to nvcarchar

--- admin/data/sql/migrations/20200904113433.do.create-user-input-type-table.sql
+++ admin/data/sql/migrations/20200904113433.do.create-user-input-type-table.sql
@@ -5,7 +5,7 @@ CREATE TABLE mtc_results.userInputType
  [createdAt] DATETIMEOFFSET(3)     NOT NULL DEFAULT GETUTCDATE(),
  [updatedAt] DATETIMEOFFSET(3)     NOT NULL DEFAULT GETUTCDATE(),
  [version]   ROWVERSION,
- [name]      VARCHAR(10)           NOT NULL,
+ [name]      NVARCHAR(10)          NOT NULL,
  [code]      CHAR(1)               NOT NULL,
  CONSTRAINT [PK_userInputType] PRIMARY KEY CLUSTERED ([id] ASC) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON),
  CONSTRAINT [userInputType_code_uindex] UNIQUE ([code])
