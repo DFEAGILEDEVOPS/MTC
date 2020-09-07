bd29309ca9a728fdb3d1db6f672cb6458152f587
Jon Shipley <js@ovix.co.uk>
2020-09-07 08:31:27 +0100

    Update

--- admin/data/sql/migrations/20200904145650.do.create-results-user-device.sql
+++ admin/data/sql/migrations/20200904145650.do.create-results-user-device.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS [mtc_results].[userDevice];
 
 CREATE TABLE mtc_results.userDevice
-([id]                                      [INT] IDENTITY ( 1, 1 ) NOT NULL,
- [createdAt]                               [DATETIMEOFFSET](3)     NOT NULL DEFAULT GETUTCDATE(),
- [updatedAt]                               [DATETIMEOFFSET](3)     NOT NULL DEFAULT GETUTCDATE(),
- [version]                                 [ROWVERSION],
- [check_id]                                INT                     NOT NULL,
+([id]                                      INT IDENTITY ( 1, 1 ) NOT NULL,
+ [createdAt]                               DATETIMEOFFSET(3)     NOT NULL DEFAULT GETUTCDATE(),
+ [updatedAt]                               DATETIMEOFFSET(3)     NOT NULL DEFAULT GETUTCDATE(),
+ [version]                                 ROWVERSION,
+ [check_id]                                INT                   NOT NULL,
  [batteryIsCharging]                       BIT,
  [batteryLevelPercent]                     TINYINT,
  [batteryChargingTimeSecs]                 INT,     -- charging time in seconds until full
@@ -26,13 +26,13 @@ CREATE TABLE mtc_results.userDevice
  [innerWidth]                              INT,     -- width of the browser window available for content in pixels
  [innerHeight]                             INT,     -- height of the browser window available for content in pixels <https://developer.mozilla.org/en-US/docs/Web/API/Window/outerHeight>
  [colorDepth]                              TINYINT,
- [deviceOrientationLookup_id]                    INT,
+ [deviceOrientationLookup_id]              INT,
  [appUsageCount]                           TINYINT, -- number of checks taken since app was initialised
  CONSTRAINT [PK_userDevice] PRIMARY KEY CLUSTERED ([id] ASC) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON),
  CONSTRAINT [FK_userDevice_check_id] FOREIGN KEY (check_id) REFERENCES [mtc_admin].[check] (id),
- CONSTRAINT [FK_userAgentLookup_id] FOREIGN KEY (userAgentLookup_id) REFERENCES [mtc_results].[userAgentLookup](id),
- CONSTRAINT [FK_navigatorPlatformLookup_id] FOREIGN KEY (navigatorPlatformLookup_id) REFERENCES [mtc_results].[navigatorPlatformLookup](id),
- CONSTRAINT [FK_navigatorLanguageLookup_id] FOREIGN KEY (navigatorLanguageLookup_id) REFERENCES [mtc_results].[navigatorLanguageLookup](id),
- CONSTRAINT [FK_networkConnectionEffectiveType_id] FOREIGN KEY (networkConnectionEffectiveTypeLookup_id) REFERENCES [mtc_results].[networkConnectionEffectiveTypeLookup](id),
- CONSTRAINT [FK_deviceOrientationLookup_id] FOREIGN KEY (deviceOrientationLookup_id) REFERENCES [mtc_results].[deviceOrientationLookup](id)
+ CONSTRAINT [FK_userAgentLookup_id] FOREIGN KEY (userAgentLookup_id) REFERENCES [mtc_results].[userAgentLookup] (id),
+ CONSTRAINT [FK_navigatorPlatformLookup_id] FOREIGN KEY (navigatorPlatformLookup_id) REFERENCES [mtc_results].[navigatorPlatformLookup] (id),
+ CONSTRAINT [FK_navigatorLanguageLookup_id] FOREIGN KEY (navigatorLanguageLookup_id) REFERENCES [mtc_results].[navigatorLanguageLookup] (id),
+ CONSTRAINT [FK_networkConnectionEffectiveType_id] FOREIGN KEY (networkConnectionEffectiveTypeLookup_id) REFERENCES [mtc_results].[networkConnectionEffectiveTypeLookup] (id),
+ CONSTRAINT [FK_deviceOrientationLookup_id] FOREIGN KEY (deviceOrientationLookup_id) REFERENCES [mtc_results].[deviceOrientationLookup] (id)
 );
