edb00d1eca5bc43de2975232ebc8887e3b6b3692
Jon Shipley <js@ovix.co.uk>
2020-09-08 10:42:54 +0100

    format

--- admin/data/sql/migrations/20200904145620.do.create-results-device-orientation-lookup.sql
+++ admin/data/sql/migrations/20200904145620.do.create-results-device-orientation-lookup.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS mtc_results.deviceOrientationLookup;
 
 CREATE TABLE mtc_results.deviceOrientationLookup
-([id]          [INT] IDENTITY ( 1, 1 ) NOT NULL,
- [createdAt]   [DATETIMEOFFSET](3)     NOT NULL DEFAULT GETUTCDATE(),
- [updatedAt]   [DATETIMEOFFSET](3)     NOT NULL DEFAULT GETUTCDATE(),
- [version]     [ROWVERSION],
+([id]          INT IDENTITY ( 1, 1 ) NOT NULL,
+ [createdAt]   DATETIMEOFFSET(3)     NOT NULL DEFAULT GETUTCDATE(),
+ [updatedAt]   DATETIMEOFFSET(3)     NOT NULL DEFAULT GETUTCDATE(),
+ [version]     ROWVERSION,
  [orientation] NVARCHAR(32),
  CONSTRAINT [PK_deviceOrientationLookup] PRIMARY KEY CLUSTERED ([id] ASC) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON),
  CONSTRAINT [deviceOrientationLookup_orientation_uindex] UNIQUE (orientation)
