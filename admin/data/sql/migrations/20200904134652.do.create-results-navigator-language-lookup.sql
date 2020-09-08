caa2edf7f00063faeec79b1d297d912d6f4800fa
Jon Shipley <js@ovix.co.uk>
2020-09-08 10:38:39 +0100

    format

--- admin/data/sql/migrations/20200904134652.do.create-results-navigator-language-lookup.sql
+++ admin/data/sql/migrations/20200904134652.do.create-results-navigator-language-lookup.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS mtc_results.navigatorLanguageLookup;
 CREATE TABLE mtc_results.navigatorLanguageLookup
-([id]           [INT] IDENTITY ( 1, 1 ) NOT NULL,
- [createdAt]    [DATETIMEOFFSET](3)     NOT NULL DEFAULT GETUTCDATE(),
- [updatedAt]    [DATETIMEOFFSET](3)     NOT NULL DEFAULT GETUTCDATE(),
- [version]      [ROWVERSION],
- [platformLang] nvarchar(36),
+([id]           INT IDENTITY ( 1, 1 ) NOT NULL,
+ [createdAt]    DATETIMEOFFSET(3)     NOT NULL DEFAULT GETUTCDATE(),
+ [updatedAt]    DATETIMEOFFSET(3)     NOT NULL DEFAULT GETUTCDATE(),
+ [version]      ROWVERSION,
+ [platformLang] NVARCHAR(36),
  CONSTRAINT [PK_navigatorLanguageLookup] PRIMARY KEY CLUSTERED ([id] ASC) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON),
  CONSTRAINT [navigatorLanguageLookup_platformLang_uindex] UNIQUE (platformLang)
 );
