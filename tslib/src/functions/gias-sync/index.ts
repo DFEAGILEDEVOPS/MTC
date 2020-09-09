5916982b47cf666085a5f99f6cde31ab81df6b65
Guy Harwood <GuyHarwood@users.noreply.github.com>
2020-09-09 17:15:19 +0100

    Feature/39712 GIAS API integration (#1702)
    * gias soap api app
    
    * ideas
    
    * implement message
    
    * finish off message body
    
    * 500 error, no details :/
    
    * use service url for namespace
    
    * added timestamps
    
    * working example
    
    * working getestab implementation
    
    * multipart extract processor
    
    * consolidate implementation
    
    * ignore code-workspace files
    
    * function definition
    
    * service definitions
    
    * soap service definition
    
    * security header specs
    
    * date time mocking
    
    * body spec
    
    * rename to builder
    
    * gias service
    
    * extract call spec
    
    * results prop
    
    * next
    
    * soap request definition
    
    * end to end successful call
    
    * remove debug
    
    * config checks
    
    * credentials and config
    
    * fix credentials issue
    
    * xml response parsing issue
    
    * parse response
    
    * trim output
    
    * strip prefixes and return estab object
    
    * extract xml parser implementation
    
    * convert es to ts for multipart message util
    
    * disable for now
    
    * response parsing
    
    * boundary parser complete
    
    * lint
    
    * buffer or string issue
    
    * working extract as test
    
    * zip stream parsing
    
    * working zip extract
    
    * gias svc implementation
    
    * gias function working and writing to blob storage
    
    * final changes
    
    * attachment id extractor
    
    * integrate attachment parser
    
    * remove unused dependencies
    
    * Update restarts_steps.rb
    
    Co-authored-by: Mohsen Qureshi <mjq@Mohsens-Mac-mini.local>

---
+++ tslib/src/functions/gias-sync/index.ts
@@ -0,0 +1,20 @@
+import { AzureFunction, Context } from '@azure/functions'
+import { performance } from 'perf_hooks'
+const functionName = 'gias-sync'
+import { GiasService } from './gias.service'
+import config from '../../config'
+
+const timerTrigger: AzureFunction = async function (context: Context, timer: any): Promise<void> {
+  const start = performance.now()
+
+  const giasService = new GiasService()
+  const extractXml = await giasService.GetExtract(config.Gias.ExtractId)
+  context.bindings.giasExtractFile = extractXml
+
+  const end = performance.now()
+  const durationInMilliseconds = end - start
+  const timeStamp = new Date().toISOString()
+  context.log(`${functionName}: ${timeStamp} run complete: ${durationInMilliseconds} ms`)
+}
+
+export default timerTrigger
---
+++ tslib/src/functions/gias-sync/xml-parser.ts
@@ -0,0 +1,30 @@
+import * as xmlParser from 'fast-xml-parser'
+
+const xmlParserOptions = {
+  attributeNamePrefix : '',
+  attrNodeName: 'attr', // default is 'false'
+  textNodeName : 'value',
+  ignoreAttributes : false,
+  ignoreNameSpace : true,
+  allowBooleanAttributes : false,
+  parseNodeValue : true,
+  parseAttributeValue : false,
+  trimValues: true,
+  cdataTagName: '__cdata', // default is 'false'
+  cdataPositionChar: '\\c',
+  parseTrueNumberOnly: false,
+  arrayMode: false, // "strict"
+  attrValueProcessor: (a: any, attrName: string) => a,
+  tagValueProcessor : (t: any, tagName: string) => t,
+  stopNodes: []
+}
+
+export class XmlParser implements IXmlParser {
+  parse (xml: string): any {
+    return xmlParser.parse(xml, xmlParserOptions)
+  }
+}
+
+export interface IXmlParser {
+  parse (xml: string): any
+}
