179f52640c1319921f32dc323c6bb23d74fdbd91
Jon Shipley <js@ovix.co.uk>
2020-09-23 14:11:55 +0100

    Tidy up

--- functions-app/sync-results-to-sql/v1.js
+++ functions-app/sync-results-to-sql/v1.js
@@ -4,31 +4,9 @@ const base = require('../lib/logger')
 const resultsService = require('./service/results.service.js')
 const sqlService = require('../lib/sql/sql.service')
 const name = 'sync-results-to-sql'
+let questionHash
 
 const service = {
-  // /**
-  //  * Process a school - fetch the data from Table Storage and save it in SQL DB
-  //  * @param {id: number, name: string, schoolGuid: string} school
-  //  * @return {Promise<Number>}
-  //  */
-  // processSchool: async function processSchool (school) {
-  //   // Find all outstanding checks in the school
-  //   const newChecks = await resultsService.getNewChecks(school.id)
-  //
-  //   // Retrieve the entire schools marking data from Table Storage, for new and old checks
-  //   const markedChecks = await resultsService.getSchoolResults(school.schoolGuid)
-  //
-  //   // Filter the marking data to only the new checks we need
-  //   const newMarkedChecks = await resultsService.findNewMarkedChecks(newChecks, markedChecks)
-  //
-  //   await resultsService.persistMarkingData(newMarkedChecks)
-  //
-  //   // Invalidate caches: e.g. school result data
-  //   await resultsService.dropCaches(school.id)
-  //
-  //   return newChecks.length
-  // },
-
   /**
    * Entry point for the module.
    * @return {Promise<{}>}
@@ -38,20 +16,26 @@ const service = {
       this.setLogger(loggerFunc)
       resultsService.setLogger(loggerFunc)
     }
-    console.log('message received', checkCompletionMessage)
 
-    // const questionHash = await resultsService.getQuestionData()
+    console.log('message received: ', checkCompletionMessage)
+    console.log('markedAnswers', checkCompletionMessage.markedCheck.markedAnswers)
+
+    this.logger.info(`${name}: message received for check [${checkCompletionMessage.markedCheck.checkCode}]`)
+
+    // Retrieve the entire list of questions, and cache it.
+    if (!questionHash) {
+      console.info(`${name}: fetching question data`)
+      questionHash = await resultsService.getQuestionData()
+    }
 
     // Prepare checkResult insert statement
     const [checkResultSql, checkResultParams] = resultsService.prepareCheckResult(checkCompletionMessage.markedCheck)
 
     // Prepare SQL statements and variables for the Answers
-    // const { answersSql, answersVar } = await resultsService.prepareAnswers(checkCompletionMessage.markedAnswer, questionHash)
+    const [answersSql, answersParams] = resultsService.prepareAnswers(checkCompletionMessage.markedCheck, questionHash)
 
-    const res = await sqlService.modifyWithTransaction(checkResultSql, checkResultParams)
-    console.log('RES', res)
-
-    return {}
+    // Persist everything to the DB in a single transaction
+    return sqlService.modifyWithTransaction(checkResultSql.concat(answersSql), checkResultParams.concat(answersParams))
   }
 }
 
