77b8616c5730933af2a8e59bd0d039045a23a1a8
Jon Shipley <js@ovix.co.uk>
2020-09-24 12:08:31 +0100

    Fix undo migration

--- admin/data/sql/migrations/20200921155139.undo.func-user-permissions.js
+++ admin/data/sql/migrations/20200921155139.undo.func-user-permissions.js
@@ -4,7 +4,7 @@ const config = require('../../../config')
 
 module.exports.generateSql = function () {
   return `
-  REVOKE SELECT ON object::[mtc_admin].[check].[id] to [${config.Sql.FunctionsApp.Username}];
+  REVOKE SELECT ON object::[mtc_admin].[check] (id) to [${config.Sql.FunctionsApp.Username}];
   REVOKE INSERT ON schema::[mtc_results] to [${config.Sql.FunctionsApp.Username}];
   `
 }
