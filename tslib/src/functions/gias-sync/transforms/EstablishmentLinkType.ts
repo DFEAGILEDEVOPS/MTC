7546b41ebcd8c7ef1cb8dc306363c39b56891cae
Guy Harwood <guy@milkchip.com>
2020-09-25 15:42:48 +0100

    update enum from official list

--- tslib/src/functions/gias-sync/transforms/EstablishmentLinkType.ts
+++ tslib/src/functions/gias-sync/transforms/EstablishmentLinkType.ts
@@ -1,13 +1,22 @@
 
 export enum EstablishmentLinkType {
   ChildrensCentreLink = 'CC',
-  Other = '9',
-  Predecessor = '1',
+  Closure = '1I',
+  Expansion = '2A',
+  MergedChangeInAgeRange = '2O',
+  MergedExpansionInSchoolCapacityAndChangerInAgeRange =	'2Q',
+  MergedExpansionOfSchoolCapacity =	'2P',
+  NotApplicable =	'0',
+  Other	= '9',
   PredecessorAmalgamated = '6.1',
+  Predecessor = '1',
   PredecessorMerged = '1L',
+  PredecessorSplitSchool = '1F',
+  ResultOfAmalgamation = '2K',
   SixthFormCentreLink = '4',
   SixthFormCentreSchool = '5',
   Successor = '2',
   SuccessorAmalgamated = '6.2',
-  SuccessorMerged = '6'
+  SuccessorMerged = '6',
+  SuccessorSplitSchool = '2F'
 }
