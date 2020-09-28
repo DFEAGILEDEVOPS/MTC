d3ac07d4abd8f6dba0e7451e44bb8984c85651c5
Guy Harwood <guy@milkchip.com>
2020-09-28 14:48:46 +0100

    indexes

---
+++ admin/data/sql/migrations/20200928103917.do.hdf-indexes.sql
@@ -0,0 +1,3 @@
+
+CREATE UNIQUE INDEX ix_hdf_school_checkWindow_id ON [mtc_admin].hdf (school_id, checkWindow_id);
+CREATE INDEX ix_hdf_user_id ON [mtc_admin].hdf (user_id);
---
+++ admin/data/sql/migrations/20200928103917.undo.hdf-indexes.sql
@@ -0,0 +1,2 @@
+DROP INDEX [mtc_admin].hdf.ix_hdf_school_checkWindow_id;
+DROP INDEX [mtc_admin].hdf.ix_hdf_user_id;
