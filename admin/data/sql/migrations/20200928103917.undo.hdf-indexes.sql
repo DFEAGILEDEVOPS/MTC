b9dff027a8004c5cf70a1d18191443895e87ad7c
Guy Harwood <guy@milkchip.com>
2020-09-29 09:21:51 +0100

    make idempotent

--- admin/data/sql/migrations/20200928103917.undo.hdf-indexes.sql
+++ admin/data/sql/migrations/20200928103917.undo.hdf-indexes.sql
@@ -1,2 +1,9 @@
-DROP INDEX [mtc_admin].hdf.ix_hdf_school_checkWindow_id;
-DROP INDEX [mtc_admin].hdf.ix_hdf_user_id;
+IF EXISTS(SELECT * FROM sys.indexes WHERE object_id = object_id('[mtc_admin].hdf') AND NAME ='ix_hdf_school_checkWindow_id')
+BEGIN
+  DROP INDEX [mtc_admin].hdf.ix_hdf_school_checkWindow_id
+END
+
+IF EXISTS(SELECT * FROM sys.indexes WHERE object_id = object_id('[mtc_admin].hdf') AND NAME ='ix_hdf_user_id')
+BEGIN
+  DROP INDEX [mtc_admin].hdf.ix_hdf_user_id
+END
