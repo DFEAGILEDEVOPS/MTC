61aab780a50b86ca9226286ba5823830d0cf641d
Jon Shipley <jon-shipley@users.noreply.github.com>
2020-10-09 11:19:12 +0100

    Update 20201006113920.do.add-event-type-lookup.sql
    add newline

--- admin/data/sql/migrations/20201006113920.do.add-event-type-lookup.sql
+++ admin/data/sql/migrations/20201006113920.do.add-event-type-lookup.sql
@@ -6,3 +6,4 @@ IF ((SELECT id
         VALUES ('QuestionRendered',
                 'The question screen was shown. For spoken questions this event is registered when the screen is showm, but the question itself is not shown until after the speech has finished.');
     END
+    
