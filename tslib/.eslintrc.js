718dc38b4650268a430bec5903aa9da496d20fad
Guy Harwood <guy@milkchip.com>
2020-10-12 15:38:16 +0100

    turn off no-explicit-any

--- tslib/.eslintrc.js
+++ tslib/.eslintrc.js
@@ -12,5 +12,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     // 'plugin:@typescript-eslint/recommended-requiring-type-checking'
-  ]
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
 }
