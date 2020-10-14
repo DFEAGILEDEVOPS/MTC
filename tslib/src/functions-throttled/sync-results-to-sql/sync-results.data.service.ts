d965801b66d2e43058fa74480c3d699bbebb0c5f
Jon Shipley <js@ovix.co.uk>
2020-10-14 19:53:07 +0100

    Persist navigator + network data

--- tslib/src/functions-throttled/sync-results-to-sql/sync-results.data.service.ts
+++ tslib/src/functions-throttled/sync-results-to-sql/sync-results.data.service.ts
@@ -110,6 +110,11 @@ export class SyncResultsDataService implements ISyncResultsDataService {
     const batteryChargingTimeSecs = R.pathOr(null, ['battery', 'chargingTime'], device)
     const batteryDischargingTimeSecs = R.pathOr(null, ['battery', 'dischargingTime'], device)
     const cpuHardwareConcurrency = R.pathOr(null, ['cpu', 'hardwareConcurrency'], device)
+    const navigatorPlatform = R.pathOr(null, ['navigator', 'platform'], device)
+    const navigatorLanguage = R.pathOr(null, ['navigator', 'language'], device)
+    const cookieEnabled = R.pathOr(null, ['navigator', 'cookieEnabled'], device)
+    const networkConnectionDownlink = R.pathOr(null, ['networkConnection', 'downlink'], device)
+    const networkConnectionEffectiveType = R.pathOr(null, ['networkConnection', 'effectiveType'], device)
 
     // Parse the user-agent
     const userAgent = R.pathOr(null, ['navigator', 'userAgent'], device)
@@ -130,6 +135,11 @@ export class SyncResultsDataService implements ISyncResultsDataService {
     params.push({ name: 'uaOperatingSystemMajorVersion', type: TYPES.Int, value: agent ? agent.getOperatingSystemMajorVersion() : null })
     params.push({ name: 'uaOperatingSystemMinorVersion', type: TYPES.Int, value: agent ? agent.getOperatingSystemMinorVersion() : null })
     params.push({ name: 'uaOperatingSystemPatchVersion', type: TYPES.Int, value: agent ? agent.getOperatingSystemPatchVersion() : null })
+    params.push({ name: 'navigatorPlatform', type: TYPES.NVarChar(255), value: navigatorPlatform })
+    params.push({ name: 'navigatorLanguage', type: TYPES.NVarChar(36), value: navigatorLanguage })
+    params.push({ name: 'cookieEnabled', type: TYPES.Bit, value: cookieEnabled })
+    params.push({ name: 'networkConnectionDownlink', type: TYPES.Float, value: networkConnectionDownlink })
+    params.push({ name: 'networkConnectionEffectiveType', type: TYPES.NVarChar(10), value: networkConnectionEffectiveType })
 
     // tslint:disable:no-trailing-whitespace
     const sql = `
@@ -137,31 +147,60 @@ export class SyncResultsDataService implements ISyncResultsDataService {
         DECLARE @userDeviceId INT;
         DECLARE @browserFamily_lookup_id INT;
         DECLARE @uaOperatingSystemLookup_id INT;
-        
-        
+        DECLARE @navigatorPlatformLookup_id INT;
+        DECLARE @navigatorLanguageLookup_id INT;
+        DECLARE @networkConnectionEffectiveTypeLookup_id INT;
+                
         -- 
         -- See if we can find an existing id for the browser family; create a new one if not
         --
         SET @browserFamily_lookup_id = (SELECT id FROM mtc_results.browserFamilyLookup WHERE family = UPPER(TRIM(@browserFamily)));
-        IF (@browserFamily_lookup_id IS NULL)
+        IF (@browserFamily_lookup_id IS NULL AND @browserFamily IS NOT NULL)
             BEGIN
                -- Create a new browser family
                 INSERT INTO mtc_results.browserFamilyLookup (family) VALUES (UPPER(TRIM(@browserFamily)));
                 SET @browserFamily_lookup_id = (SELECT SCOPE_IDENTITY());
-            END
-        
-        
+            END                
+                
         -- 
         -- See if we can find an Operating System Lookup id, or if not, create a new one
         -- 
         SET @uaOperatingSystemLookup_id = (SELECT id FROM mtc_results.uaOperatingSystemLookup WHERE os = UPPER(TRIM(@uaOperatingSystem)));
-        IF (@uaOperatingSystemLookup_id IS NULL)
+        IF (@uaOperatingSystemLookup_id IS NULL AND @uaOperatingSystem IS NOT NULL)
             BEGIN
-                -- Create a new operating system entry
                 INSERT INTO mtc_results.uaOperatingSystemLookup (os) VALUES (UPPER(TRIM(@uaOperatingSystem)));
                 SET @uaOperatingSystemLookup_id = (SELECT SCOPE_IDENTITY());                
             END
-            
+                
+        -- 
+        -- See if we can find the navigatorPlatform id, or create it if needed
+        -- 
+        SET @navigatorPlatformLookup_id = (SELECT id FROM mtc_results.navigatorPlatformLookup WHERE platform = UPPER(TRIM(@navigatorPlatform)));
+        IF (@navigatorPlatformLookup_id IS NULL AND @navigatorPlatform IS NOT NULL)
+            BEGIN
+                INSERT INTO mtc_results.navigatorPlatformLookup (platform) VALUES (UPPER(TRIM(@navigatorPlatform)));
+                SET @navigatorPlatformLookup_id = (SELECT SCOPE_IDENTITY());
+            END
+        
+        -- 
+        -- See if we can find the navigatorLanguage id, or create it if needed
+        -- 
+        SET @navigatorLanguageLookup_id = (SELECT id FROM mtc_results.navigatorLanguageLookup WHERE platformLang = UPPER(TRIM(@navigatorLanguage)));
+        IF (@navigatorLanguageLookup_id IS NULL AND @navigatorLanguage IS NOT NULL)
+            BEGIN
+                INSERT INTO mtc_results.navigatorLanguageLookup (platformLang) VALUES (UPPER(TRIM(@navigatorLanguage)));
+                SET @navigatorLanguageLookup_id = (SELECT SCOPE_IDENTITY());
+            END
+        
+        -- 
+        -- See if we can find the network connection effective type lookup, otherwise create a new entry
+        -- 
+        SET @networkConnectionEffectiveTypeLookup_id = (SELECT id FROM mtc_results.networkConnectionEffectiveTypeLookup WHERE effectiveType = TRIM(@networkConnectionEffectiveType));
+        IF (@networkConnectionEffectiveTypeLookup_id IS NULL AND @networkConnectionEffectiveType IS NOT NULL)
+            BEGIN
+                INSERT INTO mtc_results.networkConnectionEffectiveTypeLookup (effectiveType) VALUES (@networkConnectionEffectiveType);
+                SET @networkConnectionEffectiveTypeLookup_id = (SELECT SCOPE_IDENTITY());
+            END
         
         --
         -- Insert the data into the userDevice table
@@ -178,7 +217,12 @@ export class SyncResultsDataService implements ISyncResultsDataService {
                                             uaOperatingSystemLookup_id,
                                             uaOperatingSystemMajorVersion,
                                             uaOperatingSystemMinorVersion,
-                                            uaOperatingSystemPatchVersion)
+                                            uaOperatingSystemPatchVersion,
+                                            navigatorPlatformLookup_id,
+                                            navigatorLanguageLookup_id,
+                                            navigatorCookieEnabled,
+                                            networkConnectionDownlink,
+                                            networkConnectionEffectiveTypeLookup_id)
         VALUES (@batteryIsCharging,
                 @batteryLevelPercent,
                 @batteryChargingTimeSecs,
@@ -191,7 +235,12 @@ export class SyncResultsDataService implements ISyncResultsDataService {
                 @uaOperatingSystemLookup_id,
                 @uaOperatingSystemMajorVersion,
                 @uaOperatingSystemMinorVersion,
-                @uaOperatingSystemPatchVersion);
+                @uaOperatingSystemPatchVersion,
+                @navigatorPlatformLookup_id,
+                @navigatorLanguageLookup_id,
+                @cookieEnabled,
+                @networkConnectionDownlink,
+                @networkConnectionEffectiveTypeLookup_id);
 
         SET @userDeviceId = (SELECT SCOPE_IDENTITY());
 
