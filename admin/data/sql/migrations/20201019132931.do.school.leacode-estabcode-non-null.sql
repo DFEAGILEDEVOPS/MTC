3d3e4158b8b9139da0150f5531b4e6d01290bcb0
Guy Harwood <guy@milkchip.com>
2020-10-19 14:00:22 +0100

    migrations for non null

---
+++ admin/data/sql/migrations/20201019132931.do.school.leacode-estabcode-non-null.sql
@@ -0,0 +1,2 @@
+ALTER TABLE [mtc_admin].[school] ALTER COLUMN [leaCode] int NOT NULL
+ALTER TABLE [mtc_admin].[school] ALTER COLUMN [estabCode] int NOT NULL
---
+++ admin/data/sql/migrations/20201019132931.undo.school.leacode-estabcode-non-null.sql
@@ -0,0 +1,2 @@
+ALTER TABLE [mtc_admin].[school] ALTER COLUMN [leaCode] int NULL
+ALTER TABLE [mtc_admin].[school] ALTER COLUMN [estabCode] nvarchar(max) NULL
