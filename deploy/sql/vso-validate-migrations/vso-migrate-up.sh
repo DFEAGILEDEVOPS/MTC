1b95cd31a37a04d599e77eb75b12aa334426dc9d
Guy Harwood <guy@milkchip.com>
2020-11-04 14:31:33 +0000

    remove npm install

--- deploy/sql/vso-validate-migrations/vso-migrate-up.sh
+++ deploy/sql/vso-validate-migrations/vso-migrate-up.sh
@@ -6,5 +6,6 @@
 # $4 is the SQL_DATABASE (database name)
 cd $BUILD_REPOSITORY_LOCALPATH
 cd admin
-npm install
+# not required as retained from last step?
+# npm install
 SQL_ADMIN_USER=$1 SQL_ADMIN_USER_PASSWORD=$2 SQL_APP_USER_PASSWORD=$3 SQL_DATABASE=$4 SQL_AZURE_SCALE=S0 npm run migrate-sql
