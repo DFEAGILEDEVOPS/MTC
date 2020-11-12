ba0204d7bdcfabe9802dfd672c0030c9a0eb151e
Guy Harwood <guy@milkchip.com>
2020-11-12 10:00:39 +0000

    standard-ify

--- _spikes-poc/poison-replay/PoisonReader/index.js
+++ _spikes-poc/poison-replay/PoisonReader/index.js
@@ -1,38 +1,40 @@
-const { ServiceBusClient } = require("@azure/service-bus");
-const connectionString = process.env.AZURE_SERVICE_BUS_CONNECTION_STRING;
+'use strict'
+
+const { ServiceBusClient } = require('@azure/service-bus')
+const connectionString = process.env.AZURE_SERVICE_BUS_CONNECTION_STRING
 
 module.exports = async function (context, req) {
-    context.log(`attempting to read poison queue at ${new Date().toISOString()}`)
-    const queueName = 'message-replay-testing/$deadletterqueue'
-    const sbClient = ServiceBusClient.createFromConnectionString(connectionString);
-    const queueClient = sbClient.createQueueClient(queueName);
+  context.log(`attempting to read poison queue at ${new Date().toISOString()}`)
+  const queueName = 'message-replay-testing/$deadletterqueue'
+  const sbClient = ServiceBusClient.createFromConnectionString(connectionString)
+  const queueClient = sbClient.createQueueClient(queueName)
 
-    const batchCount = 10;
-    const batchSize = 50;
-    let totalPeeked = 0;
+  const batchCount = 10
+  const batchSize = 50
+  let totalPeeked = 0
 
-    try {
-      for (let i = 0; i < batchCount; i++) {
-        const messages = await queueClient.peek(batchSize);
-        if (!messages.length) {
-          context.log("No more messages to peek");
-          break;
-        }
-        totalPeeked += messages.length;
-        context.log(`${messages.length} in current batch`)
-        context.log(`Peeking message #${i}: ${JSON.stringify(messages[0].body)}`);
+  try {
+    for (let i = 0; i < batchCount; i++) {
+      const messages = await queueClient.peek(batchSize)
+      if (!messages.length) {
+        context.log('No more messages to peek')
+        break
       }
-      await queueClient.close();
-      context.log(`seen ${totalPeeked} messages in total`)
+      totalPeeked += messages.length
+      context.log(`${messages.length} in current batch`)
+      context.log(`Peeking message #${i}: ${JSON.stringify(messages[0].body)}`)
     }
-    catch (error) {
-      context.log.error(error)
-    } finally {
-      await sbClient.close();
-    }
+    await queueClient.close()
+    context.log(`seen ${totalPeeked} messages in total`)
+  }
+  catch (error) {
+    context.log.error(error)
+  } finally {
+    await sbClient.close()
+  }
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: 'finished'
-    };
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: 'finished'
+  }
 }
--- _spikes-poc/poison-replay/PoisonReplay/index.js
+++ _spikes-poc/poison-replay/PoisonReplay/index.js
@@ -1,63 +1,64 @@
-const { ServiceBusClient, ReceiveMode } = require("@azure/service-bus");
-const connectionString = process.env.AZURE_SERVICE_BUS_CONNECTION_STRING;
+'use strict'
+
+const { ServiceBusClient, ReceiveMode } = require('@azure/service-bus')
+const connectionString = process.env.AZURE_SERVICE_BUS_CONNECTION_STRING
 
 module.exports = async function (context, req) {
-
-    const queueName = (req.query.name || (req.body && req.body.name));
-    if (!queueName || queueName.length === 0) {
-      context.res = {
-        status: 400,
-        body: 'name is required as query or body property'
-      }
-      return;
-    }
-
-    const deadLetterQueueName = `${queueName}/$deadletterqueue`
-    const sbClient = ServiceBusClient.createFromConnectionString(connectionString);
-    const deadLetterQueueClient = sbClient.createQueueClient(deadLetterQueueName);
-    const queueClient = sbClient.createQueueClient(queueName);
-
-    const batchSize = 50;
-    const deadLetterReceiver = deadLetterQueueClient.createReceiver(ReceiveMode.peekLock)
-    const sender = queueClient.createSender()
-
-    try {
-      while(true) {
-        const deadLetterMessages = await deadLetterReceiver.receiveMessages(batchSize)
-        if (!deadLetterMessages.length) {
-          context.log(`No more messages on queue ${deadLetterQueueName}`);
-          break;
-        }
-        context.log(`received ${deadLetterMessages.length} messages from ${deadLetterQueueName}.`)
-        const newMessages = []
-        for (let index = 0; index < deadLetterMessages.length; index++) {
-          const message = deadLetterMessages[index];
-          newMessages.push({
-            body: message.body
-          })
-        }
-        await sender.sendBatch(newMessages)
-        for (let index = 0; index < deadLetterMessages.length; index++) {
-          const msg = deadLetterMessages[index];
-          await msg.complete()
-        }
-      }
-    } catch (error) {
-      context.log.error(error)
-      context.res = {
-        status: 500,
-        body: `error occured:${error.message}`
-      }
-    } finally {
-      await sender.close()
-      await deadLetterReceiver.close()
-      await queueClient.close()
-      await deadLetterQueueClient.close()
-      await sbClient.close()
-    }
-
+  const queueName = (req.query.name || (req.body && req.body.name))
+  if (!queueName || queueName.length === 0) {
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: 'done'
-    };
+      status: 400,
+      body: 'name is required as query or body property'
+    }
+    return
+  }
+
+  const deadLetterQueueName = `${queueName}/$deadletterqueue`
+  const sbClient = ServiceBusClient.createFromConnectionString(connectionString)
+  const deadLetterQueueClient = sbClient.createQueueClient(deadLetterQueueName)
+  const queueClient = sbClient.createQueueClient(queueName)
+
+  const batchSize = 50
+  const deadLetterReceiver = deadLetterQueueClient.createReceiver(ReceiveMode.peekLock)
+  const sender = queueClient.createSender()
+
+  try {
+    while (true) {
+      const deadLetterMessages = await deadLetterReceiver.receiveMessages(batchSize)
+      if (!deadLetterMessages.length) {
+        context.log(`No more messages on queue ${deadLetterQueueName}`)
+        break
+      }
+      context.log(`received ${deadLetterMessages.length} messages from ${deadLetterQueueName}.`)
+      const newMessages = []
+      for (let index = 0; index < deadLetterMessages.length; index++) {
+        const message = deadLetterMessages[index]
+        newMessages.push({
+          body: message.body
+        })
+      }
+      await sender.sendBatch(newMessages)
+      for (let index = 0; index < deadLetterMessages.length; index++) {
+        const msg = deadLetterMessages[index]
+        await msg.complete()
+      }
+    }
+  } catch (error) {
+    context.log.error(error)
+    context.res = {
+      status: 500,
+      body: `error occured:${error.message}`
+    }
+  } finally {
+    await sender.close()
+    await deadLetterReceiver.close()
+    await queueClient.close()
+    await deadLetterQueueClient.close()
+    await sbClient.close()
+  }
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: 'done'
+  }
 }
