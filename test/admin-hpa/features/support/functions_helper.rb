cfa1e0d0d905de19f0b8ab4862ef32737defe14b
Mohsen Qureshi <activemq@users.noreply.github.com>
2020-11-27 14:37:47 +0000

    updated functions request to include required header (#1755)
    Co-authored-by: Mohsen Qureshi <mjq@Mohsens-Mac-mini.local>

--- test/admin-hpa/features/support/functions_helper.rb
+++ test/admin-hpa/features/support/functions_helper.rb
@@ -2,7 +2,7 @@ class FunctionsHelper
   include HTTParty
 
   def self.trigger_func(function_name)
-    HTTParty.post(ENV['FUNC_THROTTLED_BASE_URL'] + "/#{function_name}", :body => {'input' => "test"}.to_json, headers: {'Content-Type' => 'application/json'})
+    HTTParty.post(ENV['FUNC_THROTTLED_BASE_URL'] + "/#{function_name}", :body => {'input' => "test"}.to_json, headers: {'Content-Type' => 'application/json', 'x-functions-key' => ENV['FUNC_THROTTLED_MASTER_KEY']})
   end
 
 end
