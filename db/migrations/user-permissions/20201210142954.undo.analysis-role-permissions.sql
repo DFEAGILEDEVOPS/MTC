fa6984488ae11f4f3963f0f563e24b633cd29f16
Guy Harwood <guy@milkchip.com>
2020-12-10 16:31:13 +0000

    align undo script

--- db/migrations/user-permissions/20201210142954.undo.analysis-role-permissions.sql
+++ db/migrations/user-permissions/20201210142954.undo.analysis-role-permissions.sql
@@ -1,3 +1,4 @@
 REVOKE SELECT ON SCHEMA ::[mtc_admin] TO [data_analysis_role]
 REVOKE SELECT ON SCHEMA ::[mtc_results] TO [data_analysis_role]
-REVOKE CONTROL ON SCHEMA ::[mtc_analysis] TO [data_analysis_role]
+REVOKE SELECT, UPDATE, INSERT, DELETE, EXECUTE, ALTER ON SCHEMA ::[mtc_analysis] TO [data_analysis_role]
+REVOKE CREATE VIEW TO [data_analysis_role]
