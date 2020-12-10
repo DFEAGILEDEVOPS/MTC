d1f5170030d6769d867dfe2bf4d56022cf41ab4a
Guy Harwood <guy@milkchip.com>
2020-12-10 16:28:48 +0000

    fix permissions

--- db/migrations/user-permissions/20201210142954.do.analysis-role-permissions.sql
+++ db/migrations/user-permissions/20201210142954.do.analysis-role-permissions.sql
@@ -1,3 +1,5 @@
 GRANT SELECT ON SCHEMA ::[mtc_admin] TO [data_analysis_role]
 GRANT SELECT ON SCHEMA ::[mtc_results] TO [data_analysis_role]
-GRANT CONTROL ON SCHEMA ::[mtc_analysis] TO [data_analysis_role]
+GRANT SELECT, UPDATE, INSERT, DELETE, EXECUTE, ALTER ON SCHEMA ::[mtc_analysis] TO [data_analysis_role]
+GRANT CREATE VIEW TO [data_analysis_role]
+
