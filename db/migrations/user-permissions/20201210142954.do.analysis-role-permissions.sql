ce23f3485ea87f28c51421f8ea92d35d9082c894
Guy Harwood <guy@milkchip.com>
2020-12-10 14:37:30 +0000

    analysis schema, role and permissions

---
+++ db/migrations/user-permissions/20201210142954.do.analysis-role-permissions.sql
@@ -0,0 +1,3 @@
+GRANT SELECT ON SCHEMA ::[mtc_admin] TO [data_analysis_role]
+GRANT SELECT ON SCHEMA ::[mtc_results] TO [data_analysis_role]
+GRANT CONTROL ON SCHEMA ::[mtc_analysis] TO [data_analysis_role]
---
+++ db/migrations/user-permissions/20201210142954.undo.analysis-role-permissions.sql
@@ -0,0 +1,3 @@
+REVOKE SELECT ON SCHEMA ::[mtc_admin] TO [data_analysis_role]
+REVOKE SELECT ON SCHEMA ::[mtc_results] TO [data_analysis_role]
+REVOKE CONTROL ON SCHEMA ::[mtc_analysis] TO [data_analysis_role]
