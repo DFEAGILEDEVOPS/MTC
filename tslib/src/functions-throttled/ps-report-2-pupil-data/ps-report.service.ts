b793ab12b525d7ab113fd4e505317703807a6db5
Jon Shipley <jon-shipley@users.noreply.github.com>
2021-01-11 14:41:23 +0000

    Feature/44783 ps report func 2 (#1783)
    * [tslib] add new function skeleton
    
    * [tslib] bug fix timer trigger definition
    
    * [tslib] change line ending to \n
    
    * [tslib] Initial data service work for retrieving pupil results
    
    WIP
    
    * [tslib] add check entity
    
    * Add doc
    
    * [tslib] add checkForm
    
    * [tslib] add answers
    
    * [tslib] add inputs
    
    * [tslib] add device
    
    * Whitespace changes only  - convert newlines to \n
    
    * [tslib] add events
    
    * [tslib] Type incoming message
    
    * Tweaks
    
    * Update doc
    
    * Fix a test
    
    Typo was causing the live check end date to not be setup correctly.  The default is 'now plus 4 days',
    so this bug was only apparent inthe last 4 days of the year.
    
    * add tests
    
    * add tests
    
    * cherry pick fix for test
    
    * add documentation
    
    * refactor
    
    remove all duplicate calls to `getSchool()`
    
    * remove TODO item
    
    * refactor
    
    cache the checkForm lookups as there are only a small number of checkForms
    
    * whitespace change only
    
    newlines converted to \n
    
    * update function gulpfiles
    
    refactor for gulp v4
    remove duplicate step in `yarn build` to remove spec files from the dist folder.  Not needed as this is covered in the gulp task.
    
    * [func-throttled] Add watch task to gulp
    
    * Freeze outputs
    
    * Mod gulpfiles
    
    Only run watch when called by start:dev
    
    * [tslib, func-consumption, func-throttled] upgrade deps
    
    * Convert line endings to unix standard
    
    * Clean up TODO items
    
    remove a method
    add documentation
    
    * Remove dev packages that are no longer used
    
    * Update docs

---
+++ tslib/src/functions-throttled/ps-report-2-pupil-data/ps-report.service.ts
@@ -0,0 +1,42 @@
+import { ConsoleLogger, ILogger } from '../../common/logger'
+import { IPsReportDataService, PsReportDataService } from './ps-report.data.service'
+import { Pupil, PupilResult, School } from './models'
+const functionName = 'ps-report-2-pupil-data'
+
+export class PsReportService {
+  private readonly logger: ILogger
+  private readonly dataService: IPsReportDataService
+  private readonly outputBinding: PupilResult[]
+
+  constructor (outputBinding: any[], logger: ILogger, dataService?: IPsReportDataService) {
+    this.outputBinding = outputBinding
+    this.logger = logger ?? new ConsoleLogger()
+    this.dataService = dataService ?? new PsReportDataService(this.logger)
+  }
+
+  async process (schoolUuid: string): Promise<void> {
+    let pupils: readonly Pupil[]
+    let school: School | undefined
+    try {
+      pupils = await this.dataService.getPupils(schoolUuid)
+    } catch (error) {
+      this.logger.error(`${functionName} ERROR - unable to fetch pupils for school ${schoolUuid}`)
+      throw error
+    }
+    for (let i = 0; i < pupils.length; i++) {
+      const pupil = pupils[i]
+      if (school === undefined) {
+        school = await this.dataService.getSchool(pupil.schoolId)
+      }
+      try {
+        const result: PupilResult = await this.dataService.getPupilData(pupil, school)
+        // this.logger.verbose(`${functionName}: pupil result retrieved: ${JSON.stringify(result, null, 4)}`)
+        this.outputBinding.push(result)
+      } catch (error) {
+        // Ignore the error on the particular pupil and carry on so it reports on the rest of the school
+        this.logger.error(`${functionName} ERROR: Failed to retrieve pupil data for pupil ${pupil.slug} in school ${schoolUuid}
+        Error was ${error.message}`)
+      }
+    }
+  }
+}
---
+++ tslib/src/functions-throttled/ps-report-2-pupil-data/readme.md
@@ -0,0 +1,55 @@
+# Psychometric report pipeline #2 (throttled)
+
+This is the second function in the psychometric report pipeline.  It extracts the pupil data and places the results on another service bus queue `ps-report-staging`.
+
+## Incoming message - school entity
+
+```typescript
+{ 
+	name: string
+	uuid: string
+}
+```
+
+
+
+## Processing
+
+The function extracts a lot of data for each pupil from the SQL Server.
+
+## Inputs
+
+Azure service bus queue trigger `ps-report-schools`
+
+## Output
+
+Azure service bus queue `ps-report-staging` queue. See [the models](./models.ts)
+
+
+
+## Data extracted
+
+Data is read from SQL Server: School, Pupil, Check, CheckConfig, Answer, Device, Events, UserInputs
+
+
+
+## Error handling
+
+Individual pupil records may throw and be logged. The error is caught so that other pupils can still be updated.
+
+
+
+## Consumers
+
+Data Team, Psychometric Team
+
+
+
+## Notes
+
+none
+
+
+
+
+
