acc73d58abaffc1817b3c2d0956789d7176c95fe
Guy Harwood <GuyHarwood@users.noreply.github.com>
2021-01-11 17:02:35 +0000

    Feature/44820 document check payload and check events (#1784)
    * document events as table
    
    * document events, create received check lookup function
    
    * wire up function
    
    * function bindings error
    
    * include extensions.csproj
    
    * working version of check-viewer.  add payload example
    
    * add compression detail to docs
    
    * lint fix
    
    * corrections from comments
    
    * rename const

---
+++ tslib/src/functions/util-received-check-viewer/index.ts
@@ -0,0 +1,43 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions'
+import { performance } from 'perf_hooks'
+import config from '../../config'
+import * as azh from '../../azure/storage-helper'
+import { CompressionService } from '../../common/compression-service'
+
+const functionName = 'util-received-check-reader'
+
+function finish (start: number, context: Context): void {
+  const end = performance.now()
+  const durationInMilliseconds = end - start
+  const timeStamp = new Date().toISOString()
+  context.log(`${functionName}: ${timeStamp} run complete: ${durationInMilliseconds} ms`)
+}
+
+const checkRetriever: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+  if (!config.DevTestUtils.TestSupportApi) {
+    context.log('exiting as not enabled (default behaviour)')
+    context.done()
+    return
+  }
+  const start = performance.now()
+  if (req.query.checkCode === undefined || req.query.schoolUUID === undefined) {
+    context.res = {
+      statusCode: 400,
+      body: 'checkCode and schoolUUID properties are required'
+    }
+  }
+  const tableService = new azh.AsyncTableService()
+  const receivedCheck = await tableService.retrieveEntityAsync('receivedCheck', req.query.schoolUUID, req.query.checkCode)
+  const archive = receivedCheck.archive._
+  const compressionService = new CompressionService()
+  const decompressed = compressionService.decompress(archive)
+  context.res = {
+    body: decompressed,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  finish(start, context)
+}
+
+export default checkRetriever
