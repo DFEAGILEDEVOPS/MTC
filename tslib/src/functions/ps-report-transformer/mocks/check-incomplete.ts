2a53d042a6607de6ce06a39a762238393cedc624
Jon Shipley <js@ovix.co.uk>
2021-01-19 16:10:43 +0000

    Add missing mock

---
+++ tslib/src/functions/ps-report-transformer/mocks/check-incomplete.ts
@@ -0,0 +1,17 @@
+import { Check } from '../../../functions-throttled/ps-report-2-pupil-data/models'
+
+export const check: Check = {
+  id: 31,
+  checkCode: 'xyz-def-988',
+  checkFormId: 9,
+  checkWindowId: 1,
+  complete: false,
+  completedAt: null,
+  inputAssistantAddedRetrospectively: false,
+  isLiveCheck: true,
+  mark: null,
+  processingFailed: false,
+  pupilLoginDate: null,
+  received: true,
+  restartNumber: 0
+}
