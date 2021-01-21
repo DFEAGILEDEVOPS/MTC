aa925ddec9236d2404f5557fdcbb046bc91ed7ab
Jon Shipley <js@ovix.co.uk>
2021-01-21 12:02:37 +0000

    refactor part 2

--- tslib/src/functions/ps-report-transformer/psychometrician-report-line-answer.class.spec.ts
+++ tslib/src/functions/ps-report-transformer/psychometrician-report-line-answer.class.spec.ts
@@ -1,6 +1,6 @@
-import { ReportLineAnswer } from './models'
 import { Input } from '../../functions-throttled/ps-report-2-pupil-data/models'
 import moment from 'moment/moment'
+import { ReportLineAnswer } from './report-line-answer.class'
 
 describe('PsychometricReportLineAnswer', () => {
   let sut: ReportLineAnswer
