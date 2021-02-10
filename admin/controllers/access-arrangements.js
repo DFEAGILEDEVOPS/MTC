30e17e2e1999290601ad3313379841496d3776af
Guy Harwood <guy@milkchip.com>
2021-02-10 16:58:40 +0000

    break test, fix tomorrow

--- admin/controllers/access-arrangements.js
+++ admin/controllers/access-arrangements.js
@@ -206,7 +206,7 @@ const controller = {
   getDeleteAccessArrangements: async function getDeleteAccessArrangements (req, res, next) {
     const aaViewMode = await accessArrangementsService.getCurrentViewMode(req.user.timezone)
     if (aaViewMode !== aaViewModes.edit) {
-      throw new AccessArrangementsNotEditableError()
+      next(new AccessArrangementsNotEditableError())
     }
     let pupil
     try {
