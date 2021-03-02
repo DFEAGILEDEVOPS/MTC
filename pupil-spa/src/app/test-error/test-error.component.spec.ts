c8dda1890fb469943dd617f8cb4b369b1f60fdbc
Jon Shipley <js@ovix.co.uk>
2021-03-02 09:55:08 +0000

    [pupil-spa] add test error page

---
+++ pupil-spa/src/app/test-error/test-error.component.spec.ts
@@ -0,0 +1,33 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestErrorComponent } from './test-error.component';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: ''
+})
+export class MockAppHeaderComponent {
+}
+
+describe('TestErrorComponent', () => {
+  let component: TestErrorComponent;
+  let fixture: ComponentFixture<TestErrorComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TestErrorComponent ]
+    })
+    .compileComponents();
+  }));
+
+  it('should create', () => {
+    try {
+      fixture = TestBed.createComponent(TestErrorComponent);
+      component = fixture.componentInstance;
+      component.ngOnInit();
+      fail('expected to throw');
+    } catch (error) {
+      expect(error).toMatch('Pupil SPA Front end test error');
+    }
+  });
+});
