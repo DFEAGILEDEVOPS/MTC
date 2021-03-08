396de7ab40312f80a56fe7a0984624bd9f05f765
Guy Harwood <guy@milkchip.com>
2021-03-08 12:14:36 +0000

    storage script is broken due to jq syntax. needs refining

--- deploy/azure/key-vault/key-rotation/storage.sh
+++ deploy/azure/key-vault/key-rotation/storage.sh
@@ -9,8 +9,7 @@ STORAGE_ACCOUNT_NAME=$3 # storage account
 KEY_TYPE=$4 # accepted values are 'primary' or 'secondary'
 
 STORAGE_ACCOUNT_KEY_TYPE="" # valid values are 'primary', 'secondary'
-KEY_IDENTIFIER="" # valid values are 'key1', 'key2'
-KEY_INDEX="" # as its an array, key1 is '0' and key2 is '1'
+KEY_IDENTIFIER="" # as its an array, key1 is '0' and key2 is '1'
 
 # Azure CLI uses slightly different identifiers for key type across different services :-/
 # align them for each service...
@@ -18,15 +17,14 @@ if [ "$KEY_TYPE" = "primary" ]
 then
   # set keys to primary
   STORAGE_ACCOUNT_KEY_TYPE="primary"
-  KEY_IDENTIFIER="key1"
-  KEY_INDEX=0
+  KEY_IDENTIFIER="'.[0] | .value'"
 
 elif [ "$KEY_TYPE" = "secondary" ]
 then
   # set keys to secondary
   STORAGE_ACCOUNT_KEY_TYPE="secondary"
-  KEY_IDENTIFIER="key2"
-  KEY_INDEX=1
+  KEY_IDENTIFIER="'.[1] | .value'"
+  KEY_INDEX="1"
 
 else
   # throw error
@@ -51,7 +49,7 @@ fi
 #   }
 # ]
 echo "renewing $STORAGE_ACCOUNT_KEY_TYPE key for storage account $STORAGE_ACCOUNT_NAME..."
-KEY_VALUE=$(az storage account keys renew --resource-group $RES_GROUP --account-name $STORAGE_ACCOUNT_NAME --key $STORAGE_ACCOUNT_KEY_TYPE | jq -r .[$KEY_INDEX].$KEY_IDENTIFIER)
+KEY_VALUE=$(az storage account keys renew --resource-group $RES_GROUP --account-name $STORAGE_ACCOUNT_NAME --key $STORAGE_ACCOUNT_KEY_TYPE | jq $KEY_IDENTIFIER)
 
 # temp debug output...
 echo "key value is $KEY_VALUE"
