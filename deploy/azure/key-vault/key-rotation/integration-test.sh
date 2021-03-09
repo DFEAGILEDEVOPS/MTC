9e1846c3028125ea6fe038fbf4dd54e6103f9cb7
Guy Harwood <guy@milkchip.com>
2021-03-09 21:04:47 +0000

    still having issues with secondary key format

--- deploy/azure/key-vault/key-rotation/integration-test.sh
+++ deploy/azure/key-vault/key-rotation/integration-test.sh
@@ -87,7 +87,7 @@ SB_CON_STRING_KV=$(az keyvault secret show --name "ServiceBusConnectionString-$S
 echo "extracting current $KEY_TYPE service values for comparison..."
 REDIS_KEY_ACTUAL=$(az redis list-keys --name $REDIS_NAME --resource-group $RES_GROUP | jq -r .secondaryKey)
 STORAGE_KEY_ACTUAL=$(az storage account keys list --resource-group $RES_GROUP -n $STORAGE_ACCOUNT_NAME | jq -r .[1].value)
-STORAGE_CON_STRING_ACTUAL=$(az storage account show-connection-string --resource-group $RES_GROUP -n $STORAGE_ACCOUNT_NAME --key secondary | jq -r .connectionString)
+STORAGE_CON_STRING_ACTUAL=$(az storage account show-connection-string -g $RES_GROUP -n $STORAGE_ACCOUNT_NAME --key secondary | jq -r .connectionString)
 SB_CON_STRING_ACTUAL=$(az servicebus namespace authorization-rule keys list --resource-group $RES_GROUP --namespace-name $SERVICE_BUS_NAME --name $SERVICE_BUS_USER | jq -r .primaryConnectionString)
 
 echo "comparing secondary keys..."
