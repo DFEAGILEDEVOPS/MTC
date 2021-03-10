bd06b18f32a485b61666a5392a8930b37657c467
Guy Harwood <guy@milkchip.com>
2021-03-10 15:24:32 +0000

    break up script into functions

--- deploy/azure/key-vault/key-rotation/integration-test.sh
+++ deploy/azure/key-vault/key-rotation/integration-test.sh
@@ -5,16 +5,6 @@ set -e
 
 # integration test for redis.sh, service-bus.sh and storage.sh
 
-# test failure handler
-fail()
-{
-  HASHLINE="#############################################################################################"
-  echo $HASHLINE
-  echo "Test Failure: $1" 1>&2
-  echo $HASHLINE
-  exit 1
-}
-
 # input parameters
 RES_GROUP=$1 # target resource group
 KEY_VAULT_NAME=$2 # key vault instance
@@ -23,92 +13,89 @@ STORAGE_ACCOUNT_NAME=$4 # storage account
 REDIS_NAME=$5 # redis instance
 SERVICE_BUS_USER=$6 # target user for service bus key
 
-# 1. renew primary keys...
-KEY_TYPE="primary"
-source redis.sh $RES_GROUP $KEY_VAULT_NAME $REDIS_NAME $KEY_TYPE
-source service-bus.sh $RES_GROUP $KEY_VAULT_NAME $SERVICE_BUS_NAME $KEY_TYPE $SERVICE_BUS_USER
-source storage.sh $RES_GROUP $KEY_VAULT_NAME $STORAGE_ACCOUNT_NAME $KEY_TYPE
+# test failure handler
+function fail()
+{
+  HASHLINE="#############################################################################################"
+  echo $HASHLINE
+  echo "Test Failure: $1" 1>&2
+  echo $HASHLINE
+  exit 1
+}
 
-# 2. extract current values from key vault
-echo "extracting current key vault values for comparison..."
-REDIS_KEY_KV=$(az keyvault secret show --name "RedisKey" --vault-name $KEY_VAULT_NAME | jq -r .value)
-STORAGE_KEY_KV=$(az keyvault secret show --name "StorageAccountKey" --vault-name $KEY_VAULT_NAME | jq -r .value)
-STORAGE_CON_STRING_KV=$(az keyvault secret show --name "StorageAccountConnectionString" --vault-name $KEY_VAULT_NAME | jq -r .value)
-SB_CON_STRING_KV=$(az keyvault secret show --name "ServiceBusConnectionString-$SERVICE_BUS_USER" --vault-name $KEY_VAULT_NAME | jq -r .value)
+function renew_keys()
+{
+  local KEY_TYPE=$1
+  source redis.sh $RES_GROUP $KEY_VAULT_NAME $REDIS_NAME $KEY_TYPE
+  source service-bus.sh $RES_GROUP $KEY_VAULT_NAME $SERVICE_BUS_NAME $KEY_TYPE $SERVICE_BUS_USER
+  source storage.sh $RES_GROUP $KEY_VAULT_NAME $STORAGE_ACCOUNT_NAME $KEY_TYPE
 
-# 3. extract current values from each service
-echo "extracting current $KEY_TYPE key values from azure services for comparison..."
-REDIS_KEY_ACTUAL=$(az redis list-keys --name $REDIS_NAME --resource-group $RES_GROUP | jq -r .primaryKey)
-STORAGE_KEY_ACTUAL=$(az storage account keys list -g $RES_GROUP -n $STORAGE_ACCOUNT_NAME | jq -r .[0].value)
-STORAGE_CON_STRING_ACTUAL=$(az storage account show-connection-string -g $RES_GROUP -n $STORAGE_ACCOUNT_NAME --key primary | jq -r .connectionString)
-SB_CON_STRING_ACTUAL=$(az servicebus namespace authorization-rule keys list --resource-group $RES_GROUP --namespace-name $SERVICE_BUS_NAME --name $SERVICE_BUS_USER | jq -r .primaryConnectionString)
+}
 
-# 4. compare values for equality
-if [[ $REDIS_KEY_KV != $REDIS_KEY_ACTUAL ]];
-then
-  fail "Redis Primary Key in key vault does not match actual value"
-fi
+function get_key_vault_values()
+{
+  echo "extracting current key vault values for comparison..."
+  REDIS_KEY_KV=$(az keyvault secret show --name "RedisKey" --vault-name $KEY_VAULT_NAME | jq -r .value)
+  STORAGE_KEY_KV=$(az keyvault secret show --name "StorageAccountKey" --vault-name $KEY_VAULT_NAME | jq -r .value)
+  STORAGE_CON_STRING_KV=$(az keyvault secret show --name "StorageAccountConnectionString" --vault-name $KEY_VAULT_NAME | jq -r .value)
+  SB_CON_STRING_KV=$(az keyvault secret show --name "ServiceBusConnectionString-$SERVICE_BUS_USER" --vault-name $KEY_VAULT_NAME | jq -r .value)
 
-if [[ $STORAGE_KEY_KV != $STORAGE_KEY_ACTUAL ]];
-then
-  fail "Storage Account Primary Key in key vault does not match actual value"
-fi
+}
 
-if [[ $STORAGE_CON_STRING_KV != $STORAGE_CON_STRING_ACTUAL ]];
-then
-  fail "Storage Account Primary Connection String in key vault does not match actual value"
-fi
+function get_service_primary_key_values()
+{
+  echo "extracting primary key values from azure services for comparison..."
+  REDIS_KEY_ACTUAL=$(az redis list-keys --name $REDIS_NAME --resource-group $RES_GROUP | jq -r .primaryKey)
+  STORAGE_KEY_ACTUAL=$(az storage account keys list -g $RES_GROUP -n $STORAGE_ACCOUNT_NAME | jq -r .[0].value)
+  STORAGE_CON_STRING_ACTUAL=$(az storage account show-connection-string -g $RES_GROUP -n $STORAGE_ACCOUNT_NAME --key primary | jq -r .connectionString)
+  SB_CON_STRING_ACTUAL=$(az servicebus namespace authorization-rule keys list --resource-group $RES_GROUP --namespace-name $SERVICE_BUS_NAME --name $SERVICE_BUS_USER | jq -r .primaryConnectionString)
+}
 
-if [[ $SB_CON_STRING_KV != $SB_CON_STRING_ACTUAL ]];
-then
-  fail "Service Bus Connection String for $SERVICE_BUS_USER in key vault does not match actual value"
-fi
+function get_service_secondary_key_values()
+{
+  echo "extracting secondary key values from azure services for comparison..."
+  REDIS_KEY_ACTUAL=$(az redis list-keys --name $REDIS_NAME --resource-group $RES_GROUP | jq -r .secondaryKey)
+  STORAGE_KEY_ACTUAL=$(az storage account keys list --resource-group $RES_GROUP -n $STORAGE_ACCOUNT_NAME | jq -r .[1].value)
+  STORAGE_CON_STRING_ACTUAL=$(az storage account show-connection-string -g $RES_GROUP -n $STORAGE_ACCOUNT_NAME --key secondary | jq -r .connectionString)
+  SB_CON_STRING_ACTUAL=$(az servicebus namespace authorization-rule keys list --resource-group $RES_GROUP --namespace-name $SERVICE_BUS_NAME --name $SERVICE_BUS_USER | jq -r .secondaryConnectionString)
+}
 
+function compare_key_vault_to_service()
+{
+  if [[ $REDIS_KEY_KV != $REDIS_KEY_ACTUAL ]];
+  then
+    fail "Redis Key in key vault does not match actual value"
+  fi
+
+  if [[ $STORAGE_KEY_KV != $STORAGE_KEY_ACTUAL ]];
+  then
+    fail "Storage Account Key in key vault does not match actual value"
+  fi
+
+  if [[ $STORAGE_CON_STRING_KV != $STORAGE_CON_STRING_ACTUAL ]];
+  then
+    fail "Storage Account Connection String in key vault does not match actual value"
+  fi
+
+  if [[ $SB_CON_STRING_KV != $SB_CON_STRING_ACTUAL ]];
+  then
+    fail "Service Bus Connection String for $SERVICE_BUS_USER in key vault does not match actual value"
+  fi
+}
+
+echo "testing primary key rotation..."
+renew_keys "primary"
+get_key_vault_values
+get_service_primary_key_values
+compare_key_vault_to_service
 echo "primary keys test successful."
 
-# 1. renew secondary keys...
-KEY_TYPE="secondary"
-source redis.sh $RES_GROUP $KEY_VAULT_NAME $REDIS_NAME $KEY_TYPE
-source service-bus.sh $RES_GROUP $KEY_VAULT_NAME $SERVICE_BUS_NAME $KEY_TYPE $SERVICE_BUS_USER
-source storage.sh $RES_GROUP $KEY_VAULT_NAME $STORAGE_ACCOUNT_NAME $KEY_TYPE
-
-# 2. extract current values from key vault
-echo "extracting current key vault values for comparison..."
-REDIS_KEY_KV=$(az keyvault secret show --name "RedisKey" --vault-name $KEY_VAULT_NAME | jq -r .value)
-STORAGE_KEY_KV=$(az keyvault secret show --name "StorageAccountKey" --vault-name $KEY_VAULT_NAME | jq -r .value)
-STORAGE_CON_STRING_KV=$(az keyvault secret show --name "StorageAccountConnectionString" --vault-name $KEY_VAULT_NAME | jq -r .value)
-SB_CON_STRING_KV=$(az keyvault secret show --name "ServiceBusConnectionString-$SERVICE_BUS_USER" --vault-name $KEY_VAULT_NAME | jq -r .value)
-
-# 3. extract current values from each service
-echo "extracting current $KEY_TYPE service values for comparison..."
-REDIS_KEY_ACTUAL=$(az redis list-keys --name $REDIS_NAME --resource-group $RES_GROUP | jq -r .secondaryKey)
-STORAGE_KEY_ACTUAL=$(az storage account keys list --resource-group $RES_GROUP -n $STORAGE_ACCOUNT_NAME | jq -r .[1].value)
-STORAGE_CON_STRING_ACTUAL=$(az storage account show-connection-string -g $RES_GROUP -n $STORAGE_ACCOUNT_NAME --key secondary | jq -r .connectionString)
-SB_CON_STRING_ACTUAL=$(az servicebus namespace authorization-rule keys list --resource-group $RES_GROUP --namespace-name $SERVICE_BUS_NAME --name $SERVICE_BUS_USER | jq -r .secondaryConnectionString)
-
-echo "comparing secondary keys..."
-# 4. compare values for equality
-if [[ $REDIS_KEY_KV != $REDIS_KEY_ACTUAL ]];
-then
-  fail "Redis Secondary Key in key vault does not match actual value"
-fi
-
-if [[ $STORAGE_KEY_KV != $STORAGE_KEY_ACTUAL ]];
-then
-  fail "Storage Account Secondary Key in key vault does not match actual value"
-fi
-
-if [[ $STORAGE_CON_STRING_KV != $STORAGE_CON_STRING_ACTUAL ]];
-then
-  fail "Storage Account Secondary Connection String in key vault does not match actual value"
-fi
-
-if [[ $SB_CON_STRING_KV != $SB_CON_STRING_ACTUAL ]];
-then
-  fail "Service Bus Connection String for $SERVICE_BUS_USER in key vault does not match actual value"
-fi
-
-echo "Secondary keys test successful"
+echo "testing secondary key rotation..."
+renew_keys "secondary"
+get_key_vault_values
+get_service_secondary_key_values
+compare_key_vault_to_service
+echo "secondary keys test successful."
 
 echo "***************************************"
 echo "Integration test completed successfully"
