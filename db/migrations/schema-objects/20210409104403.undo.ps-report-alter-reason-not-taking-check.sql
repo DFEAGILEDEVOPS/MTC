62adf4f6676d938c7d17633a01132624552ae49f
Jon Shipley <js@ovix.co.uk>
2021-04-09 14:41:20 +0100

    [db] add data migration

--- db/migrations/schema-objects/20210409104403.undo.ps-report-alter-reason-not-taking-check.sql
+++ db/migrations/schema-objects/20210409104403.undo.ps-report-alter-reason-not-taking-check.sql
@@ -1,2 +1,22 @@
-ALTER TABLE mtc_results.psychometricReport ALTER COLUMN ReasonNotTakingCheck INT;
+ALTER TABLE mtc_results.psychometricReport ADD ReasonNotTakingCheckNumber INT;
+GO
 
+-- data migration
+UPDATE mtc_results.psychometricReport
+   SET psychometricReport.ReasonNotTakingCheckNumber = ( CASE
+       WHEN ReasonNotTakingCheck = 'Z' THEN 1
+       WHEN ReasonNotTakingCheck = 'A' THEN 2
+       WHEN ReasonNotTakingCheck = 'L' THEN 3
+       WHEN ReasonNotTakingCheck = 'U' THEN 4
+       WHEN ReasonNotTakingCheck = 'B' THEN 5
+       WHEN ReasonNotTakingCheck = 'J' THEN 6
+       ELSE NULL
+       END );
+GO
+
+-- drop old col
+ALTER TABLE mtc_results.psychometricReport DROP COLUMN ReasonNotTakingCheck;
+GO
+
+-- rename new col
+EXEC sp_rename 'mtc_results.psychometricReport.ReasonNotTakingCheckNumber', 'ReasonNotTakingCheck', 'COLUMN';
