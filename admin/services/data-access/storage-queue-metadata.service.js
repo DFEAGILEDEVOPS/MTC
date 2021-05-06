e858ef5868719d22965060ec889ff1ecc14de676
Guy Harwood <guy@milkchip.com>
2021-05-06 10:38:39 +0100

    rough sketch of storage queue service

--- admin/services/data-access/storage-queue-metadata.service.js
+++ admin/services/data-access/storage-queue-metadata.service.js
@@ -2,39 +2,28 @@
 
 const storage = require('azure-storage')
 const bluebird = require('bluebird')
-const path = require('path')
-const fs = require('fs')
-const globalDotEnvFile = path.join(__dirname, '..', '..', '.env')
 
-try {
-  if (fs.existsSync(globalDotEnvFile)) {
-    console.log('globalDotEnvFile found', globalDotEnvFile)
-    require('dotenv').config({ path: globalDotEnvFile })
-  } else {
-    console.log('No .env file found at project root')
+let queueService
+
+const service = {
+  getAllQueueMessageCounts: async function getAllQueueMessageCounts () {
+    if (!queueService) {
+      queueService = getPromisifiedService(storage.createQueueService())
+    }
+    const queues = await queueService.listQueuesSegmented(null)
+    const queueInfo = []
+    for (let index = 0; index < queues.length; index++) {
+      const queue = queues[index]
+      const info = await queueService.getQueueMetadata(queue.name)
+      queueInfo.push(info)
+    }
+    return queueInfo
   }
-} catch (error) {
-  console.error(error)
 }
 
-if (!process.env.AZURE_STORAGE_CONNECTION_STRING) {
-  process.exitCode = -1
-  console.error('env var $AZURE_STORAGE_CONNECTION_STRING is required')
-}
+module.exports = service
 
-const queueSvc = storage.createQueueService()
-queueSvc.getQueueMetadata('check-submitted-poison', (error, result) => {
-  if (error) {
-    console.error(error)
-    process.exit(-1)
-  }
-  console.dir(result)
-})
-
-/**
- * Promisify and cache the azureTableService library as it still lacks Promise support
- */
- function getPromisifiedService (storageService) {
+function getPromisifiedService (storageService) {
   bluebird.promisifyAll(storageService, {
     promisifier: (originalFunction) => function (...args) {
       return new Promise((resolve, reject) => {
@@ -43,7 +32,7 @@ queueSvc.getQueueMetadata('check-submitted-poison', (error, result) => {
             if (error) {
               return reject(error)
             }
-            resolve({ result, response })
+            resolve({ result, response})
           })
         } catch (error) {
           reject(error)
@@ -51,6 +40,5 @@ queueSvc.getQueueMetadata('check-submitted-poison', (error, result) => {
       })
     }
   })
-
   return storageService
 }
