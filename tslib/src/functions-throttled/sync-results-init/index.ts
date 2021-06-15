34901374fbc439cf6d9f8b939af11d4930f2c026
Jon Shipley <js@ovix.co.uk>
2021-06-15 15:00:15 +0100

    Improve messaging

--- tslib/src/functions-throttled/sync-results-init/index.ts
+++ tslib/src/functions-throttled/sync-results-init/index.ts
@@ -1,10 +1,10 @@
 import { AzureFunction, Context } from '@azure/functions'
 import { performance } from 'perf_hooks'
 import { SyncResultsInitService } from './sync-results-init.service'
-import * as util from 'util'
+// import * as util from 'util'
 
 const functionName = 'sync-results-init'
-const inspect = (myObject: any): void => console.log(util.inspect(myObject, false, null, true /* enable colors */))
+// const inspect = (myObject: any): void => console.log(util.inspect(myObject, false, null, true /* enable colors */))
 
 const timerTrigger: AzureFunction = async function (context: Context): Promise<void> {
   const start = performance.now()
@@ -12,7 +12,7 @@ const timerTrigger: AzureFunction = async function (context: Context): Promise<v
   try {
     const syncResultsInitService = new SyncResultsInitService(context.log)
     const messages = await syncResultsInitService.getCheckDataMessages()
-    console.log(inspect(messages))
+    // console.log(inspect(messages))
     context.bindings.checkCompletion = messages
     meta.processCount = messages.length
   } catch (error) {
@@ -24,7 +24,7 @@ const timerTrigger: AzureFunction = async function (context: Context): Promise<v
   const end = performance.now()
   const durationInMilliseconds = end - start
   const timeStamp = new Date().toISOString()
-  context.log(`${functionName}: ${timeStamp} processed ${meta.processCount} records, run took ${durationInMilliseconds} ms, total memory usage (heap used) ${Math.round(heapUsed * 100) / 100}MB`)
+  context.log(`${functionName}: ${timeStamp} processed ${meta.processCount} records, run took ${Math.round(durationInMilliseconds * 100) / 100} ms, total memory usage (heap used) ${Math.round(heapUsed * 100) / 100}MB`)
 }
 
 export default timerTrigger
