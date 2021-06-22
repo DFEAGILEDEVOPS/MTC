1637ce54a47898c0b9b71d0721b33b4fca450886
Jon Shipley <js@ovix.co.uk>
2021-06-22 19:59:28 +0100

    [tslib/sync-results-to-sql] reduce parameter usage
    SQL Server has a parameter limit of 2100 params per statement.   This commit splits the workload into more manageable chunks,
    and looks better too.

--- tslib/src/functions-throttled/sync-results-to-sql/sync-results.service.ts
+++ tslib/src/functions-throttled/sync-results-to-sql/sync-results.service.ts
@@ -1,7 +1,6 @@
 import { ICheckCompletionMessage } from './models'
 import { ISyncResultsDataService, SyncResultsDataService } from './sync-results.data.service'
 import { ConsoleLogger, ILogger } from '../../common/logger'
-import { ITransactionRequest } from '../../sql/sql.service'
 import { IRedisService, RedisService } from '../../caching/redis-service'
 import redisKeyService from '../../caching/redis-key.service'
 
@@ -37,19 +36,7 @@ export class SyncResultsService {
       // Prepare Device Info
       const deviceTran = await this.syncResultsDataService.prepareDeviceData(checkCompletionMessage.validatedCheck)
 
-      // This looks a little unusual as we could pass an array of ITransactions into insertToDatabase()
-      // which is just passing the args on to sqlService.modifyWithTransaction(). Take note that the SQL is produced in pieces but
-      // needs to be combined into a single SQL statement so we can re-use SQL variables like @checkResultId.  Therefore, the above work
-      // must take care not to re-use the same variable name.  E.g. it would be easy to clash on terms like @browserTimestamp1, so instead,
-      // rename them to @userInputBrowserTimestamp1 and use the destination table as a prefix.
-      const flattenedTransaction = flattenTransactions([
-        checkResTran,
-        answersAndInputsTran,
-        eventTran,
-        deviceTran
-      ])
-
-      await this.syncResultsDataService.insertToDatabase([flattenedTransaction], checkCompletionMessage?.markedCheck?.checkCode)
+      await this.syncResultsDataService.insertToDatabase([checkResTran, answersAndInputsTran, eventTran, deviceTran], checkCompletionMessage?.markedCheck?.checkCode)
       this.logger.info(`${name}: going to drop the redis cache for school ${checkCompletionMessage?.validatedCheck?.schoolUUID}`)
       await this.dropRedisSchoolResult(checkCompletionMessage?.validatedCheck?.schoolUUID)
       await this.syncResultsDataService.setCheckToResultsSyncComplete(checkCompletionMessage.markedCheck)
@@ -74,13 +61,4 @@ export class SyncResultsService {
     this.logger.verbose(`${name}: redis key to drop: [${key}]`)
     await this.redisService.drop([key])
   }
-}
-
-function flattenTransactions (transactions: ITransactionRequest[]): ITransactionRequest {
-  return transactions.reduce((accumulator, currentValue) => {
-    return {
-      sql: accumulator.sql.concat('\n', currentValue.sql),
-      params: accumulator.params.concat(currentValue.params)
-    }
-  })
 }
