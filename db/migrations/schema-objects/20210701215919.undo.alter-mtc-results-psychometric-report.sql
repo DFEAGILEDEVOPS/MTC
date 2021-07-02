7f765a76cb9967b6398fb03bfae42783a7aca513
Jon Shipley <js@ovix.co.uk>
2021-07-02 07:34:06 +0100

    [db, tslib] increase the size of received keystrokes fields

---
+++ db/migrations/schema-objects/20210701215919.undo.alter-mtc-results-psychometric-report.sql
@@ -0,0 +1,74 @@
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q1K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q2K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q3K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q4K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q5K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q6K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q7K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q8K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q9K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q10K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q11K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q12K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q13K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q14K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q15K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q16K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q17K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q18K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q19K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q20K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q21K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q22K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q23K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q24K NVARCHAR(256);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q25K NVARCHAR(256);
