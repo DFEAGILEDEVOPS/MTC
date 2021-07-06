03df488dce275c16d43d05321b55e1425c84d32b
Jon Shipley <jon-shipley@users.noreply.github.com>
2021-07-06 12:02:51 +0100

    Bug/47492 ps report keystrokes data length (#1945)
    * [chaos-proxy] move to util directory
    
    * [db, tslib] increase the size of received keystrokes fields
    
    Co-authored-by: Guy Harwood <GuyHarwood@users.noreply.github.com>
    Co-authored-by: Mohsen Qureshi <activemq@users.noreply.github.com>

---
+++ db/migrations/schema-objects/20210701215919.do.alter-mtc-results-psychometric-report.sql
@@ -0,0 +1,74 @@
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q1K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q2K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q3K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q4K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q5K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q6K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q7K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q8K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q9K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q10K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q11K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q12K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q13K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q14K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q15K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q16K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q17K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q18K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q19K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q20K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q21K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q22K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q23K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q24K NVARCHAR(MAX);
+
+ALTER TABLE
+    mtc_results.psychometricReport ALTER COLUMN Q25K NVARCHAR(MAX);
