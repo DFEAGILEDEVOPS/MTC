e18cb0c5a60e777ecd4543fcb79fcfdb941408c3
Guy Harwood <guy@milkchip.com>
2021-07-07 15:51:08 +0100

    implement school uuid batch functionality

--- tslib/src/functions/util-submit-check/index.ts
+++ tslib/src/functions/util-submit-check/index.ts
@@ -2,8 +2,10 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions'
 import config from '../../config'
 import { FakeSubmittedCheckMessageGeneratorService } from './fake-submitted-check-generator.service'
+import { SchoolChecksDataService } from './school-checks.data.service'
 
 const fakeSubmittedCheckBuilder = new FakeSubmittedCheckMessageGeneratorService()
+const liveSchoolChecksDataService = new SchoolChecksDataService()
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   if (!config.DevTestUtils.TestSupportApi) {
@@ -11,6 +13,20 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     context.done()
     return
   }
+
+  const schoolUuid = req.body?.schoolUuid
+  if (schoolUuid !== undefined) {
+    const liveCheckCodes = await liveSchoolChecksDataService.fetchBySchoolUuid(schoolUuid)
+    console.dir(liveCheckCodes)
+    const promises = liveCheckCodes.map(record => {
+      return fakeSubmittedCheckBuilder.createSubmittedCheckMessage(record.checkCode)
+    })
+    const messages = await Promise.all(promises)
+    context.bindings.submittedCheckQueue = messages
+    context.done()
+    return
+  }
+
   const checkCodes = req.body?.checkCodes
   if (checkCodes === undefined || !Array.isArray(checkCodes)) {
     context.res = {
--- tslib/src/functions/util-submit-check/school-checks.data.service.ts
+++ tslib/src/functions/util-submit-check/school-checks.data.service.ts
@@ -1,6 +1,14 @@
 import { TYPES } from 'mssql'
 import { ISqlParameter, ISqlService, SqlService } from '../../sql/sql.service'
 
+export interface ISchoolChecksDataService {
+  fetchBySchoolUuid (schoolUuid: string): Promise<string[]>
+}
+
+export interface ICheckCodeRecord {
+  checkCode: string
+}
+
 export class SchoolChecksDataService {
   private readonly sqlService: ISqlService
 
@@ -11,12 +19,14 @@ export class SchoolChecksDataService {
     this.sqlService = sqlService
   }
 
-  async fetchBySchoolUuid (schoolUuid: string): Promise<string[]> {
+  async fetchBySchoolUuid (schoolUuid: string): Promise<ICheckCodeRecord[]> {
     const sql = `
       SELECT checkCode FROM mtc_admin.[check] chk
       INNER JOIN mtc_admin.pupil p ON chk.pupil_id = p.id
       INNER JOIN mtc_admin.school s ON p.school_id = s.id
       WHERE s.urlSlug = @schoolUuid
+      AND chk.complete = 0 AND chk.processingFailed = 0
+      AND chk.isLiveCheck = 1 AND chk.received = 0
     `
     const param: ISqlParameter = {
       name: 'schoolUuid',
