2bd3226a575765c377b99835009de301486a0ffc
Guy Harwood <guy@milkchip.com>
2021-07-13 15:14:07 +0100

    organise packages by dependency. bump to ubuntu 20.04 LTS.  remove inbound 443 TCP

--- deploy/build-server/create-instance.sh
+++ deploy/build-server/create-instance.sh
@@ -6,6 +6,5 @@ RESOURCE_GROUP=$2
 AZP_TOKEN=$3
 AZP_URL=$4
 
-az container create -g $RESOURCE_GROUP --name $AGENT_NAME --image stamtc/devops-build-agent:latest --ports 443 \
-  --dns-name-label $AGENT_NAME --environment-variables AZP_TOKEN=$AZP_TOKEN AZP_AGENT_NAME=$AGENT_NAME \
-  AZP_POOL=MTC AZP_URL=$AZP_URL
+az container create -g $RESOURCE_GROUP --name $AGENT_NAME --image stamtc/devops-build-agent:latest \
+  --environment-variables AZP_TOKEN=$AZP_TOKEN AZP_AGENT_NAME=$AGENT_NAME AZP_POOL=MTC AZP_URL=$AZP_URL
