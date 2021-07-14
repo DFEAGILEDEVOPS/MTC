8da79b8bf0dab4ef1a6a7c81f6ecaf9dbe6e8f89
Guy Harwood <guy@milkchip.com>
2021-07-14 19:42:24 +0100

    remove docker, update comments

--- deploy/build-server/build-push-docker-hub.sh
+++ deploy/build-server/build-push-docker-hub.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
-DOCKER_BUILDKIT=1 docker build -t stamtc/devops-build-agent:latest .
-docker push stamtc/devops-build-agent:latest
+GIT_COMMIT_SHORT=$(git log -1 --format=%h)
+DOCKER_BUILDKIT=1 docker build -t stamtc/devops-build-agent:latest -t stamtc/devops-build-agent:$GIT_COMMIT_SHORT .
+docker push --all-tags stamtc/devops-build-agent
