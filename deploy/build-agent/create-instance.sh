44ce2234060d51d8bda0d46f1def6373f5142e1d
Guy Harwood <guy@milkchip.com>
2021-07-20 17:53:58 +0100

    more resources for build server

--- deploy/build-agent/create-instance.sh
+++ deploy/build-agent/create-instance.sh
@@ -7,4 +7,5 @@ AZP_TOKEN=$3
 AZP_URL=$4
 
 az container create -g $RESOURCE_GROUP --name $AGENT_NAME --image stamtc/devops-build-agent:latest \
-  --environment-variables AZP_TOKEN=$AZP_TOKEN AZP_AGENT_NAME=$AGENT_NAME AZP_POOL=MTC AZP_URL=$AZP_URL
+  --environment-variables AZP_TOKEN=$AZP_TOKEN AZP_AGENT_NAME=$AGENT_NAME AZP_POOL=MTC AZP_URL=$AZP_URL \
+  --cpu 4 --memory 8
