39f3701d6298101761299be972facfb8192a176c
Jon Shipley <js@ovix.co.uk>
2021-07-23 10:25:33 +0100

    [spa] fix broken test in config.service

--- pupil-spa/src/app/services/config/config.service.ts
+++ pupil-spa/src/app/services/config/config.service.ts
@@ -1,7 +1,7 @@
-import { first } from 'rxjs/operators';
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { default as connectivityErrorMessages} from '../connectivity-service/connectivity-error-messages';
+import { first } from 'rxjs/operators'
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { default as connectivityErrorMessages } from '../connectivity-service/connectivity-error-messages'
 
 /**
  * Declaration of config class
@@ -26,57 +26,57 @@ export interface IAppConfig {
 }
 
 export class AppConfig implements IAppConfig {
-  readonly applicationInsightsInstrumentationKey: string;
-  readonly authPingURL: string;
-  readonly authURL: string;
-  readonly checkStartAPIErrorDelay: number;
-  readonly checkStartAPIErrorMaxAttempts: number;
-  readonly checkSubmissionAPIErrorMaxAttempts: number;
-  readonly checkSubmissionApiErrorDelay: number;
-  readonly connectivityCheckEnabled: boolean;
-  readonly connectivityCheckViewMinDisplay: number;
-  readonly feedbackAPIErrorDelay: number;
-  readonly feedbackAPIErrorMaxAttempts: number;
-  readonly loginPendingViewMinDisplay: number;
-  readonly production: boolean;
-  readonly pupilPrefsAPIErrorDelay: number;
-  readonly pupilPrefsAPIErrorMaxAttempts: number;
-  readonly submissionPendingViewMinDisplay: number;
-  readonly submitsToCheckReceiver: boolean;
-  readonly supportNumber: string;
-  readonly testPupilConnectionDelay: number;
-  readonly testPupilConnectionMaxAttempts: number;
-  readonly testPupilConnectionQueueName: string;
-  readonly testPupilConnectionQueueToken: string;
-  readonly testPupilConnectionQueueUrl: string;
-  readonly websiteOffline: boolean;
+  readonly applicationInsightsInstrumentationKey: string
+  readonly authPingURL: string
+  readonly authURL: string
+  readonly checkStartAPIErrorDelay: number
+  readonly checkStartAPIErrorMaxAttempts: number
+  readonly checkSubmissionAPIErrorMaxAttempts: number
+  readonly checkSubmissionApiErrorDelay: number
+  readonly connectivityCheckEnabled: boolean
+  readonly connectivityCheckViewMinDisplay: number
+  readonly feedbackAPIErrorDelay: number
+  readonly feedbackAPIErrorMaxAttempts: number
+  readonly loginPendingViewMinDisplay: number
+  readonly production: boolean
+  readonly pupilPrefsAPIErrorDelay: number
+  readonly pupilPrefsAPIErrorMaxAttempts: number
+  readonly submissionPendingViewMinDisplay: number
+  readonly submitsToCheckReceiver: boolean
+  readonly supportNumber: string
+  readonly testPupilConnectionDelay: number
+  readonly testPupilConnectionMaxAttempts: number
+  readonly testPupilConnectionQueueName: string
+  readonly testPupilConnectionQueueToken: string
+  readonly testPupilConnectionQueueUrl: string
+  readonly websiteOffline: boolean
 }
 
 /**
  * Global variable containing actual config to use.
  */
-export let APP_CONFIG: AppConfig;
+export let APP_CONFIG: AppConfig
 
 /**
  * Exported function -- to work with AOT
  * @param {AppConfigService} configService
  * @returns {Function}
  */
-export function loadConfigService(configService: AppConfigService): Function {
+export function loadConfigService (configService: AppConfigService): Function {
   return () => {
-    return configService.load();
-  };
+    return configService.load()
+  }
 }
 
 /**
  * Mock helper
  * @returns {Function}
  */
-export function loadConfigMockService(): Function {
+export function loadConfigMockService (): Function {
   return () => {
-    APP_CONFIG = new AppConfig();
-    return Promise.resolve(true);
-  };
+    APP_CONFIG = new AppConfig()
+    return Promise.resolve(true)
+  }
 }
 
 /**
@@ -84,29 +84,25 @@ export function loadConfigMockService(): Function {
  */
 @Injectable()
 export class AppConfigService {
+  private configFileURL = '/public/config.json'
 
-  errorMessages = [];
+  errorMessages = []
 
-  constructor(private http: HttpClient) {
+  constructor (private http: HttpClient) {
   }
 
   /**
    * Parse the config file through http, throw a Server error if unavailable
    */
-  public load(): Promise<Boolean> {
-    return new Promise(async (resolve, reject) => {
-      await this.http.get('/public/config.json', { observe: 'response' })
-        .pipe(first())
-        .toPromise()
-        .then(data => {
-          const t = new AppConfig();
-          APP_CONFIG = Object.assign(t, data.body);
-          return resolve(true);
-        })
-        .catch(() => {
-          this.errorMessages.push(connectivityErrorMessages.pupilAuthError);
-          return reject(false);
-        });
-    });
+  public async load (): Promise<Boolean> {
+    try {
+      const data = await this.http.get(this.configFileURL).toPromise()
+      const t = new AppConfig()
+      APP_CONFIG = Object.assign(t, data)
+      return true
+    } catch (error) {
+      this.errorMessages.push(connectivityErrorMessages.settingsError)
+      return false
+    }
   }
 }
