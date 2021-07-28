6d210d1a051ee540c59e12fd7897f1dbc3191980
Guy Harwood <guy@milkchip.com>
2021-07-28 16:50:13 +0100

    failing on jq parsing for shell 🤔

--- deploy/web/create.sh
+++ deploy/web/create.sh
@@ -2,10 +2,27 @@
 set -e
 
 RESOURCE_GROUP=$1
-APP_NAME=$2
 APP_SVC_PLAN=$3
-IMAGE_NAME=$4
 IMAGE_VERSION=$5
+ENV=$6
 
-az webapp create --name $APP_NAME --resource-group $RESOURCE_GROUP \
-  --deployment-container-image-name "$IMAGE_NAME:$IMAGE_VERSION" --plan $APP_SVC_PLAN
+function createApp() {
+  namePostfix=$1
+  imageName=$2
+  # create web app
+  az webapp create --name "$ENV-$namePostfix" --resource-group $RESOURCE_GROUP \
+    --deployment-container-image-name "$imageName:$IMAGE_VERSION" --plan $APP_SVC_PLAN
+}
+
+IFS=$'\n' # Each iteration of the for loop should read until we find an end-of-line
+for row in $(cat defs.json | jq '. | map([.namePostfix, .imageName])' | jq -r @sh)
+do
+  # Run the row through the shell interpreter to remove enclosing double-quotes
+  stripped=$(echo $row | xargs echo)
+
+  # Call our function to process the row
+  # eval must be used to interpret the spaces in $stripped as separating arguments
+  eval processRow $stripped
+done
+unset IFS # Return IFS to its original value
+
