6f54fbdb871d8d923dede5f455eb8374a8c9ef73
Guy Harwood <guy@milkchip.com>
2021-07-28 12:55:23 +0100

    remove unused vars

--- deploy/web/create.sh
+++ deploy/web/create.sh
@@ -6,8 +6,6 @@ APP_NAME=$2
 APP_SVC_PLAN=$3
 IMAGE_NAME=$4
 IMAGE_VERSION=$5
-#DOCKER_USER=$6
-#DOCKER_PASSWORD=$7
 
 az webapp create --name $APP_NAME --resource-group $RESOURCE_GROUP \
   --deployment-container-image-name "$IMAGE_NAME:$IMAGE_VERSION" --plan $APP_SVC_PLAN
