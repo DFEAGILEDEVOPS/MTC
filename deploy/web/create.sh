4ecfb1386aedb529344a5d40e8ca50fe44d86105
Guy Harwood <guy@milkchip.com>
2021-07-29 15:37:40 +0100

    static creation script. app service plan support missing

--- deploy/web/create.sh
+++ deploy/web/create.sh
@@ -2,27 +2,26 @@
 set -e
 
 RESOURCE_GROUP=$1
-APP_SVC_PLAN=$3
-IMAGE_VERSION=$5
-ENV=$6
+IMAGE_VERSION=$2
+ENV=$3
+SINGLE_APP_SVC_PLAN=$4
 
 function createApp() {
-  namePostfix=$1
-  imageName=$2
+  role=$1
+  plan=$2
   # create web app
-  az webapp create --name "$ENV-$namePostfix" --resource-group $RESOURCE_GROUP \
-    --deployment-container-image-name "$imageName:$IMAGE_VERSION" --plan $APP_SVC_PLAN
+  az webapp create --name "$ENV$role-as-mtc-guytest" --resource-group $RESOURCE_GROUP \
+    --deployment-container-image-name "mtc-$role:$IMAGE_VERSION" --plan $plan
 }
 
-IFS=$'\n' # Each iteration of the for loop should read until we find an end-of-line
-for row in $(cat defs.json | jq '. | map([.namePostfix, .imageName])' | jq -r @sh)
-do
-  # Run the row through the shell interpreter to remove enclosing double-quotes
-  stripped=$(echo $row | xargs echo)
+if [ -z "$SINGLE_APP_SVC_PLAN" ]
+then
+  echo "TODO:create an app service plan for each app"
+fi
 
-  # Call our function to process the row
-  # eval must be used to interpret the spaces in $stripped as separating arguments
-  eval processRow $stripped
-done
-unset IFS # Return IFS to its original value
+createApp pupil $SINGLE_APP_SVC_PLAN
+createApp auth $SINGLE_APP_SVC_PLAN
+createApp admin $SINGLE_APP_SVC_PLAN
+createApp assets $SINGLE_APP_SVC_PLAN
+
 
