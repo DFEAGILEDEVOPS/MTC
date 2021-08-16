c5a1781ed782c52a03eae0fc7f1cb4696237be85
Guy Harwood <guy@milkchip.com>
2021-08-16 17:05:18 +0100

    multiple app service plans

--- deploy/web/create.sh
+++ deploy/web/create.sh
@@ -5,23 +5,43 @@ RESOURCE_GROUP=$1
 IMAGE_VERSION=$2
 ENV=$3
 SINGLE_APP_SVC_PLAN=$4
+NAME_SUFFIX="mtc-guytest"
 
 function createApp() {
   role=$1
   plan=$2
   # create web app
-  az webapp create --name "$ENV$role-as-mtc-guytest" --resource-group $RESOURCE_GROUP \
+  az webapp create --name "$ENV$role-as-$NAME_SUFFIX" --resource-group $RESOURCE_GROUP \
     --deployment-container-image-name "mtc-$role:$IMAGE_VERSION" --plan $plan
 }
 
+function createAppServicePlan() {
+  role=$1
+  az appservice plan create --name "$ENV$role-asp-$NAME_SUFFIX" --resource-group $RESOURCE_GROUP \
+    --is-linux --sku S1
+}
+
+$AUTH_APP_SVC_PLAN_NAME="$ENV$auth-asp-$NAME_SUFFIX"
+$PUPIL_APP_SVC_PLAN_NAME="$ENV$pupil-asp-$NAME_SUFFIX"
+$ADMIN_APP_SVC_PLAN_NAME="$ENV$admin-asp-$NAME_SUFFIX"
+$ASSETS_APP_SVC_PLAN_NAME="$ENV$assets-asp-$NAME_SUFFIX"
+
 if [ -z "$SINGLE_APP_SVC_PLAN" ]
 then
-  echo "TODO:create an app service plan for each app"
+  createAppServicePlan pupil
+  createAppServicePlan auth
+  createAppServicePlan admin
+  createAppServicePlan assets
+else
+  $PUPIL_APP_SVC_PLAN_NAME=$SINGLE_APP_SVC_PLAN
+  $AUTH_APP_SVC_PLAN_NAME=$SINGLE_APP_SVC_PLAN
+  $ADMIN_APP_SVC_PLAN_NAME=$SINGLE_APP_SVC_PLAN
+  $ASSETS_APP_SVC_PLAN_NAME=$SINGLE_APP_SVC_PLAN
 fi
 
-createApp pupil $SINGLE_APP_SVC_PLAN
-createApp auth $SINGLE_APP_SVC_PLAN
-createApp admin $SINGLE_APP_SVC_PLAN
-createApp assets $SINGLE_APP_SVC_PLAN
+createApp pupil $PUPIL_APP_SVC_PLAN_NAME
+createApp auth $AUTH_APP_SVC_PLAN_NAME
+createApp admin $ADMIN_APP_SVC_PLAN_NAME
+createApp assets $ASSETS_APP_SVC_PLAN_NAME
 
 
