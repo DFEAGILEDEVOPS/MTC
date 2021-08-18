9dd2a058bb4820acf2fb3072e1737834cf341677
Guy Harwood <guy@milkchip.com>
2021-08-18 11:26:40 +0100

    bulk apply security settings

--- deploy/web/create.sh
+++ deploy/web/create.sh
@@ -11,13 +11,13 @@ function createApp() {
   role=$1
   plan=$2
   # create web app
-  az webapp create --name "$ENV$role-as-$NAME_SUFFIX" --resource-group $RESOURCE_GROUP \
+  az webapp create -n "$ENV$role-as-$NAME_SUFFIX" -g $RESOURCE_GROUP \
     --deployment-container-image-name "mtc-$role:$IMAGE_VERSION" --plan $plan
 }
 
 function createAppServicePlan() {
   role=$1
-  az appservice plan create --name "$ENV$role-asp-$NAME_SUFFIX" --resource-group $RESOURCE_GROUP \
+  az appservice plan create -n "$ENV$role-asp-$NAME_SUFFIX" -g $RESOURCE_GROUP \
     --is-linux --sku S1
 }
 
@@ -44,24 +44,11 @@ createApp auth $AUTH_APP_SVC_PLAN_NAME
 createApp admin $ADMIN_APP_SVC_PLAN_NAME
 createApp assets $ASSETS_APP_SVC_PLAN_NAME
 
-# az webapp config set --always-on true --ftps-state Disabled --http20-enabled true --min-tls-version '1.2' --remote-debugging-enabled false --web-sockets-enabled false
-
-# disallow FTP
-az resource update --resource-group $RESOURCE_GROUP --name ftp --namespace Microsoft.Web \
-  --resource-type basicPublishingCredentialsPolicies --parent sites/$PUPIL_APP_SVC_PLAN_NAME --set properties.allow=false
-az resource update --resource-group $RESOURCE_GROUP --name ftp --namespace Microsoft.Web \
-  --resource-type basicPublishingCredentialsPolicies --parent sites/$AUTH_APP_SVC_PLAN_NAME --set properties.allow=false
-az resource update --resource-group $RESOURCE_GROUP --name ftp --namespace Microsoft.Web \
-  --resource-type basicPublishingCredentialsPolicies --parent sites/$ADMIN_APP_SVC_PLAN_NAME --set properties.allow=false
-az resource update --resource-group $RESOURCE_GROUP --name ftp --namespace Microsoft.Web \
-  --resource-type basicPublishingCredentialsPolicies --parent sites/$ASSETS_APP_SVC_PLAN_NAME --set properties.allow=false
-
-# disable basic auth access to webdeploy port and SCM site
-az resource update --resource-group $RESOURCE_GROUP --name scm --namespace Microsoft.Web \
-  --resource-type basicPublishingCredentialsPolicies --parent sites/$PUPIL_APP_SVC_PLAN_NAME --set properties.allow=false
-az resource update --resource-group $RESOURCE_GROUP --name scm --namespace Microsoft.Web \
-  --resource-type basicPublishingCredentialsPolicies --parent sites/$AUTH_APP_SVC_PLAN_NAME --set properties.allow=false
-az resource update --resource-group $RESOURCE_GROUP --name scm --namespace Microsoft.Web \
-  --resource-type basicPublishingCredentialsPolicies --parent sites/$ADMIN_APP_SVC_PLAN_NAME --set properties.allow=false
-az resource update --resource-group $RESOURCE_GROUP --name scm --namespace Microsoft.Web \
-  --resource-type basicPublishingCredentialsPolicies --parent sites/$ASSETS_APP_SVC_PLAN_NAME --set properties.allow=false
+az webapp config set -g $RESOURCE_GROUP -n $PUPIL_APP_SVC_PLAN_NAME --always-on true \
+  --ftps-state Disabled --http20-enabled true --min-tls-version '1.2' --remote-debugging-enabled false --web-sockets-enabled false
+az webapp config set -g $RESOURCE_GROUP -n $AUTH_APP_SVC_PLAN_NAME --always-on true \
+  --ftps-state Disabled --http20-enabled true --min-tls-version '1.2' --remote-debugging-enabled false --web-sockets-enabled false
+az webapp config set -g $RESOURCE_GROUP -n $ADMIN_APP_SVC_PLAN_NAME --always-on true \
+  --ftps-state Disabled --http20-enabled true --min-tls-version '1.2' --remote-debugging-enabled false --web-sockets-enabled false
+az webapp config set -g $RESOURCE_GROUP -n $ASSETS_APP_SVC_PLAN_NAME --always-on true \
+  --ftps-state Disabled --http20-enabled true --min-tls-version '1.2' --remote-debugging-enabled false --web-sockets-enabled false
