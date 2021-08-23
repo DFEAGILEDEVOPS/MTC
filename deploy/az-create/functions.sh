e29c3b349af3d3d616395a3d02a450cbd9ab2919
Guy Harwood <guy@milkchip.com>
2021-08-23 14:43:58 +0100

    functions deployment working

--- deploy/az-create/functions.sh
+++ deploy/az-create/functions.sh
@@ -2,20 +2,27 @@
 set -e
 
 RES_GRP=$1
-LOCATION=$2
-ENV=$3
-SUFFIX=$4
-STORAGE_ACCOUNT=$5
-APP_INSIGHTS_NAME=$6
+ENV=$2
+SUFFIX=$3
+STORAGE_ACCOUNT=$4
+APP_INSIGHTS_NAME=$5
+SKU=$6
 
-NAME="$ENV-func-1-$SUFFIX"
+# automatically creates a single app service plan to host both function apps.
+# This is solely because one function app runs during the day, and
+# the other hosts overnight jobs only.
+
+ASP_NAME="$ENV-func-asp-$SUFFIX"
+az appservice plan create -o none -n $ASP_NAME -g $RES_GRP --is-linux --sku $SKU
+
+NAME="$ENV-func-day-$SUFFIX"
 
 echo "creating function app $NAME"
 az functionapp create -o none -n $NAME -g $RES_GRP --functions-version 3 --runtime node --runtime-version 12 \
-  --storage-account $STORAGE_ACCOUNT --app-insights $APP_INSIGHTS_NAME
+  --plan $ASP_NAME --storage-account $STORAGE_ACCOUNT --app-insights $APP_INSIGHTS_NAME
 
-NAME="$ENV-func-2-$SUFFIX"
+NAME="$ENV-func-night-$SUFFIX"
 
 echo "creating function app $NAME"
 az functionapp create -o none -n $NAME -g $RES_GRP --functions-version 3 --runtime node --runtime-version 12 \
-  --storage-account $STORAGE_ACCOUNT --app-insights $APP_INSIGHTS_NAME
+  --plan $ASP_NAME --storage-account $STORAGE_ACCOUNT --app-insights $APP_INSIGHTS_NAME
