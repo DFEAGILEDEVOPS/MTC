e2e405080f41326cc7cc7d6188649ccfdedfdd06
Jon Shipley <js@ovix.co.uk>
2021-09-01 12:50:17 +0100

    [admin] refactor pupilIdentificationFlagService
    * add new method `sortAndAddIdentificationFlagService`
    
    The Pupil Identification Flag Service has a requirement that the incoming pupil arrary is sorted or it will not work.  The new method
    does the sort and adds the disambiguation data, so is now preferred.

--- admin/spec/back-end/service/pupil-identification-flag.service.spec.js
+++ admin/spec/back-end/service/pupil-identification-flag.service.spec.js
@@ -1,33 +1,69 @@
-/* global describe, it, expect */
+/* global describe, test, expect */
 const pupilMock = require('../mocks/pupil')
 const pupilIdentificationFlagService = require('../../../services/pupil-identification-flag.service')
+const moment = require('moment')
 
 describe('addIdentificationFlags', () => {
-  it('returns showDoB property as true for pupils with same fullname', () => {
+  test('returns showDoB property as true for pupils with same fullname', () => {
     const pupil1 = Object.assign({}, pupilMock)
     const pupil2 = Object.assign({}, pupilMock)
     const pupils = pupilIdentificationFlagService.addIdentificationFlags([pupil1, pupil2])
     expect(pupils[0].showDoB && pupils[1].showDoB).toBeTruthy()
   })
-  it('returns showDoB property as false for pupils with different fullname', () => {
+
+  test('returns showDoB property as false for pupils with different fullname', () => {
     const pupil1 = Object.assign({}, pupilMock)
     const pupil2 = Object.assign({}, pupilMock)
     pupil1.foreName = 'test'
     const pupils = pupilIdentificationFlagService.addIdentificationFlags([pupil1, pupil2])
     expect(pupils[0].showDoB && pupils[1].showDoB).toBeFalsy()
   })
-  it('returns showMiddleNames property as true for pupils with same fullname and DoB', () => {
+
+  test('returns showMiddleNames property as true for pupils with same fullname and DoB', () => {
     const pupil1 = Object.assign({}, pupilMock)
     const pupil2 = Object.assign({}, pupilMock)
     const pupils = pupilIdentificationFlagService.addIdentificationFlags([pupil1, pupil2])
     expect(pupils[0].showMiddleNames && pupils[1].showMiddleNames).toBeTruthy()
     expect(pupils[0].middleNames).toBe('Middle')
   })
-  it('returns showMiddleNames property as false for pupils with different fullname and Dob', () => {
+
+  test('returns showMiddleNames property as false for pupils with different fullname and Dob', () => {
     const pupil1 = Object.assign({}, pupilMock)
     const pupil2 = Object.assign({}, pupilMock)
     pupil1.foreName = 'test'
     const pupils = pupilIdentificationFlagService.addIdentificationFlags([pupil1, pupil2])
     expect(pupils[0].showDoB && pupils[1].showMiddleNames).toBeFalsy()
+  })
+
+  test('it returns a new object rather than modifying the argument', () => {
+    const pupil1 = Object.assign({}, pupilMock)
+    const pupil2 = Object.assign({}, pupilMock)
+    const arg = [pupil1, pupil2]
+    const pupils = pupilIdentificationFlagService.addIdentificationFlags(arg)
+    expect(pupils).not.toBe(arg)
+  })
+
+  test('returns the pupil full name with middle names if pupil differentiation requires a middleName sort', async () => {
+    const mockPupilData = [
+      { lastName: 'Smith', foreName: 'Jack', dateOfBirth: moment('2012-01-01'), middleNames: 'C' },
+      { lastName: 'Smith', foreName: 'Jack', dateOfBirth: moment('2012-01-01'), middleNames: 'B' },
+      { lastName: 'Smith', foreName: 'Jack', dateOfBirth: moment('2012-01-01'), middleNames: 'A' }
+    ]
+    const pupils = pupilIdentificationFlagService.sortAndAddIdentificationFlags(mockPupilData)
+    expect(pupils[0].fullName).toBe('Smith, Jack A')
+    expect(pupils[1].fullName).toBe('Smith, Jack B')
+    expect(pupils[2].fullName).toBe('Smith, Jack C')
+  })
+
+  test('sorts the pupils alphabetically - if the lastname, forename and dob are the same it sorts by middlenames', async () => {
+    const mockPupilData = [
+      { lastName: 'Smith', foreName: 'Jack', dateOfBirth: moment('2013-01-01'), middleNames: 'Zebra' },
+      { lastName: 'Smith', foreName: 'Jack', dateOfBirth: moment('2013-01-01'), middleNames: 'Xani' },
+      { lastName: 'Smith', foreName: 'Jack', dateOfBirth: moment('2013-01-01'), middleNames: 'Bea' }
+    ]
+    const pupils = pupilIdentificationFlagService.sortAndAddIdentificationFlags(mockPupilData)
+    expect(pupils[0].middleNames).toEqual('Bea')
+    expect(pupils[1].middleNames).toEqual('Xani')
+    expect(pupils[2].middleNames).toEqual('Zebra')
   })
 })
