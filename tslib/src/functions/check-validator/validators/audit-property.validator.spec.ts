c1427517507dacc2f49db02b08ef297389733ad9
Guy Harwood <guy@milkchip.com>
2021-09-16 15:01:23 +0100

    audit property validator

---
+++ tslib/src/functions/check-validator/validators/audit-property.validator.spec.ts
@@ -0,0 +1,27 @@
+import { SubmittedCheck, getSubmittedCheck } from '../../../schemas/check-schemas/submitted-check'
+import { AuditPropertyValidator } from './audit-property.validator'
+import { ICheckValidationError } from './validator-types'
+
+let sut: AuditPropertyValidator
+
+describe('audit-property.validator', () => {
+  beforeEach(() => {
+    sut = new AuditPropertyValidator()
+  })
+
+  test('if audit property undefined, validation fails', () => {
+    const check: SubmittedCheck = getSubmittedCheck()
+    delete check.audit
+    const error = sut.validate(check)
+    expect(error).toBeDefined()
+    expect((error as ICheckValidationError).message).toBe('audit property missing')
+  })
+
+  test('if audit property not an array, validation fails', () => {
+    const check: SubmittedCheck = getSubmittedCheck()
+    check.audit = {}
+    const error = sut.validate(check)
+    expect(error).toBeDefined()
+    expect((error as ICheckValidationError).message).toBe('audit property is not an array')
+  })
+})
---
+++ tslib/src/functions/check-validator/validators/audit-property.validator.ts
@@ -0,0 +1,17 @@
+import { SubmittedCheck } from '../../../schemas/check-schemas/submitted-check'
+import { CheckValidationResult, ISubmittedCheckValidator } from './validator-types'
+
+export class AuditPropertyValidator implements ISubmittedCheckValidator {
+  validate (check: SubmittedCheck): CheckValidationResult {
+    if (check.audit === undefined) {
+      return {
+        message: 'audit property missing'
+      }
+    }
+    if (!Array.isArray(check.audit)) {
+      return {
+        message: 'audit property is not an array'
+      }
+    }
+  }
+}
