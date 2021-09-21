a54703fed2d7c371691de804275ca4d6561bdf1a
Guy Harwood <guy@milkchip.com>
2021-09-21 16:10:40 +0100

    rename table migrations

---
+++ db/migrations/schema-objects/20210920173332.do.rename-pupilfontsizes-table.sql
@@ -0,0 +1,3 @@
+exec sp_rename 'mtc_admin.pupilFontSizes', 'fontSizeLookUp'
+exec sp_rename 'mtc_admin.pupilColourContrasts', 'colourContrastsLookUp'
+exec sp_rename 'mtc_admin.pupilRestartReason', 'restartReasonLookUp'
---
+++ db/migrations/schema-objects/20210920173332.undo.rename-pupilfontsizes-table.sql
@@ -0,0 +1,3 @@
+exec sp_rename 'mtc_admin.fontSizeLookUp', 'pupilFontSizes'
+exec sp_rename 'mtc_admin.colourContrastsLookUp', 'pupilColourContrasts'
+exec sp_rename 'mtc_admin.restartReasonLookUp', 'pupilRestartReason'
