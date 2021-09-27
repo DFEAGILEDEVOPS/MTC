0bde5a3ab7a97737a17975ca5cdc505cf7bf4b69
Guy Harwood <guy@milkchip.com>
2021-09-27 10:39:36 +0100

    begin sas api call

---
+++ _spikes-poc/azure-storage-account-packages/sas.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const path = require('path')
+const fs = require('fs')
+const globalDotEnvFile = path.join(__dirname, '..', '..', '.env')
+
+try {
+  if (fs.existsSync(globalDotEnvFile)) {
+    // console.log('globalDotEnvFile found', globalDotEnvFile)
+    require('dotenv').config({ path: globalDotEnvFile })
+  } else {
+    console.log('No .env file found at project root')
+  }
+} catch (error) {
+  console.error(error)
+}
+
+const { QueueServiceClient } = require("@azure/storage-queue")
+
+const queueServiceClient = QueueServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING)
+
+queueServiceClient.generateAccountSasUrl()
