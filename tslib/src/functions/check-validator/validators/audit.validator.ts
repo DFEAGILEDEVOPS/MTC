885fd942717787b4f63fce7efa0092df5a011f6e
Guy Harwood <GuyHarwood@users.noreply.github.com>
2021-10-06 10:44:04 +0100

    feature/48505 submitted check validation (#1996)
    * add answer item type validation
    
    * remove service version naming
    
    * live check validator
    
    * revised check validator
    
    * remove unused type
    
    * answer count validator and WIP
    
    * wrap up answer validation for now.  tbc after discussion
    
    * progress breaking up validators
    
    * rename type file
    
    * lint fixes
    
    * lint rule override
    
    * answers validator
    
    * audit property validator
    
    * inputs validator
    
    * questions validator and check code validator
    
    * check config validator and lint fixes
    
    * pupil and school validators
    
    * add validator provider
    
    * validator to be refactored to be testable
    
    * refactor to using anonymous object when performing validation
    
    * fix check validator tests with valid mock check
    
    * address test.todo
    
    * fixes as per PR comments
    
    * fixes
    
    * PR fixes
    
    * remove commit from wrong branch
    
    * make valid answers strings
    
    * force strings for answers
    
    * stringify before compression
    
    * fix array check and question count check
    
    * tests for check validator
    
    Co-authored-by: Mohsen Qureshi <activemq@users.noreply.github.com>
    Co-authored-by: Mohsen Qureshi <mjq@Mohsens-Mac-mini.local>

---
+++ tslib/src/functions/check-validator/validators/audit.validator.ts
@@ -0,0 +1,16 @@
+import { CheckValidationResult, ISubmittedCheckValidator } from './validator-types'
+
+export class AuditValidator implements ISubmittedCheckValidator {
+  validate (check: any): CheckValidationResult {
+    if (check.audit === undefined) {
+      return {
+        message: 'audit property missing'
+      }
+    }
+    if (!Array.isArray(check.audit)) {
+      return {
+        message: 'audit property is not an array'
+      }
+    }
+  }
+}
---
+++ tslib/src/functions/check-validator/validators/pupil.validator.ts
@@ -0,0 +1,17 @@
+import { isObj } from 'ramda-adjunct'
+import { CheckValidationResult, ISubmittedCheckValidator } from './validator-types'
+
+export class PupilValidator implements ISubmittedCheckValidator {
+  validate (check: any): CheckValidationResult {
+    if (check.pupil === undefined) {
+      return {
+        message: 'pupil property missing'
+      }
+    }
+    if (!isObj(check.pupil)) {
+      return {
+        message: 'pupil property is not an object'
+      }
+    }
+  }
+}
