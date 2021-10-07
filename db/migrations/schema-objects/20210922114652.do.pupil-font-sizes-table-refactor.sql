5b8364da60d66d052d1ce9d31b22318d9195f446
Guy Harwood <GuyHarwood@users.noreply.github.com>
2021-10-07 12:15:23 +0100

    bug/42526 lookup table name issues (#2003)
    * rename table migrations
    
    * rename script
    
    * table refactor scripts
    
    * refactor code for table changes
    
    * rename LookUp to Lookup
    
    Co-authored-by: Mohsen Qureshi <activemq@users.noreply.github.com>

---
+++ db/migrations/schema-objects/20210922114652.do.pupil-font-sizes-table-refactor.sql
@@ -0,0 +1,2 @@
+exec sp_rename 'mtc_admin.pupilFontSizes', 'fontSizeLookup'
+exec sp_rename 'mtc_admin.pupilAccessArrangements.pupilFontSizes_Id', 'fontSizeLookup_Id', 'COLUMN'
---
+++ db/migrations/schema-objects/20210922114703.undo.pupil-restart-reason-table-refactor.sql
@@ -0,0 +1,2 @@
+exec sp_rename 'mtc_admin.restartReasonLookup', 'pupilRestartReason'
+exec sp_rename 'mtc_admin.pupilRestart.restartReasonLookup_Id', 'pupilRestartReason_Id', 'COLUMN'
