eb3b3ef4777e2f9ecc5e86239c94b7e0862c9a78
Guy Harwood <guy@milkchip.com>
2021-10-08 11:53:13 +0100

    remove sinon part 1

--- admin/spec/back-end/lib/validator/pin-validator.spec.js
+++ admin/spec/back-end/lib/validator/pin-validator.spec.js
@@ -1,19 +1,16 @@
 'use strict'
-/* global describe, it, expect, beforeEach, afterEach */
-const sinon = require('sinon')
+/* global describe, it, expect, beforeEach, spyOn */
+const dateService = require('../../../../services/date.service')
 const moment = require('moment')
 const pinValidator = require('../../../../lib/validator/pin-validator')
 
 describe('pin-validator', () => {
   let pinExpiredAt
   describe('isActivePin', () => {
-    let sandbox
-    beforeEach(() => { sandbox = sinon.createSandbox() })
-    afterEach(() => sandbox.restore())
     describe('returns true if the pin is active', () => {
       beforeEach(() => {
         pinExpiredAt = moment().startOf('day')
-        sandbox.useFakeTimers(moment().startOf('day').subtract(1, 'years').valueOf())
+        spyOn(dateService, 'utcNowAsMoment').and.returnValue(moment().startOf('day').subtract(1, 'years').valueOf())
       })
       it('if pinExpiredAt date field is later than current time', async () => {
         const result = await pinValidator.isActivePin('abc1f', pinExpiredAt)
@@ -23,7 +20,7 @@ describe('pin-validator', () => {
     describe('returns false if the pin is inactive', () => {
       beforeEach(() => {
         pinExpiredAt = moment().startOf('day')
-        sandbox.useFakeTimers(moment().startOf('day').add(1, 'years').valueOf())
+        spyOn(dateService, 'utcNowAsMoment').and.returnValue(moment().startOf('day').add(1, 'years').valueOf())
       })
       it('if pinExpiredAt date field is earlier than current time', async () => {
         const result = await pinValidator.isActivePin('abc1f', pinExpiredAt)
