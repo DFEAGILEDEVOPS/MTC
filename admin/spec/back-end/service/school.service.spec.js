2307cfd3403b31e29ba5587fc08d55078aa3fbde
Jon Shipley <js@ovix.co.uk>
2021-10-15 13:37:56 +0100

    convert to jest

--- admin/spec/back-end/service/school.service.spec.js
+++ admin/spec/back-end/service/school.service.spec.js
@@ -1,26 +1,24 @@
 'use strict'
 const uuid = require('uuid')
 
-/* global describe, it, expect, spyOn fail test jest */
+/* global describe, expect test jest afterEach */
 const sut = require('../../../services/school.service')
 const schoolDataService = require('../../../services/data-access/school.data.service')
 const schoolValidator = require('../../../lib/validator/school-validator')
 const ValidationError = require('../../../lib/validation-error')
 
 describe('school.service', () => {
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
   describe('findSchoolByDfeNumber', () => {
-    it('should throw an error if school not found', async () => {
+    test('should throw an error if school not found', async () => {
       const dfeNumber = '1234567'
-      spyOn(schoolDataService, 'sqlFindOneByDfeNumber').and.returnValue(undefined)
-      try {
-        await sut.findSchoolNameByDfeNumber(dfeNumber)
-        fail('error should have been thrown')
-      } catch (error) {
-        expect(error).toBeDefined()
-        expect(error.message).toEqual(`School [${dfeNumber}] not found`)
-      }
+      jest.spyOn(schoolDataService, 'sqlFindOneByDfeNumber').mockResolvedValue(undefined)
+      await expect(sut.findSchoolNameByDfeNumber(dfeNumber)).rejects.toThrow(`School [${dfeNumber}] not found`)
     })
-    it('should return school name if found', async () => {
+
+    test('should return school name if found', async () => {
       const dfeNumber = '1234567'
       const schoolName = 'school1'
       const school = {
@@ -28,115 +26,76 @@ describe('school.service', () => {
         dfeNumber: dfeNumber,
         name: schoolName
       }
-      spyOn(schoolDataService, 'sqlFindOneByDfeNumber').and.returnValue(Promise.resolve(school))
+      jest.spyOn(schoolDataService, 'sqlFindOneByDfeNumber').mockResolvedValue(school)
       const actual = await sut.findSchoolNameByDfeNumber(dfeNumber)
       expect(actual).toEqual(school.name)
     })
   })
 
   describe('findOneById', () => {
-    it('should  throw an error if id not provided', async () => {
-      try {
-        await sut.findOneById()
-        fail('error should have been thrown')
-      } catch (error) {
-        expect(error).toBeDefined()
-        expect(error.message).toEqual('id is required')
-      }
+    test('should  throw an error if id not provided', async () => {
+      await expect(sut.findOneById()).rejects.toThrow('id is required')
     })
-    it('returns school if found', async () => {
+
+    test('returns school if found', async () => {
       const schoolName = 'school1'
       const school = {
         id: 123,
         name: schoolName
       }
-      spyOn(schoolDataService, 'sqlFindOneById').and.returnValue(Promise.resolve(school))
+      jest.spyOn(schoolDataService, 'sqlFindOneById').mockResolvedValue(school)
       const actual = await sut.findOneById(123)
       expect(actual).toBe(school)
     })
   })
 
   describe('searchForSchool', () => {
-    it('it throws if the query is empty string', async () => {
-      try {
-        await sut.searchForSchool('')
-        fail()
-      } catch (error) {
-        expect(error.message).toBe('query is required')
-      }
+    test('it throws if the query is empty string', async () => {
+      await expect(sut.searchForSchool('')).rejects.toThrow('query is required')
     })
 
-    it('it throws if the query is undefined', async () => {
-      try {
-        await sut.searchForSchool(undefined)
-        fail()
-      } catch (error) {
-        expect(error.message).toBe('query is required')
-      }
+    test('it throws if the query is undefined', async () => {
+      await expect(sut.searchForSchool(undefined)).rejects.toThrow('query is required')
     })
 
-    it('it throws if the query is null', async () => {
-      try {
-        await sut.searchForSchool(null)
-        fail()
-      } catch (error) {
-        expect(error.message).toBe('query is required')
-      }
+    test('it throws if the query is null', async () => {
+      await expect(sut.searchForSchool(null)).rejects.toThrow('query is required')
     })
 
-    it('it throws if the query is not a number', async () => {
-      try {
-        await sut.searchForSchool('abc')
-        fail()
-      } catch (error) {
-        expect(error.message).toBe('Invalid type: number required')
-      }
+    test('it throws if the query is not a number', async () => {
+      await expect(sut.searchForSchool('abc')).rejects.toThrow('Invalid type: number required')
     })
 
-    it('returns undefined if the school is not found', async () => {
-      spyOn(schoolDataService, 'sqlSearch').and.returnValue(Promise.resolve(undefined))
+    test('returns undefined if the school is not found', async () => {
+      jest.spyOn(schoolDataService, 'sqlSearch').mockResolvedValue(undefined)
       const res = await sut.searchForSchool(1234)
       expect(res).toBeUndefined()
     })
 
-    it('returns the school object if the school is found', async () => {
-      spyOn(schoolDataService, 'sqlSearch').and.returnValue(Promise.resolve({ id: 1, name: 'test school' }))
+    test('returns the school object if the school is found', async () => {
+      jest.spyOn(schoolDataService, 'sqlSearch').mockResolvedValue({ id: 1, name: 'test school' })
       const res = await sut.searchForSchool(1234)
       expect(res).toStrictEqual({ id: 1, name: 'test school' })
     })
   })
 
   describe('findOneBySlug', () => {
-    it('throws an error if the slug is an empty string', async () => {
-      try {
-        await sut.findOneBySlug('')
-        fail('expected to throw')
-      } catch (error) {
-        expect(error.message).toBe('Missing slug')
-      }
+    test('throws an error if the slug is an empty string', async () => {
+      await expect(sut.findOneBySlug('')).rejects.toThrow('Missing slug')
     })
 
-    it('throws an error if the slug is an empty string', async () => {
-      try {
-        await sut.findOneBySlug(undefined)
-        fail('expected to throw')
-      } catch (error) {
-        expect(error.message).toBe('Missing slug')
-      }
-    })
-
-    it('it returns undefined if the school is not found', async () => {
-      spyOn(schoolDataService, 'sqlFindOneBySlug').and.returnValue(Promise.resolve(undefined))
+    test('it returns undefined if the school is not found', async () => {
+      jest.spyOn(schoolDataService, 'sqlFindOneBySlug').mockResolvedValue(undefined)
       const res = await sut.findOneBySlug('abc')
       expect(res).toBeUndefined()
     })
 
-    it('it returns the school when found', async () => {
+    test('it returns the school when found', async () => {
       const school = {
         id: 123,
         name: 'test school 42'
       }
-      spyOn(schoolDataService, 'sqlFindOneBySlug').and.returnValue(Promise.resolve(school))
+      jest.spyOn(schoolDataService, 'sqlFindOneBySlug').mockResolvedValue(school)
       const res = await sut.findOneBySlug('abc')
       expect(res).toStrictEqual(school)
     })
@@ -171,12 +130,9 @@ describe('school.service', () => {
     test('it throws a ValidationError if the validation fails', async () => {
       const validationError = new ValidationError('dfeNumber', 'Test error')
       jest.spyOn(schoolValidator, 'validate').mockResolvedValue(validationError)
-      try {
-        await sut.updateSchool(uuid.NIL, {})
-        fail('expected to throw')
-      } catch (error) {
-        expect(error.name).toBe('ValidationError')
-      }
+      await expect(sut.updateSchool(uuid.NIL, {}))
+        .rejects
+        .toHaveProperty('name', 'ValidationError')
     })
   })
 })
