38eec3395473d2c77023e0f1d3a11baee094c3b3
Guy Harwood <guy@milkchip.com>
2021-10-19 17:57:16 +0100

    error in script

--- _spikes-poc/anonymise-sql-data/pupil-anon.sql
+++ _spikes-poc/anonymise-sql-data/pupil-anon.sql
@@ -1,2 +1,32 @@
--- get pupil ids into a cursor
--- loop each and use exec sp_executesql to update the record with random values targeting the id
+DECLARE @id int
+DECLARE @foreName nvarchar(128)
+DECLARE @middleNames nvarchar(128)
+DECLARE @lastName nvarchar(128)
+DECLARE @dateOfBirth datetimeoffset(3) = '1970-01-01'
+DECLARE @upn char(13)
+DECLARE @namePartLengths int = 128
+-- cursor
+DECLARE pupilCursor CURSOR LOCAL FAST_FORWARD FOR 
+SELECT id FROM [mtc_admin].pupil  
+OPEN pupilCursor 
+FETCH NEXT  FROM pupilCursor INTO @id 
+WHILE @@FETCH_STATUS = 0
+-- set vars
+    SELECT @foreName = LEFT(CAST(NEWID() AS VARCHAR(255)), FLOOR((@namePartLengths / 2.0)) + 1)
+    SELECT @middleNames = LEFT(CAST(NEWID() AS VARCHAR(255)), FLOOR((@namePartLengths / 2.0)) + 1)
+    SELECT @lastName = LEFT(CAST(NEWID() AS VARCHAR(255)), FLOOR((@namePartLengths / 2.0)) + 1)
+    SELECT @upn = LEFT(CAST(NEWID() AS VARCHAR(255)), 13)
+BEGIN 
+    BEGIN TRY
+        PRINT 'updating pupil with id:' + @id
+        UPDATE mtc_admin.pupil SET foreName=@foreName, middleNames=@middleNames, lastName=@lastName, dateOfBirth=@dateOfBirth, upn=@upn WHERE id=@id
+    END TRY 
+   BEGIN CATCH 
+      PRINT ERROR_MESSAGE()
+      -- INSERT INTO @LoggingTable 
+      -- SELECT GETDATE(), ERROR_MESSAGE() 
+END CATCH
+FETCH NEXT FROM pupilCursor INTO @id 
+END
+CLOSE pupilCursor
+DEALLOCATE pupilCursor
--- _spikes-poc/anonymise-sql-data/random-data.sql
+++ _spikes-poc/anonymise-sql-data/random-data.sql
@@ -1,3 +1,19 @@
-select CAST(ABS(CHECKSUM(NEWID())) % 2 AS char(1)) [Random Bit]
+/* select CAST(ABS(CHECKSUM(NEWID())) % 2 AS char(1)) [Random Bit]
 select CHECKSUM(NEWID()) [Random Number]
-SELECT '''' + LEFT(CAST(NEWID() AS VARCHAR(255)), FLOOR((50 / 2.0)) + 1) + '''' [Random UUID section]
+SELECT '''' + LEFT(CAST(NEWID() AS VARCHAR(255)), 13) + '''' [Random UUID section] */
+
+DECLARE @foreName nvarchar(128)
+DECLARE @middleNames nvarchar(128)
+DECLARE @lastName nvarchar(128)
+DECLARE @dateOfBirth datetimeoffset(3) = '1970-01-01'
+DECLARE @upn char(13)
+DECLARE @namePartLengths int = 128
+
+
+
+SELECT @foreName = LEFT(CAST(NEWID() AS VARCHAR(255)), FLOOR((@namePartLengths / 2.0)) + 1)
+SELECT @middleNames = LEFT(CAST(NEWID() AS VARCHAR(255)), FLOOR((@namePartLengths / 2.0)) + 1)
+SELECT @lastName = LEFT(CAST(NEWID() AS VARCHAR(255)), FLOOR((@namePartLengths / 2.0)) + 1)
+SELECT @upn = LEFT(CAST(NEWID() AS VARCHAR(255)), 13)
+
+select @foreName as [foreName], @middleNames as [middleNames], @lastName as [lastName], @upn as [upn]
