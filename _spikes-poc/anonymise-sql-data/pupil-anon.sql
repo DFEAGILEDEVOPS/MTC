a56e68d2cd47dc0a0e400c0d8ea2c8b96330911b
Guy Harwood <guy@milkchip.com>
2021-10-20 11:22:17 +0100

    working pupil script

--- _spikes-poc/anonymise-sql-data/pupil-anon.sql
+++ _spikes-poc/anonymise-sql-data/pupil-anon.sql
@@ -2,23 +2,21 @@ DECLARE @id int
 DECLARE @foreName nvarchar(128)
 DECLARE @middleNames nvarchar(128)
 DECLARE @lastName nvarchar(128)
-DECLARE @dateOfBirth datetimeoffset(3) = '1970-01-01'
+DECLARE @dateOfBirth datetimeoffset(3) = DATEADD(YEAR, -10, GETDATE())
 DECLARE @upn char(13)
-DECLARE @namePartLengths int = 128
 -- cursor
-DECLARE pupilCursor CURSOR LOCAL FAST_FORWARD FOR 
+DECLARE pupilCursor CURSOR FAST_FORWARD FOR 
 SELECT id FROM [mtc_admin].pupil  
 OPEN pupilCursor 
 FETCH NEXT  FROM pupilCursor INTO @id 
 WHILE @@FETCH_STATUS = 0
--- set vars
-    SELECT @foreName = LEFT(CAST(NEWID() AS VARCHAR(255)), FLOOR((@namePartLengths / 2.0)) + 1)
-    SELECT @middleNames = LEFT(CAST(NEWID() AS VARCHAR(255)), FLOOR((@namePartLengths / 2.0)) + 1)
-    SELECT @lastName = LEFT(CAST(NEWID() AS VARCHAR(255)), FLOOR((@namePartLengths / 2.0)) + 1)
-    SELECT @upn = LEFT(CAST(NEWID() AS VARCHAR(255)), 13)
 BEGIN 
     BEGIN TRY
-        PRINT 'updating pupil with id:' + @id
+    -- set vars
+        SELECT @foreName = CAST(NEWID() AS VARCHAR(255))
+        SELECT @middleNames = CAST(NEWID() AS VARCHAR(255))
+        SELECT @lastName = CAST(NEWID() AS VARCHAR(255))
+        SELECT @upn = LEFT(REPLACE(CAST(NEWID() AS VARCHAR(255)), '-', ''), 13)
         UPDATE mtc_admin.pupil SET foreName=@foreName, middleNames=@middleNames, lastName=@lastName, dateOfBirth=@dateOfBirth, upn=@upn WHERE id=@id
     END TRY 
    BEGIN CATCH 
