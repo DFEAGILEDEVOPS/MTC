03b6d2cee91cb10d2e5d8ca09f4c33ecf04dd9c0
Guy Harwood <guy@milkchip.com>
2021-10-21 15:38:38 +0100

    simpler approach with invalid urn, estab and lea

--- _spikes-poc/anonymise-sql-data/school-anon-complex-WIP.sql
+++ _spikes-poc/anonymise-sql-data/school-anon-complex-WIP.sql
@@ -7,16 +7,21 @@ DECLARE @leaSchoolIndex tinyint = 0
 DECLARE @leaCode int = 100
 DECLARE @urn int = 100000
 
-SET NOCOUNT ON
-
 -- to avoid leaCode and estabCode clashes, reset all to values that are typically out of bounds
 PRINT 'resetting schools...'
 update mtc_admin.school set leaCode = 99999, estabCode = id, dfeNumber=id, urn=id
 PRINT 'all schools reset'
-DECLARE schoolCursor CURSOR FAST_FORWARD FOR 
-SELECT id FROM [mtc_admin].school  
+
+-- NOTE: This is a work in progress and currently endlessly loops.
+-- it provides more granular control over lea and estab codes, but 
+-- would need converting to an updatable cursor before it can work correctly.
+
+SET NOCOUNT ON
+
+DECLARE schoolCursor CURSOR FORWARD_ONLY FOR 
+SELECT id, [name], [leaCode], [estabCode], [urn], [dfeNumber] FROM [mtc_admin].school  
 OPEN schoolCursor 
-FETCH NEXT  FROM schoolCursor INTO @id 
+FETCH NEXT  FROM schoolCursor INTO @id --TODO add variables for other columns
 WHILE @@FETCH_STATUS = 0
 BEGIN 
     BEGIN TRY
@@ -25,6 +30,7 @@ BEGIN
             SELECT @estabCode = @estabCode + 1
         ELSE
             BEGIN
+                PRINT 'moving to next lea.  current leaCode:' + CAST(@leaCode AS NVARCHAR) + ' current estab:' + CAST(@estabCode AS NVARCHAR) + ' leaSchoolIndex:' + CAST(@leaSchoolIndex AS NVARCHAR)
                 -- Next Lea, reset vars...
                 SELECT @estabCode = 1000
                 SELECT @leaCode = @leaCode + 1
@@ -32,8 +38,8 @@ BEGIN
             END
         DECLARE @dfeNumber int = CAST(CAST(@leaCode AS NVARCHAR) + CAST(@estabCode AS NVARCHAR) AS int)
         SELECT @schoolName = CAST(NEWID() AS VARCHAR(255))
-        PRINT 'leaCode:' + CAST(@leaCode AS NVARCHAR) + ' estabCode:' + CAST(@estabCode AS NVARCHAR) + ' urn:' + CAST(@urn AS NVARCHAR) + ' dfeNumber:' + CAST(@dfeNumber AS NVARCHAR)
-        UPDATE mtc_admin.school SET [name]=@schoolName, [leaCode]=@leaCode, [estabCode]=@estabCode, [urn]=@urn, [dfeNumber]=@dfeNumber WHERE id=@id
+        --PRINT 'leaCode:' + CAST(@leaCode AS NVARCHAR) + ' estabCode:' + CAST(@estabCode AS NVARCHAR) + ' urn:' + CAST(@urn AS NVARCHAR) + ' dfeNumber:' + CAST(@dfeNumber AS NVARCHAR)
+        -- TODO update cursor values
         SELECT @urn = @urn + 1
         SELECT @leaSchoolIndex = @leaSchoolIndex + 1
     END TRY 
