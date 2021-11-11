acb4787569fd1500fce80bcf94257d30d8a7c82c
Guy Harwood <guy@milkchip.com>
2021-11-11 16:22:43 +0000

    fix db check

--- admin/db-check.js
+++ admin/db-check.js
@@ -6,7 +6,9 @@ const config = require('./config')
 async function checkDatabaseIsUp () {
   try {
     // make sure that any items are correctly URL encoded in the connection string
-    await sql.connect(`mssql://${config.Sql.Application.Username}:${config.Sql.Application.Password}@${config.Sql.Server}/${config.Sql.Database}`)
+    // Server=localhost,1433;Database=database;User Id=username;Password=password;Encrypt=true'
+    await sql.connect(`Server=${config.Sql.Server},${config.Sql.Port};Database=${config.Sql.Database};User Id=${config.Sql.Application.Username};Password=${config.Sql.Application.Password};Encrypt=false`)
+    // await sql.connect(`mssql://${config.Sql.Application.Username}:${config.Sql.Application.Password}@${config.Sql.Server}/${config.Sql.Database}`)
     const result = await sql.query('SELECT * FROM mtc_admin.[role] WHERE [title] = \'TECH-SUPPORT\'')
     await sql.close()
     if (result && result.recordset && result.recordset[0] && result.recordset[0].id) {
