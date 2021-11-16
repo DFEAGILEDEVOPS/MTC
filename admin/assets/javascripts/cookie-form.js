bbf4b57388f9621f380ff1fd27b362ff36865ade
Jon Shipley <jon-shipley@users.noreply.github.com>
2021-11-16 11:59:02 +0000

    Fix: admin session timeout modal (#2021)
    * working version
    
    * add code coverage
    
    * add alternate path for front end code coverage
    
    * Split vendor files out from app.js
    
    - this is so we can get meaningful code-coverage on the app files
    
    * Fix linting issue
    
    * Make property and function consistent
    
    * Update frontend coverage to use sourcemaps
    
    * add tests
    
    * Fix lint issues
    
    * Small refactor
    
    * Bump assets version
    
    * Lint fix
    
    * Fix frontend tests
    
    Looks like a bad merge
    
    * changes to check for env var before running the tests
    
    Co-authored-by: Mohsen Qureshi <mjq@Mohsens-Mac-mini.local>

--- admin/assets/javascripts/cookie-form.js
+++ admin/assets/javascripts/cookie-form.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
   const cookieSettingsElement = document.querySelectorAll('form[data-module=cookie-settings]')
-  // eslint-disable-next-line no-undef
-  const cookieSettingsModule = new GOVUK.Modules.CookieSettings()
-  cookieSettingsModule.start(cookieSettingsElement)
+  if (cookieSettingsElement && cookieSettingsElement.length > 0) {
+    // eslint-disable-next-line no-undef
+    const cookieSettingsModule = new GOVUK.Modules.CookieSettings()
+    cookieSettingsModule.start(cookieSettingsElement)
+  }
 })
