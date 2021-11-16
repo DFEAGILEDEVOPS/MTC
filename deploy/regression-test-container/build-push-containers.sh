11967c71d1d0bd16b3b9d1ccf0ee148dfd42817c
Guy Harwood <guy@milkchip.com>
2021-11-16 12:02:44 +0000

    working build of apps and images

--- deploy/regression-test-container/build-push-containers.sh
+++ deploy/regression-test-container/build-push-containers.sh
@@ -6,6 +6,9 @@ set -eu
 # builds containers for each app in the service
 # starts them up via compose (will need to use branch name as a tag?)
 # executes ruby tests against them
+# the machine on which this executes will need the following env vars...
+# AZURE_SERVICE_BUS_CONNECTION_STRING
+# AZURE_STORAGE_CONNECTION_STRING
 ############################################################################
 
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
@@ -27,7 +30,6 @@ scriptbanner ()
   echo "+--------------------------------------------------------------------+"
 }
 
-# install and build admin app, ready for docker
 install_admin () {
   scriptbanner "Installing admin"
   cd "${mtcRoot}/admin"
@@ -37,16 +39,89 @@ install_admin () {
   yarn build
 }
 
+install_pupil_api () {
+  scriptbanner "Installing pupil API"
+  cd ${mtcRoot}/pupil-api
+  nvm use
+  yarn clean
+  yarn install
+  yarn build
+}
+
+install_pupil_spa () {
+  scriptbanner "Installing pupil SPA"
+  cd ${mtcRoot}/pupil-spa
+  nvm use
+  yarn clean
+  yarn install
+  yarn build
+}
+
+install_throttled_functions () {
+  scriptbanner "Installing throttled functions"
+  cd ${mtcRoot}/func-throttled
+  nvm use
+  yarn clean
+  yarn install
+  yarn build
+}
+
+install_consumption_functions () {
+  scriptbanner "Installing consumption functions"
+  cd ${mtcRoot}/func-consumption
+  nvm use
+  yarn clean
+  yarn install
+  yarn build
+}
+
 build_admin_image () {
-  adminImageName="$dockerTagPrefix/admin:$dockerLabel"
-  scriptbanner "building docker image:$adminImageName"
+  imageName="$dockerTagPrefix/admin:$dockerLabel"
+  scriptbanner "building docker image:$imageName"
   cd "${mtcRoot}/admin"
-  docker build -t $adminImageName .
+  docker build -t $imageName .
+}
+
+build_api_image () {
+  imageName="$dockerTagPrefix/api:$dockerLabel"
+  scriptbanner "building docker image:$imageName"
+  cd "${mtcRoot}/pupil-api"
+  docker build -t $imageName .
+}
+
+build_spa_image () {
+  imageName="$dockerTagPrefix/spa:$dockerLabel"
+  scriptbanner "building docker image:$imageName"
+  cd "${mtcRoot}/pupil-spa"
+  docker build -t $imageName .
+}
+
+build_throttled_functions_image () {
+  imageName="$dockerTagPrefix/throttled-functions:$dockerLabel"
+  scriptbanner "building docker image:$imageName"
+  cd "${mtcRoot}/func-throttled"
+  docker build -t $imageName -f test.Dockerfile .
+}
+
+build_consumption_functions_image () {
+  imageName="$dockerTagPrefix/consumption-functions:$dockerLabel"
+  scriptbanner "building docker image:$imageName"
+  cd "${mtcRoot}/func-consumption"
+  docker build -t $imageName -f test.Dockerfile .
 }
 
 start=`date +%s`
 install_admin
 build_admin_image
+install_pupil_api
+build_api_image
+install_pupil_spa
+build_spa_image
+install_consumption_functions
+build_consumption_functions_image
+install_throttled_functions
+build_throttled_functions_image
+# TODO install_db
 end=`date +%s`
 runtime=$((end-start))
 scriptbanner "Packages installed and built in: ${runtime} seconds"
