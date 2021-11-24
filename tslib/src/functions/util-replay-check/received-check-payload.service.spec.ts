3a7e3d4bb7c55e468ba8af721edae5bf4e3f3536
Guy Harwood <guy@milkchip.com>
2021-11-24 18:05:46 +0000

    finish implementation

--- tslib/src/functions/util-replay-check/received-check-payload.service.spec.ts
+++ tslib/src/functions/util-replay-check/received-check-payload.service.spec.ts
@@ -1,18 +1,23 @@
-import { CompressionService } from '../../common/compression-service'
+import { ICompressionService } from '../../common/compression-service'
 import { IReceivedCheckPayloadDataService } from './received-check-payload.data.service'
 import { ReceivedCheckPayloadService } from './received-check-payload.service'
 
 let sut: ReceivedCheckPayloadService
-let compressionService: CompressionService
+let compressionService: ICompressionService
 let dataServiceMock: IReceivedCheckPayloadDataService
 
 const DataServiceMock = jest.fn<IReceivedCheckPayloadDataService, any>(() => ({
   fetchCompressedArchive: jest.fn()
 }))
 
+const CompressionServiceMock = jest.fn<ICompressionService, any>(() => ({
+  compress: jest.fn(),
+  decompress: jest.fn()
+}))
+
 describe('received-check-payload.service', () => {
   beforeEach(() => {
-    compressionService = new CompressionService()
+    compressionService = new CompressionServiceMock()
     dataServiceMock = new DataServiceMock()
     sut = new ReceivedCheckPayloadService(compressionService, dataServiceMock)
   })
@@ -34,5 +39,13 @@ describe('received-check-payload.service', () => {
     jest.spyOn(dataServiceMock, 'fetchCompressedArchive').mockResolvedValue(undefined)
     const checkCode: string = '721fdee4-26ef-4111-8bbf-b2c5a7d602e3'
     await expect(sut.fetch(checkCode)).resolves.toBe('')
+  })
+
+  test('returns decompressed archive when found', async () => {
+    jest.spyOn(dataServiceMock, 'fetchCompressedArchive').mockResolvedValue('foo')
+    const decompressedArchive = '263ff574-c83c-4ab7-91c6-db397c63bf82'
+    jest.spyOn(compressionService, 'decompress').mockReturnValue(decompressedArchive)
+    const checkCode: string = '721fdee4-26ef-4111-8bbf-b2c5a7d602e3'
+    await expect(sut.fetch(checkCode)).resolves.toStrictEqual(decompressedArchive)
   })
 })
