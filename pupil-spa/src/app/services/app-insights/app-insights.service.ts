963184ea3a2e551cd239458643768b800919e04f
Jon Shipley <js@ovix.co.uk>
2021-11-26 19:17:25 +0000

    Add AI custom tracking to SPA

--- pupil-spa/src/app/services/app-insights/app-insights.service.ts
+++ pupil-spa/src/app/services/app-insights/app-insights.service.ts
@@ -4,18 +4,25 @@ import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { APP_CONFIG } from '../config/config.service';
 import { Meta } from '@angular/platform-browser';
+import { StorageService } from '../storage/storage.service'
+
+export interface MtcCustomProperties {
+  mtcCheckCode: string
+  mtcSchoolUUID: string
+  mtcSpaBuildNumber: string
+  mtcCommitId: string
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApplicationInsightsService {
   private appInsights: ApplicationInsights;
-  private propsToTrack = {
-    mtcCheckCode: 'TEST-1234',
-    mtcSchoolUUID: 'SCHOOL-TEST-1234'
-  }
+  private buildNumber;
+  private commitId;
 
-  constructor(private router: Router, private meta: Meta) {
+
+  constructor(private router: Router, private meta: Meta, private storageService: StorageService) {
     if (!APP_CONFIG.applicationInsightsInstrumentationKey) {
       return;
     }
@@ -43,7 +50,8 @@ export class ApplicationInsightsService {
 
   trackPageView(name?: string, uri?: string) {
     if (!this.appInsights) { return; }
-    this.appInsights.trackPageView({ name, uri, properties: this.propsToTrack});
+    const customTracking = this.getCustomProperties()
+    this.appInsights.trackPageView({ name, uri, properties: customTracking });
   }
 
   trackException(error: Error) {
@@ -51,14 +59,38 @@ export class ApplicationInsightsService {
     const exception: IExceptionTelemetry = {
       exception: error
     };
-    this.appInsights.trackException(exception);
+    const customTracking = this.getCustomProperties()
+    this.appInsights.trackException(exception, customTracking);
+  }
+
+  getCustomProperties(): MtcCustomProperties {
+    return {
+      mtcCheckCode: this.storageService.getPupil()?.checkCode,
+      mtcSchoolUUID: this.storageService.getSchool()?.uuid,
+      mtcSpaBuildNumber: this.getBuildNumber(),
+      mtcCommitId: this.getCommitId()
+    }
+  }
+
+  private getBuildNumber(): string {
+    if (this.buildNumber === undefined) {
+      this.buildNumber = this.meta.getTag('name="build:number"').content;
+    }
+    return this.buildNumber
+  }
+
+  private getCommitId(): string {
+    if (this.commitId === undefined) {
+      this.commitId = this.meta.getTag('name="build:commitId"').content;
+    }
+    return this.commitId
   }
 
   private loadCustomTelemetryProperties() {
     const buildNumberTelemetryInitializer = (envelope: ITelemetryItem) => {
       const baseData = envelope.data.baseData;
       baseData.properties = baseData.properties || {};
-      baseData.properties['buildNumber'] = this.meta.getTag('name="build:number"').content;
+      baseData.properties['buildNumber'] = this.getBuildNumber()
     };
     this.appInsights.addTelemetryInitializer(buildNumberTelemetryInitializer);
   }
