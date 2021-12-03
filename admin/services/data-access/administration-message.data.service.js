276c8a420cd857d785847c4e60e5af8288562ba6
Jon Shipley <js@ovix.co.uk>
2021-12-03 15:57:31 +0000

    Add service message edit feature

--- admin/services/data-access/administration-message.data.service.js
+++ admin/services/data-access/administration-message.data.service.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const R = require('ramda')
+const { TYPES } = require('./sql.service')
 
 const sqlService = require('./sql.service')
 const administrationMessageDataService = {}
@@ -10,8 +11,18 @@ const administrationMessageDataService = {}
  * @param data
  * @return
  */
-administrationMessageDataService.sqlCreate = async (data) => {
-  await sqlService.create('serviceMessage', data)
+administrationMessageDataService.sqlCreateOrUpdate = async (data) => {
+  if (data.id !== undefined) {
+    const sql = 'UPDATE [mtc_admin].[serviceMessage] SET title = @title, message = @message WHERE id = @id'
+    const params = [
+      { name: 'id', value: data.id, type: TYPES.Int },
+      { name: 'title', value: data.title, type: TYPES.NVarChar(TYPES.MAX) },
+      { name: 'message', value: data.message, type: TYPES.NVarChar(TYPES.MAX) }
+    ]
+    await sqlService.modify(sql, params)
+  } else {
+    await sqlService.create('serviceMessage', data)
+  }
 }
 
 /**
@@ -27,7 +38,7 @@ administrationMessageDataService.sqlDeleteServiceMessage = async () => {
 
 /**
  * Fetch active service message
- * @return {Promise<{ title: string, message: string }>}
+ * @return {Promise<{ title: string, message: string, id: number }>}
  */
 administrationMessageDataService.sqlFindActiveServiceMessage = async () => {
   const sql = `
