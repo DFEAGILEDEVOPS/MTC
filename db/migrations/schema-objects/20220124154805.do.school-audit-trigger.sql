2ad221830387132f4d23897ff11a658f3b16b1e2
Guy Harwood <guy@milkchip.com>
2022-02-01 14:24:51 +0000

    permissions for school import. fix mismatch on updatedOn field

--- db/migrations/schema-objects/20220124154805.do.school-audit-trigger.sql
+++ db/migrations/schema-objects/20220124154805.do.school-audit-trigger.sql
@@ -4,7 +4,8 @@ AS
 BEGIN
     DECLARE @auditOperationTypeLookupId int
     DECLARE @newDataJson NVARCHAR(MAX)
-    -- TODO infer audit type, populate var
+    DECLARE @updatedTimestamp DATETIMEOFFSET(3) = GETUTCDATE()
+    -- infer audit type, populate var
     IF EXISTS (SELECT 1 FROM inserted)
         BEGIN
             IF EXISTS (SELECT 1 FROM deleted)
@@ -14,7 +15,7 @@ BEGIN
                     DECLARE @schoolId INT
                     SELECT @schoolId = id FROM inserted
                     -- incorporate updatedAt trigger logic, to avoid duplicate audit entries
-                    UPDATE [mtc_admin].[school] SET updatedAt = GETUTCDATE()
+                    UPDATE [mtc_admin].[school] SET updatedAt = @updatedTimestamp
                     WHERE id = @schoolId
                 END
             ELSE
@@ -28,8 +29,11 @@ BEGIN
             -- I am a delete (not currently supported)
             SELECT @auditOperationTypeLookupId=3
         END
-    -- TODO convert incoming row into JSON, populate var
-    SELECT @newDataJson = (SELECT * FROM inserted FOR JSON AUTO)
+    -- grab incoming data as JSON
+    SELECT @newDataJson = (SELECT * FROM inserted FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER)
+    PRINT @newDataJson
+    SET @newDataJson = JSON_MODIFY(@newDataJson, '$.updatedAt', CAST(@updatedTimestamp AS NVARCHAR))
+    PRINT @newDataJson
     INSERT INTO [mtc_admin].[schoolAudit]
     (auditOperationTypeLookup_id, newData, school_id, operationBy_userId, sqlUserIdentifier)
     SELECT @auditOperationTypeLookupId, @newDataJson, i.id, i.lastModifiedBy_userId, SUSER_SNAME()
