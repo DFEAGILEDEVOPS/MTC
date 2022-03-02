c05e2b3d5cbdaf04a569204cedf3ad20cd9127eb
Guy Harwood <guy@milkchip.com>
2022-03-02 20:30:18 +0000

    add unhandled error handler

--- tslib/src/functions-throttled/census-import/v1.ts
+++ tslib/src/functions-throttled/census-import/v1.ts
@@ -45,43 +45,50 @@ export class CensusImportV1 {
   }
 
   private async handleCensusImport (blob: any, blobUri: string): Promise<number> {
-    // extract the blob name (also the job slug) from the blob Uri
-    const blobName = R.compose((arr: any[]) => arr[arr.length - 1], (r: string) => r.split('/'))(blobUri)
-    // Update job status to Processing
-    this.logger.info(`jobUrlSlug:${blobName}`)
-    const jobSlug = blobName.toString()
-    await this.jobDataService.setJobStarted(jobSlug)
-    const jobId = await this.jobDataService.getJobId(jobSlug)
-    if (jobId === undefined) throw new Error('jobId is undefined')
-    const blobContent = csvString.parse(blob.toString())
-    const censusTable = `[mtc_census_import].[census_import_${moment.utc().format('YYYYMMDDHHMMSS')}_${uuidv4()}]`
-    this.logger.info(`censusTable:${censusTable}`)
-    this.logger.info('inserting data to staging table...')
-    const stagingInsertCount = await this.censusImportDataService.loadStagingTable(censusTable, blobContent)
-    this.logger.info(`stagingInsertCount:${stagingInsertCount}`)
+    let pupilMeta: any
+    let blobName = ''
+    let jobOutput = ''
+    let errorOutput = ''
+    try {
+      // extract the blob name (also the job slug) from the blob Uri
+      blobName = R.compose((arr: any[]) => arr[arr.length - 1], (r: string) => r.split('/'))(blobUri)
+      // Update job status to Processing
+      this.logger.info(`jobUrlSlug:${blobName}`)
+      const jobSlug = blobName.toString()
+      await this.jobDataService.setJobStarted(jobSlug)
+      const jobId = await this.jobDataService.getJobId(jobSlug)
+      if (jobId === undefined) throw new Error('jobId is undefined')
+      const blobContent = csvString.parse(blob.toString())
+      const censusTable = `[mtc_census_import].[census_import_${moment.utc().format('YYYYMMDDHHMMSS')}_${uuidv4()}]`
+      this.logger.info(`censusTable:${censusTable}`)
+      this.logger.info('inserting data to staging table...')
+      const stagingInsertCount = await this.censusImportDataService.loadStagingTable(censusTable, blobContent)
+      this.logger.info(`stagingInsertCount:${stagingInsertCount}`)
 
-    const pupilMeta = await this.censusImportDataService.loadPupilsFromStaging(censusTable, jobId)
-    await this.censusImportDataService.deleteStagingTable(censusTable)
-    await this.blobService.deleteBlob(blobName, 'census')
+      pupilMeta = await this.censusImportDataService.loadPupilsFromStaging(censusTable, jobId)
+      await this.censusImportDataService.deleteStagingTable(censusTable)
+      await this.blobService.deleteBlob(blobName, 'census')
 
-    const jobOutput = `${stagingInsertCount} rows in uploaded file, ${pupilMeta.insertCount} inserted to pupil table, ${pupilMeta.errorCount} rows containing errors`
-    if (stagingInsertCount !== pupilMeta.insertCount) {
-      if (pupilMeta.errorText === undefined) {
-        pupilMeta.errorText = ''
+      jobOutput = `${stagingInsertCount} rows in uploaded file, ${pupilMeta.insertCount} inserted to pupil table, ${pupilMeta.errorCount} rows containing errors`
+      if (stagingInsertCount !== pupilMeta.insertCount) {
+        if (pupilMeta.errorText === undefined) {
+          pupilMeta.errorText = ''
+        }
+        errorOutput = `${pupilMeta.errorText}\nTip: Ensure all schools in the uploaded file have a matching entry in the MTC database.`
+        // update job to failed
+        await this.jobDataService.setJobComplete(blobName, JobStatusCode.CompletedWithErrors, jobOutput, errorOutput)
+        this.logger.warn(`census-import: ${stagingInsertCount} rows staged, but only ${pupilMeta.insertCount} rows inserted to pupil table`)
+      } else {
+        jobOutput = `${stagingInsertCount} rows staged and ${pupilMeta.insertCount} rows inserted to pupil table`
+        // update job to complete
+        await this.jobDataService.setJobComplete(blobName, JobStatusCode.CompletedSuccessfully, jobOutput)
       }
-      const errorOutput = `${pupilMeta.errorText}\nTip: Ensure all schools in the uploaded file have a matching entry in the MTC database.`
-      // update job to failed
-      await this.jobDataService.setJobComplete(blobName, JobStatusCode.CompletedWithErrors, jobOutput, errorOutput)
-      this.logger.warn(`census-import: ${stagingInsertCount} rows staged, but only ${pupilMeta.insertCount} rows inserted to pupil table`)
-    } else {
-      const jobOutput = `${stagingInsertCount} rows staged and ${pupilMeta.insertCount} rows inserted to pupil table`
-      // update job to complete
-      await this.jobDataService.setJobComplete(blobName, JobStatusCode.CompletedSuccessfully, jobOutput)
+      // Finally, delete the pupil-register cache for all schools
+      await this.deletePupilRegisterRedisCache()
+    } catch (error) {
+      errorOutput += `\n${error}`
+      await this.jobDataService.setJobComplete(blobName, JobStatusCode.Failed, jobOutput, errorOutput)
     }
-
-    // Finally, delete the pupil-register cache for all schools
-    await this.deletePupilRegisterRedisCache()
-
     return pupilMeta.insertCount
   }
 
