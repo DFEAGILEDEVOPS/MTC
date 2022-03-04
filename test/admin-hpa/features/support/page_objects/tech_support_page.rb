12c105af665bb71f6ed55be8cfc759878174fc08
Mohsen Qureshi <m-qureshi@hotmail.com>
2022-03-04 11:39:17 +0000

    Update tech_support_page.rb

--- test/admin-hpa/features/support/page_objects/tech_support_page.rb
+++ test/admin-hpa/features/support/page_objects/tech_support_page.rb
@@ -2,7 +2,7 @@ class TechSupportPage < SitePrism::Page
   set_url '/tech-support/home'
 
   element :heading, '.govuk-heading-xl', text: 'Tech Support'
-  element :check_view, '.govuk-link', text: 'Check View'
+  element :check_view, '.govuk-link', text: 'Check view'
 
 
 end
