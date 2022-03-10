3ba50e176e4331ef9b3d8d4e06283f0c249f99a4
Jon Shipley <js@ovix.co.uk>
2022-03-10 15:08:10 +0000

    Convert to jest

--- admin/spec/back-end/service/data-access/check.data.service.spec.js
+++ admin/spec/back-end/service/data-access/check.data.service.spec.js
@@ -1,17 +1,21 @@
 'use strict'
-/* global describe, beforeEach, it, expect, spyOn */
+/* global describe, beforeEach, test, expect, jest, afterEach */
 
 const checkMock = require('../../mocks/check')
 const sqlService = require('../../../../services/data-access/sql.service')
 const sut = require('../../../../services/data-access/check.data.service')
 
 describe('check.data.service', () => {
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
   describe('#sqlFindOneByCheckCode', () => {
     beforeEach(() => {
-      spyOn(sqlService, 'readonlyQuery').and.returnValue(checkMock)
+      jest.spyOn(sqlService, 'readonlyQuery').mockResolvedValue(checkMock)
     })
 
-    it('makes the expected calls', async () => {
+    test('makes the expected calls', async () => {
       await sut.sqlFindOneByCheckCode('mock-check-code')
       expect(sqlService.readonlyQuery).toHaveBeenCalled()
     })
@@ -19,10 +23,10 @@ describe('check.data.service', () => {
 
   describe('#sqlFindLatestCheck', () => {
     beforeEach(() => {
-      spyOn(sqlService, 'readonlyQuery').and.returnValue(checkMock)
+      jest.spyOn(sqlService, 'readonlyQuery').mockResolvedValue(checkMock)
     })
 
-    it('should makes the expected call', async () => {
+    test('should makes the expected call', async () => {
       await sut.sqlFindLatestCheck(1234)
       expect(sqlService.readonlyQuery).toHaveBeenCalled()
     })
@@ -30,10 +34,10 @@ describe('check.data.service', () => {
 
   describe('#sqlFindFullyPopulated', () => {
     beforeEach(() => {
-      spyOn(sqlService, 'readonlyQuery').and.returnValue(checkMock)
+      jest.spyOn(sqlService, 'readonlyQuery').mockResolvedValue(checkMock)
     })
 
-    it('makes the expected calls', async () => {
+    test('makes the expected calls', async () => {
       await sut.sqlFindFullyPopulated({ testCriteria: 'someValue' })
       expect(sqlService.readonlyQuery).toHaveBeenCalled()
     })
@@ -41,12 +45,12 @@ describe('check.data.service', () => {
 
   describe('#sqlFindNumberOfChecksStartedByPupil', () => {
     beforeEach(() => {
-      spyOn(sqlService, 'readonlyQuery').and.returnValue([{
+      jest.spyOn(sqlService, 'readonlyQuery').mockResolvedValue([{
         cnt: 5
       }])
     })
 
-    it('makes the expected calls', async () => {
+    test('makes the expected calls', async () => {
       await sut.sqlFindNumberOfChecksStartedByPupil(1234)
       expect(sqlService.readonlyQuery).toHaveBeenCalled()
     })
--- admin/spec/back-end/service/data-access/setting.data.service.spec.js
+++ admin/spec/back-end/service/data-access/setting.data.service.spec.js
@@ -1,6 +1,6 @@
 'use strict'
 
-/* global describe beforeEach it expect spyOn */
+/* global describe beforeEach test expect jest */
 
 const sqlService = require('../../../../services/data-access/sql.service')
 const sut = require('../../../../services/data-access/setting.data.service')
@@ -8,10 +8,14 @@ const sut = require('../../../../services/data-access/setting.data.service')
 describe('pupil.data.service', () => {
   describe('#sqlUpdate', () => {
     beforeEach(() => {
-      spyOn(sqlService, 'modify').and.returnValue({ rowsModified: 1 })
+      jest.spyOn(sqlService, 'modify').mockResolvedValue({ rowsModified: 1 })
     })
 
-    it('calls the model', async () => {
+    afterEach(() => {
+      jest.restoreAllMocks()
+    })
+
+    test('calls the model', async () => {
       const loadingTimeLimit = 10
       const questionTimeLimit = 20
       const checkTimeLimit = 30
