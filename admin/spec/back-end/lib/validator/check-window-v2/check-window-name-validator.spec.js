39d27524f91211b47c29715f49b4f5cd3f1ea504
Jon Shipley <js@ovix.co.uk>
2022-03-10 17:25:38 +0000

    Convert to jest

--- admin/spec/back-end/lib/validator/check-window-v2/check-window-name-validator.spec.js
+++ admin/spec/back-end/lib/validator/check-window-v2/check-window-name-validator.spec.js
@@ -1,33 +1,33 @@
 'use strict'
 
-/* global describe, it, spyOn expect */
+/* global describe, test, jest expect */
 const checkWindowErrorMessages = require('../../../../../lib/errors/check-window-v2')
 const ValidationError = require('../../../../../lib/validation-error')
 const checkWindowNameValidator = require('../../../../../lib/validator/check-window-v2/check-window-name-validator')
 
 describe('Check window name validator', function () {
   describe('validate', function () {
-    it('should not call addError method if the validation is successful', () => {
+    test('should not call addError method if the validation is successful', () => {
       const validationError = new ValidationError()
-      spyOn(validationError, 'addError')
+      jest.spyOn(validationError, 'addError').mockImplementation()
       checkWindowNameValidator.validate(validationError, 'checkWindowName')
       expect(validationError.addError).not.toHaveBeenCalled()
     })
-    it('should call addError method with checkWindowName and checkWindowNameLength errors if the check window name is missing', () => {
+    test('should call addError method with checkWindowName and checkWindowNameLength errors if the check window name is missing', () => {
       const validationError = new ValidationError()
-      spyOn(validationError, 'addError')
+      jest.spyOn(validationError, 'addError').mockImplementation()
       checkWindowNameValidator.validate(validationError, '')
       expect(validationError.addError).toHaveBeenCalledWith('checkWindowName', checkWindowErrorMessages.checkWindowNameLength)
     })
-    it('should call addError method with checkWindowName and checkWindowNameLength errors if the check window name is only 1 character', () => {
+    test('should call addError method with checkWindowName and checkWindowNameLength errors if the check window name is only 1 character', () => {
       const validationError = new ValidationError()
-      spyOn(validationError, 'addError')
+      jest.spyOn(validationError, 'addError').mockImplementation()
       checkWindowNameValidator.validate(validationError, '')
       expect(validationError.addError).toHaveBeenCalledWith('checkWindowName', checkWindowErrorMessages.checkWindowNameLength)
     })
-    it('should call addError method checkWindowNameLength errors if the check window name exceeds allowed length', () => {
+    test('should call addError method checkWindowNameLength errors if the check window name exceeds allowed length', () => {
       const validationError = new ValidationError()
-      spyOn(validationError, 'addError')
+      jest.spyOn(validationError, 'addError').mockImplementation()
       checkWindowNameValidator.validate(validationError, 'checkwindowabcdefghijklmnopqrstuvwxyz1234567890')
       expect(validationError.addError).toHaveBeenCalledWith('checkWindowName', checkWindowErrorMessages.checkWindowNameLength)
     })
