5ed163d7fcfbcf123e5f0788db3256b9fd8230d4
Guy Harwood <GuyHarwood@users.noreply.github.com>
2022-03-14 10:18:26 +0000

    Feature/51702 ps report app (#2146)
    * create app folder
    
    * move ps report functions
    
    * move ps report 3 function
    
    * fix refs
    
    * align entry points
    
    * remove moved apps
    
    * updates for tests
    
    Co-authored-by: Jon Shipley <jon-shipley@users.noreply.github.com>
    Co-authored-by: Mohsen Qureshi <activemq@users.noreply.github.com>
    Co-authored-by: Mohsen Qureshi <m-qureshi@hotmail.com>

---
+++ func-ps-report/test.Dockerfile
@@ -0,0 +1,9 @@
+# WARNING
+# this app relies on the dist output from tslib
+# this must be built before building this dockerfile
+FROM stamtc/mtc-azure-functions-base:latest
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+COPY . .
+
+CMD yarn start
---
+++ tslib/src/functions-ps-report/ps-report-3-transformer/mocks/check-incomplete.ts
@@ -0,0 +1,18 @@
+import { Check } from '../../../functions-ps-report/ps-report-2-pupil-data/models'
+
+export const check: Check = {
+  id: 31,
+  checkCode: 'xyz-def-988',
+  checkFormId: 9,
+  checkWindowId: 1,
+  complete: false,
+  completedAt: null,
+  inputAssistantAddedRetrospectively: false,
+  isLiveCheck: true,
+  mark: null,
+  processingFailed: false,
+  pupilLoginDate: null,
+  received: true,
+  restartNumber: 0,
+  restartReason: null
+}
