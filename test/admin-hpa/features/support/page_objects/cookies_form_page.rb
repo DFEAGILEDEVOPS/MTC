9ddaeb5a3990abb2734f10b0b3aeaea2c3c03c38
Mohsen Qureshi <m-qureshi@hotmail.com>
2022-03-17 12:12:39 +0000

    update to cookies selectors

--- test/admin-hpa/features/support/page_objects/cookies_form_page.rb
+++ test/admin-hpa/features/support/page_objects/cookies_form_page.rb
@@ -2,7 +2,8 @@ class CookiesFormPage < SitePrism::Page
   set_url '/cookies-form'
 
   element :heading, '.govuk-heading-xl', text: 'Cookies on the multiplication tables check (MTC)'
-  element :gov_speak_text, '.govuk-govspeak'
+  element :cookies_saved_text, 'p', text: 'Cookies are files saved on your phone, tablet or computer when you visit a website.'
+  element :cookies_usage_text, 'p', text: 'We use cookies to store information about how you use the MTC service, such as the pages you visit.'
   element :settings_heading, '.govuk-heading-m', text: 'Cookie settings'
   element :website_use_heading, 'legend', text: 'Cookies that measure website use'
   element :necessary_cookies_heading, '.govuk-heading-m', text: 'Strictly necessary cookies'
