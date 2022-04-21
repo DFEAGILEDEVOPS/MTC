9579b39809fbd233b351ddfffab79257db546d89
Guy Harwood <guy@milkchip.com>
2022-04-21 10:24:13 +0100

    types for api response and browser headers for admin app

--- pupil-api/src/controllers/api-response.ts
+++ pupil-api/src/controllers/api-response.ts
@@ -1,29 +1,30 @@
 import { Response } from 'express'
 
 const apiResponse = {
-  unauthorised: (res: Response) => {
+  unauthorised: (res: Response): Response => {
     apiResponse.setDefaultHeaders(res)
     return res.status(401).json({ error: 'Unauthorised' })
   },
 
-  badRequest: (res: Response) => {
+  badRequest: (res: Response): Response => {
     apiResponse.setDefaultHeaders(res)
     return res.status(400).json({ error: 'Bad request' })
   },
 
-  serverError: (res: Response) => {
+  serverError: (res: Response): Response => {
     apiResponse.setDefaultHeaders(res)
     return res.status(500).json({ error: 'Server error' })
   },
 
-  sendJson: (res: Response, obj: object | string, code: number = 200) => {
+  sendJson: (res: Response, obj: object | string, code: number = 200): Response => {
     apiResponse.setDefaultHeaders(res)
     return res.status(code).json(obj)
   },
 
-  setDefaultHeaders: (res: Response) => {
+  setDefaultHeaders: (res: Response): void => {
     res.setHeader('X-Content-Type-Options', 'nosniff')
     res.setHeader('Cache-Control', 'no-store')
+    res.setHeader('Pragma', 'no-cache')
     res.setHeader('Content-Type', 'application/json')
   }
 }
