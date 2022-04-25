9df7d2b5c0cdf098c3698c543b527a6875e05f57
Guy Harwood <guy@milkchip.com>
2022-04-25 14:33:26 +0100

    remove script and inclusion of both envs

---
+++ deploy/build-defs/remove-key-vault-ip.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+if [ -z "$1" ]
+  then
+    echo "azure key vault instance name required"
+    exit 1
+fi
+
+if [ -z "$2" ]
+  then
+    echo "azure resource group name required"
+    exit 1
+fi
+
+if [ -z "$3" ]
+  then
+    echo "ip address required"
+    exit 1
+fi
+
+KEY_VAULT_NAME=$1
+RES_GRP=$2
+IP_ADDRESS=$3
+az keyvault network-rule remove --name $KEY_VAULT_NAME --resource-group $RES_GRP --ip-address $IP_ADDRESS
