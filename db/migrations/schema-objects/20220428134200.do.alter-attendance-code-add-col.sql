34b600a1d38d102872325bcbf0613f778dade58c
Jon Shipley <js@ovix.co.uk>
2022-04-29 08:53:30 +0000

    db changes

--- db/migrations/schema-objects/20220428134200.do.alter-attendance-code-add-col.sql
+++ db/migrations/schema-objects/20220428134200.do.alter-attendance-code-add-col.sql
@@ -2,8 +2,10 @@ IF NOT EXISTS(
                 SELECT *
                 FROM sys.columns
                WHERE object_ID = object_id('mtc_admin.attendanceCode')
-                 AND col_name(object_ID, column_Id) = 'role_id')
+                 AND col_name(object_ID, column_Id) = 'isPrivileged')
     BEGIN
         ALTER TABLE [mtc_admin].[attendanceCode]
-            ADD [role_id] Int;
+            ADD [isPrivileged] Bit,
+              CONSTRAINT [DF_isPrivileged]
+              DEFAULT 0 FOR [isPrivileged];
     END
--- db/migrations/schema-objects/20220428134200.undo.alter-attendance-code-add-role.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE [mtc_admin].[attendanceCode] DROP COLUMN IF EXISTS  [role_id]
-;
