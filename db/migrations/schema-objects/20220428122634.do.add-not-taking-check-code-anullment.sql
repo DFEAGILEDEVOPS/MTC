b6642fe8471757bbdaa4752d1a2cf72213d4f47d
Jon Shipley <js@ovix.co.uk>
2022-05-05 16:31:16 +0000

    fix typo

--- db/migrations/schema-objects/20220428122634.do.add-not-taking-check-code-anullment.sql
+++ db/migrations/schema-objects/20220428122634.do.add-not-taking-check-code-anullment.sql
@@ -1,3 +1,3 @@
-INSERT INTO [mtc_admin].[attendanceCode] (reason, [order], code) VALUES ('Anulled by DfE', 99, 'ANLLD')
+INSERT INTO [mtc_admin].[attendanceCode] (reason, [order], code) VALUES ('Annulled by DfE', 99, 'ANLLD')
 ;
 
