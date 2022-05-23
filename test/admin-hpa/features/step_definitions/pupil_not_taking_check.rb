c3cbd3e777d8baaa51e9a4b67d864053ca03e47c
Mohsen Qureshi <m-qureshi@hotmail.com>
2022-05-23 14:46:30 +0100

    Update pupil_not_taking_check.rb

--- test/admin-hpa/features/step_definitions/pupil_not_taking_check.rb
+++ test/admin-hpa/features/step_definitions/pupil_not_taking_check.rb
@@ -25,6 +25,7 @@ end
 
 Then(/^I should see set of reasons I can choose$/) do
   expected_reason_hash = SqlDbHelper.get_attendance_codes.map {|code| code['reason']}
+  expected_reason_hash.delete("Results annulled")
   expect(pupil_reason_page.attendance_code_mapping.keys.sort).to eql pupil_reason_page.attendance_codes.map {|code| code['id']}.sort
   actual_reason_hash = pupil_reason_page.attendance_code_mapping.values
   expect(actual_reason_hash.sort).to eql expected_reason_hash.sort
