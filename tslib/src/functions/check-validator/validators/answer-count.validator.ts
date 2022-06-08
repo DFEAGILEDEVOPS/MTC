1298e88732310e7a7859acbaf6df6164bb0c2672
Jon Shipley <js@ovix.co.uk>
2022-06-08 08:42:54 +0000

    Fix missing answer(s)

--- tslib/src/functions/check-validator/validators/answer-count.validator.ts
+++ tslib/src/functions/check-validator/validators/answer-count.validator.ts
@@ -8,9 +8,16 @@ export class AnswerCountValidator implements ISubmittedCheckValidator {
         message: 'no answers property found'
       }
     }
-    if (check.answers.length < config.LiveFormQuestionCount) {
+    // there should be an answer for every question
+    const foundAnswers = Array(config.LiveFormQuestionCount).fill(0)
+    check.answers.forEach((a: { sequenceNumber: number }) => {
+      foundAnswers[a.sequenceNumber - 1] = 1
+    })
+    const isOne = (currentValue: number): boolean => currentValue === 1
+    const answerCount = foundAnswers.filter(isOne).length
+    if (answerCount < config.LiveFormQuestionCount) {
       return {
-        message: `submitted check has ${check.answers.length} answers`
+        message: `submitted check has ${answerCount} answers`
       }
     }
   }
