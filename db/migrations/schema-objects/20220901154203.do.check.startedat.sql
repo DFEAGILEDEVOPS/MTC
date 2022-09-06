a7894b00bf7699a3b334fff1c435ceb1c799d4fc
Guy Harwood <guy@milkchip.com>
2022-09-06 16:16:20 +0100

    add column documentation

--- db/migrations/schema-objects/20220901154203.do.check.startedat.sql
+++ db/migrations/schema-objects/20220901154203.do.check.startedat.sql
@@ -3,3 +3,7 @@ ALTER TABLE
   mtc_admin.[check]
 ADD
   startedAt datetimeoffset(3) NULL
+
+EXEC sys.sp_addextendedproperty @name = N'MS_Description', @value = 'the date and time the pupil clicked the button to start the official check.', @level0type = N'Schema',
+     @level0name = 'mtc_admin', @level1type = N'Table', @level1name = 'check', @level2type = N'Column',
+     @level2name = 'startedAt';
