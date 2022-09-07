d18ad46461d1d26d713b82d82a9afd617cbedfbe
Guy Harwood <GuyHarwood@users.noreply.github.com>
2022-09-07 08:31:25 +0100

    Update db/migrations/schema-objects/20220901154203.do.check.startedat.sql
    Co-authored-by: Jon Shipley <jon-shipley@users.noreply.github.com>

--- db/migrations/schema-objects/20220901154203.do.check.startedat.sql
+++ db/migrations/schema-objects/20220901154203.do.check.startedat.sql
@@ -4,6 +4,6 @@ ALTER TABLE
 ADD
   startedAt datetimeoffset(3) NULL
 
-EXEC sys.sp_addextendedproperty @name = N'MS_Description', @value = 'the date and time the pupil clicked the button to start the official check.', @level0type = N'Schema',
+EXEC sys.sp_addextendedproperty @name = N'MS_Description', @value = 'the date and time as reported by the browser that the pupil clicked the button to start the official check.', @level0type = N'Schema',
      @level0name = 'mtc_admin', @level1type = N'Table', @level1name = 'check', @level2type = N'Column',
      @level2name = 'startedAt';
