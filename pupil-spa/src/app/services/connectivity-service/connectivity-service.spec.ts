ced837ddb249dd956f37f6ea4baec98d23f1afae
Jon Shipley <js@ovix.co.uk>
2022-09-11 03:42:56 +0100

    Fix connectivity check test
    Initialise the config service mock

--- pupil-spa/src/app/services/connectivity-service/connectivity-service.spec.ts
+++ pupil-spa/src/app/services/connectivity-service/connectivity-service.spec.ts
@@ -4,6 +4,8 @@ import { AzureQueueService } from '../azure-queue/azure-queue.service'
 import { ConnectivityService } from './connectivity-service'
 import { default as connectivityErrorMessages } from './connectivity-error-messages'
 import { HttpClient } from '@angular/common/http'
+import { APP_INITIALIZER } from '@angular/core'
+import { loadConfigMockService } from '../config/config.service'
 
 let connectivityService
 
@@ -14,7 +16,8 @@ describe('ConnectivityService', () => {
       imports: [HttpClientTestingModule],
       providers: [
         AzureQueueService,
-        ConnectivityService
+        ConnectivityService,
+        { provide: APP_INITIALIZER, useFactory: loadConfigMockService, multi: true },
       ]
     })
     TestBed.inject(HttpClient)
