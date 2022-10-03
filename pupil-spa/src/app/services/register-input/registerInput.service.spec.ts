223f585f003e845ce9c4c4eb7de1b51402a4e3c2
Jon Shipley <js@ovix.co.uk>
2022-10-03 20:38:22 +0100

    refactor RegisterInput

--- pupil-spa/src/app/services/register-input/registerInput.service.spec.ts
+++ pupil-spa/src/app/services/register-input/registerInput.service.spec.ts
@@ -2,14 +2,17 @@ import { Injectable } from '@angular/core'
 import { TestBed, inject } from '@angular/core/testing'
 import { RegisterInputService } from './registerInput.service'
 import { StorageService } from '../storage/storage.service'
+import { MonotonicTimeService } from '../monotonic-time/monotonic-time.service'
+import { MonotonicTime } from '../../monotonic-time'
 
 let mockStorageService: StorageService
 
 @Injectable()
 export class TestRegisterInputService extends RegisterInputService {
 
-  constructor (protected storageService: StorageService) {
-    super(storageService)
+  constructor (protected storageService: StorageService,
+               protected monotonicTimeService: MonotonicTimeService) {
+    super(storageService, monotonicTimeService)
   }
 }
 
@@ -21,6 +24,7 @@ describe('RegisterInputService', () => {
       providers: [
         TestRegisterInputService,
         StorageService,
+        MonotonicTimeService
       ]
     })
     mockStorageService = injector.inject(StorageService)
@@ -52,7 +56,7 @@ describe('RegisterInputService', () => {
       service.storeEntry(entry.input, entry.eventType, entry.sequenceNumber, entry.question, d1.valueOf())
       expect(storageServiceSetInputSpy).toHaveBeenCalledTimes(1)
       const arg = storageServiceSetInputSpy.calls.all()[0].args[0]
-      expect(arg).toEqual(entry)
+      expect(arg).toEqual(jasmine.objectContaining(entry))
     }))
 
   it('StoreEntry will generate new Date if the event timestamp is undefined',
@@ -66,6 +70,16 @@ describe('RegisterInputService', () => {
       expect(clientTimestamp).toBeTruthy()
       const cts = new Date(clientTimestamp)
       const now = new Date()
-      expect(Math.abs(cts.getTime() - now.getTime())).toBeLessThan(10) // Should be set to this year not 1970
+      expect(Math.abs(cts.getTime() - now.getTime())).toBeLessThan(150)
+    }))
+
+  it('StoreEntry will generate a high-precision timestamp',
+    inject([TestRegisterInputService], (service: TestRegisterInputService) => {
+      const eventValue = '0'
+      const eventType = 'keydown'
+      service.storeEntry(eventValue, eventType, 7, '2x3')
+      const mtime = storageServiceSetInputSpy.calls.all()[0].args[0].monotonicTime
+      expect(mtime).toBeDefined()
+      expect(Object.keys(mtime)).toEqual(['milliseconds', 'legacyDate', 'sequenceNumber'])
     }))
 })
--- pupil-spa/src/app/services/register-input/registerInput.service.ts
+++ pupil-spa/src/app/services/register-input/registerInput.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
 import { StorageService } from '../storage/storage.service';
+import { MonotonicTimeService } from '../monotonic-time/monotonic-time.service'
 
 @Injectable()
 export class RegisterInputService {
-  constructor(protected storageService: StorageService) {}
+  constructor(protected storageService: StorageService,
+              protected monotonicTimeService: MonotonicTimeService) {}
 
   public storeEntry(eventValue: string, eventType: string, questionNumber: number, question: string, eventTimeStamp: number = null) {
     // event.timestamp is now being changed to be a DOMHiResTimestamp
-    const eventDate = eventTimeStamp !== null ? new Date(eventTimeStamp) : new Date()
+    const monotonicTime = this.monotonicTimeService.getMonotonicDateTime()
+    let eventDate: Date
+    if (eventTimeStamp === null) {
+      eventDate = monotonicTime.formatAsDate()
+    } else {
+      eventDate = new Date(eventTimeStamp)
+    }
     const questionInput = {
       input: eventValue,
       eventType: eventType,
       clientTimestamp: eventDate.toISOString(),
       question: question,
-      sequenceNumber: questionNumber
+      sequenceNumber: questionNumber,
+      monotonicTime: monotonicTime.getDto()
     };
     this.storageService.setInput(questionInput);
   }
