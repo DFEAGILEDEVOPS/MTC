b251ff0a88f9c03742c46ce2efd14d48e2ad4b86
Guy Harwood <guy@milkchip.com>
2022-10-24 15:58:20 +0100

    POC basic template of azure function with service bus queue binding

---
+++ design/spikes-poc/dotnet-servicebus-queue/myqfunc/myqfunc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace myqfunc
+{
+    public class myqfunc
+    {
+        [FunctionName("myqfunc")]
+        public void Run([ServiceBusTrigger("myqueue", Connection = "sbcon")]string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+        }
+    }
+}
+
