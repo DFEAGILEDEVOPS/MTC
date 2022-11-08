65d7f04a2aec41a7ee29c69385959e02ec617094
Jon Shipley <js@ovix.co.uk>
2022-11-08 13:25:53 +0000

    Fix - down migrations

--- db/migrations/schema-objects/20221103144346.undo.ps-report-rename-id-to-pupilid.sql
+++ db/migrations/schema-objects/20221103144346.undo.ps-report-rename-id-to-pupilid.sql
@@ -1,2 +1,15 @@
 -- Rename col
 EXEC sp_rename 'mtc_results.psychometricReport.PupilId', 'id', 'COLUMN';
+
+go
+
+-- Undo modify the updatedAt trigger as the PK is now PupilId, not id.
+ALTER TRIGGER [mtc_results].[psychometricReportUpdatedAtTrigger]
+    ON [mtc_results].[psychometricReport]
+    FOR UPDATE AS
+BEGIN
+    UPDATE [mtc_results].[psychometricReport]
+       SET updatedAt = GETUTCDATE()
+      FROM inserted
+     WHERE [psychometricReport].id = inserted.id
+END
