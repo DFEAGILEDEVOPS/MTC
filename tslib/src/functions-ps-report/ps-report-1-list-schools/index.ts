b01d4adbcc007371feac4a66ba3f9edef284b211
Guy Harwood <guy@milkchip.com>
2022-11-08 17:07:46 +0000

    modify function

--- tslib/src/functions-ps-report/ps-report-1-list-schools/index.ts
+++ tslib/src/functions-ps-report/ps-report-1-list-schools/index.ts
@@ -1,28 +1,35 @@
 import { AzureFunction, Context } from '@azure/functions'
 import { performance } from 'perf_hooks'
 import { ListSchoolsService } from './list-schools-service'
-import { IFunctionTimer } from '../../azure/functions'
 import { PsReportLogger } from '../common/ps-report-logger'
 import { PsReportSource } from '../common/ps-report-log-entry'
+import { JobDataService } from '../../services/data/job.data.service'
+import { JobStatusCode } from '../../common/job-status-code'
 
-const functionName = 'ps-report-1-list-schools'
+interface IncomingMessage {
+  requestedBy: string
+  dateTimeRequested: moment.Moment
+  jobUuid: string
+}
 
-const timerTrigger: AzureFunction = async function (context: Context, timer: IFunctionTimer): Promise<void> {
-  if (timer.isPastDue) {
-    // This function could potentially deliver a lot of work to do to the functions, and none of it is urgent. No surprises!
-    context.log(`${functionName}: timer is past due, exiting.`)
-    return
-  }
+const serviceBusTrigger: AzureFunction = async function (context: Context, jobInfo: IncomingMessage): Promise<void> {
   const logger = new PsReportLogger(context, PsReportSource.SchoolGenerator)
+  logger.verbose(`requested at ${jobInfo.dateTimeRequested.toISOString()} by ${jobInfo.requestedBy}`)
   const start = performance.now()
   const meta = { processCount: 0, errorCount: 0 }
+  const jobDataService = new JobDataService()
   try {
+    await jobDataService.setJobStarted(jobInfo.jobUuid)
     const schoolListService = new ListSchoolsService(logger)
     const messages = await schoolListService.getSchoolMessages()
     context.bindings.schoolMessages = messages
     meta.processCount = messages.length
+    await jobDataService.setJobComplete(jobInfo.jobUuid,
+      JobStatusCode.CompletedSuccessfully, `processed ${meta.processCount} records`)
   } catch (error) {
     logger.error(error.message)
+    await jobDataService.setJobComplete(jobInfo.jobUuid, JobStatusCode.Failed,
+      `processed ${meta.processCount} records`, error.message)
     throw error
   }
   const end = performance.now()
@@ -30,4 +37,4 @@ const timerTrigger: AzureFunction = async function (context: Context, timer: IFu
   logger.info(`processed ${meta.processCount} records, run took ${durationInMilliseconds} ms`)
 }
 
-export default timerTrigger
+export default serviceBusTrigger
