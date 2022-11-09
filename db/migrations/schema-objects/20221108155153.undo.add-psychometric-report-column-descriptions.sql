ca2118e8d4b779c2ad40e9e44e3457cdb58a8a1c
Jon Shipley <js@ovix.co.uk>
2022-11-09 13:33:12 +0000

    Document db cols

---
+++ db/migrations/schema-objects/20221108155153.undo.add-psychometric-report-column-descriptions.sql
@@ -0,0 +1,27 @@
+EXEC sys.sp_dropextendedproperty @name = N'MS_Description',
+@level0name = 'mtc_results',
+@level1type = N'Table',
+@level1name = 'psychometricReport',
+@level2type = N'Column',
+@level2name = 'PupilId';
+
+EXEC sys.sp_dropextendedproperty @name = N'MS_Description',
+@level0name = 'mtc_results',
+@level1type = N'Table',
+@level1name = 'psychometricReport',
+@level2type = N'Column',
+@level2name = 'PupilUPN';
+
+EXEC sys.sp_dropextendedproperty @name = N'MS_Description',
+@level0name = 'mtc_results',
+@level1type = N'Table',
+@level1name = 'psychometricReport',
+@level2type = N'Column',
+@level2name = 'DOB';
+
+EXEC sys.sp_dropextendedproperty @name = N'MS_Description',
+@level0name = 'mtc_results',
+@level1type = N'Table',
+@level1name = 'psychometricReport',
+@level2type = N'Column',
+@level2name = 'FormMark';
