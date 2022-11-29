8033a3ad459ce4494565faea031b94f05da056a5
Jon Shipley <js@ovix.co.uk>
2022-11-29 11:35:02 +0000

    Update db migrations and school seed data

--- db/migrations/schema-objects/20221128142627.do.populate-test-schools.sql
+++ db/migrations/schema-objects/20221128142627.do.populate-test-schools.sql
@@ -1,4 +1,7 @@
 -- SET the isTestSchool flag
+-- this affects prod and pre-prod test schools (as these environments have existing data).
+-- Note that for local dev, where migrations and *then* seed data is inserted, this will
+-- do precisely nothing, as the school table is empty when the migrations run.
 UPDATE mtc_admin.school
 SET isTestSchool = 1
 WHERE leaCode = 999;
--- db/seeds/20221122145201.custom.update-school-leacodes-for-dev.sql
+++ db/seeds/20221122145201.custom.update-school-leacodes-for-dev.sql
@@ -1,3 +1,6 @@
+-- Seed data script for local-dev
+-- Modify the schools to make them non-test, except school 4
+
 UPDATE mtc_admin.school
 SET
   leaCode = 201,
@@ -7,4 +10,11 @@ WHERE
   leaCode = 999
 AND
   estabCode <> 1004
+;
+
+
+-- We can now run the same code as the data migration, but this time for local-dev / seed consumers
+UPDATE mtc_admin.school
+SET isTestSchool = 1
+WHERE leaCode = 999
 ;
