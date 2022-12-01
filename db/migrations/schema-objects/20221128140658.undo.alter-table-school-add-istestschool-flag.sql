bd472c5d68146588ad91eb5bcdb2dbf7a2a5d99e
Jon Shipley <js@ovix.co.uk>
2022-12-01 16:16:32 +0000

    Update migration - drop default value in undo script

--- db/migrations/schema-objects/20221128140658.undo.alter-table-school-add-istestschool-flag.sql
+++ db/migrations/schema-objects/20221128140658.undo.alter-table-school-add-istestschool-flag.sql
@@ -1,2 +1,6 @@
 -- UNDO add column
+ ALTER TABLE [mtc_admin].[school]
+    DROP CONSTRAINT IF EXISTS [DF_isTestSchool];
+go
+
 ALTER TABLE [mtc_admin].[school] DROP COLUMN IF EXISTS  [isTestSchool];
