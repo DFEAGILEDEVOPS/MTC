ba29123bc4bcb8e9a751e07d1be2eba874be5151
Guy Harwood <guy@milkchip.com>
2022-12-08 16:33:58 +0000

    basic component with template

--- pupil-spa/src/app/qr-code-arrival/qr-code-arrival.component.ts
+++ pupil-spa/src/app/qr-code-arrival/qr-code-arrival.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
 
 @Component({
-  selector: 'qr-code-arrival'
+  selector: 'qr-code-arrival',
+  templateUrl: './qr-code-arrival.component.html'
 })
 export class QrCodeArrivalComponent implements OnInit {
 
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.router.navigate([''])
+    this.router.navigate(['sign-in'])
   }
 }
