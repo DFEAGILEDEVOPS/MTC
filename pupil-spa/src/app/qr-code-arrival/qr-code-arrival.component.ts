c0c912a761fba0a965973929cb89443e254d4a48
Guy Harwood <guy@milkchip.com>
2022-12-12 17:47:36 +0000

    add app insights

--- pupil-spa/src/app/qr-code-arrival/qr-code-arrival.component.ts
+++ pupil-spa/src/app/qr-code-arrival/qr-code-arrival.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
+import { ApplicationInsightsService } from '../services/app-insights/app-insights.service';
 
 @Component({
   selector: 'qr-code-arrival',
@@ -7,9 +8,11 @@ import { Router } from '@angular/router'
 })
 export class QrCodeArrivalComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+              private appInsightsService: ApplicationInsightsService) {}
 
   ngOnInit(): void {
+    this.appInsightsService.trackPageView('QR code', '/qr')
     this.router.navigate(['sign-in'])
   }
 }
