cd393d02f3a57748306324d69a444ae269b70f85
Guy Harwood <guy@milkchip.com>
2022-12-13 16:30:48 +0000

    implement new spa path into qr code data, add caching

--- admin/services/qr.service.js
+++ admin/services/qr.service.js
@@ -1,13 +1,22 @@
 const qrCode = require('qrcode')
+const redisCacheService = require('./data-access/redis-cache.service')
+const redisKeyService = require('./redis-key.service')
 
 const qrService = {
-  getDataURL: (url) => {
+  getDataURL: async (url) => {
     if (!url) return null
+    const cachedData = await redisCacheService.get(redisKeyService.getQrCodeUrlPrefix(url))
+    if (cachedData !== undefined) return cachedData
     return new Promise((resolve, reject) => {
       qrCode.toDataURL(url,
-        (error, url) => {
-          if (error) reject(error)
-          else return resolve(url)
+        async (error, qrCodeData) => {
+          if (error) {
+            reject(error)
+          }
+          else {
+            await redisCacheService.set(redisKeyService.getQrCodeUrlPrefix(url))
+            return resolve(qrCodeData)
+          }
         }
       )
     })
