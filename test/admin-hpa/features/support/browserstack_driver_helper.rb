065b28349cc00549cd11b9e90f006c1e94cc05d2
Mohsen Qureshi <m-qureshi@hotmail.com>
2023-01-10 13:46:34 +0000

    removing bs code

--- test/admin-hpa/features/support/browserstack_driver_helper.rb
+++ test/admin-hpa/features/support/browserstack_driver_helper.rb
@@ -1,21 +1,21 @@
-require 'yaml'
-require 'selenium/webdriver'
-require 'capybara/cucumber'
-require 'browserstack/local'
-require_relative 'browserstack_settings'
-
-BrowserstackSettings.browser_caps.keys.each do |browser|
-  Capybara.register_driver "bs_#{browser}".to_sym do |app|
-    @caps = BrowserstackSettings.common_caps.merge(BrowserstackSettings.browser_caps.send(browser))
-    @caps = @caps.merge ({"name" => "#{Time.now.strftime("%d/%m/%y-%H:%M")}"})
-    if @caps['browserstack.local'] && @caps['browserstack.local'].to_s == 'true'
-      @bs_local = BrowserStack::Local.new
-      bs_local_args = {"key" => "#{ENV['BROWSERSTACK_ACCESS_KEY']}", "force" => 'true', 'localidentifier'=> ENV['BS_ID']}
-      @bs_local.start(bs_local_args)
-    end
-    url = "http://#{ENV['BROWSERSTACK_USERNAME']}:#{ENV['BROWSERSTACK_ACCESS_KEY']}@#{BrowserstackSettings.server}/wd/hub"
-    Capybara::Selenium::Driver.new(app, browser: :remote,
-                                   url: url,
-                                   desired_capabilities: @caps)
-  end
-end
+# require 'yaml'
+# require 'selenium/webdriver'
+# require 'capybara/cucumber'
+# require 'browserstack/local'
+# require_relative 'browserstack_settings'
+#
+# BrowserstackSettings.browser_caps.keys.each do |browser|
+#   Capybara.register_driver "bs_#{browser}".to_sym do |app|
+#     @caps = BrowserstackSettings.common_caps.merge(BrowserstackSettings.browser_caps.send(browser))
+#     @caps = @caps.merge ({"name" => "#{Time.now.strftime("%d/%m/%y-%H:%M")}"})
+#     if @caps['browserstack.local'] && @caps['browserstack.local'].to_s == 'true'
+#       @bs_local = BrowserStack::Local.new
+#       bs_local_args = {"key" => "#{ENV['BROWSERSTACK_ACCESS_KEY']}", "force" => 'true', 'localidentifier'=> ENV['BS_ID']}
+#       @bs_local.start(bs_local_args)
+#     end
+#     url = "http://#{ENV['BROWSERSTACK_USERNAME']}:#{ENV['BROWSERSTACK_ACCESS_KEY']}@#{BrowserstackSettings.server}/wd/hub"
+#     Capybara::Selenium::Driver.new(app, browser: :remote,
+#                                    url: url,
+#                                    desired_capabilities: @caps)
+#   end
+# end
