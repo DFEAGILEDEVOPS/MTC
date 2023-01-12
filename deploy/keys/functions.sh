98696719bf7d641c736bc505666e6d17f4dffaf5
Guy Harwood <guy@milkchip.com>
2023-01-12 18:22:59 +0000

    rename folder, fix paths

---
+++ deploy/keys/functions.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+# https://learn.microsoft.com/en-us/cli/azure/functionapp/keys?view=azure-cli-latest#az-functionapp-keys-set
---
+++ deploy/keys/readme.md
@@ -0,0 +1,3 @@
+# Key Rotation Scripts
+
+These scripts renew keys for the relevant services and refresh the associated key vault entries.
