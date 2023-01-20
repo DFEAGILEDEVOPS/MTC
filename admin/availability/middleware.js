32a3b7c69e80a008b314a225b0d4bd612631f7be
Jon Shipley <js@ovix.co.uk>
2023-01-20 11:57:47 +0000

    Fix breadcrumb bug

--- admin/availability/middleware.js
+++ admin/availability/middleware.js
@@ -42,9 +42,9 @@ async function isPostLiveOrLaterCheckPhase (req, res, next) {
    * global.checkWindowPhase is an int. Later phases have larger number.
    */
   try {
-    res.locals.pageTitle = 'Section unavailable'
-    req.breadcrumbs(res.locals.pageTitle)
     if (global.checkWindowPhase >= checkWindowPhaseConsts.postCheckAdmin) {
+      res.locals.pageTitle = 'Section unavailable'
+      req.breadcrumbs(res.locals.pageTitle)
       return res.render('availability/section-unavailable', { breadcrumbs: req.breadcrumbs(), title: 'Section unavailable' })
     }
     return next()
