66827e81d716a61b2a16fb4edac69cb34c1485c2
Jon Shipley <js@ovix.co.uk>
2023-02-21 13:02:32 +0000

    Upgrade postgrator to v7.1.1
    + update migrator script to include execQuery
    + update migrator script to handle db connection outside of postgrator
    + load postgrator as a dynamically imported ES module (async)

--- db/engine/migrate.js
+++ db/engine/migrate.js
@@ -2,6 +2,7 @@
 
 const path = require('path')
 const fs = require('fs')
+const mssql = require('mssql')
 const globalDotEnvFile = path.join(__dirname, '..', '..', '.env')
 
 try {
@@ -15,96 +16,124 @@ try {
   console.error(error)
 }
 
-const config = require('../config')
-const sqlConfig = require('../sql.config')
-const logger = require('./log.service').getLogger()
-const Postgrator = require('postgrator')
-const {
-  sortMigrationsAsc,
-  sortMigrationsDesc
-} = require('postgrator/lib/utils.js')
-const createDatabaseIfNotExists = require('./createDatabase')
+async function main () {
+  const config = require('../config')
+  const sqlConfig = require('../sql.config')
+  const logger = require('./log.service').getLogger()
+  const createDatabaseIfNotExists = require('./createDatabase')
+  const Postgrator = await import('postgrator') // dynamic import ES Module
+  const { sortMigrationsAsc, sortMigrationsDesc } = await import('postgrator/lib/utils.js') // dynamic import
 
-class Migrator extends Postgrator {
-  /*
-    The getRunnableMigrations method returns ALL pending
-    migrations up to the specified migration
-  */
-  async getRunnableMigrations (databaseVersion, targetVersion) {
-    const result = await this.runQuery(`SELECT version FROM ${this.config.schemaTable}`)
-    const appliedMigrations = result.rows.map(r => parseInt(r.version))
-    const { migrations } = this
-    if (targetVersion >= databaseVersion) {
-      return migrations
-        .filter(
-          migration =>
-            migration.action === 'do' &&
-            (migration.version > databaseVersion || !~appliedMigrations.indexOf(migration.version)) &&
-            migration.version <= targetVersion
-        )
-        .sort(sortMigrationsAsc)
+  /**
+   *
+   * @param {string} database
+   * @param {mssql.ConnectionPool} client
+   */
+  function getMigratorConfig (database, client) {
+    return {
+      migrationPattern: path.join(__dirname, '..', 'migrations', '**'),
+      driver: 'mssql',
+      database,
+      validateChecksums: false,
+      schemaTable: 'migrationLog',
+      execQuery: (query) => {
+        return new Promise((resolve, reject) => {
+          const request = new mssql.Request(client)
+          request.query(query, (err, result) => {
+            if (err) {
+              return reject(err)
+            }
+            return resolve({
+              rows: result && result.recordset ? result.recordset : result
+            })
+          })
+        })
+      }
     }
-    if (targetVersion < databaseVersion) {
-      return migrations
-        .filter(
-          migration =>
-            migration.action === 'undo' &&
-            migration.version <= databaseVersion &&
-            migration.version > targetVersion
-        )
-        .sort(sortMigrationsDesc)
-    }
-    return []
   }
-}
 
-const migratorConfig = {
-  migrationPattern: path.join(__dirname, '..', 'migrations', '**'),
-  driver: 'mssql',
-  host: config.Sql.Server,
-  // Required for when SQL_PORT is passed in via docker-compose
-  port: parseInt(config.Sql.Port),
-  database: config.Sql.Database,
-  username: config.Sql.Migrator.Username,
-  password: config.Sql.Migrator.Password,
-  requestTimeout: config.Sql.Migrator.Timeout,
-  connectionTimeout: config.Sql.Migrator.Timeout,
-  // Schema table name. Optional. Default is schemaversion
-  schemaTable: 'migrationLog',
-  options: {
-    encrypt: sqlConfig.options.encrypt,
-    enableArithAbort: sqlConfig.options.enableArithAbort,
-    trustServerCertificate: sqlConfig.options.trustServerCertificate
-  },
-  validateChecksums: false
-}
+  class Migrator extends Postgrator.default {
+    /*
+      The getRunnableMigrations method returns ALL pending
+      migrations up to the specified migration
+    */
+    async getRunnableMigrations (databaseVersion, targetVersion) {
+      const result = await this.runQuery(`SELECT version FROM ${this.config.schemaTable}`)
+      const appliedMigrations = result.rows.map(r => parseInt(r.version))
+      const { migrations } = this
+      if (targetVersion >= databaseVersion) {
+        return migrations
+          .filter(
+            migration =>
+              migration.action === 'do' &&
+              (migration.version > databaseVersion || !~appliedMigrations.indexOf(migration.version)) &&
+              migration.version <= targetVersion
+          )
+          .sort(sortMigrationsAsc)
+      }
+      if (targetVersion < databaseVersion) {
+        return migrations
+          .filter(
+            migration =>
+              migration.action === 'undo' &&
+              migration.version <= databaseVersion &&
+              migration.version > targetVersion
+          )
+          .sort(sortMigrationsDesc)
+      }
+      return []
+    }
+  }
 
-const runMigrations = async (version) => {
+
+  const runMigrations = async (version) => {
+    try {
+      await createDatabaseIfNotExists()
+      const client = new mssql.ConnectionPool({
+        server: config.Sql.Server,
+        database: config.Sql.Database,
+        user: config.Sql.Migrator.Username,
+        password: config.Sql.Migrator.Password,
+        options: sqlConfig.options,
+        requestTimeout: config.Sql.Migrator.Timeout,
+        connectionTimeout: config.Sql.Migrator.Timeout,
+        pool:  {
+          max: 1,
+          min: 1
+        }
+      })
+      await client.connect()
+      const postgrator = new Migrator(getMigratorConfig(config.Sql.Database, client))
+      // subscribe to useful events
+      postgrator.on('migration-started', migration => logger.info(`executing ${migration.version} ${migration.action}:${migration.name}...`))
+      // Migrate to 'max' version or user-specified e.g. '008'
+      logger.info('Migrating to version: ' + version)
+      // const migrations = await postgrator.getRunnableMigrations(0, 'max')
+      await postgrator.migrate(version)
+      logger.info('SQL Migrations complete')
+      // close the db connection
+      await client.close()
+    } catch (error) {
+      logger.error(error)
+      logger.error(`${error.appliedMigrations.length} migrations were applied.`)
+      error.appliedMigrations.forEach(migration => {
+        logger.error(`- ${migration.name}`)
+      })
+      // close the db connection
+      await client.close()
+      throw error
+    }
+  }
+
+  // Run the migrations
   try {
-    await createDatabaseIfNotExists()
-    const postgrator = new Migrator(migratorConfig)
-    // subscribe to useful events
-    postgrator.on('migration-started', migration => logger.info(`executing ${migration.version} ${migration.action}:${migration.name}...`))
-    // Migrate to 'max' version or user-specified e.g. '008'
-    logger.info('Migrating to version: ' + version)
-    await postgrator.migrate(version)
-    logger.info('SQL Migrations complete')
-  } catch (error) {
-    logger.error(error)
-    logger.error(`${error.appliedMigrations.length} migrations were applied.`)
-    error.appliedMigrations.forEach(migration => {
-      logger.error(`- ${migration.name}`)
-    })
-    throw error
-  }
-}
-
-runMigrations(process.argv[2] || 'max')
-  .then(() => {
+    await runMigrations(process.argv[2] || 'max')
     logger.info('Done')
     process.exit(0)
-  })
-  .catch(() => {
+  } catch (error) {
     logger.alert('Migrations Failed')
     process.exit(1)
-  })
+  }
+}
+
+main()
