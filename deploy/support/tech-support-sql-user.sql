b8731c30234800296c68a0ad0f0ad5a642a9fbe9
Guy Harwood <guy@milkchip.com>
2023-03-08 13:06:54 +0000

    update script with token values|

--- deploy/support/tech-support-sql-user.sql
+++ deploy/support/tech-support-sql-user.sql
@@ -1,19 +1,21 @@
+/*******************
+This script is used in Azure DevOps to create a tech support account in the relevant environments
+Tokens are auto replaced during execution for secure values
+*******************/
 
-DECLARE @techSupportUser NVARCHAR(128) = '<tech_support_user>'
-
-IF USER_ID(@techSupportUser) IS NOT NULL
+IF USER_ID('$(TechSupport.Sql.Username)') IS NOT NULL
   BEGIN
     PRINT 'user already exists'
     RETURN
   END
 
-CREATE USER @techSupportUser WITH PASSWORD ='<password>', DEFAULT_SCHEMA=[mtc_admin];
+CREATE USER $(TechSupport.Sql.Username) WITH PASSWORD ='$(TechSupport.Sql.Password)', DEFAULT_SCHEMA=[mtc_admin];
 
-GRANT CONNECT TO [@techSupportUser] AS [dbo]
+GRANT CONNECT TO $(TechSupport.Sql.Username) AS [dbo]
 GO
 
-GRANT EXECUTE,SELECT ON SCHEMA::[mtc_admin] TO @techSupportUser AS [dbo]
+GRANT EXECUTE,SELECT ON SCHEMA::[mtc_admin] TO $(TechSupport.Sql.Username) AS [dbo]
 GO
 
-GRANT EXECUTE,SELECT ON SCHEMA::[mtc_results] TO @techSupportUser AS [dbo]
+GRANT EXECUTE,SELECT ON SCHEMA::[mtc_results] TO $(TechSupport.Sql.Username) AS [dbo]
 GO
