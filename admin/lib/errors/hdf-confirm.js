3623cdcb49acafd7c9d8b021743d78ceb5346862
Guy Harwood <guy@milkchip.com>
2023-03-16 14:42:57 +0000

    validation and messaging

--- admin/lib/errors/hdf-confirm.js
+++ admin/lib/errors/hdf-confirm.js
@@ -1,5 +1,6 @@
 'use strict'
 
 module.exports = {
-  confirmBoxes: 'All four checkboxes must be selected to proceed with this option'
+  confirmBoxes: 'All four checkboxes must be selected to proceed with this option',
+  noConfirmInfo: 'Information on non confirmation must be provided to proceed with this option'
 }
