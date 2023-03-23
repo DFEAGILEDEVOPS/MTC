a01c9a528b34a1d51a85e9ef03850906f11a6cb2
Guy Harwood <GuyHarwood@users.noreply.github.com>
2023-03-23 10:49:30 +0000

    Feature/58073 tech support user (#2466)
    * create user script
    
    * update script with token values|
    
    * refactor tests and remove duplicate & obsolete
    
    * remove superfluous script
    
    * retire tech support user, introduce sql support
    
    * re-add tech support user
    
    * guard against non existing target objects

--- admin/services/data-access/sql.role-connection.builder.js
+++ admin/services/data-access/sql.role-connection.builder.js
@@ -2,13 +2,13 @@
 
 const roles = require('../../lib/consts/roles')
 const sqlConfig = require('../../config/sql.config')
-const config = require('../../config')
 const R = require('ramda')
 const deepFreeze = require('../../lib/deep-freeze')
+const config = require('../../config')
 
 const builder = {
   /**
-   * @description builds a sql connection config object
+   * @description builds a sql connection config object based upon the users role
    * @param {string} roleName - the role of the user to create a connection config for
    * @param {boolean} readonly - set to true for a readonly connection, typically to a sql azure replica
    * @returns {object} a config object that works with mssql
@@ -25,7 +25,7 @@ const builder = {
         cfg.pool.max = config.Sql.TechSupport.Pool.Max
         break
       default:
-        throw new Error('role not supported')
+        throw new Error(`role '${roleName}' not yet supported in connection builder`)
     }
     cfg.options.readOnlyIntent = readonly
     return deepFreeze(cfg)
