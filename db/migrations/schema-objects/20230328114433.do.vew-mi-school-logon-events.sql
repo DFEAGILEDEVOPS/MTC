04c88a9741a86e589a02625f2464aceb1ee9733e
Guy Harwood <guy@milkchip.com>
2023-03-28 15:52:07 +0100

    remove unused output

--- db/migrations/schema-objects/20230328114433.do.vew-mi-school-logon-events.sql
+++ db/migrations/schema-objects/20230328114433.do.vew-mi-school-logon-events.sql
@@ -3,7 +3,6 @@ CREATE OR ALTER VIEW mtc_admin.[vewMiSchoolLogonEvents] AS
     ale.loginMethod,
     ale.user_id,
     ale.school_id,
-    JSON_VALUE(ale.body, '$.organisation.urn') as [urn],
     JSON_VALUE(ale.body, '$.role') as [role]
      FROM mtc_admin.adminLogonEvent ale
      WHERE ale.isAuthenticated = 1
