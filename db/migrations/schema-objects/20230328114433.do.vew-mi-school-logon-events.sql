cac2ddb59833bb96222c533183c09bebb44ba1c3
Guy Harwood <guy@milkchip.com>
2023-04-04 11:43:43 +0100

    views

--- db/migrations/schema-objects/20230328114433.do.vew-mi-school-logon-events.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR ALTER VIEW mtc_admin.[vewMiSchoolLogonEvents] AS
-  SELECT ale.createdAt as [loggedOnAt],
-    ale.loginMethod,
-    ale.user_id,
-    ale.school_id,
-    JSON_VALUE(ale.body, '$.role') as [role],
-    JSON_VALUE(ale.body, '$.organisation.urn') as [urn]
-     FROM mtc_admin.adminLogonEvent ale
-     WHERE ale.isAuthenticated = 1
--- db/migrations/schema-objects/20230328115415.do.vew-mi-schools-logged-on-at-least-once.sql
+++
@@ -1,4 +0,0 @@
-CREATE OR ALTER VIEW mtc_admin.[vewMiSchoolsLoggedOnAtLeastOnce] AS
-  SELECT COUNT(DISTINCT sle.urn) as [numberOfSchoolsAccessedService]
-    FROM [mtc_admin].[vewMiSchoolLogonEvents] sle
-    WHERE sle.role = 'TEACHER'
