3bc57beba8cf8693f00d3ca5da4b3aa41230701e
Jon Shipley <js@ovix.co.uk>
2023-04-18 11:16:07 +0000

    lint fixes

--- pupil-spa/src/app/page-not-found/page-not-found.component.ts
+++ pupil-spa/src/app/page-not-found/page-not-found.component.ts
@@ -5,11 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss']
 })
-export class PageNotFoundComponent implements OnInit {
-
+export class PageNotFoundComponent {
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
