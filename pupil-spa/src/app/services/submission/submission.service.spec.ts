483f4a0dc090d1cf3434694f06db80aa04fc70f0
Guy Harwood <guy@milkchip.com>
2023-09-28 09:09:36 +0100

    work up the submission specs

--- pupil-spa/src/app/services/submission/submission.service.spec.ts
+++ pupil-spa/src/app/services/submission/submission.service.spec.ts
@@ -2,7 +2,8 @@ import { TestBed } from '@angular/core/testing'
 import { HttpService } from '../http/http.service'
 import { SubmissionService } from './submission.service'
 import { APP_INITIALIZER } from '@angular/core'
-import { APP_CONFIG, IAppConfig, loadConfigMockService } from '../config/config.service'
+import { APP_CONFIG, loadConfigMockService } from '../config/config.service'
+import * as exp from 'constants'
 
 describe('submission service', () => {
 
@@ -23,6 +24,17 @@ describe('submission service', () => {
     .compileComponents()
     sut = TestBed.inject(SubmissionService)
   })
+
+  it('should post with valid jwt auth header', async () => {
+    const jwt = 'my-jwt-token'
+    httpServiceSpy.post.and.callFake((url: string, payload: any, headers: any) => {
+      expect(headers.get('Authorization')).toEqual(`Bearer ${jwt}`)
+    })
+    await sut.submit('payload', 'url', jwt)
+    expect(httpServiceSpy.post).toHaveBeenCalled()
+  })
+
+  xit('todo: verify content type')
 
   it('original error should bubble up when submission fails', async () => {
     httpServiceSpy.post.and.throwError(new Error('submission-failed'))
