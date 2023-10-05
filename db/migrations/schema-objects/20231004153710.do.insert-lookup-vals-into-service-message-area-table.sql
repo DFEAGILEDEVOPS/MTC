da488f55fe8922048c9e8b4060dc06a87d33ce35
Jon Shipley <js@ovix.co.uk>
2023-10-05 11:47:03 +0000

    db changes for area-specific system message

---
+++ db/migrations/schema-objects/20231004153710.do.insert-lookup-vals-into-service-message-area-table.sql
@@ -0,0 +1,9 @@
+INSERT INTO mtc_admin.serviceMessageAreaLookup (code, description) VALUES
+('A', 'Access Arrangements'),
+('H', 'Headteacher''s declaration'),
+('N', 'Pupils not taking the check (non-sitting code)'),
+('P', 'Pin generation'),
+('G', 'Pupil groups'),
+('S', 'Pupil status'),
+('R', 'Restarts')
+;
