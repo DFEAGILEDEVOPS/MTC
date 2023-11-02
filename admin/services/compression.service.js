f3af8e025f0dcdf9359413f73e5302e9f1eb40c8
Guy Harwood <guy@milkchip.com>
2023-11-02 13:15:25 +0000

    support version 3 payload in tech support

--- admin/services/compression.service.js
+++ admin/services/compression.service.js
@@ -4,21 +4,39 @@ const lzString = require('lz-string')
 
 const moduleToExport = {
   /**
-   * Compress a string
+   * Compress a string using UTF-16
    * @param {string} string
    * @return {*|string}
    */
-  compress: function compress (string) {
+  compressToUTF16: function compress (string) {
     return lzString.compressToUTF16(string)
   },
 
   /**
-   * Decompress a string
+   * Decompress a string from UTF-16
    * @param {string} string
    * @return {*|string|(string|null|undefined)}
    */
-  decompress: function decompress (string) {
+  decompressFromUTF16: function decompress (string) {
     return lzString.decompressFromUTF16(string)
+  },
+
+  /**
+   * Compress a string using Base64
+   * @param {string} string
+   * @return {*|string}
+   */
+  compressToBase64: function compress (string) {
+    return lzString.compressToBase64(string)
+  },
+
+  /**
+   * Decompress a string from Base64
+   * @param {string} string
+   * @return {*|string|(string|null|undefined)}
+   */
+  decompressFromBase64: function decompress (string) {
+    return lzString.decompressFromBase64(string)
   }
 }
 
--- admin/spec/back-end/service/payload.service.spec.js
+++ admin/spec/back-end/service/payload.service.spec.js
@@ -3,6 +3,8 @@
 const R = require('ramda')
 const service = require('../../../services/payload.service')
 const payloadDataService = require('../../../services/data-access/payload.data.service')
+const compressionService = require('../../../services/compression.service')
+const { compress } = require('lz-string')
 const mockCheckCode = '5d557d6a-b1d9-406e-b5d0-7c26e5f2bdb5'
 describe('payload.service', () => {
   afterEach(() => {
@@ -111,6 +113,24 @@ describe('payload.service', () => {
       }
       service.addRelativeTimings(mock)
       expect(service.addRelativeTimingsToSection).toHaveBeenCalledTimes(2)
+    })
+  })
+
+  describe('base64 compressed payloads', () => {
+    test('uses base64 decompression when check is version 3', async () => {
+      const archiveContents = 'contents-of-archive'
+      jest.spyOn(payloadDataService, 'sqlFindOneByCheckCode').mockResolvedValue({
+        inputs: [],
+        audit: [],
+        archive: archiveContents,
+        checkVersion: 3
+      })
+      jest.spyOn(compressionService, 'decompressFromBase64').mockReturnValueOnce(JSON.stringify({
+        inputs: [],
+        audit: []
+      }))
+      await service.getPayload(mockCheckCode)
+      expect(compressionService.decompressFromBase64).toHaveBeenCalledWith(archiveContents)
     })
   })
 })
