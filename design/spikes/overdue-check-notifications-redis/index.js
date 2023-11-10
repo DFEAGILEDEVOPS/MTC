6cccc1c69063b1d979e6779761cc84b0e1f3cafd
Guy Harwood <guy@milkchip.com>
2023-11-10 11:26:25 +0000

    tidy up and add fetching existing from redis

--- design/spikes/overdue-check-notifications-redis/index.js
+++ design/spikes/overdue-check-notifications-redis/index.js
@@ -1,5 +1,9 @@
+'use strict'
+
+require('dotenv').config()
 const Redis = require('ioredis')
-const redis = new Redis()
+const config = require('./config')
+const redis = new Redis(config)
 const R = require('ramda')
 const pipeline = redis.pipeline()
 const perf = require('perf_hooks').performance
@@ -14,12 +18,12 @@ let addNewOverdueChecksTimerEnd
 let setWorkTimerStart
 let setWorkTimerEnd
 
-// TODO connect to azure redis
+// TODO work with sets?
 
 const keyPrefix = 'school.home.overdue'
 const schoolCount = 150000
 
-function clearRedis (cb) {
+function clearRedis (done) {
   console.log('purging redis...')
   var stream = redis.scanStream({
     match: `${keyPrefix}:*`
@@ -39,11 +43,28 @@ function clearRedis (cb) {
   stream.on('end', function () {
     const average = keyLengths.reduce((a, b) => a + b, 0) / keyLengths.length
     console.log('redis purged - average key batch length: ', average.toFixed(0))
-    cb()
+    done()
   })
 }
 
-function randomIntArrayInRange (min, max, length = 1) {
+function fetchExistingOverdueSchools (items, done) {
+  fetchExistingOverdueSchoolsTimerStart = perf.now()
+  var stream = redis.scanStream({
+    match: `${keyPrefix}:*`
+  })
+
+  stream.on('data', function (keys) {
+    if (keys.length) {
+      items.push(...keys)
+    }
+  })
+  stream.on('end', function () {
+    fetchExistingOverdueSchoolsTimerEnd = perf.now()
+    done()
+  })
+}
+
+function generateArrayOfRandomNumbers (min, max, length = 1) {
   return Array.from(
     { length},
     () => Math.floor(Math.random() * (max - min + 1)) + min
@@ -51,54 +72,60 @@ function randomIntArrayInRange (min, max, length = 1) {
 }
 
 function outputTimings () {
-  console.log(`fetch existing from redis: ${((fetchExistingOverdueSchoolsTimerEnd - fetchExistingOverdueSchoolsTimerStart)/1000).toFixed(2)}s`)
-  console.log(`add new to redis: ${((addNewOverdueChecksTimerEnd - addNewOverdueChecksTimerStart)/1000).toFixed(2)}s`)
-  console.log(`set work duration: ${((setWorkTimerEnd - setWorkTimerStart)/1000).toFixed(2)}s`)
-  console.log(`overall duration: ${((overallTimerEnd - overallTimerStart)/1000).toFixed(2)}s`)
+  const msToSecs = (ms) => (ms/1000).toFixed(2)
+  const fetchExistingMs = fetchExistingOverdueSchoolsTimerEnd - fetchExistingOverdueSchoolsTimerStart
+  const addNewMs = addNewOverdueChecksTimerEnd - addNewOverdueChecksTimerStart
+  const setWorkMs = setWorkTimerEnd - setWorkTimerStart
+  const overallMs = overallTimerEnd - overallTimerStart
+  console.log(`fetch existing from redis: ${fetchExistingMs.toFixed(2)}ms (${msToSecs(fetchExistingMs)}s)`)
+  console.log(`add new to redis: ${addNewMs.toFixed(2)}ms (${msToSecs(addNewMs)}s)`)
+  console.log(`set work duration: ${setWorkMs.toFixed(2)}ms (${msToSecs(setWorkMs)}s)`)
+  console.log(`overall duration: ${overallMs.toFixed(2)}ms (${msToSecs(overallMs)}s)`)
 }
 
 function runDataExercise () {
   clearRedis(() => {
     console.log('starting exercise...')
     // datasets
-    const oldOverdueSchools = randomIntArrayInRange(1, schoolCount * 2, schoolCount)
-    const currentOverdueSchools = randomIntArrayInRange(1, schoolCount * 2, schoolCount)
+    const oldOverdueSchools = generateArrayOfRandomNumbers(1, schoolCount * 2, schoolCount)
+    const currentOverdueSchools = generateArrayOfRandomNumbers(1, schoolCount * 2, schoolCount)
 
-    // add oldOverdueChecks to redis (dont measure this)
+    // add oldOverdueChecks to redis - dont measure this
     oldOverdueSchools.forEach((schoolUuid) => {
       pipeline.set(`${keyPrefix}:${schoolUuid}`, 'old')
     })
     pipeline.exec()
 
-    // TODO fetch existing overdue checks first
-    fetchExistingOverdueSchoolsTimerStart = NaN
-    fetchExistingOverdueSchoolsTimerEnd = NaN
-
     console.log(`configured school count: ${schoolCount}`)
     setWorkTimerStart = perf.now()
-    const noLongerOverdue = R.difference(oldOverdueSchools, currentOverdueSchools)
-    const stillOverdue = R.difference(oldOverdueSchools, noLongerOverdue)
-    const newOverdue = R.difference(currentOverdueSchools, stillOverdue)
-    setWorkTimerEnd = perf.now()
-    console.log(`noLongerOverdue: ${noLongerOverdue.length}`)
-    console.log(`stillOverdue: ${stillOverdue.length}`)
-    console.log(`newOverdue: ${newOverdue.length}`)
+    const existingOverdueChecks = []
 
-    // remove noLongerOverdue from redis
-    noLongerOverdue.forEach((schoolUuid) => {
-      pipeline.del(`${keyPrefix}:${schoolUuid}`)
-    })
-    pipeline.exec()
+    fetchExistingOverdueSchools(existingOverdueChecks, () => {
+      console.dir(existingOverdueChecks[0])
+      const noLongerOverdue = R.difference(oldOverdueSchools, currentOverdueSchools)
+      const stillOverdue = R.difference(oldOverdueSchools, noLongerOverdue)
+      const newOverdue = R.difference(currentOverdueSchools, stillOverdue)
+      setWorkTimerEnd = perf.now()
+      console.log(`noLongerOverdue: ${noLongerOverdue.length}`)
+      console.log(`stillOverdue: ${stillOverdue.length}`)
+      console.log(`newOverdue: ${newOverdue.length}`)
 
-    // add newOverdue to redis
-    addNewOverdueChecksTimerStart = perf.now()
-    newOverdue.forEach((schoolUuid) => {
-      pipeline.set(`${keyPrefix}:${schoolUuid}`, 'new')
+      // remove noLongerOverdue from redis
+      noLongerOverdue.forEach((schoolUuid) => {
+        pipeline.del(`${keyPrefix}:${schoolUuid}`)
+      })
+      pipeline.exec()
+
+      // add newOverdue to redis
+      addNewOverdueChecksTimerStart = perf.now()
+      newOverdue.forEach((schoolUuid) => {
+        pipeline.set(`${keyPrefix}:${schoolUuid}`, 'new')
+      })
+      pipeline.exec()
+      addNewOverdueChecksTimerEnd = perf.now()
+      overallTimerEnd = perf.now()
+      outputTimings()
     })
-    pipeline.exec()
-    addNewOverdueChecksTimerEnd = perf.now()
-    overallTimerEnd = perf.now()
-    outputTimings()
   })
 }
 
