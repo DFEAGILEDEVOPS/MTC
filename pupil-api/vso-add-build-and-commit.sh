e87296231233f1e202b3c54eca24a949dc225e3e
Guy Harwood <guy@milkchip.com>
2023-12-07 16:55:44 +0000

    locate from dist folder

--- pupil-api/vso-add-build-and-commit.sh
+++ pupil-api/vso-add-build-and-commit.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 mkdir -p dist
-BUILD_FILE="./build.txt"
+BUILD_FILE="./dist/build.txt"
 touch $BUILD_FILE
 echo "$BUILD_BUILDNUMBER" | tr '\n' ' ' > "$BUILD_FILE"
 cat $BUILD_FILE
 
-COMMIT_FILE="./commit.txt"
+COMMIT_FILE="./dist/commit.txt"
 touch $COMMIT_FILE
-echo "$BUILD_SOURCEVERSION" | tr '\n' ' ' > $COMMIT_FILE
+echo "$BUILD_SOURCEVERSION" | tr '\n' ' ' > "$COMMIT_FILE"
 cat $COMMIT_FILE
