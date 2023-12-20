07f370d9857c4e2efa3f10df56de399fa9cefc19
Jon Shipley <js@ovix.co.uk>
2023-12-20 16:45:20 +0000

    exploratory work

--- tslib/src/functions-ps-report/ps-report-4-writer/index.ts
+++ tslib/src/functions-ps-report/ps-report-4-writer/index.ts
@@ -5,9 +5,17 @@ const blobTrigger: AzureFunction = async function (context: Context, blob: any):
   const start = performance.now()
   const funcName = 'ps-report-4-writer'
   context.log(`${funcName}: new blob detected: ${context.bindingData.uri}`)
+  context.log(`${funcName}: bindingData: ` + JSON.stringify(context.bindingData))
+  context.log(`Blob ${blob}`)
+  await bulkUpload(context)
   const end = performance.now()
   const durationInMilliseconds = end - start
   context.log(`${funcName} complete:run took ${durationInMilliseconds} ms`)
+}
+
+async function bulkUpload (context: Context): Promise<void> {
+  const baseName = context.bindingData.uri.substring(context.bindingData.uri.lastIndexOf('/') + 1)
+  context.log(`baseName ${baseName}`)
 }
 
 export default blobTrigger
