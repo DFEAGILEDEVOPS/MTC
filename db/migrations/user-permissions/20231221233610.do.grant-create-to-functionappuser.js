10546a3057821d5a1c818310a69a760271f157a3
Jon Shipley <js@ovix.co.uk>
2023-12-22 18:10:36 +0000

    Make the functions system user the mtc_results schema owner so it can create tables.

--- db/migrations/user-permissions/20231221233610.do.grant-create-to-functionappuser.js
+++ db/migrations/user-permissions/20231221233610.do.grant-create-to-functionappuser.js
@@ -2,5 +2,8 @@
 
 const config = require('../../config')
 module.exports.generateSql = function () {
-  return `GRANT CREATE TABLE TO [${config.Sql.FunctionsApp.Username}]`
+  return `
+    ALTER AUTHORIZATION ON SCHEMA::mtc_results to  [${config.Sql.FunctionsApp.Username}];
+    GRANT CREATE TABLE TO [${config.Sql.FunctionsApp.Username}]
+  `
 }
--- db/migrations/user-permissions/20231221233610.undo.grant-create-to-functionappuser.js
+++ db/migrations/user-permissions/20231221233610.undo.grant-create-to-functionappuser.js
@@ -2,5 +2,8 @@
 
 const config = require('../../config')
 module.exports.generateSql = function () {
-  return `REVOKE CREATE TABLE TO [${config.Sql.FunctionsApp.Username}]`
+  return `
+    -- ALTER AUTHORIZATION ON SCHEMA::mtc_results to  [WHO? dbo?];
+    REVOKE CREATE TABLE TO [${config.Sql.FunctionsApp.Username}]
+  `
 }
