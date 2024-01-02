7330caf1312dfa2e1e1f345062caa0ed3098fc88
Jon Shipley <js@ovix.co.uk>
2024-01-02 14:21:34 +0000

    debug

--- db/migrations/user-permissions/20231221233610.do.grant-create-to-functionappuser.js
+++ db/migrations/user-permissions/20231221233610.do.grant-create-to-functionappuser.js
@@ -6,7 +6,7 @@ module.exports.generateSql = function () {
     -- Give ownership of the schema to the Functions user so they can create tables and views securely.
     ALTER AUTHORIZATION ON SCHEMA::mtc_results to  [${config.Sql.FunctionsApp.Username}];
     -- Give permission for the migrator user to do anything.
-    GRANT CONTROL ON SCHEMA::mtc_results to [${config.Sql.Migrator.Username}];
+    -- GRANT CONTROL ON SCHEMA::mtc_results to [${config.Sql.Migrator.Username}];
     -- Still need to grant specific privileges to the Functions user.
     GRANT CREATE TABLE TO [${config.Sql.FunctionsApp.Username}]
   `
