b152d12d3bf9b5925ad5541d048f43369af01534
Jon Shipley <js@ovix.co.uk>
2024-01-02 13:58:51 +0000

    Db permissions again.

--- db/migrations/user-permissions/20231221233610.do.grant-create-to-functionappuser.js
+++ db/migrations/user-permissions/20231221233610.do.grant-create-to-functionappuser.js
@@ -3,7 +3,11 @@
 const config = require('../../config')
 module.exports.generateSql = function () {
   return `
-    ALTER AUTHORIZATION ON SCHEMA::mtc_results to  [${config.Sql.FunctionsApp.Username}] WITH GRANT OPTION;
+    -- Give ownership of the schema to the Functions user so they can create tables and views securely.
+    ALTER AUTHORIZATION ON SCHEMA::mtc_results to  [${config.Sql.FunctionsApp.Username}];
+    -- Give permission for the migrator user to do anything.
+    GRANT CONTROL ON SCHEMA::mtc_results to [${config.Sql.Migrator.Username}];
+    -- Still need to grant specific privileges to the Functions user.
     GRANT CREATE TABLE TO [${config.Sql.FunctionsApp.Username}]
   `
 }
--- db/migrations/user-permissions/20231221233610.undo.grant-create-to-functionappuser.js
+++ db/migrations/user-permissions/20231221233610.undo.grant-create-to-functionappuser.js
@@ -3,7 +3,9 @@
 const config = require('../../config')
 module.exports.generateSql = function () {
   return `
-    ALTER AUTHORIZATION ON SCHEMA::mtc_results to [${config.Sql.Migrator.Username}] WITH GRANT OPTION;
+    -- The Migrations are run by the migrations user, which cannot give AUTHORISATION ownership of the mtc_results
+    -- schema back to the Migration user due the runtime context.  To work around this the migration user is granted CONTROL
+    -- permissions in the UP migration.
     REVOKE CREATE TABLE TO [${config.Sql.FunctionsApp.Username}]
   `
 }
