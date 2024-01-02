76e4c79d075eb85dc8d968aa31d850c3d4c0d4d8
Jon Shipley <js@ovix.co.uk>
2024-01-02 13:27:51 +0000

    Perm update

--- db/migrations/user-permissions/20231221233610.do.grant-create-to-functionappuser.js
+++ db/migrations/user-permissions/20231221233610.do.grant-create-to-functionappuser.js
@@ -3,7 +3,7 @@
 const config = require('../../config')
 module.exports.generateSql = function () {
   return `
-    ALTER AUTHORIZATION ON SCHEMA::mtc_results to  [${config.Sql.FunctionsApp.Username}];
+    ALTER AUTHORIZATION ON SCHEMA::mtc_results to  [${config.Sql.FunctionsApp.Username}] WITH GRANT OPTION;
     GRANT CREATE TABLE TO [${config.Sql.FunctionsApp.Username}]
   `
 }
--- db/migrations/user-permissions/20231221233610.undo.grant-create-to-functionappuser.js
+++ db/migrations/user-permissions/20231221233610.undo.grant-create-to-functionappuser.js
@@ -3,7 +3,7 @@
 const config = require('../../config')
 module.exports.generateSql = function () {
   return `
-    ALTER AUTHORIZATION ON SCHEMA::mtc_results to [${config.Sql.Migrator.Username}];
+    ALTER AUTHORIZATION ON SCHEMA::mtc_results to [${config.Sql.Migrator.Username}] WITH GRANT OPTION;
     REVOKE CREATE TABLE TO [${config.Sql.FunctionsApp.Username}]
   `
 }
