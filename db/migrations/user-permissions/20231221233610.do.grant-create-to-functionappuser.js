763cf17ccc0f2f9a5120b35de8a282763050bf5c
Jon Shipley <js@ovix.co.uk>
2024-01-12 13:57:47 +0000

    Grant bulk upload perms when on sql azure

--- db/migrations/user-permissions/20231221233610.do.grant-create-to-functionappuser.js
+++ db/migrations/user-permissions/20231221233610.do.grant-create-to-functionappuser.js
@@ -2,10 +2,16 @@
 
 const config = require('../../config')
 module.exports.generateSql = function () {
-  return `
-    GRANT CONTROL ON DATABASE :: [${config.Sql.Database}] TO [${config.Sql.FunctionsApp.Username}];
-    -- GRANT ALTER on schema::mtc_results to [${config.Sql.FunctionsApp.Username}];
-    -- GRANT CREATE TABLE TO [${config.Sql.FunctionsApp.Username}];
-    -- GRANT CREATE VIEW TO [${config.Sql.FunctionsApp.Username}];
-  `
+  const statements = [
+    `GRANT CONTROL ON DATABASE :: [${config.Sql.Database}] TO [${config.Sql.FunctionsApp.Username}];`,
+    ` -- GRANT ALTER on schema::mtc_results to [${config.Sql.FunctionsApp.Username}];`,
+    `  -- GRANT INSERT on schema::mtc_results to [${config.Sql.FunctionsApp.Username}];`
+  ]
+
+  // If we are on SQL Azure and not Linux then we need to grant BULK permissions
+  if (config.Sql.Azure.Scale) {
+    statements.push(`GRANT ADMINISTER DATABASE BULK OPERATIONS ON DATABASE :: [${config.Sql.Database}] TO [${config.Sql.FunctionsApp.Username}];`)
+  }
+
+  return statements.join('\n')
 }
