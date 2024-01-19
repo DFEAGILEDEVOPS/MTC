7ccf3753bb0034e05f1ad450162a95853eb78939
Guy Harwood <guy@milkchip.com>
2024-01-19 16:18:42 +0000

    align params and fix line continuations

--- load-test/exec-flood.sh
+++ load-test/exec-flood.sh
@@ -2,46 +2,52 @@
 set -e
 
 # name of stream to run
-FLOOD_NAME=${1:AzureDevOps_Pipeline}
+FLOOD_NAME=${1:CICD_PinGen_LiveCheck_Submit}
 # Flood Api auth token
 FLOOD_API_TOKEN=$2
 # admin app url
-ADMIN_APP_HOST=$3
+ADMIN_APP_DOMAIN=$3
+# admin app port
+ADMIN_APP_PORT=${4:-443}
 # pupil api url
-PUPIL_API_HOST=$4
+PUPIL_API_DOMAIN=$5
 # pupil api port
-# pupil spa host (for CORS on pupil api)
-PUPIL_SPA_HOST=$5
+PUPIL_API_PORT=${6:-443}
+# storage account queue domain for posting messages
+STORAGE_ACCOUNT_QUEUE_DOMAIN=$7
 # check submit proxy function host
-CHECK_SUBMIT_HOST=$6
-# check submit proxy auth token
-CHECK_SUBMIT_AUTH_TOKEN=$7
-# web service protocol
-WEB_PROTOCOL=${8:-https}
-# web service port
-WEB_PORT=${9:-443}
+FUNC_CONS_DOMAIN=$8
+# func cons port
+FUNC_CONS_PORT=${9:-7071}
+# auth key to execute functions
+FUNC_CONS_AUTH_TOKEN=$10
+# thread count
+THREAD_COUNT=${11:-5}
+# project name
+PROJECT_NAME=${12:-MTC}
 
-# Check we have the jq binary to make parsing JSON responses a bit easier
+# Check we have the jq binary to make parsing JSON responses a little easier
 command -v jq >/dev/null 2>&1 || \
 { echo >&2 "Please install http://stedolan.github.io/jq/download/  Aborting."; exit 1; }
 
 echo
 echo "preparing to execute $FLOOD_NAME"
-echo "admin app:${WEB_PROTOCOL}://${ADMIN_APP_HOST}:${WEB_PORT}"
-echo "pupil api host:${WEB_PROTOCOL}://${PUPIL_API_HOST}:${WEB_PORT}"
-echo "pupil spa (for api cors):${WEB_PROTOCOL}://$PUPIL_SPA_HOST}:${WEB_PORT}"
-echo "check submit host:${WEB_PROTOCOL}://${CHECK_SUBMIT_HOST}:${WEB_PORT}"
+echo "admin app:https://${ADMIN_APP_DOMAIN}"
+echo "pupil api host:https://${PUPIL_API_DOMAIN}"
+echo "pupil spa (for api cors):https://$PUPIL_SPA_DOMAIN}"
+echo "consumption functions host:https://${FUNC_CONS_DOMAIN}:${FUNC_CONS_PORT}"
 
 # Start a flood
 echo
 echo "[$(date +%FT%T)+00:00] Starting flood"
 flood_uuid=$(curl -u $FLOOD_API_TOKEN: -X POST https://api.flood.io/floods \
 -F "flood[tool]=jmeter" \
--F "flood[threads]=20" \
--F "flood[project]=MTC" \
+-F "flood[threads]=$THREAD_COUNT" \
+-F "flood[project]=$PROJECT_NAME" \
 -F "flood[name]=$FLOOD_NAME" \
--F "flood_files[]=@./scenarios/_2020/live-long-teacher-journey-with-check-submit.jmx" \
--F "flood[override_parameters]=-JadminAppHost=${ADMIN_APP_HOST}, -Jprotocol=${WEB_PROTOCOL}, -Jport=${WEB_PORT}, -JpupilApiHost=${PUPIL_API_HOST}, -JproxyFunctionHost=${CHECK_SUBMIT_HOST}, -JpupilSiteUrl=${PUPIL_SPA_HOST}" \
+-F "flood_files[]=@./scenarios/live-check-journey.jmx" \
+-F "flood[override_parameters]=-JadminAppHost=${ADMIN_APP_DOMAIN}, -Jprotocol=https, -Jport=${ADMIN_APP_PORT}, -JpupilApiHost=${PUPIL_API_DOMAIN}," \
+" -JpupilApiPort=${PUPIL_API_PORT}, -JFuncConsHost=${FUNC_CONS_DOMAIN}, -JFuncConsPort=${FUNC_CONS_PORT}, -storageAccountHost=${STORAGE_ACCOUNT_QUEUE_DOMAIN}" \
 -F "flood[grids][][infrastructure]=demand" \
 -F "flood[grids][][instance_quantity]=1" \
 -F "flood[grids][][region]=uk-south-london" \
