6a5bebab4c5af4eeba443c5da6bebd8e7436c441
Guy Harwood <guy@milkchip.com>
2024-01-19 16:43:07 +0000

    wont run due to curl error 'malformed url'

--- load-test/exec-flood.sh
+++ load-test/exec-flood.sh
@@ -2,29 +2,25 @@
 set -e
 
 # name of stream to run
-FLOOD_NAME=${1:CICD_PinGen_LiveCheck_Submit}
+FLOOD_NAME=$1
 # Flood Api auth token
 FLOOD_API_TOKEN=$2
 # admin app url
 ADMIN_APP_DOMAIN=$3
-# admin app port
-ADMIN_APP_PORT=${4:-443}
 # pupil api url
-PUPIL_API_DOMAIN=$5
-# pupil api port
-PUPIL_API_PORT=${6:-443}
+PUPIL_API_DOMAIN=$4
 # storage account queue domain for posting messages
-STORAGE_ACCOUNT_QUEUE_DOMAIN=$7
+STORAGE_ACCOUNT_QUEUE_DOMAIN=$5
 # check submit proxy function host
-FUNC_CONS_DOMAIN=$8
+FUNC_CONS_DOMAIN=$6
 # func cons port
-FUNC_CONS_PORT=${9:-7071}
+FUNC_CONS_PORT=$7
 # auth key to execute functions
-FUNC_CONS_AUTH_TOKEN=$10
+FUNC_CONS_AUTH_TOKEN=$8
 # thread count
-THREAD_COUNT=${11:-5}
+THREAD_COUNT=${9:-5}
 # project name
-PROJECT_NAME=${12:-MTC}
+PROJECT_NAME=${10:-MTC}
 
 # Check we have the jq binary to make parsing JSON responses a little easier
 command -v jq >/dev/null 2>&1 || \
@@ -34,7 +30,6 @@ echo
 echo "preparing to execute $FLOOD_NAME"
 echo "admin app:https://${ADMIN_APP_DOMAIN}"
 echo "pupil api host:https://${PUPIL_API_DOMAIN}"
-echo "pupil spa (for api cors):https://$PUPIL_SPA_DOMAIN}"
 echo "consumption functions host:https://${FUNC_CONS_DOMAIN}:${FUNC_CONS_PORT}"
 
 # Start a flood
@@ -46,12 +41,10 @@ flood_uuid=$(curl -u $FLOOD_API_TOKEN: -X POST https://api.flood.io/floods \
 -F "flood[project]=$PROJECT_NAME" \
 -F "flood[name]=$FLOOD_NAME" \
 -F "flood_files[]=@./scenarios/live-check-journey.jmx" \
--F "flood[override_parameters]=-JadminAppHost=${ADMIN_APP_DOMAIN}, -Jprotocol=https, -Jport=${ADMIN_APP_PORT}, -JpupilApiHost=${PUPIL_API_DOMAIN}," \
-" -JpupilApiPort=${PUPIL_API_PORT}, -JFuncConsHost=${FUNC_CONS_DOMAIN}, -JFuncConsPort=${FUNC_CONS_PORT}, -storageAccountHost=${STORAGE_ACCOUNT_QUEUE_DOMAIN}" \
--F "flood[grids][][infrastructure]=demand" \
--F "flood[grids][][instance_quantity]=1" \
--F "flood[grids][][region]=uk-south-london" \
--F "flood[grids][][instance_type]=m5.xlarge" \
+-F "flood[override_parameters]=-JadminAppHost=${ADMIN_APP_DOMAIN}, -Jprotocol=https, -Jport=443, -JpupilApiHost=${PUPIL_API_DOMAIN}," \
+" -JpupilApiPort=443, -JFuncConsHost=${FUNC_CONS_DOMAIN}, -JFuncConsPort=${FUNC_CONS_PORT}, -storageAccountHost=${STORAGE_ACCOUNT_QUEUE_DOMAIN}" \
+-F "flood[grids][][infrastructure]=hosted" \
+-F "flood[grids][][name]=mtc-grid" \
 -F "flood[grids][][stop_after]=15" | jq -r ".uuid")
 
 # Wait for flood to finish
