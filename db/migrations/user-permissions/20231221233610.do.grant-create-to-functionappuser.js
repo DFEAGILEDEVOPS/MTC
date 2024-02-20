9ea3a3600de0a0f9f9291005550a29507c3fee18
Jon Shipley <js@ovix.co.uk>
2024-02-20 22:41:20 +0000

    Clean up migrations

--- db/migrations/user-permissions/20231221233610.do.grant-create-to-functionappuser.js
+++ db/migrations/user-permissions/20231221233610.do.grant-create-to-functionappuser.js
@@ -3,9 +3,7 @@
 const config = require('../../config')
 module.exports.generateSql = function () {
   const statements = [
-    `GRANT CONTROL ON DATABASE :: [${config.Sql.Database}] TO [${config.Sql.FunctionsApp.Username}];`,
-    ` -- GRANT ALTER on schema::mtc_results to [${config.Sql.FunctionsApp.Username}];`,
-    `  -- GRANT INSERT on schema::mtc_results to [${config.Sql.FunctionsApp.Username}];`
+    `GRANT CONTROL ON DATABASE :: [${config.Sql.Database}] TO [${config.Sql.FunctionsApp.Username}];`
   ]
 
   // If we are on SQL Azure and not Linux then we need to grant BULK permissions
--- db/migrations/user-permissions/20231221233610.undo.grant-create-to-functionappuser.js
+++ db/migrations/user-permissions/20231221233610.undo.grant-create-to-functionappuser.js
@@ -7,8 +7,5 @@ module.exports.generateSql = function () {
     -- schema back to the Migration user due the runtime context.  To work around this the migration user is granted CONTROL
     -- permissions in the UP migration.
     REVOKE CONTROL ON DATABASE :: [${config.Sql.Database}] TO [${config.Sql.FunctionsApp.Username}];
-    -- REVOKE ALTER on schema::mtc_results to [${config.Sql.FunctionsApp.Username}];
-    -- REVOKE CREATE TABLE TO [${config.Sql.FunctionsApp.Username}];
-    -- REVOKE CREATE VIEW TO [${config.Sql.FunctionsApp.Username}];
   `
 }
