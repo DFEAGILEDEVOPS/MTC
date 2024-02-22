7b7fc034e6b6dd318bff85789437f21d6d75a88f
Guy Harwood <guy@milkchip.com>
2024-02-22 15:10:53 +0000

    add default password hash

--- load-test/db-setup/004.create-users.sql
+++ load-test/db-setup/004.create-users.sql
@@ -1,7 +1,7 @@
 INSERT INTO mtc_admin.[user] (identifier, passwordHash, school_id, role_id, displayName)
 SELECT
     'teacher' + CAST(teacherId as nvarchar) as [identifier],
-    'hash' as [passwordHash],
+    '$2a$10$.WsawgZpWSAQVaa6Vz3P1.XO.1YntYJLd6Da5lrXCAkVxhhLpkOHK' as [passwordHash],
     schools.school_id,
     3 as [role_id],
     'teacher' + CAST(teacherId as nvarchar) as [displayName]
