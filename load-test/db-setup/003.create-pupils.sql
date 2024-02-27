eff10d167a0414d8150ef1fa05fc1170a18e8bea
Guy Harwood <guy@milkchip.com>
2024-02-27 12:14:22 +0000

    WIP on cursor for school pupils

--- load-test/db-setup/003.create-pupils.sql
+++ load-test/db-setup/003.create-pupils.sql
@@ -101,6 +101,61 @@ BEGIN
 END
 GO
 
+DECLARE @schoolId INT;
+DECLARE @leaCode INT;
+DECLARE @pupilCount INT = 30;
+
+DECLARE schoolCursor CURSOR FOR
+  SELECT id, leaCode FROM [mtc_admin].school
+
+OPEN schoolCursor
+FETCH NEXT FROM schoolCursor INTO @schoolId, @leaCode
+
+WHILE @@FETCH_STATUS = 0
+  BEGIN
+    --
+    -- INSERT 30 random pupils per school
+    --
+    INSERT INTO [mtc_admin].[pupil]
+        (school_id, foreName, lastName, gender, dateOfBirth, upn)
+    SELECT
+        @schoolId as schoolId,
+        CONCAT('Pupil-', pupilSet.pupilIndex) as foreName,
+        CONCAT('School-', CAST(@schoolId AS VARCHAR)) as lastName,
+        IIF(CRYPT_GEN_RANDOM(1) % 2 = 1, 'M', 'F') as gender,
+            DATEADD(
+            YEAR,
+            -8,        -- subtract 8 years to make the pupil at least 8 years old
+            DATEADD(
+              DAY,
+              - ABS( -- further subtract a random number of days between 0 and 363
+                CHECKSUM(
+                  NEWID()
+                ) % 364
+              ),
+              CONCAT(YEAR(GETDATE()), '-09-01') -- E.g. The date to start our subtractions from: e.g. '2021-09-01' which is the start of the school year
+            )
+          ) as dateOfBirth,
+        	[mtc_admin].[upnCheckLetter](
+            CAST(CONCAT(
+                CAST(@leaCode as VARCHAR),    -- 3 digit lea code
+                FORMAT(ROUND(RAND(CHECKSUM(NEWID())) * 9999, 0), '0000'), -- 4 digit estab code
+                FORMAT(ROUND(RAND(CHECKSUM(NEWID())) * 99, 0), '00'),  -- 2 digit creation year
+                FORMAT(ROUND(RAND(CHECKSUM(NEWID())) * 999, 0), '000') -- 3 digit serial
+            ) AS BIGINT)
+        ) as upn
+    FROM
+      mtc_admin.school s
+    INNER JOIN
+      (SELECT value as [pupilIndex], @schoolId as [schoolId] FROM GENERATE_SERIES(1, @pupilCount)) as pupilSet;
+    ON s.id = pupilSet.schoolId
+  END
+
+CLOSE schoolCursor
+DEALLOCATE schoolCursor
+
+-- END of new code.  delete below when cursor working
+
 --
 -- INSERT 30 random pupils per school
 --
