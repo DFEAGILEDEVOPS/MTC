a4c706dafb4effb2e76ed8e00cc9d41ef475c8f4
Jon Shipley <js@ovix.co.uk>
2024-03-04 14:59:49 +0000

    Drop old psychomricReport table
    It is now created automatically via code

--- db/migrations/schema-objects/20240103120853.do.rename-table-psychometricreport.sql
+++ db/migrations/schema-objects/20240103120853.do.rename-table-psychometricreport.sql
@@ -1,7 +1,1 @@
-IF EXISTS(SELECT *
-            FROM INFORMATION_SCHEMA.TABLES
-           WHERE TABLE_NAME = 'psychometricReport'
-             AND TABLE_SCHEMA = 'mtc_results')
-BEGIN
-    EXEC sp_rename 'mtc_results.psychometricReport', 'psychometricReport_2023'
-END
+DROP TABLE IF EXISTS mtc_results.psychometricReport;
