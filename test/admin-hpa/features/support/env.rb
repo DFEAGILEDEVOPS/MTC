ac828e5733e5f5f424f764ef1c9726f199cc2732
Mohsen Qureshi <m-qureshi@hotmail.com>
2024-03-11 16:32:20 +0000

    Update env.rb

--- test/admin-hpa/features/support/env.rb
+++ test/admin-hpa/features/support/env.rb
@@ -91,6 +91,8 @@ Capybara.register_driver :headless_chrome do |app|
   browser_options.args << '--headless=new'
   browser_options.args << '--disable-gpu'
   browser_options.args << '--no-sandbox'
+  browser_options.args << '--remote-debugging-pipe'
+  browser_options.args << '--disable-dev-shm-usage'
   browser_options.args << '--window-size=1280,1696'
   browser_options.add_preference(:download, directory_upgrade: true,
                                  prompt_for_download: false,
