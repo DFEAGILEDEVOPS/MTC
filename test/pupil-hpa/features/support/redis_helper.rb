7d916a58fdd8f8a241e1cad4be1bec1e09fa1511
Mohsen Qureshi <m-qureshi@hotmail.com>
2024-03-14 14:53:11 +0000

    Update redis_helper.rb

--- test/pupil-hpa/features/support/redis_helper.rb
+++ test/pupil-hpa/features/support/redis_helper.rb
@@ -1,20 +1,20 @@
 class RedisHelper
 
   def self.wait_for_prepared_check(school_password,pin)
-    p school_password, pin
-    found = false
-    begin
-      retries ||= 0
-      sleep 5
-      p 'before prepared check '
-      result = REDIS_CLIENT.get("preparedCheck:#{school_password}:#{pin}")
-      p 'after prepared check'
-      if (result.nil?)
-        fail "Pin not found in redis"
-      end
-    rescue
-      retry if (retries += 1) < 60
-    end
+    # sleep 5
+    # p school_password, pin
+    # found = false
+    # begin
+    #   retries ||= 0
+    #   sleep 5
+    #   p 'before prepared check '
+    #   result = REDIS_CLIENT.get("preparedCheck:#{school_password}:#{pin}")
+    #   if (result.nil?)
+    #     fail "Pin not found in redis"
+    #   end
+    # rescue
+    #   retry if (retries += 1) < 60
+    # end
   end
 
 end
