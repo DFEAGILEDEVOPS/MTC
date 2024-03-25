3dc79aca89599845ff5b28b83b142f20ac1d61ef
Jon Shipley <js@ovix.co.uk>
2024-03-25 19:40:05 +0000

    Create the ps-report-data-upload-container if needed

--- tslib/src/functions-ps-report/ps-report-3b-stage-csv-file/ps-report-staging.data.service.ts
+++ tslib/src/functions-ps-report/ps-report-3b-stage-csv-file/ps-report-staging.data.service.ts
@@ -1,31 +1,32 @@
-import { type AppendBlobClient, BlobServiceClient, type ContainerClient } from '@azure/storage-blob'
+import { BlobServiceClient } from '@azure/storage-blob'
 import type { ILogger } from '../../common/logger'
 import config from '../../config'
 
 export class PsReportStagingDataService {
   private readonly logName = 'PsReportStagingDataService'
   private readonly blobService: BlobServiceClient
-  private readonly containerService: ContainerClient
-  private readonly appendBlobService: AppendBlobClient
   private readonly blobName: string
   private readonly csvLineTerminator = '\r\n'
+  private containerName
 
   constructor (private readonly logger: ILogger, containerName = 'ps-report-bulk-upload', blobName = 'ps-report-staging.csv') {
     if (config.ServiceBus.ConnectionString === undefined) {
       throw new Error('ServiceBus ConnectionString is not defined in the environment')
     }
     this.blobName = blobName
+    this.containerName = containerName
     this.logger.verbose(`${this.logName} constructor entered`)
     this.blobService = BlobServiceClient.fromConnectionString(config.AzureStorage.ConnectionString)
-    this.containerService = this.blobService.getContainerClient(containerName)
-    this.appendBlobService = this.containerService.getAppendBlobClient(blobName)
   }
 
   /**
    * Create a zero sized append block.
    */
   public async createAppendBlock (): Promise<void> {
-    await this.appendBlobService.create()
+    const containerService = this.blobService.getContainerClient(this.containerName)
+    await containerService.createIfNotExists();
+    const appendBlobService = containerService.getAppendBlobClient(this.blobName)
+    await appendBlobService.create()
   }
 
   /**
@@ -37,7 +38,9 @@ export class PsReportStagingDataService {
       if (data.slice(-this.csvLineTerminator.length) !== this.csvLineTerminator) {
         data += this.csvLineTerminator
       }
-      await this.appendBlobService.appendBlock(data, data.length)
+      const containerService = this.blobService.getContainerClient(this.containerName)
+      const appendBlobService = containerService.getAppendBlobClient(this.blobName)
+      await appendBlobService.appendBlock(data, data.length)
     } catch (e: any) {
       this.logger.error(`${this.logName}: Failed to append data to ${this.blobName}\n${e?.message}\n`)
     }
