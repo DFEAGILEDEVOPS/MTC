397607b7636645fa11478c4c950e5b7f1ad9add0
Mohsen Qureshi <activemq@users.noreply.github.com>
2024-03-27 16:21:28 +0000

    Feature/ticket rollup (#2733)
    * Change 'Gender' to 'Sex' in census import
    
    * Update assets version
    
    * Wording change multiple pupil upload
    
    * Update e2e tests
    
    * Pupil SPA login failure copy change
    
    * [spa] dep updates: minor and patch
    
    * Update assets version
    
    * Fix html
    
    * bump deps
    
    * Remove next button information field
    
    * Update assets version
    
    * Remove pupilAge additional info
    
    * Remove column from pupil table
    * Remove pupilAgeReason table
    
    * Update assets version
    
    * remove classDisruptionInfo box
    
    * Remove restart reason Classroom Disruption
    Remove restart reason Further Information
    
    * Update assets version
    
    * Remove input assistant reason
    
    * Add pin expiry info to pin slips
    
    * Pass the timezone for MOD schools
    
    * fix lint
    
    * dependency updates
    
    * Update assets version
    
    * New video links
    
    * Fix bullet point in print pin page
    
    * Update assets version
    
    * Change output tag in CTF file
    
    * Fix test
    
    * Update assets version
    
    * Add tests for availability/middleware.js
    
    * Fix lint
    
    * Modify route guard for pupils add and sta-admin
    
    * Disable the multiple add pupil button for STA-ADMINs
    
    * assets version
    
    * test updates
    
    * api header test fix
    
    * test updates
    
    ---------
    
    Co-authored-by: Jon Shipley <js@ovix.co.uk>
    Co-authored-by: Jon Shipley <jon-shipley@users.noreply.github.com>

--- api_tests/browser-security-tests.sh
+++ api_tests/browser-security-tests.sh
@@ -2,6 +2,6 @@
 set -e
 echo "running browser security tests with tag $2 against site $1"
 export PATH="/home/builduser/.rbenv/shims:${PATH}"
-gem install bundler -v 2.1.4
+gem install bundler -v 2.4.18
 bundle install
 BASE_URL=$1 bundle exec rspec -t $2
