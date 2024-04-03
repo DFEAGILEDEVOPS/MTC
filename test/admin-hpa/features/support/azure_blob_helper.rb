b642e8e8c52116a561c2e25b081136d56542f16e
Guy Harwood <guy@milkchip.com>
2024-04-03 16:00:22 +0100

    instancing of var for unique name on each call

--- test/admin-hpa/features/support/azure_blob_helper.rb
+++ test/admin-hpa/features/support/azure_blob_helper.rb
@@ -4,9 +4,9 @@ class AzureBlobHelper
 
   def self.no_fail_create_container()
     begin
-      unique_container_name = "screenshots-#{SecureRandom.uuid}"
-      AZURE_BLOB_CLIENT.create_container(unique_container_name)
-      unique_container_name
+      @unique_container_name = "screenshots-#{SecureRandom.uuid}"
+      AZURE_BLOB_CLIENT.create_container(@unique_container_name)
+      @unique_container_name
     rescue Azure::Core::Http::HTTPError => e
       abort "unable to create blob container '#{@unique_container_name}'.
         Error:#{@e.message}"
