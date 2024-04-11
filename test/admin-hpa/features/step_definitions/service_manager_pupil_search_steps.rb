15be27c143ed54d48d203567ee2f8ba0ad97f99b
Mohsen Qureshi <m-qureshi@hotmail.com>
2024-04-11 13:53:39 +0100

    test updates

--- test/admin-hpa/features/step_definitions/service_manager_pupil_search_steps.rb
+++ test/admin-hpa/features/step_definitions/service_manager_pupil_search_steps.rb
@@ -39,7 +39,7 @@ Given(/^I have (\d+) pupils with the same upns but at different schools$/) do |a
   @urn = SqlDbHelper.get_schools_list.map {|school| school['urn']}.sort.last + 1
   dfe_number = create_dfe_number
   @school_name = "Test School - #{@urn}"
-  @school = FunctionsHelper.create_school(dfe_number[:lea_code],dfe_number[:estab_code], @school_name, @urn)
+  @school = FunctionsHelper.create_school(dfe_number[:lea_code],dfe_number[:estab_code], @school_name, @urn)['entity']
   if @school['result'] == 'Failed'
     fail "#{@school['message']}"
   end
