7be4f7efbec9fc8b3a14e0f46feaea9d339f1612
Guy Harwood <guy@milkchip.com>
2024-04-11 11:46:38 +0100

    fix 'no school teacher' population query

--- db/seeds/20240322111548.custom.teacher-per-school.sql
+++ db/seeds/20240322111548.custom.teacher-per-school.sql
@@ -1,11 +1,9 @@
 INSERT INTO mtc_admin.[user] (identifier, passwordHash, school_id, role_id, displayName)
 SELECT
-    'teacher' + CAST(teacherId as nvarchar) as [identifier],
+    'teacher' + CAST(schools.urn AS NVARCHAR) as [identifier],
     '$2a$10$.WsawgZpWSAQVaa6Vz3P1.XO.1YntYJLd6Da5lrXCAkVxhhLpkOHK' as [passwordHash],
     schools.school_id,
     3 as [role_id],
-    'teacher' + CAST(teacherId as nvarchar) as [displayName]
-FROM
-    (SELECT ROW_NUMBER() OVER(ORDER BY id) AS teacherId, id as [school_id]
-        FROM mtc_admin.school) as schools
-WHERE NOT EXISTS (SELECT 1 FROM mtc_admin.[user] WHERE [identifier] = 'teacher' + CAST(teacherId as nvarchar))
+    'teacher' + CAST(schools.urn AS NVARCHAR) as [displayName]
+FROM (SELECT id as school_id, urn FROM mtc_admin.school where id NOT IN
+  (SELECT school_id FROM mtc_admin.[user] WHERE school_id IS NOT NULL AND role_id=3)) as schools
