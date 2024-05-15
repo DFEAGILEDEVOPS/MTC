1ffe5ec42b103f79cf4110bd7719835eb3046872
Guy Harwood <guy@milkchip.com>
2024-05-15 18:01:53 +0100

    request static IP for container

--- deploy/build-agent/create-instance.sh
+++ deploy/build-agent/create-instance.sh
@@ -8,4 +8,4 @@ AZP_URL=$4
 
 az container create -g $RESOURCE_GROUP --name $AGENT_NAME --image stamtc/devops-build-agent:latest \
   --environment-variables AZP_TOKEN=$AZP_TOKEN AZP_AGENT_NAME=$AGENT_NAME AZP_POOL=MTC AZP_URL=$AZP_URL \
-  --cpu 4 --memory 8
+  --cpu 4 --memory 8 --ip-address Public
