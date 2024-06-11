56498d7081a78f50b6314f7b362d6a8a6721e6ea
Guy Harwood <guy@milkchip.com>
2024-06-11 12:07:48 +0100

    wire up durable functions and start sketching out skeleton poc

--- func-ps-durable/src/functions/psMain.ts
+++ func-ps-durable/src/functions/psMain.ts
@@ -4,6 +4,7 @@ import { type ActivityHandler, OrchestrationContext, type OrchestrationHandler }
 
 const activityName = 'psMain';
 
+// define the durable functions....
 const psMainOrchestrator: OrchestrationHandler = function* (context: OrchestrationContext) {
     const outputs = [];
     outputs.push(yield context.df.callActivity(activityName, 'Tokyo'));
@@ -12,23 +13,25 @@ const psMainOrchestrator: OrchestrationHandler = function* (context: Orchestrati
 
     return outputs;
 };
-df.app.orchestration('psMainOrchestrator', psMainOrchestrator);
 
 const psMain: ActivityHandler = (input: string): string => {
-    return `Hello, ${input}`;
+  return `Hello, ${input}`;
 };
+
+// register the durable functions
+df.app.orchestration('psMainOrchestrator', psMainOrchestrator);
 df.app.activity(activityName, { handler: psMain });
 
+// define the main trigger / input binding
 const psMainHttpStart: HttpHandler = async (request: HttpRequest, context: InvocationContext): Promise<HttpResponse> => {
     const client = df.getClient(context);
     const body: unknown = await request.text();
     const instanceId: string = await client.startNew(request.params.orchestratorName, { input: body });
-
     context.log(`Started orchestration with ID = '${instanceId}'.`);
-
     return client.createCheckStatusResponse(request, instanceId);
 };
 
+// register the main trigger and wire in the orchestrator
 app.http('psMainHttpStart', {
     route: 'orchestrators/{orchestratorName}',
     extraInputs: [df.input.durableClient()],
