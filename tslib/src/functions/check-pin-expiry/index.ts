50bdc931d916ffe5a7b114c32a69626d8d54780b
Guy Harwood <guy@milkchip.com>
2024-07-09 09:50:10 +0100

    receiver and pin expiry function v4 refactor

--- tslib/src/functions/check-pin-expiry/index.ts
+++ tslib/src/functions/check-pin-expiry/index.ts
@@ -1,10 +1,15 @@
-import { type AzureFunction, type Context } from '@azure/functions'
+import { app, type Timer, type InvocationContext } from '@azure/functions'
 import { performance } from 'perf_hooks'
 import { SqlService } from '../../sql/sql.service'
 import { CheckPinExpiryService } from './check-pin-expiry.service'
 const functionName = 'check-pin-expiry'
 
-const timerTrigger: AzureFunction = async function (context: Context): Promise<void> {
+app.timer('checkPinExpiryTrigger', {
+  schedule: '0 10 18,4 * * *', // sec, min, hour, day, month, dow
+  handler: checkPinExpiryTrigger
+})
+
+export async function checkPinExpiryTrigger (timer: Timer, context: InvocationContext): Promise<void> {
   const start = performance.now()
   const checkPinExpiryService = new CheckPinExpiryService(new SqlService())
   await checkPinExpiryService.process()
@@ -13,5 +18,3 @@ const timerTrigger: AzureFunction = async function (context: Context): Promise<v
   const timeStamp = new Date().toISOString()
   context.log(`${functionName}: ${timeStamp} run complete: ${durationInMilliseconds} ms`)
 }
-
-export default timerTrigger
--- tslib/src/functions/check-receiver-sb/index.ts
+++ tslib/src/functions/check-receiver-sb/index.ts
@@ -1,16 +1,37 @@
-import { type AzureFunction, type Context } from '@azure/functions'
+import { app, output, type InvocationContext } from '@azure/functions'
 import { performance } from 'perf_hooks'
 import { CheckReceiverServiceBus } from './check-receiver-sb'
 import { type SubmittedCheckMessage } from '../../schemas/models'
 import { SubmittedCheckVersion } from '../../schemas/SubmittedCheckVersion'
 
 const functionName = 'check-receiver-sb'
+const checkValidationQueueName = 'check-validation'
+const checkNotificationQueueName = 'check-notification'
+const checkSubmissionQueueName = 'check-submission'
 
-const queueTrigger: AzureFunction = async function (context: Context, submittedCheck: SubmittedCheckMessage): Promise<void> {
+const checkValidationOutputQueue = output.serviceBusQueue({
+  connection: 'AZURE_SERVICE_BUS_CONNECTION_STRING',
+  queueName: checkValidationQueueName
+})
+
+const checkNotificationOutputQueue = output.serviceBusQueue({
+  connection: 'AZURE_SERVICE_BUS_CONNECTION_STRING',
+  queueName: checkNotificationQueueName
+})
+
+app.serviceBusQueue('serviceBusQueueTrigger', {
+  connection: 'AZURE_SERVICE_BUS_CONNECTION_STRING',
+  queueName: checkSubmissionQueueName,
+  handler: serviceBusQueueTrigger,
+  extraOutputs: [checkValidationOutputQueue, checkNotificationOutputQueue]
+})
+
+export async function serviceBusQueueTrigger (triggerMessage: unknown, context: InvocationContext): Promise<void> {
   const start = performance.now()
+  const submittedCheck = triggerMessage as SubmittedCheckMessage
   const version = submittedCheck.version
   const expectedVersion = SubmittedCheckVersion.V3
-  context.log.info(`${functionName}: version:${version} message received for checkCode ${submittedCheck.checkCode}`)
+  context.info(`${functionName}: version:${version} message received for checkCode ${submittedCheck.checkCode}`)
   const receiver = new CheckReceiverServiceBus()
   try {
     if (version.toString() !== expectedVersion.toString()) {
@@ -23,7 +44,7 @@ const queueTrigger: AzureFunction = async function (context: Context, submittedC
     if (error instanceof Error) {
       errorMessage = error.message
     }
-    context.log.error(`${functionName}: ERROR: ${errorMessage}`)
+    context.error(`${functionName}: ERROR: ${errorMessage}`)
     throw error
   }
 
@@ -32,5 +53,3 @@ const queueTrigger: AzureFunction = async function (context: Context, submittedC
   const timeStamp = new Date().toISOString()
   context.log(`${functionName}: ${timeStamp} run complete: ${durationInMilliseconds} ms`)
 }
-
-export default queueTrigger
