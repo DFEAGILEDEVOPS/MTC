f2aa9248a36b45374f0a6645a0d2bdc5dfb51f3d
Jon Shipley <js@ovix.co.uk>
2024-07-09 19:34:14 +0000

    Mover vso migration scripts to build-defs folder

--- deploy/build-defs/vso-migrate-down.sh
+++ deploy/build-defs/vso-migrate-down.sh
@@ -3,6 +3,7 @@
 # $1 is the SQL_ADMIN_USER
 # $2 is the SQL_ADMIN_USER_PASSWORD
 # $3 is the SQL_APP_USER_PASSWORD
+# $4 is the SQL_DATABASE (database name)
 cd $BUILD_REPOSITORY_LOCALPATH
 cd db
 
@@ -10,5 +11,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm use
 
-yarn install --frozen-lockfile --production
-SQL_ADMIN_USER=$1 SQL_ADMIN_USER_PASSWORD=$2 SQL_APP_USER_PASSWORD=$3 yarn migrate
+SQL_ADMIN_USER=$1 SQL_ADMIN_USER_PASSWORD=$2 SQL_APP_USER_PASSWORD=$3 SQL_DATABASE=$4 yarn migrate 0
--- deploy/sql/anonymise-sql-data/school-anon.sql
+++
@@ -1,6 +0,0 @@
-UPDATE mtc_admin.school SET 
-    [name]=CAST(NEWID() AS VARCHAR(255)), 
-    leaCode=id, 
-    estabCode=id, 
-    urn=id, 
-    dfeNumber=CAST(CAST(leaCode AS NVARCHAR) + CAST(estabCode AS NVARCHAR) AS int)
