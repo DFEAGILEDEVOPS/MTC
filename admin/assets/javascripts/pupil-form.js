1a1884b0b244d57eac2c4f2e291c205331f5e121
Jon Shipley <js@ovix.co.uk>
2024-08-12 18:06:17 +0000

    remove debug

--- admin/assets/javascripts/pupil-form.js
+++ admin/assets/javascripts/pupil-form.js
@@ -25,9 +25,7 @@ if (!window.MTCAdmin) {
 
     function displayAgeTextArea () {
       var composedDate = $('#dob-year').val() + '-' + $('#dob-month').val().padStart(2, '0') + '-' + $('#dob-day').val().padStart(2, '0')
-      console.log('composed date', composedDate)
       var academicYear = window.MTCAdmin.determineAcademicYear()
-      console.log('Academic year', academicYear)
       var inputDate
       if (/^\d{4}-\d{2}-\d{2}$/.test(composedDate)) {
         inputDate = new Date(Date.UTC(
@@ -40,17 +38,14 @@ if (!window.MTCAdmin) {
           0)
         )
       }
-      console.log('inputDate', inputDate?.toUTCString())
       if (inputDate !== undefined &&
         (!window.MTCAdmin.isWithinAcademicYear(inputDate, academicYear, 8)) &&
         $('#dob-year').val().length === 4) {
-        console.log('Showing age warning')
         // Show age consent
         $('#js-age-warning').addClass('show-age-content')
         $('#js-age-warning').removeClass('hide-age-content')
       } else if ($('.show-age-content').length > 0) {
         // hide age consent
-        console.log('Hide age warning')
         $('#js-age-warning').addClass('hide-age-content')
         $('#js-age-warning').removeClass('show-age-content')
       }
@@ -62,9 +57,6 @@ if (!window.MTCAdmin) {
     var currentYear = (currentDate).getUTCFullYear()
     var startOfJanuary = new Date(Date.UTC(currentYear, 0, 1, 0, 0, 0))
     var endOfAugust = new Date(Date.UTC(currentYear, 7, 31, 23, 59, 59, 0))
-    console.log('start of jan', startOfJanuary)
-    console.log('end of aug', endOfAugust)
-    console.log('current date', currentDate)
     if (currentDate >= startOfJanuary && currentDate <= endOfAugust) {
       return currentYear - 1
     }
@@ -75,8 +67,6 @@ if (!window.MTCAdmin) {
     var targetAcademicYear = academicYear - targetYear
     const startDate = new Date(Date.UTC(targetAcademicYear, 8, 2, 0, 0, 0)) // 2 Sep
     const endDate = new Date(Date.UTC(targetAcademicYear + 1, 8, 1, 23, 59, 59)) // 1 Sep
-    console.log('start date: ', startDate.toUTCString())
-    console.log('end Date: ', endDate.toUTCString())
     return inputDate >= startDate && inputDate <= endDate
   }
 })()
