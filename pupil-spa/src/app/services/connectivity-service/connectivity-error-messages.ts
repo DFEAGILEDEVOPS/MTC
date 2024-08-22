ae21bc230296ce6863371e56e66e9f3100a418c1
Guy Harwood <guy@milkchip.com>
2024-08-22 10:27:59 +0100

    remove connectivity check

--- pupil-spa/src/app/services/connectivity-service/connectivity-error-messages.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  pupilAuthError: 'There is a problem connecting to the pupil authentication service',
-  testQueueError: 'There is a problem connecting to the queue submission service',
-  combinedError: 'There is a problem connecting to this service',
-  settingsError: 'There was a problem getting the settings'
-};
