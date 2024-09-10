b54d2078cc076f3d254b32312e9f8771db1bade2
Guy Harwood <guy@milkchip.com>
2024-09-10 14:37:50 +0100

    try with storage account key instead of cli login context

--- deploy/storage/delete-old-blobs.sh
+++ deploy/storage/delete-old-blobs.sh
@@ -2,6 +2,7 @@
 
 # Set the resource group and storage account name (you can also loop through multiple accounts if needed)
 STORAGE_ACCOUNT_NAME=$1
+STORAGE_ACCOUNT_KEY=$2
 
 # Get the current date in UTC and subtract 7 days
 CUTOFF_DATE=$(date -u -v-7d "+%Y-%m-%d")
@@ -13,7 +14,7 @@ delete_old_blobs() {
     echo "Checking container: $container_name in storage account: $STORAGE_ACCOUNT_NAME"
 
     # List all blobs in the container with the --include 'metadata' option to get last access time
-    blobs=$(az storage blob list --account-name "$STORAGE_ACCOUNT_NAME" --auth-mode login --container-name "$container_name" --query "[?properties.lastAccessedOn < '$CUTOFF_DATE'].name" --output tsv)
+    blobs=$(az storage blob list --account-name "$STORAGE_ACCOUNT_NAME" --account-key $STORAGE_ACCOUNT_KEY --container-name "$container_name" --query "[?properties.lastAccessedOn < '$CUTOFF_DATE'].name" --output tsv)
 
     if [[ -z "$blobs" ]]; then
         echo "No blobs found older than 7 days in container $container_name"
@@ -21,22 +22,22 @@ delete_old_blobs() {
         echo "Deleting blobs not accessed since $CUTOFF_DATE in container $container_name"
         for blob in $blobs; do
             echo "Deleting blob: $blob"
-            az storage blob delete --account-name "$STORAGE_ACCOUNT_NAME" --auth-mode login --container-name "$container_name" --name "$blob"
+            az storage blob delete --account-name "$STORAGE_ACCOUNT_NAME" --account-key $STORAGE_ACCOUNT_KEY --container-name "$container_name" --name "$blob"
         done
     fi
 
     # Check if the container is empty
-    blobs=$(az storage blob list --container-name $container --account-name "$STORAGE_ACCOUNT_NAME" --auth-mode login --query "[].name" -o tsv)
+    blobs=$(az storage blob list --container-name $container --account-name "$STORAGE_ACCOUNT_NAME" --account-key $STORAGE_ACCOUNT_KEY --query "[].name" -o tsv)
 
     if [ -z "$blobs" ]
     then
       echo "Deleting empty container: $container"
-      az storage container delete --name $container --account-name "$STORAGE_ACCOUNT_NAME" --auth-mode login
+      az storage container delete --name $container --account-name "$STORAGE_ACCOUNT_NAME" --account-key $STORAGE_ACCOUNT_KEY
     fi
 }
 
 # Get the list of containers in the storage account
-containers=$(az storage container list --account-name "$STORAGE_ACCOUNT_NAME" --auth-mode login --query "[].name" --output tsv)
+containers=$(az storage container list --account-name "$STORAGE_ACCOUNT_NAME" --account-key $STORAGE_ACCOUNT_KEY --query "[].name" --output tsv)
 
 # Loop through each container and process blobs
 for container in $containers; do
