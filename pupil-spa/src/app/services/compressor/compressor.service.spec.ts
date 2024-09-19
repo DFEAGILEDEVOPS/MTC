c56f9800358565295006cceb4234fa559abd9f88
Jon Shipley <js@ovix.co.uk>
2024-09-19 15:23:38 +0000

    Add gzip compression/decompression function Add tracing

--- pupil-spa/src/app/services/compressor/compressor.service.spec.ts
+++ pupil-spa/src/app/services/compressor/compressor.service.spec.ts
@@ -1,4 +1,5 @@
 import { CompressorService } from './compressor.service';
+import { strFromU8, strToU8, gunzipSync } from 'fflate';
 
 describe('CompressorService', () => {
 
@@ -12,4 +13,20 @@ describe('CompressorService', () => {
       expect(typeof(result)).toBe('string');
     });
   });
+
+  describe('gzip compression', () => {
+    const obj = {
+      one: [1, 2, 3],
+      two: {
+        subOne: { this: 'that' },
+        subTwo: { foo: 99.99, bar: 42 },
+        flag: false
+      }
+    }
+    it('compressToGzip works', () => {
+      const b64Comp = CompressorService.compressToGzip(JSON.stringify(obj))
+      const decomp = CompressorService.decompressFromGzip(b64Comp)
+      expect(decomp).toEqual(obj)
+    })
+  })
 });
--- pupil-spa/src/app/services/submission/submission.service.ts
+++ pupil-spa/src/app/services/submission/submission.service.ts
@@ -3,22 +3,27 @@ import { HttpHeaders } from '@angular/common/http'
 import { HttpService } from '../http/http.service'
 import { Injectable } from '@angular/core'
 import { CompressorService } from '../compressor/compressor.service'
+import { ApplicationInsightsService } from '../app-insights/app-insights.service'
 
 @Injectable()
 export class SubmissionService {
-
-  public static readonly SubmittedCheckVersion3 = 3
-  constructor(private http: HttpService) {}
+  public static readonly SubmittedCheckVersion4 = 4
+  constructor(private http: HttpService, private appInsightsService: ApplicationInsightsService) {}
 
   async submit(payload: any): Promise<any> {
+    const jsonPayload = JSON.stringify(payload)
     const submissionUrl = payload.tokens.checkSubmission.url
+    this.appInsightsService.trackTrace(`Check [${payload?.checkCode}] for school [${payload?.school?.uuid}] raw size ${jsonPayload.length}`)
     const jwtToken = payload.tokens.checkSubmission.token
+    // Compressing to gzip gives us a smaller payload to transmit across the network, as well as a better payload sizes for huge payloads.
+    const gzipArchive = CompressorService.compressToGzip(jsonPayload)
     const postBody = {
-      archive: CompressorService.compressToBase64(JSON.stringify(payload)),
-      version: SubmissionService.SubmittedCheckVersion3,
+      archive: gzipArchive,
+      version: SubmissionService.SubmittedCheckVersion4,
       checkCode: payload.checkCode,
       schoolUUID: payload.school.uuid
     }
+    this.appInsightsService.trackTrace(`Check [${payload?.checkCode}] for school [${payload?.school?.uuid}] final payload length: ${gzipArchive.length}`)
     await this.http.post(submissionUrl, postBody,
       new HttpHeaders()
       .set('Content-Type', 'application/json')
