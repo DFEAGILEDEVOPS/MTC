6422ab970fb3cb70b8796f831e49587f98d4b602
Guy Harwood <guy@milkchip.com>
2024-09-24 14:12:08 +0100

    update powershell package url

--- deploy/build-agent/build-push-registry.sh
+++ deploy/build-agent/build-push-registry.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+REGISTRY_NAME=$1
+
 GIT_STATUS_OUTPUT=$(git status ./Dockerfile --porcelain)
 if [ -n "$GIT_STATUS_OUTPUT" ]; then
   echo "There are uncommitted changes to the Dockerfile.
@@ -9,5 +11,5 @@ if [ -n "$GIT_STATUS_OUTPUT" ]; then
 fi
 
 GIT_COMMIT_SHORT=$(git log -1 --format=%h)
-docker build -t stamtc/devops-build-agent:latest -t stamtc/devops-build-agent:$GIT_COMMIT_SHORT --platform linux/amd64 .
-docker push --all-tags stamtc/devops-build-agent
+docker build -t $REGISTRY_NAME.azurecr.io/devops-build-agent:latest -t $REGISTRY_NAME.azurecr.io/devops-build-agent:$GIT_COMMIT_SHORT --platform linux/amd64 .
+docker push --all-tags $REGISTRY_NAME.azurecr.io/devops-build-agent:latest
