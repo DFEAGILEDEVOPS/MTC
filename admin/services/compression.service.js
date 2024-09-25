863adad4abdf20aa3446fa16e4861a27f99ddc8e
Jon Shipley <js@ovix.co.uk>
2024-09-25 08:39:37 +0000

    Fix tech-support

--- admin/services/compression.service.js
+++ admin/services/compression.service.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const lzString = require('lz-string')
+const fflate = require('fflate')
 
 const moduleToExport = {
   /**
@@ -37,6 +38,31 @@ const moduleToExport = {
    */
   decompressFromBase64: function decompress (string) {
     return lzString.decompressFromBase64(string)
+  },
+
+  /**
+   *
+   * @param data stringified object to compress
+   * @returns base64 encoded string after gzip compression
+   */
+   compressToGzip: function compressToGzip (data) {
+    const comp = fflate.gzipSync(strToU8(data), { level: 8, mem: 8 })
+    const b64 = btoa(fflate.strFromU8(comp, true))
+    return b64
+  },
+
+  /**
+   *
+   * @param data base64 encoded gzip data
+   * @returns a stringified object (e.g. the payload)
+   */
+  decompressFromGzip: function  decompressFromGzip (b64Data) {
+    // reverse base64 encoding
+    const comp = atob(b64Data)
+    // decompress gzip
+    const uncompData = fflate.gunzipSync(fflate.strToU8(comp, true))
+    const uncomp = fflate.strFromU8(uncompData)
+    return uncomp
   }
 }
 
