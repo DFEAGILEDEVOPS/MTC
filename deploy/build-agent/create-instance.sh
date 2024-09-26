44a73b8ededbc20267b31a2a734c190fa674be12
Guy Harwood <GuyHarwood@users.noreply.github.com>
2024-09-26 12:23:05 +0100

    bug/65481 build agent image targets azure container registry (#2922)
    * update powershell package url
    
    * put back url
    
    * remove tag when pushing all tags
    
    * add authentication info
    
    * use acr login instead of core docker

--- deploy/build-agent/create-instance.sh
+++ deploy/build-agent/create-instance.sh
@@ -5,7 +5,8 @@ AGENT_NAME=$1
 RESOURCE_GROUP=$2
 AZP_TOKEN=$3
 AZP_URL=$4
+REGISTRY=$5
 
-az container create -g $RESOURCE_GROUP --name $AGENT_NAME --image stamtc/devops-build-agent:latest \
+az container create -g $RESOURCE_GROUP --name $AGENT_NAME --image $REGISTRY_NAME.azurecr.io/devops-build-agent:latest \
   --environment-variables AZP_TOKEN=$AZP_TOKEN AZP_AGENT_NAME=$AGENT_NAME AZP_POOL=MTC AZP_URL=$AZP_URL \
   --cpu 4 --memory 8
