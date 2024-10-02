259cbb74a11ba62d70a5ed6e2f4a832104953262
Guy Harwood <guy@milkchip.com>
2024-10-02 17:53:47 +0100

    fix bugs

--- deploy/storage/delete-old-blobs.sh
+++ deploy/storage/delete-old-blobs.sh
@@ -21,7 +21,7 @@ delete_old_blobs() {
     echo "Checking container: $container_name in storage account: $STORAGE_ACCOUNT_NAME"
 
     # List all blobs in the container with the --include 'metadata' option to get last access time
-    blobs=$(az storage blob list --account-name "$STORAGE_ACCOUNT_NAME" --account-key $STORAGE_ACCOUNT_KEY --container-name "$container_name" --query "[?properties.lastAccessedOn < '$CUTOFF_DATE'].name" --output tsv)
+    blobs=$(az storage blob list --account-name "$STORAGE_ACCOUNT_NAME" --account-key "$STORAGE_ACCOUNT_KEY" --container-name "$container_name" --query "[?properties.creationTime < '$CUTOFF_DATE'].name" --output tsv)
 
     if [[ -z "$blobs" ]]; then
         echo "No blobs found older than 1 days in container $container_name"
@@ -29,20 +29,20 @@ delete_old_blobs() {
         echo "Deleting blobs not accessed since $CUTOFF_DATE in container $container_name"
         for blob in $blobs; do
             echo "Deleting blob: $blob"
-            az storage blob delete --account-name "$STORAGE_ACCOUNT_NAME" --account-key $STORAGE_ACCOUNT_KEY --container-name "$container_name" --name "$blob"
+            az storage blob delete --account-name "$STORAGE_ACCOUNT_NAME" --account-key "$STORAGE_ACCOUNT_KEY" --container-name "$container_name" --name "$blob"
         done
     fi
 
     # Check if the container is empty
-    blobs=$(az storage blob list --container-name $container --account-name "$STORAGE_ACCOUNT_NAME" --account-key $STORAGE_ACCOUNT_KEY --query "[].name" -o tsv)
+    blobs=$(az storage blob list --container-name "$container_name" --account-name "$STORAGE_ACCOUNT_NAME" --account-key "$STORAGE_ACCOUNT_KEY" --query "[].name" -o tsv)
 
     if [ -z "$blobs" ]
     then
       echo "Deleting empty container: $container"
-      az storage container delete --name $container --account-name "$STORAGE_ACCOUNT_NAME" --account-key $STORAGE_ACCOUNT_KEY
+      az storage container delete --name "$container_name" --account-name "$STORAGE_ACCOUNT_NAME" --account-key "$STORAGE_ACCOUNT_KEY"
     fi
 }
 
-delete_old_blobs $CONTAINER_NAME
+delete_old_blobs "$CONTAINER_NAME"
 
 echo "Completed deletion of old blobs in $CONTAINER_NAME container."
