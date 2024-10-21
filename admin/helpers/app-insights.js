0697552371dc4575f8100199bc56bb07e2c44b0d
Jon Shipley <js@ovix.co.uk>
2024-10-21 10:20:36 +0000

    [admin] add instanceId

--- admin/helpers/app-insights.js
+++ admin/helpers/app-insights.js
@@ -23,6 +23,7 @@ const startInsightsIfConfigured = () => {
       .setInternalLogging(false, true)
       .enableWebInstrumentation(false)
     appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = cloudRoleName
+    appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRoleInstance] = config.Monitoring.ApplicationInsights.InstanceId
     appInsights.start()
 
     // Commented out in ticket #65031 as part of maintenance upgrades
