56db3cd0f2c55d3aef4331773033c392a792a48b
Guy Harwood <GuyHarwood@users.noreply.github.com>
2024-11-11 13:52:03 +0000

    bug/65881 SPA crashes without app insights configured (#2963)
    * check for empty string instead of undefined.  as never undefined
    
    * extract logic to service and include undefined check
    
    ---------
    
    Co-authored-by: Mohsen Qureshi <activemq@users.noreply.github.com>

---
+++ pupil-spa/src/app/string-util.spec.ts
@@ -0,0 +1,17 @@
+import { stringUtil } from './string-util'
+
+sut: stringUtil
+
+describe('string-util', () => {
+  it('should return true for undefined', () => {
+    expect(stringUtil.isEmptyOrUndefined(undefined)).toBe(true)
+  })
+
+  it('should return true for empty string', () => {
+    expect(stringUtil.isEmptyOrUndefined('')).toBe(true)
+  })
+
+  it('should return false for non-empty string', () => {
+    expect(stringUtil.isEmptyOrUndefined('a')).toBe(false)
+  })
+})
