e5ec7724ad2a06c348c547c3baad81d5f9ad47e5
Guy Harwood <guy@milkchip.com>
2024-11-22 15:20:59 +0000

    POC

---
+++ z_spikes/66011-storage-queue-bug/index.js
@@ -0,0 +1,52 @@
+const { QueueServiceClient, QueueSASPermissions } = require('@azure/storage-queue');
+const queueName = 'guy-poc-66011-bug';
+// add dotenv to load environment variables
+require('dotenv').config();
+
+async function sendMessage() {
+  const queueServiceClient = QueueServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
+  const queueClient = queueServiceClient.getQueueClient(queueName);
+  await queueClient.createIfNotExists();
+  const permissions = new QueueSASPermissions()
+  const startDate = new Date(2024, 10, 1)
+  const endDate = new Date(2024, 10, 25)
+  console.log(`GUY: startDate: ${startDate}, endDate: ${endDate}`)
+  permissions.add = true
+  const sasUrl = queueClient.generateSasUrl({
+    permissions,
+    startsOn: startDate,
+    expiresOn: endDate
+  })
+  const parts = sasUrl.split('?')
+    const tokenObject = {
+      token: parts[1],
+      url: parts[0],
+      queueName
+    }
+  console.dir(tokenObject)
+  const fullUrl = getFullUrl(tokenObject)
+  console.log(`GUY: fullUrl: ${fullUrl}`)
+  const newClient = new QueueServiceClient(fullUrl)
+  const newQueueClient = newClient.getQueueClient(queueName)
+  await newQueueClient.sendMessage('Hello, world!');
+}
+
+const getFullUrl = (token) => {
+  return `${token.url.replace(`/${queueName}`, '')}?${token.token}`
+}
+
+async function main () {
+  try {
+    await sendMessage()
+  } catch (error) {
+    console.error(error.message)
+    process.exit(1)
+  }
+  console.log(`sent message to queue successfully`)
+  process.exit(0)
+}
+
+main()
+  .then(() => {})
+  .catch(err => console.error(err))
+
