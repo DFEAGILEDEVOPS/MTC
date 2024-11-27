e79019aed5f534496a905276829ace880f758e38
Jon Shipley <jon-shipley@users.noreply.github.com>
2024-11-27 16:17:25 +0000

    Feature/58891 addition of ps report for annulled pupils (#2964)
    * fix: upgrade sass from 1.78.0 to 1.79.4
    
    Snyk has created this PR to upgrade sass from 1.78.0 to 1.79.4.
    
    See this package in yarn:
    sass
    
    See this project in Snyk:
    https://app.snyk.io/org/mtc/project/c7841ec1-118f-443b-9be4-7adb6042d775?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * fix: upgrade @babel/runtime from 7.25.6 to 7.25.7
    
    Snyk has created this PR to upgrade @babel/runtime from 7.25.6 to 7.25.7.
    
    See this package in yarn:
    @babel/runtime
    
    See this project in Snyk:
    https://app.snyk.io/org/mtc/project/c7841ec1-118f-443b-9be4-7adb6042d775?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * fix: func-ps-report/package.json & func-ps-report/yarn.lock to reduce vulnerabilities
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-JS-COOKIE-8163060
    
    * fix: func-throttled/package.json & func-throttled/yarn.lock to reduce vulnerabilities
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-JS-COOKIE-8163060
    
    * fix: func-consumption/package.json & func-consumption/yarn.lock to reduce vulnerabilities
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-JS-COOKIE-8163060
    
    * fix: pupil-api/package.json & pupil-api/yarn.lock to reduce vulnerabilities
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-JS-COOKIE-8163060
    
    * fix: tslib/package.json & tslib/yarn.lock to reduce vulnerabilities
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-JS-COOKIE-8163060
    
    * Update ps report for pupils annulled due to maladmin
    
    1) Show previous pupil status and include the latest check in the report
    2) FormMark confirmed as already being set to null
    
    * Remove debug
    
    * Update lint
    Update mock
    
    * fix: upgrade moment-timezone from 0.5.45 to 0.5.46
    
    Snyk has created this PR to upgrade moment-timezone from 0.5.45 to 0.5.46.
    
    See this package in yarn:
    moment-timezone
    
    See this project in Snyk:
    https://app.snyk.io/org/mtc/project/f7ffb5b3-7413-4a3f-8248-238001d182b7?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * fix: upgrade winston from 3.14.2 to 3.15.0
    
    Snyk has created this PR to upgrade winston from 3.14.2 to 3.15.0.
    
    See this package in yarn:
    winston
    
    See this project in Snyk:
    https://app.snyk.io/org/mtc/project/f7ffb5b3-7413-4a3f-8248-238001d182b7?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * Change import to require as it is a ECMAScript module
    
    * bugfix
    
    * Manually update winston to 3.15.0
    
    * Manually bump moment-timezone
    
    * fix: upgrade sass from 1.79.3 to 1.80.3
    
    Snyk has created this PR to upgrade sass from 1.79.3 to 1.80.3.
    
    See this package in yarn:
    sass
    
    See this project in Snyk:
    https://app.snyk.io/org/mtc/project/86296478-3a5b-43ab-9fc6-4b4c5f4833ba?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * fix: upgrade dompurify from 2.5.6 to 2.5.7
    
    Snyk has created this PR to upgrade dompurify from 2.5.6 to 2.5.7.
    
    See this package in yarn:
    dompurify
    
    See this project in Snyk:
    https://app.snyk.io/org/mtc/project/86296478-3a5b-43ab-9fc6-4b4c5f4833ba?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * fix: upgrade axios from 1.7.4 to 1.7.7
    
    Snyk has created this PR to upgrade axios from 1.7.4 to 1.7.7.
    
    See this package in yarn:
    axios
    
    See this project in Snyk:
    https://app.snyk.io/org/activemq/project/d61ff12b-40e9-4b90-b85c-01f926640da1?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * Whitespace changes
    
    * Revert storage-queue to a version that works.
    
    * test updates
    
    ---------
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    Co-authored-by: Mohsen Qureshi <m-qureshi@hotmail.com>
    Co-authored-by: Mohsen Qureshi <activemq@users.noreply.github.com>

--- tslib/src/functions-ps-report/ps-report-2-pupil-data/mocks/pupil-not-attending-annulled-cheating.ts
+++ tslib/src/functions-ps-report/ps-report-2-pupil-data/mocks/pupil-not-attending-annulled-cheating.ts
@@ -1,21 +1,22 @@
 import type { Pupil } from '../pupil-data.models'
 import moment from 'moment'
 
+// An pupil who completed a check and was then annulled for cheating.
 export const pupil: Pupil = {
   checkComplete: false,
-  currentCheckId: 1,
+  currentCheckId: 2,
   dateOfBirth: moment.utc('2012-04-01'),
   forename: 'Tester',
   gender: 'M',
-  id: 3,
+  id: 4,
   isEdited: false,
   jobId: 1,
   lastname: 'Person',
-  notTakingCheckReason: 'Maladministration',
-  notTakingCheckCode: 'ANLLQ',
+  notTakingCheckReason: 'Pupil Cheating',
+  notTakingCheckCode: 'ANLLH',
   middlenames: 'middle name',
   restartAvailable: false,
   slug: 'abc-def-hij-klm',
   schoolId: 3,
-  upn: 'TEST986'
+  upn: 'TEST987'
 }
