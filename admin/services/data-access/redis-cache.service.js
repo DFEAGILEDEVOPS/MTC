930a7516fe07d0c93444b68127265e6f8402d2b4
Jon Shipley <jon-shipley@users.noreply.github.com>
2024-12-06 11:30:35 +0000

    Bug/65582 init app insights error logging asap (#2984)
    * Bump rexml from 3.3.4 to 3.3.9 in /test/admin-hpa
    
    Bumps [rexml](https://github.com/ruby/rexml) from 3.3.4 to 3.3.9.
    - [Release notes](https://github.com/ruby/rexml/releases)
    - [Changelog](https://github.com/ruby/rexml/blob/master/NEWS.md)
    - [Commits](https://github.com/ruby/rexml/compare/v3.3.4...v3.3.9)
    
    ---
    updated-dependencies:
    - dependency-name: rexml
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * fix: upgrade ramda from 0.28.0 to 0.30.1
    
    Snyk has created this PR to upgrade ramda from 0.28.0 to 0.30.1.
    
    See this package in npm:
    ramda
    
    See this project in Snyk:
    https://app.snyk.io/org/mtc/project/f2cd044f-cd02-4438-8249-c21552435905?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * fix: upgrade sass from 1.79.3 to 1.80.4
    
    Snyk has created this PR to upgrade sass from 1.79.3 to 1.80.4.
    
    See this package in yarn:
    sass
    
    See this project in Snyk:
    https://app.snyk.io/org/mtc/project/86296478-3a5b-43ab-9fc6-4b4c5f4833ba?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * fix: upgrade @babel/runtime from 7.25.6 to 7.26.0
    
    Snyk has created this PR to upgrade @babel/runtime from 7.25.6 to 7.26.0.
    
    See this package in yarn:
    @babel/runtime
    
    See this project in Snyk:
    https://app.snyk.io/org/activemq/project/cc9ba2aa-367d-4704-a7a4-c5455f64251f?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * Bump cross-spawn from 7.0.3 to 7.0.6 in /tslib
    
    Bumps [cross-spawn](https://github.com/moxystudio/node-cross-spawn) from 7.0.3 to 7.0.6.
    - [Changelog](https://github.com/moxystudio/node-cross-spawn/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/moxystudio/node-cross-spawn/compare/v7.0.3...v7.0.6)
    
    ---
    updated-dependencies:
    - dependency-name: cross-spawn
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * [admin] appInsights mod
    
    Mod appinsights to log early, e.g. due to bad Redis config.
    
    * fix: admin-assets/Dockerfile to reduce vulnerabilities
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-ALPINE319-EXPAT-7908400
    - https://snyk.io/vuln/SNYK-ALPINE319-EXPAT-7908409
    - https://snyk.io/vuln/SNYK-ALPINE319-CURL-7567383
    - https://snyk.io/vuln/SNYK-ALPINE319-CURL-7567383
    - https://snyk.io/vuln/SNYK-ALPINE319-EXPAT-7908399
    
    * Fix to pass tests
    
    * Fix logic error
    
    * Log pupil-api redis errors to App Insights
    
    * refactor name
    
    * whitespace
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>

--- admin/services/data-access/redis-cache.service.js
+++ admin/services/data-access/redis-cache.service.js
@@ -17,9 +17,15 @@ if (config.Redis.useTLS) {
 
 let redis
 
-const redisConnect = () => {
-  if (!redis) {
-    redis = new Redis(redisConfig)
+const redisConnect = async () => {
+  try {
+    if (!redis) {
+      redis = new Redis(redisConfig)
+      // Make a call to check the config is valid.  This is only done once.
+      await redis.info()
+    }
+  } catch (error) {
+    logger.alert('ALERT: REDIS CONNECTION ERROR', error)
   }
 }
 
@@ -31,7 +37,7 @@ const redisCacheService = {}
  * @returns {Promise<any>}
  */
 redisCacheService.get = async key => {
-  redisConnect()
+  await redisConnect()
   try {
     logger.debug(`REDIS (get): retrieving ${key}`)
     const cacheEntry = await redis.get(key)
@@ -50,7 +56,7 @@ redisCacheService.get = async key => {
  * @returns {Promise<void>}
  */
 redisCacheService.set = async (key, value, ttl = undefined) => {
-  redisConnect()
+  await redisConnect()
   try {
     logger.debug(`REDIS (set): adding ${key} ttl:${ttl}`)
     const storageItemString = prepareCacheEntry(value)
@@ -74,7 +80,7 @@ redisCacheService.drop = async (cacheKeys = []) => {
   if (Array.isArray(cacheKeys) && cacheKeys.length === 0) {
     return false
   }
-  redisConnect()
+  await redisConnect()
   if (typeof cacheKeys === 'string') {
     cacheKeys = [cacheKeys]
   }
@@ -103,7 +109,7 @@ redisCacheService.setMany = async (items) => {
   if (!Array.isArray(items)) {
     throw new Error('items is not an array')
   }
-  redisConnect()
+  await redisConnect()
   const multi = redis.multi()
   for (let index = 0; index < items.length; index++) {
     const item = items[index]
@@ -137,7 +143,7 @@ redisCacheService.getMany = async (keys) => {
   if (!Array.isArray(keys)) {
     throw new Error('keys is not an array')
   }
-  redisConnect()
+  await redisConnect()
   const rawData = await redis.mget(...keys)
   const data = rawData.map(raw => unwrap(raw))
   logger.debug(`(redis) getMany ${keys.join(', ')}`)
@@ -157,7 +163,7 @@ redisCacheService.getTtl = async (key) => {
   if (key.length === 0) {
     throw new Error('Invalid key length')
   }
-  redisConnect()
+  await redisConnect()
   return await redis.ttl(key)
 }
 
@@ -168,7 +174,7 @@ redisCacheService.getTtl = async (key) => {
  * @return {Promise<void>}
  */
 redisCacheService.dropByPrefix = async (prefix) => {
-  redisConnect()
+  await redisConnect()
   const cmd = `for i, name in ipairs(redis.call('KEYS', '${prefix}*')) do redis.call('DEL', name); end`
   await redis.eval(cmd, 0)
 }
