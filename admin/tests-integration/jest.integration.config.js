8ca9613809269d5cfcf78c5fa0d2c615111b3016
Jon Shipley <jon-shipley@users.noreply.github.com>
2024-12-11 13:40:34 +0000

    Feature/65231 disable gestures in pupil checks (#2979)
    * Update example env file
    The throwaway JWT token value was too short.  Changed to uuid.
    
    * Disable pull-to-refresh gestures on
    ios and chrome.
    
    * Affects ios 16 and later
    * Chrome
    
    * Fix integration tests

--- admin/tests-integration/jest.integration.config.js
+++ admin/tests-integration/jest.integration.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testMatch: ['./**/(*.)+(spec|test).[jt]s?(x)'],
   testRunner: 'jest-circus', // As of jest 27 this is the legacy mode (slow)
   moduleNameMapper: {
-    axios: 'axios/dist/node/axios.cjs'
+    axios: 'axios/dist/node/axios.cjs',
+    '@opentelemetry/semantic-conventions/incubating': '@opentelemetry/semantic-conventions/build/src/index-incubating.js'
   }
 }
