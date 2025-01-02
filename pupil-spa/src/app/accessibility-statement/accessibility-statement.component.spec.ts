29ae08a9ef8bb31b95a686a3e5eb7d55b91e59f3
Jon Shipley <js@ovix.co.uk>
2025-01-02 15:56:01 +0000

    Fix lint

--- pupil-spa/src/app/accessibility-statement/accessibility-statement.component.spec.ts
+++ pupil-spa/src/app/accessibility-statement/accessibility-statement.component.spec.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
 import { AccessibilityStatementComponent } from './accessibility-statement.component';
