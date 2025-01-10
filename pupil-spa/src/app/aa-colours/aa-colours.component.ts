5c0bb3d8072cbf6cc8d2702728cfaa5804004461
Jon Shipley <js@ovix.co.uk>
2025-01-10 15:35:35 +0000

    Reformat

--- pupil-spa/src/app/aa-colours/aa-colours.component.ts
+++ pupil-spa/src/app/aa-colours/aa-colours.component.ts
@@ -4,27 +4,27 @@ import {
   ElementRef,
   AfterViewInit,
   OnDestroy,
-} from "@angular/core";
-import { Router } from "@angular/router";
-import { AccessArrangementsConfig } from "../access-arrangements";
-import { StorageService } from "../services/storage/storage.service";
-import { RouteService } from "../services/route/route.service";
-import { PupilPrefsService } from "../services/pupil-prefs/pupil-prefs.service";
-import { SpeechService } from "../services/speech/speech.service";
-import { QuestionService } from "../services/question/question.service";
+} from '@angular/core'
+import { Router } from '@angular/router'
+import { AccessArrangementsConfig } from '../access-arrangements'
+import { StorageService } from '../services/storage/storage.service'
+import { RouteService } from '../services/route/route.service'
+import { PupilPrefsService } from '../services/pupil-prefs/pupil-prefs.service'
+import { SpeechService } from '../services/speech/speech.service'
+import { QuestionService } from '../services/question/question.service'
 
 @Component({
-  selector: "app-aa-colours",
-  templateUrl: "./aa-colours.component.html",
-  styleUrls: ["./aa-colours.component.scss"],
+  selector: 'app-aa-colours',
+  templateUrl: './aa-colours.component.html',
+  styleUrls: ['./aa-colours.component.scss'],
   standalone: false,
 })
 export class AAColoursComponent implements OnInit, AfterViewInit, OnDestroy {
-  selectedContrast: string;
-  contrastSettings: object[];
-  accessArrangements: any;
-  backLinkUrl: string;
-  speechListenerEvent: any;
+  selectedContrast: string
+  contrastSettings: object[]
+  accessArrangements: any
+  backLinkUrl: string
+  speechListenerEvent: any
 
   constructor(
     private router: Router,
@@ -35,31 +35,31 @@ export class AAColoursComponent implements OnInit, AfterViewInit, OnDestroy {
     private elRef: ElementRef,
     private speechService: SpeechService
   ) {
-    this.contrastSettings = AccessArrangementsConfig.contrastSettings;
-    this.accessArrangements = this.storageService.getAccessArrangements();
-    this.selectedContrast = this.accessArrangements.contrast || "bow";
+    this.contrastSettings = AccessArrangementsConfig.contrastSettings
+    this.accessArrangements = this.storageService.getAccessArrangements()
+    this.selectedContrast = this.accessArrangements.contrast || 'bow'
   }
 
   ngOnInit() {
-    const validBackLinks = ["/access-settings", "/font-choice"];
+    const validBackLinks = ['/access-settings', '/font-choice']
     if (validBackLinks.indexOf(this.routeService.getPreviousUrl()) !== -1) {
-      this.backLinkUrl = this.routeService.getPreviousUrl();
+      this.backLinkUrl = this.routeService.getPreviousUrl()
     }
   }
 
   selectionChange(selectedContrast: string) {
-    this.selectedContrast = selectedContrast;
+    this.selectedContrast = selectedContrast
   }
 
   onClick() {
-    this.accessArrangements.contrast = this.selectedContrast;
-    this.storageService.setAccessArrangements(this.accessArrangements);
-    this.pupilPrefsService.storePupilPrefs();
+    this.accessArrangements.contrast = this.selectedContrast
+    this.storageService.setAccessArrangements(this.accessArrangements)
+    this.pupilPrefsService.storePupilPrefs()
 
-    if (this.routeService.getPreviousUrl() === "/access-settings") {
-      this.router.navigate(["access-settings"]);
+    if (this.routeService.getPreviousUrl() === '/access-settings') {
+      this.router.navigate(['access-settings'])
     } else {
-      this.router.navigate(["sign-in-success"]);
+      this.router.navigate(['sign-in-success'])
     }
   }
 
@@ -68,30 +68,30 @@ export class AAColoursComponent implements OnInit, AfterViewInit, OnDestroy {
     if (this.questionService.getConfig().questionReader) {
       this.speechService.speakElement(this.elRef.nativeElement).then(() => {
         this.speechService.focusEndOfSpeech(
-          this.elRef.nativeElement.querySelector("#next")
-        );
-      });
+          this.elRef.nativeElement.querySelector('#next')
+        )
+      })
 
       this.speechListenerEvent = this.elRef.nativeElement.addEventListener(
-        "focus",
+        'focus',
         (event: any) => {
-          this.speechService.focusEventListenerHook(event);
+          this.speechService.focusEventListenerHook(event)
         },
         true
-      );
+      )
     }
   }
 
   async ngOnDestroy(): Promise<void> {
     // stop the current speech process if the page is changed
     if (this.questionService.getConfig().questionReader) {
-      await this.speechService.cancel();
+      await this.speechService.cancel()
 
       this.elRef.nativeElement.removeEventListener(
-        "focus",
+        'focus',
         this.speechListenerEvent,
         true
-      );
+      )
     }
   }
 }
