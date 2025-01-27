3ca7bab63845eebd8823f6c672fd255dd2956b87
Jon Shipley <js@ovix.co.uk>
2025-01-27 15:12:47 +0000

    Remove lz-string  * lint

--- pupil-spa/src/app/services/feedback/feedback.service.mock.ts
+++ pupil-spa/src/app/services/feedback/feedback.service.mock.ts
@@ -3,5 +3,5 @@ import { IFeedbackService } from './feedback.service';
 export class FeedbackServiceMock implements IFeedbackService {
   /* eslint-disable @typescript-eslint/no-unused-vars */
   async postFeedback(): Promise<boolean> { return true }
-  async submitFeedback(payload: Object): Promise<void> { return }
+  async submitFeedback(payload: object): Promise<void> { return }
 }
