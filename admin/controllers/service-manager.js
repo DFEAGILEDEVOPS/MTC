aa38522c7f5a5c681c9142245576bfed11001572
Martin Carson <martinc@kainos.com>
2026-08-11 13:44:09 +0100

    Added a trim to strip all non numeric characters so only numbers are searched for in the DB

--- admin/controllers/service-manager.js
+++ admin/controllers/service-manager.js
@@ -407,7 +407,9 @@ const controller = {
       }
       let school
       try {
-        school = await schoolService.searchForSchool(parseInt(req.body.q?.trim(), 10))
+        const stripNonDigitsRegex = /[^0-9]/g
+        const sanitisedDfeNumber = parseInt(req.body.q?.trim().replace(stripNonDigitsRegex, ''), 10)
+        school = await schoolService.searchForSchool(sanitisedDfeNumber)
       } catch {
         return noSchoolFound(req, res, next)
       }
